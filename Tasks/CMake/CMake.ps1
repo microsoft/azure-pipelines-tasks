8e3c4d0480546502addff54c9b78d3d9e510c1c4
Chris Patterson <chrispat@microsoft.com>
2014-11-13 09:53:39 -0500

    Fixing CMAke powershell to align with xplat changes

--- Tasks/CMake/CMake.ps1
+++ Tasks/CMake/CMake.ps1
@@ -1,61 +1,41 @@
 ﻿param (
-	[string]$srcRoot,
-	[string]$buildDirName,
+	[string]$cwd,
 	[string]$args
 )
 
 Write-Verbose 'Entering CMake.ps1'
-Write-Verbose "srcRoot = $srcRoot"
-Write-Verbose "buildDirName = $buildDirName"
+Write-Verbose "cwd = $cwd"
 Write-Verbose "args = $args"
 
-if(!$srcRoot)
+if(!$cwd)
 {
-    throw "srcRoot parameter is not set"
+    throw "cwd parameter is not set"
 }
 
-if(!(Test-Path $srcRoot -PathType Container))
+if(!(Test-Path $cwd -PathType Container))
 {
-    throw "srcRoot does not exist"
+	Write-Verbose "Creating directory $cwd"
+    New-Item -Path $cwd -ItemType Container
 }
 
 try
 {
 	$cmake = Get-Command cmake.exe
+	Write-Verbose "Using $cmake.Source"
 }
 catch
 {
 	throw 'Unable to find cmake.exe'
 }
 
-Set-Location $srcRoot
+Write-Verbose "Setting working directory to $cwd"
+Set-Location $cwd
 
-$buildPath = Join-Path -Path $srcRoot -ChildPath $buildDirName
-
-if(!(Test-Path $buildPath -PathType Container))
-{
-    New-Item -Path $buildPath -ItemType Container
-}
-
-Set-Location $buildPath
 
 # Import the Task.Common dll that has all the cmdlets we need for Build
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 
-Write-Output "Building from $buildPath"
-Write-Output "Generating Files"
-
-Invoke-Tool -Path $cmake.Source -Arguments $srcRoot -WorkingFolder $buildPath
-
-if($args)
-{
-    $args = "--build $args $buildPath"
-}
-else
-{
-    $args = "--build $buildPath"
-}
-
+Write-Verbose "Running CMake..."
 Invoke-Tool -Path $cmake.Source -Arguments $args -WorkingFolder $buildPath
 
 Write-Verbose "Leaving script CMake.ps1"
