931bf3e0f44f82aba404fed09659e63bd89c0329
Jeff Young <jeyou@microsoft.com>
2014-11-14 13:23:26 -0500

    Remove code to allow running straight from command line

--- Tasks/AzurePowerShell/RunAzurePowerShell.ps1
+++ Tasks/AzurePowerShell/RunAzurePowerShell.ps1
@@ -2,15 +2,6 @@
 param
 (
     [String] [Parameter(Mandatory = $true)]
-    $DeploymentEnvironmentName,
-
-    [String] [Parameter(Mandatory = $false)]
-    $CollectionUrl,
-
-    [String] [Parameter(Mandatory = $false)]
-    $TeamProject,
-
-    [String] [Parameter(Mandatory = $true)]
     $ScriptPath,
 
     [String] [Parameter(Mandatory = $true)]
@@ -18,44 +9,6 @@ param
 )
 
 Write-Verbose "Entering script RunAzurePowerShell.ps1"
-
-Write-Verbose "DeploymentEnvironmentName= $DeploymentEnvironmentName"
-Write-Verbose "CollectionUrl= $CollectionUrl"
-Write-Verbose "TeamProject= $TeamProject"
-
-if (!$distributedTaskContext) #running directly via command line
-{
-    # Load the required assemblies before importing the modules
-    $currentLocation = Split-Path -parent $MyInvocation.MyCommand.Definition
-    $assembliesToLoad = "Microsoft.TeamFoundation.DistributedTask.Agent.Interfaces.dll;Microsoft.TeamFoundation.Client.dll;Microsoft.TeamFoundation.DistributedTask.Agent.Strings.dll"
-    $splitOn = ,";"
-    $assemblies = $assembliesToLoad.Split($splitOn, [System.StringSplitOptions]::RemoveEmptyEntries)
-    foreach($assembly in $assemblies)
-    {
-        $assemblyLocation = "$currentLocation\Agent\Worker\$assembly"
-        [void][Reflection.Assembly]::LoadFrom($assemblyLocation)
-    }
-
-    import-module "..\..\..\Agent\Worker\Modules\Microsoft.TeamFoundation.DistributedTask.Task.Common"
-    import-module "..\..\..\Agent\Worker\Modules\Microsoft.TeamFoundation.DistributedTask.Task.Build"
-    import-module "..\..\..\Agent\Worker\Modules\Microsoft.TeamFoundation.DistributedTask.Task.Deployment.Azure\Microsoft.TeamFoundation.DistributedTask.Task.Deployment.Azure.psm1"
-
-    Write-Host "Using CollectionUrl and TeamProject from script parameters" -ForegroundColor Yellow
-    $collectionUrl = $CollectionUrl
-    $teamProject = $TeamProject
-
-    if(!$collectionUrl)
-    {
-        throw "CollectionUrl was not passed on the command line and no distributedTaskContext was found."
-    }
-    if (!$teamProject)
-    {
-        throw "TeamProject was not passed on the command line and no distributedTaskContext was found."
-    }
-
-    #Call the function in the module to get the deployment environment set up here.  Function name is Setup-AzureSubscription
-    Setup-AzureSubscription -DeploymentEnvironmentName $DeploymentEnvironmentName -CollectionUrl $collectionUrl -TeamProject $teamProject
-}
 
 #ENSURE: We pass arguments verbatim on the command line to the custom script
 Write-Host "ScriptArguments= " $ScriptArguments
--- Tasks/AzureWebPowerShellDeployment/Publish-AzureWebDeployment.ps1
+++ Tasks/AzureWebPowerShellDeployment/Publish-AzureWebDeployment.ps1
@@ -4,12 +4,6 @@ param
     [String] [Parameter(Mandatory = $true)]
     $DeploymentEnvironmentName,
 
-    [String] [Parameter(Mandatory = $false)] #false since it can be passed in via JSON
-    $CollectionUrl,
-
-    [String] [Parameter(Mandatory = $false)] #false since it can be passed in via JSON
-    $TeamProject,
-
     [String] [Parameter(Mandatory = $true)]
     $WebSiteName,
 
@@ -26,29 +20,6 @@ Write-Host "DeploymentEnvironmentName= $DeploymentEnvironmentName"
 Write-Host "WebSiteName= $WebSiteName"
 Write-Host "Package= $Package"
 Write-Verbose "AdditionalArguments= $AdditionalArguments"
-
-if (!$distributedTaskContext) #running directly via command line
-{
-    import-module "..\..\..\Agent\Worker\Modules\Microsoft.TeamFoundation.DistributedTask.Task.Common"
-    import-module "..\..\..\Agent\Worker\Modules\Microsoft.TeamFoundation.DistributedTask.Task.Build"
-    import-module "..\..\..\Agent\Worker\Modules\Microsoft.TeamFoundation.DistributedTask.Task.Deployment.Azure\Microsoft.TeamFoundation.DistributedTask.Task.Deployment.Azure.psm1"
-
-    Write-Host "Using CollectionUrl and TeamProject from script parameters" -ForegroundColor Yellow
-    $collectionUrl = $CollectionUrl
-    $teamProject = $TeamProject
-
-    if(!$collectionUrl)
-    {
-        throw "CollectionUrl was not passed on the command line and no distributedTaskContext was found."
-    }
-    if (!$teamProject)
-    {
-        throw "TeamProject was not passed on the command line and no distributedTaskContext was found."
-    }
-
-    #Call the function in the module to get the deployment environment set up here.  Function name is Setup-AzureSubscription
-    Setup-AzureSubscription -DeploymentEnvironmentName $DeploymentEnvironmentName -CollectionUrl $collectionUrl -TeamProject $teamProject
-}
 
 #Find the package to deploy
 Write-Host "packageFile = Find-Files -SearchPattern $Package"
