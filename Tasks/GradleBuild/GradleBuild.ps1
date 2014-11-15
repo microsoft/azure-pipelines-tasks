1f080bedc67569dd8a340dce741799dcb7bbe3ef
Caleb Tseng <calebt@microsoft.com>
2014-11-14 16:01:20 -0800

    Jenkins feature parity

--- Tasks/GradleBuild/GradleBuild.ps1
+++ Tasks/GradleBuild/GradleBuild.ps1
@@ -1,35 +1,27 @@
 param(
-    [string]$gradleProj, 
-    [string]$arguments, 
-    [string]$startEmulator
+    [string]$gradleWrapper,  # Path to gradle wrapper. Empty if using gradle installation. 
+    [string]$gradleProj,     # Optional - Root directory of gradle project. Defaults to root of working directory if empty. 
+    [string]$arguments,      # Gradle arguments
+    [string]$startEmulator   # True if emulator start required. Converted to Boolean
 )
 
-Write-Output "Entering script GradleBuild.ps1"
-Write-Output "gradleProj = $gradleProj"
-Write-Output "arguments = $arguments"
-Write-Output "startEmulator = $startEmulator"
-
-if (!$distributedTaskContext) #running directly via command line
-{
-    import-module "C:\Agent\src\Package\Agent\Worker\Modules\Microsoft.TeamFoundation.DistributedTask.Task.Common"
-    #import-module "C:\Agent\src\Package\Agent\Worker\ModulesMicrosoft.TeamFoundation.DistributedTask.Task.Build"
-}
+Write-Verbose "Entering script GradleBuild.ps1"
+Write-Verbose "gradleWrapper = $gradleWrapper"
+Write-Verbose "gradleProj = $gradleProj"
+Write-Verbose "arguments = $arguments"
+Write-Verbose "startEmulator = $startEmulator"
 
 # Import the Task.Common dll that has all the cmdlets we need for Build
-#import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 
-$startEmulator2 = Convert-String $startEmulator Boolean
-#$startEmulator = [System.Convert]::ToBoolean($startEmulator) 
-Write-Output "startEmulator (converted) = $startEmulator"
+$emulator = Convert-String $startEmulator Boolean
+Write-Verbose "startEmulator (converted) = $startEmulator"
 
 $adbexe = $env:ANDROID_HOME + "\platform-tools\adb.exe"
 $androidbat = $env:ANDROID_HOME + "\tools\android.bat"
 
-#TODO: Check for file existence first. If gradle file not there, don't bother starting the emulator. 
-
-if($startEmulator2)
+if($emulator)
 {
-	Write-Output "startEmulator (converted 2) = $startEmulator"
     Stop-Process -processname emulator-x86 2> $null
     & $adbexe kill-server 2> $null
     & $androidbat delete avd -n Nexus5 2> $null
@@ -41,7 +33,7 @@ if($startEmulator2)
     # Start emulator
     $emublock = {
         pushd $env:ANDROID_HOME
-        Write-Output "*****INFO: Starting emulator!"
+        Write-Verbose "INFO: Starting emulator!"
         .\tools\emulator.exe -avd "Nexus5" -prop persist.sys.language=en -prop persist.sys.country=US -no-snapshot-load -no-snapshot-save
         popd
 
@@ -55,22 +47,22 @@ if($startEmulator2)
     # dev.bootcomplete is "1" when the device is fully booted
     $devBootComplete = 0
     $numLoops = 0  # fail out if it takes too long
-    Write-Output "*****INFO: Waiting for emulator to fully boot!"
+    Write-Verbose "INFO: Waiting for emulator to fully boot!"
     while ($devBootComplete[0] -ne "1")
     {
         ($bootComplete = Start-Job -Name jobBootComplete -ScriptBlock {
             param($adbexe)
             & $adbexe shell getprop dev.bootcomplete 2> $null
-        } -argumentlist $adbexe) |Out-Null
+        } -argumentlist $adbexe) | Out-Null
         Wait-Job $bootComplete | Out-Null
         Receive-Job $bootComplete -OutVariable devBootComplete | Out-Null
-        $output = "*****INFO: Waiting for emulator to boot..." + $numLoops
-        Write-Output $output
+        $output = "INFO: Waiting for emulator to boot..." + $numLoops
+        Write-Verbose $output
         $numLoops++
 
         # Try to restart adb server every once in a while to see if that is the problem
         if ($numLoops % 25 -eq 0) {
-            Write-Output "*****INFO: Restarting adb server!"
+            Write-Verbose "INFO: Restarting adb server!"
             & $adbexe kill-server
             & $adbexe start-server
         }
@@ -83,24 +75,41 @@ if($startEmulator2)
     }
 }
 
-# Check for file existence
-$gradlebat = Join-Path $gradleProj -Child "gradlew.bat"
-if ([System.IO.File]::Exists($gradlebat))
-{
-    Write-Output "Invoking script $gradlebat with arguments $arguments"
-    Invoke-BatchScript $gradlebat –Arguments $arguments
-}
-else
-{
-    Write-Error "Unable to find script $filename"
+# Change working directory to specified gradle project. 
+if($gradleProj) {
+    Write-Verbose "Setting working directory to $gradleProj"
+    pushd $gradleProj
 }
 
+if($gradleWrapper){
+    # Use Gradle Wrapper
+    if ([System.IO.File]::Exists($gradleWrapper)) {
+        Write-Verbose "Invoking gradle wrapper $gradleWrapper with arguments $arguments"
+        Invoke-BatchScript $gradleWrapper –Arguments $arguments
+    }
+    else {
+        Write-Error "Unable to find script $gradleWrapper"
+    }
+}
+else {
+    # Use gradle installation. Path variable must be set to include GRADLE_HOME/bin, per Gradle documentation. 
+    Write-Verbose "Invoking gradle with arguments $arguments"
+    $gradleCommand = "gradle " + $arguments
+    Invoke-Expression $gradleCommand
+}
+
+if($gradleProj) {
+    popd
+}
+
+
+
 # Delete emulator device.  Stop-Process is used because Wait-Job or Stop-Job hangs.
-if($startEmulator)
+if($emulator)
 {
     Stop-Process -processname emulator-x86
     & $adbexe kill-server 
     & $androidbat delete avd -n Nexus5
 }
 
-Write-Output "Leaving script GradleBuild.ps1"
+Write-Verbose "Leaving script GradleBuild.ps1"
