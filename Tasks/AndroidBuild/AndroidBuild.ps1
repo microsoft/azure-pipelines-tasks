545b1d4866c164b6a95c58e25626bf30cf373a10
Caleb Tseng <calebt@microsoft.com>
2014-12-01 15:48:06 -0800

    Renaming to AndroidBuild and parameterizing emulator arguments

--- Tasks/AndroidBuild/AndroidBuild.ps1
+++ Tasks/AndroidBuild/AndroidBuild.ps1
@@ -2,43 +2,55 @@ param(
     [string]$gradleWrapper,  # Path to gradle wrapper. Empty if using gradle installation. 
     [string]$gradleProj,     # Optional - Root directory of gradle project. Defaults to root of working directory if empty. 
     [string]$arguments,      # Gradle arguments
-    [string]$startEmulator   # True if emulator start required. Converted to Boolean
+    [string]$startEmulator,  # True if emulator start required. Converted to Boolean
+	[string]$emulatorTarget, # Emulator target version
+	[string]$emulatorDevice  # Emulator device 
 )
 
-Write-Verbose "Entering script GradleBuild.ps1"
-Write-Verbose "gradleWrapper = $gradleWrapper"
-Write-Verbose "gradleProj = $gradleProj"
-Write-Verbose "arguments = $arguments"
-Write-Verbose "startEmulator = $startEmulator"
+Write-Output "Entering script AndroidBuild.ps1"
+Write-Output "gradleWrapper = $gradleWrapper"
+Write-Output "gradleProj = $gradleProj"
+Write-Output "arguments = $arguments"
+Write-Output "startEmulator = $startEmulator"
+Write-Output "emulatorTarget = $emulatorTarget"
+Write-Output "emulatorDevice = $emulatorDevice"
 
 # Import the Task.Common dll that has all the cmdlets we need for Build
-import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+import-module "C:\Agent\src\Package\Agent\Worker\Modules\Microsoft.TeamFoundation.DistributedTask.Task.Common"
+#import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 
-$emulator = Convert-String $startEmulator Boolean
-Write-Verbose "startEmulator (converted) = $startEmulator"
+#$emulator = Convert-String $startEmulator Boolean
+$emulator = [System.Convert]::ToBoolean($startEmulator)
+Write-Output "startEmulator (converted) = $startEmulator"
 
 $adbexe = $env:ANDROID_HOME + "\platform-tools\adb.exe"
 $androidbat = $env:ANDROID_HOME + "\tools\android.bat"
 
+
 if($emulator)
 {
+    # Set up default emulator settings 
+	$emuName = "AndroidBuildEmulator"
+	$abi = "default/x86"
+
     Stop-Process -processname emulator-x86 2> $null
     & $adbexe kill-server 2> $null
-    & $androidbat delete avd -n Nexus5 2> $null
+    & $androidbat delete avd -n $emuName 2> $null
 
     # Create an emulator device
-    & $androidbat list targets
-    & $androidbat create avd --name "Nexus5" --target "android-19" --device "Nexus 5" --abi "default/x86" --force
+    #& $androidbat list targets
+    #& $androidbat create avd --name "AndroidBuildEmulator" --target "$emulatorTarget" --device "Nexus 5" --abi "default/x86" --force
+	& $androidbat create avd --name $emuName --target $emulatorTarget --device $emulatorDevice --abi $abi  --force
 
     # Start emulator
     $emublock = {
-        pushd $env:ANDROID_HOME
-        Write-Verbose "INFO: Starting emulator!"
-        .\tools\emulator.exe -avd "Nexus5" -prop persist.sys.language=en -prop persist.sys.country=US -no-snapshot-load -no-snapshot-save
-        popd
-
+		param($emuName)
+        Push-Location $env:ANDROID_HOME
+        Write-Output "INFO: Starting emulator!"
+        .\tools\emulator.exe -avd $emuName -prop persist.sys.language=en -prop persist.sys.country=US -no-snapshot-load -no-snapshot-save
+        Pop-Location
     }
-    Start-Job -Name openEmulator -ScriptBlock $emublock | Out-Null
+    Start-Job -Name openEmulator -ScriptBlock $emublock -ArgumentList $emuName | Out-Null
 
     # Connect to emulator
     & $adbexe start-server
@@ -47,7 +59,7 @@ if($emulator)
     # dev.bootcomplete is "1" when the device is fully booted
     $devBootComplete = 0
     $numLoops = 0  # fail out if it takes too long
-    Write-Verbose "INFO: Waiting for emulator to fully boot!"
+    Write-Output "INFO: Waiting for emulator to fully boot!"
     while ($devBootComplete[0] -ne "1")
     {
         ($bootComplete = Start-Job -Name jobBootComplete -ScriptBlock {
@@ -57,12 +69,12 @@ if($emulator)
         Wait-Job $bootComplete | Out-Null
         Receive-Job $bootComplete -OutVariable devBootComplete | Out-Null
         $output = "INFO: Waiting for emulator to boot..." + $numLoops
-        Write-Verbose $output
+        Write-Output $output
         $numLoops++
 
         # Try to restart adb server every once in a while to see if that is the problem
         if ($numLoops % 25 -eq 0) {
-            Write-Verbose "INFO: Restarting adb server!"
+            Write-Output "INFO: Restarting adb server!"
             & $adbexe kill-server
             & $adbexe start-server
         }
@@ -77,14 +89,14 @@ if($emulator)
 
 # Change working directory to specified gradle project. 
 if($gradleProj) {
-    Write-Verbose "Setting working directory to $gradleProj"
-    pushd $gradleProj
+    Write-Output "Setting working directory to $gradleProj"
+    Push-Location $gradleProj
 }
 
 if($gradleWrapper){
     # Use Gradle Wrapper
     if ([System.IO.File]::Exists($gradleWrapper)) {
-        Write-Verbose "Invoking gradle wrapper $gradleWrapper with arguments $arguments"
+        Write-Output "Invoking gradle wrapper $gradleWrapper with arguments $arguments"
         Invoke-BatchScript $gradleWrapper –Arguments $arguments
     }
     else {
@@ -93,13 +105,13 @@ if($gradleWrapper){
 }
 else {
     # Use gradle installation. Path variable must be set to include GRADLE_HOME/bin, per Gradle documentation. 
-    Write-Verbose "Invoking gradle with arguments $arguments"
+    Write-Output "Invoking gradle with arguments $arguments"
     $gradleCommand = "gradle " + $arguments
     Invoke-Expression $gradleCommand
 }
 
 if($gradleProj) {
-    popd
+    Pop-Location
 }
 
 
@@ -109,7 +121,7 @@ if($emulator)
 {
     Stop-Process -processname emulator-x86
     & $adbexe kill-server 
-    & $androidbat delete avd -n Nexus5
+    & $androidbat delete avd -n $emuName
 }
 
-Write-Verbose "Leaving script GradleBuild.ps1"
+Write-Output "Leaving script AndroidBuild.ps1"
