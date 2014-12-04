2b9140e94cb9e2e3255a24a48b91f189c6cb9a56
Madhuri Gummalla <madhurig@microsoft.com>
2014-12-04 11:33:09 -0500

    Basic ANT task for .NET and xPlat agent
    Related Work Items: #171938

---
+++ Tasks/ANT/ant.js
@@ -0,0 +1,58 @@
+﻿// 
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// 
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+var fs = require('fs');
+var path = require('path');
+var shell = require('shelljs/global');
+
+exports.execute = function (ctx, callback) {
+
+    //Verify ant is installed correctly
+    var antPath = which('ant');
+    if (!antPath) {
+        callback(new Error('Unable to find ANT, verify it is installed correctly, ANT_HOME is set and ANT_HOME\bin is added to the PATH on the build agent'));
+        return;
+    }
+
+    ctx.verbose('using ant: ' + antPath);
+
+    //Find working directory to run ANT in
+    var cwd = ctx.inputs.cwd;
+    if (!fs.existsSync(cwd)) {
+        callback(new Error('Working Directory not exist: ' + cwd));
+        return;
+    }
+
+    cd(cwd);
+    ctx.verbose('Working Folder: ' + cwd);
+    var cwd = process.cwd();
+
+    // options and targets are optional - invoke ant without any arguments if nothing is specified
+    var options = ctx.inputs.options;
+    var targets = ctx.inputs.targets;
+
+    var antArguments = options + targets;
+    ctx.verbose('ANT arguments: ' + antArguments);
+        
+    var ops = {
+        cwd: path.resolve(cwd),
+        env: process.env
+    };
+
+    // calling spawn instead of fork so we can easily capture output --> logs
+    ctx.info('Running ant: ');
+    ctx.util.spawn(antPath, antArguments, ops, callback);
+}
---
+++ Tasks/ANT/ant.ps1
@@ -0,0 +1,52 @@
+﻿param (
+	[string]$cwd,
+	[string]$options,
+	[string]$targets
+)
+
+Write-Verbose 'Entering ant.ps1'
+Write-Verbose "cwd = $cwd"
+Write-Verbose "options = $options"
+Write-Verbose "targets = $targets"
+
+#Verify ant is installed correctly
+try
+{
+	$ant = Get-Command ant
+	$antPath = $ant.Path
+	Write-Verbose "Found ANT at $antPath"
+}
+catch
+{
+	throw 'Unable to find ANT, verify it is installed correctly, ANT_HOME is set and ANT_HOME\bin is added to the PATH on the build agent'
+}
+
+# Find working directory to run ANT in
+if(!$cwd)
+{
+    throw "Working Directory is not set"
+}
+
+if(!(Test-Path $cwd -PathType Container))
+{
+	Write-Verbose "Creating directory $cwd"
+    New-Item -Path $cwd -ItemType Container
+}
+
+Write-Verbose "Setting Working Directory to $cwd"
+Set-Location $cwd
+
+$antArguments = $options + ' ' + $targets
+Write-Verbose "Using ANT arguments $antArguments"
+
+# Import the Task.Common dll that has all the cmdlets we need for Build
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+
+Write-Verbose "Running Ant..."
+Invoke-Tool -Path $ant.Path -Arguments $antArguments -WorkingFolder $cwd
+
+Write-Verbose "Leaving script Ant.ps1"
+
+
+
+
