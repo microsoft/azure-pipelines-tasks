f21ff855b2f84c3ee0703334adc48303c0c4e6f9
Caleb Tseng <calebt@microsoft.com>
2015-01-09 16:16:17 -0800

    Clean up temporary emu files

--- Tasks/AndroidBuild/KillAndroidEmulator.ps1
+++ Tasks/AndroidBuild/KillAndroidEmulator.ps1
@@ -13,4 +13,11 @@ Stop-Process -processname emulator-x86
 & $adbexe kill-server 
 & $androidbat delete avd -n $emulatorName
 
+# Remove temporary emulator file
+$user = [Environment]::UserName
+$tempDirectory = "C:\Users\" + $user + "\AppData\Local\Temp\AndroidEmulator"
+if(Test-Path $tempDirectory) {
+	Remove-Item $tempDirectory -recurse
+}
+
 Write-Verbose "Leaving script KillAndroidEmulator.ps1"
