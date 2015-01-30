b793d7b04e9b0b3bfe7e8a4925a7cbec9a3447e4
Aditya Agrawal <aditya@microsoft.com>
2015-01-30 16:14:56 +0530

    CR Comments

--- Tasks/RunDistributedTests/RunDistributedTests.ps1
+++ Tasks/RunDistributedTests/RunDistributedTests.ps1
@@ -1,6 +1,6 @@
 param(
     [string]$environment, 
-    [string]$testassemblies,
+    [string]$sourcefilters,
     [string]$testFilterCriteria,
     [string]$runSettingsFile,
     [string]$alternateCredsUserName,
@@ -9,7 +9,7 @@ param(
 
 Write-Verbose "Entering script RunDistributedTests.ps1"
 Write-Verbose "environment = $environment"
-Write-Verbose "Source Filter = $testassemblies"
+Write-Verbose "Source Filter = $sourcefilters"
 Write-Verbose "testFilterCriteria = $testFilterCriteria"
 Write-Verbose "runSettingsFile = $runSettingsFile"
 Write-Verbose "AlternateuserName = $alternateCredsUserName"
@@ -19,6 +19,6 @@ import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.DistributedTestAutomation"
 
 Write-Verbose "Calling Invoke-RunDistributedTests"
-Invoke-RunDistributedTests -EnvironmentName $environment -SourceFilter $testassemblies -TestCaseFilter $testFilterCriteria -RunSettingsPath $runSettingsFile -AlternateCredUserName $alternateCredsUserName -AlternateCredPassword $alternateCredsPassword
+Invoke-RunDistributedTests -EnvironmentName $environment -SourceFilter $sourcefilters -TestCaseFilter $testFilterCriteria -RunSettingsPath $runSettingsFile -AlternateCredUserName $alternateCredsUserName -AlternateCredPassword $alternateCredsPassword
 
 Write-Verbose "Leaving script RunDistributedTests.ps1"
