2f5c8ab9bcf318dd69240f2d552878731eea4776
Nipun Jain <nipunja@microsoft.com>
2015-01-30 10:29:43 +0000

    History Integration

--- Tasks/DeployUsingPowerShell/RemotePowerShellRunner.ps1
+++ Tasks/DeployUsingPowerShell/RemotePowerShellRunner.ps1
@@ -19,15 +19,24 @@ import-module "Microsoft.TeamFoundation.DistributedTask.Task.DevTestLabs"
 
 $port = '5985'
 
-$resources = Get-EnvironmentResources -EnvironmentName $environmentName -ResourceFilter $machineNames
+$resources = Get-EnvironmentResources -EnvironmentName $environmentName -ResourceFilter $machineNames -ErrorAction Stop
 
-$machineUserName = Get-EnvironmentProperty -EnvironmentName $environmentName -Key "Username"
-$machinePassword = Get-EnvironmentProperty -EnvironmentName $environmentName -Key "Password"
+$machineUserName = Get-EnvironmentProperty -EnvironmentName $environmentName -Key "Username" -ErrorAction Stop
+
+$machinePassword = Get-EnvironmentProperty -EnvironmentName $environmentName -Key "Password" -ErrorAction Stop
 
 $credential = New-Object 'System.Net.NetworkCredential' -ArgumentList $machineUserName, $machinePassword
 
+$envOperationId = Invoke-EnvironmentOperation -EnvironmentName $environmentName -OperationName "Deployment" -ErrorAction Stop
+
+Write-Verbose "EnvironmentOperationId = $envOperationId" -Verbose
+
 foreach ($resource in $resources)
 {
+	$resOperationId = Invoke-ResourceOperation -EnvironmentName $environmentName -ResourceName $resource.Name -EnvironmentOperationId $envOperationId -ErrorAction Stop
+
+	Write-Verbose "ResourceOperationId = $resOperationId" -Verbose
+	
     $fqdn = $resource.Name
 
     Write-Verbose "Initiating copy on $fqdn, username: $machineUserName" -Verbose
@@ -44,32 +53,39 @@ foreach ($resource in $resources)
     {
         Write-Verbose "Finished Copying" -Verbose
     }
+
+    $response = $copyResponse
     
-    if ($copyResponse.Status -ne "Passed")
+    if ($copyResponse.Status -eq "Passed")
     {
-        # TODO : Should we fail at first error encountered?
-        throw $copyResponse.Error;
+        Write-Verbose "Initiating deployment on $fqdn" -Verbose
+
+        $deploymentResponse = Invoke-PsOnRemote -MachineDnsName $fqdn -ScriptPath $scriptPath -WinRMPort $port -Credential $credential -InitializationScriptPath $initializationScriptPath –SkipCACheck -UseHttp
+
+        $log = $deploymentResponse.Log;
+
+        if ([string]::IsNullOrEmpty($log) -eq $false)
+        {
+            Write-Verbose "Deployment response for $fqdn is $log" -Verbose
+        }
+        else
+        {
+            Write-Verbose "Finished Deployment" -Verbose
+        }
+
+        $response = $deploymentResponse
     }
 
-    Write-Verbose "Initiating deployment on $fqdn" -Verbose
-
-    $deploymentResponse = Invoke-PsOnRemote -MachineDnsName $fqdn -ScriptPath $scriptPath -WinRMPort $port -Credential $credential -InitializationScriptPath $initializationScriptPath –SkipCACheck -UseHttp
-
-    $log = $deploymentResponse.Log;
-
-    if ([string]::IsNullOrEmpty($log) -eq $false)
-    {
-        Write-Verbose "Deployment response for $fqdn is $log" -Verbose
-    }
-    else
-    {
-        Write-Verbose "Finished Deployment" -Verbose
-    }
+	Complete-ResourceOperation -EnvironmentName $environmentName -EnvironmentOperationId $envOperationId -ResourceOperationId $resOperationId -Status $response.Status -ErrorMessage $response.Error -Logs $log -ErrorAction Stop
     
-    if ($deploymentResponse.Status -ne "Passed")
+    if ($response.Status -ne "Passed")
     {
-        throw $deploymentResponse.Error;
+		Complete-EnvironmentOperation -EnvironmentName $environmentName -EnvironmentOperationId $envOperationId -Status "Failed" -ErrorAction Stop
+
+        throw $response.Error;
     }
 }
+
+Complete-EnvironmentOperation -EnvironmentName $environmentName -EnvironmentOperationId $envOperationId -Status "Passed" -ErrorAction Stop
 
 Write-Verbose "Leaving script RemotePowerShellRunner.ps1" -Verbose
