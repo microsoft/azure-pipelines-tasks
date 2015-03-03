bc6e59915d3b17c941777c4a4a158b3131f04ee0
Shashwat Gupta <sgupta@microsoft.com>
2015-03-03 10:20:18 +0530

    Changes in Task

--- Tasks/DeployTestAgent/DeployTestAgent.ps1
+++ Tasks/DeployTestAgent/DeployTestAgent.ps1
@@ -5,7 +5,8 @@ param(
     [string]$machineUserName,
     [string]$machinePassword,
     [string]$alternateCredsUserName,
-    [string]$alternateCredsPassword
+    [string]$alternateCredsPassword,
+    [string]$agentLocation
 )
 
 # If Run as process (Run UI Tests) is true both autologon and disable screen saver needs to be true.
@@ -19,11 +20,21 @@ Write-Verbose "runAsProcess = $runAsProcess"
 Write-Verbose "logonAutomatically = $logonAutomatically"
 Write-Verbose "disableScreenSaver = $disableScreenSaver"
 
+
+if (([string]::IsNullOrWhiteSpace($agentLocation))
+{
+   Write-Verbose "Download of testagent would begin from internet"
+}
+else
+{
+   Write-Verbose "agentLocation = $agentLocation"
+}
+
 # Import the Task.Common dll that has all the cmdlets we need for Build
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.DistributedTestAutomation"
 
 Write-Verbose "Calling Invoke-DeployTestAgent"
-Invoke-DeployTestAgent -MachineNames $testMachines -UserName $machineUserName -Password $machinePassword -PowerShellPort 5985 -EnvironmentName $environment -RunAsProcess $runAsProcess -LogonAutomatically $logonAutomatically -DisableScreenSaver $disableScreenSaver -AlternateCredUserName $alternateCredsUserName -AlternateCredPassword $alternateCredsPassword
+Invoke-DeployTestAgent -MachineNames $testMachines -UserName $machineUserName -Password $machinePassword -PowerShellPort 5985 -EnvironmentName $environment -RunAsProcess $runAsProcess -LogonAutomatically $logonAutomatically -DisableScreenSaver $disableScreenSaver -AlternateCredUserName $alternateCredsUserName -AlternateCredPassword $alternateCredsPassword -AgentLocation $agentLocation
 
 Write-Verbose "Leaving script DeployTestAgent.ps1"
