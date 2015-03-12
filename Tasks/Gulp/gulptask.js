df7b880ef720a5c57e8d168050661daa4abe8364
Bryan MacFarlane <bryanmac@microsoft.com>
2015-03-12 08:32:28 -0400

    finish up gulp task

--- Tasks/Gulp/gulptask.js
+++ Tasks/Gulp/gulptask.js
@@ -1,11 +1,22 @@
+var path = require('path');
 var tl = require('vso-task-lib');
 
 var gt = new tl.ToolRunner(tl.which('gulp', true));
 
 // optional - no tasks will concat nothing
 gt.arg(tl.getDelimitedInput('tasks', ' ', false));
+
 gt.arg('--gulpfile');
-gt.arg(tl.getInput('gulpFile', true));
+var gulpFile = tl.getPathInput('gulpFile', true);
+gt.arg(gulpFile);
+
+gt.arg(tl.getDelimitedInput('arguments', ' ', false));
+
+var cwd = tl.getInput('cwd', false);
+if (!cwd) {
+	cwd = path.dirname(gulpFile);
+}
+tl.cd(cwd);
 
 gt.exec()
 .then(function(code) {
