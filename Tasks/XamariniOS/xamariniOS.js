d86eb4e480ba02948ec2c92cd9b0dfd7c740dd63
Yang Cao <yacao@microsoft.com>
2015-03-26 15:12:14 -0400

    task template for Xamarin.iOS

---
+++ Tasks/XamariniOS/xamariniOS.js
@@ -0,0 +1,8 @@
+﻿var tl = require('vso-task-lib');
+var path = require('path');
+
+// if output is rooted ($(build.buildDirectory)/output/...), will resolve to fully qualified path, 
+// else relative to repo root
+var out = path.resolve(tl.getVariable('build.sourceDirectory'),
+	                            tl.getInput('outputPattern', true));
+tl.mkdirP(out);
