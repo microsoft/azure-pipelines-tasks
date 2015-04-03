8b0c47508efe9844f16b42276f2324a22e8fdb39
Bryan MacFarlane <bryanmac@microsoft.com>
2015-04-03 13:06:13 -0400

    gulp packaging: part 1

--- package.js
+++ package.js
@@ -4,30 +4,64 @@ var path = require('path');
 var fs = require('fs');
 var check = require('validator');
 var shell = require('shelljs');
+var Q = require('q');
 
 var createError = function(msg) {
 	return new gutil.PluginError('PackageTask', msg);
 }
 
-var validate = function(folderName, task, done) {
+var validate = function(folderName, task) {
+	var defer = Q.defer();
+
 	var vn = (task.name  || folderName);
 
 	if (!task.id || !check.isUUID(task.id)) {
-		done(createError(vn + ': id is a required guid'));
-		return;
+		defer.reject(createError(vn + ': id is a required guid'));
 	};
 
 	if (!task.name || !check.isAlphanumeric(task.name)) {
-		done(createError(vn + ': name is a required alphanumeric string'));
-		return;
+		defer.reject(createError(vn + ': name is a required alphanumeric string'));
 	}
 
 	if (!task.friendlyName || !check.isLength(task.friendlyName, 1, 40)) {
-		done(createError(vn + ': friendlyName is a required string <= 40 chars'));
-		return;
+		defer.reject(createError(vn + ': friendlyName is a required string <= 40 chars'));
 	}
 
-	done();
+	// resolve if not already rejected
+	defer.resolve();
+	return defer.promise;
+};
+
+var createStrings = function(task, pkgPath) {
+	var defer = Q.defer();
+
+	var strPath = path.join(pkgPath, 'Strings', 'resources.resjson', 'en-US');
+	shell.mkdir('-p', strPath);
+
+	var strings = {};
+	strings['loc.friendlyName'] = task.friendlyName;
+	task['friendlyName'] = 'ms-resource:loc.friendlyName';
+
+	var enPath = path.join(strPath, 'resources.resjson');
+	fs.writeFile(enPath, JSON.stringify(strings, null, 2), function(err) {
+		if (err) {
+			defer.reject(createError('could not create: ' + enPath + ' - ' + err.message));
+			return;
+		}
+
+		var taskPath = path.join(pkgPath, 'task.loc.json');
+		fs.writeFile(taskPath, JSON.stringify(task, null, 2), function(err) {
+			if (err) {
+				defer.reject(createError('could not create: ' + taskPath + ' - ' + err.message));
+				return;
+			}
+
+			defer.resolve();			
+		});
+
+	})
+
+	return defer.promise;
 };
 
 function packageTask(pkgPath){
@@ -55,19 +89,26 @@ function packageTask(pkgPath){
 	        	return
 	        }
 
-	        validate(folderName, task, function(err) {
-	        	if (err) {
-	        		done(err);
-	        		return;
-	        	}
+	        var tgtPath;
 
+	        validate(folderName, task)
+	        .then(function() {
 				gutil.log('Packaging: ' + task.name);
-	        	var verStr = task.version.Major + '.' + task.version.Minor + '.' + task.version.Patch;
-	        	var verPath = path.join(pkgPath, task.name, verStr);
-	        	shell.mkdir('-p', verPath);
-	        	shell.cp('-R', path.join(dirName, '*'), verPath);
-	        	shell.rm(path.join(verPath, '*.csproj'));
+	        	
+	        	tgtPath = path.join(pkgPath, task.name);
+	        	shell.mkdir('-p', tgtPath);
+	        	shell.cp('-R', path.join(dirName, '*'), tgtPath);
+	        	shell.rm(path.join(tgtPath, '*.csproj'));
+	        	return;        	
+	        })
+	        .then(function() {
+	        	return createStrings(task, tgtPath);
+	        })
+	        .then(function() {
 	        	done();
+	        })
+	        .fail(function(err) {
+	        	done(err);
 	        })
 		});    
 }
