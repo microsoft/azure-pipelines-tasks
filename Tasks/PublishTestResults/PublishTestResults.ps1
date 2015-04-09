cdb243930275352c2fe054047bc4170f7daa092c
madhurig <madhurig@microsoft.com>
2015-04-09 13:49:15 -0400

    update task to search for files and pass the list to the cmdlet

--- Tasks/PublishTestResults/PublishTestResults.ps1
+++ Tasks/PublishTestResults/PublishTestResults.ps1
@@ -1,7 +1,6 @@
 param(
     [string]$testRunner,    
-    [string]$testResultsFiles,  
-    [string]$cwd,
+    [string]$testResultsFiles,
     [string]$platform,
     [string]$configuration
 )
@@ -19,6 +18,25 @@ if(!$testRunner)
 if (!$testResultsFiles)
 {
     throw "testResultsFiles parameter has to be specified"
+}
+
+# check for pattern in testResultsFiles
+if ($testResultsFiles.Contains("*") -or $testResultsFiles.Contains("?"))
+{
+    Write-Verbose "Pattern found in testResultsFiles parameter."
+    Write-Verbose "Find-Files -SearchPattern $testResultsFiles"
+    $matchingTestResultsFiles = Find-Files -SearchPattern $testResultsFiles
+    Write-Verbose "matchingTestResultsFiles = $matchingTestResultsFiles"
+}
+else
+{
+    Write-Verbose "No Pattern found in testResultsFiles parameter."
+    $matchingTestResultsFiles = ,$testResultsFiles
+}
+
+if (!$matchingTestResultsFiles)
+{
+    throw "No test results files with search pattern '$testResultsFiles' were found."
 }
 
 Write-Verbose "Calling Publish-TestResults"
