78712532eb98a09496ac817a813a61fed78f5fea
Eric Sciple <ersciple@microsoft.com>
2015-05-07 22:29:11 -0400

    Fixed (cosmetic) bug writing verbose output.

--- Tasks/AzurePowerShell/RunAzurePowerShell.ps1
+++ Tasks/AzurePowerShell/RunAzurePowerShell.ps1
@@ -14,11 +14,11 @@ param
 Write-Verbose "Entering script RunAzurePowerShell.ps1"
 
 #ENSURE: We pass arguments verbatim on the command line to the custom script
-Write-Verbose "ScriptArguments= " $ScriptArguments
-Write-Verbose "ScriptPath= " $ScriptPath
+Write-Verbose "ScriptArguments= $ScriptArguments"
+Write-Verbose "ScriptPath= $ScriptPath"
 
 $scriptCommand = "& `"$ScriptPath`" $scriptArguments"
-Write-Verbose "scriptCommand=" $scriptCommand
+Write-Verbose "scriptCommand= $scriptCommand"
 Invoke-Expression -Command $scriptCommand
 
 Write-Verbose "Leaving script RunAzurePowerShell.ps1"
