5861e0fbc3f02f23b788e8017bb2b49a6bee7d1b
Anubhav Aggarwal <anaggar@microsoft.com>
2015-05-29 08:41:53 +0530

    1) making string localized which we are outputting to console 2) improved logging for task

--- Tasks/AzureFileCopy/AzureFileCopy.ps1
+++ Tasks/AzureFileCopy/AzureFileCopy.ps1
@@ -18,6 +18,8 @@ Write-Verbose "connectedServiceName = $connectedServiceName" -Verbose
 Write-Verbose "sourcePath = $sourcePath" -Verbose
 Write-Verbose "storageAccount = $storageAccount" -Verbose
 Write-Verbose "destination = $destination" -Verbose
+Write-Verbose "containerName = $containerName" -Verbose
+Write-Verbose "blobPrefix = $blobPrefix" -Verbose
 Write-Verbose "environmentName = $environmentName" -Verbose
 Write-Verbose "machineNames = $machineNames" -Verbose
 Write-Verbose "targetPath = $targetPath" -Verbose
@@ -27,6 +29,7 @@ Write-Verbose "copyFilesInParallel = $copyFilesInParallel" -Verbose
 # Constants #
 $defaultWinRMPort = '5986'
 $defaultProtocolOption = ''
+$defaultSasTokenTimeOutInHours = 2
 
 $useHttpProtocolOption = '-UseHttp'
 $useHttpsProtocolOption = ''
@@ -34,15 +37,16 @@ $useHttpsProtocolOption = ''
 $doSkipCACheckOption = '-SkipCACheck'
 $doNotSkipCACheckOption = ''
 
-$envOperationStatus = 'Passed'
-$defaultSasTokenTimeOutInHours = 2
+$azureFileCopyOperation = 'AzureFileCopy'
 
 . ./AzureFileCopyJob.ps1
 
+# Import all the dlls and modules which have cmdlets we need
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.DevTestLabs"
 Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Deployment.Internal"
 
+# Getting resource tag key name for corresponding tag
 $resourceFQDNKeyName = Get-ResourceFQDNTagKey
 $resourceWinRMHttpPortKeyName = Get-ResourceHttpTagKey
 $resourceWinRMHttpsPortKeyName = Get-ResourceHttpsTagKey
@@ -51,7 +55,7 @@ $skipCACheckKeyName = Get-SkipCACheckTagKey
 function Remove-AzureContainer
 {
     param([string]$containerName,
-          [object]$storageContext,
+          [Microsoft.WindowsAzure.Commands.Common.Storage.AzureStorageContext]$storageContext,
           [string]$storageAccount)
 
     Write-Verbose "Deleting container: $containerName in storage account: $storageAccount" -Verbose
@@ -63,7 +67,7 @@ function Get-ResourceCredentials
     param([object]$resource)
 
     $machineUserName = $resource.Username
-    Write-Verbose "`t`t Resource Username - $machineUserName" -Verbose
+    Write-Verbose "`t Resource Username - $machineUserName" -Verbose
 
     $machinePassword = $resource.Password
     $credential = New-Object 'System.Net.NetworkCredential' -ArgumentList $machineUserName, $machinePassword
@@ -74,13 +78,13 @@ function Get-ResourceCredentials
 function Get-ResourceConnectionDetails
 {
     param([object]$resource,
-          [object]$connection)
+          [Microsoft.VisualStudio.Services.Client.VssConnection]$connection)
 
     $resourceProperties = @{}
     $resourceName = $resource.Name
 
     $fqdn = Get-EnvironmentProperty -EnvironmentName $environmentName -Key $resourceFQDNKeyName -Connection $connection -ResourceName $resourceName -ErrorAction Stop
-    Write-Verbose "`t`t Resource fqdn - $fqdn" -Verbose
+    Write-Verbose "`t Resource fqdn - $fqdn" -Verbose
 
     $winrmPortToUse = ''
     $protocolToUse = ''
@@ -88,13 +92,13 @@ function Get-ResourceConnectionDetails
     $winrmHttpsPort = Get-EnvironmentProperty -EnvironmentName $environmentName -Key $resourceWinRMHttpsPortKeyName -Connection $connection -ResourceName $resourceName -ErrorAction Stop
     if ([string]::IsNullOrEmpty($winrmHttpsPort))
     {
-        Write-Verbose "`t`t Resource: $resourceName does not have any winrm https port defined, checking for winrm http port" -Verbose
+        Write-Verbose "`t Resource: $resourceName does not have any winrm https port defined, checking for winrm http port" -Verbose
 
         $winrmHttpPort = Get-EnvironmentProperty -EnvironmentName $environmentName -Key $resourceWinRMHttpPortKeyName -Connection $connection -ResourceName $resourceName -ErrorAction Stop
         # if resource does not have any port defined then, use https port by default
         if ([string]::IsNullOrEmpty($winrmHttpPort))
         {
-            Write-Verbose "`t`t Resource: $resourceName does not have any winrm http port or https port defined, using https port by default" -Verbose
+            Write-Verbose "`t Resource: $resourceName does not have any winrm http port or https port defined, using https port by default" -Verbose
             $winrmPortToUse = $defaultWinRMPort
             $protocolToUse = $defaultProtocolOption
         }
@@ -112,7 +116,7 @@ function Get-ResourceConnectionDetails
         $protocolToUse = $useHttpsProtocolOption
     }
 
-    Write-Verbose "`t`t Trying to use port: $winrmPortToUse" -Verbose
+    Write-Verbose "`t Using port: $winrmPortToUse" -Verbose
 
     $resourceProperties.fqdn = $fqdn
     $resourceProperties.winrmPort = $winrmPortToUse
@@ -125,7 +129,7 @@ function Get-ResourceConnectionDetails
 function Get-SkipCACheckOption
 {
     param([string]$environmentName,
-          [object]$connection)
+          [Microsoft.VisualStudio.Services.Client.VssConnection]$connection)
 
     $skipCACheckOption = $doSkipCACheckOption
 
@@ -142,7 +146,7 @@ function Get-SkipCACheckOption
 function Get-ResourcesProperties
 {
     param([object]$resources,
-          [object]$connection)
+          [Microsoft.VisualStudio.Services.Client.VssConnection]$connection)
 
     $skipCACheckOption = Get-SkipCACheckOption -environmentName $environmentName -connection $connection
 
@@ -152,7 +156,7 @@ function Get-ResourcesProperties
         $resourceName = $resource.Name
         Write-Verbose "Get Resource properties for $resourceName" -Verbose
 
-        # Get other connection details for resource like - fqdn wirmport, http protocol, skipCACheckOption, resource credentials
+        # Get other connection details for resource like - fqdn winrmport, http protocol, skipCACheckOption, resource credentials
         $resourceProperties = Get-ResourceConnectionDetails -resource $resource -connection $connection
         $resourceProperties.skipCACheckOption = $skipCACheckOption
 
@@ -167,8 +171,10 @@ $agentHomeDir = $env:AGENT_HOMEDIRECTORY
 $azCopyLocation = Join-Path $agentHomeDir -ChildPath "Agent\Worker\Tools\AzCopy"
 
 # getting storage account key
+Write-Verbose "Retrieving storage key for the storage account: $storageAccount" -Verbose
 $storageKeyDetails = Get-AzureStorageKey -StorageAccountName $storageAccount
 $storageKey = $storageKeyDetails.Primary
+Write-Verbose "Retrieved storage key successfully for the storage account: $storageAccount" -Verbose
 
 # creating storage context to be used while creating container, sas token, deleting container
 $storageContext = New-AzureStorageContext -StorageAccountName $storageAccount -StorageAccountKey $storageKey
@@ -177,14 +183,15 @@ $storageContext = New-AzureStorageContext -StorageAccountName $storageAccount -S
 if ([string]::IsNullOrEmpty($containerName))
 {
     $containerName = [guid]::NewGuid().ToString();
+    Write-Verbose "Creating container: $containerName in storage account: $storageAccount" -Verbose
     $container = New-AzureStorageContainer -Name $containerName -Context $storageContext -Permission Container
-    Write-Verbose "Created container: $containerName in storage account: $storageAccount" -Verbose
+    Write-Verbose "Created container: $containerName successfully in storage account: $storageAccount" -Verbose
 }
 
 # uploading files to container
 try
 {
-    Write-Output "Uploading files from source path: $sourcePath to storage account: $storageAccount in container: $containerName with blobprefix: $blobPrefix" -Verbose
+    Write-Output (Get-LocalizedString -Key "Uploading files from source path: '{0}' to storage account: '{1}' in container: '{2}' with blobprefix: '{3}'" -ArgumentList $sourcePath, $storageAccount, $containerName, $blobPrefix)
     $uploadResponse = Copy-FilesToAzureBlob -SourcePathLocation $sourcePath -StorageAccountName $storageAccount -ContainerName $containerName -BlobPrefix $blobPrefix -StorageAccountKey $storageKey -AzCopyLocation $azCopyLocation
 }
 catch
@@ -195,7 +202,9 @@ catch
         Remove-AzureContainer -containerName $containerName -storageContext $storageContext -storageAccount $storageAccount
     }
 
-    throw "Upload to blob failed."
+    Write-Verbose $_.Exception.ToString() -Verbose
+    $error = $_.Exception.Message
+    throw (Get-LocalizedString -Key "Upload to container: '{0}' in storage account: '{1}' with blobprefix: '{2}' failed with error: '{3}'" -ArgumentList $containerName, $storageAccount, $blobPrefix, $error)
 }
 finally
 {
@@ -208,11 +217,11 @@ finally
         }
 
         $error = $uploadResponse.Error
-        throw "Upload to blob failed. $error"
+        throw (Get-LocalizedString -Key "Upload to container: '{0}' in storage account: '{1}' with blobprefix: '{2}' failed with error: '{3}'" -ArgumentList $containerName, $storageAccount, $blobPrefix, $error)
     }
     elseif ($uploadResponse.Status -eq "Succeeded")
     {
-        Write-Output "Uploaded files successfully from source path: $sourcePath to storage account: $storageAccount in container: $containerName with blobprefix: $blobPrefix" -Verbose
+        Write-Output (Get-LocalizedString -Key "Uploaded files successfully from source path: '{0}' to storage account: '{1}' in container: '{2}' with blobprefix: '{3}'" -ArgumentList $sourcePath, $storageAccount, $containerName, $blobPrefix)
     }
 }
 
@@ -223,6 +232,7 @@ if ($destination -eq "AzureBlob")
     return
 }
 
+$envOperationStatus = 'Passed'
 # copying files to azure vms
 try
 {
@@ -230,14 +240,15 @@ try
 
     $resources = Get-EnvironmentResources -EnvironmentName $environmentName -ResourceFilter $machineNames -Connection $connection -ErrorAction Stop
 
-    $envOperationId = Invoke-EnvironmentOperation -EnvironmentName $environmentName -OperationName "Azure File Copy" -Connection $connection -ErrorAction Stop
-    Write-Verbose "envOperationId = $envOperationId" -Verbose
+    $envOperationId = Invoke-EnvironmentOperation -EnvironmentName $environmentName -OperationName $azureFileCopyOperation -Connection $connection -ErrorAction Stop
+    Write-Verbose "Invoking Azure File Copy Operation on environment: $environmentName with operationId: $envOperationId" -Verbose
 
     $resourcesPropertyBag = Get-ResourcesProperties -resources $resources -connection $connection
 
     # create container sas token with full permissions
+    Write-Verbose "Generating SasToken for container: $containerName in storage: $storageAccount with expiry time: $defaultSasTokenTimeOutInHours hours" -Verbose
     $containerSasToken = New-AzureStorageContainerSASToken -Name $containerName -ExpiryTime (Get-Date).AddHours($defaultSasTokenTimeOutInHours) -Context $storageContext -Permission rwdl
-    Write-Verbose "Created SasToken: $containerSasToken for container: $containerName in storage: $storageAccount" -Verbose
+    Write-Verbose "Generated SasToken: $containerSasToken successfully for container: $containerName in storage: $storageAccount" -Verbose
 
     # copies files sequentially
     if ($copyFilesInParallel -eq "false" -or ( $resources.Count -eq 1 ))
@@ -247,7 +258,7 @@ try
             $resourceProperties = $resourcesPropertyBag.Item($resource.Name)
             $machine = $resourceProperties.fqdn
 
-            Write-Output "Copy Started for - $machine"
+            Write-Output (Get-LocalizedString -Key "Copy started for - '{0}'" -ArgumentList $machine)
 
             $resOperationId = Invoke-ResourceOperation -EnvironmentName $environmentName -ResourceName $resource.Name -EnvironmentOperationId $envOperationId -Connection $connection -ErrorAction Stop
             Write-Verbose "ResourceOperationId = $resOperationId" -Verbose
@@ -255,16 +266,19 @@ try
             $copyResponse = Invoke-Command -ScriptBlock $AzureFileCopyJob -ArgumentList $machine, $storageAccount, $containerName, $containerSasToken, $azCopyLocation, $targetPath, $resourceProperties.credential, $cleanTargetBeforeCopy, $resourceProperties.winrmPort, $resourceProperties.httpProtocolOption, $resourceProperties.skipCACheckOption
             $status = $copyResponse.Status
 
-            Write-ResponseLogs -operationName "copy" -fqdn $machine -deploymentResponse $copyResponse
-            Write-Output "Copy Status for machine $machine : $status"
+            Write-ResponseLogs -operationName $azureFileCopyOperation -fqdn $machine -deploymentResponse $copyResponse
+            Write-Output (Get-LocalizedString -Key "Copy status for machine '{0}' : '{1}'" -ArgumentList $machine, $status)
 
-            Write-Verbose "Complete ResourceOperation for  - $machine" -Verbose
+            Write-Verbose "Complete ResourceOperation for resource: $($resource.Name)" -Verbose
             $logs = Get-ResourceOperationLogs -deploymentResponse $copyResponse
             Complete-ResourceOperation -EnvironmentName $environmentName -EnvironmentOperationId $envOperationId -ResourceOperationId $resOperationId -Status $copyResponse.Status -ErrorMessage $copyResponse.Error -Logs $logs -Connection $connection -ErrorAction Stop
 
             if ($status -ne "Passed")
             {
+                Write-Verbose "Completed operation: $azureFileCopyOperation with operationId: $envOperationId on environment: $environmentName with status: Failed" -Verbose
                 Complete-EnvironmentOperation -EnvironmentName $environmentName -EnvironmentOperationId $envOperationId -Status "Failed" -Connection $connection -ErrorAction Stop
+                
+                Write-Verbose $copyResponse.Error.ToString() -Verbose
                 throw $copyResponse.Error
             }
         }
@@ -278,7 +292,7 @@ try
             $resourceProperties = $resourcesPropertyBag.Item($resource.Name)
             $machine = $resourceProperties.fqdn
 
-            Write-Output "Copy Started for - $machine"
+            Write-Output (Get-LocalizedString -Key "Copy started for - '{0}'" -ArgumentList $machine)
 
             $resOperationId = Invoke-ResourceOperation -EnvironmentName $environmentName -ResourceName $resource.Name -EnvironmentOperationId $envOperationId -Connection $connection -ErrorAction Stop
             Write-Verbose "ResourceOperationId = $resOperationId" -Verbose
@@ -307,10 +321,10 @@ try
                     $machineName = $Jobs.Item($job.Id).fqdn
                     $resOperationId = $Jobs.Item($job.Id).resOperationId
 
-                    Write-ResponseLogs -operationName "copy" -fqdn $machineName -deploymentResponse $output
-                    Write-Output "Copy Status for machine $machineName : $status"
+                    Write-ResponseLogs -operationName $azureFileCopyOperation -fqdn $machineName -deploymentResponse $output
+                    Write-Output (Get-LocalizedString -Key "Copy status for machine '{0}' : '{1}'" -ArgumentList $machine, $status)
 
-                    Write-Verbose "Complete ResourceOperation for machine: $machine" -Verbose
+                    Write-Verbose "Complete ResourceOperation for resource: $($resource.Name)" -Verbose
                     $logs = Get-ResourceOperationLogs -deploymentResponse $output
                     Complete-ResourceOperation -EnvironmentName $environmentName -EnvironmentOperationId $envOperationId -ResourceOperationId $resOperationId -Status $output.Status -ErrorMessage $output.Error -Logs $logs -Connection $connection -ErrorAction Stop
                 }
@@ -318,15 +332,16 @@ try
         }
     }
 
+    Write-Verbose "Completed operation: $azureFileCopyOperation with operationId: $envOperationId on environment: $environmentName with status: $envOperationStatus" -Verbose
     Complete-EnvironmentOperation -EnvironmentName $environmentName -EnvironmentOperationId $envOperationId -Status $envOperationStatus -Connection $connection -ErrorAction Stop
 
     if ($envOperationStatus -ne "Passed")
     {
-        throw "Copy to one or more machine failed."
+        throw (Get-LocalizedString -Key 'Copy to one or more machines failed')
     }
     else
     {
-        Write-Output "Copied files from source path: $sourcePath to target azure vms in environment: $environmentName successfully." -Verbose
+        Write-Output (Get-LocalizedString -Key "Copied files from source path: '{0}' to target azure vms in environment: '{1}' successfully" -ArgumentList $sourcePath, $environmentName)
     }
 }
 catch
