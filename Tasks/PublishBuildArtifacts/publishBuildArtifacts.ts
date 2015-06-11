e0e3c23f00249a66789272b4c5eaac28e11d3c3a
Yaswanth Raj <yaananth@microsoft.com>
2015-06-11 12:28:46 -0400

    publish artifacts associate to right location for fileshare

--- Tasks/PublishBuildArtifacts/publishBuildArtifacts.ts
+++ Tasks/PublishBuildArtifacts/publishBuildArtifacts.ts
@@ -195,6 +195,7 @@ else {
                 tl.command("artifact.upload", data, "Uploading artifact");
             }
             else if (artifactType === "filepath") {
+                targetPath = path.join(targetPath, artifactName);
                 tl.mkdirP(targetPath);
                 tl.cp("-Rf", stagingFolder, targetPath);
 
