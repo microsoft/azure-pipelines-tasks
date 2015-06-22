a4da7fb1a2cc50cd68f13c0021a2d8383ee675b9
abmodi <abmodi@microsoft.com>
2015-06-22 18:25:07 +0530

    removing localization

--- Tasks/DeployAzureResourceGroup/AzureResourceManagerHelper.ps1
+++ Tasks/DeployAzureResourceGroup/AzureResourceManagerHelper.ps1
@@ -43,7 +43,7 @@ function Create-AzureResourceGroup
 
                 foreach($error in $deploymentError)
                 {
-                    Write-Host (Get-LocalizedString -Key "'{0}'" -ArgumentList $error)
+                    Write-Host $error
                 }
 
                 Write-Host (Get-LocalizedString -Key "Resource group deployment '{0}' failed" -ArgumentList $resourceGroupName)
