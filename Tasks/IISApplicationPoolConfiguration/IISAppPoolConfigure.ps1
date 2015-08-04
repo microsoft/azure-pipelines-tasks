947eba99b16753a15ab8122b4303642aa573c80e
adsurang <adsurang@microsoft.com>
2015-08-04 10:45:20 +0530

    IISAppPoolTask

---
+++ Tasks/IISApplicationPoolConfiguration/IISAppPoolConfigure.ps1
@@ -0,0 +1,53 @@
+param (
+    [string]$environmentName,
+    [string]$resourceFilteringMethod,
+    [string]$machineNames,
+    [string]$applicationPoolName,
+    [string]$dotNetVersion,
+    [string]$pipeLineMode,
+    [string]$identity,
+    [string]$username,
+    [string]$password,
+    [string]$additionalArguments,
+    [string]$configureAppPoolInParallel
+    )
+
+Write-Verbose "Entering script IISAppPoolConfigure.ps1" -Verbose
+Write-Verbose "environmentName = $environmentName" -Verbose
+Write-Verbose "resourceFilteringMethod = $resourceFilteringMethod" -Verbose
+Write-Verbose "machineNames = $machineNames" -Verbose
+Write-Verbose "applicationPoolName = $applicationPoolName" -Verbose
+Write-Verbose "dotNetVersion = $dotNetVersion" -Verbose
+Write-Verbose "pipeLineMode = $pipeLineMode" -Verbose
+Write-Verbose "identity = $identity" -Verbose
+Write-Verbose "additionalArguments = $additionalArguments" -Verbose
+Write-Verbose "configureAppPoolInParallel = $configureAppPoolInParallel" -Verbose
+
+
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.DevTestLabs"
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Deployment.Internal"
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Deployment.RemoteDeployment"
+
+#Triming Application pool name, username , password and additional arguments
+$applicationPoolName = $applicationPoolName.Trim()
+$username = $username.Trim()
+$password =$password.Trim()
+$additionalArguments = $additionalArguments.Trim()
+#Extra handling beacuse of Bug 390666:Invoke-RemoteDeployment Method fails if the script arguments contain double quotes
+$additionalArguments = $additionalArguments.Replace("`"","'")
+
+$iisAppPoolConfigBlock = Get-Content ./IisAppPoolConfigOnTargetMachine.ps1 | Out-String
+$ScriptArguments =  "-applicationPoolName `"$applicationPoolName`" -dotNetVersion `"$dotNetVersion`" -pipeLineMode $pipeLineMode -identity $identity -username `"$username`" -password `"$password`" -additionalArguments `"$additionalArguments`""
+
+Write-Host (Get-LocalizedString -Key "Starting configuring IIS application pool with name : {0}" -ArgumentList  $applicationPoolName)
+if($resourceFilteringMethod -eq "tags")
+{
+    Invoke-RemoteDeployment -environmentName $environmentName -tags $machineFilter -scriptBlock $iisAppPoolConfigBlock -scriptArguments $ScriptArguments -runPowershellInParallel $deployInParallel
+}
+else
+{
+    Invoke-RemoteDeployment -environmentName $environmentName -machineNames $machineFilter -scriptBlock $iisAppPoolConfigBlock -scriptArguments $ScriptArguments  -runPowershellInParallel $deployInParallel
+}
+Write-Host (Get-LocalizedString -Key "Successfully configured IIS application pool with name : {0}" -ArgumentList  $applicationPoolName)
