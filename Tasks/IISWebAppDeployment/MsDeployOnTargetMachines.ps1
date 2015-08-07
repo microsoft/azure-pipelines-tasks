a8fc90de9dc278d7922655c8525f9d8567a5d33b
Subrahmanyam Mandavilli <subraman@microsoft.com>
2015-08-07 15:48:42 +0530

    Updated Readme links to forward links

--- Tasks/IISWebAppDeployment/MsDeployOnTargetMachines.ps1
+++ Tasks/IISWebAppDeployment/MsDeployOnTargetMachines.ps1
@@ -15,7 +15,7 @@ function ThrowError
 {
     param([string]$errorMessage)
 
-        $readmelink = "https://github.com/Microsoft/vso-agent-tasks/blob/master/Tasks/IISWebAppDeployment/README.md"
+        $readmelink = "http://aka.ms/iiswebappdeployreadme"
         $helpMessage = [string]::Format("For more info please refer to {0}", $readmelink)
         throw "$errorMessage $helpMessage"
 }
