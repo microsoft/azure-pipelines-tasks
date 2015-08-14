fb67ae7c37951854b5591ff0058d1486dd83b14f
Subrahmanyam Mandavilli <subraman@microsoft.com>
2015-08-14 11:40:54 +0530

    Adding Create WebSite support for MsDeploy Task

--- Tasks/IISWebAppDeployment/DeployIISWebApp.ps1
+++ Tasks/IISWebAppDeployment/DeployIISWebApp.ps1
@@ -2,13 +2,26 @@
     [string]$environmentName,
     [string]$resourceFilteringMethod,
     [string]$machineFilter,
-    [string]$webDeployPackage,
+    [string]$webDeployPackage,    
     [string]$webDeployParamFile,
     [string]$overRideParams,
-    [string]$deployInParallel
+    [string]$webSiteName,
+    [string]$appPoolName,
+    [string]$webSitePhysicalPath,
+    [string]$webSitePhysicalPathAuth,
+    [string]$webSiteAuthUserName,
+    [string]$webSiteAuthUserPassword,
+    [string]$addBinding,
+    [string]$protocol,
+    [string]$ipAddress,
+    [string]$port,
+    [string]$hostName,
+    [string]$serverNameIndication,
+    [string]$sslCertThumbPrint,
+    [string]$appCmdArgs,
+    [string]$deployInParallel    
     )
 
-$ErrorActionPreference = 'Stop'
 Write-Verbose "Entering script DeployIISWebApp.ps1" -Verbose
 Write-Verbose "environmentName = $environmentName" -Verbose
 Write-Verbose "resourceFilteringMethod = $resourceFilteringMethod" -Verbose
@@ -18,6 +31,22 @@ Write-Verbose "webDeployParamFile = $webDeployParamFile" -Verbose
 Write-Verbose "overRideParams = $overRideParams" -Verbose
 Write-Verbose "deployInParallel = $deployInParallel" -Verbose
 
+Write-Verbose "webSiteName = $webSiteName" -Verbose
+Write-Verbose "appPoolName = $appPoolName" -Verbose
+Write-Verbose "webSitePhysicalPath = $webSitePhysicalPath" -Verbose
+Write-Verbose "webSitePhysicalPathAuth = $webSitePhysicalPathAuth" -Verbose
+Write-Verbose "webSiteAuthUserName = $webSiteAuthUserName" -Verbose
+Write-Verbose "webSiteAuthUserPassword = $webSiteAuthUserPassword" -Verbose
+Write-Verbose "addBinding = $addBinding" -Verbose
+Write-Verbose "protocol = $protocol" -Verbose
+Write-Verbose "ipAddress = $ipAddress" -Verbose
+Write-Verbose "port = $port" -Verbose
+Write-Verbose "hostName = $hostName" -Verbose
+Write-Verbose "serverNameIndication = $serverNameIndication" -Verbose
+Write-Verbose "sslCertThumbPrint = $sslCertThumbPrint" -Verbose
+Write-Verbose "appCmdArgs = $appCmdArgs" -Verbose
+
+
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.DevTestLabs"
@@ -26,9 +55,16 @@ Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Deployment.RemoteDe
 
 $webDeployPackage = $webDeployPackage.Trim('"', ' ')
 $webDeployParamFile = $webDeployParamFile.Trim('"', ' ')
+$webSiteName = $webSiteName.Trim('"', ' ')
+$appPoolName = $appPoolName.Trim('"', ' ')
+$webSitePhysicalPath = $webSitePhysicalPath.Trim('"', ' ')
+
 
 $msDeployOnTargetMachinesBlock = Get-Content  ./MsDeployOnTargetMachines.ps1 | Out-String
-$scriptArgs = [string]::Format(' -WebDeployPackage "{0}" -WebDeployParamFile "{1}" -OverRideParams {2} -MethodToInvoke Deploy-WebSite', $webDeployPackage, $webDeployParamFile, $overRideParams)
+#$scriptArgs = [string]::Format(' -WebDeployPackage "{0}" -WebDeployParamFile "{1}" -OverRideParams {2}  -MethodToInvoke Execute-Main -WebSiteName "{3}" -AppPoolName "{4}" -WebSitePhysicalPath "{5}" -WebSitePhysicalPathAuth {6} -WebSiteAuthUserName {7} -WebSiteAuthUserPassword {8} -AddBinding {9} -Protocol {10} -IpAddress {11} -Port {12} -HostName {13} -ServerNameIndication {14} -SslCertThumbPrint {15} -AppCmdArgs {16}', $webDeployPackage, $webDeployParamFile, $overRideParams, $webSiteName, $appPoolName, $webSitePhysicalPath, $webSitePhysicalPathAuth, $webSiteAuthUserName, $webSiteAuthUserPassword, $addBinding, $protocol, $ipAddress, $port, $hostName, $serverNameIndication, $sslCertThumbPrint, $appCmdArgs )
+
+$scriptArgs = [string]::Format(' -WebDeployPackage "{0}" -WebDeployParamFile "{1}" -OverRideParams {2}  -WebSiteName "{3}" -AppPoolName "{4}" -WebSitePhysicalPath "{5}" -WebSitePhysicalPathAuth {6} -WebSiteAuthUserName {7} -WebSiteAuthUserPassword {8} -AddBinding {9} -Protocol {10} -IpAddress "{11}" -Port {12} -HostName {13} -ServerNameIndication {14} -SslCertThumbPrint {15} -AppCmdArgs {16} -MethodToInvoke Execute-Main', $webDeployPackage, $webDeployParamFile, $overRideParams, $webSiteName, $appPoolName,  $webSitePhysicalPath, $webSitePhysicalPathAuth, $webSiteAuthUserName, $webSiteAuthUserPassword, $addBinding, $protocol, $ipAddress, $port, $hostName, $serverNameIndication, $sslCertThumbPrint, $appCmdArgs)
+
 Write-Verbose "MsDeployOnTargetMachines Script Arguments : $scriptArgs" -Verbose
 
 Write-Output ( Get-LocalizedString -Key "Starting deployment of IIS Web Deploy Package : {0}" -ArgumentList $webDeployPackage)
--- Tasks/IISWebAppDeployment/MsDeployOnTargetMachines.ps1
+++ Tasks/IISWebAppDeployment/MsDeployOnTargetMachines.ps1
@@ -2,6 +2,20 @@
     [string]$WebDeployPackage,
     [string]$WebDeployParamFile,
     [string]$OverRideParams,
+    [string]$WebSiteName,
+    [string]$AppPoolName,
+    [string]$WebSitePhysicalPath,
+    [string]$WebSitePhysicalPathAuth,
+    [string]$WebSiteAuthUserName,
+    [string]$WebSiteAuthUserPassword,
+    [string]$AddBinding,
+    [string]$Protocol,
+    [string]$IpAddress,
+    [string]$Port,
+    [string]$HostName,
+    [string]$ServerNameIndication,
+    [string]$SslCertThumbPrint,
+    [string]$AppCmdArgs,
     [string]$MethodToInvoke
     )
 
@@ -9,7 +23,26 @@ Write-Verbose "Entering script MsDeployOnTargetMachines.ps1" -Verbose
 Write-Verbose "WebDeployPackage = $WebDeployPackage" -Verbose
 Write-Verbose "WebDeployParamFile = $WebDeployParamFile" -Verbose
 Write-Verbose "OverRideParams = $OverRideParams" -Verbose
+
+Write-Verbose "WebSiteName = $WebSiteName" -Verbose
+Write-Verbose "AppPoolName = $AppPoolName" -Verbose
+Write-Verbose "WebSitePhysicalPath = $WebSitePhysicalPath" -Verbose
+Write-Verbose "WebSitePhysicalPathAuth = $WebSitePhysicalPathAuth" -Verbose
+Write-Verbose "WebSiteAuthUserName = $WebSiteAuthUserName" -Verbose
+Write-Verbose "WebSiteAuthUserPassword = $WebSiteAuthUserPassword" -Verbose
+Write-Verbose "AddBinding = $AddBinding" -Verbose
+Write-Verbose "Protocol = $Protocol" -Verbose
+Write-Verbose "IpAddress = $IpAddress" -Verbose
+Write-Verbose "Port = $Port" -Verbose
+Write-Verbose "HostName = $HostName" -Verbose
+Write-Verbose "ServerNameIndication = $ServerNameIndication" -Verbose
+Write-Verbose "SslCertThumbPrint = $SslCertThumbPrint" -Verbose
+Write-Verbose "AppCmdArgs = $AppCmdArgs" -Verbose
+
 Write-Verbose "MethodToInvoke = $MethodToInvoke" -Verbose
+
+$AppCmdRegKey = "HKLM:\SOFTWARE\Microsoft\InetStp"
+$MsDeployInstallPathRegKey = "HKLM:\SOFTWARE\Microsoft\IIS Extensions\MSDeploy"
 
 function ThrowError
 {
@@ -18,6 +51,31 @@ function ThrowError
         $readmelink = "http://aka.ms/iiswebappdeployreadme"
         $helpMessage = [string]::Format("For more info please refer to {0}", $readmelink)
         throw "$errorMessage $helpMessage"
+}
+
+function Run-Command
+{
+    param(
+        [string]$command
+    )
+
+    $result = cmd.exe /c "`"$command`""
+
+    if(-not ($LASTEXITCODE -eq 0))
+    {
+        throw $result
+    }     
+    
+    return $result   
+}
+
+function IsInputNullOrEmpty
+{
+    param(
+        [string]$str
+    )
+
+    return ([string]::IsNullOrEmpty($str) -or $str -eq "`"`"")
 }
 
 function Get-MsDeployLocation
@@ -45,6 +103,40 @@ function Get-MsDeployLocation
     return (Join-Path $path msDeploy.exe)
 }
 
+function Get-AppCmdLocation
+{
+    param(
+    [Parameter(Mandatory=$true)]
+    [string]$regKeyPath
+    )
+    
+    $appCmdNotFoundError = "Can not find appcmd.exe location. Verify IIS is configured on $env:ComputeName and try operation again."
+    $appCmdMinVersionError = "Version of IIS is less than 7.0 on machine $env:COMPUTERNAME. Minimum version of IIS required is 7.0"
+    
+    try
+    {
+        $regKey = Get-Item -Path $regKeyPath
+        $path = $regKey.GetValue("InstallPath")
+        $version = $regKey.GetValue("MajorVersion")
+        
+        if($version -le "6.0")
+        {
+            ThrowError -errorMessage $appCmdMinVersionError
+        }
+        
+        if( -not (Test-Path $path))
+        {
+            ThrowError -errorMessage $appCmdNotFoundError 
+        }
+    }
+    catch
+    {
+        ThrowError -errorMessage $appCmdNotFoundError
+    }
+
+    return (Join-Path $path appcmd.exe)
+}
+
 function Get-MsDeployCmdArgs
 {
     param(
@@ -59,29 +151,277 @@ function Get-MsDeployCmdArgs
     $overRideParams = $overRideParams.Trim()
     
     $msDeployCmdArgs = [string]::Empty
-    if(-not [string]::IsNullOrEmpty($webDeployParamFile) -and $webDeployParamFile -ne "`"`"")
+    if(-not (IsInputNullOrEmpty -str $webDeployParamFile))
     {    
         $msDeployCmdArgs = [string]::Format(' -setParamFile={0}', $webDeployParamFile)
     }
 
-    if(-not [string]::IsNullOrEmpty($overRideParams) -and $overRideParams -ne "`"`"")
+    if(-not (IsInputNullOrEmpty -str $overRideParams))
     {
-        $msDeployCmdArgs = [string]::Format("{0} -setParam:{1}", $msDeployCmdArgs, $overRideParams)
+        $msDeployCmdArgs = [string]::Format('{0} -setParam:{1}', $msDeployCmdArgs, $overRideParams)
     }
     
     $msDeployCmdArgs = [string]::Format(' -verb:sync -source:package={0} {1} -dest:auto -verbose -retryAttempts:3 -retryInterval:3000', $webDeployPackage, $msDeployCmdArgs)
     return $msDeployCmdArgs
 }
 
-function Deploy-WebSite
+function Does-WebSiteExists
 {
-    $msDeployInstallPathRegKey = "HKLM:\SOFTWARE\Microsoft\IIS Extensions\MSDeploy"
-    $msDeployExePath = Get-MsDeployLocation -regKeyPath $msDeployInstallPathRegKey
-    $msDeployCmdArgs = Get-MsDeployCmdArgs -webDeployPackage $WebDeployPackage -webDeployParamFile $WebDeployParamFile -overRideParams $OverRideParams
+    param([string] $siteName)
+
+    $appCmdPath = Get-AppCmdLocation -regKeyPath $AppCmdRegKey
+    $appCmdArgs = [string]::Format(' list site /name:{0}',$siteName)
+    $command = "`"$appCmdPath`" $appCmdArgs"
+    Write-Verbose "Checking WebSite Exists. Running Command : $command"
+    
+    $webSite = cmd.exe /c "`"$command`""
+    
+    if($webSite -ne $null)
+    {
+        return $true
+    }
+    
+    return $false
+}
+
+function Is-HttpsChangedToHttp
+{
+    param(
+        [string]$siteName,
+        [string]$protocal
+    )
+
+    $appCmdPath = Get-AppCmdLocation -regKeyPath $AppCmdRegKey
+    $appCmdArgs = [string]::Format(' list site /name:{0}',$siteName)
+    $command = "`"$appCmdPath`" $appCmdArgs"
+    Write-Verbose "Checking Protocal changed from Https to Http. Running Command : $command"
+        
+    $result = cmd.exe /c "`"$command`""
+
+    if($protocal -eq "http" -and $result.Contains("https"))
+    {
+        return $true
+    }
+
+    return $false
+}
+
+function Set-SslFlags
+{
+    param(
+        [string]$siteName,
+        [string]$sni,
+        [string]$sslFlag
+    )
+
+    if($sni -eq "false" -or (IsInputNullOrEmpty -str $sni))
+    {
+        Write-Verbose "Enable SNI is set to false .. returning" -Verbose
+        return
+    }
+
+    $appCmdPath = Get-AppCmdLocation -regKeyPath $AppCmdRegKey 
+    $appCmdArgs = [string]::Format(' set config {0} /section:access -sslFlags:"{1}" /commit:AppHost',$siteName, $sslFlag)
+    $command = "`"$appCmdPath`" $appCmdArgs"       
+    
+    Write-Verbose "Setting Ssl Flags. Running Command : $command"    
+    Run-Command -command $command
+}
+
+function Is-Port-Or-Cert-Changed
+{
+    param(
+        [string]$port,
+        [string]$certhash,
+        $result
+    )
+
+    $isPortChanged = ($result.Get(4).Contains([string]::Format("0.0.0.0:{0}", $port)) -eq $false)
+    $isCertChanged = ($result.Get(5).Contains([string]::Format("{0}", $certhash)) -eq $false)
+    return ($isPortChanged -or $isCertChanged)
+
+}
+
+function Add-Or-Remove-SslCert
+{
+    param(
+        [string]$port,
+        [string]$certhash,
+        [string]$action
+    )
+
+    if((IsInputNullOrEmpty -str $certhash) -and $action -eq "add")
+    {
+        Write-Verbose "CertHash is empty .. returning" -Verbose
+        return
+    }
+
+    $command = [string]::Format("netsh http show sslcert ipport=0.0.0.0:{0}", $port)
+    Write-Verbose "Checking SslCert binding already Present. Running Command : $command" -Verbose
+    $result = cmd.exe /c "`"$command`""
+
+    if( (Is-Port-Or-Cert-Changed -port $port -certhash $certhash -result $result) -and $action -eq "add")
+    {
+        Write-Verbose "SSL Cert binding already present.. returning" -Verbose
+        return
+    }
+
+    if($action -eq "add")
+    {
+        $command = [string]::Format("netsh http add sslcert ipport=0.0.0.0:{0} certhash={1} appid={{{2}}}", $port, $certhash, [System.Guid]::NewGuid().toString())
+        Write-Verbose "Setting SslCert for Web Site. Running Command: $command" -Verbose
+    }
+    else
+    {
+        $command = [string]::Format("netsh http delete sslcert ipport=0.0.0.0:{0}", $port)
+        Write-Verbose "Removing SslCert for Web Site. Running Command: $command" -Verbose
+    }    
+    
+    Run-Command -command $command
+}
+
+function Deploy-WebSite
+{    
+    param(
+        [string]$webDeployPkg,
+        [string]$webDeployParamFile,
+        [string]$overRiderParams
+    )
+
+    $msDeployExePath = Get-MsDeployLocation -regKeyPath $MsDeployInstallPathRegKey
+    $msDeployCmdArgs = Get-MsDeployCmdArgs -webDeployPackage $webDeployPkg -webDeployParamFile $webDeployParamFile -overRideParams $overRiderParams
 
     $msDeployCmd = "`"$msDeployExePath`" $msDeployCmdArgs"
-    Write-Verbose "Executing MSDeploy Command: $msDeployCmd" -Verbose
-    cmd.exe /c "`"$msDeployCmd`""
+    Write-Verbose "Deploying WebSite. Running Command: $msDeployCmd" -Verbose
+    Run-Command -command $msDeployCmd
+}
+
+function Create-WebSite
+{
+    param(
+    [string]$siteName,
+    [string]$physicalPath
+    )
+
+    $appCmdPath = Get-AppCmdLocation -regKeyPath $AppCmdRegKey 
+    $appCmdArgs = [string]::Format(' add site /name:{0} /physicalPath:{1}',$siteName, $physicalPath)
+    $command = "`"$appCmdPath`" $appCmdArgs"       
+    
+    Write-Verbose "Creating WebSite. Running Command : $command"    
+    Run-Command -command $command
+}
+
+function Update-WebSite
+{
+    param(
+        [string]$siteName,
+        [string]$appPoolName,
+        [string]$physicalPath,
+        [string]$authType,
+        [string]$userName,
+        [string]$password,
+        [string]$addBinding,
+        [string]$protocal,
+        [string]$ipAddress,
+        [string]$port,
+        [string]$hostname,
+        [string]$additionalArgs
+    )
+
+    $appCmdArgs = [string]::Format(' set site /site.name:{0}', $siteName)
+
+    if(-not (IsInputNullOrEmpty -str $appPoolName))
+    {    
+        $appCmdArgs = [string]::Format('{0} -applicationDefaults.applicationPool:{1}', $appCmdArgs, $appPoolName)
+    }
+
+    if(-not (IsInputNullOrEmpty -str $physicalPath))
+    {
+        $appCmdArgs = [string]::Format("{0} -[path='/'].[path='/'].physicalPath:{1}", $appCmdArgs, $physicalPath)
+    }
+
+    if(-not (IsInputNullOrEmpty -str $userName) -and $authType -eq "WebSiteWindowsAuth")
+    {
+        $appCmdArgs = [string]::Format("{0} -[path='/'].[path='/'].userName:{1}", $appCmdArgs, $userName)
+    }
+
+    if(-not (IsInputNullOrEmpty -str $password) -and $authType -eq "WebSiteWindowsAuth")
+    {
+        $appCmdArgs = [string]::Format("{0} -[path='/'].[path='/'].password:{1}", $appCmdArgs, $password)
+    }
+
+    if($ipAddress -eq "`"All Unassigned`"")
+    {
+        $ipAddress = "*"
+    }
+
+    if($addBinding -eq "true")
+    {
+        $appCmdArgs = [string]::Format("{0} /bindings:{1}/{2}:{3}:{4}", $appCmdArgs, $protocal, $ipAddress, $port, $hostname)    
+    }    
+
+    if(-not (IsInputNullOrEmpty -str $additionalArgs))
+    {
+        $appCmdArgs = [string]::Format("{0} {1}", $appCmdArgs, $additionalArgs)
+    }
+        
+    $appCmdPath = Get-AppCmdLocation -regKeyPath $appCmdRegKey    
+    $command = "`"$appCmdPath`" $appCmdArgs"       
+    
+    Write-Verbose "Updating WebSite Properties. Running Command : $command"    
+    Run-Command -command $command
+}
+
+function Create-Or-Update-WebSite
+{
+    param(
+        [string]$siteName,
+        [string]$appPoolName,
+        [string]$physicalPath,
+        [string]$authType,
+        [string]$userName,
+        [string]$password,
+        [string]$addBinding,
+        [string]$protocal,
+        [string]$ipAddress,
+        [string]$port,
+        [string]$hostname,        
+        [string]$additionalArgs
+    )
+
+    $doesWebSiteExists = Does-WebSiteExists -siteName $siteName
+    
+    if( -not $doesWebSiteExists)
+    {
+        Create-WebSite -siteName $siteName -physicalPath $physicalPath        
+    }
+    elseif(Is-HttpsChangedToHttp -siteName $siteName -protocal $protocal)
+    {
+        Set-SslFlags -siteName $WebSiteName -sni "true" -sslFlag "None"
+        Add-Or-Remove-SslCert -port $Port -certhash $SslCertThumbPrint -action "delete"
+    }
+
+    Update-WebSite -siteName $siteName -appPoolName $appPoolName -physicalPath $physicalPath -authType $authType -userName $userName -password $password `
+    -addBinding $addBinding -protocal $protocal -ipAddress $ipAddress -port $port -hostname $hostname -additionalArgs $additionalArgs
+}
+
+
+function Execute-Main
+{
+    Write-Verbose "Entering Execute-Main function" -Verbose
+
+    if(-not (IsInputNullOrEmpty -str $WebSiteName))
+    {    
+        Create-Or-Update-WebSite -siteName $WebSiteName -appPoolName $AppPoolName -physicalPath $WebSitePhysicalPath -authType $WebSitePhysicalPathAuth -userName $WebSiteAuthUserName `
+         -password $WebSiteAuthUserPassword -addBinding $AddBinding -protocal $Protocol -ipAddress $IpAddress -port $Port -hostname $HostName -additionalArgs $AppCmdArgs
+
+        Add-Or-Remove-SslCert -port $Port -certhash $SslCertThumbPrint -action "add"
+        Set-SslFlags -siteName $WebSiteName -sni $ServerNameIndication -sslFlag "Ssl"
+    }
+
+    Deploy-WebSite -webDeployPkg $WebDeployPackage -webDeployParamFile $WebDeployParamFile -overRiderParams $OverRideParams
+
+    Write-Verbose "Existing Execute-Main function" -Verbose
 }
 
 Invoke-Expression $MethodToInvoke
