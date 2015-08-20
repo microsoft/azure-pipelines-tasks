639f5bbf5dfae1c4fe76d3a86a2263f77950a13a
Leah Antkiewicz <leantk@microsoft.com>
2015-08-20 09:52:00 -0400

    Adding option to specify JDK path for Maven Task

--- Tasks/Maven/maven.ps1
+++ Tasks/Maven/maven.ps1
@@ -4,8 +4,10 @@
     [string]$goals,
     [string]$publishJUnitResults,   
     [string]$testResultsFiles, 
+    [string]$javaHomeSelection,
     [string]$jdkVersion,
-    [string]$jdkArchitecture
+    [string]$jdkArchitecture,
+    [string]$jdkUserInputPath
 )
 
 Write-Verbose 'Entering Maven.ps1'
@@ -14,8 +16,10 @@ Write-Verbose "options = $options"
 Write-Verbose "goals = $goals"
 Write-Verbose "publishJUnitResults = $publishJUnitResults"
 Write-Verbose "testResultsFiles = $testResultsFiles"
+Write-Verbose "javaHomeSelection = $javaHomeSelection"
 Write-Verbose "jdkVersion = $jdkVersion"
 Write-Verbose "jdkArchitecture = $jdkArchitecture"
+Write-Verbose "jdkUserInputPath = $jdkUserInputPath"
 
 #Verify Maven POM file is specified
 if(!$mavenPOMFile)
@@ -28,14 +32,37 @@ import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.TestResults"
 
-if($jdkVersion -and $jdkVersion -ne "default")
+# If JAVA_HOME is being set by choosing a JDK version find the path to that specified version else use the path given by the user
+$jdkPath = $null
+if($javaHomeSelection -eq 'JDKVersion')
 {
-    $jdkPath = Get-JavaDevelopmentKitPath -Version $jdkVersion -Arch $jdkArchitecture
-    if (!$jdkPath) 
+    Write-Verbose "Using JDK version to find and set JAVA_HOME"
+    # If the JDK version is not the deafult set the jdkPath to the new JDK version selected
+    if($jdkVersion -and $jdkVersion -ne "default")
     {
-        throw "Could not find JDK $jdkVersion $jdkArchitecture, please make sure the selected JDK is installed properly"
+        $jdkPath = Get-JavaDevelopmentKitPath -Version $jdkVersion -Arch $jdkArchitecture
+        if (!$jdkPath) 
+        {
+            throw "Could not find JDK $jdkVersion $jdkArchitecture, please make sure the selected JDK is installed properly"
+        }
     }
+}
+else
+{
+    Write-Verbose "Using path from user input to set JAVA_HOME"
+    if($jdkUserInputPath -and (Test-Path -Path $jdkUserInputPath))
+    {
+        $jdkPath = $jdkUserInputPath
+    }
+    else
+    {
+         throw "Path given for the JDK does not exist. Please make sure to enter in a valid path."
+    }
+}
 
+# If jdkPath is set to something other than the default then update JAVA_HOME
+if ($jdkPath)
+{
     Write-Host "Setting JAVA_HOME to $jdkPath"
     $env:JAVA_HOME = $jdkPath
     Write-Verbose "JAVA_HOME set to $env:JAVA_HOME"
