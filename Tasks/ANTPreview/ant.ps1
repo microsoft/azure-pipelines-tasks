61fc3c60f2d1445aaeb88aa8a912d2dbb49223e3
ashwini reddy <achalla@microsoft.com>
2015-09-07 17:55:21 +0530

    enable cc for ant

--- Tasks/ANTPreview/ant.ps1
+++ Tasks/ANTPreview/ant.ps1
@@ -7,11 +7,10 @@
     [string]$jdkVersion,
     [string]$jdkArchitecture,
 	
-    [string]$publishCodeCoverageResults,
     [string]$codeCoverageTool,
-    [string]$summaryFileLocation,
-    [string]$reportDirectory,
-    [string]$additionalCodeCoverageFiles
+    [string]$classFilesDirectory,
+    [string]$classFilter,
+    [string]$srcDirectory
 )
 
 Write-Verbose 'Entering Ant.ps1'
@@ -23,12 +22,15 @@ Write-Verbose "testResultsFiles = $testResultsFiles"
 Write-Verbose "jdkVersion = $jdkVersion"
 Write-Verbose "jdkArchitecture = $jdkArchitecture"
 
-Write-Verbose "publishCodeCoverageResults = $publishCodeCoverageResults"
-Write-Verbose "codeCoverageTool = $codeCoverageTool"
-Write-Verbose "summaryFileLocation = $summaryFileLocation"
-Write-Verbose "reportDirectory = $reportDirectory"
-Write-Verbose "additionalCodeCoverageFiles = $additionalCodeCoverageFiles"
-
+$isCoverageEnabled = !$codeCoverageTool.equals("NoCoverage")
+if($isCoverageEnabled)
+{
+    Write-Verbose "codeCoverageTool = $codeCoverageTool"
+    Write-Verbose "classFilesDirectory = $classFilesDirectory"
+    Write-Verbose "classFilter = $classFilter"
+    Write-Verbose "srcDirectory = $srcDirectory"
+}
+	
 #Verify Ant build file is specified
 if(!$antBuildFile)
 {
@@ -53,14 +55,45 @@ if($jdkVersion -and $jdkVersion -ne "default")
     Write-Verbose "JAVA_HOME set to $env:JAVA_HOME"
 }
 
+$buildRootPath = Split-Path $antBuildFile -Parent
+$reportDirectoryName = "CodeCoverage"
+$reportDirectory = Join-Path $buildRootPath $reportDirectoryName
+
+if(Test-Path $reportDirectory)
+{
+   # delete any previous code coverage data 
+   rm -r $reportDirectory -force 
+}
+
+$summaryFileName = "summary.xml"
+$summaryFile = Join-Path $buildRootPath $reportDirectoryName 
+$summaryFile = Join-Path $summaryFile $summaryFileName
+# ensuring unique code coverage report task name by using guid
+$CCReportTask = "CodeCoverage_" +[guid]::NewGuid()
+
+if($isCoverageEnabled)
+{
+   # Enable code coverage in build file
+   Enable-CodeCoverage -BuildTool 'Ant' -BuildFile $antBuildFile -CodeCoverageTool $codeCoverageTool -ClassFilter $classFilter -ClassFilesDirectory $classFilesDirectory -SourceDirectory $srcDirectory -SummaryFile $summaryFileName -ReportDirectory $reportDirectoryName -CCReportTask $CCReportTask
+   Write-Verbose "code coverage is successfully enabled."
+}
+	
+
 Write-Verbose "Running Ant..."
 Invoke-Ant -AntBuildFile $antBuildFile -Options $options -Targets $targets
+
+if($isCoverageEnabled)
+{
+   # run report code coverage task which generates code coverage reports.
+   Write-Verbose "Reporting code coverage"
+   Invoke-Ant -AntBuildFile $antBuildFile -Targets $CCReportTask
+}
 
 # Publish test results files
 $publishJUnitResultsFromAntBuild = Convert-String $publishJUnitResults Boolean
 if($publishJUnitResultsFromAntBuild)
 {
-   # check for JUnit test result files
+    # check for JUnit test result files
     $matchingTestResultsFiles = Find-Files -SearchPattern $testResultsFiles
     if (!$matchingTestResultsFiles)
     {
@@ -77,18 +110,10 @@ else
     Write-Verbose "Option to publish JUnit Test results produced by Ant build was not selected and is being skipped."
 }
 
-
-if($publishCodeCoverageResults)
+# check if code coverage has been enabled
+if($isCoverageEnabled)
 {
-   # Publish Code Coverage Files
-   $codeCoverageFiles = Find-Files -SearchPattern $additionalCodeCoverageFiles
-   
-   if(-not $codeCoverageFiles)
-   {
-      Write-Warning "No code coverage files matching pattern '$additionalCodeCoverageFiles' were found."  
-   }
-   
-   Publish-CodeCoverage -CodeCoverageTool $codeCoverageTool -SummaryFileLocation $summaryFileLocation -ReportDirectory $reportDirectory -AdditionalCodeCoverageFiles $codeCoverageFiles -Context $distributedTaskContext    
+   Publish-CodeCoverage -CodeCoverageTool $codeCoverageTool -SummaryFileLocation $summaryFile -ReportDirectory $reportDirectory -Context $distributedTaskContext    
 }
 else
 {
