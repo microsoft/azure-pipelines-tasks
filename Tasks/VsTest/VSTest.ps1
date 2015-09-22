6182cd00f967a71b743f8a9922f178a35bbf0c9c
Kavipriya Adhinarayanan <kaadhina@microsoft.com>
2015-09-22 20:37:17 +0530

    Moving get for build sources.
    Scenarios with no search pattern need this fix.

--- Tasks/VsTest/VSTest.ps1
+++ Tasks/VsTest/VSTest.ps1
@@ -24,22 +24,23 @@ if (!$testAssembly)
     throw (Get-LocalizedString -Key "Test assembly parameter not set on script")
 }
 
+$sourcesDirectory = Get-TaskVariable -Context $distributedTaskContext -Name "Build.SourcesDirectory"
+if(!$sourcesDirectory)
+{
+    # For RM, look for the test assemblies under the release directory.
+    $sourcesDirectory = Get-TaskVariable -Context $distributedTaskContext -Name "Agent.ReleaseDirectory"
+}
+if(!$sourcesDirectory)
+{
+    # If there is still no sources directory, error out immediately.
+    throw "No source directory found."
+}
+
 # check for solution pattern
 if ($testAssembly.Contains("*") -or $testAssembly.Contains("?"))
 {
     Write-Verbose "Pattern found in solution parameter. Calling Find-Files."
-    Write-Verbose "Calling Find-Files with pattern: $testAssembly"
-    $sourcesDirectory = Get-TaskVariable -Context $distributedTaskContext -Name "Build.SourcesDirectory"
-    if(!$sourcesDirectory)
-    {
-        # For RM, look for the test assemblies under the release directory.
-        $sourcesDirectory = Get-TaskVariable -Context $distributedTaskContext -Name "Agent.ReleaseDirectory"
-    }
-    if(!$sourcesDirectory)
-    {
-        # If there is still no sources directory, error out immediately.
-        throw "No source directory found."
-    }
+    Write-Verbose "Calling Find-Files with pattern: $testAssembly"    
     $testAssemblyFiles = Find-Files -SearchPattern $testAssembly -RootFolder $sourcesDirectory
     Write-Verbose "Found files: $testAssemblyFiles"
 }
