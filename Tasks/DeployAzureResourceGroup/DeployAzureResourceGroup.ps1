6fe0202569aa60066838efcb85b83bff04903052
Niadak <niadak@microsoft.com>
2015-11-27 15:07:11 +0530

    Adding Telemetry points to AzureRG tasks
    Adding Telemetry points to AzureRG tasks

--- Tasks/DeployAzureResourceGroup/DeployAzureResourceGroup.ps1
+++ Tasks/DeployAzureResourceGroup/DeployAzureResourceGroup.ps1
@@ -32,6 +32,7 @@ $csmFile = $csmFile.Trim('"', ' ')
 $csmParametersFile = $csmParametersFile.Trim('"', ' ')
 $overrideParameters = $overrideParameters.Trim()
 $outputVariable = $outputVariable.Trim()
+$telemetrySet = $false
 
 import-module Microsoft.TeamFoundation.DistributedTask.Task.Internal
 import-module Microsoft.TeamFoundation.DistributedTask.Task.Common
@@ -54,55 +55,68 @@ if(-not $UnderTestCondition)
     Import-Module ./AzureUtility.ps1 -Force
 }
 
-Initialize-GlobalMaps
-
-Validate-AzurePowershellVersion
-
-#Handle-SwitchAzureMode
-$isSwitchAzureModeRequired = Is-SwitchAzureModeRequired
-
-. ./AzureResourceManagerHelper.ps1
-
-if($isSwitchAzureModeRequired)
+try
 {
-    . ./AzureResourceManagerWrapper.ps1
-}
+    Initialize-GlobalMaps
 
-if( $action -eq "Select Resource Group")
-{
-    if([string]::IsNullOrEmpty($outputVariable))
+    Validate-AzurePowershellVersion
+
+    #Handle-SwitchAzureMode
+    $isSwitchAzureModeRequired = Is-SwitchAzureModeRequired
+
+    . ./AzureResourceManagerHelper.ps1
+
+    if($isSwitchAzureModeRequired)
     {
-        Write-TaskSpecificTelemetry "PREREQ_NoOutputVariableForSelectActionInAzureRG"
-        throw (Get-LocalizedString -Key "Please provide the output variable name since you have specified the 'Select Resource Group' option.")
+    .     ./AzureResourceManagerWrapper.ps1
     }
+
+    if( $action -eq "Select Resource Group")
+    {
+        if([string]::IsNullOrEmpty($outputVariable))
+        {
+            Write-TaskSpecificTelemetry "PREREQ_NoOutputVariableForSelectActionInAzureRG"
+            throw (Get-LocalizedString -Key "Please provide the output variable name since you have specified the 'Select Resource Group' option.")
+        }
     
-    Instantiate-Environment -resourceGroupName $resourceGroupName -outputVariable $outputVariable
-    return
-}
-
-$serviceEndpoint = Get-ServiceEndpoint -Name "$ConnectedServiceName" -Context $distributedTaskContext
-if ($serviceEndpoint.Authorization.Scheme -eq 'Certificate')
-{
-    throw (Get-LocalizedString -Key "Certificate based authentication only works with the 'Select Resource Group' action. Please select an Azure subscription with either Credential or SPN based authentication.")
-}
-
-if($isSwitchAzureModeRequired)
-{
-    Switch-AzureMode AzureResourceManager
-}
-
-if( $action -eq "Create Or Update Resource Group" )
-{
-    Create-AzureResourceGroupHelper -csmFile $csmFile -csmParametersFile $csmParametersFile -resourceGroupName $resourceGroupName -location $location -overrideParameters $overrideParameters -isSwitchAzureModeRequired $isSwitchAzureModeRequired
-    if(-not [string]::IsNullOrEmpty($outputVariable))
-    {
         Instantiate-Environment -resourceGroupName $resourceGroupName -outputVariable $outputVariable
+        return
     }
-}
 
-else
+    $serviceEndpoint = Get-ServiceEndpoint -Name "$ConnectedServiceName" -Context $distributedTaskContext
+    if ($serviceEndpoint.Authorization.Scheme -eq 'Certificate')
+    {
+        Write-TaskSpecificTelemetry "PREREQ_InvalidServiceConnectionType"
+        throw (Get-LocalizedString -Key "Certificate based authentication only works with the 'Select Resource Group' action. Please select an Azure subscription with either Credential or SPN based authentication.")
+    }
+
+    if($isSwitchAzureModeRequired)
+    {
+        Switch-AzureMode AzureResourceManager
+    }
+
+    if( $action -eq "Create Or Update Resource Group" )
+    {
+        Create-AzureResourceGroupHelper -csmFile $csmFile -csmParametersFile $csmParametersFile -resourceGroupName $resourceGroupName -location $location -overrideParameters $overrideParameters -isSwitchAzureModeRequired $isSwitchAzureModeRequired
+        if(-not [string]::IsNullOrEmpty($outputVariable))
+        {
+            Instantiate-Environment -resourceGroupName $resourceGroupName -outputVariable $outputVariable
+        }
+    }
+
+    else
+    {
+        Perform-Action -action $action -resourceGroupName $resourceGroupName
+    }
+
+    Write-Verbose -Verbose "Completing Azure Resource Group Deployment Task"
+}
+catch
 {
-    Perform-Action -action $action -resourceGroupName $resourceGroupName
-}
+    if(-not $telemetrySet)
+    {
+        Write-TaskSpecificTelemetry "UNKNOWN_Error"
+    }
 
-Write-Verbose -Verbose "Completing Azure Resource Group Deployment Task"
+    throw
+}
