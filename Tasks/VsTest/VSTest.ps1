847045a0caafcaf45609353f1e064ea5104aeb03
Allen Mathias <allendm@microsoft.com>
2015-12-02 19:35:55 +0530

    adding ps based tests and fixing get-localized for vstest task

--- Tasks/VsTest/VSTest.ps1
+++ Tasks/VsTest/VSTest.ps1
@@ -14,78 +14,6 @@ param(
     [string]$runInParallel
 )
 
-    
-function CmdletHasMember($memberName) {
-    $publishParameters = (gcm Publish-TestResults).Parameters.Keys.Contains($memberName) 
-    return $publishParameters
-}
-
-function SetRegistryKeyForParallel($vsTestVersion) {
-    $regkey = "HKCU\SOFTWARE\Microsoft\VisualStudio\" + $vsTestVersion + "_Config\FeatureFlags\TestingTools\UnitTesting\Taef"
-    reg add $regkey /v Value /t REG_DWORD /d 1 /f /reg:32
-}
-
-function IsVisualStudio2015Update1OrHigherInstalled($vsTestVersion) {
-    $version = [int]($vsTestVersion)
-    if($version -ge 14)
-    {
-        # checking for dll introduced in vs2015 update1
-        # since path of the dll will change in dev15+ using vstestversion>14 as a blanket yes
-        if((Test-Path "$env:VS140COMNTools\..\IDE\CommonExtensions\Microsoft\TestWindow\TE.TestModes.dll") -Or ($version -gt 14))
-        {
-            # ensure the registry is set otherwise you need to launch VSIDE
-            SetRegistryKeyForParallel $vsTestVersion
-            
-            return $true
-        }
-    }
-    
-    return $false
-}
-
-function SetupRunSettingsFileForParallel($runInParallelFlag, $runSettingsFilePath, $defaultCpuCount) {
-
-    if($runInParallelFlag -eq "True")
-    {        
-        $runSettingsForParallel = [xml]'<?xml version="1.0" encoding="utf-8"?>'
-        if([System.String]::IsNullOrWhiteSpace($runSettingsFilePath) -Or (-Not [io.path]::HasExtension($runSettingsFilePath)))  # no file provided so create one and use it for the run
-        {
-            Write-Verbose "No runsettings file provided"
-            $runSettingsForParallel = [xml]'<?xml version="1.0" encoding="utf-8"?>
-<RunSettings>
-  <RunConfiguration>
-    <MaxCpuCount>0</MaxCpuCount>
-  </RunConfiguration>
-</RunSettings>
-'
-        }
-        else 
-        {
-            Write-Verbose "Adding maxcpucount element to runsettings file provided"
-            $runSettingsForParallel = [System.Xml.XmlDocument](Get-Content $runSettingsFilePath)
-            $runConfigurationElement = $runSettingsForParallel.SelectNodes("//RunSettings/RunConfiguration")
-            if($runConfigurationElement.Count -eq 0)
-            {
-                 $runConfigurationElement = $runSettingsForParallel.RunSettings.AppendChild($runSettingsForParallel.CreateElement("RunConfiguration"))
-            }
-
-            $maxCpuCountElement = $runSettingsForParallel.SelectNodes("//RunSettings/RunConfiguration/MaxCpuCount")
-            if($maxCpuCountElement.Count -eq 0)
-            {
-                 $newMaxCpuCountElement = $runConfigurationElement.AppendChild($runSettingsForParallel.CreateElement("MaxCpuCount"))
-            }    
-        }
-
-        $runSettingsForParallel.RunSettings.RunConfiguration.MaxCpuCount = $defaultCpuCount
-        $tempFile = [io.path]::GetTempFileName()
-        $runSettingsForParallel.Save($tempFile)
-        Write-Verbose "Temporary runsettings file created at $tempFile"
-        return $tempFile
-    }
-    return $runSettingsFilePath
-}
-
-
 Write-Verbose "Entering script VSTestConsole.ps1"
 Write-Verbose "vsTestVersion = $vsTestVersion"
 Write-Verbose "testAssembly = $testAssembly"
@@ -106,7 +34,8 @@ import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 # Import the Task.TestResults dll that has the cmdlet we need for publishing results
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.TestResults"
 
-
+. $PSScriptRoot\Helpers.ps1
+    
 if (!$testAssembly)
 {
     Write-Host "##vso[task.logissue type=error;code=002001;]" 
@@ -163,7 +92,7 @@ if($testAssemblyFiles)
         $rightVSVersionAvailable = IsVisualStudio2015Update1OrHigherInstalled $vsTestVersion
         if(-Not $rightVSVersionAvailable)
         {
-            Write-Warning "Install Visual Studio 2015 Update 1 or higher on your build agent machine to run the tests in parallel."
+            Write-Warning (Get-LocalizedString -Key "Install Visual Studio 2015 Update 1 or higher on your build agent machine to run the tests in parallel.")
             $runInParallel = "false"
         }
     }
@@ -193,7 +122,7 @@ if($testAssemblyFiles)
             {
                 if(!$publishResultsOption)
                 {
-                    Write-Warning "Update the build agent to be able to opt out of test run attachment upload."
+                    Write-Warning (Get-LocalizedString -Key "Update the agent to try out the '{0}' feature." -ArgumentList "opt in/out of publishing test run attachments")
                 }
                 Publish-TestResults -Context $distributedTaskContext -TestResultsFiles $resultFiles -TestRunner "VSTest" -Platform $platform -Configuration $configuration -RunTitle $testRunTitle
             }
@@ -202,7 +131,7 @@ if($testAssemblyFiles)
         {
             if($testRunTitle)
             {
-                Write-Warning "Update the build agent to be able to use the custom run title feature."
+                Write-Warning (Get-LocalizedString -Key "Update the agent to try out the '{0}' feature." -ArgumentList "custom run title")
             }
             
             if($publishRunLevelAttachmentsExists)		
@@ -213,7 +142,7 @@ if($testAssemblyFiles)
             {
                 if(!$publishResultsOption)
                 {
-                    Write-Warning "Update the build agent to be able to opt out of test run attachment upload."
+                    Write-Warning (Get-LocalizedString -Key "Update the agent to try out the '{0}' feature." -ArgumentList "opt in/out of publishing test run attachments")
                 }
                 Publish-TestResults -Context $distributedTaskContext -TestResultsFiles $resultFiles -TestRunner "VSTest" -Platform $platform -Configuration $configuration
             }		
@@ -222,14 +151,14 @@ if($testAssemblyFiles)
     else
     {
         Write-Host "##vso[task.logissue type=warning;code=002003;]"
-        Write-Warning (Get-LocalizedString "No results found to publish.")
+        Write-Warning (Get-LocalizedString -Key "No results found to publish.")
     }
     
 }
 else
 {
     Write-Host "##vso[task.logissue type=warning;code=002004;]"
-    Write-Warning (Get-LocalizedString "No test assemblies found matching the pattern: '{0}'." -ArgumentList $testAssembly)
+    Write-Warning (Get-LocalizedString -Key "No test assemblies found matching the pattern: '{0}'." -ArgumentList $testAssembly)
 }
 
 Write-Verbose "Leaving script VSTestConsole.ps1"
--- Tests/L0/VsTest/_suite.ts
+++ Tests/L0/VsTest/_suite.ts
@@ -10,7 +10,7 @@ import path = require('path');
 var shell = require('shelljs');
 var ps = shell.which('powershell');
 
-describe('MSBuild Suite', function () {
+describe('VsTest Suite', function () {
     this.timeout(10000);
 
     before((done) => {
@@ -23,10 +23,28 @@ describe('MSBuild Suite', function () {
 
     if (ps) {
         it('(VsTest-NoTestAssemblies) throws if no test assemblies provided as input', (done) => {
-            psm.runPS(path.join(__dirname, 'VsTest.ThrowsIfAssembliesNotProvided.ps1'), done);
+            psm.runPS(path.join(__dirname, 'ThrowsIfAssembliesNotProvided.ps1'), done);
         })
         it('(VsTest-NoSourceDiretory) throws if no source directory is found', (done) => {
-            psm.runPS(path.join(__dirname, 'VsTest.ThrowsIfNoSourceDirectoryFound.ps1'), done);
+            psm.runPS(path.join(__dirname, 'ThrowsIfNoSourceDirectoryFound.ps1'), done);
+        })
+        it('(2015U1Check.ReturnsTrueIfTaefFileIsFound) returns true if taef file is found', (done) => {
+            psm.runPS(path.join(__dirname, '2015U1Check.ReturnsTrueIfTaefFileIsFound.ps1'), done);
+        })
+        it('(2015U1Check.ReturnsFalseForOlderVSTestVersion.ps1) returns false if vstest version less than 14', (done) => {
+            psm.runPS(path.join(__dirname, '2015U1Check.ReturnsFalseForOlderVSTestVersion.ps1'), done);
+        })
+        it('(2015U1Check.ReturnsTrueForLaterVSTestVersion) returns true if vstest version greater than 14', (done) => {
+            psm.runPS(path.join(__dirname, '2015U1Check.ReturnsTrueForLaterVSTestVersion.ps1'), done);
+        })
+        it('(RunSettingsForParallel.ReturnsSameFileIfParallelIsFalse) returns same file if parallel flag is false', (done) => {
+            psm.runPS(path.join(__dirname, 'RunSettingsForParallel.ReturnsSameFileIfParallelIsFalse.ps1'), done);
+        })
+        it('(RunSettingsForParallel.ReturnsNewFileIfParallelIsTrueAndFileNameEmpty) returns new file if parallel flag is true and no runsettings file is provided', (done) => {
+            psm.runPS(path.join(__dirname, 'RunSettingsForParallel.ReturnsNewFileIfParallelIsTrueAndFileNameEmpty.ps1'), done);
+        })
+        it('(RunSettingsForParallel.ReturnsSameFileIfParallelIsTrueAndFileNameNotEmpty) returns same but updated file if parallel flag is true and a runsettings file is provided', (done) => {
+            psm.runPS(path.join(__dirname, 'RunSettingsForParallel.ReturnsSameFileIfParallelIsTrueAndFileNameNotEmpty.ps1'), done);
         })
     }
 });
