8a8c2985ab78293618d7632e4b0aa765a21e36df
Anubhav Aggarwal <anaggar@microsoft.com>
2015-12-17 01:26:28 +0530

    added functions in IazureUtility

--- Tasks/DeployAzureResourceGroup/IAzureUtility.ps1
+++ Tasks/DeployAzureResourceGroup/IAzureUtility.ps1
@@ -2,7 +2,7 @@
 function Create-AzureResourceGroupIfNotExist
 {
     param([string]$resourceGroupName,
-    [string]$location)
+          [string]$location)
 }
 
 # Deploy $csmFile template on ResourceGroup $resourceGroupName.
@@ -14,18 +14,11 @@ function Deploy-AzureResourceGroup
         [System.Collections.Hashtable]$csmParametersObject,
         [string]$resourceGroupName,
         [string]$overrideParameters)
-
 }
 
 # Return instanceView of all VMs in ResourceGroup $resourceGroupName.
 # Return type is a hash with key = VMName, Value = InstanceView_of_VM
 function Get-AllVmInstanceView
-{
-    param([string]$resourceGroupName)
-}
-
-# Returns all VM Resourcecs in ResourceGroup $resourceGroupName
-function Get-AzureVMsInResourceGroup
 {
     param([string]$resourceGroupName)
 }
@@ -58,18 +51,33 @@ function Delete-ResourceGroup
     param([string]$resourceGroupName)
 }
 
-# Return Details of all Classic VMs in ResourceGroup $resourceGroupName
-# Return type is hash table with key = VMName, Value = resourceProperties
-# Where resourceProperties = @{"Name" = resourceName; "fqdn" = resourceFQDN; "winRMHttpsPort" = resourceWinRmHttpsPort}
-function Get-AzureClassicVMsDetailsInResourceGroup
+# Returns all RM VM Resources in ResourceGroup $resourceGroupName
+function Get-AzureRMVMsInResourceGroup
 {
     param([string]$resourceGroupName)
 }
 
-# Returns All Details related to RM VM Connection
-# Return type is hash table in following format: @{"azureVMResources" = azureVMResources; "networkInterfaceResources" = networkInterfaceResources; "publicIPAddressResources" = publicIPAddressResources; "loadBalancerResources" = LoadBalancerDestails}
-# Where LoadBalancerDestails is hash table in following format: @{"frontEndIPConfigs" = frontEndIPConfigs; "inboundRules" = inboundRules}
-function Get-AzureVMsConnectionDetailsInResourceGroup
+# Returns All Details related to RG Resources which will be used to get connection information for RM VMs
+# Return type is hash table in following format:
+# @{"networkInterfaceResources" = networkInterfaceResources; "publicIPAddressResources" = publicIPAddressResources; "loadBalancerResources" = LoadBalancerDetails}
+# Where LoadBalancerDetails is hash table in following format: @{"frontEndIPConfigs" = frontEndIPConfigs; "inboundRules" = inboundRules}
+function Get-AzureRMResourceGroupResourcesDetails
+{
+    param([string]$resourceGroupName,
+          [object]$azureRMVMResources)
+}
+
+# Returns all classic VM Resources in ResourceGroup $resourceGroupName
+function Get-AzureClassicVMsInResourceGroup
 {
     param([string]$resourceGroupName)
+}
+
+# Return Details of all Classic VMs in ResourceGroup $resourceGroupName
+# Return type is hash table with key = VMName, Value = resourceProperties
+# Where resourceProperties = @{"Name" = resourceName; "fqdn" = resourceFQDN; "winRMHttpsPort" = resourceWinRmHttpsPort}
+function Get-AzureClassicVMsConnectionDetailsInResourceGroup
+{
+    param([string]$resourceGroupName,
+          [object]$azureClassicVMResources)
 }
