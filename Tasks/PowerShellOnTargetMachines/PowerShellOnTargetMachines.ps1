d89e6bc1432a36c5743948441ba81c6a8e738890
Praneeth Yenugutala <pryenugu@microsoft.com>
2015-12-21 19:14:14 +0530

    Making frontend changes to output tags in the register-environment in Deploy RG. ALso making changes to not persist in task context in consumer(of output variables) tasks.

--- Tasks/PowerShellOnTargetMachines/PowerShellOnTargetMachines.ps1
+++ Tasks/PowerShellOnTargetMachines/PowerShellOnTargetMachines.ps1
@@ -320,7 +320,7 @@ try
     $connection = Get-VssConnection -TaskContext $distributedTaskContext
 
     Write-Verbose "Starting Register-Environment cmdlet call for environment : $environmentName with filter $machineFilter" -Verbose
-    $environment = Register-Environment -EnvironmentName $environmentName -EnvironmentSpecification $environmentName -UserName $adminUserName -Password $adminPassword -WinRmProtocol $protocol -TestCertificate ($testCertificate -eq "true") -Connection $connection -TaskContext $distributedTaskContext -ResourceFilter $machineFilter
+    $environment = Register-Environment -EnvironmentName $environmentName -EnvironmentSpecification $environmentName -UserName $adminUserName -Password $adminPassword -WinRmProtocol $protocol -TestCertificate ($testCertificate -eq "true") -Connection $connection -TaskContext $distributedTaskContext -ResourceFilter $machineFilter -DoNotPersist
     Write-Verbose "Completed Register-Environment cmdlet call for environment : $environmentName" -Verbose
 
     Write-Verbose "Starting Get-EnvironmentResources cmdlet call on environment name: $environmentName" -Verbose
