1cd97a0a067a33237ba8f9d5d7ac575789269b12
prmittal <prmittal@microsoft.com>
2016-01-26 16:34:10 +0530

    renamng file and adding assert

--- Tests/L0/VsTest/RunSettingsForParallel.ReturnsNewFileIfParallelIsTrueAndFileNameIsANonRunsettingsFile.ps1
+++ Tests/L0/VsTest/RunSettingsForParallel.ReturnsNewFileIfParallelIsTrueAndFileNameIsANonRunsettingsFile.ps1
@@ -22,6 +22,8 @@ $returnedFilePath = SetupRunSettingsFileForParallel "true" $temptestsettingsfile
 $fileExists = Test-Path $returnedFilePath
 Assert-AreEqual $true $fileExists
 
+Assert-AreNotEqual $temptestsettingsfile $returnedFilePath
+
 $readRunSettingsFile=[System.Xml.XmlDocument](Get-Content $returnedFilePath)
 Assert-AreEqual $cpuCount $readRunSettingsFile.RunSettings.RunConfiguration.MaxCpuCount
 
