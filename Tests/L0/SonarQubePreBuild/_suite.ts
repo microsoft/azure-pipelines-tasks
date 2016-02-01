1e832856a64df0cd304f72c487abd91196ed551f
Bogdan Gavril <bgavril@microsoft.com>
2016-02-01 16:02:04 +0000

    Fix https://jira.sonarsource.com/browse/TBSTFNP-43 and add unit tests

---
+++ Tests/L0/SonarQubePreBuild/_suite.ts
@@ -0,0 +1,28 @@
+/// <reference path="../../../definitions/mocha.d.ts"/>
+/// <reference path="../../../definitions/node.d.ts"/>
+/// <reference path="../../../definitions/Q.d.ts"/>
+
+import Q = require('q');
+import psm = require('../../lib/psRunner');
+import path = require('path');
+var shell = require('shelljs');
+var ps = shell.which('powershell');
+
+describe('Common-MSBuildHelpers Suite', function () {
+    this.timeout(20000);
+
+    before((done) => {
+        // init here
+        done();
+    });
+
+    after(function () {
+    });
+
+    if (ps) {
+        it('CreateCommandLineArgs tests', (done) => {
+            psm.runPS(path.join(__dirname, 'CreateCommandLineArgs.ps1'), done);
+        })
+       
+    }
+});
