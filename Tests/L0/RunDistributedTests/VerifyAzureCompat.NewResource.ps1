57f7a1efb014f7b504c43e7349bdef1050da7aac
Nitin Gurram <nigurr@microsoft.com>
2016-02-04 02:45:25 +0530

    Fixed Issues in merging with Test Plan which caused mis-config

--- Tests/L0/RunDistributedTests/VerifyAzureCompat.NewResource.ps1
+++ Tests/L0/RunDistributedTests/VerifyAzureCompat.NewResource.ps1
@@ -5,7 +5,7 @@ param()
 
 $distributedTaskContext = 'Some distributed task context'
 Register-Mock Get-VssConnection { $connection } -TaskContext $distributedTaskContext
-Register-Mock CmdletHasMember { $true }
+Register-Mock CmdletHasMember { $true } -memberName "TaskContext"
 Register-Mock Invoke-RunDistributedTests { $true } -- -TestMachineGroup $testMachineGroup -SourceFilter $sourcefilters -TestCaseFilter $testFilterCriteria -RunSettingsPath $runSettingsFile -Platform $platform -Configuration $configuration -CodeCoverageEnabled $codeCoverageEnabled -TestRunParams $overrideRunParams -TestDropLocation $dropLocation -Connection $connection -TestConfiguration $testConfigurations -AutMachineGroup $autMachineGroup -UnregisterTestAgentScriptLocation $unregisterTestAgentScriptLocation -TestRunTitle $testRunTitle -TestSelection $testSelection -TestPlan $testPlanId -TestSuites $testSuites -TestConfig $testConfigurationId -TaskContext $distributedTaskContext
 
 $input = @{
@@ -21,14 +21,14 @@ $input = @{
     'overrideRunParams' = 'overrideRunParams'
     'testConfigurations' = 'testConfigurations'
     'autMachineGroup' = 'autMachineGroup'
-    'testSelection' = 'testSelection'
-    'testPlan' = 'testPlan'
-    'testSuite' = 'testSuite'
-    'testConfiguration' = 'testConfiguration'
+    'testSelection' = ''
+    'testPlan' = ''
+    'testSuite' = ''
+    'testConfiguration' = ''
 }
 & $PSScriptRoot\..\..\..\Tasks\RunVisualStudioTestsusingTestAgent\RunDistributedTests.ps1 @input
 
 
 Assert-WasCalled Get-VssConnection -Times 1
-Assert-WasCalled CmdletHasMember -Times 1
+Assert-WasCalled CmdletHasMember -Times 2
 Assert-WasCalled Invoke-RunDistributedTests -Times 1
