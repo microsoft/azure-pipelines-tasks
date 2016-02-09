1c6e617ad8fd1d4ba9ddaf55f3ddc8f8f8254ce1
Ryu Yu <ryuyu@microsoft.com>
2016-02-09 12:53:59 -0800

    Remove cocoapods demand and add a message to output if pods command is not found

--- Tasks/CocoaPods/cocoapods.ts
+++ Tasks/CocoaPods/cocoapods.ts
@@ -5,17 +5,23 @@ import tl = require('vsts-task-lib/task');
 tl.cd(tl.getPathInput('cwd', true, true));
 
 tl.debug('Setting locale to UTF8 - required by CocoaPods');
-process.env['LC_ALL']='en_US.UTF-8';
+process.env['LC_ALL'] = 'en_US.UTF-8';
 
-var tool = tl.which('pod', true);
-var pod = tl.createToolRunner(tool);
-pod.arg('install');
+var tool = tl.which('pod');
+if (tool) {
+    var pod = tl.createToolRunner(tool);
+    pod.arg('install');
 
-pod.exec()
-.then(function(code) {
-	tl.exit(code);
-})
-.fail(function(err) {
-	tl.debug('taskRunner fail');
-	tl.exit(1);
-})
+    pod.exec()
+        .then(function(code) {
+            tl.exit(code);
+        })
+        .fail(function(err) {
+            tl.debug('taskRunner fail');
+            tl.exit(1);
+        });
+} else {
+    tl.error('command pod was not found. Please install Cocoapods on the build machine (https://cocoapods.org)');
+    tl.debug('taskRunner fail: pod not found');
+    tl.exit(1);
+}
