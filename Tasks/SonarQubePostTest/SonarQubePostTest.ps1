45990f0398589f2acfbc8c4b5bb88f3c6dcbe647
Bogdan Gavril <bgavril@microsoft.com>
2016-02-22 18:04:55 +0000

    Add a build var that disables SQ tasks in PR builds

--- Tasks/SonarQubePostTest/SonarQubePostTest.ps1
+++ Tasks/SonarQubePostTest/SonarQubePostTest.ps1
@@ -3,9 +3,18 @@ Write-Verbose "Starting SonarQube PostBuild Step"
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 
 . $PSScriptRoot/Common/SonarQubeHelpers/SonarQubeHelper.ps1
-. $PSScriptRoot//SonarQubePostTestImpl.ps1
-. $PSScriptRoot//SonarQubeBuildBreaker.ps1
+
+if (IsPrBuild -and (GetTaskContextVariable "DisableSQAnalysisOnPrBuilds" -eq "true")) 
+{
+	Write-Host "DisableSQAnalysisOnPrBuilds is set and this is a PR build - ignoring the analysis tasks"
+	return
+}
+
+. $PSScriptRoot/SonarQubePostTestImpl.ps1
+. $PSScriptRoot/SonarQubeBuildBreaker.ps1
+. $PSScriptRoot/CodeAnalysisFilePathComputation.ps1
 
 InvokeMSBuildRunnerPostTest
 UploadSummaryMdReport
+HandleCodeAnalysisReporting
 BreakBuildOnQualityGateFailure
