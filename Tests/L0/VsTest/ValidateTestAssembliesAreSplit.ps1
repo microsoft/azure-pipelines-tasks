8d4d1db17dcf5a36bbab684cb96554cb57deb331
Nitin Gurram <nigurr@microsoft.com>
2016-02-22 15:54:47 +0530

    Reformatting code

--- Tests/L0/VsTest/ValidateTestAssembliesAreSplit.ps1
+++ Tests/L0/VsTest/ValidateTestAssembliesAreSplit.ps1
@@ -5,10 +5,9 @@ param()
 . $PSScriptRoot\..\..\lib\Initialize-Test.ps1
 Register-Mock Get-TaskVariable { "c:\testSource" }
 Register-Mock Convert-String { "true" }
-Register-Mock Find-Files {"true"}
-Register-Mock Invoke-VSTest {$true} -TestAssemblies @("c:\test1.dll","c:\test2.dll") -VSTestVersion $vsTestVersion -TestFiltercriteria "testFiltercriteria" -RunSettingsFile "runSettingsFile" -PathtoCustomTestAdapters "pathtoCustomTestAdapters" -CodeCoverageEnabled $true -OverrideTestrunParameters "overrideTestrunParameters" -OtherConsoleOptions "otherConsoleOptions" -WorkingFolder "c:\testSource" -TestResultsFolder "c:\testSource\TestResults" -SourcesDirectory "c:\testSource"
-Register-Mock Publish-TestResults {$true}
-
+Register-Mock Find-Files { "true" }
+Register-Mock Invoke-VSTest { $true } -TestAssemblies @("c:\test1.dll","c:\test2.dll") -VSTestVersion $vsTestVersion -TestFiltercriteria "testFiltercriteria" -RunSettingsFile "runSettingsFile" -PathtoCustomTestAdapters "pathtoCustomTestAdapters" -CodeCoverageEnabled $true -OverrideTestrunParameters "overrideTestrunParameters" -OtherConsoleOptions "otherConsoleOptions" -WorkingFolder "c:\testSource" -TestResultsFolder "c:\testSource\TestResults" -SourcesDirectory "c:\testSource"
+Register-Mock Publish-TestResults { $true }
 
 $splat = @{
     'vsTestVersion' = 'vsTestVersion'
