80781520714689262ede4255f349d789c23b0a73
Nitin Gurram <nigurr@microsoft.com>
2016-02-22 13:48:07 +0530

    Split Test assemblies if they are seperated by ;

--- Tasks/VsTest/VSTest.ps1
+++ Tasks/VsTest/VSTest.ps1
@@ -68,6 +68,7 @@ if ($testAssembly.Contains("*") -Or $testAssembly.Contains("?"))
 else
 {
     Write-Verbose "No Pattern found in solution parameter."
+    $testAssembly = $testAssembly.Split(";")
     $testAssemblyFiles = ,$testAssembly
 }
 
