85468fd0e22e3b86bdb604edd4123838b0dde7be
Nitin Gurram <nigurr@microsoft.com>
2016-02-24 10:30:29 +0530

    Testing ; handling in Assembly names

--- Tests/L0/VsTest/ValidateTestAssembliesAreSplit.ps1
+++ Tests/L0/VsTest/ValidateTestAssembliesAreSplit.ps1
@@ -6,12 +6,12 @@ param()
 Register-Mock Get-TaskVariable { "c:\testSource" }
 Register-Mock Convert-String { $true }
 Register-Mock Find-Files { $true }
-Register-Mock Invoke-VSTest { $true } -TestAssemblies @("c:\test1.dll","c:\test2.dll") -VSTestVersion "vsTestVersion" -TestFiltercriteria "testFiltercriteria" -RunSettingsFile "runSettingsFile" -PathtoCustomTestAdapters "pathtoCustomTestAdapters" -CodeCoverageEnabled $true -OverrideTestrunParameters "overrideTestrunParameters" -OtherConsoleOptions "otherConsoleOptions" -WorkingFolder "c:\testSource" -TestResultsFolder "c:\testSource\TestResults" -SourcesDirectory "c:\testSource"
+Register-Mock Invoke-VSTest { $true } -- -TestAssemblies @("c:\test1.dll","c:\test2.dll") -VSTestVersion "vsTestVersion" -TestFiltercriteria "testFiltercriteria" -RunSettingsFile "runSettingsFile" -PathtoCustomTestAdapters "pathtoCustomTestAdapters" -CodeCoverageEnabled $true -OverrideTestrunParameters "overrideTestrunParameters" -OtherConsoleOptions "otherConsoleOptions" -WorkingFolder "c:\testSource" -TestResultsFolder "c:\testSource\TestResults" -SourcesDirectory "c:\testSource"
 Register-Mock Publish-TestResults { $true }
 
 $splat = @{
     'vsTestVersion' = 'vsTestVersion'
-    'testAssembly' = 'c:\test1.dll;c:\test2.dll' 
+    'testAssembly' = 'c:\test1.dll;c:\test2;;.dll;c:\test3.dll' 
     'testFiltercriteria' = 'testFiltercriteria' 
     'runSettingsFile' = 'runSettingsFile' 
     'codeCoverageEnabled' = 'true'
@@ -27,3 +27,4 @@ $splat = @{
 & $PSScriptRoot\..\..\..\Tasks\VsTest\VsTest.ps1 @splat
 
 Assert-WasCalled Invoke-VSTest -Times 1
+Assert-WasCalled Publish-TestResults  -Times 1
