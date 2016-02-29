be536f8affdb3b56b3f91f330c44fc14debe2e5a
ashwini reddy <achalla@microsoft.com>
2016-02-29 14:55:11 +0530

    inital commit

---
+++ Tasks/PublishCodeCoverageResults/publishcodecoverageresults.ts
@@ -0,0 +1,23 @@
+/// <reference path="../../definitions/vsts-task-lib.d.ts" />
+
+import tl = require('vsts-task-lib/task');
+
+var codeCoverageTool = tl.getInput('codeCoverageTool', true);
+var summaryFileLocation = tl.getInput('summaryFileLocation', true);
+var reportDirectory = tl.getInput('reportDirectory');
+var additionalCodeCoverageFiles = tl.getInput('additionalCodeCoverageFiles');
+
+tl.debug('codeCoverageTool: ' + codeCoverageTool);
+tl.debug('summaryFileLocation: ' + summaryFileLocation);
+tl.debug('reportDirectory: ' + reportDirectory);
+tl.debug('additionalCodeCoverageFiles: ' + additionalCodeCoverageFiles);
+
+if(additionalCodeCoverageFiles)
+{
+  var buildFolder = tl.getVariable('agent.buildDirectory');
+  var allFiles = tl.find(buildFolder);
+  var codeCoverageFiles = tl.match(allFiles, additionalCodeCoverageFiles, { matchBase: true });
+}
+
+var tp = new tl.CodeCoveragePublisher();
+tp.publish(codeCoverageTool, summaryFileLocation, reportDirectory, codeCoverageFiles);
