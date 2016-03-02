7d4bc051d6428829b48f653ced980cef7719b297
Bogdan Gavril <bgavril@microsoft.com>
2016-03-02 10:13:40 +0000

    wip

--- Tasks/SonarQubePostTest/PRCA/IssuesProcessor.ps1
+++ Tasks/SonarQubePostTest/PRCA/IssuesProcessor.ps1
@@ -1,6 +1,7 @@
 
 #region Public
 
+
 function ProcessIssues
 {
     param ([ValidateNotNull()][Array]$issues,
@@ -15,6 +16,53 @@ function ProcessIssues
                         
         
         
+}
+
+#endregion
+
+#region Private
+
+function GetArtifactUri
+{
+    param ([int]$codeReviewId, [Guid]$teamProjectId, [int]$pullRequestId)
+    
+    if ($codeReviewId -eq 0)
+    {        
+        $artifactUri = [String]::Format("vstfs:///CodeReview/CodeReviewId/{0}%2f{1}", $teamProjectId, $pullRequestId);        
+        Write-Verbose "Legacy code review. The artifact uri is $artifactUri"
+        return $artifactUri
+    }
+
+    $artifactUri = [Microsoft.VisualStudio.Services.CodeReview.WebApi.CodeReviewSdkArtifactId]::GetArtifactUri($teamProjectId, $pullRequestId)
+    Write-Verbose "New style code review. The artifact uri is $artifactUri"
+    return $artifactUri
+}
+
+
+function PostIssuesToCodeReview
+{       
+    param ([Microsoft.VisualStudio.Services.Client.VssConnection][ValidateNotNull()]$vssConnection, [string]$newIssues, [int]$pullRequestId)     
+        
+    $gitClient = $vssConnection.GetClient("Microsoft.TeamFoundation.SourceControl.WebApi.GitHttpClient")            
+    $discussionClient = $vssConnection.GetClient("Microsoft.VisualStudio.Services.CodeReview.Discussion.WebApi.DiscussionHttpClient")    
+    $codeReviewClient = $vssConnection.GetClient("Microsoft.VisualStudio.Services.CodeReview.WebApi.CodeReviewHttpClient") # ???
+        
+    Assert ( $gitClient -ne $null ) "Internal error: could not retrieve the GitHttpClient object"
+    Assert ( $discussionClient -ne $null ) "Internal error: could not retrieve the DiscussionHttpClient object"
+    Assert ( $codeReviewClient -ne $null ) "Internal error: could not retrieve the CodeReviewHttpClient object"
+             
+ 
+    
+    $repositoryId = GetTaskContextVariable "build.repository.id"    
+    $teamProject = GetTaskContextVariable "system.teamProject"
+
+    Assert (![String]::IsNullOrWhiteSpace($repositoryId)) "Internal error: could not determine the build.repository.id"
+    Assert (![String]::IsNullOrWhiteSpace($teamProject)) "Internal error: could not determine the system.teamProject"   
+
+    $pullRequest = $gitClient.GetPullRequestAsync($project, $repositoryId, $pullRequestId).Result;
+    $artifactUri = GetArtifactUri $pullRequest.CodeReviewId $pullRequest.Repository.ProjectReference.Id $pullRequestId
+           
+    ProcessIssues $newIssues $codeReviewClient $gitClient $discussionClient $pullRequest $teamProject $repositoryId $sourceBranch $artifactUri        
 }
 
 #endregion
--- Tasks/SonarQubePostTest/PRCA/Orchestrator.ps1
+++ Tasks/SonarQubePostTest/PRCA/Orchestrator.ps1
@@ -15,43 +15,28 @@ function HandleCodeAnalysisReporting
             return
         }
 
-        LoadClientOMAssemblies 
-        PostIssuesToCodeReview $newIssues   
+    
+        LoadClientOMAssemblies
+        
+        $vssConnection = GetVssConnection 
+        $pullRequestId = GetPullRequestId
+    
+        PostIssuesToCodeReview $newIssues $vssConnection  $pullRequestId
     }	
 }
 
-#region Private 
 
-function PostIssuesToCodeReview
-{       
-    param ([string]$newIssues)     
-    
+function GetVssConnection
+{
     $vssConnection = [Microsoft.TeamFoundation.DistributedTask.Task.Internal.Core.TaskContextHelper]::GetVssConnection($distributedTaskContext); 
     Assert ( $vssConnection -ne $null ) "Internal error: could not retrieve the VssConnection object"
-        
-    $gitClient = $vssConnection.GetClient("Microsoft.TeamFoundation.SourceControl.WebApi.GitHttpClient")            
-    $discussionClient = $vssConnection.GetClient("Microsoft.VisualStudio.Services.CodeReview.Discussion.WebApi.DiscussionHttpClient")    
-    $codeReviewClient = $vssConnection.GetClient("Microsoft.VisualStudio.Services.CodeReview.WebApi.CodeReviewHttpClient") # ???
-        
-    Assert ( $gitClient -ne $null ) "Internal error: could not retrieve the GitHttpClient object"
-    Assert ( $discussionClient -ne $null ) "Internal error: could not retrieve the DiscussionHttpClient object"
-    Assert ( $codeReviewClient -ne $null ) "Internal error: could not retrieve the CodeReviewHttpClient object"
-             
-    $sourceBranch =  GetTaskContextVariable "Build.SourceBranch"
-    $pullRequestId = RetrievePullRequestId $sourceBranch
-    Write-Verbose  "Pull Request Id $pullRequestId"
     
-    $repositoryId = GetTaskContextVariable "build.repository.id"    
-    $teamProject = GetTaskContextVariable "system.teamProject"
-
-    Assert (![String]::IsNullOrWhiteSpace($repositoryId)) "Internal error: could not determine the build.repository.id"
-    Assert (![String]::IsNullOrWhiteSpace($teamProject)) "Internal error: could not determine the system.teamProject"   
-
-    $pullRequest = $gitClient.GetPullRequestAsync($project, $repositoryId, $pullRequestId).Result;
-    $artifactUri = GetArtifactUri $pullRequest.CodeReviewId $pullRequest.Repository.ProjectReference.Id $pullRequestId
-           
-    ProcessIssues $newIssues $codeReviewClient $gitClient $discussionClient $pullRequest $teamProject $repositoryId $sourceBranch $artifactUri        
+    echo $vssConnection.GetType()
+    
+    return $vssConnection
 }
+
+#region Private 
 
 function LoadClientOMAssemblies
 {                    
@@ -76,10 +61,9 @@ function LoadClientOMAssemblies
     Write-Verbose "Loaded $externalAssemblyPaths"
 }
 
-function RetrievePullRequestId 
+function GetPullRequestId 
 {
-    param ([ValidateNotNullOrEmpty()][string]$sourceBranch)
-    
+    $sourceBranch =  GetTaskContextVariable "Build.SourceBranch"
     Assert ($sourceBranch.StartsWith("refs/pull/", [StringComparison]::OrdinalIgnoreCase)) "Internal Error: source branch $sourceBranch is not in a recognized format"  
     
     $parts = $sourceBranch.Split('/');
@@ -94,20 +78,5 @@ function RetrievePullRequestId
     return $prId    
 }
  
-function GetArtifactUri
-{
-    param ([int]$codeReviewId, [Guid]$teamProjectId, [int]$pullRequestId)
-    
-    if ($codeReviewId -eq 0)
-    {        
-        $artifactUri = [String]::Format("vstfs:///CodeReview/CodeReviewId/{0}%2f{1}", $teamProjectId, $pullRequestId);        
-        Write-Verbose "Legacy code review. The artifact uri is $artifactUri"
-        return $artifactUri
-    }
-
-    $artifactUri = [Microsoft.VisualStudio.Services.CodeReview.WebApi.CodeReviewSdkArtifactId]::GetArtifactUri($teamProjectId, $pullRequestId)
-    Write-Verbose "New style code review. The artifact uri is $artifactUri"
-    return $artifactUri
-}
 
 #endregion
