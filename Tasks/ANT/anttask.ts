bb3fd45d7d4acd0ca20a9f6751662e3c910ff596
Nitin Gurram <nigurr@microsoft.com>
2016-03-16 12:52:16 +0530

    Publish by default Test Reports as merged

--- Tasks/ANT/anttask.ts
+++ Tasks/ANT/anttask.ts
@@ -84,7 +84,7 @@ function publishTestResults(publishJUnitResults, testResultsFiles: string) {
     }
 
     var tp = new tl.TestPublisher("JUnit");
-    tp.publish(matchingTestResultsFiles, false, "", "","", "");
+    tp.publish(matchingTestResultsFiles, true, "", "","", true);
   } 
 }
 
