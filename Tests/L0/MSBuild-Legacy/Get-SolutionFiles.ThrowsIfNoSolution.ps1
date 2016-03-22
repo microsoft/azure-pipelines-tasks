1c67eea60c1af397b957f67f66d39a7fd9f91791
Eric Sciple <ersciple@microsoft.com>
2016-03-21 22:04:40 -0400

    Delete legacy implementations.

--- Tests/L0/MSBuild-Legacy/Get-SolutionFiles.ThrowsIfNoSolution.ps1
+++
@@ -1,9 +0,0 @@
-[cmdletbinding()]
-param()
-
-# Arrange.
-. $PSScriptRoot\..\..\lib\Initialize-Test.ps1
-. $PSScriptRoot\..\..\..\Tasks\MSBuild\LegacyHelpers.ps1
-
-# Act/Assert.
-Assert-Throws { Get-SolutionFiles -Solution '' }
