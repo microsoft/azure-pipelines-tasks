4a8cc5dded4821e8675b790ec3acbcc11a6154f4
Ivy Zhu <ivyzhu@microsoft.com>
2016-03-28 18:38:08 -0700

    add feed auth

--- Tasks/NuGetInstaller/nugetinstaller.ts
+++ Tasks/NuGetInstaller/nugetinstaller.ts
@@ -2,9 +2,11 @@
 /// <reference path="../../definitions/Q.d.ts" />
 /// <reference path="../../definitions/vsts-task-lib.d.ts" />
 
+import fs = require('fs');
 import path = require('path');
 import Q = require('q');
 import tl = require('vsts-task-lib/task');
+import helper = require('./vso-nuget-helper');
 
 //read inputs
 var solution = tl.getPathInput('solution', true, false);
@@ -24,15 +26,18 @@ if(!nuGetPathToUse) {
 }
 tl.checkPath(nuGetPathToUse, 'nuget');
 
+var sourcesFolder = tl.getVariable('build.sourcesdirectory');
 var runnuget = function(fn) {
     return Q.fcall(() => {
         var nugetTool = tl.createToolRunner(nuGetPathToUse);
 
         nugetTool.arg('restore');
         nugetTool.pathArg(fn);
-        var sourcesFolder = tl.getVariable('build.sourcesdirectory');
+
         tl.debug('sourcesFolder = ' + sourcesFolder);
         if (nugetConfigPath && nugetConfigPath.toLowerCase() != sourcesFolder.toLowerCase()) {
+           
+            tl._writeLine('nuget config path: ' + nugetConfigPath);
             nugetTool.arg('-configFile');
             nugetTool.pathArg(nugetConfigPath);
         }
@@ -47,62 +52,110 @@ var runnuget = function(fn) {
     })
 }
 
-// Resolve files for the specified value or pattern
-var filesList : string [];
-if (solution.indexOf('*') == -1 && solution.indexOf('?') == -1) {
-    // No pattern found, check literal path to a single file
-    tl.checkPath(solution, 'files');
+// get auth token and set credentials to NuGet config file
+var tempNugetConfigPath = '';
+if (nugetConfigPath && nugetConfigPath.toLowerCase() != sourcesFolder.toLowerCase()) {
 
-    // Use the specified single file
-    filesList = [solution];
-
-} else {
-    var firstWildcardIndex = function(str) {
-        var idx = str.indexOf('*');
-
-        var idxOfWildcard = str.indexOf('?');
-        if (idxOfWildcard > -1) {
-            return (idx > -1) ?
-                Math.min(idx, idxOfWildcard) : idxOfWildcard;
-        }
-
-        return idx;
+    tl._writeLine('Get end point authorization');
+    var auth = tl.getEndpointAuthorization('SYSTEMVSSCONNECTION', false);
+    if (auth.scheme == 'OAuth') {
+        var accessToken = auth.parameters['AccessToken'];
+        tl.debug('accessToken: ' + accessToken);
+    }
+    else {
+        tl.warning('Could not determine credentials to use for NuGet');
     }
 
-    // Find app files matching the specified pattern
-    tl.debug('Matching glob pattern: ' + solution);
-
-    // First find the most complete path without any matching patterns
-    var idx = firstWildcardIndex(solution);
-    tl.debug('Index of first wildcard: ' + idx);
-    var findPathRoot = path.dirname(solution.slice(0, idx));
-
-    tl.debug('find root dir: ' + findPathRoot);
-
-    // Now we get a list of all files under this root
-    var allFiles = tl.find(findPathRoot);
-
-    // Now matching the pattern against all files
-    filesList = tl.match(allFiles, solution, {matchBase: true});
-
-    // Fail if no matching .sln files were found
-    if (!filesList || filesList.length == 0) {
-        tl.error('No matching files were found with search pattern: ' + solution);
+    var nugetHelper = new helper.VsoNuGetHelper(nuGetPathToUse, nugetConfigPath, accessToken);
+    tempNugetConfigPath = nugetHelper.tempNugetConfigPath;
+ 
+    // add credentials to the user's nuget.config
+     nugetHelper.setCredentialsNuGetConfigAndSaveTemp().then((res) => {  
+        // reset nuget config path 
+        nugetConfigPath = tempNugetConfigPath;
+        restorePacakages();
+    },
+    (err) => {
+        tl.error(err);
+        removeTempConfig();
         tl.exit(1);
-    }
+    });
+}
+else {
+    restorePacakages();
 }
 
-var result = Q({});
-filesList.forEach((fn) => {
-    result = result.then(() => {
-        return runnuget(fn);
-    })
-})
 
-result.then(() => {
-    tl.exit(0);
-})
-.fail((err) => {
-    tl.error(err);
-    tl.exit(1);
-});
+function restorePacakages() {
+    // Resolve files for the specified value or pattern
+    var filesList: string[];
+    if (solution.indexOf('*') == -1 && solution.indexOf('?') == -1) {
+        // No pattern found, check literal path to a single file
+        tl.checkPath(solution, 'files');
+
+        // Use the specified single file
+        filesList = [solution];
+
+    } else {
+        var firstWildcardIndex = function (str) {
+            var idx = str.indexOf('*');
+
+            var idxOfWildcard = str.indexOf('?');
+            if (idxOfWildcard > -1) {
+                return (idx > -1) ?
+                    Math.min(idx, idxOfWildcard) : idxOfWildcard;
+            }
+
+            return idx;
+        }
+
+        // Find app files matching the specified pattern
+        tl.debug('Matching glob pattern: ' + solution);
+
+        // First find the most complete path without any matching patterns
+        var idx = firstWildcardIndex(solution);
+        tl.debug('Index of first wildcard: ' + idx);
+        var findPathRoot = path.dirname(solution.slice(0, idx));
+
+        tl.debug('find root dir: ' + findPathRoot);
+
+        // Now we get a list of all files under this root
+        var allFiles = tl.find(findPathRoot);
+
+        // Now matching the pattern against all files
+        filesList = tl.match(allFiles, solution, { matchBase: true });
+
+        // Fail if no matching .sln files were found
+        if (!filesList || filesList.length == 0) {
+            tl.error('No matching files were found with search pattern: ' + solution);
+            tl.exit(1);
+        }
+    }
+
+    var result = Q({});
+    filesList.forEach((fn) => {
+        result = result.then(() => {
+            return runnuget(fn);
+        })
+    })
+
+    result.then(() => {
+        removeTempConfig();
+        tl._writeLine("packages are installed successfully.");
+        tl.exit(0);
+    })
+        .fail((err) => {
+            tl.error(err)
+            removeTempConfig();
+            tl.error("packages failed to install.");
+            tl.exit(1);
+        });
+}
+
+function removeTempConfig() {
+    if (tempNugetConfigPath.length > 0) {
+        if (fs.existsSync(tempNugetConfigPath)) {        
+            fs.unlinkSync(tempNugetConfigPath);
+        };
+    };
+}
