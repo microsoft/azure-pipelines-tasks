759aeac54e4045d9c205f4f7d03b6688790465af
Eric Sciple <ersciple@microsoft.com>
2016-04-05 11:15:40 -0400

    Override AZPS debug preference.

--- Tasks/Common/AzureHelpers/AzureHelpers.psm1
+++ Tasks/Common/AzureHelpers/AzureHelpers.psm1
@@ -1,4 +1,10 @@
-﻿Import-VstsLocStrings -LiteralPath $PSScriptRoot\module.json
+﻿# Override the DebugPreference.
+if ($global:DebugPreference -eq 'Continue') {
+    Write-Verbose '$OVERRIDING $global:DebugPreference from ''Continue'' to ''SilentlyContinue''.'
+    $global:DebugPreference = 'SilentlyContinue'
+}
+
+Import-VstsLocStrings -LiteralPath $PSScriptRoot\module.json
 . $PSScriptRoot\PrivateFunctions.ps1
 . $PSScriptRoot\PublicFunctions.ps1
 Export-ModuleMember -Function Initialize-Azure
