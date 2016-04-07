6ca63ef02d6ee827b3d4acfb62baeb6a6b7336ac
Ryu Yu <ryuyu@microsoft.com>
2016-04-07 15:04:15 -0700

    add warning to xcodePackgeiOS if xcode 7+ is detected

--- Tasks/XcodePackageiOS/xcodepkgios.ts
+++ Tasks/XcodePackageiOS/xcodepkgios.ts
@@ -2,6 +2,7 @@
 
 import tl = require('vsts-task-lib/task');
 import path = require('path');
+var execSync = require('child_process').execSync;
 
 var sourceDir = tl.getVariable('build.sourcesDirectory');
 
@@ -12,6 +13,18 @@ xcv.arg('--version');
 //creates the dir if it's not there; note that this needs to be an absolute path for the .ipa to be generated.
 var ipaPath = tl.getInput('ipaPath', true);
 tl.mkdirP(path.join(sourceDir, ipaPath));
+
+var xCode7Plus = false;
+try {
+    var version = parseInt(execSync('xcodebuild -version').toString().match(/\d+(?=\.\d+)/g));
+    xCode7Plus = version >= 7;
+} catch (e) {
+    // The only reason for this to fail is if the call to xcodebuild fails, which means we aren't xcode 7+, which is ok
+}
+
+if (xCode7Plus) {
+    console.warn("Xcode 7+ detected. The Xcode Export Archive task should be used instead of this one.");
+}
 
 //
 // xcrun -sdk $sdk PackageApplication -v "$sourceDir/$appPath/$appFilename" -o "$sourceDir/$ipaPath/$ipaFilename" -embed '$provProfile'
