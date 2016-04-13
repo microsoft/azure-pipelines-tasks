772cdc0e4a33fd61aac6484e135f93aa7a693ea8
Ajay yadav <ajya@microsoft.com>
2016-04-13 19:42:08 +0530

    variable substitution

---
+++ Tasks/AzureRmWebAppDeployment/VariableSustituter.ps1
@@ -0,0 +1,51 @@
+﻿Param(
+  [Parameter(mandatory=$true)][string]$webAppFolderPath,
+  [Parameter(mandatory=$true)][string]$ConfigFileRegex
+)
+
+# define mime supported mime type
+$ApplicationXMLMimeType = "application/xml" 
+
+function Get-MimeType()
+{
+    param($extension = $null);
+ 
+    $mimeType = $null;
+    
+    if ( $null -ne $extension )
+    {
+        $drive = Get-PSDrive HKCR -ErrorAction SilentlyContinue;
+        if ( $null -eq $drive )
+        {
+            $drive = New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+        }
+        $mimeType = (Get-ItemProperty HKCR:$extension)."Content Type";
+    }
+    $mimeType;
+}
+
+
+# getting variables defined in environment
+$definedVariables = Get-ChildItem Env:
+
+# Find all configuration files in extracted path of webapp
+$webConfigFiles = Get-ChildItem -Path $WebAppFolderPath -Filter $ConfigFileRegex -Recurse -ErrorAction SilentlyContinue -Force
+
+foreach( $file in $WebConfigFiles )
+{
+    # Get mime type of each file
+    $fileMimeType = Get-MimeType -extension $file.Extension
+
+    foreach($variable in $definedVariables){
+
+        # Compare mime type of file and invoke corresponding variable substituter
+        if( $fileMimeType -eq $ApplicationXMLMimeType ){
+
+            .\XMLSubstituter.ps1 -xmlFile $file.FullName -tag "appSettings" -conditionalAttributeKey "key" -conditionalAttributeValue "$($variable.Name)" -targetKey "value" -targetValue "$($variable.Value)"
+            .\XMLSubstituter.ps1 -xmlFile $file.FullName -tag "MyConnectionString"  -targetKey "$($variable.Name)" -targetValue "$($variable.Value)" 
+        
+        }
+    }
+}
+
+
---
+++ Tasks/AzureRmWebAppDeployment/XMLSubstituter.ps1
@@ -0,0 +1,74 @@
+﻿Param(
+  [Parameter(mandatory=$true)][string]$xmlFilePath,
+  [Parameter(mandatory=$true)][string]$tag,
+  [string]$conditionalAttributeKey,
+  [string]$conditionalAttributeValue,
+  [Parameter(mandatory=$true)][string]$targetKey,
+  [Parameter(mandatory=$true)][string]$targetValue
+)
+
+Write-Verbose "xmlFile = $xmlFilePath" -Verbose
+Write-Verbose "Tag = $tag" -Verbose
+Write-Verbose "conditionalAttribute = $conditionalAttributeKey" -Verbose
+Write-Verbose "conditionalValue = $conditionalAttributeValue" -Verbose
+Write-Verbose "targetKey = $targetKey" -Verbose
+Write-Verbose "targetValue = $targetValue" -Verbose
+
+
+if( $conditionalAttribute -eq $null -xor $conditionalValue -eq $null) {
+    Write-Verbose "Either conditionalAttribute and conditionalValue should be set or both should be blank" -Verbose
+}
+
+if (-not (test-path $xmlFilePath)) {
+    throw "XML file path is not present."
+
+}
+
+
+<#
+   UpdateXMLAttribute function takes xml node as argument and check if for any node it match with 
+   target key for which value need to be changed, It update attribute value. Otherwise, it recursivly
+   checks for all its child node.
+#>
+function UpdateXMLAttribute
+{
+
+    Param(
+          [Parameter(mandatory=$true)][string]$tag,[string] $conditionalAttributeKey , [string] $conditionalAttributeValue ,[string] $targetKey, [string] $targetValue ,[Xml.XmlElement] $xml
+    )
+    
+    if( $xml -eq $null ){
+        return
+    }
+
+    foreach( $node in $xml.ChildNodes )
+    {
+        
+        if( $node.Name -eq $tag ) 
+        {
+            foreach( $childNode in $node.ChildNodes ){   
+                if(  (([string]::IsNullOrEmpty($conditionalAttributeKey) -eq $false -and  $childNode.GetAttribute($conditionalAttributeKey) -eq $conditionalAttributeValue) -or ([string]::IsNullOrEmpty($conditionalAttributeKey))) -and $childNode.GetAttribute($targetKey) -ne $null){
+                    $childNode.SetAttribute($targetKey,$targetValue)
+                    return
+                }
+            }
+            
+        }
+
+        # If node has child recursivly check for each of child for properties
+        if($node.ChildNodes.Count -gt 0 )
+        {
+           UpdateXMLAttribute -xml $node -tag $tag -conditionalAttributeKey $conditionalAttributeKey -conditionalAttributeValue $conditionalAttributeValue -targetKey $targetKey -targetValue $targetValue
+        }
+    }
+}
+
+
+$xml = [xml](Get-Content $xmlFilePath)
+UpdateXMLAttribute -xml $xml.DocumentElement -tag $tag -conditionalAttributeKey $conditionalAttributeKey -conditionalAttributeValue $conditionalAttributeValue -targetKey $targetKey -targetValue $targetValue   
+$xml.Save($file.FullName)
+
+
+
+
+
