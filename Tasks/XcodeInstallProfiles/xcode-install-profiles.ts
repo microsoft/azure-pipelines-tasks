fa3a163c8b258467b6091b951b49b3ecded241f9
Ryu Yu <ryuyu@microsoft.com>
2016-04-18 13:22:45 -0700

    set up template for new task

---
+++ Tasks/XcodeInstallProfiles/xcode-install-profiles.ts
@@ -0,0 +1,6 @@
+/// <reference path="../../definitions/vsts-task-lib.d.ts" />
+
+import taskLibrary = require("vsts-task-lib/task");
+import path = require("path");
+var execSync = require("child_process").execSync;
+
