9cd4bf29f9948ea2e2e637844f94dfc9780e6fa1
Bogdan Gavril <bgavrilMS@users.noreply.github.com>
2016-04-29 17:51:56 +0200

    Users/bgavril/m95fix (#1611)
    * Fix https://jira.sonarsource.com/browse/TBSTFNP-44 and move the sonarqube helpers to a common location
    
    Remove a logging statement
    
    * Other minor changes
    
    * Bump SonarQube PostTest task version

---
+++ Tests/L0/Common-SonarQubeHelpers/InvokeRestApi.ps1
@@ -0,0 +1,71 @@
+[CmdletBinding()]
+param()
+
+. $PSScriptRoot\..\..\lib\Initialize-Test.ps1
+. $PSScriptRoot\..\..\..\Tasks\SonarQubePostTest\Common\SonarQubeHelpers\SonarQubeHelper.ps1
+
+# Common setup
+$distributedTaskContext = 'Some distributed task context'
+Register-Mock Get-TaskVariable { 'http://testsqserver:9999' } -- -Context $distributedTaskContext -Name 'MSBuild.SonarQube.HostUrl'
+Register-Mock Invoke-RestMethod # Hijack the PowerShell function used to make REST calls
+
+
+# Test case 1: unauthenticated users can make calls to the SQ server
+
+# Act - no credentials
+InvokeGetRestMethod -query "/api/test" -useAuth $true         
+
+# Assert
+Assert-WasCalled Invoke-RestMethod -ParametersEvaluator { 
+                        $Uri -eq 'http://testsqserver:9999/api/test' -and
+                        $Method -eq 'Get' -and
+                        $Headers -eq $null     # no (auth) headers
+                        }
+
+Unregister-Mock Invoke-RestMethod
+
+# Test case 2: authenticated users can make authenticated calls to the SQ server
+
+Register-Mock Get-TaskVariable { 'user' } -- -Context $distributedTaskContext -Name "MSBuild.SonarQube.ServerUsername"
+Register-Mock Get-TaskVariable { 'pa$$word' } -- -Context $distributedTaskContext -Name "MSBuild.SonarQube.ServerPassword"
+Register-Mock Invoke-RestMethod
+
+# Act
+InvokeGetRestMethod -query "/api/test" -useAuth $true               
+
+# Assert
+Assert-WasCalled Invoke-RestMethod -ParametersEvaluator { 
+                        $Uri -eq 'http://testsqserver:9999/api/test' -and
+                        $Method -eq 'Get' -and
+                        $Headers.Count -eq 1 -and
+                        $Headers['Authorization'] -eq 'Basic dXNlcjpwYSQkd29yZA==' #this is base64 slang for 'user:pa$$word'
+                        }
+
+Unregister-Mock Invoke-RestMethod
+
+
+# Test case 3: authenticated users can make unauthenicated calls to the SQ server
+
+Register-Mock Get-TaskVariable { 'user' } -- -Context $distributedTaskContext -Name "MSBuild.SonarQube.ServerUsername"
+Register-Mock Get-TaskVariable { 'pa$$word' } -- -Context $distributedTaskContext -Name "MSBuild.SonarQube.ServerPassword"
+Register-Mock Invoke-RestMethod
+
+# Act 
+InvokeGetRestMethod -query "/api/test" -useAuth $false         
+
+# Assert
+Assert-WasCalled Invoke-RestMethod -ParametersEvaluator { 
+                        $Uri -eq 'http://testsqserver:9999/api/test' -and
+                        $Method -eq 'Get' -and
+                        $Headers -eq $null  # No (auth) headers                 
+                        }
+
+
+
+
+
+
+
+
+
+
---
+++ Tests/L0/Common-SonarQubeHelpers/_suite.ts
@@ -0,0 +1,28 @@
+/// <reference path="../../../definitions/mocha.d.ts"/>
+/// <reference path="../../../definitions/node.d.ts"/>
+/// <reference path="../../../definitions/Q.d.ts"/>
+
+import Q = require('q');
+import psm = require('../../lib/psRunner');
+import path = require('path');
+var shell = require('shelljs');
+var ps = shell.which('powershell');
+
+describe('Common-SonarQubeHelpers Suite', function () {
+    this.timeout(20000);
+
+    before((done) => {
+        // init here
+        done();
+    });
+
+    after(function () {
+    });
+
+    if (ps) {
+        it('A tests', (done) => {
+            psm.runPS(path.join(__dirname, 'InvokeRestApi.ps1'), done);
+        })
+       
+    }
+});
