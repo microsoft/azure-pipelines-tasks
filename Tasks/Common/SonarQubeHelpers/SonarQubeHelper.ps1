a6dbb85617eb8f8ea6dca83913fae6ae82d4b5e7
Bogdan Gavril <bgavril@microsoft.com>
2016-05-09 12:24:04 +0100

    Enable PRCA by default

--- Tasks/Common/SonarQubeHelpers/SonarQubeHelper.ps1
+++ Tasks/Common/SonarQubeHelpers/SonarQubeHelper.ps1
@@ -268,7 +268,7 @@ function IsFeatureEnabled
 #
 function ExitOnPRBuild
 {    
-    if ((IsPrBuild) -and !(IsFeatureEnabled "SQPullRequestBot" $false))
+    if ((IsPrBuild) -and !(IsFeatureEnabled "SQPullRequestBot" $true))
     {
         Write-Host "SonarQube analysis is disabled during builds triggered by pull requests. Set a build variable named 'SQPullRequestBot' to 'true' to have the task post code analysis issues as comments in the PR. More information at http://go.microsoft.com/fwlink/?LinkID=786316"
         exit
