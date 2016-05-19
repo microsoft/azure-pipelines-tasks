edf87ba5841c6698013a051f01bbe45c53fd18a3
Bryan MacFarlane <bryanmac@microsoft.com>
2016-05-18 20:08:56 -0400

    commenting out some tests that are failing on OSX and taking a long time to fail

--- Tasks/ShellScript/shellscript.ts
+++ Tasks/ShellScript/shellscript.ts
@@ -1,42 +1,42 @@
+/// <reference path="../../definitions/node.d.ts" />
+/// <reference path="../../definitions/Q.d.ts" />
 /// <reference path="../../definitions/vsts-task-lib.d.ts" />
 
 import path = require('path');
 import tl = require('vsts-task-lib/task');
 
 async function run() {
-    tl.setResourcePath(path.join( __dirname, 'task.json'));
+    try {    
+        tl.setResourcePath(path.join( __dirname, 'task.json'));
 
-    var bash = tl.createToolRunner(tl.which('bash', true));
+        var bash = tl.createToolRunner(tl.which('bash', true));
 
-    var scriptPath: string = tl.getPathInput('scriptPath', true, true);
-    var cwd: string = tl.getPathInput('cwd', true, false);
+        var scriptPath: string = tl.getPathInput('scriptPath', true, true);
+        var cwd: string = tl.getPathInput('cwd', true, false);
 
-    // if user didn't supply a cwd (advanced), then set cwd to folder script is in.
-    // All "script" tasks should do this
-    if (!tl.filePathSupplied('cwd') && !tl.getBoolInput('disableAutoCwd', false)) {
-        cwd = path.dirname(scriptPath);
-    }
-    tl.mkdirP(cwd);
-    tl.cd(cwd);
+        // if user didn't supply a cwd (advanced), then set cwd to folder script is in.
+        // All "script" tasks should do this
+        if (!tl.filePathSupplied('cwd') && !tl.getBoolInput('disableAutoCwd', false)) {
+            cwd = path.dirname(scriptPath);
+        }
+        tl.mkdirP(cwd);
+        tl.cd(cwd);
 
-    bash.pathArg(scriptPath);
+        bash.pathArg(scriptPath);
 
-    // additional args should always call argString.  argString() parses quoted arg strings
-    bash.argString(tl.getInput('args', false));
+        // additional args should always call argString.  argString() parses quoted arg strings
+        bash.argString(tl.getInput('args', false));
 
-    // determines whether output to stderr will fail a task.
-    // some tools write progress and other warnings to stderr.  scripts can also redirect.
-    var failOnStdErr: boolean = tl.getBoolInput('failOnStandardError', false);
+        // determines whether output to stderr will fail a task.
+        // some tools write progress and other warnings to stderr.  scripts can also redirect.
+        var failOnStdErr: boolean = tl.getBoolInput('failOnStandardError', false);
 
-    try{
         var code: number = await bash.exec(<any>{failOnStdErr: failOnStdErr});
         tl.setResult(tl.TaskResult.Succeeded, tl.loc('BashReturnCode', code));
     }
-    catch (err) {
-        tl.debug('taskRunner fail');
+    catch(err) {
         tl.setResult(tl.TaskResult.Failed, tl.loc('BashFailed', err.message));
     }    
 }
 
 run();
-
