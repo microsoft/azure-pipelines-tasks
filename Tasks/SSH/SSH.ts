fce8496100df9f934384b6643fcb7f3f0a981632
madhurig <madhuri.gummalla@hotmail.com>
2016-06-07 15:14:04 -0400

    ssh task L0 tests

--- Tasks/SSH/SSH.ts
+++ Tasks/SSH/SSH.ts
@@ -6,73 +6,73 @@ import fs = require('fs');
 var Client = require('ssh2').Client;
 
 function runCommandsUsingSSH(sshConfig, commands) {
-    var stdout:string = '';
+    try {
+        var stdout:string = '';
 
-    var client = new Client();
-    client.on('ready', function () {
-        tl.debug('SSH connection succeeded, client is ready.');
-        client.shell(function (err, stream) {
-            if (err) {
-                tl._writeError(err);
-            }
-            stream.on('close', function () {
-                tl._writeLine(stdout);
-                client.end();
-            }).on('data', function (data) {
-                stdout = stdout.concat(data);
-                if (stdout.endsWith('\n')) {
-                    tl._writeLine(stdout);
-                    stdout = '';
+        var client = new Client();
+        client.on('ready', function () {
+            tl.debug('SSH connection succeeded, client is ready.');
+            client.shell(function (err, stream) {
+                if (err) {
+                    tl._writeError(err);
                 }
-            }).stderr.on('data', function (data) {
-                    tl._writeError(data);
-                });
-            stream.end(commands);
-        });
-    }).on('error', function (err) {
-        tl.setResult(tl.TaskResult.Failed, err);
-    }).connect(sshConfig);
+                stream.on('close', function () {
+                    tl._writeLine(stdout);
+                    client.end();
+                }).on('data', function (data) {
+                    stdout = stdout.concat(data);
+                    if (stdout.endsWith('\n')) {
+                        tl._writeLine(stdout);
+                        stdout = '';
+                    }
+                }).stderr.on('data', function (data) {
+                        tl._writeError(data);
+                    });
+                stream.end(commands);
+            });
+        }).on('error', function (err) {
+            tl.setResult(tl.TaskResult.Failed, 'Failed to connect to remote machine. Verify the SSH endpoint details. Error: '  + err);
+        }).connect(sshConfig);
+    } catch(err) {
+        tl.setResult(tl.TaskResult.Failed, 'Failed to connect to remote machine. Verify the SSH endpoint details. Error: ' + err);
+    }
 }
 
 var sshEndpoint = tl.getInput('sshEndpoint', true);
-if(!sshEndpoint) {
-    tl.setResult(tl.TaskResult.Failed, 'SSH endpoint could not be found.');
+var username:string = tl.getEndpointAuthorizationParameter(sshEndpoint, 'username', false);
+var password:string = tl.getEndpointAuthorizationParameter(sshEndpoint, 'password', false);
+
+var privateKey:string = tl.getEndpointDataParameter(sshEndpoint, 'privateKey', true);
+var hostname:string = tl.getEndpointDataParameter(sshEndpoint, 'host', false);
+var port:string = tl.getEndpointDataParameter(sshEndpoint, 'port', true);
+if(!port || port === '') {
+    tl._writeLine('Using port 22 which is the default for SSH since no port was specified.');
+    port = '22';
+}
+
+var sshConfig;
+if (privateKey && privateKey !== '') {
+    tl.debug('Using private key for ssh connection.');
+    sshConfig = {
+        host: hostname,
+        port: port,
+        username: username,
+        privateKey: privateKey,
+        passphrase: password
+    }
 } else {
-    var username:string = tl.getEndpointAuthorizationParameter(sshEndpoint, 'username', false);
-    var password:string = tl.getEndpointAuthorizationParameter(sshEndpoint, 'password', false);
-
-    var privateKey:string = tl.getEndpointDataParameter(sshEndpoint, 'privateKey', true);
-    var hostname:string = tl.getEndpointDataParameter(sshEndpoint, 'host', false);
-    var port:string = tl.getEndpointDataParameter(sshEndpoint, 'port', true);
-    if(!port || port === '') {
-        tl.debug('Using port 22 which is the default for SSH since no port was specified.')
-        port = '22';
+    //use password
+    tl.debug('Using username and password for ssh connection.');
+    sshConfig = {
+        host: hostname,
+        port: port,
+        username: username,
+        password: password
     }
+}
 
-    var sshConfig;
-    if (privateKey && privateKey !== '') {
-        tl.debug('Using private key for ssh connection.');
-        sshConfig = {
-            host: hostname,
-            port: port,
-            username: username,
-            privateKey: privateKey,
-            passphrase: password
-        }
-    } else {
-        //use password
-        tl.debug('Using username and password for ssh connection.');
-        sshConfig = {
-            host: hostname,
-            port: port,
-            username: username,
-            password: password
-        }
-    }
-
-    var commands = tl.getInput('commands');
-    if (commands) {
-        commands = commands.concat('\nexit\n');
-        runCommandsUsingSSH(sshConfig, commands);
-    }
+var commands = tl.getInput('commands');
+if (commands) {
+    commands = commands.concat('\nexit\n');
+    runCommandsUsingSSH(sshConfig, commands);
 }
