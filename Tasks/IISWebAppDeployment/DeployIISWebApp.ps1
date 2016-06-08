7838946e0449b6c69e2e33417405d879cff00133
Christopher Johnson (CJ) <chjohn@microsoft.com>
2016-06-08 14:26:51 -0400

    Replacing http links with https where available

--- Tasks/IISWebAppDeployment/DeployIISWebApp.ps1
+++ Tasks/IISWebAppDeployment/DeployIISWebApp.ps1
@@ -36,7 +36,7 @@
     [string]$deployInParallel
     )
 
-Write-Warning "The preview IIS Web App Deployment task has been deprecated and will be removed soon. An IIS Web App Deployment extension has been released in the Visual Studio Team Services marketplace at http://aka.ms/iisextn. Install the extension, and use its tasks in the Build/Release definitions, and delete the preview task from the definition."
+Write-Warning "The preview IIS Web App Deployment task has been deprecated and will be removed soon. An IIS Web App Deployment extension has been released in the Visual Studio Team Services marketplace at https://aka.ms/iisextn. Install the extension, and use its tasks in the Build/Release definitions, and delete the preview task from the definition."
 Write-Verbose "Entering script DeployIISWebApp.ps1" -Verbose
 
 $hostName = [string]::Empty
--- Tasks/XamarinAndroid/xamarinandroid.ts
+++ Tasks/XamarinAndroid/xamarinandroid.ts
@@ -141,7 +141,7 @@ result.then(() => {
 })
 .fail((err) => {
     tl.error(err);
-    tl.error('See http://go.microsoft.com/fwlink/?LinkId=760847');
+    tl.error('See https://go.microsoft.com/fwlink/?LinkId=760847');
     tl.exit(1);
 });
 
