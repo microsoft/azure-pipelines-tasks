962a842c7163ee0c561ae7700cc8c245a783498a
Sachin Aggarwal <t-saagg@microsoft.com>
2016-06-10 17:48:39 +0530

    Changed gulp and grunt tasks

--- Tasks/Grunt/grunttask.ts
+++ Tasks/Grunt/grunttask.ts
@@ -2,6 +2,8 @@
 
 import path = require('path');
 import tl = require('vsts-task-lib/task');
+import fs = require('fs');
+import os = require('os');
 
 tl.setResourcePath(path.join( __dirname, 'task.json'));
 
@@ -10,6 +12,10 @@ var gruntFile = tl.getPathInput('gruntFile', true, true);
 var cwd = tl.getPathInput('cwd', true, false);
 tl.mkdirP(cwd);
 tl.cd(cwd);
+
+var npm = tl.createToolRunner(tl.which('npm', true));
+npm.arg('install');
+npm.arg('istanbul');
 
 var grunt = tl.which('grunt', false);
 
@@ -41,11 +47,81 @@ gt.pathArg(gruntFile);
 
 gt.argString(tl.getInput('arguments', false));
 
+var enableCoverage = tl.getBoolInput('enableCodeCoverage');
+
+if(enableCoverage){
+	var testSrc = tl.getPathInput('testFiles', true, true);
+	
+	if(os.type().match(/^Win/)){
+		var istanbul = tl.createToolRunner(tl.which('istanbul', true));
+	}else{
+		var istanbul = tl.createToolRunner(tl.which('./node_modules/istanbul/lib/cli.js', true));
+	}
+	
+	istanbul.argString('cover --report cobertura');
+	istanbul.arg('./node_modules/mocha/bin/_mocha');
+	istanbul.arg(testSrc);
+	istanbul.argString('-- --ui bdd -t 5000');
+	
+	var buildFolder = tl.getVariable('System.DefaultWorkingDirectory');
+
+	var summaryFile = path.join(buildFolder,'coverage/cobertura-coverage.xml');
+}
+
+var publishJUnitResults = tl.getBoolInput('publishJUnitResults');
+var testResultsFiles = tl.getInput('testResultsFiles', publishJUnitResults);
+
+npm.exec().then(function(){
 gt.exec()
 .then(function(code) {
-	tl.setResult(tl.TaskResult.Succeeded, tl.loc('GruntReturnCode', code));
+	if(enableCoverage){
+		istanbul.exec().then(function(code){
+			publishTestResults(publishJUnitResults, testResultsFiles);
+			publishCodeCoverage(summaryFile);
+			tl.setResult(tl.TaskResult.Succeeded, tl.loc('GruntReturnCode', code));
+		});
+	}else{
+		publishTestResults(publishJUnitResults, testResultsFiles);
+		publishCodeCoverage(summaryFile);
+		tl.setResult(tl.TaskResult.Succeeded, tl.loc('GruntReturnCode', code));
+	}
+	
 })
 .fail(function(err) {
+	publishTestResults(publishJUnitResults, testResultsFiles);
 	tl.debug('taskRunner fail');
 	tl.setResult(tl.TaskResult.Failed, tl.loc('GruntFailed', err.message));
 })
+}).fail(function(err){
+	tl.debug('taskRunner fail');
+	tl.setResult(tl.TaskResult.Failed, tl.loc('NpmFailed', err.message));
+})
+
+function publishTestResults(publishJUnitResults, testResultsFiles: string) {
+    if (publishJUnitResults) {
+        //check for pattern in testResultsFiles
+        if (testResultsFiles.indexOf('*') >= 0 || testResultsFiles.indexOf('?') >= 0) {
+            tl.debug('Pattern found in testResultsFiles parameter');
+            var buildFolder = tl.getVariable('System.DefaultWorkingDirectory');
+            var allFiles = tl.find(buildFolder);
+            var matchingTestResultsFiles = tl.match(allFiles, testResultsFiles, { matchBase: true });
+        }
+        else {
+            tl.debug('No pattern found in testResultsFiles parameter');
+            var matchingTestResultsFiles = [testResultsFiles];
+        }
+
+        if (!matchingTestResultsFiles) {
+            tl.warning('No test result files matching ' + testResultsFiles + ' were found, so publishing JUnit test results is being skipped.');
+            return 0;
+        }
+
+        var tp = new tl.TestPublisher("JUnit");
+        tp.publish(matchingTestResultsFiles, true, "", "", "", true);
+    }
+}
+
+function publishCodeCoverage(summaryFile){
+	var ccPublisher = new tl.CodeCoveragePublisher();
+	ccPublisher.publish('cobertura', summaryFile,"","");
+}
