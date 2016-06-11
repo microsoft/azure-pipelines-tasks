1ef62364c8d5ec7aea140a2f28e65701bbc295e0
Eric Sciple <ersciple@microsoft.com>
2016-06-11 05:59:15 -0400

    Fix busted unit tests.

--- Tests/L0/VSBuild/PassesArguments.ps1
+++ Tests/L0/VSBuild/PassesArguments.ps1
@@ -3,36 +3,40 @@ param()
 
 # Arrange.
 . $PSScriptRoot\..\..\lib\Initialize-Test.ps1
-foreach ($clean in @($true, $false)) {
-    foreach ($restoreNuGetPackages in @($true, $false)) {
-        foreach ($logProjectEvents in @($true, $false)) {
-            Unregister-Mock Get-VstsInput
-            Unregister-Mock Get-SolutionFiles
-            Unregister-Mock Select-VSVersion
-            Unregister-Mock Select-MSBuildLocation
-            Unregister-Mock Format-MSBuildArguments
-            Unregister-Mock Invoke-BuildTools
-            Register-Mock Get-VstsInput { 'Some input VS version' } -- -Name VSVersion
-            Register-Mock Get-VstsInput { 'Some input architecture' } -- -Name MSBuildArchitecture
-            Register-Mock Get-VstsInput { 'Some input arguments' } -- -Name MSBuildArgs
-            Register-Mock Get-VstsInput { 'Some input solution' } -- -Name Solution -Require
-            Register-Mock Get-VstsInput { 'Some input platform' } -- -Name Platform
-            Register-Mock Get-VstsInput { 'Some input configuration' } -- -Name Configuration
-            Register-Mock Get-VstsInput { $clean } -- -Name Clean -AsBool
-            Register-Mock Get-VstsInput { $restoreNuGetPackages } -- -Name RestoreNuGetPackages -AsBool
-            Register-Mock Get-VstsInput { $logProjectEvents } -- -Name LogProjectEvents -AsBool
-            Register-Mock Get-SolutionFiles { 'Some solution 1', 'Some solution 2' } -- -Solution 'Some input solution'
-            Register-Mock Select-VSVersion { 'Some VS version' } -- -PreferredVersion 'Some input VS version'
-            Register-Mock Select-MSBuildLocation { 'Some MSBuild location' } -- -VSVersion 'Some VS version' -Architecture 'Some input architecture'
-            Register-Mock Format-MSBuildArguments { 'Some formatted arguments' } -- -MSBuildArguments 'Some input arguments' -Platform 'Some input platform' -Configuration 'Some input configuration' -VSVersion 'Some VS version'
-            Register-Mock Invoke-BuildTools { 'Some build output' }
+$variableSets = @(
+    @{ Clean = $false ; RestoreNugetPackages = $false ; LogProjectEvents = $false ; CreateLogFile = $false }
+    @{ Clean = $false ; RestoreNugetPackages = $false ; LogProjectEvents = $false ; CreateLogFile = $true }
+    @{ Clean = $false ; RestoreNugetPackages = $false ; LogProjectEvents = $true ; CreateLogFile = $false }
+    @{ Clean = $false ; RestoreNugetPackages = $true ; LogProjectEvents = $false ; CreateLogFile = $false }
+    @{ Clean = $true ; RestoreNugetPackages = $false ; LogProjectEvents = $false ; CreateLogFile = $false }
+)
+foreach ($variableSet in $variableSets) {
+    Unregister-Mock Get-VstsInput
+    Unregister-Mock Get-SolutionFiles
+    Unregister-Mock Select-VSVersion
+    Unregister-Mock Select-MSBuildLocation
+    Unregister-Mock Format-MSBuildArguments
+    Unregister-Mock Invoke-BuildTools
+    Register-Mock Get-VstsInput { 'Some input VS version' } -- -Name VSVersion
+    Register-Mock Get-VstsInput { 'Some input architecture' } -- -Name MSBuildArchitecture
+    Register-Mock Get-VstsInput { 'Some input arguments' } -- -Name MSBuildArgs
+    Register-Mock Get-VstsInput { 'Some input solution' } -- -Name Solution -Require
+    Register-Mock Get-VstsInput { 'Some input platform' } -- -Name Platform
+    Register-Mock Get-VstsInput { 'Some input configuration' } -- -Name Configuration
+    Register-Mock Get-VstsInput { $variableSet.Clean } -- -Name Clean -AsBool
+    Register-Mock Get-VstsInput { $variableSet.RestoreNuGetPackages } -- -Name RestoreNuGetPackages -AsBool
+    Register-Mock Get-VstsInput { $variableSet.LogProjectEvents } -- -Name LogProjectEvents -AsBool
+    Register-Mock Get-VstsInput { $variableSet.CreateLogFile } -- -Name CreateLogFile -AsBool
+    Register-Mock Get-SolutionFiles { 'Some solution 1', 'Some solution 2' } -- -Solution 'Some input solution'
+    Register-Mock Select-VSVersion { 'Some VS version' } -- -PreferredVersion 'Some input VS version'
+    Register-Mock Select-MSBuildLocation { 'Some MSBuild location' } -- -VSVersion 'Some VS version' -Architecture 'Some input architecture'
+    Register-Mock Format-MSBuildArguments { 'Some formatted arguments' } -- -MSBuildArguments 'Some input arguments' -Platform 'Some input platform' -Configuration 'Some input configuration' -VSVersion 'Some VS version'
+    Register-Mock Invoke-BuildTools { 'Some build output' }
 
-            # Act.
-            $output = & $PSScriptRoot\..\..\..\Tasks\VSBuild\VSBuild.ps1
+    # Act.
+    $output = & $PSScriptRoot\..\..\..\Tasks\VSBuild\VSBuild.ps1
 
-            # Assert.
-            Assert-AreEqual 'Some build output' $output
-            Assert-WasCalled Invoke-BuildTools -- -NuGetRestore: $restoreNuGetPackages -SolutionFiles @('Some solution 1', 'Some solution 2') -MSBuildLocation 'Some MSBuild location' -MSBuildArguments 'Some formatted arguments' -Clean: $clean -NoTimelineLogger: $(!$logProjectEvents)
-        }
-    }
+    # Assert.
+    Assert-AreEqual 'Some build output' $output
+    Assert-WasCalled Invoke-BuildTools -- -NuGetRestore: $variableSet.RestoreNuGetPackages -SolutionFiles @('Some solution 1', 'Some solution 2') -MSBuildLocation 'Some MSBuild location' -MSBuildArguments 'Some formatted arguments' -Clean: $variableSet.Clean -NoTimelineLogger: $(!$variableSet.LogProjectEvents) -CreateLogFile: $variableSet.CreateLogFile
 }
