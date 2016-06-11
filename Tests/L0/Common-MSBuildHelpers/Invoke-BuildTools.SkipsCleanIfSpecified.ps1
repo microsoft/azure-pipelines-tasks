5f55b94db359034c65295ebdab01638e91fe5a3a
Eric Sciple <ersciple@microsoft.com>
2016-06-11 06:22:50 -0400

    Fix MSBuild tests.

--- Tests/L0/Common-MSBuildHelpers/Invoke-BuildTools.SkipsCleanIfSpecified.ps1
+++ Tests/L0/Common-MSBuildHelpers/Invoke-BuildTools.SkipsCleanIfSpecified.ps1
@@ -4,15 +4,14 @@ param()
 # Arrange.
 . $PSScriptRoot\..\..\lib\Initialize-Test.ps1
 Microsoft.PowerShell.Core\Import-Module $PSScriptRoot\..\..\..\Tasks\MSBuild\ps_modules\MSBuildHelpers
-$directory = 'Some drive:\Some directory'
 $file = "$directory1\Some solution"
 $msBuildLocation = 'Some MSBuild location'
 $msBuildArguments = 'Some MSBuild arguments'
 Register-Mock Invoke-NuGetRestore { 'Some NuGet output' } -- -File $file
-Register-Mock Invoke-MSBuild { 'Some MSBuild output' } -- -ProjectFile $file -LogFile "$file.log" -MSBuildPath $msBuildLocation -AdditionalArguments $msBuildArguments -NoTimelineLogger: $true
+Register-Mock Invoke-MSBuild { 'Some MSBuild output' } -- -ProjectFile $file -MSBuildPath $msBuildLocation -AdditionalArguments $msBuildArguments -NoTimelineLogger: $true -LogFile: "$file.log"
 
 # Act.
-$actual = Invoke-BuildTools -NuGetRestore -SolutionFiles $file -MSBuildLocation 'Some MSBuild location' -MSBuildArguments 'Some MSBuild arguments' -NoTimelineLogger
+$actual = Invoke-BuildTools -NuGetRestore -SolutionFiles $file -MSBuildLocation 'Some MSBuild location' -MSBuildArguments 'Some MSBuild arguments' -NoTimelineLogger -CreateLogFile
 
 # Assert.
 Assert-AreEqual -Expected @(
