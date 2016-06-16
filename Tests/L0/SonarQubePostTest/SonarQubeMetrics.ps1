585a4cd792ca9f28a97215d53a416e45b1e026f8
Bogdan Gavril <bgavril@microsoft.com>
2016-06-16 16:55:56 +0100

    Do not rely on task variables to be available in the same task

--- Tests/L0/SonarQubePostTest/SonarQubeMetrics.ps1
+++ Tests/L0/SonarQubePostTest/SonarQubeMetrics.ps1
@@ -6,26 +6,13 @@ param()
 . $PSScriptRoot\..\..\..\Tasks\SonarQubePostTest\SonarQubeMetrics.ps1
 . $PSScriptRoot\..\..\lib\Initialize-Test.ps1
 
-$distributedTaskContext = "context"
+#$distributedTaskContext = "context"
 
-# Test 1 - this test ensures the quality gate status is read from the build variable MSBuild.SonarQube.QualityGateStatus if it exists
-
-# Arrange   
-Register-Mock Get-TaskVariable { "OK" } -- -Context $distributedTaskContext -Name 'MSBuild.SonarQube.QualityGateStatus'
-
-# Act
-$actualStatus = GetOrFetchQualityGateStatus
-
-# Assert
-Assert-AreEqual "OK" $actualStatus "The cached quality gate status should be returned" 
-
-# Cleanup
-Unregister-Mock Get-TaskVariable
 
 # Test 2 - this goes through the whole E2E - waiting for the SQ analysis to complete and querying for the quality gate status
 
 # Arrange 
-Register-Mock Get-TaskVariable { $null } -- -Context $distributedTaskContext -Name 'MSBuild.SonarQube.QualityGateStatus'
+#Register-Mock Get-TaskVariable { $null } -- -Context $distributedTaskContext -Name 'MSBuild.SonarQube.QualityGateStatus'
 Register-Mock CompareSonarQubeVersionWith52 { 1 } # SQ version is greater than 5.2 
 Register-Mock GetSonarScannerDirectory { "$PSScriptRoot\data\MetricsTest" }
 Register-Mock SetTaskContextVariable
@@ -43,10 +30,16 @@ Register-Mock InvokeGetRestMethod {$qualityGateResponse} -- "/api/qualitygates/p
 # Act 1 - calling GetOrFetchQualityGateStatus without first calling WaitForAnalysisToFinish fails
 Assert-Throws {GetOrFetchQualityGateStatus} "*WaitForAnalysisToFinish*"
  
+# Act 2- wait for the analysis to finish and query the quality gate status
 WaitForAnalysisToFinish
-Assert-WasCalled SetTaskContextVariable -- 'MSBuild.SonarQube.AnalysisId' '10337' 
-Register-Mock GetTaskContextVariable {'10337'} -- 'MSBuild.SonarQube.AnalysisId' 
+Assert-WasCalled SetTaskContextVariable -- 'MSBuild.SonarQube.AnalysisId' '10337'  
 $actualStatus = GetOrFetchQualityGateStatus
+
+Assert-WasCalled InvokeGetRestMethod -Times 1 -- "/api/qualitygates/project_status?analysisId=10337"
+
+# Act 3- query again and assert that we don't hit the server with a REST API call
+$actualStatus = GetOrFetchQualityGateStatus
+Assert-WasCalled InvokeGetRestMethod -Times 1 -- "/api/qualitygates/project_status?analysisId=10337"
 
 # Assert
 Assert-AreEqual "OK" $actualStatus
