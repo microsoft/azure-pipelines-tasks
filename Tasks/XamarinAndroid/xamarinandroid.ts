d1896cc9bc8656133dd7db75affc06c8067582f6
Alexandre Zollinger Chohfi <chohfi@outlook.com>
2016-06-27 18:38:22 -0300

    Added option to Clean the project in a Xamarin Android Build Task. (#1840)
    * Added option to Clean the project in a Xamarin Android Build Task.
    
    * Reverted localization for "clean" in XamarinAndroid build Task.
    
    * Changed order of /t:Clean to make sure it cleans before it builds and not the other way around.
    
    * Fix tests for new Clean parameter order.
    
    * Changed the helpMarkDown of the Clean param.
    
    * Clean build to regenerate task.loc.json.

--- Tasks/XamarinAndroid/xamarinandroid.ts
+++ Tasks/XamarinAndroid/xamarinandroid.ts
@@ -11,6 +11,7 @@ var project = tl.getPathInput('project', true);
 var target = tl.getInput('target');
 var outputDir = tl.getInput('outputDir');
 var configuration = tl.getInput('configuration');
+var clean = tl.getBoolInput('clean');
 var xbuildLocation = tl.getPathInput('msbuildLocation');
 var msbuildArguments = tl.getInput('msbuildArguments');
 
@@ -63,6 +64,9 @@ var runxbuild = function (fn) {
         var xbuild = tl.createToolRunner(xbuildToolPath);
         xbuild.pathArg(fn);
 
+		if(clean) {
+            xbuild.arg('/t:Clean');
+        }
         if (target) {
             xbuild.arg('/t:' + target);
         }
