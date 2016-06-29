cd7c911bcb0e90d0b57e13a9567ea507f5c02093
ashwini reddy <achalla@microsoft.com>
2016-06-29 12:30:45 +0530

    fix

--- Tasks/VsTest/vstest.ts
+++ Tasks/VsTest/vstest.ts
@@ -472,7 +472,17 @@ function getTestResultsDirectory(settingsFile: string, defaultResultsDirectory: 
             parser.parseString(xmlContents, function (err, result) {
                 if (!err && result.RunSettings && result.RunSettings.RunConfiguration && result.RunSettings.RunConfiguration[0] &&
                     result.RunSettings.RunConfiguration[0].ResultsDirectory && result.RunSettings.RunConfiguration[0].ResultsDirectory[0].length > 0) {
-                    defer.resolve(result.RunSettings.RunConfiguration[0].ResultsDirectory[0]);
+                    var resultDirectory = result.RunSettings.RunConfiguration[0].ResultsDirectory[0];
+                    resultDirectory = resultDirectory.trim();
+                    if (resultDirectory && path.isAbsolute(resultDirectory)) {
+                        defer.resolve(resultDirectory);
+                    }
+                    else if (resultDirectory) {
+                        defer.resolve(path.resolve(path.dirname(runSettingsFile), resultDirectory));
+                    }
+                    else {
+                        defer.resolve(defaultResultsDirectory);
+                    }
                 }
                 else {
                     defer.resolve(defaultResultsDirectory);
