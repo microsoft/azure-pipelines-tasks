47764425955866192452b1939c4e6314f019d44f
Vincent A <vinca@microsoft.com>
2016-07-06 10:47:26 +0530

    Bug Fix - 525654 & 592833 (#2025)
    * Changed 'vms' to 'VMs'
    
    * Fixed Bug-525654

--- Tasks/AzureFileCopy/Utility.ps1
+++ Tasks/AzureFileCopy/Utility.ps1
@@ -783,7 +783,7 @@ function Check-AzureCloudServiceExists
             if($connectionType -eq 'Certificate')
             {
                 Write-TaskSpecificTelemetry "PREREQ_ResourceGroupNotFound"
-                throw (Get-LocalizedString -Key "Using selected Connection '{0}' unable to find the resource '{1}'. Selected connection '{0}' supports classic resources only (Service Management model)." -ArgumentList $connectionType, $cloudServiceName)
+                throw (Get-LocalizedString -Key "Unable to find the resource '{1}' using selected connection '{0}'. Selected connection '{0}' supports classic resources only (Service Management model)." -ArgumentList $connectionType, $cloudServiceName)
             }
         }
     }
@@ -1057,7 +1057,7 @@ function Copy-FilesToAzureVMsFromStorageContainer
     }
 
     # if no error thrown, copy successfully succeeded
-    Write-Output (Get-LocalizedString -Key "Copied files from source path: '{0}' to target azure vms in resource group: '{1}' successfully" -ArgumentList $sourcePath, $resourceGroupName)
+    Write-Output (Get-LocalizedString -Key "Copied files from source path: '{0}' to target azure VMs in resource group: '{1}' successfully" -ArgumentList $sourcePath, $resourceGroupName)
 }
 
 function Validate-CustomScriptExecutionStatus
