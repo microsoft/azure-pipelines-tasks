d21f2948fc0631621a94b57f888cba57e6f3ed94
Shu Sam Chen <sschen@microsoft.com>
2016-07-18 13:38:50 +0100

    SonarQube build summary now reports on quality gate status, too.

---
+++ Tasks/Gradle/sonarqube-common/sonarqube-qualitygate.ts
@@ -0,0 +1,42 @@
+/// <reference path="../../../definitions/vsts-task-lib.d.ts" />
+
+import tl = require('../testlib/tasklib-wrapper');
+
+// Creates a build summary section to display the quality gate status.
+// Returns null if analysisStatus was null.
+export function createBuildSummaryQualityGateSection(analysisStatus:string):string {
+    if (!analysisStatus) {
+        return null;
+    }
+
+    var visualStatus:QualityGateVisualStatus = QualityGateVisualStatus.createFromAnalysisStatus(analysisStatus);
+
+    // ES6 template literal usage to streamline creating this section.
+    var reportContents:string  = `<div style="padding:5px 0px">
+        <span>Quality Gate</span>
+    <span style="padding:4px 10px; margin-left: 5px; background-color:${visualStatus.Color}; color:#fff; display:inline-block">${visualStatus.Label}</span>
+        </div>`;
+
+    return reportContents;
+}
+
+class QualityGateVisualStatus {
+    constructor(public Color: string, public Label: string) {
+    }
+
+    public static createFromAnalysisStatus(analysisStatus:string):QualityGateVisualStatus {
+        switch (analysisStatus.toUpperCase()) {
+            case 'OK':
+                return new QualityGateVisualStatus('#85BB43', 'Passed');
+            case 'WARN':
+                return new QualityGateVisualStatus('#F90', 'Warning');
+            case 'ERROR':
+                return new QualityGateVisualStatus('#D4333F', 'Failed');
+            case 'NONE':
+                return new QualityGateVisualStatus('#BBB', 'None');
+            default:
+                tl.warning('Could not detect the quality gate status or a new status has been introduced.'); // TODO: LOC
+                return new QualityGateVisualStatus('#BBB', 'Unknown');
+        }
+    }
+}
