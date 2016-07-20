e32056b7ba89cb7559f3f4cfa2ef5096b4f3a8da
madhurig <madhuri.gummalla@hotmail.com>
2016-07-20 18:55:20 -0400

    SCP task with common module

--- Tasks/SCP/scp.ts
+++ Tasks/SCP/scp.ts
@@ -2,13 +2,112 @@
 
 import path = require('path');
 import tl = require('vsts-task-lib/task');
+import os = require('os');
 import fs = require('fs');
-import sshHelper = require('./ssh2helpers');
-import {RemoteCommandOptions} from './ssh2helpers'
+import Q = require('q');
+import sshCommon = require('ssh-common/ssh-common');
+import {SshHelper} from 'ssh-common/ssh-common';
+
+function getFilesToCopy(sourceFolder, contents: string []) : string [] {
+    // include filter
+    var includeContents: string[] = [];
+    // exclude filter
+    var excludeContents: string[] = [];
+
+    for (var i: number = 0; i < contents.length; i++){
+        var pattern = contents[i].trim();
+        var negate: Boolean = false;
+        var negateOffset: number = 0;
+        for (var j = 0; j < pattern.length && pattern[j] === '!'; j++){
+            negate = !negate;
+            negateOffset++;
+        }
+
+        if(negate){
+            tl.debug('exclude content pattern: ' + pattern);
+            var realPattern = pattern.substring(0, negateOffset) + path.join(sourceFolder, pattern.substring(negateOffset));
+            excludeContents.push(realPattern);
+        }
+        else{
+            tl.debug('include content pattern: ' + pattern);
+            var realPattern = path.join(sourceFolder, pattern);
+            includeContents.push(realPattern);
+        }
+    }
+
+    // enumerate all files
+    var files: string[] = [];
+    var allPaths: string[] = tl.find(sourceFolder);
+    var allFiles: string[] = [];
+
+    // remove folder path
+    for (var i: number = 0; i < allPaths.length; i++) {
+        if (!tl.stats(allPaths[i]).isDirectory()) {
+            allFiles.push(allPaths[i]);
+        }
+    }
+
+    // if we only have exclude filters, we need add a include all filter, so we can have something to exclude.
+    if(includeContents.length == 0 && excludeContents.length > 0) {
+        includeContents.push('**');
+    }
+
+    if (includeContents.length > 0 && allFiles.length > 0) {
+        tl.debug("allFiles contains " + allFiles.length + " files");
+
+        // a map to eliminate duplicates
+        var map = {};
+
+        // minimatch options
+        var matchOptions = { matchBase: true };
+        if(os.type().match(/^Win/))
+        {
+            matchOptions["nocase"] = true;
+        }
+
+        // apply include filter
+        for (var i: number = 0; i < includeContents.length; i++) {
+            var pattern = includeContents[i];
+            tl.debug('Include matching ' + pattern);
+
+            // let minimatch do the actual filtering
+            var matches: string[] = tl.match(allFiles, pattern, matchOptions);
+
+            tl.debug('Include matched ' + matches.length + ' files');
+            for (var j: number = 0; j < matches.length; j++) {
+                var matchPath = matches[j];
+                if (!map.hasOwnProperty(matchPath)) {
+                    map[matchPath] = true;
+                    files.push(matchPath);
+                }
+            }
+        }
+
+        // apply exclude filter
+        for (var i: number = 0; i < excludeContents.length; i++) {
+            var pattern = excludeContents[i];
+            tl.debug('Exclude matching ' + pattern);
+
+            // let minimatch do the actual filtering
+            var matches: string[] = tl.match(files, pattern, matchOptions);
+
+            tl.debug('Exclude matched ' + matches.length + ' files');
+            files = [];
+            for (var j: number = 0; j < matches.length; j++) {
+                var matchPath = matches[j];
+                files.push(matchPath);
+            }
+        }
+    }
+    else {
+        tl.debug("Either includeContents or allFiles is empty");
+    }
+
+    return files;
+}
 
 async function run() {
-    var sshClientConnection: any;
-
+    var sshHelper : SshHelper;
     try {
         tl.setResourcePath(path.join(__dirname, 'task.json'));
 
@@ -56,36 +155,69 @@ async function run() {
         var overwrite:boolean = tl.getBoolInput('overwrite', false);
         var flattenFolders:boolean = tl.getBoolInput('flattenFolders', false);
 
-        //setup the SSH connection
-        tl._writeLine(tl.loc('SettingUpSshConnection', sshConfig.username, sshConfig.host, sshConfig.port));
-        try {
-            sshClientConnection = await sshHelper.setupSshClientConnection(sshConfig);
-        } catch (err) {
-            tl.setResult(tl.TaskResult.Failed, tl.loc('ConnectionFailed', err));
-            throw tl.loc('ConnectionFailed', err);
+        if(!tl.stats(sourceFolder).isDirectory()) {
+            tl.setResult(tl.TaskResult.Failed, 'Source folder has to be a directory.');
+            throw 'Source folder has to be a directory.';
         }
 
-        //SSH connection successful
+        //initialize the SSH helpers
+        sshHelper = new sshCommon.SshHelper(sshConfig);
+
         if(cleanTargetFolder) {
-            var cleanTargetFolderCmd = 'cd "' + targetFolder + '";rm -rf *';
+            tl._writeLine(tl.loc('CleanTargetFolder', targetFolder));
+            var cleanTargetFolderCmd = 'rm -rf "' + targetFolder + path.posix.sep + '*"';
             try {
-                await
-                sshHelper.runCommandOnRemoteMachine(cleanTargetFolderCmd, sshClientConnection, null);
+                await sshHelper.runCommandOnRemoteMachine(cleanTargetFolderCmd, null);
             } catch (err) {
                 tl.setResult(tl.TaskResult.Failed, tl.loc('CleanTargetFolderFailed', err));
                 throw tl.loc('CleanTargetFolderFailed', err);
             }
         }
 
+        //copy Files
+        var filesToCopy: string [] = [];
+        filesToCopy = getFilesToCopy(sourceFolder, contents);
+        tl.debug('Number of files to copy = ' + filesToCopy.length);
+        tl.debug('filesToCopy = ' + filesToCopy);
+
+        tl._writeLine(tl.loc('CopyingFiles', filesToCopy.length));
+        var fileCopyProgress : Q.Promise<string> [] = [];
+        for(var i : number = 0; i < filesToCopy.length; i ++) {
+            var fileToCopy = filesToCopy[i];
+            tl.debug('fileToCopy = ' + fileToCopy);
+
+            var relativePath;
+            if(flattenFolders) {
+                relativePath = path.basename(fileToCopy);
+            } else {
+                relativePath = fileToCopy.substring(sourceFolder.length)
+                    .replace(/^\\/g, "")
+                    .replace(/^\//g, "");
+            }
+            tl.debug('relativePath = ' + relativePath);
+            var targetPath = path.posix.join(targetFolder, relativePath);
+
+            tl._writeLine(tl.loc('StartedFileCopy', fileToCopy, targetPath));
+            if(!overwrite) {
+                var fileExists : boolean = await sshHelper.checkRemotePathExists(targetPath);
+                if(fileExists) {
+                    throw tl.loc('FileExists', targetPath);
+                }
+            }
+            fileCopyProgress.push(sshHelper.uploadFile(fileToCopy, targetPath));
+        }
+
+        //wait for all files to be copied
+        await Q.all(fileCopyProgress);
     } catch(err) {
         tl.setResult(tl.TaskResult.Failed, err);
     } finally {
         //close the client connection to halt build execution
-        if (sshClientConnection) {
-            tl.debug('Closing the SSH client connection.');
-            sshClientConnection.end();
+        if(sshHelper) {
+            tl.debug('Closing the client connection');
+            sshHelper.closeConnection();
+            sshHelper = null;
         }
     }
-
 }
 run();
