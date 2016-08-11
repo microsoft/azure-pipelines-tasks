5c87a1c390e1384e5d4699f0a4461238991cc857
vinca <vinca@microsoft.com>
2016-08-11 21:06:13 +0530

    Migrating from Powershell to Node.js

---
+++ Tasks/AzureRmWebAppDeployment/AzureRMUtil.ts
@@ -0,0 +1,294 @@
+/// <reference path="../../definitions/node.d.ts" />
+/// <reference path="../../definitions/q.d.ts" />
+/// <reference path="../../definitions/vsts-task-lib.d.ts" />
+
+var adal = require ('adal-node');
+
+import taskLib = require ('vsts-task-lib/task');
+var request = require ('request');
+import Q = require ('q');
+var parseString = require('xml2js').parseString;
+
+var AuthenticationContext = adal.AuthenticationContext;
+
+function getAuthorizationToken ( SPN ) : Q.Promise <string> {
+
+	var deferred = Q.defer<string>();
+	var authorityUrl = 'https://login.windows.net/' + SPN.tenantID; 
+	var resource = 'https://management.azure.com/';
+
+	var context = new AuthenticationContext (authorityUrl);
+	context.acquireTokenWithClientCredentials (resource, SPN.servicePrincipalClientID, SPN.servicePrincipalKey, function ( error, tokenResponse) {
+		if ( error ) 
+			deferred.reject ( error );
+		else {
+			deferred.resolve ( tokenResponse.accessToken );
+		}
+	});
+
+	return deferred.promise;
+}
+
+exports.getAzureRMWebAppDetails = function ( SPN, webAppName : string ) : Q.Promise<any> {
+
+	var deferred = Q.defer<any> ();
+
+	getAuthorizationToken(SPN)
+	.then(function (accessToken : string) {
+		
+ 		var requestOptions = {
+ 		 	url : 'https://management.azure.com/subscriptions/' + SPN.subscriptionId + '/resources?$filter=resourceType eq \'Microsoft.Web%2FSites\'&api-version=2016-02-01',
+ 		 	auth : {
+ 		 		bearer : accessToken
+ 		 	}
+ 		 };
+
+ 		request (requestOptions, function (error, response, body) {
+ 		 	if ( error ) 
+ 		 		deferred.reject (error);
+ 		 	else {
+ 		 		if ( response.statusCode === 200 ) {
+ 		 			var obj = JSON.parse (body);
+ 		 			for ( var i = 0; i < Object.keys (obj.value).length; i++ ) {
+ 		 				if ( obj.value[i].name == webAppName ) {
+ 		 					deferred.resolve (obj.value[i]);
+						}
+					}
+ 		 			deferred.reject ('Error Occurred : No such web app');
+ 		 		}
+ 		 		else deferred.reject ('Error Occurred : [Staus Code : '+response.statusCode+']');
+
+ 		 	}
+ 		});
+ 		
+	}, function (error) {
+		taskLib.error (error);
+	});
+
+	return deferred.promise;
+
+};
+
+function getPublishProfileUtil ( SPN, webAppName: string, resourceGroupName: string, publishMethod: string, deployToSlotFlag: boolean, slotName: string ): Q.Promise<any> {
+
+    var deferred = Q.defer();
+    var insertInUrl = deployToSlotFlag ? "/slots/" + slotName : "";
+    getAuthorizationToken(SPN)
+        .then(function (accessToken) {
+        var requestOptions = {
+            url: 'https://management.azure.com/subscriptions/' + SPN.subscriptionId + '/resourceGroups/' + resourceGroupName + '/providers/Microsoft.Web/sites/' + webAppName + insertInUrl + '/publishxml?api-version=2015-08-01',
+            auth: {
+                bearer: accessToken
+            },
+            method: 'POST'
+        };
+        request(requestOptions, function (error, response, body) {
+            if (error)
+                deferred.reject(error);
+            else {
+                if (response.statusCode === 200) {
+                    parseString(body, function (error, result) {
+                        for (var i in result.publishData.publishProfile) {
+                            if (result.publishData.publishProfile[i].$.publishMethod === publishMethod)
+                                deferred.resolve(result.publishData.publishProfile[i].$);
+						}
+                        deferred.reject('Error : No Such Deploying Method Exists');
+                    });
+                }
+                else {
+                    deferred.reject('Error Fetching Deployment Publish Profile [Staus Code : '+response.statusCode+']');
+                }
+            }
+        });
+    }, function (error) {
+        taskLib.error(error);
+    });
+
+    return deferred.promise;
+}
+exports.getAzureRMWebAppPublishingProfileDetails = function (SPN, webAppName: string, resourceGroupName: string, publishMethod: string, deployToSlotFlag: boolean, slotName: string): Q.Promise<any> {
+    var deferred = Q.defer();
+      
+    if ( !deployToSlotFlag ) {
+        getAzureRMWebAppDetails_version2 ( SPN, webAppName, 'Microsoft.Web/sites' ).then ( function (webApp) {
+            resourceGroupName = webApp.id.split ('/')[4];
+            getPublishProfileUtil ( SPN, webAppName, resourceGroupName, publishMethod, deployToSlotFlag, slotName).then ( function (publishProfile) {
+                deferred.resolve (publishProfile);
+            }, function (error) {
+                deferred.reject (error);
+            });
+        }, function ( error ) {
+            deferred.reject (error);
+        });
+    } 
+    else  {
+        
+        getPublishProfileUtil ( SPN, webAppName, resourceGroupName, publishMethod, deployToSlotFlag, slotName).then ( function (publishProfile) {
+                deferred.resolve (publishProfile);
+        }, function (error) {
+            deferred.reject (error);
+        });
+
+    }
+    
+    return deferred.promise;
+};
+
+function getAzureRMWebAppDetails_version2 ( SPN, webAppName : string, resourceType : string ) : Q.Promise<any> {
+
+	var deferred = Q.defer<any>();
+
+	getAuthorizationToken(SPN)
+	.then(function (accessToken : string) {
+		
+ 		var requestOptions = {
+ 		 	url : 'https://management.azure.com/subscriptions/' + SPN.subscriptionId + '/resources?$filter=resourceType EQ \'' + resourceType + '\' AND substringof(\'' + webAppName + '\', name)&api-version=2016-07-01',
+ 		 	auth : {
+ 		 		bearer : accessToken
+ 		 	}
+ 		 };
+
+ 		request (requestOptions, function (error, response, body) {
+ 		 	if ( error ) 
+ 		 		deferred.reject (error);
+ 		 	else {
+ 		 		if ( response.statusCode === 200 ) {
+ 		 			var obj = JSON.parse (body);
+ 		 			deferred.resolve (obj.value[0]);
+ 		 		}
+ 		 		else deferred.reject ('Error Occurred : [Staus Code : '+response.statusCode+']');
+
+ 		 	}
+ 		});
+ 		
+	}, function (error) {
+		taskLib.error(error);
+	});
+
+	return deferred.promise;
+
+}
+exports.getAzureRMWebAppDetails_version2 = getAzureRMWebAppDetails_version2;
+
+function updateDeploymentStatus (azureRMWebAppConnectionDetails, deployAzureWebsiteError : boolean) {
+
+	var webAppPublishKuduUrl = azureRMWebAppConnectionDetails.publishUrl;
+	
+	if ( webAppPublishKuduUrl ) {
+
+		var status = 3;	 // Set default status as failed 
+		var status_text = 'failed';
+
+		if ( !deployAzureWebsiteError ) {
+			status = 4;
+			status_text = 'succeeded';
+		}
+
+		/* ----------------------------------------------------------------------------------------
+		username = azureRMWebAppConnectionDetails.UserName;
+		securePwd = < convert to secure string > azureRMWebAppConnectionDetails.UserPassword;
+
+		credential = System.Management.Automation.PSCredential (username, securePwd);
+		------------------------------------------------------------------------------------------ */
+
+		// Get Author of build or release 
+
+		var author = taskLib.getVariable ('build.sourceVersionAuthor');
+		if ( author === undefined )  {
+			author = taskLib.getVariable ('build.requestedfor');
+			if ( author === undefined )
+				author = taskLib.getVariable ('release.requestedfor');
+
+			if ( author === undefined )
+				author = taskLib.getVariable ('agent.name');
+		}
+		
+		// Use buildId/releaseId to update deployment Status 
+		// Use buildUrl/releaseUrl to update deployment message 
+
+		var buildUrlTaskVar = taskLib.getVariable ('build.buildUri'), 
+			releaseUrlTaskVar = taskLib.getVariable ('release.releaseUri');
+
+		// Get the build id/number
+		var buildIdTaskVar = taskLib.getVariable ('build.buildId'), 
+			releaseIdTaskVar = taskLib.getVariable ('release.releaseId');
+
+		var collectionUrl = taskLib.getVariable ('system.TeamFoundationCollectionUri'), 
+			teamProject = taskLib.getVariable ('system.teamProject');	
+
+		var buildOrReleaseUrl = "" ;
+		var uniqueId = Date.now();		// //uniqueId = Get current date ddMMyyhhmmss ;
+		var deploymentId = "";
+		var message = "";
+
+		if ( releaseUrlTaskVar != undefined ) {
+			deploymentId = releaseIdTaskVar + uniqueId;
+			buildOrReleaseUrl = collectionUrl + teamProject + "/_apps/hub/ms.vss-releaseManagement-web.hub-explorer?releaseId=" + releaseIdTaskVar + "&_a=release-summary";
+			message = "Updating Deployment History For Deployment " + buildOrReleaseUrl;
+
+		}		
+		else {
+			deploymentId = buildIdTaskVar + uniqueId;
+			buildOrReleaseUrl = collectionUrl + teamProject + "/_build?buildId=" + buildIdTaskVar + "&_a=summary";
+			message = "Updating Deployment History For Deployment " + buildOrReleaseUrl;
+		}
+
+		if ( deploymentId === undefined ) {
+			// no point in proceeding further 
+			taskLib.error('Cannot update deployment status : uniquedeploymentIdCannotBeRetrieved');
+			return ;
+		}
+
+		var body = {
+			status : status, 
+			status_text : status_text, 
+			message : message, 
+			author : author, 
+			deployer : 'VSTS', 
+			details : buildOrReleaseUrl
+		};
+		
+		var webAppHostUrl = webAppPublishKuduUrl.split(':')[0];
+		var RESTurl = "https://" + encodeURIComponent(webAppHostUrl) + "/deployments/" + encodeURIComponent(deploymentId);
+
+		try {
+
+			/*
+			INVOKE THE REST METHOD ...
+			GET THE REST API FOR Invoke-RestMethod RESTurl -Credential credential -Method PUT -Body body
+							-ContentType "application/json" -UserAgent 'myuseragent'
+			*/
+			var requestOptions = {
+				url : RESTurl,
+				method : 'PUT', 
+				json : body, 
+				auth: {
+					username : azureRMWebAppConnectionDetails.userName, 
+					password : azureRMWebAppConnectionDetails.userPWD
+				}
+			};
+
+			request ( requestOptions , function (error, response, body ) { 
+				if ( error ) 
+					taskLib.debug(error);
+				else {
+					if ( response.statusCode === 200 ) {
+						taskLib.debug(body);
+					}
+					else {
+						taskLib.debug(response);
+					}
+				}
+			});
+
+		}
+		catch ( error ) {
+			taskLib.error(error);
+		} 
+	}
+	else {
+		taskLib.error('WARNING : Cannot update deployment status : SCM endpoint is not enabled for this website');
+	}
+
+
+}
---
+++ Tasks/AzureRmWebAppDeployment/MSDeployUtility.ts
@@ -0,0 +1,175 @@
+/// <reference path="../../definitions/node.d.ts" />
+/// <reference path="../../definitions/q.d.ts" />
+/// <reference path="../../definitions/vsts-task-lib.d.ts" />
+import tl = require('vsts-task-lib/task');
+var Q = require('q');
+var regedit = require('regedit');
+import fs = require('fs');
+
+//Error Handler
+var onError = function(error) {
+	tl.error(error);
+	tl.exit(1);
+}
+
+
+function isFileExists(filePath: string) : boolean {
+    try{
+        return fs.statSync(filePath).isFile();
+    }
+    catch(e) {
+        onError("Cannot access "+filePath+" in machine");
+        return false;
+    }
+}
+module.exports.isFileExists = isFileExists;
+
+function getWebAppNameForMSDeployCmd(webAppName: string, deployToSlotFlag: boolean, slotName: string) : string {
+    if(deployToSlotFlag) {
+        webAppName = webAppName + "(" + slotName + ")";
+    }
+    tl.debug("WebApp Name to be used in msdeploy command is: "+webAppName);
+    return webAppName;
+}
+module.exports.getWebAppNameForMSDeployCmd = getWebAppNameForMSDeployCmd;
+
+//Get Parameters for MSDeploy Command
+function getMSDeployCmdArgs(packageFile: string, webAppNameForMSDeployCmd: string, azureRMWebAppConnectionDetails: Array<String>, removeAdditionalFilesFlag: boolean, excludeFilesFromAppDataFlag: boolean,
+                            takeAppOfflineFlag: boolean, virtualApplication: string, setParametersFile: string, additionalArguments: string) : string {
+
+    // msdeploy argument containing source and destination details to sync
+    var msDeployCmdArgs: string = " -verb:sync";
+    msDeployCmdArgs += " -source:package='"+packageFile+"'";
+    msDeployCmdArgs += " -dest:auto,ComputerName='https://"+azureRMWebAppConnectionDetails["KuduHostName"]+"/msdeploy.axd?site="+webAppNameForMSDeployCmd+"',";
+    msDeployCmdArgs += "UserName='"+azureRMWebAppConnectionDetails["UserName"]+"',Password='"+azureRMWebAppConnectionDetails["UserPassword"]+"',AuthType='Basic'";
+
+    // msdeploy argument to set destination IIS App Name for deploy
+    if(virtualApplication) {
+        msDeployCmdArgs += " -setParam:name='IIS Web Application Name',value='"+webAppNameForMSDeployCmd+"/"+virtualApplication+"'";
+    }
+    else {
+        msDeployCmdArgs += " -setParam:name='IIS Web Application Name',value='"+webAppNameForMSDeployCmd+"'";
+    }
+
+    // msdeploy argument to block deletion from happening
+    if(!removeAdditionalFilesFlag) {
+        msDeployCmdArgs += " -enableRule:DoNotDeleteRule";
+    }
+
+    // msdeploy argument to take app offline
+    if(takeAppOfflineFlag) {
+        msDeployCmdArgs +=  " -enableRule:AppOffline";
+    }
+
+    // msdeploy argument to exclude files in App_Data folder
+    if(excludeFilesFromAppDataFlag) {
+        msDeployCmdArgs += " -skip:Directory='\\App_Data'";
+    }
+
+    // msdeploy argument to set paramater files
+    if(setParametersFile) {
+        msDeployCmdArgs += " -setParamFile:'"+setParametersFile+"'";
+    }
+
+    // msdeploy additional arguments
+    if(additionalArguments) {
+        msDeployCmdArgs += " "+additionalArguments;
+    }
+
+    var userAgent = tl.getVariable("AZURE_HTTP_USER_AGENT");
+    if(userAgent) {
+        msDeployCmdArgs += " -userAgent:'"+userAgent+"'";
+    }
+
+    tl.debug("Constructed msdeploy command arguments to deploy to deploy to azureRM WebApp: "+webAppNameForMSDeployCmd+
+            " from source Web App zip package: "+packageFile);
+    
+    return msDeployCmdArgs;
+}
+module.exports.getMSDeployCmdArgs = getMSDeployCmdArgs;
+
+function getMSDeployCmdForLogs(msDeployCmdArgs: string) : string {
+    var msDeployCmdSplitByComma = msDeployCmdArgs.split(",");
+    var msDeployCmdArray = [];
+    for(var msDeployCommandAttr in msDeployCmdSplitByComma) {
+        if(msDeployCmdSplitByComma[msDeployCommandAttr].indexOf('Password=') === 0) {
+            msDeployCmdArray.push("Password=*****");
+        }
+        else {
+            msDeployCmdArray.push(msDeployCmdSplitByComma[msDeployCommandAttr]);
+        }
+    }
+    return msDeployCmdArray.join(",");
+}
+
+function runMSDeployCommand(msDeployExePath: string, msDeployCmdArgs: string) {
+    var msDeployCmdForLogs = getMSDeployCmdForLogs(msDeployCmdArgs);
+    tl.debug("[command] "+msDeployExePath+" "+msDeployCmdForLogs);
+
+    tl.exec(msDeployExePath, msDeployCmdArgs)
+	.then((code) => {
+		tl.debug("MSDeploy executed Successfully !");
+        tl.debug("Return Code : "+code);
+	})
+	.fail((error) => {
+		tl.error("MSDeploy failed !");
+        onError(error);
+	});
+}
+
+// Get the latest version of MSDeploy installed
+function getMSDeployVersion(registryKey: string) : Q.Promise<String> {
+    var defer = Q.defer();
+    regedit.list(registryKey)
+    .on('data', function(entry) {
+        var keys = entry.data.keys;
+        keys.sort();
+        defer.resolve(keys[keys.length-1]);
+    })
+    .on('error', function(error) {
+        defer.reject(error);
+    });
+    
+    return defer.promise;
+}
+
+//Get the absolut path of MSDeploy.exe
+function getMSDeployInstallPath(registryKey: string) : Q.Promise<string> {
+    var defer = Q.defer();
+    regedit.list(registryKey)
+    .on('data', function(entry) {
+        defer.resolve(entry.data.values.InstallPath.value);
+    })
+    .on('error', function(error) {
+        defer.reject(error);
+    });
+
+    return defer.promise;
+}
+
+//
+function runMSDeployCommandWrapper(msDeployCmdArgs: string): void {
+    var msDeployInstallPathRegKey = "HKLM\\SOFTWARE\\Microsoft\\IIS Extensions\\MSDeploy";
+    getMSDeployVersion(msDeployInstallPathRegKey)
+    .then(function(version){
+        if(version == null) {
+            onError('Unable to find the location of MS Deploy location');
+            return;
+        }
+        
+        var msDeployLatestPathRegKey = msDeployInstallPathRegKey+"\\"+version;
+        getMSDeployInstallPath(msDeployLatestPathRegKey)
+        .then(function(msDeployPath) {
+            //Append msdeploy.exe to get the absolute path
+            msDeployPath = msDeployPath+"\\msdeploy.exe";
+            runMSDeployCommand(msDeployPath, msDeployCmdArgs);
+        },
+        function(error) {
+            onError(error);
+        });
+    },
+    function(error) {
+        onError(error);
+    });
+}
+module.exports.runMSDeployCommandWrapper = runMSDeployCommandWrapper;
