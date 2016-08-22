prashkan/vsts-tasks

Delete files no follow symlinks.

Tasks/DeleteFiles/deletefiles.ts
@@ -5,40 +5,41 @@ import os = require('os');
 import tl = require('vsts-task-lib/task');
 tl.setResourcePath(path.join(__dirname, 'task.json'));
 
-// contents is a multiline input containing glob patterns
-var contents: string[] = tl.getDelimitedInput('Contents', '\n', true);
-var sourceFolder = tl.getPathInput('SourceFolder', true, false);
+(() => {
+    // contents is a multiline input containing glob patterns
+    let patterns: string[] = tl.getDelimitedInput('Contents', '\n', true);
 
-// Input that is used for backward compatibility with pre-sprint 95 symbol store artifacts.
-// Pre-95 symbol store artifacts were simply file path artifacts, so we need to make sure
-// not to delete the artifact share if it's a symbol store.
-var buildCleanup = tl.getBoolInput('BuildCleanup');
+    let sourceFolder: string = tl.getPathInput('SourceFolder', true, false);
 
-// include filter
-var includeContents = [];
+    // Input that is used for backward compatibility with pre-sprint 95 symbol store artifacts.
+    // Pre-95 symbol store artifacts were simply file path artifacts, so we need to make sure
+    // not to delete the artifact share if it's a symbol store.
+    let buildCleanup: boolean = tl.getBoolInput('BuildCleanup');
 
-for (var i = 0; i < contents.length; i++) {
-    var pattern = contents[i].trim();
-    tl.debug('include content pattern: ' + pattern);
-    var realPattern = path.join(sourceFolder, pattern);
-    includeContents.push(realPattern);        
-}
+    // trim whitespace and root each pattern
+    patterns = patterns
+        .map((pattern: string) => pattern.trim())
+        .filter((pattern: string) => pattern != '')
+        .map((pattern: string) => path.join(sourceFolder, pattern));
+    tl.debug(`patterns: ${patterns}`);
 
-// enumerate all files
-var files = [];
-var allPaths = tl.find(sourceFolder);
-tl.debug('allPaths: ' + allPaths);
-if (!allPaths || allPaths.length === 0) {
-    tl.debug('source folder not found. nothing to delete.');
-    tl.setResult(tl.TaskResult.Succeeded, tl.loc("NoFiles"));
-    process.exit(0);
-}
+    // short-circuit if no patterns
+    if (!patterns.length) {
+        tl.debug('no patterns specified');
+        return;
+    }
 
-var allFiles: string[] = [];
-var allFolders: string[] = [];
+    // find all files
+    let foundPaths = tl.find(sourceFolder);
+    tl.debug(`raw find result: ${foundPaths}`);
 
-// folders should be deleted last
-for (var i = 0; i < allPaths.length; i++) {
+    // short-circuit if not exists
+    if (!foundPaths.length) {
+        tl.debug('source folder not found. nothing to delete.');
+        tl.setResult(tl.TaskResult.Succeeded, tl.loc("NoFiles"));
+        return;
+    }
+
     // Don't delete symbol store shares if this is a cleanup job for file-path artifacts.
     //
     // This check needs to be made based on the result of tl.find(). Otherwise intermittent network
@@ -50,63 +51,44 @@ for (var i = 0; i < allPaths.length; i++) {
     // a normalization function that could be trusted 100% to match the format produced by tl.find().
     // For example if the input contains "\\\share", it would need to be normalized as "\\share". To
     // avoid worrying about catching every normalization edge case, checking the item name suffices instead.
-    if (buildCleanup && path.basename(allPaths[i]).toLowerCase() == '000admin') {
+    if (buildCleanup &&
+        foundPaths.some((itemPath: string) => path.basename(itemPath).toLowerCase() == '000admin')) {
+
         tl.warning(tl.loc('SkippingSymbolStore', sourceFolder))
-        process.exit(0);
+        return;
     }
 
-    tl.debug("checking for directory: " + allPaths[i]);
-    if (!tl.stats(allPaths[i]).isDirectory()) {
-        allFiles.push(allPaths[i]);
-    }
-    else {
-        allFolders.push(allPaths[i]);
-    }
-}
-allFiles = allFiles.concat(allFolders);
-
-if (includeContents.length > 0 && allFiles.length > 0) {
-    tl.debug("allFiles contains " + allFiles.length + " files");
-    // a map to eliminate duplicates
-    var map = {};
     // minimatch options
-    var matchOptions = { matchBase: true };
+    let matchOptions = { matchBase: true };
     if (os.type().match(/^Win/)) {
         matchOptions["nocase"] = true;
     }
-    // apply include filter
-    for (var i = 0; i < includeContents.length; i++) {
-        var pattern: string = includeContents[i];
-        tl.debug('Include matching: ' + pattern);
-        // let minimatch do the actual filtering
-        var matches = tl.match(allFiles, pattern, matchOptions);
-        tl.debug('Include matched ' + matches.length + ' files');
-        for (var j = 0; j < matches.length; j++) {
-            var matchPath = matches[j];
-            if (!map.hasOwnProperty(matchPath)) {
-                map[matchPath] = true;
-                files.push(matchPath);
-            }
+
+    // apply the match patterns
+    let matches: string[] = tl.match(foundPaths, patterns, matchOptions);
+
+    // sort by length (descending) so files are deleted before folders
+    matches = matches.sort((a: string, b: string) => {
+        if (a.length == b.length) {
+            return 0;
+        }
+
+        return a.length > b.length ? -1 : 1;
+    });
+
+    // try to delete all files/folders, even if one errs
+    let errorHappened: boolean = false;
+    for (let itemPath of matches) {
+        try {
+            tl.rmF(itemPath);
+        }
+        catch (err) {
+            tl.error(err);
+            errorHappened = true;
         }
     }
-}
-else {
-    tl.debug("Either includeContents or allFiles is empty");
-}
 
-// try to delete all files/folders, even if one errs
-var errorHappened: boolean = false;
-for (var i: number = 0; i < files.length; i++){
-    try {
-        tl.debug("trying to delete: " + files[i]);
-        tl.rmRF(files[i]);
+    if (errorHappened) {
+        tl.setResult(tl.TaskResult.Failed, tl.loc("CantDeleteFiles"));
     }
-    catch (err) {
-        tl.error(err);
-        errorHappened = true;
-    }
-}
-
-if (errorHappened) {
-    tl.setResult(tl.TaskResult.Failed, tl.loc("CantDeleteFiles"));
-}
+})();

microsoft/azure-pipelines-tasks

Tasks/DeleteFiles/deletefiles.ts
@@ -5,40 +5,40 @@ import os = require('os');
 import tl = require('vsts-task-lib/task');
 tl.setResourcePath(path.join(__dirname, 'task.json'));
 
-// contents is a multiline input containing glob patterns
-var contents: string[] = tl.getDelimitedInput('Contents', '\n', true);
-var sourceFolder = tl.getPathInput('SourceFolder', true, false);
+(() => {
+    // contents is a multiline input containing glob patterns
+    let patterns: string[] = tl.getDelimitedInput('Contents', '\n', true);
 
-// Input that is used for backward compatibility with pre-sprint 95 symbol store artifacts.
-// Pre-95 symbol store artifacts were simply file path artifacts, so we need to make sure
-// not to delete the artifact share if it's a symbol store.
-var buildCleanup = tl.getBoolInput('BuildCleanup');
+    let sourceFolder: string = tl.getPathInput('SourceFolder', true, false);
 
-// include filter
-var includeContents = [];
+    // Input that is used for backward compatibility with pre-sprint 95 symbol store artifacts.
+    // Pre-95 symbol store artifacts were simply file path artifacts, so we need to make sure
+    // not to delete the artifact share if it's a symbol store.
+    let buildCleanup: boolean = tl.getBoolInput('BuildCleanup');
 
-for (var i = 0; i < contents.length; i++) {
-    var pattern = contents[i].trim();
-    tl.debug('include content pattern: ' + pattern);
-    var realPattern = path.join(sourceFolder, pattern);
-    includeContents.push(realPattern);        
-}
+    // trim whitespace and root each pattern
+    patterns = patterns
+        .map((pattern: string) => pattern.trim())
+        .filter((pattern: string) => pattern != '')
+        .map((pattern: string) => path.join(sourceFolder, pattern));
+    tl.debug(`patterns: ${patterns}`);
 
-// enumerate all files
-var files = [];
-var allPaths = tl.find(sourceFolder);
-tl.debug('allPaths: ' + allPaths);
-if (!allPaths || allPaths.length === 0) {
-    tl.debug('source folder not found. nothing to delete.');
-    tl.setResult(tl.TaskResult.Succeeded, tl.loc("NoFiles"));
-    process.exit(0);
-}
+    // short-circuit if no patterns
+    if (!patterns.length) {
+        tl.debug('no patterns specified');
+        return;
+    }
 
-var allFiles: string[] = [];
-var allFolders: string[] = [];
+    // find all files
+    let foundPaths = tl.find(sourceFolder);
 
-// folders should be deleted last
-for (var i = 0; i < allPaths.length; i++) {
+    // short-circuit if not exists
+    if (!foundPaths.length) {
+        tl.debug('source folder not found. nothing to delete.');
+        tl.setResult(tl.TaskResult.Succeeded, tl.loc("NoFiles"));
+        return;
+    }
+
     // Don't delete symbol store shares if this is a cleanup job for file-path artifacts.
     //
     // This check needs to be made based on the result of tl.find(). Otherwise intermittent network
@@ -50,63 +50,44 @@ for (var i = 0; i < allPaths.length; i++) {
     // a normalization function that could be trusted 100% to match the format produced by tl.find().
     // For example if the input contains "\\\share", it would need to be normalized as "\\share". To
     // avoid worrying about catching every normalization edge case, checking the item name suffices instead.
-    if (buildCleanup && path.basename(allPaths[i]).toLowerCase() == '000admin') {
+    if (buildCleanup &&
+        foundPaths.some((itemPath: string) => path.basename(itemPath).toLowerCase() == '000admin')) {
+
         tl.warning(tl.loc('SkippingSymbolStore', sourceFolder))
-        process.exit(0);
+        return;
     }
 
-    tl.debug("checking for directory: " + allPaths[i]);
-    if (!tl.stats(allPaths[i]).isDirectory()) {
-        allFiles.push(allPaths[i]);
-    }
-    else {
-        allFolders.push(allPaths[i]);
-    }
-}
-allFiles = allFiles.concat(allFolders);
-
-if (includeContents.length > 0 && allFiles.length > 0) {
-    tl.debug("allFiles contains " + allFiles.length + " files");
-    // a map to eliminate duplicates
-    var map = {};
     // minimatch options
-    var matchOptions = { matchBase: true };
+    let matchOptions = { matchBase: true };
     if (os.type().match(/^Win/)) {
         matchOptions["nocase"] = true;
     }
-    // apply include filter
-    for (var i = 0; i < includeContents.length; i++) {
-        var pattern: string = includeContents[i];
-        tl.debug('Include matching: ' + pattern);
-        // let minimatch do the actual filtering
-        var matches = tl.match(allFiles, pattern, matchOptions);
-        tl.debug('Include matched ' + matches.length + ' files');
-        for (var j = 0; j < matches.length; j++) {
-            var matchPath = matches[j];
-            if (!map.hasOwnProperty(matchPath)) {
-                map[matchPath] = true;
-                files.push(matchPath);
-            }
+
+    // apply the match patterns
+    let matches: string[] = tl.match(foundPaths, patterns, matchOptions);
+
+    // sort by length (descending) so files are deleted before folders
+    matches = matches.sort((a: string, b: string) => {
+        if (a.length == b.length) {
+            return 0;
+        }
+
+        return a.length > b.length ? -1 : 1;
+    });
+
+    // try to delete all files/folders, even if one errs
+    let errorHappened: boolean = false;
+    for (let itemPath of matches) {
+        try {
+            tl.rmRF(itemPath);
+        }
+        catch (err) {
+            tl.error(err);
+            errorHappened = true;
         }
     }
-}
-else {
-    tl.debug("Either includeContents or allFiles is empty");
-}
 
-// try to delete all files/folders, even if one errs
-var errorHappened: boolean = false;
-for (var i: number = 0; i < files.length; i++){
-    try {
-        tl.debug("trying to delete: " + files[i]);
-        tl.rmRF(files[i]);
+    if (errorHappened) {
+        tl.setResult(tl.TaskResult.Failed, tl.loc("CantDeleteFiles"));
     }
-    catch (err) {
-        tl.error(err);
-        errorHappened = true;
-    }
-}
-
-if (errorHappened) {
-    tl.setResult(tl.TaskResult.Failed, tl.loc("CantDeleteFiles"));
-}
+})();