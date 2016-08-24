b6f3080e297fb4d72c1a056717d35c70ef2ad387
vinca <vinca@microsoft.com>
2016-08-24 18:59:16 +0530

    CheckFile Function added

--- Tasks/AzureRmWebAppDeployment/MSDeployUtility.ts
+++ Tasks/AzureRmWebAppDeployment/MSDeployUtility.ts
@@ -4,8 +4,22 @@
 
 import Q = require('q');
 import tl = require('vsts-task-lib/task');
+var fs = require('fs');
 var regedit = require('regedit');
 var azureRmUtil = require('./AzureRMUtil.js');
+
+export function fileExists(path) {
+  try  {
+    return fs.statSync(path).isFile();
+  }
+  catch (e) {
+    if (e.code == 'ENOENT') {
+      return false;
+    }
+    tl.debug("Exception fs.statSync (" + path + "): " + e);
+    throw e;
+  }
+}
 
 export function getMSDeployCmdArgs(packageFile: string, webAppNameForMSDeployCmd: string, azureRMWebAppConnectionDetails: Array<String>,
                              removeAdditionalFilesFlag: boolean, excludeFilesFromAppDataFlag: boolean, takeAppOfflineFlag: boolean,
@@ -15,27 +29,33 @@ export function getMSDeployCmdArgs(packageFile: string, webAppNameForMSDeployCmd
     msDeployCmdArgs += ' -source:package=\'' + packageFile + '\'';
     msDeployCmdArgs += ' -dest:auto,ComputerName="https://' + azureRMWebAppConnectionDetails["KuduHostName"] + '/msdeploy.axd?site=' + webAppNameForMSDeployCmd + '",';
     msDeployCmdArgs += 'UserName="' + azureRMWebAppConnectionDetails['UserName'] + '",Password="' + azureRMWebAppConnectionDetails['UserPassword'] + '",AuthType="Basic"';
-    if (virtualApplication) {
+
+    if (setParametersFile) {
+        msDeployCmdArgs += ' -setParamFile=' + setParametersFile;
+    }
+    else if (virtualApplication) {
         msDeployCmdArgs += ' -setParam:name="IIS Web Application Name",value="' + webAppNameForMSDeployCmd + '/' + virtualApplication + '"';
     }
     else {
         msDeployCmdArgs += ' -setParam:name=\'IIS Web Application Name\',value="' + webAppNameForMSDeployCmd + '"';
     }
+
     if (!removeAdditionalFilesFlag) {
         msDeployCmdArgs += ' -enableRule:DoNotDeleteRule';
     }
+
     if (takeAppOfflineFlag) {
         msDeployCmdArgs += ' -enableRule:AppOffline';
     }
+
     if (excludeFilesFromAppDataFlag) {
         msDeployCmdArgs += ' -skip:Directory="\\App_Data"';
     }
-    if (setParametersFile) {
-        msDeployCmdArgs += ' -setParamFile:\'' + setParametersFile + '\'';
-    }
+    
     if (additionalArguments) {
         msDeployCmdArgs += ' ' + additionalArguments;
     }
+
     var userAgent = tl.getVariable("AZURE_HTTP_USER_AGENT");
     if (userAgent) {
         msDeployCmdArgs += ' -userAgent:"' + userAgent + '"';
@@ -48,7 +68,7 @@ export async function executeMSDeployCmd(msDeployCmdArgs: string, azureRMWebAppC
     var msDeployPath = await getMSDeployFullPath();
     var maskedCommand = maskPasswordDetails(msDeployCmdArgs);
     tl.debug(tl.loc('Runningcommand01', msDeployPath, maskedCommand));
-    var statusCode = await tl.exec(msDeployPath, msDeployCmdArgs);
+    var statusCode = await tl.exec(msDeployPath, msDeployCmdArgs,  <any> {failOnStdErr: true});
     if ( statusCode === 0 ) {
         tl.debug(tl.loc('Successfullydeployedwebsite'));
         var deploymentResult = await azureRmUtil.updateDeploymentStatus(azureRMWebAppConnectionDetails, true);
