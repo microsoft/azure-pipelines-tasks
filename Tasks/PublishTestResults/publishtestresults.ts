1eb6a6df942730df66cf3c6fc79f41d97d88c35e
Satish Thareja <sathar@microsoft.com>
2016-08-29 14:40:57 +0530

    initial commit

--- Tasks/PublishTestResults/publishtestresults.ts
+++ Tasks/PublishTestResults/publishtestresults.ts
@@ -1,6 +1,7 @@
 /// <reference path="../../definitions/vsts-task-lib.d.ts" />
 
 import tl = require('vsts-task-lib/task');
+import path = require('path');
 
 var testRunner = tl.getInput('testRunner', true);
 var testResultsFiles = tl.getInput('testResultsFiles', true);
@@ -18,16 +19,27 @@ tl.debug('config: ' + config);
 tl.debug('testRunTitle: ' + testRunTitle);
 tl.debug('publishRunAttachments: ' + publishRunAttachments);
 
+var matchingTestResultsFiles = [];
 //check for pattern in testResultsFiles
 if(testResultsFiles.indexOf('*') >= 0 || testResultsFiles.indexOf('?') >= 0) {
   tl.debug('Pattern found in testResultsFiles parameter');
-  var buildFolder = tl.getVariable('System.DefaultWorkingDirectory');
-  var allFiles = tl.find(buildFolder);
-  var matchingTestResultsFiles = tl.match(allFiles, testResultsFiles, { matchBase: true });
+      if (path.isAbsolute(testResultsFiles)) {
+        var pathParts = testResultsFiles.split(path.sep);
+        var basePath = "";
+        var part= "";
+        for (part in pathParts) {
+            if (isRegexPath(pathParts[part]) == false) {
+                basePath += pathParts[part] + path.sep;
+            }
+        }
+        matchingTestResultsFiles = matchFiles(basePath, testResultsFiles);
+    } else {
+        matchingTestResultsFiles = matchFiles(tl.getVariable('System.DefaultWorkingDirectory'), testResultsFiles);
+    }
 }
 else {
   tl.debug('No pattern found in testResultsFiles parameter');
-  var matchingTestResultsFiles = [testResultsFiles];
+  matchingTestResultsFiles = [testResultsFiles];
 }
 
 if(!matchingTestResultsFiles || matchingTestResultsFiles.length == 0) {
@@ -37,4 +49,14 @@ if(!matchingTestResultsFiles || matchingTestResultsFiles.length == 0) {
 else{
   var tp = new tl.TestPublisher(testRunner);
   tp.publish(matchingTestResultsFiles, mergeResults, platform, config, testRunTitle, publishRunAttachments);
+}
+
+function isRegexPath(filePath: string): boolean {
+    return (filePath.indexOf('*') >= 0 || filePath.indexOf('?') >= 0);
+}
+
+function matchFiles(basePath: string, filePattern: string): string[] {
+    tl.debug("Base path for matching files: " + basePath);
+    var allFiles = tl.find(basePath);
+    return tl.match(allFiles, filePattern, { matchBase: true });
 }
