781579198192de316cbd0738da62226dfc691445
Satish Thareja <sathar@microsoft.com>
2016-09-02 17:04:55 +0530

    merging PR#2408

--- Tasks/DeployVisualStudioTestAgent/DeployTestAgent.ps1
+++ Tasks/DeployVisualStudioTestAgent/DeployTestAgent.ps1
@@ -38,8 +38,6 @@ Write-Verbose "disableScreenSaver = $disableScreenSaver"
 Write-Verbose "updateTestAgent = $updateTestAgent"
 Write-Verbose "isDataCollectionOnly = $isDataCollectionOnly"
 
-Write-Host "##vso[task.logissue type=warning;TaskName=DTA]"
-
 if ([string]::IsNullOrWhiteSpace($agentLocation))
 {
    Write-Verbose "Download of testagent would begin from internet"
