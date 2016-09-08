2ef28e7b4e6f88f9ba3e2e71e6fc0ef0a2e31518
Yang Cao <yacao@microsoft.com>
2016-09-08 15:46:55 -0400

    Fix definition file so we can compile with line function.

--- definitions/vsts-task-lib.d.ts
+++ definitions/vsts-task-lib.d.ts
@@ -74,6 +74,14 @@ declare module 'vsts-task-lib/toolrunner' {
 	     */
 	    argString(val: string): void;
 	    /**
+	     * Append argument command line string
+	     * e.g. '"arg one" two -z' would append args[]=['arg one', 'two', '-z']
+	     *
+	     * @param     val        string cmdline
+	     * @returns   void
+	     */
+	    line(val: string): void;
+	    /**
 	     * Add path argument
 	     * Add path string to argument, path string should not contain double quoted
 	     * This will call arg(val, literal?) with literal equal 'true'
