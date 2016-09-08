4bfecd5aaee6972e94463cc6af1388f9cfb8a8ba
Shu Sam Chen <sschen@microsoft.com>
2016-09-08 18:01:15 +0100

    Added Findbugs integration to the Maven task.

--- Tasks/Maven/maventask.ts
+++ Tasks/Maven/maventask.ts
@@ -12,8 +12,9 @@ import sqMaven = require('./CodeAnalysis/mavensonar');
 
 import {CodeAnalysisOrchestrator} from "./CodeAnalysis/Common/CodeAnalysisOrchestrator";
 import {BuildOutput, BuildEngine} from './CodeAnalysis/Common/BuildOutput';
-import {PmdTool} from './CodeAnalysis/Common/PmdTool';
 import {CheckstyleTool} from './CodeAnalysis/Common/CheckstyleTool';
+import {PmdTool} from './CodeAnalysis/Common/PmdTool';
+import {FindbugsTool} from './CodeAnalysis/Common/FindbugsTool';
 
 // Set up localization resource file
 tl.setResourcePath(path.join( __dirname, 'task.json'));
@@ -31,7 +32,8 @@ var isCodeCoverageOpted = (typeof ccTool != "undefined" && ccTool && ccTool.toLo
 let buildOutput: BuildOutput = new BuildOutput(tl.getVariable('build.sourcesDirectory'), BuildEngine.Maven);
 var codeAnalysisOrchestrator:CodeAnalysisOrchestrator = new CodeAnalysisOrchestrator(
     [new CheckstyleTool(buildOutput, 'checkstyleAnalysisEnabled'),
-        new PmdTool(buildOutput, 'pmdAnalysisEnabled')]);
+        new PmdTool(buildOutput, 'pmdAnalysisEnabled'),
+        new FindbugsTool(buildOutput, 'findbugsAnalysisEnabled')]);
 
 // Determine the version and path of Maven to use
 var mvnExec: string = '';
