5b25c4ab72025c170e56dde2607b378148b09714
bgavrilMS <bgavril@microsoft.com>
2016-09-19 13:49:15 +0100

    Update Gradle to use the same aproach

--- Tasks/Gradle/CodeAnalysis/SonarQube/common.ts
+++ Tasks/Gradle/CodeAnalysis/SonarQube/common.ts
@@ -2,6 +2,7 @@
 
 import Q = require('q');
 import path = require('path');
+import glob = require('glob');
 
 import {ToolRunner} from 'vsts-task-lib/toolrunner';
 
@@ -40,9 +41,15 @@ export function applySonarQubeParameters(toolRunner: ToolRunner): ToolRunner {
  * @param sqBuildFolder
  * @returns {Promise<void>} Promise resolved when all SQ integration actions are complete.
  */
-export function processSonarQubeIntegration(sqBuildFolder:string):Q.Promise<void> {
-    var sqRunSettings:SonarQubeRunSettings = getSonarQubeRunSettings(sqBuildFolder);
-    var sqMetrics:SonarQubeMetrics = getSonarQubeMetrics(sqRunSettings);
+export function processSonarQubeIntegration(sqTaskReportGlob: string): Q.Promise<void> {
+
+    let sqTaskReportPath: string = findReportTaskFile(sqTaskReportGlob);
+    if (sqTaskReportPath === null) {
+        return Q.when();
+    }
+
+    var sqRunSettings: SonarQubeRunSettings = getSonarQubeRunSettings(sqTaskReportPath);
+    var sqMetrics: SonarQubeMetrics = getSonarQubeMetrics(sqRunSettings);
 
     // Wait for all promises to complete before proceeding (even if one or more promises reject).
     return Q.all([
@@ -58,16 +65,15 @@ export function processSonarQubeIntegration(sqBuildFolder:string):Q.Promise<void
  * Returns, as an object, the contents of the report-task.txt file created by SonarQube plugins
  * The returned object contains the following properties:
  *     projectKey, serverUrl, dashboardUrl, ceTaskId, ceTaskUrl
- * @param sonarPluginFolder The folder created by SonarQube integration during build
+ * @param sqTaskReportPath Path to the report-task.txt file
  * @returns {SonarQubeRunSettings} An object with fields corresponding to the properties exposed in report-task.txt, or null if PR build
  */
-function getSonarQubeRunSettings(sonarPluginFolder: string): SonarQubeRunSettings {
+function getSonarQubeRunSettings(sqTaskReportPath: string): SonarQubeRunSettings {
     if (VstsServerUtils.isPrBuild()) {
         return null;
     }
 
-    var reportFilePath:string = path.join(sonarPluginFolder, 'report-task.txt');
-    return SonarQubeRunSettings.createRunSettingsFromFile(reportFilePath);
+    return SonarQubeRunSettings.createRunSettingsFromFile(sqTaskReportPath);
 }
 
 /**
@@ -75,11 +81,30 @@ function getSonarQubeRunSettings(sonarPluginFolder: string): SonarQubeRunSetting
  * @param sqRunSettings An object with fields corresponding to the properties exposed in report-task.txt
  * @returns {SonarQubeMetrics} An SQMetrics instance for this build on the SQ server, or null if PR build
  */
-function getSonarQubeMetrics(sqRunSettings:SonarQubeRunSettings): SonarQubeMetrics {
+function getSonarQubeMetrics(sqRunSettings: SonarQubeRunSettings): SonarQubeMetrics {
     if (VstsServerUtils.isPrBuild()) {
         return null;
     }
 
-    var sqServer:ISonarQubeServer = new SonarQubeServer(SonarQubeEndpoint.getTaskSonarQubeEndpoint());
+    var sqServer: ISonarQubeServer = new SonarQubeServer(SonarQubeEndpoint.getTaskSonarQubeEndpoint());
     return new SonarQubeMetrics(sqServer, sqRunSettings.ceTaskId);
+}
+
+function findReportTaskFile(reportTaskGlob: string): string {
+
+    // the output folder may not be directly in the build root, for example if the entire project is in a top-lvel dir
+    let reportTaskGlobResults: string[] = glob.sync(reportTaskGlob);
+
+    tl.debug(`[SQ] Searching for ${reportTaskGlob} - found ${reportTaskGlobResults.length} file(s)`);
+
+    if (reportTaskGlobResults.length === 0) {
+        tl.warning(tl.loc('sqAnalysis_NoReportTask'));
+        return null;
+    }
+
+    if (reportTaskGlobResults.length > 1) {
+        tl.warning(tl.loc('sqAnalysis_MultipleReportTasks'));
+    }
+
+    return reportTaskGlobResults[0];
 }
