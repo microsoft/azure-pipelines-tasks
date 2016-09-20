0d0ad475ad1d9fb57d702f6fddaa7a5d63f9b066
Bryan MacFarlane <bryanmac@microsoft.com>
2016-09-20 18:57:16 -0400

    cmdline

--- Tasks/CmdLine/cmdlinetask.ts
+++ Tasks/CmdLine/cmdlinetask.ts
@@ -1,27 +1,25 @@
-/// <reference path="../../definitions/vsts-task-lib.d.ts" />
-
 import path = require('path');
 import tl = require('vsts-task-lib/task');
+import trm = require('vsts-task-lib/toolrunner');
 
-tl.setResourcePath(path.join( __dirname, 'task.json'));
+async function run() {
+    try {   
+		tl.setResourcePath(path.join( __dirname, 'task.json'));
 
-var tool = tl.which(tl.getInput('filename', true), true);
+		var tool: string = tl.which(tl.getInput('filename', true), true);
+		var tr: trm.ToolRunner = tl.tool(tool);
 
-var tr = tl.createToolRunner(tool);
+		var cwd = tl.getPathInput('workingFolder', true, false);
+		tl.mkdirP(cwd);
+		tl.cd(cwd);
+		tr.line(tl.getInput('arguments', false));
 
-var cwd = tl.getPathInput('workingFolder', true, false);
-tl.mkdirP(cwd);
-tl.cd(cwd);
-
-tr.argString(tl.getInput('arguments', false));
-
-var failOnStdErr = tl.getBoolInput('failOnStandardError', false);
-
-tr.exec(<any>{failOnStdErr: failOnStdErr})
-.then(function(code) {
-	tl.setResult(tl.TaskResult.Succeeded, tl.loc('CmdLineReturnCode', tool, code));
-})
-.fail(function(err) {
-	tl.debug('taskRunner fail');
-	tl.setResult(tl.TaskResult.Failed, tl.loc('CmdLineFailed', tool, err.message));
-})
+		var failOnStdErr: boolean = tl.getBoolInput('failOnStandardError', false);
+		var code: number = await tr.exec(<trm.IExecOptions>{failOnStdErr: failOnStdErr});
+        tl.setResult(tl.TaskResult.Succeeded, tl.loc('CmdLineReturnCode', tool, code));
+    }
+    catch(err) {
+        tl.error(err.message);
+        tl.setResult(tl.TaskResult.Failed, tl.loc('CmdLineFailed', tool, err.message));
+    }
+}
