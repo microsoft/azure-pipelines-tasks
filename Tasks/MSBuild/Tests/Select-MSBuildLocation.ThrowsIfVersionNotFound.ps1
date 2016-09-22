28e78fc9d6604542587d4290a8eb25a6f5d7b114
Eric Sciple <ersciple@microsoft.com>
2016-09-22 05:15:47 -0400

    Convert L0 for AzurePowerShell VstsAzureHelpers MSBuild PublishSymbols

---
+++ Tasks/MSBuild/Tests/Select-MSBuildLocation.ThrowsIfVersionNotFound.ps1
@@ -0,0 +1,10 @@
+[cmdletbinding()]
+param()
+
+# Arrange.
+. $PSScriptRoot\..\..\..\Tests\lib\Initialize-Test.ps1
+. $PSScriptRoot\..\Select-MSBuildLocation.ps1
+Register-Mock Get-MSBuildPath
+
+# Act/Assert.
+Assert-Throws { Select-MSBuildLocation -Method 'Version' -Location '' -Version '' -Architecture 'Some architecture' }
