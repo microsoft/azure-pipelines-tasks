16e10a271d66560f86686a9f7f684b1d697078da
Eric Sciple <ersciple@microsoft.com>
2016-09-22 05:15:47 -0400

    Convert L0 for AzurePowerShell VstsAzureHelpers MSBuild PublishSymbols

---
+++ Tasks/AzurePowerShell/Tests/DoesNotUnravelOutput.ps1
@@ -0,0 +1,22 @@
+[CmdletBinding()]
+param()
+
+# Arrange.
+. $PSScriptRoot\..\..\..\Tests\lib\Initialize-Test.ps1
+Register-Mock Get-VstsInput { "$PSScriptRoot/DoesNotUnravelOutput_TargetScript.ps1" } -- -Name ScriptPath -Require
+Register-Mock Initialize-Azure
+
+# Act.
+$actual = @( & $PSScriptRoot\..\AzurePowerShell.ps1 )
+$global:ErrorActionPreference = 'Stop' # Reset to stop.
+
+# Assert the correct number of elements is returned.
+Assert-AreEqual 2 $actual.Length
+
+# Assert item 1 and 2 are in an array together.
+Assert-AreEqual 2 @($actual[0]).Length
+Assert-AreEqual 'item 1' $actual[0][0]
+Assert-AreEqual 'item 2' $actual[0][1]
+
+# Assert item 3 is separate.
+Assert-AreEqual 'item 3' $actual[1]
---
+++ Tasks/PublishSymbols/Tests/Invoke-IndexSources.ReturnIfSourceProviderIsNull.ps1
@@ -0,0 +1,26 @@
+[CmdletBinding()]
+param()
+
+# Arrange.
+. $PSScriptRoot\..\..\..\Tests\lib\Initialize-Test.ps1
+. $PSScriptRoot\..\IndexHelpers\IndexFunctions.ps1
+$script:pdbstrExePath = 'SomeDrive:\SomeDir\pdbstr.exe'
+Register-Mock Get-VstsTaskVariable { 'SomeDrive:\AgentHome' } -- -Name Agent.HomeDirectory -Require
+Register-Mock Assert-VstsPath { $script:pdbstrExePath } -- -LiteralPath "SomeDrive:\AgentHome\Externals\Pdbstr\pdbstr.exe" -PathType Leaf -PassThru
+Register-Mock Push-Location
+$script:libraryHandle = -1234
+Register-Mock Add-DbghelpLibrary { $script:libraryHandle }
+$script:sourcesRoot = 'SomeDrive:\SomeSourcesRoot'
+Register-Mock Get-SourceProvider
+Register-Mock Get-SourceFilePaths
+Register-Mock New-SrcSrvIniContent
+Register-Mock Add-SourceServerStream
+Register-Mock Remove-DbghelpLibrary
+
+# Act.
+Invoke-IndexSources -SymbolsFilePaths 'SomeDrive:\SomeDir\SomeLibrary1.pdb' -TreatNotIndexedAsWarning
+
+# Assert.
+Assert-WasCalled Push-Location $env:TEMP
+Assert-WasCalled Get-SourceFilePaths -Times 0
+Assert-WasCalled Remove-DbghelpLibrary -- -HModule $libraryHandle
