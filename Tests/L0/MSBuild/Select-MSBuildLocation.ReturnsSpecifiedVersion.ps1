9bdd82b0561bff127f89716f658e68031b41f5fe
Eric Sciple <ersciple@microsoft.com>
2016-09-28 11:36:28 -0400

    MSBuild Willow

--- Tests/L0/MSBuild/Select-MSBuildLocation.ReturnsSpecifiedVersion.ps1
+++ Tests/L0/MSBuild/Select-MSBuildLocation.ReturnsSpecifiedVersion.ps1
@@ -4,10 +4,10 @@ param()
 # Arrange.
 . $PSScriptRoot\..\..\lib\Initialize-Test.ps1
 . $PSScriptRoot\..\..\..\Tasks\MSBuild\Select-MSBuildLocation.ps1
-Register-Mock Get-MSBuildPath { 'Some resolved location' } -- -Version 'Some version' -Architecture 'Some architecture'
+Register-Mock Get-MSBuildPath { 'Some resolved location' } -- -Version '14.0' -Architecture 'Some architecture' -SearchCom: $false # Should not search COM since version is not 15.0.
 
 # Act.
-$actual = Select-MSBuildLocation -Method 'Version' -Location 'Some input location' -Version 'Some version' -Architecture 'Some architecture'
+$actual = Select-MSBuildLocation -Method 'Version' -Location 'Some input location' -Version '14.0' -Architecture 'Some architecture'
 
 # Assert.
 Assert-AreEqual 'Some resolved location' $actual
