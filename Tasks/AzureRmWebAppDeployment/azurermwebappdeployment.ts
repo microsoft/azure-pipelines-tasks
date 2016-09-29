2b1a3844ddfe01a21030e458e43da04e4693586e
Ajay Kumar Yadav <ajya@microsoft.com>
2016-09-29 17:47:51 +0530

    check of error file for msdeploy

--- Tasks/AzureRmWebAppDeployment/azurermwebappdeployment.ts
+++ Tasks/AzureRmWebAppDeployment/azurermwebappdeployment.ts
@@ -103,14 +103,9 @@ async function DeployUsingMSDeploy(webDeployPkg, webAppName, publishingProfile, 
 
         var msDeployBatchFile = tl.getVariable('System.DefaultWorkingDirectory') + '\\' + 'msDeployCommand.bat';
         var msDeployCommand = '@echo off \n';
-        msDeployCommand += 'echo "">error.txt%\n';
         msDeployCommand += '"' + msDeployPath + '" ' + msDeployCmdArgs + ' 2>error.txt\n';
-        msDeployCommand += 'set /p VAR1=<error.txt\n';
-        msDeployCommand += 'if "%VAR1%"=="Error Code: ERROR_INSUFFICIENT_ACCESS_TO_SITE_FOLDER" (echo "Try to deploy again with app_offline option selected.")\n';
-        msDeployCommand += 'if defined VAR1 ( type error.txt && exit 1 )';
-        var batchCommand =  msDeployCommand;
-
-        tl.writeFile(msDeployBatchFile, batchCommand);
+        msDeployCommand += 'if %errorlevel% neq 0 exit /b %errorlevel%';
+        tl.writeFile(msDeployBatchFile, msDeployCommand);
         tl._writeLine(tl.loc("Runningcommand", msDeployCommand));
         await tl.exec("cmd", ['/C', msDeployBatchFile], <any> {failOnStdErr: true});
         tl._writeLine(tl.loc('WebappsuccessfullypublishedatUrl0', publishingProfile.destinationAppUrl));
@@ -119,6 +114,7 @@ async function DeployUsingMSDeploy(webDeployPkg, webAppName, publishingProfile, 
         tl.error(tl.loc('Failedtodeploywebsite'));
         isDeploymentSuccess = false;
         deploymentError = error;
+        redirectMSDeployErrorToConsole();
     }
 
     try {
@@ -245,6 +241,22 @@ function getSetParamFilePath(setParametersFile: string) : string {
 function canUseWebDeploy(useWebDeploy: boolean) {
     var win = tl.osType().match(/^Win/);
     return (useWebDeploy || win);
+}
+
+/**
+ * 1. Checks if msdeploy during execution redirected any error to 
+ * error stream ( saved in error.txt) , display error to console
+ * 2. Checks if there is file in use error , suggest to try app offline.
+ */
+function redirectMSDeployErrorToConsole() {
+    var msDeployErrorFilePath = tl.getVariable('System.DefaultWorkingDirectory') + '\\error.txt';
+    if(tl.exist(msDeployErrorFilePath)) {
+        var errorFileContent = fs.readFileSync(msDeployErrorFilePath);
+        if(errorFileContent.toString().indexOf("ERROR_INSUFFICIENT_ACCESS_TO_SITE_FOLDER") !== -1){
+            tl.warning(tl.loc("Trytodeploywebappagainwithappofflineoptionselected"));
+        }
+        tl.error(errorFileContent.toString());
+    }
 }
 
 run();
