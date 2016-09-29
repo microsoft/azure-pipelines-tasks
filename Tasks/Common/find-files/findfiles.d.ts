1ca2c0526b12c6d70404fb5673edc8a54eba4d26
Nitin Gurram <nigurr@microsoft.com>
2016-09-29 13:04:19 +0530

    Added Find-Files functionality as common module

---
+++ Tasks/Common/find-files/findfiles.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="../../../../Tasks/Common/find-files/typings/index.d.ts" />
+export declare function findFiles(pattern: string, cwd?: string): string[];
+export declare function findFiles(pattern: string[], cwd?: string): string[];
