a9409e8edbc3e0d0862759e68d08b5957d9f9edc
Nitin Gurram <nigurr@microsoft.com>
2016-09-29 14:21:14 +0530

    Missing typings are added

--- Tasks/Common/find-files/findfiles.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="../../../../Tasks/Common/find-files/typings/index.d.ts" />
-export declare function findFiles(pattern: string, cwd?: string): string[];
-export declare function findFiles(pattern: string[], cwd?: string): string[];
