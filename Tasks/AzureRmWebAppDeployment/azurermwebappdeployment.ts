cf0748e674336edd215b509d18c531de7955d242
Ajay Kumar Yadav <ajya@microsoft.com>
2016-10-17 22:38:53 +0530

    Migration from adm to decompress

--- Tasks/AzureRmWebAppDeployment/azurermwebappdeployment.ts
+++ Tasks/AzureRmWebAppDeployment/azurermwebappdeployment.ts
@@ -187,7 +187,7 @@ async function DeployUsingKuduDeploy(webDeployPkg, azureWebAppDetails, publishin
         var virtualApplicationMappings = azureWebAppDetails.properties.virtualApplications;
         var webAppZipFile = webDeployPkg;
         if(isFolderBasedDeployment) {
-            webAppZipFile = await kuduUtility.archiveFolder(webDeployPkg);
+            webAppZipFile = await zipUtility.archiveFolder(webDeployPkg, tl.getVariable('System.DefaultWorkingDirectory'), 'temp_web_app_package.zip');
             tl.debug(tl.loc("Compressedfolderintozip", webDeployPkg, webAppZipFile));
         } else {
             if (await kuduUtility.containsParamFile(webAppZipFile)) {
