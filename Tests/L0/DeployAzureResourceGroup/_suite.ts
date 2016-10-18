0be993a408eb9761dd24b98d623c74be54658f82
desattir <desattir@microsoft.com>
2016-10-18 19:51:37 +0530

    Added tests for RG Actions. Create: Success Tests Select: Success and Failure Tests

--- Tests/L0/DeployAzureResourceGroup/_suite.ts
+++ Tests/L0/DeployAzureResourceGroup/_suite.ts
@@ -9,27 +9,29 @@ import os = require('os');
 import fs = require('fs');
 import shell = require ('shelljs');
 
+import tl = require("../../lib/vsts-task-lib/task");
+
 function setResponseFile(name: string) {
     process.env['MOCK_RESPONSES'] = path.join(__dirname, name);
 }
 
 describe('Resource Group Operations', function () {
-    this.timeout(20000);
-    var taskSrcPath = path.join (__dirname, '..', '..', '..', 'Tasks', 'DeployAzureResourceGroup');
-    var testSrcPath = path.join (__dirname, '..', '..', '..',  'Tests', 'L0', 'DeployAzureResourceGroup');
+    this.timeout(30000);
+    var taskSrcPath = path.join (__dirname, '..', '..', '..', 'Tasks', 'AzureResourceGroupDeployment');
+    var testSrcPath = path.join (__dirname, '..', '..', '..', "..", 'Tests', 'L0', 'DeployAzureResourceGroup');
 
     before((done) => {
         // init here
-        
         if(shell.test ('-d', taskSrcPath)) {
-             
-            // Move mocked AzureRMUtil, MSDeployUtility and KuduUtility Libraries to task's test location
             shell.mv( '-f', path.join (taskSrcPath,'node_modules',"azure-arm-compute"), path.join (taskSrcPath,'azure-arm-compute-backup'));
             shell.mv( '-f', path.join (taskSrcPath,'node_modules',"azure-arm-network"), path.join (taskSrcPath,'azure-arm-network-backup'));
             shell.mv( '-f', path.join (taskSrcPath,'node_modules',"azure-arm-resource"), path.join (taskSrcPath,'azure-arm-resource-backup'));
-            shell.cp('-r', path.join (testSrcPath, 'mock_node_modules', "*"), path.join (taskSrcPath,'node_modules'));
+            
+            shell.cp( '-rf', path.join (testSrcPath,'mock_node_modules',"azure-arm-compute"), path.join (taskSrcPath,'node_modules'));
+            shell.cp( '-rf', path.join (testSrcPath,'mock_node_modules',"azure-arm-resource"), path.join (taskSrcPath,'node_modules'));
+            shell.cp( '-rf', path.join (testSrcPath,'mock_node_modules',"azure-arm-network"), path.join (taskSrcPath,'node_modules'));
         }
-        
+        setResponseFile("defaults.json")
         done();
     });
     before((done) => {
@@ -40,35 +42,74 @@ describe('Resource Group Operations', function () {
     });
 
     function createOrUpdateRG_Success() {
-        var tr = new trm.TaskRunner('AzureDeploymentResourceGroup');
+        var tr = new trm.TaskRunner('AzureResourceGroupDeployment');
         tr.setInput("action", "Create Or Update Resource Group");
         tr.setInput("ConnectedServiceNameSelector", "ConnectedServiceName");
-        tr.setInput("ConnectedServiceName", "");
+        tr.setInput("ConnectedServiceName", "AzureRM");
         tr.setInput("resourceGroupName", "dummy");
         tr.setInput("location", "West US");
-        tr.setInput("csmFile", "");
-        tr.setInput("csmParametersFile", "");
+        tr.setInput("csmFile", testSrcPath + "\\defaults.json");
+        tr.setInput("csmParametersFile", testSrcPath + "\\defaults.json");
         tr.setInput("overrideParameters", "");
         tr.setInput("deploymentMode","Complete");
         return tr;
     }
+
+    it('Successfully triggered createOrUpdate', (done) => {
+        var tr = createOrUpdateRG_Success();
+        tr.run()
+            .then(() => {
+                assert(tr.succeeded, "Should have succeeded");
+                done();
+          }).fail((err) => {
+              console.log(tr.stdout);
+              console.log(tr.stderr);
+                done(err);
+        });
+    });
+
     
-    function selectRG_Success() {
-        var tr = new trm.TaskRunner('AzureDeploymentResourceGroup');
-        tr.setInput("action", "Create Or Update Resource Group");
+    function selectRG() {
+        var tr = new trm.TaskRunner('AzureResourceGroupDeployment');
+        tr.setInput("action", "Select Resource Group");
         tr.setInput("ConnectedServiceNameSelector", "ConnectedServiceName");
-        tr.setInput("ConnectedServiceName", "");
-        tr.setInput("resourceGroupName", "dummy");
-        tr.setInput("outputVariable", "output");
+        tr.setInput("ConnectedServiceName", "AzureRM");
+        tr.setInput("resourceGroupName", "AzureRM");
         return tr;
     }
 
-    it('Selected Resource Group', (done) => {
-        var tr = selectRG_Success();
+    it('Selected Resource Group successfully', (done) => {
+        var tr = selectRG();
+        tr.setInput("outputVariable", "output.variable.custom");
         tr.run()
             .then(() => {
                 assert(tr.succeeded, "Task should have succeeded");
-                assert(process.env["output"], "Should have written to the output variable")
+                assert(tr.stdout.indexOf("set output.variable.custom")>=0, "Should have written to the output variable ......... ");
+                done();                
+            }).fail((err) => {
+                done(err);
+            });
+    });
+
+    it('Select Resource Group failed on null or empty output Variable', (done) => {
+        var tr = selectRG();
+        tr.run()
+            .then(() => {
+                assert(tr.failed, "Task should have failed");
+                done();                
+            }).fail((err) => {
+                console.log(tr.stdout);
+                done(err);
+        });
+
+        tr.setInput("outputVariable", "");
+        tr.run()
+            .then(() => {
+                assert(tr.failed, "Task should have failed");
+                done();                
+            }).fail((err) => {
+                console.log(tr.stdout);
+                done(err);
             });
     });
 
