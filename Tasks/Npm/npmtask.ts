4e20d2d4edd7fd68cbfdfe9352737b718d386a0d
Murali Medisetty <mmediset@microsoft.com>
2016-10-24 10:58:48 -0700

    Most PR feedback completed

--- Tasks/Npm/npmtask.ts
+++ Tasks/Npm/npmtask.ts
@@ -1,8 +1,9 @@
+import Q = require('q');
 import fs = require('fs');
 import path = require('path');
 import url = require('url');
 import tl = require('vsts-task-lib/task');
-import tr = require('vsts-task-lib/toolrunner');
+import trm = require('vsts-task-lib/toolrunner');
 
 tl.setResourcePath(path.join( __dirname, 'task.json'));
 
@@ -17,7 +18,7 @@ if (command.indexOf(' ') >= 0) {
 	tl.setResult(tl.TaskResult.Failed, tl.loc("InvalidCommand"));
 }
 
-if(tl.getVariable('USE_DEPRECATED_TASK_VERSION') != undefined && tl.getVariable('USE_DEPRECATED_TASK_VERSION').toLowerCase() === 'true') {
+if(shouldUseDeprecatedTask()) {
 	npm.arg(command);
 	npm.argString(tl.getInput('arguments', false));
 
@@ -31,8 +32,9 @@ if(tl.getVariable('USE_DEPRECATED_TASK_VERSION') != undefined && tl.getVariable(
 	})
 } else {
 	// new task version with auth support
+	
 	var npmrcPath: string = path.join(cwd, '.npmrc');
-	var tempUserNpmrPath: string = npmrcPath.concat('.', tl.getVariable('build.buildId'), '.auth');
+	var tempNpmrcPath : string = getTempNpmrcPath();
     var debugLog: boolean = true;
 	if (tl.getVariable('system.debug') != undefined && tl.getVariable('system.debug').toLowerCase() === 'false') {
         debugLog = false;
@@ -41,47 +43,86 @@ if(tl.getVariable('USE_DEPRECATED_TASK_VERSION') != undefined && tl.getVariable(
 	npm.arg(command);
 	npm.argString(tl.getInput('arguments', false));
 
-	getNpmAuthHelperRunner(npmrcPath, tempUserNpmrPath, debugLog).exec()
-	.then(function(code){
+	runNpmWithAuthAsync(
+		getNpmAuthHelperRunner(npmrcPath, tempNpmrcPath, debugLog),
+		getNpmConfigRunner(debugLog),
+		npm,
+		tempNpmrcPath,
+		debugLog)
+	.then(code => {
+		tl.setResult(code, tl.loc('NpmReturnCode', code));
+	})
+	.catch(err => {
+		tl.setResult(tl.TaskResult.Failed, tl.loc('NpmFailed', err.message));
+	});
+}
+
+async function runNpmWithAuthAsync(
+	npmAuthRunner: trm.ToolRunner, 
+	npmConfigRunner: trm.ToolRunner, 
+	npmCommandRunner: trm.ToolRunner,
+	tempUserNpmrPath: string,
+	debugLog: boolean) : Promise<number> {
+		
+	try {
+		
 		if(debugLog) {
 			tl.setEnvVar('npm_config_loglevel', 'verbose');
 		}
 		tl.setEnvVar('npm_config_userconfig', tempUserNpmrPath);
-		getNpmConfigRunner(debugLog).exec()
-		.fail(function(err){
-			tl.warning(tl.loc('NpmConfigFailed', err.Message));
-		})
-		.finally(function(){
-			npm.exec()
-			.then(function(code) {
-				cleanUpTempNpmrcPath(tempUserNpmrPath);
-				tl.setResult(code, tl.loc('NpmReturnCode', code));
-			})
-			.fail(function(err) {
-				cleanUpTempNpmrcPath(tempUserNpmrPath);
-				tl.setResult(tl.TaskResult.Failed, tl.loc('NpmFailed', err.message));
-			})	
-		})
-	})
-	.fail(function(err) {
+
+		var code : number = await runNpmAuthHelperAsync(npmAuthRunner);
+		await tryRunNpmConfigAsync(npmConfigRunner);
+		code = await runNpmCommandAsync(npmCommandRunner);
+		return Q(code);
+	} finally {
 		cleanUpTempNpmrcPath(tempUserNpmrPath);
-		tl.setResult(tl.TaskResult.Failed, tl.loc('NpmAuthFailed', err.message));
-	})
+	}
 }
 
-function getNpmAuthHelperRunner(npmrcPath: string, tempUserNpmrcPath: string, includeDebugLogs: boolean): tr.ToolRunner {
+async function runNpmAuthHelperAsync(npmAuthRunner: trm.ToolRunner) : Promise<number> {
+	try{
+		var code : number = await npmAuthRunner.exec();
+		tl.debug('Authentication succeeded with code: ' + code);
+		return Q(code);
+	} catch (err) {
+		tl.debug(tl.loc('NpmAuthFailed', err.message));
+		throw err;
+	}
+}
+
+async function tryRunNpmConfigAsync(npmConfigRunner: trm.ToolRunner) {
+	try {
+		var code = await npmConfigRunner.exec();
+	} catch (err) {
+		// do not throw on this failure.
+		tl.warning(tl.loc('NpmConfigFailed', err.Message));
+	}
+}
+
+async function runNpmCommandAsync(npmCommandRunner: trm.ToolRunner) : Promise<number> {
+	try{
+		var code: number = await npmCommandRunner.exec();
+		tl.debug('Npm command succeeded with code: ' + code);
+		return Q(code);
+	} catch (err) {
+		tl.debug(tl.loc('NpmFailed', err.message));
+		throw err;
+	}
+}
+
+function shouldUseDeprecatedTask() : boolean {
+	return tl.getVariable('USE_DEPRECATED_TASK_VERSION') != undefined && tl.getVariable('USE_DEPRECATED_TASK_VERSION').toLowerCase() === 'true';
+}
+
+function getNpmAuthHelperRunner(npmrcPath: string, tempUserNpmrcPath: string, includeDebugLogs: boolean): trm.ToolRunner {
 	var npmAuthHelper = tl.createToolRunner(path.join(__dirname, 'Npm/vsts-npm-auth/bin/vsts-npm-auth.exe'));
-	npmAuthHelper.arg('-N')
-	npmAuthHelper.arg('-V')
-	npmAuthHelper.arg(includeDebugLogs ? 'Detailed' : 'Normal')
-	npmAuthHelper.argString('-C');
-	npmAuthHelper.arg(npmrcPath);
-	npmAuthHelper.arg('-T');
-	npmAuthHelper.arg(tempUserNpmrPath);
+	var verbosityString = includeDebugLogs ? 'Detailed' : 'Normal';
+	npmAuthHelper.argString(`-NonInteractive -Verbosity ${verbosityString} -Config "${npmrcPath}" -TargetConfig "${tempUserNpmrcPath}"`);
 	return npmAuthHelper;
 }
 
-function getNpmConfigRunner(includeDebugLogs: boolean): tr.ToolRunner {
+function getNpmConfigRunner(includeDebugLogs: boolean): trm.ToolRunner {
 	var npmConfig = tl.createToolRunner(tl.which('npm', true));
 	npmConfig.argString('config list');
 	if(includeDebugLogs) {
@@ -90,11 +131,23 @@ function getNpmConfigRunner(includeDebugLogs: boolean): tr.ToolRunner {
 	return npmConfig;
 }
 
+function getTempNpmrcPath() : string {
+	var tempNpmrcDir
+        = tl.getVariable('Agent.BuildDirectory')
+        || tl.getVariable('Agent.ReleaseDirectory')
+        || process.cwd();
+    tempNpmrcDir = path.join(tempNpmrcDir, 'npm');
+	tl.mkdirP(tempNpmrcDir);
+	var tempUserNpmrcPath: string = path.join(tempNpmrcDir, 'auth.' + tl.getVariable('build.buildId') + '.npmrc');
+	return tempUserNpmrcPath;
+}
+
 function cleanUpTempNpmrcPath(tempUserNpmrcPath: string) {
 	tl.debug('cleaning up...')
 	tl.setEnvVar('npm_config_userconfig', '');
+	tl.setEnvVar('npm_config_loglevel', '');
 	if(tl.exist(tempUserNpmrcPath)) {
 		tl.debug('deleting temporary npmrc...');
-		tl.rmRF(tempUserNpmrcPath, false);
+		tl.rmRF(tempUserNpmrcPath, /* continueOnError */ false);
 	}
 }
