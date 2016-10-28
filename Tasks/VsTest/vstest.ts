1b23550b7f6cbd6c9d70190efff562bf74cb87e7
Tanvi Soni <tasoni@microsoft.com>
2016-10-28 10:01:37 +0530

    bug fix

--- Tasks/VsTest/vstest.ts
+++ Tasks/VsTest/vstest.ts
@@ -45,7 +45,7 @@ try {
     var vstestDiagFile = path.join(os.tmpdir(), uuid.v1() + ".txt");
     var useNewCollectorFlag = tl.getVariable('tia.useNewCollector');
     var isPrFlow = tl.getVariable('tia.isPrFlow');
-    var vsTestVersionForTIA: number[] = [];
+    var vsTestVersionForTIA: number[] = null;
 
     var useNewCollector = false;
     if (useNewCollectorFlag && useNewCollectorFlag.toUpperCase() == "TRUE") {
@@ -164,7 +164,7 @@ function getVstestArguments(settingsFile: string, tiaEnabled: boolean): string[]
                 testAssemblyPath = expandedPath;
             }
         }
-        argsArray.push("\"" + testAssemblyPath + "\"");
+       argsArray.push(testAssemblyPath);       
     });
     if (testFiltercriteria) {
         if (!tiaEnabled) {
@@ -197,24 +197,32 @@ function getVstestArguments(settingsFile: string, tiaEnabled: boolean): string[]
         argsArray.push("/TestAdapterPath:\"" + systemDefaultWorkingDirectory + "\"");
     }
 
-    if (vsTestVersionForTIA !== null && (vsTestVersionForTIA[0] > 15 || (vsTestVersionForTIA[0] == 15 && (vsTestVersionForTIA[1] > 0 || vsTestVersionForTIA[2] > 25428)))) {
-        argsArray.push("/diag:" + vstestDiagFile);
-    }
-    else {
-        tl.warning(tl.loc("VstestDiagNotSupported"));
+    let sysDebug = tl.getVariable("System.Debug");
+    if (sysDebug !== undefined && sysDebug.toLowerCase() === "true") {
+        if (vsTestVersionForTIA !== null && (vsTestVersionForTIA[0] > 15 || (vsTestVersionForTIA[0] == 15 && (vsTestVersionForTIA[1] > 0 || vsTestVersionForTIA[2] > 25428)))) {
+            argsArray.push("/diag:" + vstestDiagFile);
+        }
+        else {
+            tl.warning(tl.loc("VstestDiagNotSupported"));
+        }
     }
     
     return argsArray;
 }
 
 function addVstestArgs(argsArray: string[], vstest: any) {
-    argsArray.forEach(function (arr) {
+    argsArray.forEach(function (arr) {            
         vstest.arg(arr);
     });
 }
 
 function updateResponseFile(argsArray: string[], responseFile: string): Q.Promise<string> {
     var defer = Q.defer<string>();
+    argsArray.forEach(function (arr, i) {
+        if (!arr.startsWith('/')) {
+            argsArray[i] = "\"" + arr + "\"";
+        }
+    }); 
     fs.appendFile(responseFile, os.EOL + argsArray.join(os.EOL), function (err) {
         if (err) {
             defer.reject(err);
@@ -703,11 +711,15 @@ function invokeVSTest(testResultsDirectory: string): Q.Promise<number> {
                         if (disableTIA !== undefined && disableTIA.toLowerCase() === "true") {
                             tiaEnabled = false;
                         }
-                        vsTestVersionForTIA = getVsTestVersion();
-                        if (vsTestVersionForTIA == null || (vsTestVersionForTIA[0] < 15 || (vsTestVersionForTIA[0] == 15 && vsTestVersionForTIA[1] == 0 && vsTestVersionForTIA[2] < 25807))) {
-                            tl.warning(tl.loc("VstestTIANotSupported"));
-                            tiaEnabled = false;
-                        }                        
+                        let sysDebug = tl.getVariable("System.Debug");
+                        if ((sysDebug !== undefined && sysDebug.toLowerCase() === "true") || tiaEnabled) {
+                            vsTestVersionForTIA = getVsTestVersion();
+
+                            if (tiaEnabled && (vsTestVersionForTIA == null || (vsTestVersionForTIA[0] < 15 || (vsTestVersionForTIA[0] == 15 && vsTestVersionForTIA[1] == 0 && vsTestVersionForTIA[2] < 25727)))) {
+                                tl.warning(tl.loc("VstestTIANotSupported"));
+                                tiaEnabled = false;
+                            }
+                        }                                            
                     } catch (e) {
                         tl.error(e.message);
                         defer.resolve(1);
@@ -963,15 +975,31 @@ function pushImpactLevelAndRootPathIfNotFound(dataCollectorArray): void {
                 }
             }
             else if (!dataCollectorArray[i].Configuration.TestImpact && dataCollectorArray[i].Configuration.RootPath) {
-                dataCollectorArray[i].Configuration = { ImpactLevel: getTIALevel() };
+                if (getTIALevel() == 'file') {
+                    dataCollectorArray[i].Configuration = { ImpactLevel: getTIALevel(), LogFilePath: 'true' };
+                }
+                else {
+                    dataCollectorArray[i].Configuration = { ImpactLevel: getTIALevel() };
+                }
             }
             else if (dataCollectorArray[i].Configuration && !dataCollectorArray[i].Configuration.TestImpact && !dataCollectorArray[i].Configuration.RootPath) {
                 if (context && context == "CD")
                 {
-                    dataCollectorArray[i].Configuration = { ImpactLevel: getTIALevel(), RootPath: "" };
+                    if (getTIALevel() == 'file') {
+                        dataCollectorArray[i].Configuration = { ImpactLevel: getTIALevel(), LogFilePath: 'true', RootPath: "" };
+                    }
+                    else {
+                        dataCollectorArray[i].Configuration = { ImpactLevel: getTIALevel(), RootPath: "" };
+                    }                    
                 }
-                else{
-                    dataCollectorArray[i].Configuration = { ImpactLevel: getTIALevel(), RootPath: sourcesDir };
+                else
+                {
+                    if (getTIALevel() == 'file') {
+                        dataCollectorArray[i].Configuration = { ImpactLevel: getTIALevel(), LogFilePath: 'true', RootPath: sourcesDir };
+                    }
+                    else {
+                        dataCollectorArray[i].Configuration = { ImpactLevel: getTIALevel(), RootPath: sourcesDir };
+                    }
                 }
             }
 
@@ -1204,7 +1232,14 @@ function createRunSettingsForTestImpact(vsVersion: number, settingsFile: string,
     runSettingsForTIA = runSettingsForTIA +
         ' >' +
         '<Configuration>' +
-        '<ImpactLevel>' + getTIALevel() + '</ImpactLevel>' +
+        '<ImpactLevel>' + getTIALevel() + '</ImpactLevel>';
+
+    if (getTIALevel() == 'file') {
+        runSettingsForTIA = runSettingsForTIA +
+            '<LogFilePath>' + 'true' + '</LogFilePath>';
+    }
+
+    runSettingsForTIA = runSettingsForTIA +
         '<RootPath>' + (context == "CD" ? "" : sourcesDir) + '</RootPath>' +
         '</Configuration>' +
         '</DataCollector>' +
