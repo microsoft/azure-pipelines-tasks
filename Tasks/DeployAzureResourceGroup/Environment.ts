951b9de4501dae0200d0281ea5498ccdf8186f0a
desattir <desattir@microsoft.com>
2016-11-14 12:24:59 +0530

    Select RG Pointing to correct ports

--- Tasks/DeployAzureResourceGroup/Environment.ts
+++ Tasks/DeployAzureResourceGroup/Environment.ts
@@ -66,6 +66,7 @@ class Environment {
         this.Url = "null";
         this.Revision = 1;
         this.Project = new Project(projectName, projectName);
+        var user = new User(userId);
         this.ModifiedBy = user;
         this.Resources = resources;
         this.Properties = { 
@@ -75,7 +76,6 @@ class Environment {
         this.Name = environmentName;
         this.IsReserved = false;
         this.CreatedBy = user;
-        var user = new User(userId);
         var timestamp = new Date();
         this.CreatedDate = util.format("%s-%s-%sT%s:%s:%s.%sZ", timestamp.getFullYear(), timestamp.getMonth(), timestamp.getDate(), 
                                         timestamp.getHours(), timestamp.getMinutes(), timestamp.getSeconds(), timestamp.getMilliseconds());
@@ -90,6 +90,10 @@ export class RegisterEnvironment {
     private publicAddressToNetworkIdMap;
     private publicAddressToFqdnMap;
     private networkIdToTagsMap;
+    private inboundNatRuleMap;
+    private networkIds;
+    private loadBalancerToPortMap;
+    private loadBalancerToPublicIPAddressMap;
     private outputVariable: string;
 
     constructor(credentials, subscriptionId, resourceGroupName, outputVariable) {
@@ -104,14 +108,43 @@ export class RegisterEnvironment {
         this.publicAddressToNetworkIdMap = null;
         this.networkIdToTagsMap = null;
         this.publicAddressToFqdnMap = null;
+        this.loadBalancerToPortMap = null;
+        this.networkIds = null;
+        this.loadBalancerToPublicIPAddressMap = null;
+        this.inboundNatRuleMap = null;
         this.getVMDetails();
         this.getNetworkInterfaceDetails();
         this.getPublicIPAddresses();
+        this.getLoadBalancers();
 
+    }
+    private getLoadBalancers() {
+        var armClient = new networkManagementClient(this.credentials, this.subscriptionId);
+        armClient.loadBalancers.list(this.resourceGroupName, (error, loadbalancers, request, response) => {
+            if (error){
+                console.log("Error while getting list of Load Balancers", error);
+                throw new Error("FailedToFetchLoadBalancers");
+            }
+            var inboundNatRuleMap = {};
+            for (var i=0; i < loadbalancers.length; i++) {
+                var lb = loadbalancers[i];
+                var publicAddress = lb["frontendIPConfigurations"][0]["publicIPAddress"]["id"];
+                for (var j=0; j < lb["inboundNatRules"].length; j++) {
+                    var natRule = lb["inboundNatRules"][j];
+                    inboundNatRuleMap[natRule["id"]] = {
+                        frontendPort : natRule["frontendPort"],
+                        backendPort : natRule["backendPort"],
+                        publicAddress : publicAddress
+                    }
+                }
+            }
+            this.inboundNatRuleMap = inboundNatRuleMap;
+            this.InstantiateEnvironment();
+        });
     }
 
     private InstantiateEnvironment() {
-        if (this.publicAddressToNetworkIdMap == null || this.publicAddressToFqdnMap == null || this.networkIdToTagsMap == null) {
+        if (this.publicAddressToNetworkIdMap == null || this.publicAddressToFqdnMap == null || this.networkIdToTagsMap == null || this.inboundNatRuleMap == null) {
             return;
         }
         var resources = this.getResources();
@@ -120,21 +153,47 @@ export class RegisterEnvironment {
         tl.setVariable(this.outputVariable, JSON.stringify(environment));
     }
 
-    private getTags(addressId: string){
-        var networkId =  this.publicAddressToNetworkIdMap[addressId];
+    private getTags(networkId: string) {
         return this.networkIdToTagsMap[networkId];
     }
 
+    private getPort(networkId: string) {
+        var interfaceDetails = this.publicAddressToNetworkIdMap[networkId];
+        var port = "5986";
+        if (interfaceDetails.inboundNatRule) {
+            var natRules = interfaceDetails.inboundNatRule;
+            for (var i=0; i < natRules.length; i++) {
+                var natRule = natRules[i];
+                if (this.inboundNatRuleMap[natRule.id].backendPort == 5986) {
+                    port = this.inboundNatRuleMap[natRule.id].frontendPort
+                }
+            }
+        }
+        return port;      
+    }
+
+    private getFQDN(networkId) {
+        var interfaceDetails = this.publicAddressToNetworkIdMap[networkId];
+        if (interfaceDetails.publicAddress) {
+            return this.publicAddressToFqdnMap[interfaceDetails.publicAddress];
+        } else {
+            var natRule = interfaceDetails.inboundNatRule[0].id;
+            var publicAddress = this.inboundNatRuleMap[natRule].publicAddress;
+            return this.publicAddressToFqdnMap[publicAddress];
+        }
+    }
+
+    
     private getResources() {
         var resources = new Array<Resource>();
         var id = 1;
-        for (var addressId in this.publicAddressToFqdnMap) {
-            var fqdn = this.publicAddressToFqdnMap[addressId];
+        for (var i=0; i < this.networkIds.length; i++) {
+            var networkId = this.networkIds[i];
+            var fqdn = this.getFQDN(networkId);
             var resource = new Resource(id++ , fqdn);
             resource.addOrUpdateProperty("Microsoft-Vslabs-MG-Resource-FQDN", new PropertyValue(fqdn));
-            // Default
-            resource.addOrUpdateProperty("WinRM_Https", new PropertyValue("5986")); 
-            var tags = this.getTags(addressId);
+            resource.addOrUpdateProperty("WinRM_Https", new PropertyValue(this.getPort(networkId)));
+            var tags = this.getTags(networkId);
             if (tags) {
                 for (var tag in tags) {
                     resource.addOrUpdateProperty(tag, new PropertyValue(tags[tag]));
@@ -152,10 +211,12 @@ export class RegisterEnvironment {
                 console.log("Error while getting list of Virtual Machines", error);
                 throw new Error("FailedToFetchVMs");
             }
+            this.networkIds = [];
             var tags = {};
             for (var i = 0; i < virtualMachines.length; i++) {
                 var vm = virtualMachines[i];
                 var networkId = vm["networkProfile"]["networkInterfaces"][0]["id"];
+                this.networkIds.push(networkId);
                 if (vm["tags"] != undefined)
                     tags[networkId] = vm["tags"];
             }
@@ -175,7 +236,12 @@ export class RegisterEnvironment {
             for (var i = 0; i < networkInterfaces.length; i++) {
                 var networkInterface = networkInterfaces[i];
                 var networkId = networkInterface["id"];
-                interfaces[networkInterface["ipConfigurations"][0]["publicIPAddress"]["id"]] = networkId;
+                var ipConfig = networkInterface["ipConfigurations"][0];
+                if (ipConfig["publicIPAddress"]){
+                    interfaces[networkId] = { publicAddress: ipConfig["publicIPAddress"]["id"] };
+                } else if (ipConfig["loadBalancerInboundNatRules"]) {
+                    interfaces[networkId] ={ inboundNatRule: ipConfig["loadBalancerInboundNatRules"] };
+                }
             }
             this.publicAddressToNetworkIdMap = interfaces;
             this.InstantiateEnvironment();  
