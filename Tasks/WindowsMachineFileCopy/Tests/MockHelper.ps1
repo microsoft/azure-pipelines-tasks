22c2df92c608a6fbae53c729d66660dfdf036af2
vinca <vinca@microsoft.com>
2016-11-17 18:35:39 +0530

    removed comments and incrmented task version

--- Tasks/WindowsMachineFileCopy/Tests/MockHelper.ps1
+++ Tasks/WindowsMachineFileCopy/Tests/MockHelper.ps1
@@ -11,9 +11,3 @@ Register-Mock Get-VssConnection { return $null }
 
 Register-Mock Invoke-Command { }
 Register-Mock Get-ResourceCredentials { }
-
-
-#Register-Mock Get-EnvironmentProperty { return $validMachineName1 } -ParametersEvaluator {$Key -eq $resourceFQDNKeyName -and $ResourceId -eq $validMachineId1}
-#Register-Mock Get-EnvironmentProperty { return $validMachineName2 } -ParametersEvaluator {$Key -eq $resourceFQDNKeyName -and $ResourceId -eq $validMachineId2}
-#Register-Mock Get-EnvironmentProperty { return $validResources } -ParametersEvaluator {$EnvironmentName -eq $invalidEnvironmentNameForFailCopy}
-#Register-Mock Get-EnvironmentProperty { return $validResources } -ParametersEvaluator {$EnvironmentName -eq $validEnvironmentName}
