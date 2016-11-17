7d2f8e7277e7ab178d95d3e8031bd50fb7c0075e
Vinit Suri <visuri@microsoft.com>
2016-11-17 16:22:22 +0530

    changes for failure criteria

--- Tasks/QuickPerfTest/VssConnectionHelper.ps1
+++ Tasks/QuickPerfTest/VssConnectionHelper.ps1
@@ -1,13 +1,12 @@
 function Get-CltEndpoint($connectedServiceUrl, $headers)
 {
-    # Load all dependent files for execution
-    . $PSScriptRoot/CltTasksUtility.ps1
-    $vsoUrl = $connectedServiceUrl
-    Write-Host "Fetching the Clt endpoint for $vsoUrl"
-    $spsLocation = Get-SpsLocation $vsoUrl $headers
-    $cltLocation = Get-CltLocation $spsLocation $headers
-
-    return $cltLocation
+	# Load all dependent files for execution
+	. $PSScriptRoot/CltTasksUtility.ps1
+	$vsoUrl = $connectedServiceUrl
+	Write-Host "Fetching the Clt endpoint for $vsoUrl"
+	$spsLocation = Get-SpsLocation $vsoUrl $headers
+	$cltLocation = Get-CltLocation $spsLocation $headers
+	return $cltLocation
 
 }
 
@@ -15,27 +14,26 @@ function Get-SpsLocation($vsoUrl, $headers)
 {
 	Write-Host "Fetching the SPS endpoint for $vsoUrl"
 	$spsUniqueIdentifier = "951917AC-A960-4999-8464-E3F0AA25B381"
-    $spsLocation = Get-ServiceLocation $vsoUrl $headers $spsUniqueIdentifier
-    return $spsLocation
+	$spsLocation = Get-ServiceLocation $vsoUrl $headers $spsUniqueIdentifier
+	return $spsLocation
 }
 
 function Get-CltLocation($spsUrl, $headers)
 {
 	Write-Host "Fetching the CLT endpoint for $vsoUrl"
-    $cltUniqueIdentifier = "6C404D78-EF65-4E65-8B6A-DF19D6361EAE"
-    return Get-ServiceLocation $spsUrl $headers $cltUniqueIdentifier
+	$cltUniqueIdentifier = "6C404D78-EF65-4E65-8B6A-DF19D6361EAE"
+	return Get-ServiceLocation $spsUrl $headers $cltUniqueIdentifier
 }
 
 function Get-ServiceLocation($baseUrl, $headers, $serviceUniqueIdentifier)
 {
 	# Load all dependent files for execution
-  . $PSScriptRoot/CltTasksUtility.ps1
-    $locationCallUri = [string]::Format("{0}/_apis/servicedefinitions/LocationService2/{1}", $baseUrl, $serviceUniqueIdentifier)
-    $locationCallJsonResponse = InvokeRestMethod -Uri $locationCallUri -contentType "application/json" -headers $headers -Method Get
-    if($locationCallJsonResponse)
-    {
-     return $locationCallJsonResponse.locationMappings.location|Select -First 1
-    }
-
+	. $PSScriptRoot/CltTasksUtility.ps1
+	$locationCallUri = [string]::Format("{0}/_apis/servicedefinitions/LocationService2/{1}", $baseUrl, $serviceUniqueIdentifier)
+	$locationCallJsonResponse = InvokeRestMethod -Uri $locationCallUri -contentType "application/json" -headers $headers -Method Get
+	if($locationCallJsonResponse)
+	{
+		return $locationCallJsonResponse.locationMappings.location|Select -First 1
+	}
 	return $null
 }
