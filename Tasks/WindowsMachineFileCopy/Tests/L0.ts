e1ae70d294fa7b58b3c6337155bade1fe5352754
vinca <vinca@microsoft.com>
2016-11-25 10:58:17 +0530

    L0 for ResourceProperty

--- Tasks/WindowsMachineFileCopy/Tests/L0.ts
+++ Tasks/WindowsMachineFileCopy/Tests/L0.ts
@@ -32,35 +32,30 @@ describe('WindowsMachineFileCopy Suite', function () {
         it('Throw error if Source Path is invalid or empty', (done) => {
             psr.run(path.join(__dirname, 'L0ValidateSourcePath.ps1'), done);
         });
-
         it('Throw error if Destination Path is invalid or empty', (done) => {
             psr.run(path.join(__dirname, 'L0ValidateDestinationPath.ps1'), done);
         });
-
         it('Should copy on local machine when no machine name is given', (done) => {
             psr.run(path.join(__dirname, 'L0ShouldCopyOnLocalMachine.ps1'), done);
         });
-
+        it('Validate Get-ResourcesProperties Utility', (done) => {
+            psr.run(path.join(__dirname, 'L0GetResourcesProperties.ps1'), done);
+        });
+        it('Validate Get-ResourceConnectionDetails Utility', (done) => {
+            psr.run(path.join(__dirname, 'L0GetResourceConnectionDetails.ps1'), done);
+        });
         it('Throw Error for invalid machine names', (done) => {
             psr.run(path.join(__dirname, 'L0InvalidEnvironmentResource.ps1'), done);
         });
-
-        it('Throw Error for Environment property not present', (done) => {
-            psr.run(path.join(__dirname, 'L0InvalidGetEnvironmentProperty.ps1'), done);
-        });
-
         it('Throw Error if Copy Files To Target Machine fails', (done) => {
             psr.run(path.join(__dirname, 'L0SequentialCopyFail.ps1'), done);
         });
-
         it('Performs Sequential copy on all machines and works correctly for valid input', (done) => {
             psr.run(path.join(__dirname, 'L0ValidInputSequentialCopy.ps1'), done);
         });
-
         it('Performs Parallel copy on all machines and works correctly for valid input', (done) => {
             psr.run(path.join(__dirname, 'L0ValidInputParallelCopy.ps1'), done);
         });
-
         it('Throw error if job fails for resource in Parallel Copy', (done) => {
            psr.run(path.join(__dirname, 'L0ParallelCopyFail.ps1'), done);
         });
