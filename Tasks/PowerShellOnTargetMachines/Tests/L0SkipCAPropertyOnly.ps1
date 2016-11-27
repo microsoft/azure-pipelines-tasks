e5e0ea2562a8d8820970e835af590d2510146be8
vinca <vinca@microsoft.com>
2016-11-27 23:03:58 +0530

    Utility Test 1

--- Tasks/PowerShellOnTargetMachines/Tests/L0SkipCAPropertyOnly.ps1
+++ Tasks/PowerShellOnTargetMachines/Tests/L0SkipCAPropertyOnly.ps1
@@ -4,23 +4,13 @@ param()
 . $PSScriptRoot\..\..\..\Tests\lib\Initialize-Test.ps1
 . $PSScriptRoot\MockVariable.ps1
 . $PSScriptRoot\MockModule.ps1
+. $PSScriptRoot\..\Utility.ps1
+$doNotSkipCACheckOption = ''
 
-$remotePowershellRunnerPath = "$PSScriptRoot\..\PowerShellOnTargetMachines.ps1"
+Register-Mock Register-Environment { return GetEnvironmentWithAzureProvider $environmentWithSkipCASet } -ParametersEvaluator { $EnvironmentName -eq $environmentWithSkipCASet }
+$environment = Register-Environment -EnvironmentName $environmentWithSkipCASet
+Register-Mock Get-EnvironmentProperty { $environmentWinRMHttpsPort} -ParametersEvaluator { $Environment.Name -eq $environmentWithSkipCASet }
 
-Register-Mock Invoke-PsOnRemote { }
-Register-Mock Invoke-Command {
-    $deploymentResponse = @{}
-    $deploymentResponse.Status = "Passed"
-    return $deploymentResponse
-}
+Get-SkipCACheckOption -environmentName $environmentWithSkipCASet
 
-Register-Mock Register-Environment { return GetEnvironmentWithAzureProvider $EnvironmentName } -ParametersEvaluator { $EnvironmentName -eq $environmentWithSkipCASet }
-Register-Mock Get-EnvironmentResources { $validResources } -ParametersEvaluator { $EnvironmentName -eq $environmentWithSkipCASet }
-
-Register-Mock Get-EnvironmentProperty { $environmentWinRMHttpsPort} -ParametersEvaluator { $Environment.Name -eq $environmentWithSkipCASet -and $Key -eq $resourceWinRMHttpsPortKeyName }
-Register-Mock Get-ParsedSessionVariables { }
-
-& "$remotePowershellRunnerPath" -environmentName $environmentWithSkipCASet -machineNames $validMachineName1 -scriptPath $validScriptPath -runPowershellInParallel $false
-
-Assert-WasCalled Get-EnvironmentProperty -Times 1 -ParametersEvaluator { $Environment.Name -eq $environmentWithSkipCASet -and $Key -eq $skipCACheckKeyName }
-Assert-WasCalled Get-EnvironmentProperty -Times 0 -ParametersEvaluator { $Key -eq $resourceWinRMHttpPortKeyName }
+Assert-WasCalled Get-EnvironmentProperty -Times 1 -ParametersEvaluator { $Environment.Name -eq $environmentWithSkipCASet }
