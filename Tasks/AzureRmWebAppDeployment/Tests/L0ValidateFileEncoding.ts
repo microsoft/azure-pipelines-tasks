28306b9e307a1a4b7caa58ed3aac3be812a01ef4
vinca <vinca@microsoft.com>
2016-11-28 14:18:06 +0530

    Review Comments Fix

--- Tasks/AzureRmWebAppDeployment/Tests/L0ValidateFileEncoding.ts
+++ Tasks/AzureRmWebAppDeployment/Tests/L0ValidateFileEncoding.ts
@@ -1,63 +1,63 @@
 var fileEncoding = require('../node_modules/webdeployment-common/fileencoding.js');
 
-var fileEncodeType = fileEncoding.detectFileEncoding('utf8.txt', new Buffer([239, 187, 191, 0]));
+var fileEncodeType = fileEncoding.detectFileEncoding('utf-8.txt', new Buffer([239, 187, 191, 0]));
 if(fileEncodeType[0] === 'utf-8' && fileEncodeType[1]) {
     console.log('UTF-8 with BOM validated');
 }
 
 try {
-    var fileEncodeType = fileEncoding.detectFileEncoding('utf32le.txt', new Buffer([255, 254, 0, 0]));
+    var fileEncodeType = fileEncoding.detectFileEncoding('utf-32le.txt', new Buffer([255, 254, 0, 0]));
 }
 catch(exception) {
     console.log('UTF-32LE with BOM validated');
 }
 
 try {
-    var fileEncodeType = fileEncoding.detectFileEncoding('utf16betxt', new Buffer([254, 255, 0 ,0]));
+    var fileEncodeType = fileEncoding.detectFileEncoding('utf-16be.txt', new Buffer([254, 255, 0 ,0]));
 }
 catch(exception) {
     console.log('UTF-16BE with BOM validated');
 }
 
-var fileEncodeType = fileEncoding.detectFileEncoding('utf8.txt', new Buffer([255, 254, 10, 10]));
+var fileEncodeType = fileEncoding.detectFileEncoding('utf-16le.txt', new Buffer([255, 254, 10, 10]));
 if(fileEncodeType[0] === 'utf-16le' && fileEncodeType[1]) {
     console.log('UTF-16LE with BOM validated');
 }
 
 try {
-    var fileEncodeType = fileEncoding.detectFileEncoding('utf32BE.txt', new Buffer([0, 0, 254, 255]));
+    var fileEncodeType = fileEncoding.detectFileEncoding('utf-32BE.txt', new Buffer([0, 0, 254, 255]));
 }
 catch(exception) {
     console.log('UTF-32BE with BOM validated');
 }
 
-var fileEncodeType = fileEncoding.detectFileEncoding('utf8.txt', new Buffer([10, 11, 12, 13]));
+var fileEncodeType = fileEncoding.detectFileEncoding('utf-8.txt', new Buffer([10, 11, 12, 13]));
 if(fileEncodeType[0] === 'utf-8' && !fileEncodeType[1]) {
     console.log('UTF-8 without BOM validated');
 }
 
 try {
-    var fileEncodeType = fileEncoding.detectFileEncoding('utf32le.txt', new Buffer([255, 0, 0, 0]));
+    var fileEncodeType = fileEncoding.detectFileEncoding('utf-32le.txt', new Buffer([255, 0, 0, 0]));
 }
 catch(exception) {
     console.log('UTF-32LE without BOM validated');
 }
 
 try {
-    var fileEncodeType = fileEncoding.detectFileEncoding('utf32le.txt', new Buffer([0, 0, 0, 255]));
+    var fileEncodeType = fileEncoding.detectFileEncoding('utf-32be.txt', new Buffer([0, 0, 0, 255]));
 }
 catch(exception) {
     console.log('UTF-32BE without BOM validated');
 }
 
 try {
-    var fileEncodeType = fileEncoding.detectFileEncoding('utf16be.txt', new Buffer([0, 10, 0 ,20]));
+    var fileEncodeType = fileEncoding.detectFileEncoding('utf-16be.txt', new Buffer([0, 10, 0 ,20]));
 }
 catch(exception) {
     console.log('UTF-16BE without BOM validated');
 }
 
-var fileEncodeType = fileEncoding.detectFileEncoding('utf8.txt', new Buffer([20, 0, 10, 0]));
+var fileEncodeType = fileEncoding.detectFileEncoding('utf-16le.txt', new Buffer([20, 0, 10, 0]));
 if(fileEncodeType[0] === 'utf-16le' && !fileEncodeType[1]) {
     console.log('UTF-16LE without BOM validated');
 }
