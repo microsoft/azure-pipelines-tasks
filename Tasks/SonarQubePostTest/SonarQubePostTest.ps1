4827d72e82b4bd7bd26543501ada366603169a54
bgavril <bgavril@BGAVRIL-WORK2>
2016-11-29 13:27:03 +0000

    Deprecate the SonarQube msbuild tasks

--- Tasks/SonarQubePostTest/SonarQubePostTest.ps1
+++ Tasks/SonarQubePostTest/SonarQubePostTest.ps1
@@ -2,6 +2,10 @@ Write-Verbose "Starting SonarQube PostBuild Step"
 
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 
+Write-Warning "We are transferring ownership of the SonarQube related build tasks to Sonar Source. Please replace these build tasks with the ones provided by Sonar Source. 
+To do this, visit the marketplace to install the extension created by Sonar Source: https://aka.ms/sqextension
+For more details go to https://aka.ms/sqdeprecation"
+
 . $PSScriptRoot/Common/SonarQubeHelpers/SonarQubeHelper.ps1
 
 # During PR builds only an "issues mode" analysis is allowed. The resulting issues are posted as code review comments. 
--- Tasks/SonarQubePreBuild/SonarQubePreBuild.ps1
+++ Tasks/SonarQubePreBuild/SonarQubePreBuild.ps1
@@ -27,6 +27,10 @@ Write-Verbose "includeFullReport = $includeFullReport"
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
 
+Write-Warning "We are transferring ownership of the SonarQube related build tasks to Sonar Source. Please replace these build tasks with the ones provided by Sonar Source. 
+To do this, visit the marketplace to install the extension created by Sonar Source: https://aka.ms/sqextension
+For more details go to https://aka.ms/sqdeprecation"
+
 . $PSScriptRoot/Common/SonarQubeHelpers/SonarQubeHelper.ps1
 
 # During PR builds only an "issues mode" analysis is allowed. The resulting issues are posted as code review comments. 
