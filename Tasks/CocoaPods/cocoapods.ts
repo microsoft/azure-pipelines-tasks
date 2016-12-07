c3b0348a85402bac6b3e88abb88501cbe21360eb
David Staheli <dastahel@microsoft.com>
2016-12-07 12:49:26 -0500

    CocoaPods string refinements

--- Tasks/CocoaPods/cocoapods.ts
+++ Tasks/CocoaPods/cocoapods.ts
@@ -3,25 +3,33 @@ import tl = require('vsts-task-lib/task');
 import trm = require('vsts-task-lib/toolrunner');
 
 async function run() {
-    try {   
-		tl.setResourcePath(path.join( __dirname, 'task.json'));
+    try {
+        // Set path to resource strings
+        tl.setResourcePath(path.join(__dirname, 'task.json'));
 
+        // Change to configured working directory
         tl.cd(tl.getPathInput('cwd', true, true));
-        tl.debug('Setting locale to UTF8 - required by CocoaPods');
+
+        // Set locale to UTF-8
+        tl.debug('Setting locale to UTF8 as required by CocoaPods');
         process.env['LC_ALL'] = 'en_US.UTF-8';
 
+        // Locate the CocoaPods 'pod' command
         var podPath: string = tl.which('pod');
         if (!podPath) {
             throw new Error(tl.loc('CocoaPodsNotFound'));
         }
 
+        // Run 'pod install'
         var pod: trm.ToolRunner = tl.tool(podPath);
         pod.arg('install');
 
-		var code: number = await pod.exec();
+        // Get the result code and set the task result accordingly
+        var code: number = await pod.exec();
         tl.setResult(tl.TaskResult.Succeeded, tl.loc('PodReturnCode', code));
     }
     catch(err) {
+        // Report failure
         tl.error(err.message);
         tl.setResult(tl.TaskResult.Failed, tl.loc('PodFailed', err.message));
     }
