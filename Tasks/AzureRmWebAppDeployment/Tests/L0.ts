3a94832311f733beec995ae93a158756a93cbeec
vinca <vinca@microsoft.com>
2016-12-30 15:20:42 +0530

    removed debug logs

--- Tasks/AzureRmWebAppDeployment/Tests/L0.ts
+++ Tasks/AzureRmWebAppDeployment/Tests/L0.ts
@@ -371,7 +371,7 @@ describe('AzureRmWebAppDeployment Suite', function() {
         let tp = path.join(__dirname, 'L0WindowsManyPackage.js');
         let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
         tr.run();
-        console.log(tr.stdout);
+
         assert(tr.invokedToolCount == 0, 'should not have invoked any tool');
         assert(tr.stderr.length > 0 || tr.errorIssues.length > 0, 'should have written to stderr');
         var expectedErr = 'Error: loc_mock_MorethanonepackagematchedwithspecifiedpatternPleaserestrainthesearchpattern'; 
