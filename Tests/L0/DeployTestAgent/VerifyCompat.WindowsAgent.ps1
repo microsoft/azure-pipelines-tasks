72431487b9d4874248837df440d46674913947d5
Nitin Gurram <nigurr@microsoft.com>
2017-01-02 20:17:29 +0530

    Revert "Removing old files"
    This reverts commit c8e661dc4fd989e3461109483cd1c353f55b6919.

---
+++ Tests/L0/DeployTestAgent/VerifyCompat.WindowsAgent.ps1
@@ -0,0 +1,48 @@
+[cmdletbinding()]
+param()
+
+. $PSScriptRoot\..\..\lib\Initialize-Test.ps1
+
+$connection = 'some connection'
+$distributedTaskContext = 'Some distributed task context'
+Register-Mock Get-VssConnection { $connection } -TaskContext $distributedTaskContext
+Register-Mock Get-ServiceEndPoint { $vssEndPoint } -Context $distributedTaskContext -Name "SystemVssConnection"
+Register-Mock Get-PersonalAccessToken { 'Some token' }
+Register-Mock CmdletHasMember { $true }
+Register-Mock Invoke-DeployTestAgent
+Register-Mock Register-Environment { $true }
+
+$input = @{
+    'testMachineGroup' = 'testMachineGroup'
+    'adminUserName' = 'adminUserName' 
+    'adminPassword' = 'adminPassword' 
+    'winRmProtocol' = 'winRmProtocol' 
+    'testCertificate' = 'true'
+    'resourceFilteringMethod' = 'resourceFilteringMethod'
+    'testMachines' = 'testMachines'
+    'runAsProcess' = 'runAsProcess'
+    'machineUserName' = 'machineUserName'
+    'machinePassword' = 'machinePassword'
+    'agentLocation' = 'agentLocation'
+    'updateTestAgent' = 'updateTestAgent'
+    'isDataCollectionOnly' = 'isDataCollectionOnly'
+}
+& $PSScriptRoot\..\..\..\Tasks\DeployVisualStudioTestAgent\DeployTestAgent.ps1 @input
+
+Assert-WasCalled Register-Environment -ParametersEvaluator {
+    $EnvironmentName -eq 'testMachineGroup' -and
+    $EnvironmentSpecification -eq 'testMachineGroup' -and
+    $UserName -eq 'adminUserName' -and
+    $Password -eq 'adminPassword' -and
+    $TestCertificate -eq $true -and
+    $Connection -eq 'some connection' -and
+    $TaskContext -eq 'Some distributed task context' -and
+    $WinRmProtocol -eq 'winRmProtocol' -and
+    $ResourceFilter -eq 'testMachines' -and
+    $Persist
+}
+Assert-WasCalled Get-VssConnection -Times 1
+Assert-WasCalled Get-ServiceEndPoint -Times 1
+Assert-WasCalled Get-PersonalAccessToken -Times 1
+Assert-WasCalled CmdletHasMember -Times 1
+Assert-WasCalled Invoke-DeployTestAgent -Times 1
