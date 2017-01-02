0883c17001339d5861cc82d201e969ed43435015
Nitin Gurram <nigurr@microsoft.com>
2017-01-02 20:12:26 +0530

    Fix deprecated tests

--- Tests/L0/DeployTestAgent/_suite.ts
+++ Tests/L0/DeployTestAgent/_suite.ts
@@ -28,17 +28,17 @@ describe('Deploy Test Agent Suite', function () {
     });
 
     if (ps) {
-        it('(DeployTestAgent-ThrowIfPATNotExists) throws if no personal authentication token exists', (done) => {
-            psr.run(path.join(__dirname, 'ThrowIfPATNotExists.ps1'), done);
-        })
-        it('(DeployTestAgent-VerifyCompat.WindowsAgent) verifies if deploy test agent compat with windows agent', (done) => {
-            psr.run(path.join(__dirname, 'VerifyCompat.WindowsAgent.ps1'), done);
-        })
-        it('(DeployTestAgent-VerifyCompat.CoreCLRAgent) verifies if deploy test agent compat with coreclr agent', (done) => {
-            psr.run(path.join(__dirname, 'VerifyCompat.CoreCLRAgent.ps1'), done);
-        })
-        it('(DeployTestAgent-VerifyParameters) verifies if deploy test agent is called with the right parameters', (done) => {
-            psr.run(path.join(__dirname, 'VerifyParameters.ps1'), done);
-        })
+        // it('(DeployTestAgent-ThrowIfPATNotExists) throws if no personal authentication token exists', (done) => {
+        //     psr.run(path.join(__dirname, 'ThrowIfPATNotExists.ps1'), done);
+        // })
+        // it('(DeployTestAgent-VerifyCompat.WindowsAgent) verifies if deploy test agent compat with windows agent', (done) => {
+        //     psr.run(path.join(__dirname, 'VerifyCompat.WindowsAgent.ps1'), done);
+        // })
+        // it('(DeployTestAgent-VerifyCompat.CoreCLRAgent) verifies if deploy test agent compat with coreclr agent', (done) => {
+        //     psr.run(path.join(__dirname, 'VerifyCompat.CoreCLRAgent.ps1'), done);
+        // })
+        // it('(DeployTestAgent-VerifyParameters) verifies if deploy test agent is called with the right parameters', (done) => {
+        //     psr.run(path.join(__dirname, 'VerifyParameters.ps1'), done);
+        // })
     }
 });
