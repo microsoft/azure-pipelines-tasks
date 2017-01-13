7056b7155e7de96338df1e8fbea9ea212cc0f405
arjgupta <arjgupta@microsoft.com>
2017-01-13 12:42:15 +0530

    added option for cwd for exec commands

--- Tasks/Common/webdeployment-common/msdeployutility.ts
+++ Tasks/Common/webdeployment-common/msdeployutility.ts
@@ -104,7 +104,7 @@ export async  function containsParamFile(webAppPackage: string ) {
     var outputObj = fs.createWriteStream("",{fd: fd});
     try {
         var msDeployCheckParamFileCmdArgs = "-verb:getParameters -source:package=\'" + webAppPackage + "\'";
-        await tl.exec("msdeploy", msDeployCheckParamFileCmdArgs, <any>{ failOnStdErr: true, outStream: outputObj});
+        await tl.exec("msdeploy", msDeployCheckParamFileCmdArgs, <any>{ failOnStdErr: true, outStream: outputObj, cwd: tl.getVariable('System.DefaultWorkingDirectory')});
     }
     catch(error) {
         throw Error(error);
