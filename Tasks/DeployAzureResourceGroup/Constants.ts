affc1d54510c4543ed52ddb1773f8fd356d7a9fe
Tejas Deshpande <tedeshpa@microsoft.com>
2017-01-17 13:05:13 +0530

    removing old files

--- Tasks/DeployAzureResourceGroup/Constants.ts
+++
@@ -1,8 +0,0 @@
-export var publisher = "Microsoft.VisualStudio.Services";
-export var extensionType = "Microsoft.Compute/virtualMachines/extensions";
-export var mgExtensionNameWindows = "TeamServicesAgent";
-export var vmExtensionTypeWindows = "TeamServicesAgent";
-export var mgExtensionNameLinux = "TeamServicesAgentLinux";
-export var vmExtensionTypeLinux = "TeamServicesAgentLinux";
-export var version = "1.0";
-export var enablePrereqMG = "ConfigureVMWithMGAgent";
