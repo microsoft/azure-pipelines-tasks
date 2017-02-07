81b7f1d8b483f2c678c051070bcec446da0d2cdc
arjgupta <arjgupta@microsoft.com>
2017-02-07 18:10:30 +0530

    minor case change

--- Tasks/Common/webdeployment-common/utility.ts
+++ Tasks/Common/webdeployment-common/utility.ts
@@ -148,7 +148,7 @@ export async function isMSDeployPackage(webAppPackage: string ) {
     var isParamFilePresent = false;
     var pacakgeComponent = await zipUtility.getArchivedEntries(webAppPackage);
     if (((pacakgeComponent["entries"].indexOf("parameters.xml") > -1) || (pacakgeComponent["entries"].indexOf("Parameters.xml") > -1)) && 
-    ((pacakgeComponent["entries"].indexOf("systeminfo.xml") > -1) || (pacakgeComponent["entries"].indexOf("systemInfo.xml") > -1))) {
+    ((pacakgeComponent["entries"].indexOf("systemInfo.xml") > -1) || (pacakgeComponent["entries"].indexOf("systeminfo.xml") > -1))) {
         isParamFilePresent = true;
     }
     tl.debug("Is the package an msdeploy package : " + isParamFilePresent);
