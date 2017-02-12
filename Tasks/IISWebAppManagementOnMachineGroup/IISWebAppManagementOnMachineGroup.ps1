8f13b8ad5a6cae36ef3496d62d494cd6f45b657a
asranja <asranja@microsoft.com>
2017-02-12 13:30:50 +0530

    Updated the IIS Manage task

--- Tasks/IISWebAppManagementOnMachineGroup/IISWebAppManagementOnMachineGroup.ps1
+++ Tasks/IISWebAppManagementOnMachineGroup/IISWebAppManagementOnMachineGroup.ps1
@@ -5,62 +5,102 @@ Trace-VstsEnteringInvocation $MyInvocation
 Import-VstsLocStrings "$PSScriptRoot\Task.json"
 
 . $PSScriptRoot\Utility.ps1
-Import-Module $PSScriptRoot\ps_modules\TaskModuleIISManageUtility
+
+# Get all inputs 
+
+# Website related inputs 
+$actionIISWebsite = Get-VstsInput -Name "ActionIISWebsite"
+$websiteName = Get-VstsInput -Name "WebsiteName" 
+$websitePhysicalPath = Get-VstsInput -Name "WebsitePhysicalPath" 
+$websitePhysicalPathAuth = Get-VstsInput -Name "WebsitePhysicalPathAuth" 
+$websiteAuthUserName = Get-VstsInput -Name "WebsiteAuthUserName"
+$websiteAuthUserPassword = Get-VstsInput -Name "WebsiteAuthUserPassword"
+$addBinding = Get-VstsInput -Name "AddBinding"
+$protocol = Get-VstsInput -Name "Protocol" 
+$ipAddress = Get-VstsInput -Name "IPAddress"
+$port = Get-VstsInput -Name "Port"
+$serverNameIndication = Get-VstsInput -Name "ServerNameIndication"
+$hostNameWithOutSNI = Get-VstsInput -Name "HostNameWithOutSNI"
+$hostNameWithHttp = Get-VstsInput -Name "HostNameWithHttp"
+$hostNameWithSNI = Get-VstsInput -Name "HostNameWithSNI"
+$sslCertThumbPrint = Get-VstsInput -Name "SSLCertThumbPrint"
+
+$createOrUpdateAppPoolForWebsite = Get-VstsInput -Name "CreateOrUpdateAppPoolForWebsite"
+$appPoolNameForWebsite = Get-VstsInput -Name "AppPoolNameForWebsite"
+$dotNetVersionForWebsite = Get-VstsInput -Name "DotNetVersionForWebsite"
+$pipeLineModeForWebsite = Get-VstsInput -Name "PipeLineModeForWebsite"
+$appPoolIdentityForWebsite = Get-VstsInput -Name "AppPoolIdentityForWebsite"
+$appPoolUsernameForWebsite = Get-VstsInput -Name "AppPoolUsernameForWebsite"
+$appPoolPasswordForWebsite = Get-VstsInput -Name "AppPoolPasswordForWebsite"
+
+# Virtual directory related inputs 
+$parentWebsiteNameForVD = Get-VstsInput -Name "ParentWebsiteNameForVD"
+$virtualPathForVD = Get-VstsInput -Name "VirtualPathForVD"
+$physicalPathForVD = Get-VstsInput -Name "PhysicalPathForVD"
+$vdPhysicalPathAuth = Get-VstsInput -Name "VDPhysicalPathAuth"
+$vdAuthUserName = Get-VstsInput -Name "VDAuthUserName"
+$vdAuthUserPassword = Get-VstsInput -Name "VDAuthUserPassword"
+
+# Application related inputs 
+$parentWebsiteNameForApplication = Get-VstsInput -Name "ParentWebsiteNameForApplication"
+$virtualPathForApplication = Get-VstsInput -Name "VirtualPathForApplication"
+$physicalPathForApplication = Get-VstsInput -Name "PhysicalPathForApplication"
+$applicationPhysicalPathAuth = Get-VstsInput -Name "ApplicationPhysicalPathAuth"
+$applicationAuthUserName = Get-VstsInput -Name "ApplicationAuthUserName"
+$applicationAuthUserPassword = Get-VstsInput -Name "ApplicationAuthUserPassword"
+
+$createOrUpdateAppPoolForApplication = Get-VstsInput -Name "CreateOrUpdateAppPoolForApplication"
+$appPoolNameForApplication = Get-VstsInput -Name "AppPoolNameForApplication"
+$dotNetVersionForApplication = Get-VstsInput -Name "DotNetVersionForApplication"
+$pipeLineModeForApplication = Get-VstsInput -Name "PipeLineModeForApplication"
+$appPoolIdentityForApplication = Get-VstsInput -Name "AppPoolIdentityForApplication"
+$appPoolUsernameForApplication = Get-VstsInput -Name "AppPoolUsernameForApplication"
+$appPoolPasswordForApplication = Get-VstsInput -Name "AppPoolPasswordForApplication"
+
+# Application pool related inputs 
+$actionIISApplicationPool = Get-VstsInput -Name "ActionIISApplicationPool"
+$appPoolName = Get-VstsInput -Name "AppPoolName"
+$dotNetVersion = Get-VstsInput -Name "DotNetVersion"
+$pipeLineMode = Get-VstsInput -Name "PipeLineMode"
+$appPoolIdentity = Get-VstsInput -Name "AppPoolIdentity"
+$appPoolUsername = Get-VstsInput -Name "AppPoolUsername"
+$appPoolPassword = Get-VstsInput -Name "AppPoolPassword"
+
+# Additional appcmd inputs 
+$appCmdCommands = Get-VstsInput -Name "AppCmdCommands"
 
 try {
     
-    $createWebsite = Get-VstsInput -Name "CreateWebsite"
-    $websiteName = Get-VstsInput -Name "WebsiteName" 
-    $websitePhysicalPath = Get-VstsInput -Name "WebsitePhysicalPath" 
-    $websitePhysicalPathAuth = Get-VstsInput -Name "WebsitePhysicalPathAuth" 
-    $websiteAuthUserName = Get-VstsInput -Name "WebsiteAuthUserName"
-    $websiteAuthUserPassword = Get-VstsInput -Name "WebsiteAuthUserPassword"
-    $addBinding = Get-VstsInput -Name "AddBinding"
-    $protocol = Get-VstsInput -Name "Protocol" 
-    $ipAddress = Get-VstsInput -Name "IPAddress"
-    $port = Get-VstsInput -Name "Port"
-    $serverNameIndication = Get-VstsInput -Name "ServerNameIndication"
-    $hostNameWithOutSNI = Get-VstsInput -Name "HostNameWithOutSNI"
-    $hostNameWithHttp = Get-VstsInput -Name "HostNameWithHttp"
-    $hostNameWithSNI = Get-VstsInput -Name "HostNameWithSNI"
-    $sslCertThumbPrint = Get-VstsInput -Name "SSLCertThumbPrint"
+    $iisDeploymentType = Get-VstsInput -Name "IISDeploymentType"
 
-    $createAppPool = Get-VstsInput -Name "CreateAppPool"
-    $appPoolName = Get-VstsInput -Name "AppPoolName"
-    $dotNetVersion = Get-VstsInput -Name "DotNetVersion"
-    $pipeLineMode = Get-VstsInput -Name "PipeLineMode"
-    $appPoolIdentity = Get-VstsInput -Name "AppPoolIdentity"
-    $appPoolUsername = Get-VstsInput -Name "AppPoolUsername"
-    $appPoolPassword = Get-VstsInput -Name "AppPoolPassword"
-    $appCmdCommands = Get-VstsInput -Name "AppCmdCommands"
-    $deployInParallel = Get-VstsInput -Name "DeployInParallel"
-
-    $hostName = Get-HostName -protocol $protocol -hostNameWithHttp $hostNameWithHttp -hostNameWithSNI $hostNameWithSNI -hostNameWithOutSNI $hostNameWithOutSNI -sni $serverNameIndication
-    
-    Trim-Inputs -siteName ([ref]$websiteName) -physicalPath ([ref]$websitePhysicalPath)  -poolName ([ref]$appPoolName) -websitePathAuthuser ([ref]$websiteAuthUserName) -appPoolUser ([ref]$appPoolUsername) -sslCertThumbPrint ([ref]$sslCertThumbPrint)
-
-    Validate-Inputs -createWebsite $createWebsite -websiteName $websiteName -createAppPool $createAppPool -appPoolName $appPoolName -addBinding $addBinding -protocol $protocol -sslCertThumbPrint $sslCertThumbPrint
-    
-    if ($createWebsite -and $websitePhysicalPathAuth -ieq "WebsiteWindowsAuth") {
-        $websiteAuthUserPassword = Escape-SpecialChars -str $WebsiteAuthUserPassword
-        $websitePhysicalPathAuthPassword = "$websiteAuthUserPassword" | ConvertTo-SecureString -AsPlainText -Force
-        $websitePhysicalPathAuthCredentials = New-Object System.Management.Automation.PSCredential ("$websiteAuthUserName", $websitePhysicalPathAuthPassword)
+    switch ($iisDeploymentType)
+    {
+        "IISWebsite" 
+        {
+            Manage-IISWebsite
+            break
+        }
+        "IISWebApplication" 
+        {
+            Manage-IISWebApplication
+            break
+        }
+        "IISVirtualDirectory" 
+        {
+            Manage-IISVirtualDirectory
+            break
+        }
+        "IISApplicationPool" 
+        {
+            Manage-IISApplicationPool
+        }
     }
-
-    if ($createAppPool -and $appPoolIdentity -ieq "SpecificUser") {
-        $appPoolPassword = Escape-SpecialChars -str $appPoolPassword
-        $appPoolPassword = "$appPoolPassword" | ConvertTo-SecureString -AsPlainText -Force
-        $appPoolCredentials = New-Object System.Management.Automation.PSCredential ("$appPoolUsername", $appPoolPassword)
-    }
-
-    $appCmdCommands = Escape-SpecialChars -str $appCmdCommands
-
-    Execute-Main -CreateWebsite $createWebsite -WebsiteName $websiteName -WebsitePhysicalPath $websitePhysicalPath -WebsitePhysicalPathAuth $websitePhysicalPathAuth -websitePhysicalPathAuthCredentials $websitePhysicalPathAuthCredentials -AddBinding $addBinding -Protocol $protocol -IpAddress $ipAddress -Port $port -HostName $hostName -ServerNameIndication $serverNameIndication -SslCertThumbPrint $sslCertThumbPrint -CreateAppPool $createAppPool -AppPoolName $appPoolName -DotNetVersion $dotNetVersion -PipeLineMode $pipeLineMode -AppPoolIdentity $appPoolIdentity -AppPoolCredentials $appPoolCredentials -AppCmdCommands $appCmdCommands
 }
-catch [Exception] {
-    
+catch [Exception] 
+{    
     Write-Error ($_.Exception.Message)
 }
-finally {
+finally 
+{
     Trace-VstsLeavingInvocation $MyInvocation
 }
