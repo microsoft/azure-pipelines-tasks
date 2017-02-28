67428e4e3cf3f5cc368c71f2b2a7ef6f7b86e9da
Disha Khakhar <dikhakha@microsoft.com>
2017-03-01 05:07:52 +0530

    support pattern for gulp file

--- Tasks/Gulp/gulptask.ts
+++ Tasks/Gulp/gulptask.ts
@@ -2,113 +2,153 @@
 
 import path = require('path');
 import tl = require('vsts-task-lib/task');
+import trm = require('vsts-task-lib/toolrunner');
 
 tl.setResourcePath(path.join(__dirname, 'task.json'));
 
-var gulpFile = tl.getPathInput('gulpFile', true, true);
+var gulpFilePath = tl.getPathInput('gulpFile', true, false);
 var gulp = tl.which('gulp', false);
 var isCodeCoverageEnabled = tl.getBoolInput('enableCodeCoverage');
 var publishJUnitResults = tl.getBoolInput('publishJUnitResults');
 var testResultsFiles = tl.getInput('testResultsFiles', publishJUnitResults);
 var cwd = tl.getPathInput('cwd', true, false);
-tl.mkdirP(cwd);
-tl.cd(cwd);
+var cwdGivenFlag = tl.getInput('cwd', true) ? true : false;
 
-tl.debug('check path : ' + gulp);
-if (!tl.exist(gulp)) {
-	tl.debug('not found global installed gulp, try to find gulp locally.');
-	var gt = tl.tool(tl.which('node', true));
-	var gulpjs = tl.getInput('gulpjs', true);
-	gulpjs = path.resolve(cwd, gulpjs);
-	tl.debug('check path : ' + gulpjs);
-	if (!tl.exist(gulpjs)) {
-		tl.setResult(tl.TaskResult.Failed, tl.loc('GulpNotInstalled', gulpjs));
-	}
-	gt.arg(gulpjs);
-}
-else {
-	var gt = tl.tool(gulp);
-}
+executeTask();
 
-if (isCodeCoverageEnabled) {
-	var npm = tl.tool(tl.which('npm', true));
-	npm.line('install istanbul');
-	var testFramework = tl.getInput('testFramework', true);
-	var srcFiles = tl.getInput('srcFiles', false);
-	var testSrc = tl.getPathInput('testFiles', true, false);
-	var istanbul = tl.tool(tl.which('node', true));
-	istanbul.arg('./node_modules/istanbul/lib/cli.js');
-	istanbul.line('cover --report cobertura --report html');
-	if (srcFiles) {
-		istanbul.line('-i .' + path.sep + path.join(srcFiles));
+function executeTask() {
+	tl.debug('check path : ' + gulp);
+	if (!tl.exist(gulp)) {
+		tl.debug('not found global installed gulp, try to find gulp locally.');
+		var gt = tl.tool(tl.which('node', true));
+		var gulpjs = tl.getInput('gulpjs', true);
+		gulpjs = path.resolve(cwd, gulpjs);
+		tl.debug('check path : ' + gulpjs);
+		if (!tl.exist(gulpjs)) {
+			tl.setResult(tl.TaskResult.Failed, tl.loc('GulpNotInstalled', gulpjs));
+		}
+		gt.arg(gulpjs);
 	}
-	if (testFramework.toLowerCase() == 'jasmine') {
-		istanbul.line('./node_modules/jasmine/bin/jasmine.js JASMINE_CONFIG_PATH=node_modules/jasmine/lib/examples/jasmine.json');
-	} else {
-		istanbul.arg('./node_modules/mocha/bin/_mocha');
+	else {
+		var gt = tl.tool(gulp);
 	}
-	istanbul.arg(testSrc);
-	var summaryFile = path.join(cwd, 'coverage/cobertura-coverage.xml');
-	var reportDirectory = path.join(cwd, 'coverage/');
-}
 
-// optional - no targets will concat nothing
-gt.arg(tl.getDelimitedInput('targets', ' ', false));
-gt.arg('--gulpfile');
-gt.arg(gulpFile);
-gt.line(tl.getInput('arguments', false));
-gt.exec().then(function (code) {
-	publishTestResults(publishJUnitResults, testResultsFiles);
 	if (isCodeCoverageEnabled) {
-		npm.exec().then(function () {
-			istanbul.exec().then(function (code) {
-				publishCodeCoverage(summaryFile);
-				tl.setResult(tl.TaskResult.Succeeded, tl.loc('GulpReturnCode', code));
-			}).fail(function (err) {
-				publishCodeCoverage(summaryFile);
-				tl.debug('taskRunner fail');
-				tl.setResult(tl.TaskResult.Failed, tl.loc('IstanbulFailed', err.message));
-			});
-		}).fail(function (err) {
-			tl.debug('taskRunner fail');
-			tl.setResult(tl.TaskResult.Failed, tl.loc('NpmFailed', err.message));
-		})
-	} else {
-		tl.setResult(tl.TaskResult.Succeeded, tl.loc('GulpReturnCode', code));
+		var npm = tl.tool(tl.which('npm', true));
+		npm.line('install istanbul');
+		var testFramework = tl.getInput('testFramework', true);
+		var srcFiles = tl.getInput('srcFiles', false);
+		var testSrc = tl.getPathInput('testFiles', true, false);
+		var istanbul = tl.tool(tl.which('node', true));
+		istanbul.arg('./node_modules/istanbul/lib/cli.js');
+		istanbul.line('cover --report cobertura --report html');
+		if (srcFiles) {
+			istanbul.line('-i .' + path.sep + path.join(srcFiles));
+		}
+		if (testFramework.toLowerCase() == 'jasmine') {
+			istanbul.line('./node_modules/jasmine/bin/jasmine.js JASMINE_CONFIG_PATH=node_modules/jasmine/lib/examples/jasmine.json');
+		} else {
+			istanbul.arg('./node_modules/mocha/bin/_mocha');
+		}
+		istanbul.arg(testSrc);
 	}
-}).fail(function (err) {
-	publishTestResults(publishJUnitResults, testResultsFiles);
-	tl.debug('taskRunner fail');
-	tl.setResult(tl.TaskResult.Failed, tl.loc('GulpFailed', err.message));
-})
+	try {
+		var gulpFiles = [];
+		if (/[\*?+@!{}\[\]]/.test(gulpFilePath)) {
+			gulpFiles = tl.findMatch(tl.getVariable("System.DefaultWorkingDirectory"), gulpFilePath);
+			tl.debug("number of files matching filePath: " + gulpFiles.length);
+		}
+		else {
+			gulpFiles = [gulpFilePath];
+		}
+	}
+	catch (error) {
+		tl.warning(error);
+		tl.setResult(tl.TaskResult.Succeeded, "");
+		return;
+	}
+
+	tl.mkdirP(cwd);
+	tl.cd(cwd);
+
+	gt.arg(tl.getDelimitedInput('targets', ' ', false));
+	gt.arg('--gulpfile');
+	gt.arg("");
+	gt.line(tl.getInput('arguments', false));
+
+	var dirName = cwd;
+	for (var gulpFile of gulpFiles) {
+		// optional - no targets will concat nothing
+		substituteGulpFilePath(gt, gulpFile);
+		gt.exec().then(function (code) {
+			publishTestResults(publishJUnitResults, testResultsFiles);
+			if (isCodeCoverageEnabled) {
+				if (!cwdGivenFlag) {
+					dirName = path.dirname(gulpFile);
+				}
+				var summaryFile = path.join(dirName, 'coverage/cobertura-coverage.xml');
+				var reportDirectory = path.join(dirName, 'coverage/');
+				npm.exec().then(function () {
+					istanbul.exec().then(function (code) {
+						publishCodeCoverage(summaryFile, reportDirectory);
+						tl.setResult(tl.TaskResult.Succeeded, tl.loc('GulpReturnCode', code));
+					}).fail(function (err) {
+						publishCodeCoverage(summaryFile, reportDirectory);
+						tl.debug('taskRunner fail');
+						tl.setResult(tl.TaskResult.Failed, tl.loc('IstanbulFailed', err.message));
+						return;
+					});
+				}).fail(function (err) {
+					tl.debug('taskRunner fail');
+					tl.setResult(tl.TaskResult.Failed, tl.loc('NpmFailed', err.message));
+					return;
+				})
+			} else {
+				tl.setResult(tl.TaskResult.Succeeded, tl.loc('GulpReturnCode', code));
+			}
+		}).fail(function (err) {
+			publishTestResults(publishJUnitResults, testResultsFiles);
+			tl.debug('taskRunner fail');
+			tl.setResult(tl.TaskResult.Failed, tl.loc('GulpFailed', err.message));
+			return;
+		})
+	}
+}
+
+function substituteGulpFilePath(gt: trm.ToolRunner, filePath: string) {
+	var indexOfGulpFile = gt.args.indexOf("--gulpfile");
+	if (indexOfGulpFile >= 0) {
+		gt.args[indexOfGulpFile + 1] = filePath;
+	}
+}
 
 function publishTestResults(publishJUnitResults, testResultsFiles: string) {
-    if (publishJUnitResults) {
-        //check for pattern in testResultsFiles
-        if (testResultsFiles.indexOf('*') >= 0 || testResultsFiles.indexOf('?') >= 0) {
-            tl.debug('Pattern found in testResultsFiles parameter');
-            var buildFolder = tl.getVariable('System.DefaultWorkingDirectory');
-            var allFiles = tl.find(buildFolder);
-            var matchingTestResultsFiles = tl.match(allFiles, testResultsFiles, "", { matchBase: true });
-        }
-        else {
-            tl.debug('No pattern found in testResultsFiles parameter');
-            var matchingTestResultsFiles = [testResultsFiles];
-        }
-        if (!matchingTestResultsFiles || matchingTestResultsFiles.length == 0) {
-            tl.warning('No test result files matching ' + testResultsFiles + ' were found, so publishing JUnit test results is being skipped.');
-            return 0;
-        }
-        var tp = new tl.TestPublisher("JUnit");
+	if (publishJUnitResults) {
+		//check for pattern in testResultsFiles
+		if (testResultsFiles.indexOf('*') >= 0 || testResultsFiles.indexOf('?') >= 0) {
+			tl.debug('Pattern found in testResultsFiles parameter');
+			var buildFolder = tl.getVariable('System.DefaultWorkingDirectory');
+			var allFiles = tl.find(buildFolder);
+			var matchingTestResultsFiles = tl.match(allFiles, testResultsFiles, "", { matchBase: true });
+		}
+		else {
+			tl.debug('No pattern found in testResultsFiles parameter');
+			var matchingTestResultsFiles = [testResultsFiles];
+		}
+		if (!matchingTestResultsFiles || matchingTestResultsFiles.length == 0) {
+			tl.warning('No test result files matching ' + testResultsFiles + ' were found, so publishing JUnit test results is being skipped.');
+			return 0;
+		}
+		var tp = new tl.TestPublisher("JUnit");
 		try {
 			tp.publish(matchingTestResultsFiles, true, "", "", "", true);
 		} catch (error) {
 			tl.warning(error);
 		}
-    }
+	}
 }
 
-function publishCodeCoverage(summaryFile) {
+function publishCodeCoverage(summaryFile, reportDirectory) {
 	try {
 		var ccPublisher = new tl.CodeCoveragePublisher();
 		ccPublisher.publish('cobertura', summaryFile, reportDirectory, "");
