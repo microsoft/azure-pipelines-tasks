106978f4df2ccd64f01997d24934edba204490a1
Tejas Deshpande <tedeshpa@microsoft.com>
2017-03-16 15:39:17 +0530

    changing pat token input to service endpoint type

--- Tasks/AzureResourceGroupDeployment/models/DeployAzureRG.ts
+++ Tasks/AzureResourceGroupDeployment/models/DeployAzureRG.ts
@@ -4,6 +4,32 @@
 import tl = require("vsts-task-lib/task");
 import msRestAzure = require("./../operations/azure-rest/azure-arm-common");
 
+class TokenCredentials {
+    private hostUrl: string;
+    private patToken: string;
+
+    constructor(hostUrl: string, patToken: string){
+        if (!Boolean(hostUrl) || typeof hostUrl.valueOf() !== 'string') {
+            throw new Error(tl.loc("HostUrlCannotBeEmpty"));
+        }
+
+        if (!Boolean(patToken) || typeof patToken.valueOf() !== 'string') {
+            throw new Error(tl.loc("PatTokenCannotBeEmpty"));
+        }
+
+        this.hostUrl = hostUrl;
+        this.patToken = patToken;
+    }
+
+    public getPatToken(): string{
+        return this.patToken;
+    }
+
+    public getHostUrl(): string{
+        return this.hostUrl;
+    }
+}
+
 export class AzureRGTaskParameters {
 
     public action: string;
@@ -18,13 +44,13 @@ export class AzureRGTaskParameters {
     public enableDeploymentPrerequisites: string;
     public machineGroupName: string;
     public copyAzureVMTags: boolean;
-    public vstsPATToken: string;
     public outputVariable: string;
     public subscriptionId: string;
     public deploymentMode: string;
     public credentials: msRestAzure.ApplicationTokenCredentials;
     public machineGroupCollectionUrl = "";
     public machineGroupProjectName = "";
+    public tokenCredentials: TokenCredentials;
 
     constructor() {
         try {
@@ -45,7 +71,8 @@ export class AzureRGTaskParameters {
             this.enableDeploymentPrerequisites = tl.getInput("enableDeploymentPrerequisites");
             this.machineGroupName = tl.getInput("machineGroupName");
             this.copyAzureVMTags = tl.getBoolInput("copyAzureVMTags");
-            this.vstsPATToken = tl.getInput("vstsPATToken");
+            var vstsPatTokenEndpointName = tl.getInput("vstsPATTokenEndpoint", true);
+            this.tokenCredentials = this.getVSTSPatToken(vstsPatTokenEndpointName);
             this.outputVariable = tl.getInput("outputVariable");
             this.deploymentMode = tl.getInput("deploymentMode");
             this.credentials = this.getARMCredentials(connectedService);
@@ -56,6 +83,19 @@ export class AzureRGTaskParameters {
         }
         catch (error) {
             throw (tl.loc("ARGD_ConstructorFailed", error.message));
+        }
+    }
+
+    private getVSTSPatToken(vstsPATTokenEndpointName: string): TokenCredentials{
+        var endpointAuth = tl.getEndpointAuthorization(vstsPATTokenEndpointName, true);
+        if(endpointAuth.scheme == 'Token'){
+            var hostUrl = tl.getEndpointUrl(vstsPATTokenEndpointName, true);
+            var patToken: string = endpointAuth.parameters["apitoken"];
+            var credentials = new TokenCredentials(hostUrl, patToken);
+            return credentials;
+        }
+        else{
+            throw("Endpoint authorization type can only be of type PAT token.");
         }
     }
 
