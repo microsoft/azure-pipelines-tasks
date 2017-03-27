d11c025a374d39b89af76d800a4f50e26c7b15ab
Bishal Prasad <biprasad@microsoft.com>
2017-03-27 16:57:47 +0530

    testing-2

--- Tasks/PackerBuild/Tests/L0LinuxInstallPacker.ts
+++ Tasks/PackerBuild/Tests/L0LinuxInstallPacker.ts
@@ -2,7 +2,7 @@ import ma = require('vsts-task-lib/mock-answer');
 import tmrm = require('vsts-task-lib/mock-run');
 import path = require('path');
 
-let taskPath = path.join(__dirname, '..\\src\\main.js');
+let taskPath = path.join(__dirname, '..', 'src', 'main.js');
 let tr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
 process.env['MOCK_NORMALIZE_SLASHES'] = 'true';
