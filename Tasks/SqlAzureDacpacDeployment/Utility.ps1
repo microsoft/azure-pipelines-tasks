29ca76a2baae406f3e35e4664331e5d98b4b00ad
arjgupta <arjgupta@microsoft.com>
2017-04-19 21:01:12 +0530

    removed Get-AgentStartIpAddress function

--- Tasks/SqlAzureDacpacDeployment/Utility.ps1
+++ Tasks/SqlAzureDacpacDeployment/Utility.ps1
@@ -53,38 +53,6 @@ function Get-AgentIPRange
     return $IPRange
 }
 
-
-function Get-AgentStartIPAddress
-{
-    $endpoint = (Get-VstsEndpoint -Name SystemVssConnection -Require)
-    $vssCredential = [string]$endpoint.auth.parameters.AccessToken
-
-    $vssUri = $env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI
-    if ($vssUri.IndexOf("visualstudio.com", [System.StringComparison]::OrdinalIgnoreCase) -ne -1) {
-        # This hack finds the DTL uri for a hosted account. Note we can't support devfabric since the
-        # there subdomain is not used for DTL endpoint
-        $vssUri = $vssUri.Replace("visualstudio.com", "vsdtl.visualstudio.com")
-    }
-
-    Write-Verbose "Querying VSTS uri '$vssUri' to get external ip address"
-
-    # Getting start ip address from dtl service
-    Write-Verbose "Getting external ip address by making call to dtl service"
-    $vssUri = $vssUri + "/_apis/vslabs/ipaddress"
-    $username = ""
-    $password = $vssCredential
-
-    $basicAuth = ("{0}:{1}" -f $username, $password)
-    $basicAuth = [System.Text.Encoding]::UTF8.GetBytes($basicAuth)
-    $basicAuth = [System.Convert]::ToBase64String($basicAuth)
-    $headers = @{Authorization=("Basic {0}" -f $basicAuth)}
-
-    $response = Invoke-RestMethod -Uri $($vssUri) -headers $headers -Method Get -ContentType "application/json"
-    Write-Verbose "Response: $response"
-
-    return $response.Value
-}
-
 function Get-Endpoint
 {
     param([String] [Parameter(Mandatory=$true)] $connectedServiceName)
