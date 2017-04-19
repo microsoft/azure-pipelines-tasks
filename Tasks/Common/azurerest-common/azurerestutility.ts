6f799aea73a82a979d2bad02d79b6e1a3cd48fc0
Arjun Gupta <arjgupta@microsoft.com>
2017-04-19 12:51:20 +0530

    Users/arjgupta/wait for app to start (#4058)
    * added functionality to wait for the azure app service state to change to running for the azure app service manage task
    
    * added functionality to wait for app state to change to running for appservicemanage task
    
    * handling slots now as well
    
    * minor typo

--- Tasks/Common/azurerest-common/azurerestutility.ts
+++ Tasks/Common/azurerest-common/azurerestutility.ts
@@ -556,6 +556,35 @@ export async function testAzureWebAppAvailability(webAppUrl, availabilityTimeout
     return deferred.promise;
 }
 
+export async function getAppServiceDetails(endpoint, resourceGroupName: string, webAppName: string, specifySlotFlag: boolean, slotName: string) {
+    
+    var deferred = Q.defer<any>();
+    var slotUrl = (specifySlotFlag) ? "/slots/" + slotName : "";
+    var url = endpoint.url + 'subscriptions/' + endpoint.subscriptionId + '/resourceGroups/' + resourceGroupName +
+                '/providers/Microsoft.Web/sites/' + webAppName + slotUrl + "?" + azureApiVersion;
+
+    var accessToken = await getAuthorizationToken(endpoint);
+    var headers = {
+        'Authorization': 'Bearer '+ accessToken
+    };
+    var webAppNameWithSlot = (specifySlotFlag) ? webAppName + '-' + slotName : webAppName;
+    tl.debug('Request to get App State: ' + webAppNameWithSlot);
+    httpObj.send('GET', url, null, headers, (error, response, body) => {
+        if(error) {
+            console.log(body);
+            deferred.reject(error);
+        }
+        if(response.statusCode === 200 || response.statusCode === 204) {
+            deferred.resolve(JSON.parse(body));
+        }
+        else {
+            console.log(body);
+            deferred.reject(tl.loc("FailedToFetchAppServiceState", webAppNameWithSlot, response.statusCode, response.statusMessage));
+        }
+    });
+    return deferred.promise;
+}
+
 function sleep(timeInMilliSecond) {
   return new Promise(resolve => setTimeout(resolve,timeInMilliSecond));
 }
