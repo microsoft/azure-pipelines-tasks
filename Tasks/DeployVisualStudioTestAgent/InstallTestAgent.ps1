081999c1ec26373b08dcbc89af06b049c00173e2
😊Nitin Gurram😊 <nigurr@microsoft.com>
2017-04-20 10:09:54 +0530

    Bump up task version and add if check for process exit

--- Tasks/DeployVisualStudioTestAgent/InstallTestAgent.ps1
+++ Tasks/DeployVisualStudioTestAgent/InstallTestAgent.ps1
@@ -13,19 +13,14 @@ function ExtractAgentArchive ($SetupArchive, $Destination) {
     }
 }
 
-function FindAgentSetupExe ($SetupPath) {
-    return (Get-ChildItem -Filter "*testagent.exe" -Path $SetupPath -ErrorAction SilentlyContinue | Select-Object -First 1).FullName
-}
-
-function InstallTestAgent2017
-{
+function InstallTestAgent2017 {
     param
     (
         [String] $SetupPath,
         [String] $InstallPath
     )
 
-    if(-not (Test-Path -Path $SetupPath)) {
+    if (-not (Test-Path -Path $SetupPath)) {
         throw "Test agent source path '{0}' is not accessible to the test machine. Please check if the file exists and that test machine has access to that machine" -f $SetupPath
     }
 
@@ -45,16 +40,21 @@ function InstallTestAgent2017
     $p.StartInfo = $Processinfo
     $p.Start()
 
-    $p.WaitForExit(1500000) # it shouldn't take more than 25mins. so set time out
+    # it shouldn't take more than 1hr.
+    if ($p.WaitForExit(3600000)) {
+        Write-Warning "Installation couldn't get completed in an hour. Terminating the process".
+        Stop-Process $p -ErrorAction SilentlyContinue
+        return -1
+    } 
 
     return $p.ExitCode
 }
 
-function Install-Product($SetupPath, $ProductVersion, $Update)
-{
+function Install-Product($SetupPath, $ProductVersion, $Update) {
     $exitCode = 0
 
-    if(-not (Test-Path $SetupPath)){
+    Write-Verbose "Setup path: $SetupPath" -Verbose
+    if (-not (Test-Path $SetupPath)) {
         Write-Verbose "Test Agent path $SetupPath is invalid. Skipping the installation"
         return 1801
     }
@@ -63,110 +63,103 @@ function Install-Product($SetupPath, $ProductVersion, $Update)
     if ($SetupPath -ilike "*TestAgent.zip") {
         $SetupDir = Split-Path -Path $SetupPath
         ExtractAgentArchive -SetupArchive $SetupPath -Destination $SetupDir
-        $SetupPath = FindAgentSetupExe -SetupPath $SetupDir
+        $SetupPath = Join-Path $SetupDir "vs_TestAgent.exe"
     }
 
     $versionToInstall = ((Get-Item $SetupPath).VersionInfo.ProductVersion)
     $versionInstalled = Get-TestAgentInstalledVersion -ProductVersion $ProductVersion # Get installed test agent version as per user requested version
 
-    if($versionToInstall -ne $null) {
+    if ($versionToInstall -ne $null) {
         $versionToInstall = $versionToInstall.SubString(0, $versionToInstall.LastIndexOf('.'))
     }
 
     Write-Verbose "Installed Test Agent version: $versionInstalled"
     Write-Verbose "Requested Test Agent version: $versionToInstall"
 
-    if([version]$versionInstalled -gt ([version]"0.0") -and $Update -ieq "false") {
+    if ([version]$versionInstalled -gt ([version]"0.0") -and $Update -ieq "false") {
         # Test Agent installed with major version matching. No update requested
         Write-Verbose -Message ("Test Agent already exists") -verbose
         return $exitCode
     }
-    if([version]$versionInstalled -gt ([version]"0.0") -and [version]$versionInstalled -le [version]$versionToInstall)  {
+    if ([version]$versionInstalled -gt ([version]"0.0") -and [version]$versionInstalled -le [version]$versionToInstall) {
         # Already upto date. Ignore Update flag
         Write-Verbose -Message ("Test Agent is already upto date") -verbose
         return $exitCode
     }
 
-    if([version]$versionInstalled -eq ([version]"0.0")) {
+    if ([version]$versionInstalled -eq ([version]"0.0")) {
         # Test Agent is not installed with major version matching. Any other installations are ignored
         Write-Verbose -Message ("Test Agent will be installed") -verbose
     }
-    if([version]$versionInstalled -gt ([version]"0.0") -and [version]$versionInstalled -lt [version]$versionToInstall -and $Update -ieq "true") {
+    if ([version]$versionInstalled -gt ([version]"0.0") -and [version]$versionInstalled -lt [version]$versionToInstall -and $Update -ieq "true") {
         Write-Verbose -Message ("Test Agent will be updated from: {0} to: {1}" -f $versionInstalled, $versionToInstall) -verbose
     }
 
-    try
-    {
+    try {
         # Invoke the TA installation
-        if($ProductVersion -eq "15.0") {
+        if ($ProductVersion -eq "15.0") {
             $tpPath = Join-Path $env:SystemDrive TestAgent2017
             $retCode = InstallTestAgent2017 -SetupPath $SetupPath -InstallPath $tpPath
-            if($retCode -is [System.Array]) {
-                $exitCode = $retCode[$retCode.Length-1]
-            } else {
+            if ($retCode -is [System.Array]) {
+                $exitCode = $retCode[$retCode.Length - 1]
+            }
+            else {
                 $exitCode = $retCode
             }
             Write-Verbose -Message ("Exit code from installation $exitCode ") -verbose
-        } else {
-            $argumentsarr = @("/Quiet","/NoRestart")
+        }
+        else {
+            $argumentsarr = @("/Quiet", "/NoRestart")
             Write-Verbose -Message ("Invoking the command {0} with arguments {1}" -f $SetupPath, $Arguments) -verbose
-            $retCode = Invoke-Command -ScriptBlock { cmd.exe /c $args[0] $args[1]; $LASTEXITCODE } -ArgumentList $SetupPath,$argumentsarr -ErrorAction Stop
-            if($retCode -is [System.Array]) {
-                $exitCode = $retCode[$retCode.Length-1]
-            } else {
+            $retCode = Invoke-Command -ScriptBlock { cmd.exe /c $args[0] $args[1]; $LASTEXITCODE } -ArgumentList $SetupPath, $argumentsarr -ErrorAction Stop
+            if ($retCode -is [System.Array]) {
+                $exitCode = $retCode[$retCode.Length - 1]
+            }
+            else {
                 $exitCode = $retCode
             }
         }
     }
-    catch
-    {
+    catch {
         Write-Verbose -Verbose "Caught exception while installing Test Agent"
         throw $_.Exception
     }
 
-    if($exitCode -eq -2147185721)
-    {
+    if ($exitCode -eq -2147185721) {
         # pending restart .
-        try
-        {
+        try {
             $testAgentFile = "$env:SystemDrive\TestAgent\testagent.txt"
             $testAgentFileExists = Test-Path $testAgentFile
-            if($testAgentFileExists)
-            {
+            if ($testAgentFileExists) {
                 # delete the file which indicated that test agent installation failed.
                 Remove-Item $testAgentFile -force | Out-Null
                 # we have retried once .Now fail with appropriate message
                 Write-Verbose -Verbose "Retried to install Test Agent"
                 throw ("The return code {0} was not expected during installation of Test Agent. Check the installation logs for more details." -f $exitCode.ToString())
             }
-            else
-            {
+            else {
                 #creating testagent file to indicate testagent installation failed.
                 New-Item -Path $testAgentFile -type File | Out-Null
                 Write-Verbose -Message ("Installation of Test Agent failed with Error code {0}. Retrying once by rebooting machine" -f $exitCode.ToString()) -Verbose
                 return 3010;
             }
         }
-        catch
-        {
+        catch {
             Write-Verbose -Verbose "Error occurred while retrying the Test Agent installation"
             throw ("The return code {0} was not expected during installation of Test Agent. Check the installation logs for more details." -f $exitCode.ToString())
         }
     }
 
-    if($exitCode -eq -2147205120)
-    {
+    if ($exitCode -eq -2147205120) {
         # pending windows update.
         throw ("Pending windows update. The return code {0} was not expected during installation of Test Agent. Install windows update and try again." -f $exitCode.ToString())
     }
 
-    if(-not ($exitCode -eq 0 -or $exitCode -eq 3010 -or $exitCode -eq 3015 -or $exitCode -eq 1641))
-    {
+    if (-not ($exitCode -eq 0 -or $exitCode -eq 3010 -or $exitCode -eq 3015 -or $exitCode -eq 1641)) {
         throw ("The return code {0} was not expected during installation of Test Agent. Check the installation logs for more details." -f $exitCode.ToString())
     }
 
-    if($exitCode -eq 3010 -or $exitCode -eq 3015 -or $exitCode -eq 1641)
-    {
+    if ($exitCode -eq 3010 -or $exitCode -eq 3015 -or $exitCode -eq 1641) {
         # Return the required reboot code 3010
         Write-Verbose "Reboot required post test agent installation , return 3010" -Verbose
         return 3010;
