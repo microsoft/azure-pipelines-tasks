73a69f7c580848e86f6b26194c5ad25115e5d01c
Siddhartha Pandey <siddhap@microsoft.com>
2017-04-21 18:07:40 +0530

    Adding check for test case filter as well in the response file.

--- Tasks/VsTest/vstest.ts
+++ Tasks/VsTest/vstest.ts
@@ -955,7 +955,7 @@ function getTIALevel() {
 
 function responseContainsNoTests(filePath: string): Q.Promise<boolean> {
     return utils.Helper.readFileContents(filePath, 'utf-8').then(function (resp) {
-        if (resp === '/Tests:') {
+        if (resp === '/Tests:' || resp === '/TestCaseFilter:') {
             return true;
         }
         else {
