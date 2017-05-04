977feed3086655d1f57f1eb288be68771afc3459
Lucas Killgore <lukillgo@microsoft.com>
2017-05-04 17:00:23 -0400

    #939802: Moved common logic out into common file.

--- Tasks/InstallSSHKey/postinstallsshkey.ts
+++ Tasks/InstallSSHKey/postinstallsshkey.ts
@@ -1,51 +1,19 @@
-import fs = require('fs');
-import path = require('path');
 import tl = require('vsts-task-lib/task');
 import ps = require('process');
-
-import { ToolRunner } from 'vsts-task-lib/toolrunner';
-
-import trm = require('vsts-task-lib/toolrunner');
-
-function debugOutput(results: trm.IExecSyncResult) {
-    tl.debug('stdout=' + results.stdout);
-    tl.debug('stderr=' + results.stderr);
-    tl.debug('code  =' + results.code);
-    tl.debug('error =' + results.error);
-}
+import util = require('./installsshkey-util');
 
 async function run() {
     try {
-        let external: string = 'C:\\Program Files\\Git\\usr\\bin\\';
+        util.tryRestoreKnownHosts();
 
-        let results: trm.IExecSyncResult = tl.execSync('D:\\redist\\set.cmd', null);
-        debugOutput(results);
-
-
-        // Setup in Pre phase
-        const postKillAgentSetting: string = 'INSTALL_SSH_KEY_KILL_SSH_AGENT_PID';
-        const postDeleteKeySetting: string = 'INSTALL_SSH_KEY_DELETE_KEY';
-        const postKnownHostsContentsSetting: string = 'INSTALL_SSH_KEY_KNOWN_HOSTS_CONTENTS';
-        const postKnownHostsLocationSetting: string = 'INSTALL_SSH_KEY_KNOWN_HOSTS_LOCATION';
-
-        let agentPid: string = tl.getTaskVariable(postKillAgentSetting);
+        let agentPid: string = tl.getTaskVariable(util.postKillAgentSetting);
         if (agentPid) {
             tl.debug('Killing SSH Agent PID: ' + agentPid);
             ps.kill(+agentPid);
         } else {
-            let deleteKey: string = tl.getTaskVariable(postDeleteKeySetting);
-            if (deleteKey) {
-                tl.debug('Deleting Key: ' + deleteKey);
-                tl.execSync(path.join(external, 'ssh-add.exe'), '-d ' + deleteKey);
-            }
-        }
-        let knownHostsContents: string = tl.getTaskVariable(postKnownHostsContentsSetting);
-        let knownHostsLocation: string = tl.getTaskVariable(postKnownHostsLocationSetting);
-        tl.debug('Restoring known_hosts');
-        if (knownHostsContents && knownHostsLocation) {
-            fs.writeFileSync(knownHostsLocation, knownHostsContents);
-        } else if (knownHostsLocation || knownHostsContents) {
-            tl.warning('Inconsistency with known_hosts cannot reset (location=' + knownHostsLocation + ' content=' + knownHostsContents + ')');
+            let deleteKey: string = tl.getTaskVariable(util.postDeleteKeySetting);
+            let sshTool: util.SshToolRunner = new util.SshToolRunner();
+            sshTool.deleteKey(deleteKey)
         }
     } catch (err) {
         tl.setResult(tl.TaskResult.Failed, err);
