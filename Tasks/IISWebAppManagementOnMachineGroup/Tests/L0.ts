662b8711d55eb1639d878d5da7b98f417ac0848a
Ashish Ranjan <asranja@microsoft.com>
2017-05-05 11:00:33 +0530

    Added multiple binding support in IIS Manage (#3952)
    * Added multiple binding support in IIS Manage and updated major version
    
    * Updated the task to handle both current and previous inputs
    
    * Addressed review comments and minor fixes
    
    * bug fixes

--- Tasks/IISWebAppManagementOnMachineGroup/Tests/L0.ts
+++ Tasks/IISWebAppManagementOnMachineGroup/Tests/L0.ts
@@ -27,55 +27,6 @@ describe('IISWebAppManagementOnMachineGroup Suite', function () {
     });
 
     if (ps) {
-
-        it('test website exists', (done) => {
-            psr.run(path.join(__dirname, 'L0AppcmdTestWebsiteExists.ps1'), done);
-        }) 
-
-        it('test add and update website', (done) => {
-            psr.run(path.join(__dirname, 'L0AppcmdAddUpdateWebsite.ps1'), done);
-        }) 
-  		
-  		it('test add binding for website', (done) => {
-            psr.run(path.join(__dirname, 'L0AppcmdTestBinding.ps1'), done);
-        })
-
-        it('test sni and sslcert addition for https binding', (done) => {
-            psr.run(path.join(__dirname, 'L0AppcmdTestSSLandSNI.ps1'), done);
-        })
-
-        it('test application pool exists', (done) => {
-            psr.run(path.join(__dirname, 'L0AppcmdTestApplicationPoolExists.ps1'), done);
-        })
-
-        it('test add and update appPool', (done) => {
-            psr.run(path.join(__dirname, 'L0AppcmdAddUpdateAppPool.ps1'), done);
-        }) 
-
-        it('test application exists', (done) => {
-            psr.run(path.join(__dirname, 'L0AppcmdTestApplicationExists.ps1'), done);
-        })
-
-        it('test add and update application', (done) => {
-            psr.run(path.join(__dirname, 'L0AppcmdAddUpdateApplication.ps1'), done);
-        })
-     	
-        it('test virtual directory exists', (done) => {
-            psr.run(path.join(__dirname, 'L0AppcmdTestVirtualDirExists.ps1'), done);
-        }) 
-
-        it('test add and update virtual directory', (done) => {
-            psr.run(path.join(__dirname, 'L0AppcmdAddUpdateVDir.ps1'), done);
-        })
-
-        it('test additional actions for website and application pool', (done) => {
-            psr.run(path.join(__dirname, 'L0AppcmdAdditionalActions.ps1'), done);
-        }) 
-
-        it('test execute main for appcmd', (done) => {
-            psr.run(path.join(__dirname, 'L0AppcmdExecuteMain.ps1'), done);
-        }) 
-
         it('test iis manage utility - manage website', (done) => {
             psr.run(path.join(__dirname, 'L0UtilityManageWebsite.ps1'), done);
         }) 
