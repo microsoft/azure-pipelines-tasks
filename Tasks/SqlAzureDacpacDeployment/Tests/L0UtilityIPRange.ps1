fbdf7ebbd1520c101367f4a88fffd618733c12db
Arjun Gupta <arjgupta@microsoft.com>
2017-05-13 01:39:25 +0530

    updated L0 test for getip range

--- Tasks/SqlAzureDacpacDeployment/Tests/L0UtilityIPRange.ps1
+++ Tasks/SqlAzureDacpacDeployment/Tests/L0UtilityIPRange.ps1
@@ -8,7 +8,7 @@ param()
 . "$PSScriptRoot\..\Utility.ps1"
 
 # If connection failed because of firewall exception
-$sqlErrorMsg = "Sqlcmd: Error: Microsoft ODBC Driver 13 for SQL Server : Cannot open server 'a0nuel7r2k' requested by the login. Client with IP address '167.220.238.x' is not allowed to access the server.  To enable access, use the Windows Azure Management P
+$sqlErrorMsg = "123456 Sqlcmd: Error: Microsoft ODBC Driver 13 for SQL Server : Cannot open server 'a0nuel7r2k' requested by the login. Client with IP address '167.220.238.x' is not allowed to access the server.  To enable access, use the Windows Azure Management P
 ortal or run sp_set_firewall_rule on the master database to create a firewall rule for this IP address or address range.  It may take up to five minutes for this change to take effect..
 "
 $firewallException = New-Object -TypeName System.Management.Automation.RemoteException -ArgumentList $sqlErrorMsg
