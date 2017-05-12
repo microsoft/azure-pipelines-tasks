8d09d45c5df9b035764b661491a83f75ff854567
Arjun Gupta <arjgupta@microsoft.com>
2017-05-12 12:32:50 +0530

    Users/arjgupta/sqlcmdfix (#4077) (#4299)
    * using bundled sql cmd and using sqlcmdpassword env variable for passing password
    
    * removed Get-AgentStartIpAddress function
    
    * setting $env:SQLCMDPASSWORD only where it is required
    
    * fixed a test
    
    * fixed wrt review comments
    
    * updating wrt review comments
    
    * updating location to be used to fetch sqlcmd
    
    * updated link to sqlcmd
    
    * updated url for sqlcmd.zip
    
    * updated location of sqlcmd
    
    * Fixing no errors issue for auto detect (#4296)

--- Tasks/SqlAzureDacpacDeployment/Tests/L0UtilityIPRange.ps1
+++ Tasks/SqlAzureDacpacDeployment/Tests/L0UtilityIPRange.ps1
@@ -15,10 +15,6 @@ $firewallException = New-Object -TypeName System.Management.Automation.RemoteExc
 $errors = @()
 $errors += $firewallException
 
-$sqlCmd1 = New-Object PsObject
-$sqlCmd1 | Add-Member -MemberType NoteProperty -Name "Path" -Value "SqlCmd.exe"
-Register-Mock Get-Command { Write-Output $sqlCmd1 } -ParametersEvaluator { $Name -eq "SqlCmd.exe" -and $ErrorAction -eq 'SilentlyContinue' }
-
 $startIP = "167.220.238.0"
 $endIP = "167.220.238.255"
 
