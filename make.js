d1efbb10198eacef06c3d8b38294b06d3564b8cf
ericsciple <ericsciple@users.noreply.github.com>
2017-05-19 16:41:21 -0400

    Pin aggregate zip to 108..116

--- make.js
+++ make.js
@@ -446,25 +446,8 @@ target.publish = function() {
         return;
     }
 
-    // get the branch/commit info
-    var refs = util.getRefs();
-
-    // test whether to publish the non-aggregated tasks zip
-    // skip if not the tip of a release branch
-    var release = refs.head.release;
-    var commit = refs.head.commit;
-    if (!release ||
-        !refs.releases[release] ||
-        commit != refs.releases[release].commit) {
-
-        // warn not publishing the non-aggregated
-        console.log(`##vso[task.logissue type=warning]Skipping publish for non-aggregated tasks zip. HEAD is not the tip of a release branch.`);
-    }
-    else {
-        // store the non-aggregated tasks zip
-        var nonAggregatedZipPath = path.join(packagePath, 'non-aggregated-tasks.zip');
-        util.storeNonAggregatedZip(nonAggregatedZipPath, release, commit);
-    }
+    // warn not publishing the non-aggregated
+    console.log(`##vso[task.logissue type=warning]Skipping publish for non-aggregated tasks zip.`);
 
     // resolve the nupkg path
     var nupkgFile;
