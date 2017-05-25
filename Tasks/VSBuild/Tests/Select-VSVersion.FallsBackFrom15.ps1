60f938b5199cce1c913796d0414541f7bfe70b4d
ericsciple <ericsciple@users.noreply.github.com>
2017-05-25 16:53:41 -0400

    Fallback to and from vs 15 (#4404)

--- Tasks/VSBuild/Tests/Select-VSVersion.FallsBackFrom15.ps1
+++ Tasks/VSBuild/Tests/Select-VSVersion.FallsBackFrom15.ps1
@@ -8,9 +8,9 @@ Register-Mock Write-Warning
 Register-Mock Get-VSPath { "Some resolved location" } -- -Version '14.0'
 
 # Act.
-$actual = Select-VSVersion -PreferredVersion '12.0'
+$actual = Select-VSVersion -PreferredVersion '15.0'
 
 # Assert.
 Assert-WasCalled Write-Warning
-Assert-WasCalled Get-VSPath -Times 3
+Assert-WasCalled Get-VSPath -Times 2
 Assert-AreEqual -Expected '14.0' -Actual $actual
