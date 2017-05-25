d2c1828782eae123625608d698c630cd7c8ce345
arjgupta <arjgupta@microsoft.com>
2017-05-25 19:49:41 +0530

    passing an array of arguments instead of an argument string, so that we can enclose certain arguments inside double quotes

--- Tasks/Common/webdeployment-common/deployusingmsdeploy.ts
+++ Tasks/Common/webdeployment-common/deployusingmsdeploy.ts
@@ -78,6 +78,55 @@ export async function DeployUsingMSDeploy(webDeployPkg, webAppName, publishingPr
     }
 }
 
+function argStringToArray(argString): string[] {
+    var args = [];
+    var inQuotes = false;
+    var escaped = false;
+    var arg = '';
+    var append = function (c) {
+        // we only escape double quotes.
+        if (escaped && c !== '"') {
+            arg += '\\';
+        }
+        arg += c;
+        escaped = false;
+    };
+    for (var i = 0; i < argString.length; i++) {
+        var c = argString.charAt(i);
+        if (c === '"') {
+            if (!escaped) {
+                inQuotes = !inQuotes;
+            }
+            else {
+                append(c);
+            }
+            continue;
+        }
+        if (c === "\\" && inQuotes) {
+            if(escaped) {
+                append(c);
+            }
+            else {
+                escaped = true;
+            }
+
+            continue;
+        }
+        if (c === ' ' && !inQuotes) {
+            if (arg.length > 0) {
+                args.push(arg);
+                arg = '';
+            }
+            continue;
+        }
+        append(c);
+    }
+    if (arg.length > 0) {
+        args.push(arg.trim());
+    }
+    return args;
+}
+
 async function executeMSDeploy(msDeployCmdArgs) {
     var deferred = Q.defer();
 
@@ -93,6 +142,7 @@ async function executeMSDeploy(msDeployCmdArgs) {
     });
 
     try {
+        msDeployCmdArgs = argStringToArray(msDeployCmdArgs);
         await tl.exec("msdeploy", msDeployCmdArgs, <any>{failOnStdErr: true, errStream: errObj});
         deferred.resolve("Azure App service successfully deployed");
     } catch (error) {
