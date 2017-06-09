dadc25cf8cd79e49f3b22c0b096cf568a4ce5576
siddhap <siddhap@microsoft.com>
2017-06-09 13:27:58 +0530

    fix check for empty response file

--- Tasks/VsTest/vstest.ts
+++ Tasks/VsTest/vstest.ts
@@ -946,7 +946,6 @@ function getTIALevel() {
 
 function responseContainsNoTests(filePath: string): Q.Promise<boolean> {
     return utils.Helper.readFileContents(filePath, 'utf-8').then(function (resp) {
-        if (resp === '/Tests:' || resp === '/TestCaseFilter:') {
             return true;
         }
         else {
