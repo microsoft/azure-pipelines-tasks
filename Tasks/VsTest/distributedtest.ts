088aee77fe97a5f93a518f74a9297186a58ca89a
😊Nitin Gurram😊 <nigurr@microsoft.com>
2017-07-03 16:07:05 +0530

    Adding test case based slicing

--- Tasks/VsTest/distributedtest.ts
+++ Tasks/VsTest/distributedtest.ts
@@ -176,9 +176,10 @@ export class DistributedTest {
         utils.Helper.setEnvironmentVariableToString(envVars, 'codecoverageenabled', this.dtaTestConfig.codeCoverageEnabled);
         utils.Helper.setEnvironmentVariableToString(envVars, 'testplan', this.dtaTestConfig.testplan);
         utils.Helper.setEnvironmentVariableToString(envVars, 'testplanconfigid', this.dtaTestConfig.testPlanConfigId);
-        // In the phases world we will distribute based on number of agents
         utils.Helper.setEnvironmentVariableToString(envVars, 'customslicingenabled', 'true');
         utils.Helper.setEnvironmentVariableToString(envVars, 'maxagentphaseslicing', this.dtaTestConfig.numberOfAgentsInPhase.toString());
+        utils.Helper.setEnvironmentVariableToString(envVars, 'numberoftestcasesperslice',
+            this.dtaTestConfig.numberOfTestCasesPerSlice.toString());
 
         await runDistributesTestTool.exec(<tr.IExecOptions>{ cwd: path.join(__dirname, 'modules'), env: envVars });
         await this.cleanUp(settingsFile);
--- Tasks/VsTest/models.ts
+++ Tasks/VsTest/models.ts
@@ -1,74 +1,75 @@
 import * as version from './vstestversion';
 
-export interface TestConfigurations {
-    testSelection: string;
+export class TestConfigurations {
+    public testSelection: string;
 
     // ranjanar : TODO : Plan for better modelling of these
     // Test Assembly Related Properties
-    sourceFilter: string[];
-    testcaseFilter: string;
+    public sourceFilter: string[];
+    public testcaseFilter: string;
 
     // Test Plan related Properties
-    testplan: number;
-    testSuites: number[];
-    testPlanConfigId: number;
+    public testplan: number;
+    public testSuites: number[];
+    public testPlanConfigId: number;
 
     // Test Run Related Properties
-    onDemandTestRunId: string;
+    public onDemandTestRunId: string;
 
     // Common Properties
-    settingsFile: string;
-    testDropLocation: string; // search folder
-    overrideTestrunParameters: string;
-    codeCoverageEnabled: boolean;
-    videoCoverageEnabled: boolean;
-    buildConfig: string;
-    buildPlatform: string;
-    testRunTitle: string;
-    vsTestLocationMethod: string;
-    vsTestVersion: string;
-    vsTestLocation: string;
-    vsTestVersionDetais: version.VSTestVersion;
-    pathtoCustomTestAdapters: string;
-    tiaConfig: TiaConfiguration;
-    runInParallel: boolean;
-    runTestsInIsolation: boolean;
-    otherConsoleOptions: string;
+    public settingsFile: string;
+    public testDropLocation: string; // search folder
+    public overrideTestrunParameters: string;
+    public codeCoverageEnabled: boolean;
+    public videoCoverageEnabled: boolean;
+    public buildConfig: string;
+    public buildPlatform: string;
+    public testRunTitle: string;
+    public vsTestLocationMethod: string;
+    public vsTestVersion: string;
+    public vsTestLocation: string;
+    public vsTestVersionDetais: version.VSTestVersion;
+    public pathtoCustomTestAdapters: string;
+    public tiaConfig: TiaConfiguration;
+    public runInParallel: boolean;
+    public runTestsInIsolation: boolean;
+    public otherConsoleOptions: string;
 }
 
-export interface DtaTestConfigurations extends TestConfigurations {
-    testConfigurationMapping: string; // TODO : What is this?
-    customSlicingenabled: boolean;
-    dtaEnvironment: DtaEnvironment;
-    numberOfAgentsInPhase: number;
+export class DtaTestConfigurations extends TestConfigurations {
+    public testConfigurationMapping: string; // TODO : What is this?
+    public customSlicingenabled: boolean;
+    public dtaEnvironment: DtaEnvironment;
+    public numberOfAgentsInPhase: number;
+    public numberOfTestCasesPerSlice: number = 0;
 }
 
-export interface DtaEnvironment {
-    tfsCollectionUrl: string;
-    patToken: string;
-    environmentUri: string;
-    dtaHostLogFilePath: string;
-    agentName: string;
+export class DtaEnvironment {
+    public tfsCollectionUrl: string;
+    public patToken: string;
+    public environmentUri: string;
+    public dtaHostLogFilePath: string;
+    public agentName: string;
 }
 
-export interface VsTestConfigurations extends TestConfigurations {
-    publishRunAttachments: string;
-    vstestDiagFile: string;
-    ignoreVstestFailure: string;
+export class VsTestConfigurations extends TestConfigurations {
+    public publishRunAttachments: string;
+    public vstestDiagFile: string;
+    public ignoreVstestFailure: string;
 }
 
-export interface TiaConfiguration {
-    tiaEnabled: boolean;
-    tiaRebaseLimit: string;
-    tiaFilterPaths: string;
-    fileLevel: string;
-    sourcesDir: string;
-    runIdFile: string;
-    baseLineBuildIdFile: string;
-    useNewCollector: boolean;
-    isPrFlow: string;
-    context: string;
-    useTestCaseFilterInResponseFile: string;
-    userMapFile: string;
-    disableEnablingDataCollector: boolean;
+export class TiaConfiguration {
+    public tiaEnabled: boolean;
+    public tiaRebaseLimit: string;
+    public tiaFilterPaths: string;
+    public fileLevel: string;
+    public sourcesDir: string;
+    public runIdFile: string;
+    public baseLineBuildIdFile: string;
+    public useNewCollector: boolean;
+    public isPrFlow: string;
+    public context: string;
+    public useTestCaseFilterInResponseFile: string;
+    public userMapFile: string;
+    public disableEnablingDataCollector: boolean;
 }
