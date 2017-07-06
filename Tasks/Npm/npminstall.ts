5735c8b4063e2f032ba85731898a770b941d9311
Jake Kurlander <t-jakurl@microsoft.com>
2017-07-06 15:44:38 -0700

    reverting npminstall

--- Tasks/Npm/npminstall.ts
+++ Tasks/Npm/npminstall.ts
@@ -1,9 +1,9 @@
 import * as path from 'path';
+
 import * as tl from 'vsts-task-lib/task';
+
 import * as npmCustom from './npmcustom';
-import { NpmTaskInput} from './constants';
 
 export async function run(): Promise<void> {
-    let Command = 'install';
-    return npmCustom.run(Command);
+    return npmCustom.run('install');
 }
