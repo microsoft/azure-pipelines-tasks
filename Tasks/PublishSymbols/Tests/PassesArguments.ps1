76a0b64257fa369153dd4b26b65166ee0dee1102
Arun Kumar M <arun.m@microsoft.com>
2017-07-10 10:16:13 -0700

    Disable Shipping 2.0 PublishSymbol Task due to SymSrv http bug (#4745)
    * Disable Shipping 2.0 PublishSymbol Task due to SymSrv http bug
    
    * Revert "Merging Symbol Task with VSTS SymbolServer support (#4468)"
    
    This reverts commit 12966384ef93960a661c99c56342134914f3cb3c.

--- Tasks/PublishSymbols/Tests/PassesArguments.ps1
+++ Tasks/PublishSymbols/Tests/PassesArguments.ps1
@@ -12,7 +12,7 @@ $env:System_TeamProject = 'Some team project'
 $env:System_TeamFoundationCollectionUri = 'Some team foundation collection URI'
 Register-Mock Get-Date { $now }
 Register-Mock Get-VstsTaskVariable
-Register-Mock Find-VstsMatch { 'Some PDB file 1', 'Some PDB file 2' }
+Register-Mock Find-VstsFiles { 'Some PDB file 1', 'Some PDB file 2' }
 Register-Mock Invoke-IndexSources
 Register-Mock Invoke-PublishSymbols
 Register-Mock Invoke-UnpublishSymbols
@@ -20,8 +20,6 @@ foreach ($treatNotIndexedAsWarning in @($true, $false)) {
     Unregister-Mock Get-VstsInput
     Register-Mock Get-VstsInput { '123' } -- -Name 'SymbolsMaximumWaitTime' -Default '0' -AsInt
     Register-Mock Get-VstsInput { 'Some input symbols path' } -- -Name 'SymbolsPath'
-    Register-Mock Get-VstsInput { 'FileShare' } -ParametersEvaluator { $Name -eq 'SymbolServerType' }
-    Register-Mock Get-VstsInput { $true } -ParametersEvaluator { $Name -eq 'IndexSources' }
     Register-Mock Get-VstsInput { 'Some input search pattern' } -ParametersEvaluator { $Name -eq 'SearchPattern' }
     Register-Mock Get-VstsInput { 'Some input symbols product' } -ParametersEvaluator { $Name -eq 'SymbolsProduct' }
     Register-Mock Get-VstsInput { 'Some input symbols version' } -ParametersEvaluator { $Name -eq 'SymbolsVersion' }
@@ -35,7 +33,7 @@ foreach ($treatNotIndexedAsWarning in @($true, $false)) {
 
     # Assert.
     Assert-WasCalled Invoke-UnpublishSymbols -Times 0
-    Assert-WasCalled Find-VstsMatch -- -DefaultRoot 'Some input symbols folder' -Pattern 'Some input search pattern'
+    Assert-WasCalled Find-VstsFiles -- -LiteralDirectory 'Some input symbols folder' -LegacyPattern 'Some input search pattern'
     Assert-WasCalled Invoke-IndexSources -- -SymbolsFilePaths ('Some PDB file 1', 'Some PDB file 2') -TreatNotIndexedAsWarning: $treatNotIndexedAsWarning
     $semaphoreMessage = "Machine: $env:ComputerName, BuildUri: Some build URI, BuildNumber: Some build number, RepositoryName: Some build repository name, RepositoryUri: Some build repository URI, Team Project: Some team project, CollectionUri: Some team foundation collection URI at $($now.ToUniversalTime()) UTC"
     Assert-WasCalled Invoke-PublishSymbols -- -PdbFiles ('Some PDB file 1', 'Some PDB file 2') -Share 'Some input symbols path' -Product 'Some input symbols product' -Version 'Some input symbols version' -MaximumWaitTime ([timespan]::FromMinutes(123)) -ArtifactName 'Some symbols artifact name' -SemaphoreMessage $semaphoreMessage
--- Tasks/PublishSymbols/Tests/SetsFallbackMaxWaitTime.ps1
+++ Tasks/PublishSymbols/Tests/SetsFallbackMaxWaitTime.ps1
@@ -5,12 +5,10 @@ param()
 . $PSScriptRoot\..\..\..\Tests\lib\Initialize-Test.ps1
 $now = Get-Date
 Register-Mock Get-Date { $now }
-Register-Mock Find-VstsMatch { 'Some PDB file 1', 'Some PDB file 2' }
+Register-Mock Find-VstsFiles { 'Some PDB file 1', 'Some PDB file 2' }
 Register-Mock Invoke-IndexSources
 Register-Mock Invoke-PublishSymbols
 Register-Mock Get-VstsTaskVariable
-Register-Mock Get-VstsInput { 'FileShare' } -ParametersEvaluator { $Name -eq 'SymbolServerType' }
-Register-Mock Get-VstsInput { $true } -ParametersEvaluator { $Name -eq 'IndexSources' }
 Register-Mock Get-VstsInput { '-1' } -- -Name 'SymbolsMaximumWaitTime' -Default '0' -AsInt
 Register-Mock Get-VstsInput { 'Some input symbols path' } -- -Name 'SymbolsPath'
 Register-Mock Get-VstsInput { 'Some input search pattern' } -ParametersEvaluator { $Name -eq 'SearchPattern' }
