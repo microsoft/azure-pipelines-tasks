553b1ae39784f0686ab074f2562310a17e4702dc
Jitendra Kumar <jikuma@microsoft.com>
2017-07-14 17:04:54 +0530

    docker test case complete

--- Tasks/Docker/Tests/L0.ts
+++ Tasks/Docker/Tests/L0.ts
@@ -63,7 +63,6 @@ describe('Docker Suite', function() {
             process.env["__command__"] = "Push an image";
             tr.run();
             process.env["__command__"] = "Build an image";
-            console.log(tr.stdout);
 
             assert(tr.invokedToolCount == 1, 'should have invoked tool one times. actual: ' + tr.invokedToolCount);
             assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
@@ -88,16 +87,147 @@ describe('Docker Suite', function() {
             done();
         });
 
-    } else {
-        it('Runs successfully for linux template', (done:MochaDone) => {
-            let tp = path.join(__dirname, 'L0Linux.js');
+        it('Runs successfully for windows docker build with ACR', (done:MochaDone) => {
+            let tp = path.join(__dirname, 'L0Windows.js');
             let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+            process.env["__command__"] = "Build an image";
+            process.env["__container_type__"] = "Azure Container Registry";
             tr.run();
-            
-            assert(tr.invokedToolCount == 4, 'should have invoked tool four times. actual: ' + tr.invokedToolCount);
+            process.env["__container_type__"] = "Container Registry";
+
+            assert(tr.invokedToolCount == 1, 'should have invoked tool one times. actual: ' + tr.invokedToolCount);
             assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
             assert(tr.succeeded, 'task should have succeeded');
+            assert(tr.stdout.indexOf("[command]docker build -f F:\\dir1\\DockerFile -t test/test:2") != -1, "docker build should run");
+            console.log(tr.stderr);
+            done();
+        });
 
+        it('Runs successfully for windows docker build with ACR and qualify image name', (done:MochaDone) => {
+            let tp = path.join(__dirname, 'L0Windows.js');
+            let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+            process.env["__command__"] = "Build an image";
+            process.env["__container_type__"] = "Azure Container Registry";
+            process.env["__qualifyImageName__"] = "true";
+            tr.run();
+            process.env["__container_type__"] = "Container Registry";
+            process.env["__qualifyImageName__"] = "false";
+            console.log(tr.stdout);
+
+            assert(tr.invokedToolCount == 1, 'should have invoked tool one times. actual: ' + tr.invokedToolCount);
+            assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
+            assert(tr.succeeded, 'task should have succeeded');
+            assert(tr.stdout.indexOf("[command]docker build -f F:\\dir1\\DockerFile -t ajgtestacr1.azurecr.io/test/test:2") != -1, "docker build should run");
+            done();
+        });
+
+    } else {
+        it('Runs successfully for linux docker build', (done:MochaDone) => {
+            let tp = path.join(__dirname, 'L0Linux.js');
+            let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+            process.env["__command__"] = "Build an image";
+            tr.run();
+
+            assert(tr.invokedToolCount == 1, 'should have invoked tool one times. actual: ' + tr.invokedToolCount);
+            assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
+            assert(tr.succeeded, 'task should have succeeded');
+            assert(tr.stdout.indexOf("[command]docker build -f /tmp/tempdir/100/DockerFile -t test/test:2") != -1, "docker build should run");
+            console.log(tr.stderr);
+            done();
+        });
+
+        it('Runs successfully for linux docker build with latest tag', (done:MochaDone) => {
+            let tp = path.join(__dirname, 'L0Linux.js');
+            let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+            process.env["__command__"] = "Build an image";
+            process.env["__includeLatestTag__"] = "true";
+            tr.run();
+            process.env["__includeLatestTag__"] = "false";
+
+            assert(tr.invokedToolCount == 1, 'should have invoked tool one times. actual: ' + tr.invokedToolCount);
+            assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
+            assert(tr.succeeded, 'task should have succeeded');
+            assert(tr.stdout.indexOf("[command]docker build -f /tmp/tempdir/100/DockerFile -t test/test:2 -t test/test") != -1, "docker build should run");
+            console.log(tr.stderr);
+            done();
+        });
+
+        it('Runs successfully for linux docker run image', (done:MochaDone) => {
+            let tp = path.join(__dirname, 'L0Linux.js');
+            let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+            process.env["__command__"] = "Run an image";
+            tr.run();
+            process.env["__command__"] = "Build an image";
+
+            assert(tr.invokedToolCount == 1, 'should have invoked tool one times. actual: ' + tr.invokedToolCount);
+            assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
+            assert(tr.succeeded, 'task should have succeeded');
+            assert(tr.stdout.indexOf("[command]docker run --rm test/test:2") != -1, "docker run should run");
+            console.log(tr.stderr);
+            done();
+        });
+
+        it('Runs successfully for linux docker push image', (done:MochaDone) => {
+            let tp = path.join(__dirname, 'L0Linux.js');
+            let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+            process.env["__command__"] = "Push an image";
+            tr.run();
+            process.env["__command__"] = "Build an image";
+
+            assert(tr.invokedToolCount == 1, 'should have invoked tool one times. actual: ' + tr.invokedToolCount);
+            assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
+            assert(tr.succeeded, 'task should have succeeded');
+            assert(tr.stdout.indexOf("[command]docker push test/test:2") != -1, "docker push should run");
+            console.log(tr.stderr);
+            done();
+        });
+
+        it('Runs successfully for linux docker pull image', (done:MochaDone) => {
+            let tp = path.join(__dirname, 'L0Linux.js');
+            let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+            process.env["__command__"] = "Run a Docker command";
+            tr.run();
+            process.env["__command__"] = "Build an image";
+
+            assert(tr.invokedToolCount == 1, 'should have invoked tool one times. actual: ' + tr.invokedToolCount);
+            assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
+            assert(tr.succeeded, 'task should have succeeded');
+            assert(tr.stdout.indexOf("[command]docker pull test/test:2") != -1, "docker pull should run");
+            console.log(tr.stderr);
+            done();
+        });
+
+        it('Runs successfully for linux docker build with ACR', (done:MochaDone) => {
+            let tp = path.join(__dirname, 'L0Linux.js');
+            let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+            process.env["__command__"] = "Build an image";
+            process.env["__container_type__"] = "Azure Container Registry";
+            tr.run();
+            process.env["__container_type__"] = "Container Registry";
+
+            assert(tr.invokedToolCount == 1, 'should have invoked tool one times. actual: ' + tr.invokedToolCount);
+            assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
+            assert(tr.succeeded, 'task should have succeeded');
+            assert(tr.stdout.indexOf("[command]docker build -f /tmp/tempdir/100/DockerFile -t test/test:2") != -1, "docker build should run");
+            console.log(tr.stderr);
+            done();
+        });
+
+        it('Runs successfully for linux docker build with ACR and qualify image name', (done:MochaDone) => {
+            let tp = path.join(__dirname, 'L0Linux.js');
+            let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+            process.env["__command__"] = "Build an image";
+            process.env["__container_type__"] = "Azure Container Registry";
+            process.env["__qualifyImageName__"] = "true";
+            tr.run();
+            process.env["__container_type__"] = "Container Registry";
+            process.env["__qualifyImageName__"] = "false";
+            console.log(tr.stdout);
+
+            assert(tr.invokedToolCount == 1, 'should have invoked tool one times. actual: ' + tr.invokedToolCount);
+            assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
+            assert(tr.succeeded, 'task should have succeeded');
+            assert(tr.stdout.indexOf("[command]docker build -f /tmp/tempdir/100/DockerFile -t ajgtestacr1.azurecr.io/test/test:2") != -1, "docker build should run");
             done();
         });
     }
--- Tasks/Docker/Tests/L0Linux.ts
+++ Tasks/Docker/Tests/L0Linux.ts
@@ -2,15 +2,16 @@ import ma = require('vsts-task-lib/mock-answer');
 import tmrm = require('vsts-task-lib/mock-run');
 import path = require('path');
 
-const DefaultWorkingDirectory: string = "C:\\a\\w\\";
-let taskPath = path.join(__dirname, '..\\container.js');
+const DefaultWorkingDirectory: string = "/a/w";
+
+let taskPath = path.join(__dirname, '..', 'container.js');
 let tr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
 tr.setInput('containerregistrytype', process.env["__container_type__"] || 'Container Registry');
 tr.setInput('action', process.env["__command__"] || 'Build an image');
 tr.setInput('imageName', 'test/test:2');
 tr.setInput('dockerRegistryEndpoint', 'dockerhubendpoint');
-tr.setInput('dockerFile', 'F:\\dir1\\DockerFile');
+tr.setInput('dockerFile', '/tmp/tempdir/100/DockerFile');
 tr.setInput('customCommand', "pull test/test:2");
 tr.setInput('includeLatestTag', process.env["__includeLatestTag__"] || "false");
 tr.setInput('qualifyImageName', process.env["__qualifyImageName__"] || "false");
@@ -39,11 +40,11 @@ let a: any = <any>{
         "docker": true
     },
     "exist": {
-        "F:\\dir1\\DockerFile": true,
+        "/tmp/tempdir/100/DockerFile": true,
         "docker": true       
     },
     "exec": {
-        "docker build -f F:\\dir1\\DockerFile -t test/test:2" :{
+        "docker build -f /tmp/tempdir/100/DockerFile -t test/test:2" :{
             "code": 0,
             "stdout": "successfully build test/test:2 image"
         },
@@ -59,11 +60,11 @@ let a: any = <any>{
            "code": 0,
            "stdout": "successfully pulled test/test:2 image"
        },
-       "docker build -f F:\\dir1\\DockerFile -t test/test:2 -t test/test" :{
+       "docker build -f /tmp/tempdir/100/DockerFile -t test/test:2 -t test/test" :{
            "code": 0,
            "stdout": "successfully build test/test image with latest tag"
        },
-       "docker build -f F:\\dir1\\DockerFile -t ajgtestacr1.azurecr.io/test/test:2":{
+       "docker build -f /tmp/tempdir/100/DockerFile -t ajgtestacr1.azurecr.io/test/test:2":{
            "code": 0,
            "stdout": "successfully build ajgtestacr1.azurecr.io/test/test image with latest tag"
        }
