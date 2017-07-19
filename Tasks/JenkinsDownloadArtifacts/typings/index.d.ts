ffe5b35969c18f4288bd9223b611e40f3d8fd52b
Jeff Young <jeyou@microsoft.com>
2017-07-19 10:57:38 -0400

    Add typings for mocha

--- Tasks/JenkinsDownloadArtifacts/typings/index.d.ts
+++ Tasks/JenkinsDownloadArtifacts/typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/q/index.d.ts" />
 /// <reference path="globals/shelljs/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
