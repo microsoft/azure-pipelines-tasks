cf7c8f4cde58e27a6a419c74edda1d660164571a
Arun Kumar M <arun.m@microsoft.com>
2017-07-20 18:59:56 -0700

    Remove deletion todo

--- Tasks/PublishSymbols/PublishSymbols.ps1
+++ Tasks/PublishSymbols/PublishSymbols.ps1
@@ -45,13 +45,8 @@ try {
             [string]$SymbolsPath = Get-VstsInput -Name 'SymbolsPath' -Require
             [string]$TransactionId = Get-VstsInput -Name 'TransactionId' -Require
 
-            if ($SymbolsPath.StartsWith("https://")) {
-                Write-Host "Received url for FileShare deletion"
-            }
-            else {
-                Import-Module -Name $PSScriptRoot\PublishHelpers\PublishHelpers.psm1
-                Invoke-UnpublishSymbols -Share $SymbolsPath -TransactionId $TransactionId -MaximumWaitTime $SymbolsMaximumWaitTime -SemaphoreMessage $SemaphoreMessage
-            }
+            Import-Module -Name $PSScriptRoot\PublishHelpers\PublishHelpers.psm1
+            Invoke-UnpublishSymbols -Share $SymbolsPath -TransactionId $TransactionId -MaximumWaitTime $SymbolsMaximumWaitTime -SemaphoreMessage $SemaphoreMessage
             return
         }
 
@@ -137,7 +132,7 @@ try {
         }
 
         [string] $encodedRequestName = [System.Web.HttpUtility]::UrlEncode($RequestName)
-        [string] $requestUrl = "$SymbolServiceUri/DefaultCollection/_apis/Symbol/requests?requestName=$encodedRequestName"
+        [string] $requestUrl = "$SymbolServiceUri/_apis/Symbol/requests?requestName=$encodedRequestName"
         Write-VstsAssociateArtifact -Name "$RequestName" -Path $requestUrl -Type "SymbolRequest" -Properties @{}
 
         & "$PSScriptRoot\Publish-Symbols.ps1" -SymbolServiceUri $SymbolServiceUri -RequestName $RequestName -SourcePath $SourcePath -SourcePathListFileName $tmpFileName -PersonalAccessToken $PersonalAccessToken -ExpirationInDays 3653
