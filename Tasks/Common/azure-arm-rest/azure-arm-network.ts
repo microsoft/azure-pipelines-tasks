9e373fe3c622a47163906c55fbce16a2608f8281
Ajay Kumar Yadav <ajya@microsoft.com>
2017-07-21 15:04:38 +0530

    Users/ajya/azurestacktasksupport (#4670)
    * AzureStackChanges
    
    * Azure stack tasks support
    
    * added support of Azure Stack to Azure PS, AFC and Azure RG tasks
    
    * Make-options reverted
    
    * Added entry for azure stack module
    
    * Reverted changes for Azure Rm and App Service task
    
    * Reverted app service changes
    
    * Revert Azure Stack REST URL changes
    
    * Revert Azure SQL changes
    
    * Review comment corrections
    
    * Review comment corrections
    
    * Review comment - Added logic for Dns suffix
    
    * used substring instead of remove method
    
    * Review comment correction
    
    * resolved merge conflicts
    
    * Rebuild after resolving conflicts
    
    * Added version check for Azure RG task
    
    * Review comments and  Few corrections
    
    * L0 for azure stack for Azure Rg task
    
    * Removed duplicated code

--- Tasks/Common/azure-arm-rest/azure-arm-network.ts
+++ Tasks/Common/azure-arm-rest/azure-arm-network.ts
@@ -15,7 +15,7 @@ export class NetworkManagementClient extends azureServiceClient.ServiceClient {
     constructor(credentials: msRestAzure.ApplicationTokenCredentials, subscriptionId, baseUri?: any, options?: any) {
         super(credentials, subscriptionId);
 
-        this.apiVersion = '2016-09-01';
+        this.apiVersion = (credentials.isAzureStackEnvironment) ? '2015-06-15' : '2016-09-01' ;
         this.acceptLanguage = 'en-US';
         this.generateClientRequestId = true;
 
