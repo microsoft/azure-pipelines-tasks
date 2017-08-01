8ef20e16666fc05206c0c3f5342aec54eeb2dc50
Ajay Kumar Yadav <ajya@microsoft.com>
2017-08-01 18:49:38 +0530

    for VS17 removed dependency on registry (#4915)

--- Tasks/SqlAzureDacpacDeployment/FindSqlPackagePath.ps1
+++ Tasks/SqlAzureDacpacDeployment/FindSqlPackagePath.ps1
@@ -278,6 +278,26 @@ function Locate-HighestVersionSqlPackageWithDacMsi()
     return $null, 0
 }
 
+function Locate-SqlPackageInVS_15_0()
+{
+    $vs15 = Get-VisualStudio_15_0
+    if ($vs15 -and $vs15.installationPath) {
+        # End with "\" for consistency with old ShellFolder values.
+        $shellFolder15 = $vs15.installationPath.TrimEnd('\'[0]) + "\"
+
+        # Test for the DAC directory.
+        $dacParentDir = [System.IO.Path]::Combine($shellFolder15, 'Common7', 'IDE', 'Extensions', 'Microsoft', 'SQLDB', 'DAC')
+        $dacInstallPath, $dacInstallVersion = Get-LatestVersionSqlPackageInDacDirectory -dacParentDir $dacParentDir
+        
+        if($dacInstallPath)
+        {
+            return $dacInstallPath, $dacInstallVersion
+        }
+    }
+
+    return $null, 0
+}
+
 function Locate-SqlPackageInVS([string] $version)
 {
     $vsRegKeyForVersion = "SOFTWARE", "Microsoft", "VisualStudio", $version -join [System.IO.Path]::DirectorySeparatorChar
@@ -323,6 +343,15 @@ function Locate-SqlPackageInVS([string] $version)
 
 function Locate-HighestVersionSqlPackageInVS()
 {
+    # Locate SqlPackage.exe in VS 2017 or above
+    $dacFullPath, $dacVersion = Locate-SqlPackageInVS2017 
+
+    if ($dacFullPath -ne $null)
+    {
+        return $dacFullPath, $dacVersion
+    }
+
+    #Locate SqlPackage.exe in older version 
     $vsRegKey = "HKLM:", "SOFTWARE", "Wow6432Node", "Microsoft", "VisualStudio" -join [System.IO.Path]::DirectorySeparatorChar
     $vsRegKey64 = "HKLM:", "SOFTWARE", "Microsoft", "VisualStudio" -join [System.IO.Path]::DirectorySeparatorChar
 
@@ -357,6 +386,58 @@ function Locate-HighestVersionSqlPackageInVS()
     return $null, 0
 }
 
+function Get-VisualStudio_15_0 {
+    [CmdletBinding()]
+    param()
+
+    $visualStudioInstallDir = $null
+    try {
+        # Query for the latest 15.* version.
+        #
+        # Note, the capability is registered as VisualStudio_15.0, however the actual version
+        # may be something like 15.2.
+        Write-Verbose "Getting latest Visual Studio 15 setup instance."
+        $output = New-Object System.Text.StringBuilder
+        Invoke-VstsTool -FileName "$PSScriptRoot\vswhere.exe" -Arguments "-version [15.0,16.0) -latest -format json" -RequireExitCodeZero 2>&1 |
+            ForEach-Object {
+                if ($_ -is [System.Management.Automation.ErrorRecord]) {
+                    Write-Verbose "STDERR: $($_.Exception.Message)"
+                }
+                else {
+                    Write-Verbose $_
+                    $null = $output.AppendLine($_)
+                }
+            }
+        $visualStudioInstallDir = (ConvertFrom-Json -InputObject $output.ToString()) |
+            Select-Object -First 1
+        if (!$visualStudioInstallDir) {
+            # Query for the latest 15.* BuildTools.
+            #
+            # Note, whereas VS 15.x version number is always 15.0.*, BuildTools does not follow the
+            # the same scheme. It appears to follow the 15.<UPDATE_NUMBER>.* versioning scheme.
+            Write-Verbose "Getting latest BuildTools 15 setup instance."
+            $output = New-Object System.Text.StringBuilder
+            Invoke-VstsTool -FileName "$PSScriptRoot\vswhere.exe" -Arguments "-version [15.0,16.0) -products Microsoft.VisualStudio.Product.BuildTools -latest -format json" -RequireExitCodeZero 2>&1 |
+                ForEach-Object {
+                    if ($_ -is [System.Management.Automation.ErrorRecord]) {
+                        Write-Verbose "STDERR: $($_.Exception.Message)"
+                    }
+                    else {
+                        Write-Verbose $_
+                        $null = $output.AppendLine($_)
+                    }
+                }
+            $visualStudioInstallDir  = (ConvertFrom-Json -InputObject $output.ToString()) |
+                Select-Object -First 1
+        }
+    } catch {
+        Write-Verbose ($_ | Out-String)
+        $visualStudioInstallDir = $null
+    }
+    
+    return $visualStudioInstallDir
+}
+
 function Get-SQLPackagePath
 {
  
@@ -364,5 +445,3 @@ function Get-SQLPackagePath
   
     return $sqlPackage
 }
-
-
