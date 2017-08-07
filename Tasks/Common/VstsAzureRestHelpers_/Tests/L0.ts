13fc2d66409a59649096d7c3f99f17482580bc04
Ajay Kumar Yadav <ajya@microsoft.com>
2017-08-07 18:53:09 +0530

    Added L0 tests for PS based rest helper - Azure Stack

---
+++ Tasks/Common/VstsAzureRestHelpers_/Tests/L0.ts
@@ -0,0 +1,37 @@
+/// <reference path="../../../../definitions/mocha.d.ts"/>
+/// <reference path="../../../../definitions/node.d.ts"/>
+/// <reference path="../../../../definitions/Q.d.ts"/>
+
+import Q = require('q');
+import assert = require('assert');
+import path = require('path');
+var psm = require('../../../../Tests/lib/psRunner');
+var psr = null;
+
+describe('Common-VstsAzureHelpers_ Suite', function () {
+    this.timeout(20000);
+
+    before((done) => {
+        if (psm.testSupported()) {
+            psr = new psm.PSRunner();
+            psr.start();
+        }
+
+        done();
+    });
+
+    after(function () {
+        if (psr) {
+            psr.kill();
+        }
+    });
+
+    if (psm.testSupported()) {
+        it('Add-AzureStackDependencyData to populate Azure Stack endpoint data.', (done) => {
+            psr.run(path.join(__dirname, 'Populate-AzureStackDependencyData.ps1'), done);
+        })
+        it('Verify if Get-AzureActiveDirectoryResourceId returns correct URL.', (done) => {
+            psr.run(path.join(__dirname, 'Get-AzureActiveDirectoryResourceId.ps1'), done);
+        })
+    }
+});
