e7eba33fc633f5d2d71f319cab2dc6eb3c606965
Bishal Prasad <biprasad@microsoft.com>
2017-08-16 11:58:48 +0530

    Remove custom script extension and azure storagenode sdk usage. This should be added back next sprint once OSS scanning is done.

--- Tasks/AzureVmssDeployment/blobservice.ts
+++ Tasks/AzureVmssDeployment/blobservice.ts
@@ -1,4 +1,3 @@
-import * as azureStorage from 'azure-storage';
 import * as path from 'path';
 import * as util from 'util'
 import * as tl from 'vsts-task-lib/task';
@@ -16,41 +15,7 @@ export class BlobService {
      * uploads a local folder content to blob container
      */
     public async uploadBlobs(source: string, destUrlOrContainer: string): Promise<void> {
-        let destUrl = destUrlOrContainer;
-        if(!this._isUrl(destUrlOrContainer)) {
-            destUrl = util.format("https://%s.blob.core.windows.net/%s", this._storageAccountName, destUrlOrContainer);
-        }
-
-        if(tl.osType().match(/^Win/)) {
-            //let azCopy = new azCopyProvider.AzCopyWindows();
-            //return azCopy.uploadBlobs(source, destUrl, this._storageAccessKey);
-            var self = this;
-            var blobSvc = azureStorage.createBlobService(this._storageAccountName, this._storageAccessKey);
-            blobSvc.createContainerIfNotExists(destUrlOrContainer, function(error, result, response){
-            if(!error){
-                // Container exists and is private
-                    let fileList: string[] = tl.findMatch(source, "**/*.*");
-                    let baseFolder = Date.now().toString();
-
-                    let fileUris: string[] = [];
-                    fileList.forEach((filePath) => {
-                        let fileName = path.basename(filePath);
-                        let relativePath = path.relative(source, filePath);
-                        let normalizedRelativePath = self.normalizeRelativePath(relativePath);
-                        //blobSvc.createBlockBlobFromLocalFile(destUrlOrContainer, baseFolder + "/" + normalizedRelativePath, filePath, function(error, result, response){
-                        blobSvc.createBlockBlobFromLocalFile(destUrlOrContainer, normalizedRelativePath, filePath, function(error, result, response){
-                            if(error) {
-                                console.log(error.message);
-                            }
-                        });
-
-                    });
-                }
-            });
-        } else {
-            // use REST APIs to upload
-            throw "Not implemented!!";
-        }
+        throw "Not implemented!!";
     }
 
     private _isUrl(str: string): boolean {
