1c97273fd382f09091c6e9f3a58dad635bee43f3
😊Nitin Gurram😊 <nigurr@microsoft.com>
2017-08-21 10:19:01 +0530

    Adding TestExecution Zip changes

--- Tasks/DeployVisualStudioTestAgent/CreateOfflineTestAgent.ps1
+++ Tasks/DeployVisualStudioTestAgent/CreateOfflineTestAgent.ps1
@@ -2,12 +2,16 @@ function Download-TestAgentBootStrapper ($Uri) {
     Invoke-WebRequest -Uri $uri -OutFile "$PSScriptRoot\vs_TestAgent.exe"
 }
 
+function Download-TestExecutionEngine ($Uri) {
+    Invoke-WebRequest -Uri $uri -OutFile "$PSScriptRoot\TestExecution.zip"
+}
+
 function Create-OfflineCache ($language) {
     $p = Start-Process -FilePath "$PSScriptRoot\vs_TestAgent.exe" -ArgumentList "--layout $PSScriptRoot\TestAgent2017\ --lang $language" -PassThru -Wait
     $p.WaitForExit()
 }
 
-if($PSVersionTable.PSVersion.Major -gt 5){
+if ($PSVersionTable.PSVersion.Major -gt 5) {
     Write-Error "Please install Powershell 5 or more https://docs.microsoft.com/en-us/powershell/wmf/5.0/requirements"
 }
 
@@ -31,6 +35,17 @@ Write-Host "Completed prepraring the offline cache"
 # 4. Compress the archive
 Write-Host "Archiving the Test agent offline cache"
 Compress-Archive -Path "$PSScriptRoot\TestAgent2017\*" -CompressionLevel Fastest -DestinationPath "$PSScriptRoot\TestAgent.zip"
-Write-Host "Completed archiving the Test agent offline.
-            Please use this archive $PSScriptRoot\TestAgent.zip for offline Test agent installation.
-            For more info: https://blogs.msdn.microsoft.com/devops/2017/05/05/using-visual-studio-agent-deployment-task-on-machines-not-connected-to-the-internet/"
+Write-Host "Completed archiving the Test agent offline."
+
+# 5. Download the Test Execution engine
+if (Test-Path "$PSScriptRoot\TestExecution.zip") {
+    Write-Host "Skipping downloading of Test execution as it's already available."
+}
+else {
+    Write-Host "Downloading the Test execution engine"
+    Download-TestExecutionEngine -Uri "https://testexecution.blob.core.windows.net/testexecution/4071087/TestExecution.zip"
+    Write-Host "Completed downloading the Test execution engine"
+}
+
+Write-Host "Please use these archive(s) $PSScriptRoot\TestAgent.zip and $PSScriptRoot\TestExecution.zip for offline Test agent installation.
+    For more info: https://blogs.msdn.microsoft.com/devops/2017/05/05/using-visual-studio-agent-deployment-task-on-machines-not-connected-to-the-internet/"
