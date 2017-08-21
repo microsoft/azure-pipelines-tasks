7951a1f52a290721c639ffcdb6cc16cd0f7d8a44
😊Nitin Gurram😊 <nigurr@microsoft.com>
2017-08-21 12:34:00 +0530

    Adding test to validate make.json with script

---
+++ Tests-Legacy/L0/DeployVisualStudioTestAgent/VerifyTestExecutionEnginePackage.ps1
@@ -0,0 +1,10 @@
+[cmdletbinding()]
+param()
+
+. $PSScriptRoot\..\..\lib\Initialize-Test.ps1
+
+$json = Get-Content "$PSScriptRoot\..\..\..\..\Tasks\DeployVisualStudioTestAgent\make.json" | ConvertFrom-Json
+$testExecutionUrl = $json.externals.files.url
+
+$isSameVersion = Get-Content "$PSScriptRoot\..\..\..\..\Tasks\DeployVisualStudioTestAgent\CreateOfflineTestAgent.ps1" | Select-String $testExecutionUrl -quiet
+Assert-AreEqual -Expected 1 -Actual $isSameVersion.Count -Message "$testExecutionUrl is not found in CreateOfflineTestAgent.ps1"
