817e48c491264a7695f09f2fb66622a64bb7dc74
Nitin Gurram <nigurr@microsoft.com>
2017-08-30 14:06:41 +0530

    Fixed running tests under non-admin account for UI tests (#5089)
    * Fixed running tests under non-admin account for UI tests
    
    * Update TestAgentConfiguration.ps1
    
    * Fixing failing test case in CI

--- Tests-Legacy/L0/DeployVisualStudioTestAgent/VerifyTestExecutionEnginePackage.ps1
+++ Tests-Legacy/L0/DeployVisualStudioTestAgent/VerifyTestExecutionEnginePackage.ps1
@@ -3,7 +3,7 @@ param()
 
 . $PSScriptRoot\..\..\lib\Initialize-Test.ps1
 
-$json = Get-Content "$PSScriptRoot\..\..\..\..\Tasks\DeployVisualStudioTestAgent\make.json" | ConvertFrom-Json
+$json = (( Get-Content "$PSScriptRoot\..\..\..\..\Tasks\DeployVisualStudioTestAgent\make.json") -Join " ") | ConvertFrom-Json
 $testExecutionUrl = $json.externals.files.url
 
 $isSameVersion = Get-Content "$PSScriptRoot\..\..\..\..\Tasks\DeployVisualStudioTestAgent\CreateOfflineTestAgent.ps1" | Select-String $testExecutionUrl -quiet
