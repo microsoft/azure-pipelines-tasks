3b32e2b520048c5376f2a8179fd5b30df7dd699d
Lucas Killgore <Lucas.Killgore@microsoft.com>
2017-08-31 08:42:16 -0400

    #1062865: Xcode removed remnants of xcrun

--- Tests-Legacy/L0/Xcode/_suite.ts
+++ Tests-Legacy/L0/Xcode/_suite.ts
@@ -32,7 +32,7 @@ describe('Xcode Suite', function() {
         tr.setInput('sdk', '$(SDK)');
         tr.setInput('xcWorkspacePath', '**/*.xcodeproj/*.xcworkspace');
         tr.setInput('scheme', '');
-        tr.setInput('packageApp', 'true');
+        tr.setInput('packageApp', 'false');
         tr.setInput('signMethod', 'file');
         tr.setInput('p12', '/user/build');
         tr.setInput('p12pwd', '');
@@ -58,11 +58,7 @@ describe('Xcode Suite', function() {
                     'SYMROOT=/user/build/output/$(SDK)/$(Configuration)/build.sym ' +
                     'SHARED_PRECOMPS_DIR=/user/build/output/$(SDK)/$(Configuration)/build.pch'),
                 'xcodebuild for building the ios project/workspace should have been run.');
-            assert(tr.ran('/home/bin/xcrun -sdk $(SDK) PackageApplication ' +
-                    '-v /user/build/output/$(SDK)/$(Configuration)/build.sym/Release.iphoneos/fun.app ' +
-                    '-o /user/build/output/$(SDK)/$(Configuration)/build.sym/Release.iphoneos/fun.ipa'),
-                "xcrun to package the app and generate an .ipa should have been run.");
-            assert(tr.invokedToolCount == 3, 'should have xcodebuild for version, xcodebuild for build and xcrun for packaging');
+            assert(tr.invokedToolCount == 2, 'should have xcodebuild for version, xcodebuild for build and xcrun for packaging');
             assert(tr.resultWasSet, 'task should have set a result');
             assert(tr.stderr.length == 0, 'should not have written to stderr');
             assert(tr.succeeded, 'task should have succeeded');
@@ -82,7 +78,7 @@ describe('Xcode Suite', function() {
         tr.setInput('sdk', '$(SDK)');
         tr.setInput('xcWorkspacePath', '/user/build');
         tr.setInput('scheme', '');
-        tr.setInput('packageApp', 'true');
+        tr.setInput('packageApp', 'false');
         tr.setInput('signMethod', 'file');
         tr.setInput('p12', '/user/build');
         tr.setInput('p12pwd', '');
@@ -108,13 +104,9 @@ describe('Xcode Suite', function() {
                         'SHARED_PRECOMPS_DIR=/user/build/output/$(SDK)/$(Configuration)/build.pch ' +
                         '-project test.xcodeproj'),
                     'xcodebuild for building the ios project should have been run.');
-                assert(tr.ran('/home/bin/xcrun -sdk $(SDK) PackageApplication ' +
-                        '-v /user/build/output/$(SDK)/$(Configuration)/build.sym/Release.iphoneos/fun.app ' +
-                        '-o /user/build/output/$(SDK)/$(Configuration)/build.sym/Release.iphoneos/fun.ipa'),
-                    "xcrun to package the app and generate an .ipa should have been run.");
-                assert(tr.invokedToolCount == 3, 'should have xcodebuild for version, xcodebuild for build and xcrun for packaging');
+                assert(tr.invokedToolCount == 2, 'should have xcodebuild for version, xcodebuild for build and xcrun for packaging');
                 assert(tr.resultWasSet, 'task should have set a result');
-                assert(tr.stderr.length == 0, 'should not have written to stderr');
+                assert(tr.stderr.length == 0, 'should not have written to stderr std=' + tr.stdout + ' err=' + tr.stderr);
                 assert(tr.succeeded, 'task should have succeeded');
                 done();
             })
