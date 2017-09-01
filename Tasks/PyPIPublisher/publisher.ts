df27229c2d1686dfb020ba272b3f357bc0b0b95d
vinod953 <connectwithvinod@gmail.com>
2017-09-01 18:05:27 +0530

    Review comments inplace

--- Tasks/PyPIPublisher/publisher.ts
+++ Tasks/PyPIPublisher/publisher.ts
@@ -1,34 +1,36 @@
 var tl = require('vsts-task-lib');
+var os = require('os');
+var path = require('path');
+var util = require("util");
 var workingDirectory = tl.getInput('wd', true);
 var serviceEndpointId = tl.getInput('serviceEndpoint', true);
-var os = require('os');
-var shell = require('shelljs');
 var homedir = os.homedir();
+var pypircFilePath = path.join(homedir,".pypirc");
+var setupFilePath = path.join(workingDirectory, "setup.py");
 
 //Generic service endpoint
-var auth = tl.getEndpointAuthorization(serviceEndpointId, false);
 var pythonServer = tl.getEndpointUrl(serviceEndpointId, false);
-var username = auth['parameters']['username'];
-var password = auth['parameters']['password'];
+var username = tl.getEndpointAuthorizationParameter(serviceEndpointId, 'username', false);
+var password = tl.getEndpointAuthorizationParameter(serviceEndpointId, 'password', false);
 
 //Create .pypirc file
-var text = "[distutils] \nindex-servers =\n    pypi \n[pypi] \nrepository=" + pythonServer + " \nusername=" + username + " \npassword=" + password;
-tl.writeFile(homedir + '\\.pypirc', text, 'utf8');
+var text = util.format("[distutils] \nindex-servers =\n    pypi \n[pypi] \nrepository=%s \nusername=%s \npassword=%s", pythonServer, username, password);
+tl.writeFile(pypircFilePath, text, 'utf8');
 
 //PyPI upload command
-var cmdToRun = "python " + workingDirectory + "\\setup.py sdist upload -r pypi";
-
-shell.exec(cmdToRun, (err, stdout, stderr) => {
-    try {
-        if (err) {
-            tl.setResult(tl.TaskResult.Failed, stderr);
-        }
-        else {
-            tl.setResult(tl.TaskResult.Succeeded, "Upload Successful");
-        }
-    }
-    finally {
+var cmdToRun = util.format(" %s sdist upload -r pypi", setupFilePath);
+try {
+    var pythonTool = tl.tool(tl.which('python', true));
+    pythonTool.line(cmdToRun);
+    pythonTool.exec().then(function () {
+        tl.setResult(tl.TaskResult.Succeeded, "Upload Successful");
+    }).fail(function (err) {
+        tl.setResult(tl.TaskResult.Failed, err);
+    }).finally(function (err) {
         //Delete .pypirc file
-        tl.rmRF(homedir + '\\.pypirc');
-    }
-});
+        tl.rmRF(pypircFilePath);
+    });
+}
+catch (err) {
+    tl.setResult(tl.TaskResult.Failed, err);
+}
