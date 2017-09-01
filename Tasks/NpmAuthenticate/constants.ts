5da0cc1059d5ef1942f646a1d5b7a6081e8fb08f
Jake Kurlander <t-jakurl@microsoft.com>
2017-09-01 14:13:48 -0700

    Revert "Adding npm authenticate task, npm-common, and refactoring npm task (#5027)"
    This reverts commit d93ba1d4a41bd084dce0582dcb4f2bfd55abc6c0.

--- Tasks/NpmAuthenticate/constants.ts
+++
@@ -1,5 +0,0 @@
-export class NpmAuthenticateTaskInput  {
-    public static WorkingFile: string = 'workingFile';
-    public static CustomRegistry: string = 'customRegistry';
-    public static CustomEndpoint: string = 'customEndpoint';
-}
