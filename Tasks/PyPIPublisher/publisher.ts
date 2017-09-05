cc955f969348bae84cd79302bec34ab1fd7bd25f
vinod953 <connectwithvinod@gmail.com>
2017-09-05 19:07:33 +0530

    Adding dependency over Twine for PyPI Upload

--- Tasks/PyPIPublisher/publisher.ts
+++ Tasks/PyPIPublisher/publisher.ts
@@ -5,8 +5,9 @@ var util = require("util");
 var workingDirectory = tl.getInput('wd', true);
 var serviceEndpointId = tl.getInput('serviceEndpoint', true);
 var homedir = os.homedir();
-var pypircFilePath = path.join(homedir,".pypirc");
+var pypircFilePath = path.join(homedir, ".pypirc");
 var setupFilePath = path.join(workingDirectory, "setup.py");
+var distDirectoryPath = path.join(path.normalize(path.join(workingDirectory, "..")), "dist")+"/*";
 
 //Generic service endpoint
 var pythonServer = tl.getEndpointUrl(serviceEndpointId, false);
@@ -17,20 +18,24 @@ var password = tl.getEndpointAuthorizationParameter(serviceEndpointId, 'password
 var text = util.format("[distutils] \nindex-servers =\n    pypi \n[pypi] \nrepository=%s \nusername=%s \npassword=%s", pythonServer, username, password);
 tl.writeFile(pypircFilePath, text, 'utf8');
 
-//PyPI upload command
-var cmdToRun = util.format(" %s sdist upload -r pypi", setupFilePath);
-try {
-    var pythonTool = tl.tool(tl.which('python', true));
-    pythonTool.line(cmdToRun);
-    pythonTool.exec().then(function () {
-        tl.setResult(tl.TaskResult.Succeeded, "Upload Successful");
-    }).fail(function (err) {
-        tl.setResult(tl.TaskResult.Failed, err);
-    }).finally(function (err) {
-        //Delete .pypirc file
-        tl.rmRF(pypircFilePath);
-    });
+//PyPI upload
+try{
+    executePythonTool("-m pip install wheel twine --user");
+    executePythonTool(util.format("%s sdist bdist_wheel", setupFilePath));    
+    executePythonTool(util.format("-m twine upload %s", distDirectoryPath));    
 }
-catch (err) {
+catch(err){
     tl.setResult(tl.TaskResult.Failed, err);
+}
+finally{
+    tl.setResult(tl.TaskResult.Succeeded, "Upload Successful");
+    //Delete .pypirc file
+    tl.rmRF(pypircFilePath);
+};
+
+function executePythonTool(lineToAdd){
+    var result = tl.tool(tl.which('python', true)).line(lineToAdd).execSync();
+    if(result.code != 0) {
+        throw new Error(result.error ? result.error.message : result.stderr);
+    }
 }
