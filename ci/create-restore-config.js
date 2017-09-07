f2c71c4f346dba68e5dbaf56c0cf14f386aff247
Eric Sciple <ersciple@microsoft.com>
2017-09-07 00:26:07 -0400

    Relax aggregate commit constraint for master

--- ci/create-restore-config.js
+++ ci/create-restore-config.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var path = require('path');
+var syncRequest = require('sync-request');
 var util = require('./ci-util');
 
 // get branch/commit info
@@ -16,6 +17,30 @@ Object.keys(refs.releases)
         // skip the current release (already covered by current build)
         if (release == refs.head.release) {
             return;
+        }
+
+        var commit = refs.releases[release].commit;
+
+        // relax the commit constraint to 1 hour when building the aggregate package for master
+        if (refs.head.branch == 'refs/heads/master') {
+            while (true) {
+                var packageUrl = process.env.PACKAGE_URL_FORMAT
+                    .replace('{FEED_NAME}', encodeURIComponent(process.env.TASK_MILESTONE_FEED_NAME))
+                    .replace('{PACKAGE_NAME}', encodeURIComponent('vsts-tasks-milestone'))
+                    .replace('{VERSION}', encodeURIComponent(`1.0.0-m${release}-${commit}`));
+                console.log(`Checking whether package exists: ${packageUrl}`);
+                var options = { headers: { Authorization: `Bearer ${process.env.SYSTEM_ACCESSTOKEN}` } };
+                var response = syncRequest('GET', packageUrl, options);
+                if (response.statusCode == 200) {
+                    break;
+                }
+
+                if (response.statusCode == 404) {
+                    // get the time of the commit. if it is within an hour, get the parent commit.
+                }
+
+                throw new Error(`Unexpected HTTP response '${response.statusCode}'`);
+            }
         }
 
         var commit = refs.releases[release].commit;
