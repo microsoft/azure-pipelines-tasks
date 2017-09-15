1c83272da876881053f55c9035eddfc5abff6e1f
Sachin Mangla <sachinma@microsoft.com>
2017-09-15 17:38:12 +0530

    Fixing typo

--- Tasks/Common/azure-arm-rest/webClient.ts
+++ Tasks/Common/azure-arm-rest/webClient.ts
@@ -59,7 +59,7 @@ export async function sendRequest(request: WebRequest, options?: WebRequestOptio
 async function sendReqeustInternal(request: WebRequest): Promise<WebResponse> {
     tl.debug(util.format("[%s]%s", request.method, request.uri));
     var response: httpClient.HttpClientResponse = await httpCallbackClient.request(request.method, request.uri, request.body, request.headers);
-    return await this.toWebResponse(response);
+    return await toWebResponse(response);
 }
 
 async function toWebResponse(response: httpClient.HttpClientResponse): Promise<WebResponse> {
