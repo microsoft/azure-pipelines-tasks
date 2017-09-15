d226e225e69338c179bdf087d82bd7158e456193
Madhuri Gummalla <madhurig@users.noreply.github.com>
2017-09-15 13:07:53 -0400

    Users/madhurig/androidsigning (#5330)
    * Update android keyfile to be stored and retrieved from VSTS securely
    
    * Move existing L0 tests to new framework
    
    * update task version
    
    * Delete legacy tests, ported over to new framework
    
    * Add L0 test for download from secure file
    
    * Clean up keysotre file after signing
    
    * PR feedback
    
    * fix L0 test failure

--- Tasks/AndroidSigning/Tests/L0AndroidSignAlignMultipleFiles.ts
+++ Tasks/AndroidSigning/Tests/L0AndroidSignAlignMultipleFiles.ts
@@ -1,8 +1,24 @@
-{
-    "getVariable": {
-        "JAVA_HOME": "/fake/java/home",
-        "ANDROID_HOME": "/fake/android/home"
-    },
+import ma = require('vsts-task-lib/mock-answer');
+import tmrm = require('vsts-task-lib/mock-run');
+import path = require('path');
+
+let taskPath = path.join(__dirname, '..', 'androidsigning.js');
+let tr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+
+tr.setInput('files', '/some/path/*.apk');
+tr.setInput('jarsign', 'true');
+tr.setInput('keystoreFile', '/some/store');
+tr.setInput('keystorePass', 'pass1');
+tr.setInput('keystoreAlias', 'somealias');
+tr.setInput('keyPass', 'pass2');
+tr.setInput('zipalign', 'true');
+
+process.env['VSTS_TASKVARIABLE_KEYSTORE_FILE_PATH'] = '/some/store';
+process.env['JAVA_HOME'] = '/fake/java/home';
+process.env['ANDROID_HOME'] = '/fake/android/home';
+
+// provide answers for task mock
+let a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
     "exec": {
         "/fake/java/home/bin/jarsigner -keystore /some/store -storepass pass1 -keypass pass2 -signedjar /some/path/a.apk /some/path/a.apk.unsigned somealias": {
             "code": 0,
@@ -12,7 +28,7 @@
             "code": 0,
             "stdout": "jarsigner output here"
         },
-         "/fake/android/home/sdk1/zipalign -v 4 /some/path/a.apk.unaligned /some/path/a.apk": {
+        "/fake/android/home/sdk1/zipalign -v 4 /some/path/a.apk.unaligned /some/path/a.apk": {
             "code": 0,
             "stdout": "zipalign output here"
         },
@@ -25,25 +41,23 @@
         "/some/path/a.apk": true,
         "/some/path/b.apk": true
     },
-    "find": {
-        "/some/path": [
-            "/some/path/a.apk",
-            "/some/path/b.apk",
-            "/some/path/notapkfile"
-        ],
-        "/fake/android/home": [
-            "/fake/android/home/sdk1",
-            "/fake/android/home/sdk2"
-        ]
-    },
-    "match": {
+    "findMatch": {
         "/some/path/*.apk": [
             "/some/path/a.apk",
             "/some/path/b.apk"
         ],
+        "/fake/android/home": [
+            "/fake/android/home/sdk1",
+            "/fake/android/home/sdk2"
+        ],
         "zipalign*": [
             "/fake/android/home/sdk1/zipalign",
-            "/fake/android/home/sdk2/zipalign"       
+            "/fake/android/home/sdk2/zipalign"
         ]
     }
 }
+
+tr.setAnswers(a);
+
+tr.run();
+
