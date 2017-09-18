0a7f3aa7bad8dfa6d5488f5a21576c6af5d0acf4
Emanuel Quintero <jahquin@microsoft.com>
2017-09-18 10:10:05 -0700

    Placing pack utilities in common location (#5347)

--- Tasks/DotNetCoreCLI/Common/utility.ts
+++ Tasks/DotNetCoreCLI/Common/utility.ts
@@ -12,8 +12,3 @@ export function getProjectFiles(projectPattern: string[]): string[] {
 
     return projectFiles;
 }
-
-export function getUtcDateString(): string {
-    let now: Date = new Date();
-    return `${now.getFullYear()}${now.getUTCMonth()}${now.getUTCDate()}-${now.getUTCHours()}${now.getUTCMinutes()}${now.getUTCSeconds()}`;
-}
