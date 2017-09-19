60e43129da251daa4c4a00f63b9705dac6616159
hiyadav <hiyada@microsoft.com>
2017-09-19 15:08:31 +0530

    renaming inputs and doing grouping. resolved comments.

--- Tasks/JenkinsDownloadArtifacts/AzureStorageArtifacts/AzureStorageArtifactDownloader.ts
+++ Tasks/JenkinsDownloadArtifacts/AzureStorageArtifacts/AzureStorageArtifactDownloader.ts
@@ -20,34 +20,21 @@ export class AzureStorageArtifactDownloader {
     this.commonVirtualPath = tl.getInput('commonVirtualPath', false);
   }
 
-  public async downloadArtifacts(downloadToPath: string) {
+  public async downloadArtifacts(downloadToPath: string): Promise<void>{
     console.log(tl.loc('DownloadFromAzureBlobStorage', this.containerName));
 
     let storageAccount: StorageAccountInfo = await this._getStorageAccountDetails(this.azureRmStorageAccount);
 
     let blobService = new BlobService.BlobService(storageAccount.name, storageAccount.primaryAccessKey);
 
-    blobService.downloadBlobs(downloadToPath, this.containerName, this.commonVirtualPath, tl.getInput('itemPattern', false) || "**");
-  }
-
-  private _getARMCredentials(connectedService: string): msRestAzure.ApplicationTokenCredentials {
-    var servicePrincipalId: string = tl.getEndpointAuthorizationParameter(connectedService, "serviceprincipalid", false);
-    var servicePrincipalKey: string = tl.getEndpointAuthorizationParameter(connectedService, "serviceprincipalkey", false);
-    var tenantId: string = tl.getEndpointAuthorizationParameter(connectedService, "tenantid", false);
-    var armUrl: string = tl.getEndpointUrl(connectedService, true);
-    var envAuthorityUrl: string = tl.getEndpointDataParameter(connectedService, 'environmentAuthorityUrl', true);
-    envAuthorityUrl = (envAuthorityUrl != null) ? envAuthorityUrl : "https://login.windows.net/";
-    var activeDirectoryResourceId: string = tl.getEndpointDataParameter(connectedService, 'activeDirectoryServiceEndpointResourceId', false);
-    activeDirectoryResourceId = (activeDirectoryResourceId != null) ? activeDirectoryResourceId : armUrl;
-    var credentials = new msRestAzure.ApplicationTokenCredentials(servicePrincipalId, tenantId, servicePrincipalKey, armUrl, envAuthorityUrl, activeDirectoryResourceId, false);
-    return credentials;
+    await blobService.downloadBlobs(downloadToPath, this.containerName, this.commonVirtualPath, tl.getInput('itemPattern', false) || "**");
   }
 
   private async _getStorageAccountDetails(storageAccountName: string): Promise<StorageAccountInfo> {
     tl.debug("Getting storage account details for " + storageAccountName);
 
     let subscriptionId: string = tl.getEndpointDataParameter(this.connectedService, "subscriptionId", false);
-    let credentials = this._getARMCredentials(this.connectedService);
+    let credentials = this._getARMCredentials();
     let storageArmClient = new armStorage.StorageManagementClient(credentials, subscriptionId);
     let storageAccount: Model.StorageAccount = await storageArmClient.storageAccounts.get(storageAccountName);
 
@@ -62,6 +49,19 @@ export class AzureStorageArtifactDownloader {
       resourceGroupName: storageAccountResourceGroupName,
       primaryAccessKey: accessKeys[0]
     }
+  }
+
+  private _getARMCredentials(): msRestAzure.ApplicationTokenCredentials {
+    var servicePrincipalId: string = tl.getEndpointAuthorizationParameter(this.connectedService, "serviceprincipalid", false);
+    var servicePrincipalKey: string = tl.getEndpointAuthorizationParameter(this.connectedService, "serviceprincipalkey", false);
+    var tenantId: string = tl.getEndpointAuthorizationParameter(this.connectedService, "tenantid", false);
+    var armUrl: string = tl.getEndpointUrl(this.connectedService, true);
+    var envAuthorityUrl: string = tl.getEndpointDataParameter(this.connectedService, 'environmentAuthorityUrl', true);
+    envAuthorityUrl = (envAuthorityUrl != null) ? envAuthorityUrl : "https://login.windows.net/";
+    var activeDirectoryResourceId: string = tl.getEndpointDataParameter(this.connectedService, 'activeDirectoryServiceEndpointResourceId', false);
+    activeDirectoryResourceId = (activeDirectoryResourceId != null) ? activeDirectoryResourceId : armUrl;
+    var credentials = new msRestAzure.ApplicationTokenCredentials(servicePrincipalId, tenantId, servicePrincipalKey, armUrl, envAuthorityUrl, activeDirectoryResourceId, false);
+    return credentials;
   }
 }
 
