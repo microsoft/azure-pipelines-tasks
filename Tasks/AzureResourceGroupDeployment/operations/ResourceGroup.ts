b421859151aaded8f2a90cbb41f7b4fc77a86c8e
Deepak Sattiraju <desattir@microsoft.com>
2017-09-19 08:38:33 +0530

    Adding seconds to deployment name

--- Tasks/AzureResourceGroupDeployment/operations/ResourceGroup.ts
+++ Tasks/AzureResourceGroupDeployment/operations/ResourceGroup.ts
@@ -203,7 +203,7 @@ export class ResourceGroup {
         }
         name = path.basename(name).split(".")[0].replace(" ", "");
         var ts = new Date(Date.now());
-        var depName = util.format("%s-%s%s%s-%s%s", name, ts.getFullYear(), ts.getMonth(), ts.getDate(), ts.getHours(), ts.getMinutes());
+        var depName = util.format("%s-%s%s%s-%s%s%s", name, ts.getFullYear(), ts.getMonth(), ts.getDate(), ts.getHours(), ts.getMinutes(), ts.getSeconds());
         return depName;
     }
 
