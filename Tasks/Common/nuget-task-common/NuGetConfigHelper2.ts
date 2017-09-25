2c79523723aaa0b7760d8603764337006d1ee303
Aldo Mendoza <aldoms@microsoft.com>
2017-09-25 10:55:04 -0700

    Fix use NuGet to modify .config (#5459)

--- Tasks/Common/nuget-task-common/NuGetConfigHelper2.ts
+++ Tasks/Common/nuget-task-common/NuGetConfigHelper2.ts
@@ -26,10 +26,10 @@ export class NuGetConfigHelper2 {
         private authInfo: auth.NuGetExtendedAuthInfo,
         private environmentSettings: ngToolRunner.NuGetEnvironmentSettings,
         private tempConfigPath: string /*optional*/,
-        private useNuGetToModifyConfigFile?: boolean /* optional */)
+        useNuGetToModifyConfigFile?: boolean /* optional */)
     {
         this.tempNugetConfigPath = tempConfigPath || this.getTempNuGetConfigPath();
-        this.useNuGetToModifyConfigFile = useNuGetToModifyConfigFile || true;
+        useNuGetToModifyConfigFile = useNuGetToModifyConfigFile || true;
         this.nugetXmlHelper = useNuGetToModifyConfigFile ?
             new NuGetExeXmlHelper(this.nugetPath, this.tempNugetConfigPath, this.authInfo, this.environmentSettings) :
             new NuGetXmlHelper(this.tempNugetConfigPath);
