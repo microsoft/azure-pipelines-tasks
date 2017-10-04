0b3b7a316f5896e4088aee5f415d8d3441724f48
Kavipriya Adhinarayanan <kaadhina@microsoft.com>
2017-10-04 14:32:13 +0530

    Fixing indentation

--- Tasks/VsTest/helpers.ts
+++ Tasks/VsTest/helpers.ts
@@ -163,7 +163,7 @@ export class Helper {
         return argument;
     }
 	
-	public static setConsoleCodePage() {
+    public static setConsoleCodePage() {
         tl.debug("Changing active code page to UTF-8");
         const chcp = tl.tool(path.resolve(process.env.windir, "system32", "chcp.com"));
         chcp.arg(["65001"]);
