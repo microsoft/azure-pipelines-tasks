fec6779f59d07ba4fb922fce8e11af25025202c8
Parag Gangil <pagangil@microsoft.com>
2017-10-16 15:34:52 +0530

    Changing Publish test results task flow to use TestResultsPublisher.exe for windows platform

--- Tasks/PublishTestResults/publishtestresults.ts
+++ Tasks/PublishTestResults/publishtestresults.ts
@@ -1,4 +1,6 @@
 import * as tl from 'vsts-task-lib/task';
+import * as tr from 'vsts-task-lib/toolrunner';
+import * as path from 'path';
 
 const testRunner = tl.getInput('testRunner', true);
 const testResultsFiles: string[] = tl.getDelimitedInput('testResultsFiles', '\n', true);
@@ -24,12 +26,72 @@ if (isNullOrWhitespace(searchFolder)) {
 let matchingTestResultsFiles: string[] = tl.findMatch(searchFolder, testResultsFiles);
 if (!matchingTestResultsFiles || matchingTestResultsFiles.length === 0) {
     tl.warning('No test result files matching ' + testResultsFiles + ' were found.');
-} else {
-    let tp: tl.TestPublisher = new tl.TestPublisher(testRunner);
-    tp.publish(matchingTestResultsFiles, mergeResults, platform, config, testRunTitle, publishRunAttachments);
+}
+else {
+    tl.debug('OS type: ' + tl.osType());
+
+    // For windows platform, publish results using TestResultsPublisher.exe
+    if (tl.osType() === 'Windows_NT') {
+        publishResultsThroughExe();
+    }
+    else {
+        // For all OS other than Windows, continue using older flow
+        let tp: tl.TestPublisher = new tl.TestPublisher(testRunner);
+        tp.publish(matchingTestResultsFiles, mergeResults, platform, config, testRunTitle, publishRunAttachments);
+    }
 }
 
 tl.setResult(tl.TaskResult.Succeeded, '');
+
+
+function publishResultsThroughExe(): void {
+    let testResultsPublisherTool: tr.ToolRunner = tl.tool(getTestResultsPublisherLocation());
+    let envVars: { [key: string]: string; } = getEnvironmentVariables();
+    let args: string[] = getArguments();
+    testResultsPublisherTool.arg(args);
+    let TestResultsPublisherExecutionResult = testResultsPublisherTool.execSync(<tr.IExecSyncOptions>{ env: envVars }).stdout;
+}
+
+function getTestResultsPublisherLocation(): string {
+    return path.join(__dirname, 'TestResultsPublisher.exe');
+}
+
+function getArguments(): string[] {
+    return matchingTestResultsFiles;
+}
+
+function getEnvironmentVariables(): { [key: string]: string; } {
+    let envVars: { [key: string]: string; } = {};
+
+    addToProcessEnvVars(envVars, 'collectionUrl', tl.getVariable('System.TeamFoundationCollectionUri'));
+    addToProcessEnvVars(envVars, 'accessToken', tl.getEndpointAuthorizationParameter('SystemVssConnection', 'AccessToken', false));
+
+    addToProcessEnvVars(envVars, 'testRunner', testRunner);
+    addToProcessEnvVars(envVars, 'mergeResults', mergeResults);
+    addToProcessEnvVars(envVars, 'platform', platform);
+    addToProcessEnvVars(envVars, 'config', config);
+    addToProcessEnvVars(envVars, 'publishRunAttachments', publishRunAttachments);
+    addToProcessEnvVars(envVars, 'testRunTitle', testRunTitle);
+    addToProcessEnvVars(envVars, 'publishRunAttachments', publishRunAttachments);
+    addToProcessEnvVars(envVars, 'projectName', tl.getVariable('System.TeamProject'));
+    addToProcessEnvVars(envVars, 'owner', tl.getVariable('Build.RequestedFor'));
+    addToProcessEnvVars(envVars, 'buildId', tl.getVariable('Build.BuildId'));
+    addToProcessEnvVars(envVars, 'buildUri', tl.getVariable('Build.BuildUri'));
+    addToProcessEnvVars(envVars, 'releaseUri', tl.getVariable('Release.ReleaseUri'));
+    addToProcessEnvVars(envVars, 'releaseEnvironmentUri', tl.getVariable('Release.ReleaseEnvironmentUri'));
+
+    return envVars;
+}
+
+function addToProcessEnvVars(envVars: { [key: string]: string; }, name: string, value: string): void {
+    if (!isNullEmptyOrUndefinedddd(value)) {
+        envVars[name] = value;
+    }
+}
+
+function isNullEmptyOrUndefinedddd(obj): boolean {
+    return obj === null || obj === '' || obj === undefined;
+}
 
 function isNullOrWhitespace(input: any) {
     if (typeof input === 'undefined' || input === null) {
