d842e7794522ceffc9ec0aa9fb61e5644aa45396
Ashish Ranjan <asranja@microsoft.com>
2017-10-16 15:16:05 +0530

    Update L0UpdateAlertsSuccess.ts

--- Tasks/AzureMonitorAlerts/Tests/L0UpdateAlertsSuccess.ts
+++ Tasks/AzureMonitorAlerts/Tests/L0UpdateAlertsSuccess.ts
@@ -69,6 +69,7 @@ nock("http://example.com", {
 		reqheaders: {
         	"authorization": "Bearer accessToken",
         	"accept": "application/json",
+		"content-type": "application/json; charset=utf-8",
     		"user-agent": "TFS_useragent"
       	}
 	})
