96692512b973b579a8643190a8914fabd7ce36b4
arjgupta <arjgupta@microsoft.com>
2017-10-16 14:52:35 +0530

    updating azure rg task to remove vso-node-api dependency

--- Tasks/AzureResourceGroupDeployment/operations/ResourceGroup.ts
+++ Tasks/AzureResourceGroupDeployment/operations/ResourceGroup.ts
@@ -12,10 +12,21 @@ import { PowerShellParameters, NameValuePair } from "./ParameterParser";
 import utils = require("./Utils");
 import fileEncoding = require('./FileEncoding');
 import { ParametersFileObject, TemplateObject, ParameterValue } from "../models/Types";
+var hm = require("typed-rest-client/HttpClient");
+import httpInterfaces = require("typed-rest-client/Interfaces");
 
+let proxyUrl: string = tl.getVariable("agent.proxyurl"); 
+var requestOptions: httpInterfaces.IRequestOptions = proxyUrl ? { 
+    proxy: { 
+        proxyUrl: proxyUrl, 
+        proxyUsername: tl.getVariable("agent.proxyusername"), 
+        proxyPassword: tl.getVariable("agent.proxypassword"), 
+        proxyBypassHosts: tl.getVariable("agent.proxybypasslist") ? JSON.parse(tl.getVariable("agent.proxybypasslist")) : null 
+    } 
+} : null;
+
+let hc = new hm.HttpClient(tl.getVariable("AZURE_HTTP_USER_AGENT"), null, requestOptions);
 var uuid = require("uuid");
-var httpClient = require('vso-node-api/HttpClient');
-var httpObj = new httpClient.HttpCallbackClient("VSTS_AGENT");
 
 function stripJsonComments(content) {
     if (!content || (content.indexOf("//") < 0 && content.indexOf("/*") < 0)) {
@@ -265,7 +276,18 @@ export class ResourceGroup {
 
     private downloadFile(url): Promise<string> {
         return new Promise<string>((resolve, reject) => {
-            httpObj.get("GET", url, {}, (error, result, contents) => {
+            hc.get(url, {}).then(async (response) => {
+                if(response.message.statusCode == 200) {
+                    let contents: string = await response.readBody();
+                    resolve(contents);
+                } else {
+                    var errorMessage = response.message.statusCode.toString() + ": " + response.message.statusMessage;
+                    return reject(tl.loc("FileFetchFailed", url, errorMessage));
+                }
+            }, (error) => {
+                return reject(tl.loc("FileFetchFailed", url, error));
+            });
+            /*httpObj.get("GET", url, {}, (error, result, contents) => {
                 if (error) {
                     return reject(tl.loc("FileFetchFailed", url, error));
                 }
@@ -275,7 +297,7 @@ export class ResourceGroup {
                     var errorMessage = result.statusCode.toString() + ": " + result.statusMessage;
                     return reject(tl.loc("FileFetchFailed", url, errorMessage));
                 }
-            });
+            });*/
         });
     }
 
