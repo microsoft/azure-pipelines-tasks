543f838034d4e366c5cce75db78f2e3963c910e3
😊Nitin Gurram😊 <nigurr@microsoft.com>
2017-10-30 15:01:20 +0530

    Removing semver dependency

--- Tasks/VsTest/cieventlogger.ts
+++ Tasks/VsTest/cieventlogger.ts
@@ -1,5 +1,4 @@
 import * as tl from 'vsts-task-lib/task';
-import semver = require('semver');
 
 const area: string = 'TestExecution';
 const feature: string = 'TestExecutionTask';
@@ -15,13 +14,10 @@ function getDefaultProps() {
 
 export function publishEvent(properties: { [key: string]: any }): void {
     try {
-        const agentVersion: string = tl.getVariable('Agent.Version');
-        if (agentVersion && !semver.lt(agentVersion, '2.125.0')) {
-            tl.publishTelemetry(area, feature, Object.assign(getDefaultProps(), properties));
-        } else {
-            tl.debug('Unable to publish telemetry due to lower agent version: ' + agentVersion);
-        }
+        tl.assertAgent('2.125.0');
+        tl.publishTelemetry(area, feature, Object.assign(getDefaultProps(), properties));
+
     } catch (err) {
-        //ignore
+        tl.debug('Unable to publish telemetry due to lower agent version.');
     }
 }
