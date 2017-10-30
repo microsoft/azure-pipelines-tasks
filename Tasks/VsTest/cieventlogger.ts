2d002ef9e45e03db0221c781b7fe381d470e24ce
😊Nitin Gurram😊 <nigurr@microsoft.com>
2017-10-30 14:11:21 +0530

    Disabling telemetry till 2.125 agent is out

--- Tasks/VsTest/cieventlogger.ts
+++ Tasks/VsTest/cieventlogger.ts
@@ -1,4 +1,5 @@
 import * as tl from 'vsts-task-lib/task';
+import semver = require('semver');
 
 const area: string = 'TestExecution';
 const feature: string = 'TestExecutionTask';
@@ -14,7 +15,12 @@ function getDefaultProps() {
 
 export function publishEvent(properties: { [key: string]: any }): void {
     try {
-        tl.publishTelemetry(area, feature, Object.assign(getDefaultProps(), properties));
+        const agentVersion: string = tl.getVariable('Agent.Version');
+        if (agentVersion && !semver.lt(agentVersion, '2.125.0')) {
+            tl.publishTelemetry(area, feature, Object.assign(getDefaultProps(), properties));
+        } else {
+            tl.debug('Unable to publish telemetry due to lower agent version: ' + agentVersion);
+        }
     } catch (err) {
         //ignore
     }
