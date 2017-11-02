0ecf58f38807833532568fa49a2b017f47bfccf5
Jason Ricke <jason.ricke@sterlingemarketing.com>
2017-11-02 10:05:57 -0500

    add system definition id and current build id to temp path for artifact download. will make artifact download unique for current build

--- Tasks/ServiceFabricUpdateManifests/Get-VstsBuild.psm1
+++ Tasks/ServiceFabricUpdateManifests/Get-VstsBuild.psm1
@@ -64,7 +64,7 @@
             }
             elseif ($artifact.resource.type -eq "container") # The artifact is in a hosted server and must be downloaded into a temp folder
             {
-                $agentTmpFolder = Join-Path $env:Temp $build.buildNumber
+                $agentTmpFolder = [io.path]::combine($env:Temp, $env:SYSTEM_DEFINITIONID, $env:BUILD_BUILDNUMBER, $build.buildNumber)
                 $artifactZipFile = Join-Path $agentTmpFolder "$PkgArtifactName.zip"
                 $artifactPath = Join-Path $agentTmpFolder $PkgArtifactName
                 $downloadArtifact = $true
