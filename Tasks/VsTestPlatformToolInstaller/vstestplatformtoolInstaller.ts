33e2d4f05fa9e0d67b8d0299be4b889b001076ff
Shreyas R <shrer@microsoft.com>
2017-11-02 16:58:07 +0530

    Fixed loc string and added feature to select latest version

--- Tasks/VsTestPlatformToolInstaller/vstestplatformtoolInstaller.ts
+++ Tasks/VsTestPlatformToolInstaller/vstestplatformtoolInstaller.ts
@@ -2,6 +2,7 @@ import tl = require('vsts-task-lib/task');
 import * as toolLib from 'vsts-task-tool-lib/tool';
 import * as taskLib from 'vsts-task-lib/task';
 import * as restm from 'typed-rest-client/RestClient';
+import {IExecOptions, IExecSyncResult, ToolRunner} from "vsts-task-lib/toolrunner";
 import * as os from 'os';
 import * as path from 'path';
 
@@ -15,38 +16,52 @@ async function startInstaller() {
     }
 
     try {
+        tl.setResourcePath(path.join(__dirname, 'task.json'));
+
         console.log(tl.loc('StartingInstaller'));
-        console.log('========================================================');
+        console.log('==============================================================================');
 
         // Read inputs
-        let testPlatformVersion = taskLib.getInput('testPlatformVersion', true);
-        let checkForLatestVersion: boolean = taskLib.getBoolInput('checkLatest', false);
+        let versionSelectorInput = taskLib.getInput('versionSelector', true);
+        let testPlatformVersion = taskLib.getInput('testPlatformVersion', false); 
         
         // Get the required version of the platform and make necessary preparation to allow its consumption down the phase 
-        await getVsTestPlatformTool(testPlatformVersion, checkForLatestVersion);
+        await getVsTestPlatformTool(testPlatformVersion, versionSelectorInput);
     }
     catch (error) {
         taskLib.setResult(taskLib.TaskResult.Failed, error.message);
     }
 }
 
-async function getVsTestPlatformTool(testPlatformVersion: string, checkForLatestVersion: boolean) {
-    // Check cache for the specified version
-    //taskLib.assertAgent('2.115.0');
-
+async function getVsTestPlatformTool(testPlatformVersion: string, versionSelectorInput: string) {
     // Should point to the location where the VsTest platform tool will be
     let toolPath: string;
 
-    if (!checkForLatestVersion) {
-        tl.debug(`Check for latest version option was not ticked. Looking for ${testPlatformVersion} in the tools cache.`);
-        toolPath = toolLib.findLocalTool('VsTest', testPlatformVersion);
-    }
-    else {
-        // TODO: Code to find latest version and look for it in the cache anmd if not available in the cache then download and cache it. 
-        tl.debug('Check for latest version option was ticked.');
+    if (versionSelectorInput == 'latestStable') {
+        testPlatformVersion = getLatestPackageVersionNumber(false);
+
+        if(testPlatformVersion == null) {
+            throw new Error(tl.loc('CouldNotFindLatestStableVersion', packageName));
+        }
+
+        tl.debug(`Found the latest stable version to be ${testPlatformVersion}`);
+    } 
+    else if(versionSelectorInput == 'latestPreRelease') {
+        testPlatformVersion = getLatestPackageVersionNumber(true);
+
+        if(testPlatformVersion == null) {
+            throw new Error(tl.loc('CouldNotFindLatestPreReleaseVersion', packageName));
+        }
+
+        tl.debug(`Found the latest pre-release version to be ${testPlatformVersion}`);
     }
 
+    // Check cache for the specified version
+    tl.debug(`Looking for ${testPlatformVersion} in the tools cache.`);
+    toolPath = toolLib.findLocalTool('VsTest', testPlatformVersion);
+
     if (!toolPath) {
+        tl.debug(`Could not find ${packageName}.${testPlatformVersion} in the tools cache. Fetching it from nuget.`);
         if (toolLib.isExplicitVersion(testPlatformVersion)) {
             // Download the required version and cache it
             toolPath = await acquireAndCacheVsTestPlatformNuget(testPlatformVersion);
@@ -59,6 +74,32 @@ async function getVsTestPlatformTool(testPlatformVersion: string, checkForLatest
     // Set the task variable so that the VsTest task can consume this path
     tl.setVariable('VsTestToolsInstallerInstalledToolLocation', toolPath);
     tl.debug('Set variable VsTestToolsInstallerInstalledToolLocation value to ' + toolPath);
+}
+
+function getLatestPackageVersionNumber(includePreRelease: boolean): string {
+    const nugetTool = tl.tool(path.join(__dirname, 'nuget.exe'));
+
+    if(includePreRelease == true) {
+        nugetTool.line('list ' + packageName + ' -PreRelease' + ' -Source ' + packageSource);
+    }
+    else {
+        nugetTool.line('list ' + packageName + ' -Source ' + packageSource);
+    }
+
+    let options= <IExecOptions>{};
+    options.silent = true;
+    var result = nugetTool.execSync(options);
+    var listOfPackages = result.stdout.split('\n');
+    var version: string;
+
+    listOfPackages.forEach(nugetPackage => {
+        if(nugetPackage.split(' ')[0] == packageName) {
+            version = nugetPackage.split(' ')[1];
+            return;
+        }
+    });
+
+    return version;
 }
 
 async function acquireAndCacheVsTestPlatformNuget(testPlatformVersion: string): Promise<string> {
