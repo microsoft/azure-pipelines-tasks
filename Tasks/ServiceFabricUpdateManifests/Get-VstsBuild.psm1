07a5377dce85e6b598cabeecbd6f73ab21e04bd0
Jason Ricke <jason.ricke@sterlingemarketing.com>
2017-11-03 14:51:04 -0500

    used proper name for agent temp dir

--- Tasks/ServiceFabricUpdateManifests/Get-VstsBuild.psm1
+++ Tasks/ServiceFabricUpdateManifests/Get-VstsBuild.psm1
@@ -64,7 +64,7 @@
             }
             elseif ($artifact.resource.type -eq "container") # The artifact is in a hosted server and must be downloaded into a temp folder
             {
-                $agentTmpFolder = Join-Path $env:Temp $build.buildNumber
+                $agentTmpFolder = Join-Path $ENV:AGENT_TEMPDIRECTORY $build.buildNumber
                 $artifactZipFile = Join-Path $agentTmpFolder "$PkgArtifactName.zip"
                 $artifactPath = Join-Path $agentTmpFolder $PkgArtifactName
                 $downloadArtifact = $true
