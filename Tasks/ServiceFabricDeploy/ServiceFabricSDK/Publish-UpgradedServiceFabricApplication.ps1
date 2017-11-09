37a01968e2d37fcfb7dca6193a0633f0f2e86054
Vinod Kumar <kuvinod@microsoft.com>
2017-11-09 13:08:39 +0530

    Updated patch

--- Tasks/ServiceFabricDeploy/ServiceFabricSDK/Publish-UpgradedServiceFabricApplication.ps1
+++ Tasks/ServiceFabricDeploy/ServiceFabricSDK/Publish-UpgradedServiceFabricApplication.ps1
@@ -209,6 +209,8 @@ function Publish-UpgradedServiceFabricApplication
             $errMsg = (Get-VstsLocString -Key SFSDK_UpgradeInProgressError -ArgumentList $ApplicationName)
             throw $errMsg
         }
+        $upgradeStatus = "##########"
+        Write-Host $upgradeStatus
 
         $reg = Get-ServiceFabricApplicationType -ApplicationTypeName $names.ApplicationTypeName | Where-Object  { $_.ApplicationTypeVersion -eq $names.ApplicationTypeVersion }
         if ($reg)
@@ -322,7 +324,7 @@ function Publish-UpgradedServiceFabricApplication
         Write-Host (Get-VstsLocString -Key SFSDK_WaitingForUpgrade)
         $upgradeStatusFetcher = { Get-ServiceFabricApplicationUpgrade -ApplicationName $ApplicationName }
         $upgradeStatusValidator = { param($upgradeStatus) return ($upgradeStatus.UpgradeState -eq "RollingBackCompleted" -or $upgradeStatus.UpgradeState -eq "RollingForwardCompleted") }
-        Invoke-ActionWithRetries -Action $upgradeStatusFetcher `
+        $upgradeStatus = Invoke-ActionWithRetries -Action $upgradeStatusFetcher `
                                 -ActionSuccessValidator $upgradeStatusValidator `
                                 -MaxTries 2147483647 `
                                 -RetryIntervalInSeconds 3 `
@@ -344,8 +346,6 @@ function Publish-UpgradedServiceFabricApplication
                 }
             }
         }
-
-        $upgradeStatus = & $upgradeStatusFetcher
 
         if($upgradeStatus.UpgradeState -eq "RollingForwardCompleted")
         {
