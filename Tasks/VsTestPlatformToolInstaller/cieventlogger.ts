5b049eabb807bacd19c34cbd57dd042cc6b6684a
Shreyas R <shrer@microsoft.com>
2017-11-15 14:37:19 +0530

    Telemetry for VsTest Tools Installer

---
+++ Tasks/VsTestPlatformToolInstaller/cieventlogger.ts
@@ -0,0 +1,24 @@
+import * as tl from 'vsts-task-lib/task';
+
+const area: string = 'TestExecution';
+const feature: string = 'VsTestToolsInstaller';
+
+function getDefaultProps() {
+    return {
+        releaseuri: tl.getVariable('Release.ReleaseUri'),
+        releaseid: tl.getVariable('Release.ReleaseId'),
+        builduri: tl.getVariable('Build.BuildUri'),
+        buildid: tl.getVariable('Build.Buildid')
+    };
+}
+
+export function publishEvent(subFeature: string, properties: { [key: string]: any }): void {
+    try {
+        tl.assertAgent('2.125.0');
+        properties['subFeature'] = subFeature;
+        tl.publishTelemetry(area, feature, Object.assign(getDefaultProps(), properties));
+
+    } catch (err) {
+        tl.debug('Unable to publish telemetry due to lower agent version.');
+    }
+}
