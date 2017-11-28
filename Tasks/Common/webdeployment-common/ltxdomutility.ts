f71708845f59e54cf2c105371c3cfe35eca81f3a
Ajay Kumar Yadav <ajya@microsoft.com>
2017-11-28 16:49:16 +0530

    Fix for Xml variable substitution (#5912)
    * Fix for Xml variable substitution
    
    * Addressed review comments

--- Tasks/Common/webdeployment-common/ltxdomutility.ts
+++ Tasks/Common/webdeployment-common/ltxdomutility.ts
@@ -2,89 +2,96 @@ var ltx = require("ltx");
 var varUtility = require("./variableutility.js");
 var Q = require('q');
 
-var xmlDomLookUpTable = {};
-var headerContent;
+export class LtxDomUtility  {
 
-export function initializeDOM(xmlContent) {
-    xmlDomLookUpTable = {};
-    headerContent = null;
-    var xmlDom = ltx.parse(xmlContent);
-    readHeader(xmlContent);
-    buildLookUpTable(xmlDom);
-    return xmlDom;
-}
+    private xmlDomLookUpTable = {};
+    private headerContent;
+    private xmlDom;
 
-function readHeader(xmlContent) {
-    var index = xmlContent.indexOf('\n');
-    if(index > -1) {
-        var firstLine = xmlContent.substring(0,index).trim();
-        if(firstLine.startsWith("<?") && firstLine.endsWith("?>")) {
-            headerContent = firstLine;
+    public constructor(xmlContent) {
+        this.xmlDomLookUpTable = {};
+        this.headerContent = null;
+        this.xmlDom = ltx.parse(xmlContent);
+        this.readHeader(xmlContent);
+        this.buildLookUpTable(this.xmlDom);
+    }
+
+    public getXmlDom() {
+        return this.xmlDom;
+    }
+
+    private readHeader(xmlContent) {
+        var index = xmlContent.indexOf('\n');
+        if(index > -1) {
+            var firstLine = xmlContent.substring(0,index).trim();
+            if(firstLine.startsWith("<?") && firstLine.endsWith("?>")) {
+                this.headerContent = firstLine;
+            }
         }
     }
-}
 
-export function getContentWithHeader(xmlDom) {
-    return xmlDom ? (headerContent ? headerContent + "\n" : "") + xmlDom.root().toString() : "";
-}
+    public getContentWithHeader(xmlDom) {
+        return xmlDom ? (this.headerContent ? this.headerContent + "\n" : "") + xmlDom.root().toString() : "";
+    }
 
-/**
- * Define method to create a lookup for DOM 
- */
-function buildLookUpTable(node) {
-    if(node){
-        var nodeName = node.name;
-        if(nodeName){
-            nodeName = nodeName.toLowerCase();
-            var listOfNodes = xmlDomLookUpTable[nodeName];
-            if(listOfNodes == null || !(Array.isArray(listOfNodes))) {
-                listOfNodes = [];
-                xmlDomLookUpTable[nodeName] = listOfNodes;
-            }
-            listOfNodes.push(node);
-            var childNodes = node.children;
-            for(var i=0 ; i < childNodes.length; i++){
-                var childNodeName = childNodes[i].name;
-                if(childNodeName) {
-                    buildLookUpTable(childNodes[i]);
+    /**
+     * Define method to create a lookup for DOM 
+     */
+    private buildLookUpTable(node) {
+        if(node){
+            var nodeName = node.name;
+            if(nodeName){
+                nodeName = nodeName.toLowerCase();
+                var listOfNodes = this.xmlDomLookUpTable[nodeName];
+                if(listOfNodes == null || !(Array.isArray(listOfNodes))) {
+                    listOfNodes = [];
+                    this.xmlDomLookUpTable[nodeName] = listOfNodes;
+                }
+                listOfNodes.push(node);
+                var childNodes = node.children;
+                for(var i=0 ; i < childNodes.length; i++){
+                    var childNodeName = childNodes[i].name;
+                    if(childNodeName) {
+                        this.buildLookUpTable(childNodes[i]);
+                    }
                 }
             }
         }
     }
-}
 
-/**
- *  Returns array of nodes which match with the tag name.
- */
-export function getElementsByTagName(nodeName) {
-    if(varUtility.isEmpty(nodeName))
-        return [];
-    var selectedElements = xmlDomLookUpTable[nodeName.toLowerCase()];
-    if(!selectedElements){
-        selectedElements = [];
+    /**
+     *  Returns array of nodes which match with the tag name.
+     */
+    public getElementsByTagName(nodeName) {
+        if(varUtility.isEmpty(nodeName))
+            return [];
+        var selectedElements = this.xmlDomLookUpTable[nodeName.toLowerCase()];
+        if(!selectedElements){
+            selectedElements = [];
+        }
+        return selectedElements;
     }
-    return selectedElements;
-}
 
-/**
- *  Search in subtree with provided node name
- */
-export function getChildElementsByTagName(node, tagName) {
-    if(!varUtility.isObject(node) )
-        return [];
-    var children = node.children;
-    var liveNodes = [];
-    if(children){
-        for( var i=0; i < children.length; i++ ){
-            var childName = children[i].name;
-            if( !varUtility.isEmpty(childName) && tagName == childName){
-                liveNodes.push(children[i]);
-            }
-            var liveChildNodes = getChildElementsByTagName(children[i], tagName);
-            if(liveChildNodes && liveChildNodes.length > 0){
-            	liveNodes = liveNodes.concat(liveChildNodes);
+    /**
+     *  Search in subtree with provided node name
+     */
+    public getChildElementsByTagName(node, tagName) {
+        if(!varUtility.isObject(node) )
+            return [];
+        var children = node.children;
+        var liveNodes = [];
+        if(children){
+            for( var i=0; i < children.length; i++ ){
+                var childName = children[i].name;
+                if( !varUtility.isEmpty(childName) && tagName == childName){
+                    liveNodes.push(children[i]);
+                }
+                var liveChildNodes = this.getChildElementsByTagName(children[i], tagName);
+                if(liveChildNodes && liveChildNodes.length > 0){
+                    liveNodes = liveNodes.concat(liveChildNodes);
+                }
             }
         }
+        return liveNodes;
     }
-    return liveNodes;
 }
