ea81e0e97cd4b08751fb8d82cad13126cc2bd4ef
🏆Nitin Gurram🏆 <nigurr@microsoft.com>
2018-01-03 11:31:19 +0530

    Adding console options to telemetry

--- Tasks/VsTest/vstest.ts
+++ Tasks/VsTest/vstest.ts
@@ -69,11 +69,12 @@ export function startTest() {
             task: 'VsTestConsoleFlow',
             runInParallel: vstestConfig.runInParallel,
             result: 'Failed',
-            settingsType:  !utils.Helper.isNullOrUndefined(vstestConfig.settingsFile) ? vstestConfig.settingsFile.endsWith('.runsettings') ? 'runsettings' : vstestConfig.settingsFile.endsWith('.testsettings') ? 'testsettings' : 'none': 'none',
+            settingsType: !utils.Helper.isNullOrUndefined(vstestConfig.settingsFile) ? vstestConfig.settingsFile.endsWith('.runsettings') ? 'runsettings' : vstestConfig.settingsFile.endsWith('.testsettings') ? 'testsettings' : 'none': 'none',
             testSelection: vstestConfig.testSelection,
             tiaEnabled: vstestConfig.tiaConfig.tiaEnabled,
             vsTestVersion: vstestConfig.vsTestVersionDetails.majorVersion + '.' + vstestConfig.vsTestVersionDetails.minorversion + '.' + vstestConfig.vsTestVersionDetails.patchNumber,
-            consoleOptionsEnabled: !utils.Helper.isNullOrWhitespace(vstestConfig.otherConsoleOptions) ? 'true' : 'false'
+            consoleOptionsEnabled:
+                !utils.Helper.isNullOrWhitespace(vstestConfig.otherConsoleOptions) ? vstestConfig.otherConsoleOptions : ''
         };
 
         invokeVSTest().then(function (taskResult) {
