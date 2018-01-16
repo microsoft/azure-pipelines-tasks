a049db29f9bb205338053c59b9d4ce4afe52b069
vinca <vinca@microsoft.com>
2018-01-16 14:17:26 +0530

    Package error message updated

--- Tasks/Common/webdeployment-common/packageUtility.ts
+++ Tasks/Common/webdeployment-common/packageUtility.ts
@@ -5,11 +5,11 @@ export class PackageUtility {
     public static getPackagePath(packagePath: string): string {
         var availablePackages: string[] = utility.findfiles(packagePath);
         if(availablePackages.length == 0) {
-            throw new Error(tl.loc('Nopackagefoundwithspecifiedpattern'));
+            throw new Error(tl.loc('Nopackagefoundwithspecifiedpattern', packagePath));
         }
 
         if(availablePackages.length > 1) {
-            throw new Error(tl.loc('MorethanonepackagematchedwithspecifiedpatternPleaserestrainthesearchpattern'));
+            throw new Error(tl.loc('MorethanonepackagematchedwithspecifiedpatternPleaserestrainthesearchpattern', packagePath));
         }
 
         return availablePackages[0];
