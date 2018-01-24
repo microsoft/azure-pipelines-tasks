1a0dd634a04da3ad70ab7fa28ec45617faa2c3ea
vinca <vinca@microsoft.com>
2018-01-24 18:31:34 +0530

    make appOffline optional

--- Tasks/AzureRmWebAppDeployment/operations/KuduServiceUtility.ts
+++ Tasks/AzureRmWebAppDeployment/operations/KuduServiceUtility.ts
@@ -127,7 +127,7 @@ export class KuduServiceUtility {
         }
     }
 
-    public async zipDeploy(packagePath: string, appOffline: boolean, customParameters?: Array<string>): Promise<void> {
+    public async zipDeploy(packagePath: string, appOffline?: boolean, customParameters?: Array<string>): Promise<void> {
         try {
             if(appOffline) {
                 await this._appOfflineKuduService(physicalRootPath, true);
