7f9af4aecdd132c2385dc57030d75c42b0fb5aa8
Kellie Jos <kej@microsoft.com>
2018-01-25 15:13:22 -0500

    Fix for the backslash escaping issue in cURL

--- Tasks/cURLUploader/curluploader.ts
+++ Tasks/cURLUploader/curluploader.ts
@@ -86,12 +86,11 @@ async function run() {
             // Now we get a list of all files under this root
             var allFiles = tl.find(findPathRoot);
 
-            // IMPORTANT: The backslash character is a valid character in linux style directory and file names. However, as
-            // this is a file pattern not a file path we are converting all backslashes to forward slashes.
-            filesPattern = filesPattern.replace(/\\/g, '/');
-
             // Now matching the pattern against all files
-            var uploadFilesList = tl.match(allFiles, filesPattern, undefined, {matchBase: true});
+            var uploadFilesList = tl.match(allFiles, filesPattern, undefined, {matchBase: true}).map( (s) => {
+                console.log("Inside the pattern match method.")
+                return isWin ? s.replace(/\\/g, '/') : s.replace(/\\/g, '\\\\');
+            });
 
             // Fail if no matching app files were found
             if (!uploadFilesList || uploadFilesList.length == 0) {
