2e5a2b1e87ae76715cebd42f05458fb0dbd68f2a
chaitanya shrikhande <chshrikh@microsoft.com>
2018-02-06 14:28:45 +0530

    removed the unwanted test

--- Tasks/WindowsMachineFileCopy/Tests/L0.ts
+++ Tasks/WindowsMachineFileCopy/Tests/L0.ts
@@ -34,10 +34,7 @@ describe('WindowsMachineFileCopy Suite', function () {
         });
         it('Throw error if Destination Path is invalid or empty', (done) => {
             psr.run(path.join(__dirname, 'L0ValidateDestinationPath.ps1'), done);
-        });
-        it('Should copy on local machine when no machine name is given', (done) => {
-            psr.run(path.join(__dirname, 'L0ShouldCopyOnLocalMachine.ps1'), done);
-        });
+        });        
         it('Validate Get-ResourcesProperties Utility', (done) => {
             psr.run(path.join(__dirname, 'L0GetResourcesProperties.ps1'), done);
         });
