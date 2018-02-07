c1ad53c032d69ad88db41c67af0a9719aaf47d95
hililh <hililh@microsoft.com>
2018-02-07 15:27:33 +0530

    Golang runner task. Task to run go application

---
+++ Tasks/Go/README.md
@@ -0,0 +1,1 @@
+##TODO: Go runner task documentation
---
+++ Tasks/Go/main.ts
@@ -0,0 +1,48 @@
+import tl = require("vsts-task-lib/task");
+import tr = require("vsts-task-lib/toolrunner");
+
+
+export class goExe {
+    private command: string = "";
+    private argument: string = "";
+    private workingDir: string = "";
+    private failOnStdErr: boolean = false;
+
+    constructor() {
+        this.command = tl.getInput("command", true).trim();
+        if (this.command == "custom") {
+            this.command = tl.getInput("customCommand", true).trim();
+        }
+        this.argument = tl.getInput("arguments", false);
+        this.workingDir = tl.getInput("workingDirectory", false);
+        if (this.workingDir == null) {
+            this.workingDir = tl.getVariable("System.DefaultWorkingDirectory");
+        }
+        this.failOnStdErr = tl.getBoolInput("failOnStandardError", false);
+    }
+
+    public async execute() {
+        return new Promise<string>(async (resolve, reject) =>{
+            let goPath = tl.which("go", true);
+            let go: tr.ToolRunner = tl.tool(goPath);
+    
+            go.arg(this.command);
+            go.line(this.argument);
+    
+            try {
+                var result = await go.exec(<tr.IExecOptions>{
+                    cwd: this.workingDir,
+                    failOnStdErr: this.failOnStdErr,
+                });
+                resolve("");
+            }
+            catch (err) {
+                reject(err);
+            }
+        });
+        
+    }
+}
+
+var exe = new goExe();
+exe.execute().catch((reason) => tl.setResult(tl.TaskResult.Failed, reason));
