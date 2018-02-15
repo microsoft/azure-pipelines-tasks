6e10aa8959fe4649fb196fdeb3bf5f75da9a0732
Keith Robertson <kerobert@microsoft.com>
2018-02-14 17:45:38 -0800

    Add support for token based auth to services other than VSTS This fixes #4956 Also fixes #6411

--- Tasks/Common/npm-common/npmregistry.ts
+++ Tasks/Common/npm-common/npmregistry.ts
@@ -1,6 +1,7 @@
 import * as os from 'os';
 import * as tl from 'vsts-task-lib/task';
-import * as url from 'url';
+import * as URL from 'url';
+import * as ipaddress from 'ip-address';
 
 import { NormalizeRegistry } from './npmrcparser';
 import * as util from './util';
@@ -23,11 +24,16 @@ export class NpmRegistry implements INpmRegistry {
     }
 
     public static FromServiceEndpoint(endpointId: string, authOnly?: boolean): NpmRegistry {
-        let email: string;
-        let username: string;
-        let password: string;
+        let lineEnd = os.EOL;
         let endpointAuth: tl.EndpointAuthorization;
         let url: string;
+        let nerfed: string;
+        let auth: string;
+        let username: string;
+        let password: string;
+        let email: string;
+        let password64: string;
+        let isVstsTokenAuth: boolean = false;
         try {
             endpointAuth = tl.getEndpointAuthorization(endpointId, false);
         } catch (exception) {
@@ -35,7 +41,22 @@ export class NpmRegistry implements INpmRegistry {
         }
 
         try {
+            let collectionUrl = tl.getVariable("System.TeamFoundationCollectionUri");
+            let collectionUrlDomain = NpmRegistry.ParseHostnameForDomain(URL.parse(collectionUrl).hostname);
             url = NormalizeRegistry(tl.getEndpointUrl(endpointId, false));
+            let epUrlDomain = NpmRegistry.ParseHostnameForDomain(URL.parse(url).hostname);
+
+            // To the reader, this could be optimized here but it is broken out for readability
+            if (endpointAuth.scheme === 'Token'){
+                if (collectionUrlDomain === epUrlDomain){
+                    // Same domain _OR_ matching IP addrs.  Use PAT+Basic
+                    isVstsTokenAuth = true;
+                } else if (epUrlDomain.toUpperCase() === 'VISUALSTUDIO.COM'){
+                    // If the endpoint is VSTS, full stop.  Use PAT+Basic
+                    isVstsTokenAuth = true;
+                }
+            }
+            nerfed = util.toNerfDart(url);
         } catch (exception) {
             throw new Error(tl.loc('ServiceEndpointUrlNotDefined'));
         }
@@ -45,23 +66,32 @@ export class NpmRegistry implements INpmRegistry {
                 username = endpointAuth.parameters['username'];
                 password = endpointAuth.parameters['password'];
                 email = username; // npm needs an email to be set in order to publish, this is ignored on npmjs
+                password64 = (new Buffer(password).toString('base64'));
+
+                auth = nerfed + ":username=" + username + lineEnd;
+                auth += nerfed + ":_password=" + password64 + lineEnd;
+                auth += nerfed + ":email=" + email + lineEnd;
                 break;
             case 'Token':
-                email = 'VssEmail';
-                username = 'VssToken';
-                password = endpointAuth.parameters['apitoken'];
+                let apitoken = endpointAuth.parameters['apitoken'];
+                if (!isVstsTokenAuth){
+                    // Use Bearer auth as it was intended.
+                    auth = nerfed + ":_authToken=" + apitoken + lineEnd;
+                }else{
+                    // VSTS does not support PATs+Bearer only JWTs+Bearer
+                    email = 'VssEmail';
+                    username = 'VssToken';
+                    password64 = (new Buffer(apitoken).toString('base64'));
+                    console.log("##vso[task.setvariable variable=" + endpointId + "BASE64_PASSWORD;issecret=true;]" + password64);
+
+                    auth = nerfed + ":username=" + username + lineEnd;
+                    auth += nerfed + ":_password=" + password64 + lineEnd;
+                    auth += nerfed + ":email=" + email + lineEnd;
+                }
                 break;
         }
-       let lineEnd = os.EOL;
-        let nerfed = util.toNerfDart(url); 
-        let password64 = (new Buffer(password).toString('base64'));
-        console.log("##vso[task.setvariable variable=" + endpointId + "BASE64_PASSWORD;issecret=true;]" + password64);
 
-        let auth = nerfed + ":username=" + username + lineEnd;
-        auth += nerfed + ":_password=" + password64 + lineEnd;
-        auth += nerfed + ":email=" + email + lineEnd;
         auth += nerfed + ":always-auth=true";
-
         return new NpmRegistry(url, auth, authOnly);
     }
 
@@ -71,5 +101,22 @@ export class NpmRegistry implements INpmRegistry {
         let auth = `${nerfed}:_authToken=${util.getSystemAccessToken()}`;
 
         return new NpmRegistry(url, auth, authOnly);
+    }
+
+    /**
+     * Helper function to return the domain name given a hostname.
+     * @param hostname This should be the output from: url.parse('http://myserver.example.com').hostname
+     * @returns Given a FQDN the domain name will be returned.  Given a hostname the
+     * hostname will be return.  Given an IP address, the IP will be returned.
+     */
+    private static ParseHostnameForDomain(hostname: string): string {
+        if (hostname &&
+            (!new ipaddress.Address6(hostname).isValid() &&
+             !new ipaddress.Address4(hostname).isValid())) {
+                // We know we have a non-null string that is not an IP addr
+                let hnAry = hostname.split('.');
+                return hnAry.slice(-2).join('.')
+        }
+        return hostname;
     }
 }
