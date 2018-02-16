21e12ed5bebb6e3b3c2f7fc6efe9c53b7891a943
Brian Cristante <brcrista@microsoft.com>
2018-02-16 10:33:56 -0500

    Fixing a couple other localized strings that say 'JDK'. Cleaning up some some more code in the Python tool installer

--- Tasks/PythonToolInstaller/pythonToolInstaller.ts
+++ Tasks/PythonToolInstaller/pythonToolInstaller.ts
@@ -1,69 +1,72 @@
-import fs = require('fs');
-import path = require('path');
-import taskLib = require('vsts-task-lib/task');
-import toolLib = require('vsts-task-tool-lib/tool');
+import * as fs from 'fs';
+import * as path from 'path';
+import * as taskLib from 'vsts-task-lib/task';
+import * as toolLib from 'vsts-task-tool-lib/tool';
 
 import { AzureStorageArtifactDownloader } from "./AzureStorageArtifacts/AzureStorageArtifactDownloader";
 import { FileExtractor } from './FileExtractor/FileExtractor';
-taskLib.setResourcePath(path.join(__dirname, 'task.json'));
 
 async function run() {
     try {
-        let versionSpec = taskLib.getInput('versionSpec', true); // TODO: shouldn't actually accept a versionspec here
-        await getPython(versionSpec);
+        taskLib.setResourcePath(path.join(__dirname, 'task.json'));
+        await getPython(
+            taskLib.getInput('versionSpec', true), // TODO: shouldn't actually accept a versionspec here
+            taskLib.getInput('architectureOption', true),
+            taskLib.getInput('installationSource', true) === 'AzureStorage',
+            taskLib.getPathInput('destinationDirectory', true),
+            taskLib.getBoolInput('cleanDestinationDirectory', false));
     } catch (error) {
         taskLib.error(error.message);
         taskLib.setResult(taskLib.TaskResult.Failed, error.message);
     }
 }
 
-async function getPython(versionSpec: string) {
-    const fromAzure: boolean = ('AzureStorage' == taskLib.getInput('jdkSourceOption', true));
-    const extractLocation: string = taskLib.getPathInput('jdkDestinationDirectory', true);
-    const cleanDestinationDirectory: boolean = taskLib.getBoolInput('cleanDestinationDirectory', false);
-
+async function getPython(versionSpec: string, architecture: string, fromAzure: boolean, destination: string, cleanDestination: boolean) {
     toolLib.debug('Trying to get tool from local cache first');
     const localVersions: string[] = toolLib.findLocalToolVersions('Python');
     const version: string = toolLib.evaluateVersions(localVersions, versionSpec);
 
      // Clean the destination folder before downloading and extracting?
-     if (cleanDestinationDirectory && taskLib.exist(extractLocation) && taskLib.stats(extractLocation).isDirectory) {
-        console.log(taskLib.loc('CleanDestDir', extractLocation));
+     if (cleanDestination && taskLib.exist(destination) && taskLib.stats(destination).isDirectory) {
+        console.log(taskLib.loc('CleanDestDir', destination));
 
         // delete the contents of the destination directory but leave the directory in place
-        for (let item of fs.readdirSync(extractLocation)) {
-            const itemPath = path.join(extractLocation, item);
+        for (let item of fs.readdirSync(destination)) {
+            const itemPath = path.join(destination, item);
             taskLib.rmRF(itemPath);
         }
     }
 
-    const extractedContents = invoke(async () => {
+    const extractedContents = (async () => {
         if (version) { // Tool cache
             console.log(taskLib.loc('Info_ResolvedToolFromCache', version));
             return Promise.resolve<string>(null);
         } else if (fromAzure) { // download from Azure
-            console.log(taskLib.loc('RetrievingJdkFromAzure', version)); // TODO
-            const compressedFileExtension = getFileEnding(taskLib.getInput('azureCommonVirtualFile', true));
+            console.log(taskLib.loc('RetrievingPythonFromAzure', versionSpec, architecture));
+            const file = taskLib.getInput('azureCommonVirtualFile', true);
 
             const azureDownloader = new AzureStorageArtifactDownloader(
                 taskLib.getInput('azureResourceManagerEndpoint', true),
                 taskLib.getInput('azureStorageAccountName', true),
                 taskLib.getInput('azureContainerName', true),
-                taskLib.getInput('azureCommonVirtualFile', false));
+                file);
 
-            await azureDownloader.downloadArtifacts(extractLocation, '*' + compressedFileExtension);
+            const compressedFileExtension = getFileEnding(file);
+            await azureDownloader.downloadArtifacts(destination, '*' + compressedFileExtension);
             await sleep(250); // Wait for the file to be released before extracting it.
 
-            const extractSource = buildFilePath(extractLocation, compressedFileExtension);
-            return new FileExtractor().extractCompressedFile(extractSource, compressedFileExtension, extractLocation);
+            const compressedFile = buildFilePath(destination, file);
+            return new FileExtractor().extractCompressedFile(compressedFile, compressedFileExtension, destination);
         } else { // local directory
-            console.log(taskLib.loc('RetrievingJdkFromLocalPath', version)); // TODO
+            console.log(taskLib.loc('RetrievingPythonFromLocalPath', versionSpec, architecture));
 
-            const file = taskLib.getInput('compressedFile', true)
-            const compressedFileExtension = getFileEnding(file);
-            return new FileExtractor().extractCompressedFile(file, compressedFileExtension, extractLocation);
+            const compressedFile = taskLib.getInput('compressedFile', true)
+            const compressedFileExtension = getFileEnding(compressedFile);
+            return new FileExtractor().extractCompressedFile(compressedFile, compressedFileExtension, destination);
         }
-    });
+    })();
+
+    console.log(extractedContents);
 
     // TODO
     // taskLib.debug(`Set output variable ${x} to ${y}`);
@@ -76,16 +79,9 @@ function sleep(milliseconds: number): Promise<void> {
     });
 }
 
-function invoke<T>(f: (...args: any[]) => T): T {
-    return f();
-}
-
-function buildFilePath(localPathRoot: string, fileEnding: string): string {
-    const azureFileSource: string = taskLib.getInput('azureCommonVirtualFile', true);
-    const fileName = azureFileSource.split(/[\\\/]/).pop();
-    const extractSource = path.join(localPathRoot, fileName);
-
-    return extractSource;
+function buildFilePath(localPathRoot: string, file: string): string {
+    const fileName = file.split(/[\\\/]/).pop();
+    return path.join(localPathRoot, fileName);
 }
 
 function getFileEnding(file: string): string {
