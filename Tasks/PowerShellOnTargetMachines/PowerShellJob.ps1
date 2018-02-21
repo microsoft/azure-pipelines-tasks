ab32b4d71b088c47cf198c35ab5cc6ee67fe67a3
Roshan Kumar <rok@microsoft.com>
2018-02-21 15:57:21 +0530

    after resolving conflict with m123

--- Tasks/PowerShellOnTargetMachines/PowerShellJob.ps1
+++ Tasks/PowerShellOnTargetMachines/PowerShellJob.ps1
@@ -53,6 +53,55 @@ param (
     [String]$psOnRemoteScriptBlockString = "Invoke-PsOnRemote -MachineDnsName $fqdn -ScriptPath `$scriptPath -WinRMPort $port -Credential `$credential -ScriptArguments `$scriptArguments -InitializationScriptPath `$initializationScriptPath -SessionVariables `$parsedSessionVariables $skipCACheckOption $httpProtocolOption $enableDetailedLoggingOption"
     [scriptblock]$psOnRemoteScriptBlock = [scriptblock]::Create($psOnRemoteScriptBlockString)
     $deploymentResponse = Invoke-Command -ScriptBlock $psOnRemoteScriptBlock
-    
+   
+    # Telemetry data logic through ps session
+    try{
+        if($skipCACheckOption)
+        {
+            $sessionOption = New-PSSessionOption -SkipCACheck
+        }
+        else
+        {
+            $sessionOption = New-PSSessionOption 
+        }
+        $secpasswd = ConvertTo-SecureString  $credential.Password -AsPlainText -Force
+        $psCredential =  New-Object System.Management.Automation.PSCredential($credential.UserName, $secpasswd)
+        if($httpProtocolOption -eq '-UseHttp')
+        {
+            $session = New-PSSession -Computer $fqdn -Port $port -Credential $psCredential -SessionOption ($sessionOption )
+        }
+        else
+        {
+            $session = New-PSSession -Computer $fqdn -Port $port -Credential $psCredential -SessionOption ($sessionOption ) -UseSSL
+        }
+        $VmUuidHash  = Invoke-Command -Session  $session -ScriptBlock {
+            $sha = New-Object System.Security.Cryptography.SHA512CryptoServiceProvider
+            $computerDetails = Get-WmiObject -class Win32_ComputerSystemProduct -namespace root\CIMV2
+            $encoding = [system.Text.Encoding]::ASCII
+            $uuidHash = [System.BitConverter]::ToString( $sha.ComputeHash($encoding.GetBytes($computerDetails.UUID)))
+            $uuidHash = $uuidHash  -replace  "-"  , ""
+            return $uuidHash 
+        }
+        $isAzureVm = Invoke-Command -Session  $session -ScriptBlock {
+            (Get-Process -Name 'WindowsAzureGuestAgent' -ErrorAction Ignore) | Select-Object -First 1 |  ForEach-Object {
+                if($_.Path) 
+                {
+                    return $true
+                } 
+                else 
+                { 
+                    return $false
+                } 
+            }
+        }
+        $deploymentResponse | Add-Member "IsAzureVm" $IsAzureVm 
+        $deploymentResponse | Add-Member "VmUuidHash" $VmUuidHash     
+    }
+    catch
+    {
+        Write-Verbose "Error during fetching telemetry = $_"
+        $deploymentResponse | Add-Member "TelemetryError" $_    
+    }
+
     Write-Output $deploymentResponse
 }
