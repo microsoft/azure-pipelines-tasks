b0524ff34cec9bc71dac0bc7d5ecf870b09c5a82
Roshan Kumar <rok@microsoft.com>
2018-03-01 14:43:37 +0530

    In mysql task install expand archive is not working in powershell version lesser than 5

--- Tasks/AzureMysqlDeployment/Window-MysqlCli-Installer.ps1
+++ Tasks/AzureMysqlDeployment/Window-MysqlCli-Installer.ps1
@@ -46,6 +46,77 @@ function Install-EXE
     }
 }
 
+function ExtractFiles
+{
+    <#
+        .DESCRIPTION
+        Extracts the zip file to the location provided.
+        It uses the latest command 'Expand-Archive' if available otherwise it fallsback to shell for extraction
+
+        .PARAMETER ZipPath
+        Path of the zip file to extract.
+        
+        .PARAMETER DestinationPath
+        Directory where the zip file needs to be extracted        
+    #>
+    [CmdletBinding()]
+    Param
+    (
+        [string]$ZipPath,
+        [string]$DestinationPath
+    )
+
+    try
+    {
+        if (-not (Test-Path $ZipPath))
+        {
+            Write-Host "$ZipPath does not exist."
+            return
+        }
+
+        if (-not (Test-Path $DestinationPath))
+        {
+            New-Item -Type Directory $DestinationPath
+        }
+
+        $zipFullPath = [System.IO.Path]::GetFullPath($ZipPath)
+        Write-Host "Extracting $zipFullPath to path: $DestinationPath"
+        if (Get-Command Expand-Archive -ErrorAction SilentlyContinue)
+        {
+            Expand-Archive $zipFullPath $DestinationPath -Force
+        }
+        else 
+        {
+            $shellApplication = New-Object -ComObject Shell.Application
+            if ($shellApplication -eq $null)
+            {
+                Write-Host 'Unable to create Shell.'
+                exit  
+            }            
+            
+            $zipPackage = $shellApplication.NameSpace($zipFullPath)
+            if ($zipPackage -eq $null)
+            {
+                Write-Host 'Unable to create shell zipPackage.'
+                exit
+            }
+
+            $destinationFolder = $shellApplication.NameSpace($DestinationPath) 
+
+            # CopyHere Flags 
+            # 4 - Do not display a progress dialog box. 
+            # 16 - Respond with "Yes to All" for any dialog box that is displayed. 
+
+            $destinationFolder.CopyHere($zipPackage.Items(),20)       
+        }
+    }
+    catch
+    {
+        Write-Host $_.Exception.Message
+        exit
+    }
+}
+
 # Set new machine path 
 function Set-MachinePath{
     [CmdletBinding()]
@@ -95,7 +166,8 @@ if ($exitCode -eq 0 -or $exitCode -eq 3010)
     Invoke-WebRequest -UseBasicParsing -Uri $uri -OutFile mysql.zip
 
     # Expand the zip
-    Expand-Archive -Path mysql.zip -DestinationPath "C:\" -Force
+    $sourcePath = $pwd.Path + "mysql.zip"
+    ExtractFiles  $sourcePath "C:\"
 
     # Deleting zip folder
     Remove-Item -Recurse -Force mysql.zip
