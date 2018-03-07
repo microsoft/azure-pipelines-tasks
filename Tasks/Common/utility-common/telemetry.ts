gluip/vsts-tasks

Telemetry cleanup

Tasks/Common/utility-common/telemetry.ts
@@ -19,21 +19,12 @@ export function emitTelemetry(area: string, feature: string, taskSpecificTelemet
                 'SYSTEM_JOBID': tl.getVariable('SYSTEM_JOBID'),
                 'SYSTEM_PLANID': tl.getVariable('SYSTEM_PLANID'),
                 'SYSTEM_COLLECTIONID': tl.getVariable('SYSTEM_COLLECTIONID'),
-                'AGENT_BUILDDIRECTORY': tl.getVariable('AGENT_BUILDDIRECTORY'),
-                'AGENT_HOMEDIRECTORY': tl.getVariable('AGENT_HOMEDIRECTORY'),
-                'AGENT_WORKFOLDER': tl.getVariable('AGENT_WORKFOLDER'),
-                'AGENT_ROOTDIRECTORY': tl.getVariable('AGENT_ROOTDIRECTORY'),
-                'AGENT_TOOLSDIRECTORY': tl.getVariable('AGENT_TOOLSDIRECTORY'),
-                'AGENT_SERVEROMDIRECTORY': tl.getVariable('AGENT_SERVEROMDIRECTORY'),
-                'AGENT_TEMPDIRECTORY': tl.getVariable('AGENT_TEMPDIRECTORY'),
                 'AGENT_ID': tl.getVariable('AGENT_ID'),
                 'AGENT_MACHINENAME': tl.getVariable('AGENT_MACHINENAME'),
                 'AGENT_NAME': tl.getVariable('AGENT_NAME'),
                 'AGENT_JOBSTATUS': tl.getVariable('AGENT_JOBSTATUS'),
                 'AGENT_OS': tl.getVariable('AGENT_OS'),
                 'AGENT_VERSION': tl.getVariable('AGENT_VERSION'),
-                'BUILD_ARTIFACTSTAGINGDIRECTORY': tl.getVariable('BUILD_ARTIFACTSTAGINGDIRECTORY'),
-                'BUILD_BINARIESDIRECTORY': tl.getVariable('BUILD_BINARIESDIRECTORY'),
                 'BUILD_BUILDID': tl.getVariable('BUILD_BUILDID'),
                 'BUILD_BUILDNUMBER': tl.getVariable('BUILD_BUILDNUMBER'),
                 'BUILD_BUILDURI': tl.getVariable('BUILD_BUILDURI'),
@@ -44,13 +35,10 @@ export function emitTelemetry(area: string, feature: string, taskSpecificTelemet
                 'BUILD_REPOSITORY_CLEAN': tl.getVariable('BUILD_REPOSITORY_CLEAN'),
                 'BUILD_REPOSITORY_GIT_SUBMODULECHECKOUT': tl.getVariable('BUILD_REPOSITORY_GIT_SUBMODULECHECKOUT'),
                 'BUILD_REPOSITORY_ID': tl.getVariable('BUILD_REPOSITORY_ID'),
-                'BUILD_REPOSITORY_LOCALPATH': tl.getVariable('BUILD_REPOSITORY_LOCALPATH'),
                 'BUILD_REPOSITORY_NAME': tl.getVariable('BUILD_REPOSITORY_NAME'),
                 'BUILD_REPOSITORY_PROVIDER': tl.getVariable('BUILD_REPOSITORY_PROVIDER'),
                 'BUILD_REPOSITORY_URI': tl.getVariable('BUILD_REPOSITORY_URI'),
-                'BUILD_SOURCESDIRECTORY': tl.getVariable('BUILD_SOURCESDIRECTORY'),
                 'BUILD_SOURCEVERSION': tl.getVariable('BUILD_SOURCEVERSION'),
-                'BUILD_STAGINGDIRECTORY': tl.getVariable('BUILD_STAGINGDIRECTORY'),
                 'agent.proxyurl': tl.getVariable("agent.proxyurl")
             };
             let copy = Object.assign(commonTelem, taskSpecificTelemetry);
@@ -72,11 +60,10 @@ export function emitTelemetry(area: string, feature: string, taskSpecificTelemet
  * @param exitCode The exit code from your program
  * @param stderr STDERR from your program
  */
-export function logStderr(area: string, feature:string, exitCode: number, stderr: string) {
+export function logStderr(area: string, feature:string, exitCode: number) {
     try {
         let execResultsStr = JSON.stringify({
-            'exitCode': exitCode,
-            'stderr': (stderr) ? stderr.substr(0, 1024) : null
+            'exitCode': exitCode
         });
         let nugetExecResults = JSON.parse(execResultsStr);
         emitTelemetry(area, feature, nugetExecResults);

Tasks/Npm/npmtoolrunner.ts
@@ -59,7 +59,7 @@ export class NpmToolRunner extends tr.ToolRunner {
         const execResult = super.execSync(options);
         this._restoreProjectNpmrc();
         if (execResult.code !== 0) {
-            telemetry.logStderr('Packaging', 'npm', execResult.code, execResult.stderr);
+            telemetry.logStderr('Packaging', 'npm', execResult.code);
             this._printDebugLogSync(this._getDebugLogPath(options));
             throw new Error(tl.loc('NpmFailed', execResult.code));
         }

Tasks/NuGetCommand/nugetcustom.ts
@@ -86,7 +86,7 @@ function runNuGet(executionOptions: NuGetExecutionOptions): IExecSyncResult {
 
     let execResult = nugetTool.execSync();
     if (execResult.code !== 0) {
-        telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code, execResult.stderr);
+        telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code);
         throw tl.loc("Error_NugetFailedWithCodeAndErr",
             execResult.code,
             execResult.stderr ? execResult.stderr.trim() : execResult.stderr);

Tasks/NuGetCommand/nugetpack.ts
@@ -206,7 +206,7 @@ function pack(file: string, options: PackOptions): IExecSyncResult {
 
     let execResult = nugetTool.execSync();
     if (execResult.code !== 0) {
-        telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code, execResult.stderr);
+        telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code);
         throw tl.loc("Error_NugetFailedWithCodeAndErr",
             execResult.code,
             execResult.stderr ? execResult.stderr.trim() : execResult.stderr);

Tasks/NuGetCommand/nugetpublisher.ts
@@ -262,7 +262,7 @@ function publishPackageNuGet(packageFile: string, options: PublishOptions, authI
 
     let execResult = nugetTool.execSync();
     if (execResult.code !== 0) {
-        telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code, execResult.stderr);
+        telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code);
         throw tl.loc("Error_NugetFailedWithCodeAndErr",
             execResult.code,
             execResult.stderr ? execResult.stderr.trim() : execResult.stderr);
@@ -292,7 +292,7 @@ function publishPackageVstsNuGetPush(packageFile: string, options: IVstsNuGetPus
         return;
     }
 
-    telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code, execResult.stderr);
+    telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code);
     throw new Error(tl.loc("Error_UnexpectedErrorVstsNuGetPush",
         execResult.code,
         execResult.stderr ? execResult.stderr.trim() : execResult.stderr));

Tasks/NuGetCommand/nugetrestore.ts
@@ -222,7 +222,7 @@ function restorePackages(solutionFile: string, options: RestoreOptions): IExecSy
 
     let execResult = nugetTool.execSync({ cwd: path.dirname(solutionFile) } as IExecOptions);
     if (execResult.code !== 0) {
-        telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code, execResult.stderr);
+        telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code);
         throw tl.loc("Error_NugetFailedWithCodeAndErr",
             execResult.code,
             execResult.stderr ? execResult.stderr.trim() : execResult.stderr);

microsoft/azure-pipelines-tasks

Tasks/Common/utility-common/telemetry.ts
@@ -19,21 +19,12 @@ export function emitTelemetry(area: string, feature: string, taskSpecificTelemet
                 'SYSTEM_JOBID': tl.getVariable('SYSTEM_JOBID'),
                 'SYSTEM_PLANID': tl.getVariable('SYSTEM_PLANID'),
                 'SYSTEM_COLLECTIONID': tl.getVariable('SYSTEM_COLLECTIONID'),
-                'AGENT_BUILDDIRECTORY': tl.getVariable('AGENT_BUILDDIRECTORY'),
-                'AGENT_HOMEDIRECTORY': tl.getVariable('AGENT_HOMEDIRECTORY'),
-                'AGENT_WORKFOLDER': tl.getVariable('AGENT_WORKFOLDER'),
-                'AGENT_ROOTDIRECTORY': tl.getVariable('AGENT_ROOTDIRECTORY'),
-                'AGENT_TOOLSDIRECTORY': tl.getVariable('AGENT_TOOLSDIRECTORY'),
-                'AGENT_SERVEROMDIRECTORY': tl.getVariable('AGENT_SERVEROMDIRECTORY'),
-                'AGENT_TEMPDIRECTORY': tl.getVariable('AGENT_TEMPDIRECTORY'),
                 'AGENT_ID': tl.getVariable('AGENT_ID'),
                 'AGENT_MACHINENAME': tl.getVariable('AGENT_MACHINENAME'),
                 'AGENT_NAME': tl.getVariable('AGENT_NAME'),
                 'AGENT_JOBSTATUS': tl.getVariable('AGENT_JOBSTATUS'),
                 'AGENT_OS': tl.getVariable('AGENT_OS'),
                 'AGENT_VERSION': tl.getVariable('AGENT_VERSION'),
-                'BUILD_ARTIFACTSTAGINGDIRECTORY': tl.getVariable('BUILD_ARTIFACTSTAGINGDIRECTORY'),
-                'BUILD_BINARIESDIRECTORY': tl.getVariable('BUILD_BINARIESDIRECTORY'),
                 'BUILD_BUILDID': tl.getVariable('BUILD_BUILDID'),
                 'BUILD_BUILDNUMBER': tl.getVariable('BUILD_BUILDNUMBER'),
                 'BUILD_BUILDURI': tl.getVariable('BUILD_BUILDURI'),
@@ -44,13 +35,10 @@ export function emitTelemetry(area: string, feature: string, taskSpecificTelemet
                 'BUILD_REPOSITORY_CLEAN': tl.getVariable('BUILD_REPOSITORY_CLEAN'),
                 'BUILD_REPOSITORY_GIT_SUBMODULECHECKOUT': tl.getVariable('BUILD_REPOSITORY_GIT_SUBMODULECHECKOUT'),
                 'BUILD_REPOSITORY_ID': tl.getVariable('BUILD_REPOSITORY_ID'),
-                'BUILD_REPOSITORY_LOCALPATH': tl.getVariable('BUILD_REPOSITORY_LOCALPATH'),
                 'BUILD_REPOSITORY_NAME': tl.getVariable('BUILD_REPOSITORY_NAME'),
                 'BUILD_REPOSITORY_PROVIDER': tl.getVariable('BUILD_REPOSITORY_PROVIDER'),
                 'BUILD_REPOSITORY_URI': tl.getVariable('BUILD_REPOSITORY_URI'),
-                'BUILD_SOURCESDIRECTORY': tl.getVariable('BUILD_SOURCESDIRECTORY'),
                 'BUILD_SOURCEVERSION': tl.getVariable('BUILD_SOURCEVERSION'),
-                'BUILD_STAGINGDIRECTORY': tl.getVariable('BUILD_STAGINGDIRECTORY'),
                 'agent.proxyurl': tl.getVariable("agent.proxyurl")
             };
             let copy = Object.assign(commonTelem, taskSpecificTelemetry);
@@ -70,13 +58,11 @@ export function emitTelemetry(area: string, feature: string, taskSpecificTelemet
  * A utility function to log the first 1024 characters from SDTERR
  * @param feature The task/feature name for this telemetry
  * @param exitCode The exit code from your program
- * @param stderr STDERR from your program
  */
-export function logStderr(area: string, feature:string, exitCode: number, stderr: string) {
+export function logResult(area: string, feature:string, exitCode: number) {
     try {
         let execResultsStr = JSON.stringify({
-            'exitCode': exitCode,
-            'stderr': (stderr) ? stderr.substr(0, 1024) : null
+            'exitCode': exitCode
         });
         let nugetExecResults = JSON.parse(execResultsStr);
         emitTelemetry(area, feature, nugetExecResults);

Tasks/Npm/npmtoolrunner.ts
@@ -59,7 +59,7 @@ export class NpmToolRunner extends tr.ToolRunner {
         const execResult = super.execSync(options);
         this._restoreProjectNpmrc();
         if (execResult.code !== 0) {
-            telemetry.logStderr('Packaging', 'npm', execResult.code, execResult.stderr);
+            telemetry.logResult('Packaging', 'npm', execResult.code);
             this._printDebugLogSync(this._getDebugLogPath(options));
             throw new Error(tl.loc('NpmFailed', execResult.code));
         }

Tasks/NuGetCommand/nugetcustom.ts
@@ -86,7 +86,7 @@ function runNuGet(executionOptions: NuGetExecutionOptions): IExecSyncResult {
 
     let execResult = nugetTool.execSync();
     if (execResult.code !== 0) {
-        telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code, execResult.stderr);
+        telemetry.logResult('Packaging', 'NuGetCommand', execResult.code);
         throw tl.loc("Error_NugetFailedWithCodeAndErr",
             execResult.code,
             execResult.stderr ? execResult.stderr.trim() : execResult.stderr);

Tasks/NuGetCommand/nugetpack.ts
@@ -206,7 +206,7 @@ function pack(file: string, options: PackOptions): IExecSyncResult {
 
     let execResult = nugetTool.execSync();
     if (execResult.code !== 0) {
-        telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code, execResult.stderr);
+        telemetry.logResult('Packaging', 'NuGetCommand', execResult.code);
         throw tl.loc("Error_NugetFailedWithCodeAndErr",
             execResult.code,
             execResult.stderr ? execResult.stderr.trim() : execResult.stderr);

Tasks/NuGetCommand/nugetpublisher.ts
@@ -262,7 +262,7 @@ function publishPackageNuGet(packageFile: string, options: PublishOptions, authI
 
     let execResult = nugetTool.execSync();
     if (execResult.code !== 0) {
-        telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code, execResult.stderr);
+        telemetry.logResult('Packaging', 'NuGetCommand', execResult.code);
         throw tl.loc("Error_NugetFailedWithCodeAndErr",
             execResult.code,
             execResult.stderr ? execResult.stderr.trim() : execResult.stderr);
@@ -292,7 +292,7 @@ function publishPackageVstsNuGetPush(packageFile: string, options: IVstsNuGetPus
         return;
     }
 
-    telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code, execResult.stderr);
+    telemetry.logResult('Packaging', 'NuGetCommand', execResult.code);
     throw new Error(tl.loc("Error_UnexpectedErrorVstsNuGetPush",
         execResult.code,
         execResult.stderr ? execResult.stderr.trim() : execResult.stderr));

Tasks/NuGetCommand/nugetrestore.ts
@@ -222,7 +222,7 @@ function restorePackages(solutionFile: string, options: RestoreOptions): IExecSy
 
     let execResult = nugetTool.execSync({ cwd: path.dirname(solutionFile) } as IExecOptions);
     if (execResult.code !== 0) {
-        telemetry.logStderr('Packaging', 'NuGetCommand', execResult.code, execResult.stderr);
+        telemetry.logResult('Packaging', 'NuGetCommand', execResult.code);
         throw tl.loc("Error_NugetFailedWithCodeAndErr",
             execResult.code,
             execResult.stderr ? execResult.stderr.trim() : execResult.stderr);