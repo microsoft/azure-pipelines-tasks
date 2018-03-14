71e6d09e055ad9b7c18672e0b4fbe99398f7a3c8
Kees Schollaart <keesschollaart81>
2018-03-14 16:25:08 +0100

    DownloadBuildArtifacts: Rename ArtifactVersion to BuildVersionToDownload + perf optmization

--- Tasks/DownloadBuildArtifacts/main.ts
+++ Tasks/DownloadBuildArtifacts/main.ts
@@ -6,7 +6,7 @@ import * as tl from 'vsts-task-lib/task';
 import { IBuildApi } from './vso-node-api/BuildApi';
 import { IRequestHandler } from './vso-node-api/interfaces/common/VsoBaseInterfaces';
 import { WebApi, getHandlerFromToken } from './vso-node-api/WebApi';
-import { BuildStatus, BuildResult, BuildQueryOrder } from './vso-node-api/interfaces/BuildInterfaces';
+import { BuildStatus, BuildResult, BuildQueryOrder, Build } from './vso-node-api/interfaces/BuildInterfaces';
 
 import * as models from 'artifact-engine/Models';
 import * as engine from 'artifact-engine/Engine';
@@ -65,7 +65,7 @@ async function main(): Promise<void> {
         var definitionIdSpecified: string = null;
         var definitionIdTriggered: string = null;
         var buildId: number = null;
-        var artifactVersion: string = tl.getInput("artifactVersion", false);
+        var buildVersionToDownload: string = tl.getInput("buildVersionToDownload", false);
         var branchName: string =  tl.getInput("branchName", false);;
         var downloadPath: string = tl.getInput("downloadPath", true);
         var downloadType: string = tl.getInput("downloadType", true);
@@ -129,14 +129,15 @@ async function main(): Promise<void> {
                 // Triggering build info not found, or requested, default to specified build info
                 projectId = tl.getInput("project", true);
                 definitionId = definitionIdSpecified;
-                buildId = parseInt(tl.getInput("buildId", artifactVersion == "specific"));
+                buildId = parseInt(tl.getInput("buildId", buildVersionToDownload == "specific"));
             }
         }
 
         // verify that buildId belongs to the definition selected
         if (definitionId) {
-            if (artifactVersion != "specific"){ 
-                var branchNameFilter = (artifactVersion == "latest") ? null : branchName;
+            var build : Build;
+            if (buildVersionToDownload != "specific"){ 
+                var branchNameFilter = (buildVersionToDownload == "latest") ? null : branchName;
                 
                 // get latest successful build filtered by branch
                 var buildsForThisDefinition = await executeWithRetries("getBuildId", () => buildApi.getBuilds( projectId, [parseInt(definitionId)],null,null,null,null,null,null,BuildStatus.Completed,BuildResult.Succeeded,null,null,null,null,null,null, BuildQueryOrder.FinishTimeDescending,branchNameFilter), 4).catch((reason) => {
@@ -145,18 +146,20 @@ async function main(): Promise<void> {
                 }); 
 
                 if (!buildsForThisDefinition || buildsForThisDefinition.length == 0){ 
-                    if (artifactVersion == "latestFromBranch") reject(tl.loc("LatestBuildFromBranchNotFound", branchNameFilter));
+                    if (buildVersionToDownload == "latestFromBranch") reject(tl.loc("LatestBuildFromBranchNotFound", branchNameFilter));
                     else reject(tl.loc("LatestBuildNotFound"));
                     return;
                 }
-                console.log(tl.loc("LatestBuildFound", buildsForThisDefinition[0].id));
-                buildId = buildsForThisDefinition[0].id
+                build = buildsForThisDefinition[0];
+                console.log(tl.loc("LatestBuildFound", build.id));
+                buildId = build.id
             } 
-            
-            var build = await executeWithRetries("getBuild", () => buildApi.getBuild(buildId, projectId), 4).catch((reason) => {
-                reject(reason);
-                return;
-            });
+            if (!build){
+                build = await executeWithRetries("getBuild", () => buildApi.getBuild(buildId, projectId), 4).catch((reason) => {
+                    reject(reason);
+                    return;
+                });
+            }
 
             if (build) {
                 if (!build.definition || build.definition.id !== parseInt(definitionId)) {
