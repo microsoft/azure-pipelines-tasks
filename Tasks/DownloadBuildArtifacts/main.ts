ee50a25c8e08432878095373e0e4e9505071700c
Lydia Polyzou <lypolyzo@microsoft.com>
2018-03-26 14:44:13 -0700

    When variables are part of the Build Definition then they are resolved to a string rather than an ID at run time.

--- Tasks/DownloadBuildArtifacts/main.ts
+++ Tasks/DownloadBuildArtifacts/main.ts
@@ -6,6 +6,7 @@ import * as tl from 'vsts-task-lib/task';
 import { IBuildApi } from './vso-node-api/BuildApi';
 import { IRequestHandler } from './vso-node-api/interfaces/common/VsoBaseInterfaces';
 import { WebApi, getHandlerFromToken } from './vso-node-api/WebApi';
+import { BuildStatus, BuildResult, BuildQueryOrder, Build, BuildDefinitionReference } from './vso-node-api/interfaces/BuildInterfaces';
 
 import * as models from 'artifact-engine/Models';
 import * as engine from 'artifact-engine/Engine';
@@ -64,6 +65,8 @@ async function main(): Promise<void> {
         var definitionIdSpecified: string = null;
         var definitionIdTriggered: string = null;
         var buildId: number = null;
+        var buildVersionToDownload: string = tl.getInput("buildVersionToDownload", false);
+        var branchName: string =  tl.getInput("branchName", false);;
         var downloadPath: string = tl.getInput("downloadPath", true);
         var downloadType: string = tl.getInput("downloadType", true);
 
@@ -126,16 +129,48 @@ async function main(): Promise<void> {
                 // Triggering build info not found, or requested, default to specified build info
                 projectId = tl.getInput("project", true);
                 definitionId = definitionIdSpecified;
-                buildId = parseInt(tl.getInput("buildId", true));
+                buildId = parseInt(tl.getInput("buildId", buildVersionToDownload == "specific"));
             }
         }
 
-        // verify that buildId belongs to the definition selected
-        if (definitionId) {
-            var build = await executeWithRetries("getBuild", () => buildApi.getBuild(buildId, projectId), 4).catch((reason) => {
+        // if the definition name includes a variable then definitionIdSpecified is a name vs a number
+        if (Number.isNaN(parseInt(definitionIdSpecified))) {
+            var definitions: BuildDefinitionReference[] = await executeWithRetries("getBuildDefinitions", () => buildApi.getDefinitions(projectId, definitionIdSpecified), 4).catch((reason) => {
                 reject(reason);
                 return;
             });
+            definitionId = String(definitions[0].id);
+            console.log(tl.loc("DefinitionIDFound", definitionIdSpecified, definitionId));
+        }
+        // verify that buildId belongs to the definition selected
+        if (definitionId) {
+            var build : Build;
+            if (buildVersionToDownload != "specific"){ 
+                var branchNameFilter = (buildVersionToDownload == "latest") ? null : branchName;
+                
+                // get latest successful build filtered by branch
+                var buildsForThisDefinition = await executeWithRetries("getBuildId", () => buildApi.getBuilds( projectId, [parseInt(definitionId)],null,null,null,null,null,null,BuildStatus.Completed,BuildResult.Succeeded,null,null,null,null,null,null, BuildQueryOrder.FinishTimeDescending,branchNameFilter), 4).catch((reason) => {
+                    reject(reason);
+                    return;
+                }); 
+
+                if (!buildsForThisDefinition || buildsForThisDefinition.length == 0){ 
+                    if (buildVersionToDownload == "latestFromBranch") reject(tl.loc("LatestBuildFromBranchNotFound", branchNameFilter));
+                    else reject(tl.loc("LatestBuildNotFound"));
+                    return;
+                }
+                
+                build = buildsForThisDefinition[0];
+                console.log(tl.loc("LatestBuildFound", build.id));
+                buildId = build.id
+            } 
+
+            if (!build){
+                build = await executeWithRetries("getBuild", () => buildApi.getBuild(buildId, projectId), 4).catch((reason) => {
+                    reject(reason);
+                    return;
+                });
+            }
 
             if (build) {
                 if (!build.definition || build.definition.id !== parseInt(definitionId)) {
@@ -249,13 +284,13 @@ async function main(): Promise<void> {
 
 function executeWithRetries(operationName: string, operation: () => Promise<any>, retryCount): Promise<any> {
     var executePromise = new Promise((resolve, reject) => {
-        executeWithRetriesImplementaion(operationName, operation, retryCount, resolve, reject);
+        executeWithRetriesImplementation(operationName, operation, retryCount, resolve, reject);
     });
 
     return executePromise;
 }
 
-function executeWithRetriesImplementaion(operationName: string, operation: () => Promise<any>, currentRetryCount, resolve, reject) {
+function executeWithRetriesImplementation(operationName: string, operation: () => Promise<any>, currentRetryCount, resolve, reject) {
     operation().then((result) => {
         resolve(result);
     }).catch((error) => {
@@ -266,7 +301,7 @@ function executeWithRetriesImplementaion(operationName: string, operation: () =>
         else {
             console.log(tl.loc('RetryingOperation', operationName, currentRetryCount));
             currentRetryCount = currentRetryCount - 1;
-            setTimeout(() => executeWithRetriesImplementaion(operationName, operation, currentRetryCount, resolve, reject), 4 * 1000);
+            setTimeout(() => executeWithRetriesImplementation(operationName, operation, currentRetryCount, resolve, reject), 4 * 1000);
         }
     });
 }
