79199a76a690ae8103f01cb254da8a3433c9d5c2
Roshan Kumar <rok@microsoft.com>
2018-03-27 14:58:19 +0530

    native powershell changes

--- Tasks/AzurePowerShell/Utility.ps1
+++ Tasks/AzurePowerShell/Utility.ps1
@@ -30,7 +30,7 @@ function Update-PSModulePathForHostedAgent {
             $hostedAgentAzureModulePath  =  Get-LatestModule -patternToMatch "^azure_[0-9]+\.[0-9]+\.[0-9]+$"   -patternToExtract "[0-9]+\.[0-9]+\.[0-9]+$" -Classic:$true
         }
 
-        if($authScheme -eq 'ServicePrincipal' -or $authScheme -eq '')
+        if($authScheme -eq 'ServicePrincipal' -or $authScheme -eq '' -or $authScheme -eq 'ManagedServiceIdentity')
         {
             $env:PSModulePath = $hostedAgentAzureModulePath + ";" + $env:PSModulePath
             $env:PSModulePath = $env:PSModulePath.TrimStart(';')
--- Tasks/Common/VstsAzureHelpers_/InitializeFunctions.ps1
+++ Tasks/Common/VstsAzureHelpers_/InitializeFunctions.ps1
@@ -124,21 +124,6 @@ function Initialize-AzureSubscription {
             }
 
             Set-CurrentAzureSubscription -SubscriptionId $Endpoint.Data.SubscriptionId -StorageAccount $StorageAccount
-        }elseif ($Endpoint.Auth.Scheme -eq 'MSI') {
-            $msiUri =  "http://localhost:"+$Endpoint.Data.MsiPort+"/oauth2/token"
-            $response = Invoke-WebRequest -Uri $msiUri -Method GET -Body @{resource= $Endpoint.Url} -Headers @{Metadata="true"}
-            $content =$response.Content | ConvertFrom-Json
-            $access_token = $content.access_token
-            $accountId = "MSI@50342"
-            try {
-                Write-Host "##[command]Add-AzureAccount  -AccessToken $access_token -AccountId $accountId "
-                $null = Add-AzureAccount -AccessToken $access_token -AccountId $accountId
-            } catch {
-                # Provide an additional, custom, credentials-related error message.
-                Write-VstsTaskError -Message $_.Exception.Message
-                throw (New-Object System.Exception((Get-VstsLocString -Key AZ_ManagedServiceIdentityError), $_.Exception))
-            }
-            Set-CurrentAzureSubscription -SubscriptionId $Endpoint.Data.SubscriptionId -StorageAccount $StorageAccount
         } elseif ($script:azureModule) {
             # Throw if >=0.9.9 Azure.
             throw (Get-VstsLocString -Key "AZ_ServicePrincipalAuthNotSupportedAzureVersion0" -ArgumentList $script:azureModule.Version)
@@ -163,7 +148,32 @@ function Initialize-AzureSubscription {
 
             Set-CurrentAzureRMSubscription -SubscriptionId $Endpoint.Data.SubscriptionId -TenantId $Endpoint.Auth.Parameters.TenantId
         }
-    } else {
+    } elseif ($Endpoint.Auth.Scheme -eq 'ManagedServiceIdentity') {
+        $accountId = $env:BUILD_BUILDID 
+        if($env:RELEASE_RELEASEID){
+            $accountId = $env:RELEASE_RELEASEID 
+        }
+        $date = Get-Date -Format o
+        $accountId = -join($accountId, "-", $date)
+        $port = 50342
+        if($Endpoint.Data.MsiPort){
+            $port = $Endpoint.Data.MsiPort
+        }
+        $msiUri =  "http://localhost:$port/oauth2/token"
+        $response = Invoke-WebRequest -Uri $msiUri -Method GET -Body @{resource= $Endpoint.Url} -Headers @{Metadata="true"} -UseBasicParsing
+        $content =$response.Content | ConvertFrom-Json
+        $access_token = $content.access_token
+        try {
+            Write-Host "##[command]Add-AzureRmAccount  -AccessToken $access_token -AccountId $accountId "
+            $null = Add-AzureRmAccount -AccessToken $access_token -AccountId $accountId
+        } catch {
+            # Provide an additional, custom, credentials-related error message.
+            Write-VstsTaskError -Message $_.Exception.Message
+            throw (New-Object System.Exception((Get-VstsLocString -Key AZ_ManagedServiceIdentityError), $_.Exception))
+        }
+        
+        Set-CurrentAzureRMSubscription -SubscriptionId $Endpoint.Data.SubscriptionId -TenantId $Endpoint.Auth.Parameters.TenantId
+    }else {
         throw (Get-VstsLocString -Key AZ_UnsupportedAuthScheme0 -ArgumentList $Endpoint.Auth.Scheme)
     }
 }
