3ccac37eae923aaff6ee580c052981c1a00e34e3
Arjun Gupta <arjgupta@microsoft.com>
2018-04-29 08:54:33 +0530

    refactoring (adding utility.ps1)

--- Tasks/Common/RemoteDeployer/RemoteDeployer.psm1
+++ Tasks/Common/RemoteDeployer/RemoteDeployer.psm1
@@ -1,6 +1,7 @@
 . "$PSScriptRoot/SessionHelper.ps1"
 . "$PSScriptRoot/RunPowerShellScriptJob.ps1"
 . "$PSScriptRoot/JobHelper.ps1"
+. "$PSScriptRoot/Utility.ps1"
 Import-VstsLocStrings "$PSScriptRoot\module.json"
 
 ##############################
@@ -117,64 +118,6 @@ function Invoke-RemoteScript {
         }
         Trace-VstsLeavingInvocation $MyInvocation
     }
-}
-
-function Get-TargetMachines {
-    [CmdletBinding()]
-    Param (
-        [string[]] $targetMachineNames,
-        [pscredential] $credential,
-        [string] $authentication,
-        [string] $sessionConfigurationName,
-        [switch] $useSsl
-    )
-    Trace-VstsEnteringInvocation -InvocationInfo $MyInvocation -Parameter ''
-    try {
-        Write-Verbose "Target Machines: $($targetMachineNames -join ',')"
-        $targetMachines = @();
-        foreach($targetMachineName in $targetMachineNames) {
-            $targetMachine = @{
-                ComputerName = "";
-                WSManPort = "";
-                Credential = $credential;
-                Authentication = $authentication;
-                SessionConfigurationName = $sessionConfigurationName;
-                UseSsl = $useSsl
-            }
-        
-            $computerName, $port = $targetMachineName.Split(':');
-            if([string]::IsNullOrEmpty($port)) {
-                if($useSsl) {
-                    $port = "5986"
-                } else {
-                    $port = "5985"
-                }
-            }
-            $targetMachine.ComputerName = $computerName
-            $targetMachine.WSManPort = $port
-
-            Write-Verbose "ComputerName = $($targetMachine.ComputerName)"
-            Write-Verbose "WSManPort = $($targetMachine.WSManPort)"
-            Write-Verbose "Authentication = $($targetMachine.Authentication)"
-            Write-Verbose "SessionConfigurationName = $($targetMachine.SessionConfigurationName)"
-            Write-Verbose "UseSSL = $($targetMachine.UseSsl)"
-            $targetMachines += $targetMachine
-        }
-        Write-Verbose "Total target machines are: $($targetMachines.Count)"
-        return $targetMachines
-    } finally {
-        Trace-VstsLeavingInvocation $MyInvocation
-    }
-}
-
-$defaultErrorHandler = {
-    Param($object, $computerName)
-    Write-Host ($object | Out-String)
-}
-
-$defaultOutputHandler = {
-    Param($object, $computerName)
-    Write-Host ($object | Out-String)
 }
 
 Export-ModuleMember -Function Invoke-RemoteScript
