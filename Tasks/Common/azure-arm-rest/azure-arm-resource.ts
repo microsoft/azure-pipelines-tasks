e7157788a0b92ed33fb7e6fa075f5a5f26c9c0bd
vinca <vinca@microsoft.com>
2018-04-29 15:40:43 +0530

    handle special characters in app name

--- Tasks/Common/azure-arm-rest/azure-arm-resource.ts
+++ Tasks/Common/azure-arm-rest/azure-arm-resource.ts
@@ -40,7 +40,7 @@ export class Resources {
         httpRequest.method = 'GET';
 
         httpRequest.uri = this._client.getRequestUri('//subscriptions/{subscriptionId}/resources', {},
-        [`$filter=resourceType EQ \'${resourceType}\' AND name EQ \'${resourceName}\'`], '2016-07-01');
+        [`$filter=resourceType EQ \'${encodeURIComponent(resourceType)}\' AND name EQ \'${encodeURIComponent(resourceName)}\'`], '2016-07-01');
 
         var result = [];
         try {
