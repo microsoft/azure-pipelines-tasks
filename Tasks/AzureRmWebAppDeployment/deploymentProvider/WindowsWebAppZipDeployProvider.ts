b5198bbe449ecc93c2fb23964b70398211564ae8
Sumiran Aggarwal <suaggar@microsoft.com>
2018-05-16 17:05:23 +0530

    adding new deployment providers

---
+++ Tasks/AzureRmWebAppDeployment/deploymentProvider/WindowsWebAppZipDeployProvider.ts
@@ -0,0 +1,67 @@
+import { AzureRmWebAppDeploymentProvider } from './AzureRmWebAppDeploymentProvider';
+import tl = require('vsts-task-lib/task');
+import { FileTransformsUtility } from '../operations/FileTransformsUtility';
+import { DeployWar } from '../operations/WarDeploymentUtilities';
+import * as Constant from '../operations/Constants';
+import { WebDeployUtility } from '../operations/WebDeployUtility';
+import { AzureAppServiceUtility } from '../operations/AzureAppServiceUtility';
+import { Package } from 'webdeployment-common/packageUtility';
+import * as ParameterParser from '../operations/parameterparser'
+
+var packageUtility = require('webdeployment-common/packageUtility.js');
+var deployUtility = require('webdeployment-common/utility.js');
+var msDeploy = require('webdeployment-common/deployusingmsdeploy.js');
+const runFromZipAppSetting: string = '-WEBSITE_RUN_FROM_ZIP 1';
+
+export class WindowsWebAppZipDeployProvider extends AzureRmWebAppDeploymentProvider{
+    
+    private zipDeploymentID: string;
+ 
+    public async DeployWebAppStep() {
+        var webPackage = await FileTransformsUtility.applyTransformations(this.taskParams.Package.getPath(), this.taskParams);
+
+        if (this.taskParams.DeploymentType === "zipDeploy") {
+
+            var _isMSBuildPackage = await this.taskParams.Package.isMSBuildPackage(); 
+            if(_isMSBuildPackage){
+                throw Error(tl.loc("Publishusingzipdeploynotsupportedformsbuildpackage"));
+            }
+            else if(this.taskParams.VirtualApplication){
+                throw Error(tl.loc("Publishusingzipdeploynotsupportedforvirtualapplication"));
+            }
+
+            this.deployUsingZipDeploy(webPackage, this.taskParams.UseRunFromZip);
+        }
+        else if (this.taskParams.ScriptType) {
+            this.deployUsingZipDeploy(webPackage, false);
+        }
+        else {
+            this.deployUsingZipDeploy(webPackage, true);
+        }
+
+        await this.PostDeploymentStep();
+    }
+
+    private async deployUsingZipDeploy(webPackage, runFromZip: boolean) {
+        tl.debug("Initiated deployment via kudu service for webapp package : ");
+
+        if(runFromZip){
+            var customApplicationSetting = ParameterParser.parse(runFromZipAppSetting);
+            await this.appServiceUtility.updateAndMonitorAppSettings(customApplicationSetting);
+        }
+
+        this.zipDeploymentID = await this.kuduServiceUtility.zipDeploy(webPackage, runFromZip, this.taskParams.TakeAppOfflineFlag, 
+            { slotName: this.appService.getSlot() });
+
+        await this.appServiceUtility.updateStartupCommandAndRuntimeStack(this.taskParams.RuntimeStack, this.taskParams.StartupCommand);
+    }
+    
+    public async UpdateDeploymentStatus(isDeploymentSuccess: boolean) {
+        if(this.kuduServiceUtility) {
+            await super.UpdateDeploymentStatus(isDeploymentSuccess);
+            if(this.zipDeploymentID && this.activeDeploymentID && isDeploymentSuccess) {
+                await this.kuduServiceUtility.postZipDeployOperation(this.zipDeploymentID, this.activeDeploymentID);
+            }
+        }
+    }
+}
