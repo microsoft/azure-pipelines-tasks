d1749a8bc64fed413096a1b0afdd473bbd637483
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2018-06-04 17:14:38 -0400

    Allow Conda Environment to use the "base" environment (#7303)

--- Tasks/CondaEnvironmentV0/Tests/L0BaseEnvironment.ts
+++ Tasks/CondaEnvironmentV0/Tests/L0BaseEnvironment.ts
@@ -7,8 +7,8 @@ import { TaskMockRunner } from 'vsts-task-lib/mock-run';
 const taskPath = path.join(__dirname, '..', 'main.js');
 const taskRunner = new TaskMockRunner(taskPath);
 
-taskRunner.setInput('createCustomEnvironment', 'true');
-taskRunner.setInput('environmentName', 'test');
+taskRunner.setInput('packageSpecs', 'python=3');
+taskRunner.setInput('installOptions', '--json');
 
 // Mock vsts-task-lib
 taskRunner.setAnswers({
@@ -16,23 +16,15 @@ taskRunner.setAnswers({
         'conda': '/miniconda/bin/conda'
     },
     exec: {
-        'conda create --quiet --prefix /miniconda/envs/test --mkdir --yes': {
+        'conda install python=3 --quiet --yes --json': {
             'code': 0
-        },
-        'conda create --quiet --prefix \\miniconda\\envs\\test --mkdir --yes': {
-            'code': 0
-        },
+        }
     }
 });
 
 // Mock vsts-task-tool-lib
 taskRunner.registerMock('vsts-task-tool-lib/tool', {
     prependPath: () => undefined,
-});
-
-// Mock other dependencies
-mockery.registerMock('fs', {
-    existsSync: () => false
 });
 
 taskRunner.run();
