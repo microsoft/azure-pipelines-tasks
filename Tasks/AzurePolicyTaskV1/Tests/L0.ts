4338873c1f76b09d18201df5804b3bc623b88fa7
t-eaaror <t-eaaror@microsoft.com>
2018-06-13 20:21:22 +0530

    Added Azure Policy Task

---
+++ Tasks/AzurePolicyTaskV1/Tests/L0.ts
@@ -0,0 +1,36 @@
+import * as assert from 'assert';
+import * as ttm from 'vsts-task-lib/mock-test';
+import tl = require('vsts-task-lib');
+import * as path from 'path';
+var AppServiceTests = require("../node_modules/azure-arm-rest/Tests/L0-azure-arm-app-service.js");
+var KuduServiceTests = require("../node_modules/azure-arm-rest/Tests/L0-azure-arm-app-service-kudu-tests.js");
+var ApplicationInsightsTests = require("../node_modules/azure-arm-rest/Tests/L0-azure-arm-appinsights-tests.js");
+var AppInsightsWebTests = require("../node_modules/azure-arm-rest/Tests/L0-azure-arm-appinsights-webtests-tests.js");
+var ResourcesTests = require("../node_modules/azure-arm-rest/Tests/L0-azure-arm-resource-tests.js");
+
+describe('Azure App Service Manage Suite', function() {
+
+    this.timeout(60000);
+
+    before((done) => {
+        try {
+            if(!tl.exist(path.join(__dirname, '..', 'node_modules/azure-arm-rest/Tests/node_modules'))) {
+                tl.cp(path.join( __dirname, 'node_modules'), path.join(__dirname, '..', 'node_modules/azure-arm-rest/Tests'), '-rf', true);
+            }
+        }
+        catch(error) {
+            tl.debug(error);
+        }
+
+        done();
+    });
+    
+    after(function () {
+    });
+    
+    ApplicationInsightsTests.ApplicationInsightsTests();
+    AppServiceTests.AzureAppServiceMockTests();
+    KuduServiceTests.KuduServiceTests();
+    AppInsightsWebTests.ApplicationInsightsTests();
+    ResourcesTests.ResourcesTests();
+});
---
+++ Tasks/AzurePolicyTaskV1/operations/AzureAppServiceUtils.ts
@@ -0,0 +1,83 @@
+import tl = require('vsts-task-lib/task');
+import { AzureAppService } from 'azure-arm-rest/azure-arm-app-service';
+import webClient = require('azure-arm-rest/webClient');
+var parseString = require('xml2js').parseString;
+import Q = require('q');
+import { Kudu } from 'azure-arm-rest/azure-arm-app-service-kudu';
+
+export class AzureAppServiceUtils {
+    private _appService: AzureAppService;
+    constructor(appService: AzureAppService) {
+        this._appService = appService;
+    }
+
+    public async monitorApplicationState(state: string): Promise<void> {
+        state = state.toLowerCase();
+        if(["running", "stopped"].indexOf(state) == -1) {
+            throw new Error(tl.loc('InvalidMonitorAppState', state));
+        }
+
+        while(true) {
+            var appDetails = await this._appService.get(true);
+            if(appDetails && appDetails.properties && appDetails.properties["state"]) {
+                tl.debug(`App Service state: ${appDetails.properties["state"]}`)
+                if(appDetails.properties["state"].toLowerCase() == state) {
+                    tl.debug(`App Service state '${appDetails.properties["state"]}' matched with expected state '${state}'.`);
+                    console.log(tl.loc('AppServiceState', appDetails.properties["state"]));
+                    break;
+                }
+                await webClient.sleepFor(5);
+            }
+            else {
+                tl.debug('Unable to monitor app service details as the state is unknown.');
+                break;
+            }
+        }
+    }
+
+    public async getWebDeployPublishingProfile(): Promise<any> {
+        var publishingProfile = await this._appService.getPublishingProfileWithSecrets();
+        var defer = Q.defer<any>();
+        parseString(publishingProfile, (error, result) => {
+            for (var index in result.publishData.publishProfile) {
+                if (result.publishData.publishProfile[index].$.publishMethod === "MSDeploy") {
+                    defer.resolve(result.publishData.publishProfile[index].$);
+                }
+            }
+            defer.reject(tl.loc('ErrorNoSuchDeployingMethodExists'));
+        });
+
+        return defer.promise;
+    }
+
+    public async pingApplication(): Promise<void> {
+        try {
+            var applicationUrl: string = (await this.getWebDeployPublishingProfile()).destinationAppUrl;
+
+            if(!applicationUrl) {
+                tl.debug('Application Url not found.');
+                return;
+            }
+            var webRequest = new webClient.WebRequest();
+            webRequest.method = 'GET';
+            webRequest.uri = applicationUrl;
+            tl.debug('pausing for 5 seconds before request');
+            await webClient.sleepFor(5);
+            var response = await webClient.sendRequest(webRequest);
+            tl.debug(`App Service status Code: '${response.statusCode}'. Status Message: '${response.statusMessage}'`);
+        }
+        catch(error) {
+            tl.debug(`Unable to ping App Service. Error: ${error}`);
+        }
+    }
+
+    public async getKuduService(): Promise<Kudu> {
+        var publishingCredentials = await this._appService.getPublishingCredentials();
+        if(publishingCredentials.properties["scmUri"]) {
+            tl.setVariable(`AZURE_APP_SERVICE_KUDU_${this._appService.getSlot()}_PASSWORD`, publishingCredentials.properties["publishingPassword"], true);
+            return new Kudu(publishingCredentials.properties["scmUri"], publishingCredentials.properties["publishingUserName"], publishingCredentials.properties["publishingPassword"]);
+        }
+
+        throw Error(tl.loc('KuduSCMDetailsAreEmpty'));
+    }
+}
