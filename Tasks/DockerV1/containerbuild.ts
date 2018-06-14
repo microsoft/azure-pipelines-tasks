0130baee04c1edd2a5735b31db3c1588c36219e1
Vikranth Thati <vithati@microsoft.com>
2018-06-14 17:48:07 +0530

    Added dns to labels

--- Tasks/DockerV1/containerbuild.ts
+++ Tasks/DockerV1/containerbuild.ts
@@ -5,7 +5,9 @@ import * as tl from "vsts-task-lib/task";
 import ContainerConnection from "docker-common/containerconnection";
 import * as sourceUtils from "docker-common/sourceutils";
 import * as imageUtils from "docker-common/containerimageutils";
+import * as URL from 'url';
 import * as utils from "./utils";
+import * as util from "util";
 import { ToolRunner } from "vsts-task-lib/toolrunner";
 
 function findDockerFile(dockerfilepath : string) : string {
@@ -29,39 +31,55 @@ function findDockerFile(dockerfilepath : string) : string {
     }
 }
 
-function addCommonLabels(command: ToolRunner): void {
-    command.arg(["--label", "com.visualstudio.image.system.teamfoundationcollectionuri=" + tl.getVariable("SYSTEM_TEAMFOUNDATIONCOLLECTIONURI")]);
-    command.arg(["--label", "com.visualstudio.image.system.teamproject=" + tl.getVariable("SYSTEM_TEAMPROJECT")]);
+function addCommonLabels(command: ToolRunner, hostName: string): void {
+    command.arg(["--label", util.format("%s.image.system.teamfoundationcollectionuri=%s", hostName, tl.getVariable("SYSTEM_TEAMFOUNDATIONCOLLECTIONURI"))]);
+    command.arg(["--label", util.format("%s.image.system.teamproject=%s", hostName, tl.getVariable("SYSTEM_TEAMPROJECT"))]);
     var repoName = tl.getVariable("BUILD_REPOSITORY_NAME");
     if (repoName) {
-        command.arg(["--label", "com.visualstudio.image.build.repository.name=" + repoName]);
+        command.arg(["--label", util.format("%s.image.build.repository.name=%s", hostName, repoName)]);
     }
 }
 
-function addBuildLabels(command: ToolRunner): void {
+function addBuildLabels(command: ToolRunner, hostName: string): void {
     var repoUri = tl.getVariable("BUILD_REPOSITORY_URI");
     if (repoUri) {
-        command.arg(["--label", "com.visualstudio.image.build.repository.uri=" + repoUri]);
+        command.arg(["--label", util.format("%s.image.build.repository.uri=%s", hostName, repoUri)]);
     }
     var branchName = tl.getVariable("BUILD_SOURCEBRANCHNAME");
     if (branchName) {
-        command.arg(["--label", "com.visualstudio.image.build.sourcebranchname=" + branchName]);
+        command.arg(["--label", util.format("%s.image.build.sourcebranchname=%s", hostName, branchName)]);
     }
     var sourceVersion = tl.getVariable("BUILD_SOURCEVERSION");
     if (sourceVersion) {
-        command.arg(["--label", "com.visualstudio.image.build.sourceversion=" + sourceVersion]);
+        command.arg(["--label", util.format("%s.image.build.sourceversion=%s", hostName, sourceVersion)]);
     }
-    command.arg(["--label", "com.visualstudio.image.build.definitionname=" + tl.getVariable("BUILD_DEFINITIONNAME")]);
-    command.arg(["--label", "com.visualstudio.image.build.buildnumber=" + tl.getVariable("BUILD_BUILDNUMBER")]);
-    command.arg(["--label", "com.visualstudio.image.build.builduri=" + tl.getVariable("BUILD_BUILDURI")]);
-    command.arg(["--label", "com.visualstudio.image.build.requestedfor=" + tl.getVariable("BUILD_REQUESTEDFOR")]);
+
+    command.arg(["--label", util.format("%s.image.build.definitionname=%s", hostName, tl.getVariable("BUILD_DEFINITIONNAME"))]);
+    command.arg(["--label", util.format("%s.image.build.buildnumber=%s", hostName, tl.getVariable("BUILD_BUILDNUMBER"))]);
+    command.arg(["--label", util.format("%s.image.build.builduri=%s", hostName, tl.getVariable("BUILD_BUILDURI"))]);
+    command.arg(["--label", util.format("%s.image.build.requestedfor=%s", hostName, tl.getVariable("BUILD_REQUESTEDFOR"))]);
 }
 
-function addReleaseLabels(command: ToolRunner): void {
-    command.arg(["--label", "com.visualstudio.image.release.definitionname=" + tl.getVariable("RELEASE_DEFINITIONNAME")]);
-    command.arg(["--label", "com.visualstudio.image.release.releaseid=" + tl.getVariable("RELEASE_RELEASEID")]);
-    command.arg(["--label", "com.visualstudio.image.release.releaseweburl=" + tl.getVariable("RELEASE_RELEASEWEBURL")]);
-    command.arg(["--label", "com.visualstudio.image.release.deployment.requestedfor=" + tl.getVariable("RELEASE_DEPLOYMENT_REQUESTEDFOR")]);
+function addReleaseLabels(command: ToolRunner, hostName: string): void {
+    
+    command.arg(["--label", util.format("%s.image.release.definitionname=%s", hostName, tl.getVariable("RELEASE_DEFINITIONNAME"))]);
+    command.arg(["--label", util.format("%s.image.release.releaseid=%s", hostName, tl.getVariable("RELEASE_RELEASEID"))]);
+    command.arg(["--label", util.format("%s.image.release.releaseweburl=%s", hostName, tl.getVariable("RELEASE_RELEASEWEBURL"))]);
+    command.arg(["--label", util.format("%s.image.release.deployment.requestedfor=%s", hostName, tl.getVariable("RELEASE_DEPLOYMENT_REQUESTEDFOR"))]);
+}
+
+function getReverseDNSName(): string {
+    var teamFoundationCollectionURI = tl.getVariable("SYSTEM_TEAMFOUNDATIONCOLLECTIONURI");
+    if (teamFoundationCollectionURI) {
+        var parsedUrl = URL.parse(teamFoundationCollectionURI);
+        if (parsedUrl) {
+            var hostName = parsedUrl.hostname.split(".").reverse().join(".");
+            tl.debug(`Reverse DNS name ${hostName}`);
+            return hostName;
+        }
+    }
+
+    return null;
 }
 
 export function run(connection: ContainerConnection): any {
@@ -78,14 +96,17 @@ export function run(connection: ContainerConnection): any {
     command.arg(["-f", dockerFile]);
 
     var addDefaultLabels = tl.getBoolInput("addDefaultLabels");
-    if (addDefaultLabels) {
-        var hostType = tl.getVariable("SYSTEM_HOSTTYPE");
-        addCommonLabels(command);
-        if (hostType === "build") {            
-            addBuildLabels(command);
-        }
-        else if (hostType === "release") {
-            addReleaseLabels(command);
+    if (addDefaultLabels) {        
+        var hostName = getReverseDNSName();
+        if (hostName) {
+            var hostType = tl.getVariable("SYSTEM_HOSTTYPE");
+            addCommonLabels(command, hostName);
+            if (hostType === "build") {            
+                addBuildLabels(command, hostName);
+            }
+            else if (hostType === "release") {
+                addReleaseLabels(command, hostName);
+            }
         }
     }
 
