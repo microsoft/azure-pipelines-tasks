ded423fc3b7c0c6407a8421023af2915b8b4fe32
Brian Cristante <brcrista@microsoft.com>
2018-06-20 17:16:56 -0400

    Remove dynamic import of securefiles-common-mock

--- Tasks/AndroidSigningV2/Tests/L0DownloadKeystoreFile.ts
+++ Tasks/AndroidSigningV2/Tests/L0DownloadKeystoreFile.ts
@@ -1,8 +1,8 @@
 import * as path from 'path';
 
-import * as ma from 'vsts-task-lib/mock-answer';
 import * as tmrm from 'vsts-task-lib/mock-run';
 
+import * as secureFileHelperMock from 'securefiles-common/securefiles-common-mock';
 import * as sinon from 'sinon';
 
 let taskPath = path.join(__dirname, '..', 'preandroidsigning.js');
@@ -19,18 +19,10 @@ const getTaskVariable = sinon.stub();
 getTaskVariable.withArgs('KEYSTORE_FILE_PATH').returns('/some/store');
 tr.registerMockExport('getTaskVariable', getTaskVariable);
 
-let secureFileHelperMock = require('securefiles-common/securefiles-common-mock');
 tr.registerMock('securefiles-common/securefiles-common', secureFileHelperMock);
 
 tr.registerMock('fs', {
-    writeFileSync: function (filePath, contents) {
-    }
+    writeFileSync: () => {}
 });
 
-// provide answers for task mock
-let a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
-};
-tr.setAnswers(a);
-
 tr.run();
-
