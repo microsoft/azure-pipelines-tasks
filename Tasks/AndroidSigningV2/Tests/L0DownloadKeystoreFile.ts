8e0d2e6f853caf3e5312a4708df554d0bb1cbf1a
Brian Cristante <brcrista@microsoft.com>
2018-06-21 10:29:42 -0400

    Fix infallible DownloadKeystoreFile test

--- Tasks/AndroidSigningV2/Tests/L0DownloadKeystoreFile.ts
+++ Tasks/AndroidSigningV2/Tests/L0DownloadKeystoreFile.ts
@@ -5,11 +5,16 @@ import { TaskMockRunner } from 'vsts-task-lib/mock-run';
 import * as secureFileHelperMock from 'securefiles-common/securefiles-common-mock';
 import * as sinon from 'sinon';
 
-let taskPath = path.join(__dirname, '..', 'preandroidsigning.js');
+const taskPath = path.join(__dirname, '..', 'preandroidsigning.js');
 const taskRunner = new TaskMockRunner(taskPath);
 
-taskRunner.setInput('jarsign', 'true');
-taskRunner.setInput('keystoreFile', 'mySecureFileId');
+const getInput = sinon.stub();
+getInput.withArgs('keystoreFile').returns('mySecureFileId');
+taskRunner.registerMockExport('getInput', getInput);
+
+const getBoolInput = sinon.stub();
+getBoolInput.withArgs('jarsign').returns(true);
+taskRunner.registerMockExport('getBoolInput', getBoolInput);
 
 const getVariable = sinon.stub();
 getVariable.withArgs('AGENT_VERSION').returns('2.116.0');
