4b8a72a4df3dabd8de4afbb57d10f97d7369b63b
Vinod Kumar <kuvinod@microsoft.com>
2018-07-05 15:47:05 +0530

    Revert "typo"
    This reverts commit f04d14e51402bb74eea7944b74e7a931c09aa127.

--- Tasks/ServiceFabricDeployV1/ServiceFabricSDK/Publish-UpgradedServiceFabricApplication.ps1
+++ Tasks/ServiceFabricDeployV1/ServiceFabricSDK/Publish-UpgradedServiceFabricApplication.ps1
@@ -279,11 +279,6 @@ function Publish-UpgradedServiceFabricApplication
                 'ApplicationPathInImageStore' = $applicationPackagePathInImageStore
             }
 
-            if ($RegisterPackageTimeoutSec)
-            {
-                $registerParameters['TimeoutSec'] = $RegisterPackageTimeoutSec
-            }
-
             Write-Host (Get-VstsLocString -Key SFSDK_RegisterAppType)
             Register-ServiceFabricApplicationTypeAction -RegisterParameters $registerParameters -ApplicationTypeName $names.ApplicationTypeName -ApplicationTypeVersion $names.ApplicationTypeVersion
         }
