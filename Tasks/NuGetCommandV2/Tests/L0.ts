597fea9b063ea1a25aa4ea3389e1ffb3034f9b80
Satu Bailey <satbai@microsoft.com>
2018-07-11 15:35:14 -0700

    Change NuGet pack version regex to match dotnet pack eqivalent

--- Tasks/NuGetCommandV2/Tests/L0.ts
+++ Tasks/NuGetCommandV2/Tests/L0.ts
@@ -305,7 +305,7 @@ describe('NuGetCommand Suite', function () {
 
         tr.run()
         assert(tr.invokedToolCount == 1, 'should have run NuGet once');
-        assert(tr.ran('c:\\from\\tool\\installer\\nuget.exe pack c:\\agent\\home\\directory\\foo.nuspec -NonInteractive -OutputDirectory C:\\out\\dir -version 1.2.3'), 'it should have run NuGet');
+        assert(tr.ran('c:\\from\\tool\\installer\\nuget.exe pack c:\\agent\\home\\directory\\foo.nuspec -NonInteractive -OutputDirectory C:\\out\\dir -version 1.2.3-prerelease'), 'it should have run NuGet');
         assert(tr.stdOutContained('setting console code page'), 'it should have run chcp');
         assert(tr.stdOutContained('NuGet output here'), "should have nuget output");
         assert(tr.succeeded, 'should have succeeded');
