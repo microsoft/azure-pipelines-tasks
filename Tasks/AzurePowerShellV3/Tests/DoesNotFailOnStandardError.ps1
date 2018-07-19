9359e52ea53cc2a760537409bd7880c50818880c
asranja <asranja@microsoft.com>
2018-07-19 11:16:18 +0530

    Fixing L0 test failure

--- Tasks/AzurePowerShellV3/Tests/DoesNotFailOnStandardError.ps1
+++ Tasks/AzurePowerShellV3/Tests/DoesNotFailOnStandardError.ps1
@@ -11,6 +11,7 @@ Register-Mock Get-VstsInput { "continue" } -- -Name errorActionPreference
 Register-Mock Get-VstsInput { $false } -- -Name FailOnStandardError
 Register-Mock Update-PSModulePathForHostedAgent
 Register-Mock Initialize-Azure
+Register-Mock Remove-EndpointSecrets
 
 # Act.
 $actual = @( & $PSScriptRoot\..\AzurePowerShell.ps1 )
