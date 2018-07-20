4b9f5e70d4a90cc2c04b1a855a85e93db5af4f5c
Navin <navb@microsoft.com>
2018-07-20 11:21:57 +0530

    Seding this as the envvars for the TestResultsPublisher exe and tslint fixes.

--- Tasks/PublishTestResultsV2/publishtestresultstool.ts
+++ Tasks/PublishTestResultsV2/publishtestresultstool.ts
@@ -4,13 +4,12 @@ import * as path from 'path';
 import * as tl from 'vsts-task-lib/task';
 import * as tr from 'vsts-task-lib/toolrunner';
 import { publishEvent } from './cieventlogger';
-var uuidV4 = require('uuid/v4');
 let uuid = require('uuid');
 
 export class TestResultsPublisher {
     constructor(matchingTestResultsFiles: string[], mergeResults: string, platform: string, config: string,
-        testRunTitle: string, publishRunAttachments: string, testRunner: string) {
-        
+        testRunTitle: string, publishRunAttachments: string, testRunner: string, testRunSystem: string) {
+
         this.matchingTestResultsFiles = matchingTestResultsFiles.slice(0);
         this.mergeResults = mergeResults;
         this.platform = platform;
@@ -18,18 +17,19 @@ export class TestResultsPublisher {
         this.testRunTitle = testRunTitle;
         this.publishRunAttachments = publishRunAttachments;
         this.testRunner = testRunner;
+        this.testRunSystem = testRunSystem;
     }
 
     public async publishResultsThroughExe(): Promise<number> {
-        let testResultsPublisherTool: tr.ToolRunner = tl.tool(this.getTestResultsPublisherLocation());
-        let envVars: { [key: string]: string; } = this.getEnvironmentVariables();
-        let args: string[] = this.getArguments(this.matchingTestResultsFiles);
+        const testResultsPublisherTool: tr.ToolRunner = tl.tool(this.getTestResultsPublisherLocation());
+        const envVars: { [key: string]: string; } = this.getEnvironmentVariables();
+        const args: string[] = this.getArguments(this.matchingTestResultsFiles);
         if (testResultsPublisherTool == null || args == null) {
             return 20000;
         }
         testResultsPublisherTool.arg(args);
-        
-        let exitCode: number = await testResultsPublisherTool.exec(<tr.IExecOptions>{ env: envVars, ignoreReturnCode: true });
+
+        const exitCode: number = await testResultsPublisherTool.exec(<tr.IExecOptions>{ env: envVars, ignoreReturnCode: true });
 
         return exitCode;
     }
@@ -39,33 +39,32 @@ export class TestResultsPublisher {
     }
 
     private getArguments(matchingTestResultsFiles: string[]): string[] {
-        let responseFilePath = this.createResponseFile(matchingTestResultsFiles);
+        const responseFilePath = this.createResponseFile(matchingTestResultsFiles);
         if (responseFilePath == null) {
             return null;
         }
         // Adding '@' because this is a response file argument
-        let args = ['@' + responseFilePath];
-        
+        const args = ['@' + responseFilePath];
+
         return args;
     }
 
     private createResponseFile(matchingTestResultsFiles: string[]): string {
         let responseFilePath: string = null;
         try {
-            let agentTempDirectory = tl.getVariable('Agent.TempDirectory');
-            // The response file is being created in agent temp directory so that it is automatically deleted after.         
+            const agentTempDirectory = tl.getVariable('Agent.TempDirectory');
+            // The response file is being created in agent temp directory so that it is automatically deleted after.
             responseFilePath = path.join(agentTempDirectory, uuid.v1() + '.txt');
-            
+
             // Adding quotes around matching file names
             matchingTestResultsFiles = this.modifyMatchingFileName(matchingTestResultsFiles);
-            
+
             // Preparing File content
-            let fileContent: string = os.EOL + matchingTestResultsFiles.join(os.EOL);
+            const fileContent: string = os.EOL + matchingTestResultsFiles.join(os.EOL);
 
             // Writing matching file names in the response file
             fs.writeFileSync(responseFilePath, fileContent);
-        }
-        catch (ex) {
+        } catch (ex) {
             // Log telemetry and return null path
             publishEvent({
                 "exception": ex
@@ -90,15 +89,19 @@ export class TestResultsPublisher {
     private getEnvironmentVariables(): { [key: string]: string; } {
         let envVars: { [key: string]: string; } = {};
 
-        envVars = this.addToProcessEnvVars(envVars, 'collectionurl', tl.getVariable('System.TeamFoundationCollectionUri'));
-        envVars = this.addToProcessEnvVars(envVars, 'accesstoken', tl.getEndpointAuthorizationParameter('SystemVssConnection', 'AccessToken', false));
-
+        envVars = this.addToProcessEnvVars(envVars,
+             'collectionurl',
+              tl.getVariable('System.TeamFoundationCollectionUri'));
+        envVars = this.addToProcessEnvVars(envVars,
+            'accesstoken',
+             tl.getEndpointAuthorizationParameter('SystemVssConnection', 'AccessToken', false));
         envVars = this.addToProcessEnvVars(envVars, 'testrunner', this.testRunner);
         envVars = this.addToProcessEnvVars(envVars, 'mergeresults', this.mergeResults);
         envVars = this.addToProcessEnvVars(envVars, 'platform', this.platform);
         envVars = this.addToProcessEnvVars(envVars, 'config', this.config);
         envVars = this.addToProcessEnvVars(envVars, 'publishrunattachments', this.publishRunAttachments);
         envVars = this.addToProcessEnvVars(envVars, 'testruntitle', this.testRunTitle);
+        envVars = this.addToProcessEnvVars(envVars, 'testrunsystem', this.testRunSystem);
         envVars = this.addToProcessEnvVars(envVars, 'projectname', tl.getVariable('System.TeamProject'));
         envVars = this.addToProcessEnvVars(envVars, 'owner', tl.getVariable('Build.RequestedFor'));
         envVars = this.addToProcessEnvVars(envVars, 'buildid', tl.getVariable('Build.BuildId'));
@@ -127,4 +130,5 @@ export class TestResultsPublisher {
     private testRunTitle: string;
     private publishRunAttachments: string;
     private testRunner: string;
+    private testRunSystem: string;
 }
