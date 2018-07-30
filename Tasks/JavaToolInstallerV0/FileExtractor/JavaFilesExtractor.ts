8b19f2af5c7e48c459f67baaa2a71003473ebb55
Kellie Jos <kej@microsoft.com>
2018-07-30 15:26:47 -0400

    Added types and comments

--- Tasks/JavaToolInstallerV0/FileExtractor/JavaFilesExtractor.ts
+++ Tasks/JavaToolInstallerV0/FileExtractor/JavaFilesExtractor.ts
@@ -65,12 +65,12 @@ export class JavaFilesExtractor {
 
     private async tarExtract(file, destinationFolder) {
         console.log(taskLib.loc('TarExtractFile', file));
-        const tarLocation = taskLib.which('tar', true);
-        const tar = taskLib.tool(tarLocation);
-        tar.arg('-xvf');
-        tar.arg(file);
-        tar.arg('-C');
-        tar.arg(destinationFolder);
+        const tarLocation: string = taskLib.which('tar', true);
+        const tar: tr.ToolRunner = taskLib.tool(tarLocation);
+        tar.arg('-xvf'); // extract, verbose, show file name type of the archive file
+        tar.arg(file); // file to be extracted
+        tar.arg('-C'); // untar in specified directory
+        tar.arg(destinationFolder); // directory into which files are to be extracted
         tar.execSync();
     }
 
