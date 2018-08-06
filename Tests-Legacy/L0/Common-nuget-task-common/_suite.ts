1840480ca358de3030f6ff67c4aa37656edb1c20
Jah Emanuel Quintero <33764764+jahquin-msft@users.noreply.github.com>
2018-08-06 12:15:19 -0700

    Users/jahquin/cp test fixes (#7941)
    * Fixing tests (#7937)
    
    * Fixing tests due to Location fix (#7940)

--- Tests-Legacy/L0/Common-nuget-task-common/_suite.ts
+++ Tests-Legacy/L0/Common-nuget-task-common/_suite.ts
@@ -21,38 +21,6 @@ describe("Common-NuGetTaskCommon Suite", () => {
     after(function () {
 
     });
-    describe("assumeNuGetUriPrefixes", function () {
-        it("assume pkg service from visualtudio.com", done => {
-            locationHelper.assumeNuGetUriPrefixes("https://contoso.visualstudio.com/foo")
-                .then(assumedPrefixes => {
-                    assert.strictEqual(assumedPrefixes.length, 2, "should have exactly two prefix results");
-                    assert(assumedPrefixes.indexOf("https://contoso.pkgs.visualstudio.com/foo") !== -1, "should contain a pkgs host");
-                    assert(assumedPrefixes.indexOf("https://contoso.visualstudio.com/foo") !== -1, "should contain the original string");
-                    done();
-                })
-                .fail(done);
-        });
-
-        it("assume no pkg service from not visualtudio.com", done => {
-            locationHelper.assumeNuGetUriPrefixes("https://contoso.example.com/foo")
-                .then(assumedPrefixes => {
-                    assert.strictEqual(assumedPrefixes.length, 1, "should have exactly one prefix result")
-                    assert(assumedPrefixes.indexOf("https://contoso.example.com/foo") !== -1, "should contain the original string");
-                    done();
-                })
-                .fail(done)
-        });
-
-        it("assume no pkg service from localhost", done => {
-            locationHelper.assumeNuGetUriPrefixes("https://localhost/foo")
-                .then(assumedPrefixes => {
-                    assert.strictEqual(assumedPrefixes.length, 1, "should have exactly one prefix result")
-                    assert(assumedPrefixes.indexOf("https://localhost/foo") !== -1, "should contain the original string");
-                    done();
-                })
-                .fail(done);
-        });
-    });
 
     describe("NuGetQuirks", function () {
         // interface VersionQuirks {
