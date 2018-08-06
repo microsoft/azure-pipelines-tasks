ebc5b6001e1c2220d17681de656cf6818d4076e3
Jah Emanuel Quintero <33764764+jahquin-msft@users.noreply.github.com>
2018-08-06 09:44:39 -0700

    Fixing tests (#7937)

--- Tasks/NuGetPublisherV0/Tests/internal.ts
+++ Tasks/NuGetPublisherV0/Tests/internal.ts
@@ -43,5 +43,6 @@ nmh.registerNugetUtilityMock(["c:\\agent\\home\\directory\\package.nupkg"]);
 nmh.registerDefaultNugetVersionMock();
 nmh.registerNugetConfigMock();
 nmh.registerToolRunnerMock();
+nmh.registerNugetLocationHelpersMock();
 
 tmr.run();
