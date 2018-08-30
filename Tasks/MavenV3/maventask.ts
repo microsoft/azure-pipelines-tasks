08fe85111a9803f4dfa0fd1dd15098678ac79aef
kelliejos <31480321+kelliejos@users.noreply.github.com>
2018-08-30 12:36:30 -0400

    Updating Maven task to use new locator server calls for authenticated feeds (#8196)
    * Adding code to use new utility for authenticated feeds

--- Tasks/MavenV3/maventask.ts
+++ Tasks/MavenV3/maventask.ts
@@ -2,7 +2,6 @@
 import Q = require('q');
 import os = require('os');
 import path = require('path');
-import fs = require('fs');
 
 import tl = require('vsts-task-lib/task');
 import {ToolRunner} from 'vsts-task-lib/toolrunner';
@@ -194,7 +193,7 @@ async function execBuild() {
                     }
                     return util.mergeCredentialsIntoSettingsXml(settingsXmlFile, repositories);
                 })
-                .fail(function (err) {
+                .catch(function (err) {
                     return Q.reject(err);
                 });
             } else {
