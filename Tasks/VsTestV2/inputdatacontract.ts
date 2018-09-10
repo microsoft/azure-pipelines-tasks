3371b07a476b3940c512d2eb412566f654f92077
Faizan Ahmad <faahmad@microsoft.com>
2018-09-10 14:10:20 +0530

    changed variable name from BuildSourcesDirectory to SourcesDirectory

--- Tasks/VsTestV2/inputdatacontract.ts
+++ Tasks/VsTestV2/inputdatacontract.ts
@@ -17,7 +17,7 @@ export interface InputDataContract {
     DistributionSettings : DistributionSettings;
     ExecutionSettings : ExecutionSettings;
     Logging : Logging;
-    BuildSourcesDirectory : string;
+    SourcesDirectory : string;
 }
 
 export interface TestReportingSettings {
