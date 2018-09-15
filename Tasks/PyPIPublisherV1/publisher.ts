587661b4c7da8268fc8b4ce27c7760152a3ea49f
Brian Cristante <brcrista@microsoft.com>
2018-09-15 11:00:13 -0700

    Don't call setResult multiple times (doesn't change behavior but confusing)

--- Tasks/PyPIPublisherV1/publisher.ts
+++ Tasks/PyPIPublisherV1/publisher.ts
@@ -2,6 +2,8 @@ import * as tl from 'vsts-task-lib/task';
 import * as os from 'os';
 import * as path from 'path';
 
+tl.setResourcePath(path.join(__dirname, 'task.json'));
+
 const serviceEndpointId = tl.getInput('serviceEndpoint', true);
 
 // Generic service endpoint
@@ -23,19 +25,6 @@ password=${password}`;
 
 tl.writeFile(pypircFilePath, text, 'utf8');
 
-async function run() {
-    try {
-        await executePythonTool("-m pip install twine --user");
-        await executePythonTool("-m twine upload dist/*");
-    } catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message);
-    } finally {
-        // Delete .pypirc file
-        tl.rmRF(pypircFilePath);
-        tl.setResult(tl.TaskResult.Succeeded, '');
-    }
-}
-
 async function executePythonTool(commandToExecute: string): Promise<void> {
     const python = tl.tool('python');
     python.line(commandToExecute);
@@ -45,6 +34,19 @@ async function executePythonTool(commandToExecute: string): Promise<void> {
     } catch (err) {
         // vsts-task-lib 2.1.0: `ToolRunner` does not localize its error messages
         throw new Error(tl.loc('UploadFailed', err));
+    }
+}
+
+async function run() {
+    try {
+        await executePythonTool("-m pip install twine --user");
+        await executePythonTool("-m twine upload dist/*");
+        tl.setResult(tl.TaskResult.Succeeded, '');
+    } catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    } finally {
+        // Delete .pypirc file
+        tl.rmRF(pypircFilePath);
     }
 }
 
