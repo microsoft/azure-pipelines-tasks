21cf05dde6552b9d1a830688c67f75365225ce9e
Bishal Prasad <biprasad@microsoft.com>
2018-10-03 19:45:49 +0530

    cleanup

--- Tasks/DotNetCoreInstallerV0/releasesfetcher.ts
+++ Tasks/DotNetCoreInstallerV0/releasesfetcher.ts
@@ -29,20 +29,21 @@ export class DotNetCoreReleaseFetcher {
             let blobUrl: string = release['blob-' + type];
             let dlcUrl: string = release['dlc-' + type];
             let fileName: string = release[type + '-' + osSuffixes[0]] ? release[type + '-' + osSuffixes[0]] : release[type + '-' + osSuffixes[1]];
+            fileName = fileName.trim();
 
             if (!!fileName) {
                 // For some latest version, the filename itself can be full download url.
                 // Do a very basic check for url(instead of regex) as the url is only for downloading and
                 // is coming from .net core releases json and not some ransom user input
-                if (fileName.trim().toLowerCase().startsWith("https://")) {
-                    downloadUrls.push(fileName.trim());
+                if (fileName.toLowerCase().startsWith("https://")) {
+                    downloadUrls.push(fileName);
                 } else {
                     if (!!blobUrl) {
-                        downloadUrls.push(util.format("%s%s", blobUrl.trim(), fileName.trim()));
+                        downloadUrls.push(util.format("%s%s", blobUrl.trim(), fileName));
                     }
 
                     if (!!dlcUrl) {
-                        downloadUrls.push(util.format("%s%s", dlcUrl.trim(), fileName.trim()));
+                        downloadUrls.push(util.format("%s%s", dlcUrl.trim(), fileName));
                     }
                 }
             }
