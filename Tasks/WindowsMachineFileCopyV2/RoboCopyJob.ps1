481ea2fd8829ce175cb6b6739dba9891adcf44f8
arjgupta <arjgupta@microsoft.com>
2018-10-10 14:43:15 +0530

    use net use /delete to attempt to delete remote share

--- Tasks/WindowsMachineFileCopyV2/RoboCopyJob.ps1
+++ Tasks/WindowsMachineFileCopyV2/RoboCopyJob.ps1
@@ -255,8 +255,11 @@ param (
             $remoteSharePsDrive = Get-PSDrive -Name 'WFCPSDrive' -ErrorAction 'SilentlyContinue'
             if ($remoteSharePsDrive -ne $null)
             {
+                $remoteSharePath = $remoteSharePsDrive.Root
                 Write-Verbose "Attempting to remove PSDrive 'WFCPSDrive'"
                 Remove-PSDrive -Name 'WFCPSDrive' -Force
+                Write-Verbose "RemoveSharePath: $remoteSharePath"
+                net use /delete "$remoteSharePath"
             }
         }
     }
