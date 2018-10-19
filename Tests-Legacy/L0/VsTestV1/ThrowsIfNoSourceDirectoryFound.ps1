edc842a96bfc67d034a307f712e2ba8894536817
Shreyas R <shrer@microsoft.com>
2018-10-19 13:45:36 +0530

    Added back VsTestV1 from m109 (#8649)
    * Added back VsTestV1 from m109
    
    * Added bask tests for VsTestV1
    
    * changed machine group to deployment group

---
+++ Tests-Legacy/L0/VsTestV1/ThrowsIfNoSourceDirectoryFound.ps1
@@ -0,0 +1,28 @@
+[cmdletbinding()]
+param()
+
+# Arrange.
+. $PSScriptRoot\..\..\lib\Initialize-Test.ps1
+Register-Mock Get-LocalizedString { $OFS = " " ; "$args" }
+Register-Mock Get-TaskVariable
+
+# Act/Assert.
+Assert-Throws {             
+	# Act.
+	$splat = @{
+		'vsTestVersion' = 'vsTestVersion'
+		'testAssembly' = 'testAssembly' 
+		'testFiltercriteria' = 'testFiltercriteria' 
+		'runSettingsFile' = 'runSettingsFile' 
+		'codeCoverageEnabled' = 'codeCoverageEnabled'
+		'pathtoCustomTestAdapters' = 'pathtoCustomTestAdapters'
+		'overrideTestrunParameters' = 'overrideTestrunParameters'
+		'otherConsoleOptions' = 'otherConsoleOptions'
+		'testRunTitle' = 'testRunTitle'
+		'platform' = 'platform'
+		'configuration' = 'configuration'
+		'publishRunAttachments' = 'publishRunAttachments'
+		'$runInParallel' = '$runInParallel'
+	}
+	& $PSScriptRoot\..\..\..\Tasks\VsTestV1\VsTest.ps1 @splat
+}
