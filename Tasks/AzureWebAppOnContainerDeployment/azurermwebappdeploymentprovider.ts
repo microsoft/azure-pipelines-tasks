4887f87c31f5f471324973ce0df221644b4decdb
Sumiran Aggarwal <suaggar@microsoft.com>
2018-10-30 19:33:41 +0530

    first commit

---
+++ Tasks/AzureWebAppOnContainerDeployment/azurermwebappdeploymentprovider.ts
@@ -0,0 +1,78 @@
+import { TaskParameters } from './taskparameters';
+import { KuduServiceUtility } from 'azurermdeploycommon/operations/KuduServiceUtility';
+import { AzureAppService } from 'azurermdeploycommon/azure-arm-rest/azure-arm-app-service';
+import { AzureRMEndpoint } from 'azurermdeploycommon/azure-arm-rest/azure-arm-endpoint';
+import { Kudu } from 'azurermdeploycommon/azure-arm-rest/azure-arm-app-service-kudu';
+import { AzureAppServiceUtility } from 'azurermdeploycommon/operations/AzureAppServiceUtility';
+import { AzureEndpoint } from 'azurermdeploycommon/azure-arm-rest/azureModels';
+import { AzureResourceFilterUtility } from 'azurermdeploycommon/operations/AzureResourceFilterUtility';
+import tl = require('vsts-task-lib/task');
+import * as ParameterParser from 'azurermdeploycommon/operations/ParameterParserUtility'
+import { addReleaseAnnotation } from 'azurermdeploycommon/operations/ReleaseAnnotationUtility';
+import { ContainerBasedDeploymentUtility } from 'azurermdeploycommon/operations/ContainerBasedDeploymentUtility';
+
+export class AzureRmWebAppDeploymentProvider{
+    protected taskParams:TaskParameters;
+    protected appService: AzureAppService;
+    protected kuduService: Kudu;
+    protected appServiceUtility: AzureAppServiceUtility;
+    protected kuduServiceUtility: KuduServiceUtility;
+    protected azureEndpoint: AzureEndpoint;
+    protected activeDeploymentID;
+
+    constructor(taskParams: TaskParameters) {
+        this.taskParams = taskParams;
+    }
+
+    public async PreDeploymentStep() {
+        this.azureEndpoint = await new AzureRMEndpoint(this.taskParams.connectedServiceName).getEndpoint();
+        console.log(tl.loc('GotconnectiondetailsforazureRMWebApp0', this.taskParams.WebAppName));
+        
+        if(!this.taskParams.DeployToSlotOrASEFlag){
+            var appDetails = await AzureResourceFilterUtility.getAppDetails(this.azureEndpoint, this.taskParams.WebAppName);
+            this.taskParams.ResourceGroupName = appDetails["resourceGroupName"];
+        }
+
+        this.appService = new AzureAppService(this.azureEndpoint, this.taskParams.ResourceGroupName, this.taskParams.WebAppName, this.taskParams.SlotName);
+        this.appServiceUtility = new AzureAppServiceUtility(this.appService);
+
+        this.kuduService = await this.appServiceUtility.getKuduService();
+        this.kuduServiceUtility = new KuduServiceUtility(this.kuduService);
+        tl.debug(`Resource Group: ${this.taskParams.ResourceGroupName}`);
+    }
+
+    public async DeployWebAppStep() {
+        tl.debug("Performing container based deployment.");
+
+        let containerDeploymentUtility: ContainerBasedDeploymentUtility = new ContainerBasedDeploymentUtility(this.appService);
+        await containerDeploymentUtility.deployWebAppImage(this.taskParams);
+        
+        await this.appServiceUtility.updateScmTypeAndConfigurationDetails();
+    }
+
+    public async UpdateDeploymentStatus(isDeploymentSuccess: boolean) {
+        if(this.kuduServiceUtility) {
+            await addReleaseAnnotation(this.azureEndpoint, this.appService, isDeploymentSuccess);
+            this.activeDeploymentID = await this.kuduServiceUtility.updateDeploymentStatus(isDeploymentSuccess, null, {'type': 'Deployment', slotName: this.appService.getSlot()});
+            tl.debug('Active DeploymentId :'+ this.activeDeploymentID);
+        }
+    }
+
+    protected async PostDeploymentStep() {
+        if(this.taskParams.AppSettings) {
+            var customApplicationSettings = ParameterParser.parse(this.taskParams.AppSettings);
+            await this.appServiceUtility.updateAndMonitorAppSettings(customApplicationSettings);
+        }
+
+        if(this.taskParams.ConfigurationSettings) {
+            var customApplicationSettings = ParameterParser.parse(this.taskParams.ConfigurationSettings);
+            await this.appServiceUtility.updateConfigurationSettings(customApplicationSettings);
+        }
+
+        await this.appServiceUtility.updateScmTypeAndConfigurationDetails();
+        
+        let appServiceApplicationUrl: string = await this.appServiceUtility.getApplicationURL();
+        console.log(tl.loc('AppServiceApplicationURL', appServiceApplicationUrl));
+        tl.setVariable('AppServiceApplicationUrl', appServiceApplicationUrl);
+    }
+}
---
+++ Tasks/AzureWebAppOnContainerDeployment/taskparameters.ts
@@ -0,0 +1,47 @@
+import tl = require('vsts-task-lib/task');
+
+export enum DeploymentType {
+    webDeploy,
+    zipDeploy,
+    runFromZip,
+    warDeploy
+}
+
+const webAppKindMap = new Map([
+    [ 'app,linux,container', 'webAppContainer' ],
+    [ 'functionapp,linux,container', 'functionAppContainer' ]
+]);
+
+export class TaskParametersUtility {
+
+    public static async getParameters(): Promise<TaskParameters> {
+        var taskParameters: TaskParameters = {
+            connectedServiceName: tl.getInput('ConnectedServiceName', true),
+            ImageName: tl.getInput('ImageName', true),
+            AppSettings: tl.getInput('AppSettings', false),
+            StartupCommand: tl.getInput('StartupCommand', false),
+            ConfigurationSettings: tl.getInput('ConfigurationSettings', false),
+            WebAppName: tl.getInput('WebAppName', true),
+            DeployToSlotOrASEFlag: tl.getBoolInput('DeployToSlotOrASEFlag', false),
+            ResourceGroupName: tl.getInput('ResourceGroupName', false),
+            SlotName:tl.getInput('SlotName', false)
+        }
+
+        var endpointTelemetry = '{"endpointId":"' + taskParameters.connectedServiceName + '"}';
+        console.log("##vso[telemetry.publish area=TaskEndpointId;feature=AzureRmWebAppDeployment]" + endpointTelemetry);
+
+        return taskParameters;
+    }
+}
+
+export interface TaskParameters {
+    connectedServiceName: string;
+    WebAppName: string;
+    AppSettings?: string;
+    StartupCommand?: string;
+    ConfigurationSettings?: string;
+    ImageName?: string;
+    ResourceGroupName?: string;
+    DeployToSlotOrASEFlag?: boolean;
+    SlotName?: string;
+}
