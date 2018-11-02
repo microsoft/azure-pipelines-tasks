7659441dd188072de621da04532017f96158a09c
Sumiran Aggarwal <suaggar@microsoft.com>
2018-11-02 16:07:59 +0530

    function on container task

---
+++ Tasks/AzureFunctionOnContainerDeployment/taskparameters.ts
@@ -0,0 +1,35 @@
+import tl = require('vsts-task-lib/task');
+
+export class TaskParametersUtility {
+
+    public static async getParameters(): Promise<TaskParameters> {
+        var taskParameters: TaskParameters = {
+            connectedServiceName: tl.getInput('ConnectedServiceName', false),
+            ImageName: tl.getInput('DockerNamespace', false),
+            AppSettings: tl.getInput('AppSettings', false),
+            StartupCommand: tl.getInput('StartupCommand', false),
+            ConfigurationSettings: tl.getInput('ConfigurationSettings', false),
+            WebAppName: tl.getInput('WebAppName', true),
+            ResourceGroupName: tl.getInput('ResourceGroupName', false),
+            SlotName:tl.getInput('SlotName', false),
+            DeployToSlotOrASEFlag: tl.getBoolInput('DeployToSlotOrASEFlag', false),
+        }
+
+        var endpointTelemetry = '{"endpointId":"' + taskParameters.connectedServiceName + '"}';
+        console.log("##vso[telemetry.publish area=TaskEndpointId;feature=AzureRmWebAppDeployment]" + endpointTelemetry);
+
+        return taskParameters;
+    }
+}
+
+export interface TaskParameters {
+    connectedServiceName: string;
+    WebAppName: string;
+    AppSettings?: string;
+    StartupCommand?: string;
+    ConfigurationSettings?: string;
+    ImageName?: string;
+    ResourceGroupName?: string;
+    SlotName?: string;
+    DeployToSlotOrASEFlag?: boolean;
+}
