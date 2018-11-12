420943d743369c5f8187629ed4bbba7141a7ef8f
asranja <asranja@microsoft.com>
2018-11-12 18:10:21 +0530

    Fix indentation

--- Tasks/WindowsMachineFileCopyV2/RoboCopyJob.ps1
+++ Tasks/WindowsMachineFileCopyV2/RoboCopyJob.ps1
@@ -259,7 +259,7 @@ param (
                 Write-Verbose "Attempting to remove PSDrive 'WFCPSDrive'"
                 Remove-PSDrive -Name 'WFCPSDrive' -Force
                 Write-Verbose "RemoteSharePath: $remoteSharePath"
-				Try-CleanupPSDrive -Path $remoteSharePath
+                Try-CleanupPSDrive -Path $remoteSharePath
             }
         }
     }
