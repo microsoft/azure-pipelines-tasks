5f5c6b38347cc8d42b72f234816aaea4596a8f1b
Nitin Issac Joy <nijoy@microsoft.com>
2018-11-14 17:18:40 +0530

    Adding native build options

--- Tasks/Unity/index.ts
+++ Tasks/Unity/index.ts
@@ -10,16 +10,22 @@ async function run() {
     var unityPath = "C:/Program Files/Unity/Editor/Unity.exe";
 
     var customBuild = t1.getInput('customBuild');
-    if(customBuild == "Yes") t1.execSync(unityPath, "-batchmode -projectPath " + gamePath + " -executeMethod " + executeMethod);
-    else {
+    if(customBuild == "Yes") {
+        t1.execSync(unityPath, "-batchmode -projectPath " + gamePath + " -executeMethod " + executeMethod + " -stackTraceLogType Full -quit");
+    } else {
         var buildPlatform = t1.getInput('buildPlatform');
         var buildArg = "win64";
         switch(buildPlatform) {
-            case "WinMetro": buildArg = "-buildWindowsPlayer"; break;
-            case "IOS": buildArg = "-buildOSXPlayer"; break;
-            case "Linux": buildArg = "-buildLinuxUniversalPlayer"; break;
+            default: break;
+            case "Win32": buildArg = "-buildWindowsPlayer"; break;
+            case "Win64": buildArg = "-buildWindows64Player"; break;
+            case "OSX32": buildArg = "-buildOSXPlayer"; break;
+            case "OSX64": buildArg = "-buildOSX64Player"; break;
+            case "Linux32": buildArg = "-buildLinux32Player"; break;
+            case "Linux64": buildArg = "-buildLinux64Player"; break;
+            case "LinuxU": buildArg = "-buildLinuxUniversalPlayer"; break;
         }  
-        t1.execSync(unityPath, "-batchmode -projectPath " + gamePath + " " + buildArg + " " + gamePath + " -quit"); 
+        t1.execSync(unityPath, "-batchmode -projectPath " + gamePath + " " + buildArg + " " + gamePath + " -stackTraceLogType Full -quit");
     }
 }
 
