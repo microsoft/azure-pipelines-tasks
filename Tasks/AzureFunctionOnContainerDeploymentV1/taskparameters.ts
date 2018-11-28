3e53150556f11b5ce8d4ded8d50af27fe9f9c073
Sumiran Aggarwal <sumiran.aggarwal.eee11@itbhu.ac.in>
2018-11-28 18:16:19 +0530

    Update taskparameters.ts

--- Tasks/AzureFunctionOnContainerDeploymentV1/taskparameters.ts
+++ Tasks/AzureFunctionOnContainerDeploymentV1/taskparameters.ts
@@ -4,8 +4,8 @@ export class TaskParametersUtility {
 
     public static async getParameters(): Promise<TaskParameters> {
         var taskParameters: TaskParameters = {
-            connectedServiceName: tl.getInput('ConnectedServiceName', false),
-            ImageName: tl.getInput('imageName', false),
+            connectedServiceName: tl.getInput('ConnectedServiceName', true),
+            ImageName: tl.getInput('imageName', true),
             AppSettings: tl.getInput('appSettings', false),
             StartupCommand: tl.getInput('containerCommand', false),
             ConfigurationSettings: tl.getInput('configurationStrings', false),
@@ -25,10 +25,10 @@ export class TaskParametersUtility {
 export interface TaskParameters {
     connectedServiceName: string;
     WebAppName: string;
+    ImageName: string;
     AppSettings?: string;
     StartupCommand?: string;
     ConfigurationSettings?: string;
-    ImageName?: string;
     ResourceGroupName?: string;
     SlotName?: string;
     DeployToSlotOrASEFlag?: boolean;
