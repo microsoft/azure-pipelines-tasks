1535eb7157e57e97a3cb2184386156051ae76c1a
Danny McCormick <damccorm@microsoft.com>
2018-12-04 12:05:04 -0500

    Add tests

---
+++ Tasks/NodeToolV0/Tests/L0FirstDownloadSuccess.ts
@@ -0,0 +1,82 @@
+import ma = require('vsts-task-lib/mock-answer');
+import tmrm = require('vsts-task-lib/mock-run');
+import os = require('os');
+import path = require('path');
+
+let taskPath = path.join(__dirname, '..', 'nodetool.js');
+let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+
+tmr.setInput('versionSpec', '11.3.0');
+tmr.setInput('checkLatest', 'false');
+
+let a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
+    "assertAgent": {
+        "2.115.1": true
+    }};
+tmr.setAnswers(a);
+
+
+//Create assertAgent and getVariable mocks
+const tl = require('vsts-task-lib/mock-task');
+const tlClone = Object.assign({}, tl);
+tlClone.getVariable = function(variable: string) {
+    if (variable.toLowerCase() == 'agent.tempdirectory') {
+        return 'temp';
+    }
+    return null;
+};
+tlClone.assertAgent = function(variable: string) {
+    return;
+};
+tmr.registerMock('vsts-task-lib/mock-task', tlClone);
+
+//Create tool-lib mock
+tmr.registerMock('vsts-task-tool-lib/tool', {
+    isExplicitVersion: function(versionSpec) {
+        return false;
+    },
+    findLocalTool: function(toolName, versionSpec) {
+        if (toolName != 'node') { 
+            throw new Error('Searching for wrong tool');
+        }
+        return false;
+    },
+    evaluateVersions: function(versions, versionSpec) {
+        let version: string;
+        for (let i = versions.length - 1; i >= 0; i--) {
+            let potential: string = versions[i];
+            let satisfied: boolean = potential === 'v11.3.0';
+            if (satisfied) {
+                version = potential;
+                break;
+            }
+        }
+        return version;
+    },
+    cleanVersion: function(version) {
+        return '11.3.0';
+    },
+    downloadTool(url) {
+        if (url === `https://nodejs.org/dist/v11.3.0/node-v11.3.0-win-${os.arch()}.7z` ||
+            url === `https://nodejs.org/dist/v11.3.0/node-v11.3.0-${os.platform()}-${os.arch()}.tar.gz`) {
+            return 'location';
+        }
+        else {
+            throw new Error('Incorrect URL');
+        }
+    },
+    extract7z(downloadPath, extPath, _7zPath) {
+        return 'extPath';
+    },
+    extractTar(downloadPath, extPath, _7zPath) {
+        return 'extPath';
+    },
+    cacheDir(dir, tool, version) {
+        return 'path to tool';
+    },
+    prependPath(toolPath) {
+        return;
+    }
+});
+
+tmr.run();
