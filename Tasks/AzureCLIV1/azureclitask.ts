936033acb474705d35d1ee5ebe6754cd2c4493e4
Vineet <vineet.mimrot@gmail.com>
2018-12-05 17:14:04 +0530

    Setting up Azure Active cloud corresponding to service endpoint (#9003)
    * Setting up Azure Active cloud corresponding to service endpoint
    
    * Fix review comment

--- Tasks/AzureCLIV1/azureclitask.ts
+++ Tasks/AzureCLIV1/azureclitask.ts
@@ -57,7 +57,9 @@ export class azureclitask {
                 tool = tl.tool(tl.which(scriptPath, true));
             }
             this.throwIfError(tl.execSync("az", "--version"));
-            this.useGlobalConfig = tl.getBoolInput("useGlobalConfig");
+            // set az cli config dir
+            this.setConfigDirectory();
+            this.setAzureCloudBasedOnServiceEndpoint();
             this.loginAzure();
 
             tool.line(args); // additional args should always call line. line() parses quoted arg strings
@@ -115,7 +117,6 @@ export class azureclitask {
     private static isLoggedIn: boolean = false;
     private static cliPasswordPath: string = null;
     private static azCliConfigPath: string;
-    private static useGlobalConfig: boolean = true;
     private static servicePrincipalId: string = null;
     private static servicePrincipalKey: string = null;
 
@@ -146,9 +147,6 @@ export class azureclitask {
         var tenantId: string = tl.getEndpointAuthorizationParameter(connectedService, "tenantid", false);
         var subscriptionID: string = tl.getEndpointDataParameter(connectedService, "SubscriptionID", true);
 
-        // set az cli config dir
-        this.setConfigDirectory();
-
         //login using svn
         this.throwIfError(tl.execSync("az", "login --service-principal -u \"" + servicePrincipalId + "\" -p \"" + cliPassword + "\" --tenant \"" + tenantId + "\""), tl.loc("LoginFailed"));
         this.isLoggedIn = true;
@@ -158,7 +156,7 @@ export class azureclitask {
     }
 
     private static setConfigDirectory(): void {
-        if (this.useGlobalConfig) {
+        if (tl.getBoolInput("useGlobalConfig")) {
             return;
         }
 
@@ -175,6 +173,15 @@ export class azureclitask {
             this.azCliConfigPath = path.join(basePath, ".azclitask", configDirName);
             console.log(tl.loc('SettingAzureConfigDir', this.azCliConfigPath));
             process.env['AZURE_CONFIG_DIR'] = this.azCliConfigPath;
+        }
+    }
+
+    private static setAzureCloudBasedOnServiceEndpoint(): void {
+        var connectedService: string = tl.getInput("connectedServiceNameARM", true);
+        var environment = tl.getEndpointDataParameter(connectedService, 'environment', true);
+        if(!!environment) {
+            console.log(tl.loc('SettingAzureCloud', environment));
+            this.throwIfError(tl.execSync("az", "cloud set -n " + environment));
         }
     }
 
