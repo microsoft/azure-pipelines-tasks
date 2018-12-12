86343a718e06cc05d4d2ca0f84b0c20bedeb108f
Deepak Sattiraju <desattir@microsoft.com>
2018-12-12 17:49:11 +0530

    Annotating child pods

--- Tasks/KubernetesManifestV0/src/actions/deploy.ts
+++ Tasks/KubernetesManifestV0/src/actions/deploy.ts
@@ -5,6 +5,7 @@ import fs = require("fs");
 import ClusterConnection from "../connections/clusterconnection";
 import * as utils from "./../utilities";
 import Q = require('q');
+import { IExecOptions } from 'vsts-task-lib/toolrunner';
 
 export function deploy(clusterConnection: ClusterConnection, files?: string[]): any {
     if (!files) {
@@ -25,6 +26,12 @@ export function deploy(clusterConnection: ClusterConnection, files?: string[]): 
                 let words = line.split(" ");
                 if (recognizedType.filter(type => words[0].startsWith(type)).length > 0) {
                     promises.push(checkRolloutStatus(words[0], words[1].trim()));
+                }
+            });
+            types.forEach(line => {
+                let words = line.split(" ");
+                if (recognizedType.filter(type => words[0].startsWith(type)).length > 0) {
+                    promises.push(annotateResourceSets(words[0], words[1].trim()));
                 }
             });
             Q.all(promises).then(() => {
@@ -96,11 +103,72 @@ function annotate(configurationPath: string): any {
     command.arg("annotate");
     command.arg(getNameSpace());
     command.arg(["-f", configurationPath]);
-    command.arg([`Build.BuildId=${tl.getVariable("Build.BuildId")}`])
-    command.arg([`Build.Repository.Name=${tl.getVariable("Build.Repository.Name")}`])
-    command.arg([`BuildUri=${tl.getVariable("System.TeamFoundationCollectionUri")}_build/results?buildId=${tl.getVariable("Build.BuildId")}`])
-    command.arg([`--overwrite`])
+    command.arg(`azure-pipelines/execution=${tl.getVariable("Build.BuildNumber")}`)
+    command.arg(`azure-pipelines/pipeline="${tl.getVariable("Build.DefinitionName")}"`)
+    command.arg(`azure-pipelines/executionuri=${tl.getVariable("System.TeamFoundationCollectionUri")}_build/results?buildId=${tl.getVariable("Build.BuildId")}`)
+    command.arg(`azure-pipelines/project=${tl.getVariable("System.TeamProject")}`)
+    command.arg(`azure-pipelines/org=${tl.getVariable("System.CollectionId")}`)
+    command.arg(`--overwrite`)
     return command.execSync();
+}
+
+function annotateResourceSets(type, name) {
+    if (type.indexOf("pod") > -1) {
+        return;
+    }
+    function annotatePod(name) {
+        var command = tl.tool(tl.which("kubectl"));
+        command.arg("annotate");
+        command.arg(getNameSpace());
+        command.arg(["pod", name]);
+        command.arg(`azure-pipelines/execution=${tl.getVariable("Build.BuildNumber")}`)
+        command.arg(`azure-pipelines/pipeline="${tl.getVariable("Build.DefinitionName")}"`)
+        command.arg(`azure-pipelines/executionuri=${tl.getVariable("System.TeamFoundationCollectionUri")}_build/results?buildId=${tl.getVariable("Build.BuildId")}`)
+        command.arg(`azure-pipelines/project=${tl.getVariable("System.TeamProject")}`)
+        command.arg(`azure-pipelines/org=${tl.getVariable("System.CollectionId")}`)
+        command.arg(`--overwrite`)
+        return command.execSync();
+    }
+
+    var owner = name;
+    if (type.indexOf("deployment") > -1) {
+        owner = getNewReplicaSet(name);
+    }
+
+    var allPods = JSON.parse(getAllPods().stdout);
+    allPods["items"].forEach((pod) => {
+        let owners = pod["metadata"]["ownerReferences"];
+        owners.forEach(ownerRef => {
+            if (ownerRef["name"] == owner) {
+                annotatePod(pod["metadata"]["name"]);
+            }
+        });
+    });
+}
+
+function getNewReplicaSet(deployment) {
+    var command = tl.tool(tl.which("kubectl"));
+    command.arg("describe");
+    command.arg(getNameSpace());
+    command.arg(["deployment", deployment]);
+    let stdout = command.execSync({ silent: true } as IExecOptions).stdout.split("\n");
+    let newReplicaSet = "";
+    stdout.forEach((line: string) => {
+        if (line.indexOf("newreplicaset") > -1) {
+            newReplicaSet = line.substr(14).trim().split(" ")[0];
+        }
+    });
+
+    return newReplicaSet;
+}
+
+function getAllPods() {
+    var command = tl.tool(tl.which("kubectl"));
+    command.arg("get");
+    command.arg(getNameSpace());
+    command.arg("pods");
+    command.arg(["-o", "json"])
+    return command.execSync({ silent: true } as IExecOptions);
 }
 
 function getNameSpace(): string[] {
@@ -122,4 +190,4 @@ function checkRolloutStatus(resourceType, name) {
     return command.exec();
 }
 
-var recognizedType = ["deployment", "replicaset", "daemonset", "pod", "statefulset"];
+var recognizedType = ["deployment", "replicaset", "daemonset", "pod", "statefulset"]; 
