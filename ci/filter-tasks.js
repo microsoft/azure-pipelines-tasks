e72dc2969b9fefd176ac0abb8a0f2a05f1a4fe98
Danny McCormick <damccorm@microsoft.com>
2018-12-18 10:25:37 -0500

    Merge in upstream

--- ci/filter-tasks.js
+++ ci/filter-tasks.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-// File to filter out tasks that don't need to be built
-var fs = require('fs');
-var path = require('path');
-=======
 // Filter out tasks that don't need to be built. Determines which tasks need to be built based on the type of build.
 // If its a CI build, all tasks whose package numbers have been bumped will be built.
 // If its a PR build, all tasks that have been changed will be built.
@@ -10,7 +5,6 @@ var path = require('path');
 var fs = require('fs');
 var path = require('path');
 var semver = require('semver');
->>>>>>> upstream/master
 var restClient = require('typed-rest-client/RestClient');
 var httpHandler = require('typed-rest-client/Handlers');
 var run = require('./ci-util').run;
@@ -18,17 +12,6 @@ var run = require('./ci-util').run;
 var makeOptionsPath = path.join(__dirname, '..', 'make-options.json');
 var makeOptions = JSON.parse(fs.readFileSync(makeOptionsPath).toString());
 
-<<<<<<< HEAD
-var getTasksToBuildFromVersioning = async function() {
-    // Returns a list of tasks that have different version numbers than their current published version.
-    var token = process.env['SYSTEM_ACCESSTOKEN'];
-    var packageInfo;
-    try {
-        var handler = new httpHandler.PersonalAccessTokenCredentialHandler(token);
-        var client = new restClient.RestClient('Tasks CI', '', [handler]);
-        packageInfo = await client.get('https://mseng.feeds.visualstudio.com/_apis/packaging/Feeds/Codex-deps/Packages?packageNameQuery=Mseng.MS.TF.DistributedTask.Tasks');
-        if (packageInfo.statusCode != 200) {
-=======
 var getTasksToBuildForCI = async function() {
     // Returns a list of tasks that have different version numbers than their current published version. 
     var packageInfo;
@@ -48,27 +31,11 @@ var getTasksToBuildForCI = async function() {
         packageInfo = await client.get(packageEndpoint);
         if (packageInfo.statusCode !== 200) {
             console.log(`##vso[task.logissue type=warning;sourcepath=ci/filter-task.js;linenumber=24;]Failed to get info from package endpoint, returned with status code ${packageInfo.statusCode}`);
->>>>>>> upstream/master
             return makeOptions.tasks;
         }
     }
     catch (err) {
         // If unable to reach rest client, build everything.
-<<<<<<< HEAD
-        return makeOptions.tasks;
-    }
-    var packageMap = {};
-    packageInfo.result.value.forEach(package => {
-        if (package.name && package.versions) {
-            var packageName = package.name.slice('Mseng.MS.TF.DistributedTask.Tasks.'.length);
-            var packageVersion = package.versions[0].version;
-            package.versions.forEach(versionInfo => {
-                if (versionInfo.isLatest) {
-                    packageVersion = versionInfo.version;
-                }
-            });
-            packageMap[packageName] = packageVersion;
-=======
         console.log(`##vso[task.logissue type=warning;sourcepath=ci/filter-task.js;linenumber=30;]Failed to get info from package endpoint, client failed with error ${err.message}`);
         return makeOptions.tasks;
     }
@@ -88,32 +55,10 @@ var getTasksToBuildForCI = async function() {
                 }
                 return false;
             });
->>>>>>> upstream/master
         }
     });
 
     return makeOptions.tasks.filter(function (taskName) {
-<<<<<<< HEAD
-        if (taskName in packageMap) {
-            var packageVersion = packageMap[taskName].split('.');
-            var packageMajorVersion = packageVersion[0];
-            var packageMinorVersion = packageVersion[1];
-            var packagePatchVersion = packageVersion[2];
-
-            var taskJsonPath = path.join(__dirname, '..', 'Tasks' , taskName, 'task.json')
-            var taskJson = JSON.parse(fs.readFileSync(taskJsonPath).toString());
-            var version = taskJson.version;
-            if (packageMajorVersion != version.Major || packageMinorVersion != version.Minor || packagePatchVersion != version.Patch) {
-                // If versions are not the same, want to build this package
-                return true;
-            }
-        }
-        return false;
-    });
-}
-
-var getTasksUsingChangedCommon = function(commonFilePaths) {
-=======
         var lowerCaseName = taskName.toLowerCase();
         if (lowerCaseName in packageMap) {
             var packageVersion = packageMap[lowerCaseName]
@@ -133,7 +78,6 @@ var getTasksUsingChangedCommon = function(commonFilePaths) {
 }
 
 var getTasksDependentOnChangedCommonFiles = function(commonFilePaths) {
->>>>>>> upstream/master
     // Takes in an array of filepaths that have been changed in the Tasks/Common folder, returns any tasks that could be affected.
     if (commonFilePaths.length == 0) {
         return [];
@@ -155,27 +99,12 @@ var getTasksDependentOnChangedCommonFiles = function(commonFilePaths) {
     return changedTasks;
 }
 
-<<<<<<< HEAD
-var getTasksToBuildFromDiff = function() {
-=======
 var getTasksToBuildForPR = function() {
->>>>>>> upstream/master
     // Takes in a git source branch, diffs it with master, and returns a list of tasks that could have been affected by the changes.
     var sourceBranch = process.env['SYSTEM_PULLREQUEST_SOURCEBRANCH'];
     var prId = process.env['SYSTEM_PULLREQUEST_PULLREQUESTID'];
     var commonChanges = [];
     var toBeBuilt = [];
-<<<<<<< HEAD
-    // Check if its from a fork
-    if (sourceBranch.contains(':')) {
-        sourceBranch = sourceBranch.split(':')[1];
-        run('git fetch origin pull/' + prId + '/head:' + sourceBranch);
-    }
-    else {
-        run('git fetch origin ' + sourceBranch);
-    }
-    run ('git checkout ' + sourceBranch);
-=======
     try {
         if (sourceBranch.includes(':')) {
             // We only care about the branch name, not the source repo
@@ -189,7 +118,6 @@ var getTasksToBuildForPR = function() {
         console.log('##vso[task.logissue type=warning;sourcepath=ci/filter-task.js;linenumber=112;]Unable to reach github, building all tasks', err);
         return makeOptions.tasks;
     }
->>>>>>> upstream/master
     run('git checkout master');
     run('git diff --name-only master..' + sourceBranch).split('\n').forEach(filePath => {
         if (filePath.slice(0, 5) == 'Tasks') {
@@ -205,16 +133,8 @@ var getTasksToBuildForPR = function() {
                 }
             }
         }
-<<<<<<< HEAD
-        else if (filePath.slice(0, 5) == 'Tests') {
-            buildCommon = true;
-        }
-    });
-    var changedTasks = getTasksUsingChangedCommon(commonChanges);
-=======
     });
     var changedTasks = getTasksDependentOnChangedCommonFiles(commonChanges);
->>>>>>> upstream/master
     var shouldBeBumped = [];
     changedTasks.forEach(task => {
         if (!toBeBuilt.includes(task)) {
@@ -233,30 +153,18 @@ var setTaskVariables = function(tasks) {
     console.log('##vso[task.setVariable variable=numTasks]' + tasks.length);
 }
 
-<<<<<<< HEAD
-var buildReason = process.env['BUILD_REASON'].toLowerCase();
-var tasks;
-if (buildReason == 'individualci' || buildReason == 'batchedci') {
-    // If CI, we will compare any tasks that have updated versions.
-    getTasksToBuildFromVersioning().then(tasks => {
-=======
 var buildReason = 'pullrequest';//process.env['BUILD_REASON'].toLowerCase();
 var tasks;
 if (buildReason == 'individualci' || buildReason == 'batchedci') {
     // If CI, we will compare any tasks that have updated versions.
     getTasksToBuildForCI().then(tasks => {
->>>>>>> upstream/master
         setTaskVariables(tasks)
     });
 }
 else {
     if (buildReason == 'pullrequest') {
         // If PR, we will compare any tasks that could have been affected based on the diff.
-<<<<<<< HEAD
-        tasks = getTasksToBuildFromDiff();
-=======
         tasks = getTasksToBuildForPR();
->>>>>>> upstream/master
         setTaskVariables(tasks);
     }
     else {
