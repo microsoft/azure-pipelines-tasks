cad98940932bb35bb0c3d0f0f33d191404000633
Gyan Prakash Kumar <gykuma@microsoft.com>
2018-12-19 15:15:38 +0530

    support latest/tagged releases

--- Tasks/DownloadGithubReleasesV0/main.ts
+++ Tasks/DownloadGithubReleasesV0/main.ts
@@ -3,11 +3,15 @@ var path = require('path')
 import * as tl from 'vsts-task-lib/task';
 import * as engine from 'artifact-engine/Engine';
 import * as providers from 'artifact-engine/Providers';
+import * as httpc from 'typed-rest-client/HttpClient';
+
+var packagejson = require('./package.json');
 
 tl.setResourcePath(path.join(__dirname, 'task.json'));
 
 var taskJson = require('./task.json');
 const area: string = 'DownloadGithubReleases';
+const userAgent: string = 'download-github-releases-task-' + packagejson.version;
 
 function getDefaultProps() {
     var hostType = (tl.getVariable('SYSTEM.HOSTTYPE') || "").toLowerCase();
@@ -47,25 +51,53 @@ function publishEvent(feature, properties: any): void {
     }
 }
 
+async function getLatestRelease(repositoryName: string, handler): Promise<string> {
+    var promise = new Promise<string>((resolve, reject) => {
+        let httpClient: httpc.HttpClient = new httpc.HttpClient(userAgent, [handler]);
+        var latestReleaseUrl = "https://api.github.com/repos/" + repositoryName + "/releases/latest";
+        latestReleaseUrl = latestReleaseUrl.replace(/([^:]\/)\/+/g, "$1");
+        httpClient.get(latestReleaseUrl).then((res) => {
+            res.readBody().then((body) => {
+                var response = JSON.parse(body);
+                resolve(response["id"]);
+            });
+        }, (reason) => {
+            reject(reason);
+        });
+    });
+
+    return promise;
+}
+
+async function getTaggedRelease(repositoryName: string, tag: string, handler): Promise<string> {
+    var promise = new Promise<string>((resolve, reject) => {
+        let httpClient: httpc.HttpClient = new httpc.HttpClient(userAgent, [handler]);
+        var latestReleaseUrl = "https://api.github.com/repos/" + repositoryName + "/releases/tags/" + tag;
+        latestReleaseUrl = latestReleaseUrl.replace(/([^:]\/)\/+/g, "$1");
+        httpClient.get(latestReleaseUrl).then((res) => {
+            res.readBody().then((body) => {
+                var response = JSON.parse(body);
+                resolve(response["id"]);
+            });
+        }, (reason) => {
+            reject(reason);
+        });
+    });
+
+    return promise;
+}
+
 async function main(): Promise<void> {
     var promise = new Promise<void>(async (resolve, reject) => {
         let connection = tl.getInput("connection", true);
-        let repositoryName = tl.getInput("definition", true);
-        let releaseId = tl.getInput("versions", true);
+        let repositoryName = tl.getInput("userRepository", true);
+        let defaultVersionType = tl.getInput("defaultVersionType", true);
         let itemPattern = tl.getInput("itemPattern", false);
         let downloadPath = tl.getInput("downloadPath", true);
+        let version = tl.getInput("version", false);
+        let releaseId;
 
-        var itemsUrl = "https://api.github.com/repos/" + repositoryName + "/releases/" + releaseId + "/assets";
-        itemsUrl = itemsUrl.replace(/([^:]\/)\/+/g, "$1");
-        console.log(tl.loc("DownloadArtifacts", releaseId, itemsUrl));
-        
-        var templatePath = path.join(__dirname, 'githubreleases.handlebars.txt');
         var token = tl.getEndpointAuthorizationParameter(connection, 'AccessToken', false);
-        var githubReleasesVariables = {
-            "endpoint": {
-                "url": "https://api.github.com/"
-            }
-        };
 
         // Required to prevent typed-rest-client from adding additional 'Authorization' in header on redirect to AWS
         var customCredentialHandler = {
@@ -90,18 +122,37 @@ async function main(): Promise<void> {
             }
         }
 
+        switch (defaultVersionType) {
+            case 'latest': releaseId = await getLatestRelease(repositoryName, customCredentialHandler);
+                break;
+            case 'specificTag': releaseId = await getTaggedRelease(repositoryName, version, customCredentialHandler);
+                break;
+            case 'specificVersion':
+            default: releaseId = version;
+        }
+
+        var itemsUrl = "https://api.github.com/repos/" + repositoryName + "/releases/" + releaseId + "/assets";
+        itemsUrl = itemsUrl.replace(/([^:]\/)\/+/g, "$1");
+        console.log(tl.loc("DownloadArtifacts", releaseId, itemsUrl));
+
+        var templatePath = path.join(__dirname, 'githubreleases.handlebars.txt');
+        var githubReleasesVariables = {
+            "endpoint": {
+                "url": "https://api.github.com/"
+            }
+        };
+
         var webProvider = new providers.WebProvider(itemsUrl, templatePath, githubReleasesVariables, customCredentialHandler);
         var fileSystemProvider = new providers.FilesystemProvider(downloadPath);
-        var parallelLimit : number = +tl.getVariable("release.artifact.download.parallellimit");
+        var parallelLimit: number = +tl.getVariable("release.artifact.download.parallellimit");
 
         var downloader = new engine.ArtifactEngine();
         var downloaderOptions = new engine.ArtifactEngineOptions();
         downloaderOptions.itemPattern = itemPattern ? itemPattern : '**';
         var debugMode = tl.getVariable('System.Debug');
         downloaderOptions.verbose = debugMode ? debugMode.toLowerCase() != 'false' : false;
-        var parallelLimit : number = +tl.getVariable("release.artifact.download.parallellimit");
-        
-        if(parallelLimit){
+
+        if (parallelLimit) {
             downloaderOptions.parallelProcessingLimit = parallelLimit;
         }
 
