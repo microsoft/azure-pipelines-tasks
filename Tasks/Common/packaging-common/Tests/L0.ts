6d71c811137535c1e4457e60259fe3250e6b7428
Jovana Taylor <jotaylo@microsoft.com>
2019-01-04 15:12:26 -0800

    adding npm tests back in

--- Tasks/Common/packaging-common/Tests/L0.ts
+++ Tasks/Common/packaging-common/Tests/L0.ts
@@ -3,5 +3,5 @@ import { nugetcommon } from "./nugetL0";
 
 describe("packaging-common Task Suite", function() {
     describe("nuget common", nugetcommon);
-    describe.skip("npm common", npmcommon);
+    describe("npm common", npmcommon);
 });
