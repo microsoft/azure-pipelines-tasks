35ba442a39553d51cf74be73b531ba52601b6a87
Jeremy Gray <jegra@microsoft.com>
2019-01-04 08:49:09 -0500

    spacing issues

--- Tasks/PackerBuildV1/src/taskParameters.ts
+++ Tasks/PackerBuildV1/src/taskParameters.ts
@@ -87,7 +87,7 @@ export default class TaskParameters {
             this.additionalBuilderParameters = JSON.parse(tl.getInput("additionalBuilderParameters"));
             this.skipTempFileCleanupDuringVMDeprovision = tl.getBoolInput("skipTempFileCleanupDuringVMDeprovision", false);
             this.imageUri = tl.getInput(constants.OutputVariableImageUri, false);
-            this.imageId = tl.getInput(constants.OutputVariableImageId,false);
+            this.imageId = tl.getInput(constants.OutputVariableImageId, false);
         }
         catch (error) {
             throw (tl.loc("TaskParametersConstructorFailed", error));
