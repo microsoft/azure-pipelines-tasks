2c8501f7ee5bf1fb87b5370efc412ee697f9bd01
Danny McCormick <damccorm@microsoft.com>
2019-01-07 15:00:45 -0500

    Simplify PR filter logic

--- ci/filter-tasks.js
+++ ci/filter-tasks.js
@@ -116,16 +116,14 @@ var getTasksToBuildForPR = function() {
             // We only care about the branch name, not the source repo
             sourceBranch = sourceBranch.split(':')[1];
         }
-        run('git fetch origin pull/' + prId + '/head:' + sourceBranch);
-        run('git checkout master');
-        run('git checkout ' + sourceBranch);
     }
     catch (err) {
         // If unable to reach github, build everything.
         console.log('##vso[task.logissue type=warning;sourcepath=ci/filter-task.js;linenumber=125;]Unable to reach github, building all tasks', err);
         return makeOptions.tasks;
     }
-    var baseCommit = run('git merge-base ' + sourceBranch + ' master');
+    sourceBranch = 'origin/pull/' + prId + '/head:' + sourceBranch;
+    var baseCommit = run('git merge-base ' + sourceBranch + ' origin/master');
     run('git --no-pager diff --name-only ' + baseCommit + ' ' + sourceBranch)
         .split('\n')
         .forEach(filePath => {
