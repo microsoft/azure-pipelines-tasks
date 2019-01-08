e75bd4d44693d2088696098e5f2fdc5bf665c4af
Shivangi <Shivangi.Mittal@microsoft.com>
2019-01-08 14:43:15 +0530

    Making AzurePowerShell cross-platform

--- Tasks/AzurePowerShellV4/AzurePowerShell.ps1
+++ Tasks/AzurePowerShellV4/AzurePowerShell.ps1
@@ -86,7 +86,6 @@ try
         $__vstsAzPSInlineScriptPath = [System.IO.Path]::Combine($env:Agent_TempDirectory, ([guid]::NewGuid().ToString() + ".ps1"));
         ($scriptInline | Out-File $__vstsAzPSInlineScriptPath)
         $scriptPath = $__vstsAzPSInlineScriptPath
-        Write-Verbose "FilePathhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh  $scriptPath";
     }
 
     $scriptCommand = "& '$($scriptPath.Replace("'", "''"))' $scriptArguments"
