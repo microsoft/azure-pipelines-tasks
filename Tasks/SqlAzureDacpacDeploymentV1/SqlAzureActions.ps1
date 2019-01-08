667e6c732de02c675b3d7c7264a2b1201abb127e
Ajay Kumar Yadav <ajya@microsoft.com>
2019-01-08 18:50:58 +0530

    Update SqlAzureActions.ps1

--- Tasks/SqlAzureDacpacDeploymentV1/SqlAzureActions.ps1
+++ Tasks/SqlAzureDacpacDeploymentV1/SqlAzureActions.ps1
@@ -17,7 +17,7 @@ function Extract-Dacpac {
     Execute-SqlPackage -sqlpackageArguments $sqlpackageArguments -sqlpackageArgumentsToBeLogged $sqlpackageArgumentsToBeLogged
 
     Write-Host (Get-VstsLocString -Key "SAD_GeneratedFile" -ArgumentList "$targetDacpacFilePath")
-    Write-Host "##vso[task.uploadfile]$targetDacpacFilePath"
+    Write-Host "##vso[task.uploadfile] $targetDacpacFilePath"
     Write-Host (Get-VstsLocString -Key "SAD_SetOutputVariable" -ArgumentList "SqlDeploymentOutputFile", $targetDacpacFilePath)
     Write-Host "##vso[task.setVariable variable=SqlDeploymentOutputFile]$targetDacpacFilePath"
 }
