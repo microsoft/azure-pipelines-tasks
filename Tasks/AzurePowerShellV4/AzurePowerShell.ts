f666bc4ea52ebc3bd03f09cd1a28ac438bac7492
Shivangi <Shivangi.Mittal@microsoft.com>
2019-01-08 17:08:50 +0530

    Making AzurePowerShell cross-platform

--- Tasks/AzurePowerShellV4/AzurePowerShell.ts
+++ Tasks/AzurePowerShellV4/AzurePowerShell.ts
@@ -4,7 +4,6 @@ import os = require('os');
 import tl = require('vsts-task-lib/task');
 import tr = require('vsts-task-lib/toolrunner');
 import { AzureRMEndpoint } from 'azure-arm-rest/azure-arm-endpoint';
-import { AzureEndpoint } from 'azure-arm-rest/azureModels';
 var uuidV4 = require('uuid/v4');
 
 async function run() {
@@ -12,41 +11,42 @@ async function run() {
         tl.setResourcePath(path.join(__dirname, 'task.json'));
 
         // Get inputs.
-        let input_errorActionPreference: string = tl.getInput('errorActionPreference', false) || 'Stop';
-        switch (input_errorActionPreference.toUpperCase()) {
+        let _vsts_input_errorActionPreference: string = tl.getInput('errorActionPreference', false) || 'Stop';
+        switch (_vsts_input_errorActionPreference.toUpperCase()) {
             case 'STOP':
             case 'CONTINUE':
             case 'SILENTLYCONTINUE':
                 break;
             default:
-                throw new Error(tl.loc('JS_InvalidErrorActionPreference', input_errorActionPreference));
+                throw new Error(tl.loc('JS_InvalidErrorActionPreference', _vsts_input_errorActionPreference));
         }
 
         let scriptType: string = tl.getInput('ScriptType', /*required*/true);
         let scriptPath = tl.getPathInput('ScriptPath', false);
         let scriptInline: string = tl.getInput('Inline', false);
         let scriptArguments: string = tl.getInput('ScriptArguments', false);
-        let _vsts_input_errorActionPreference: string = tl.getInput('errorActionPreference', false) || 'Stop';
         let _vsts_input_failOnStandardError = tl.getBoolInput('FailOnStandardError', false);
         let targetAzurePs: string = tl.getInput('TargetAzurePs', false);
         let customTargetAzurePs: string = tl.getInput('CustomTargetAzurePs', false);
         let serviceName = tl.getInput('ConnectedServiceNameARM',/*required*/true);
-        let endpointt= await new AzureRMEndpoint(serviceName).getEndpoint();
+        let endpointObject= await new AzureRMEndpoint(serviceName).getEndpoint();
 
         // string constants
         let otherVersion = "OtherVersion"
-        let latestVersion = "LatestVersion"
 
         if (targetAzurePs == otherVersion) {
             if (customTargetAzurePs != "") {
                 targetAzurePs = customTargetAzurePs;
             }
             else {
-                targetAzurePs = ""
+                console.log(tl.loc('InvalidAzurePsVersion',customTargetAzurePs));
             }
         }
+        else {
+            targetAzurePs = ""
+        }
 
-        var endpoint = JSON.stringify(endpointt);
+        var endpoint = JSON.stringify(endpointObject);
 
         if (scriptType.toUpperCase() == 'FILEPATH') {
             if (!tl.stats(scriptPath).isFile() || !scriptPath.toUpperCase().match(/\.PS1$/)) {
@@ -57,10 +57,15 @@ async function run() {
         // Generate the script contents.
         console.log(tl.loc('GeneratingScript'));
         let contents: string[] = [];
-        let filePath3 = path.join(path.resolve(__dirname), 'InitializeAz.ps1');
-        contents.push(`${filePath3} -targetAzurePs  ${targetAzurePs} -serviceName ${serviceName} -endpoint '${endpoint}'`);
-        contents.push(`$ErrorActionPreference = '${input_errorActionPreference}'`); 
-
+        let azFilePath = path.join(path.resolve(__dirname), 'InitializeAz.ps1');
+        contents.push(`$ErrorActionPreference = '${_vsts_input_errorActionPreference}'`); 
+        if(targetAzurePs == "") {
+            contents.push(`${azFilePath} -serviceName ${serviceName} -endpoint '${endpoint}'`);
+        }
+        else {
+            contents.push(`${azFilePath} -targetAzurePs  ${targetAzurePs} -serviceName ${serviceName} -endpoint '${endpoint}'`);
+        }
+        
         if (scriptType.toUpperCase() == 'FILEPATH') {
             contents.push(`. '${scriptPath.replace("'", "''")}' ${scriptArguments}`.trim());
             console.log(tl.loc('JS_FormattedCommand', contents[contents.length - 1]));
@@ -97,14 +102,32 @@ async function run() {
             .arg(`. '${filePath.replace("'", "''")}'`);
 
         let options = <tr.IExecOptions>{
+            failOnStdErr: false,
             errStream: process.stdout, // Direct all output to STDOUT, otherwise the output may appear out
             outStream: process.stdout, // of order since Node buffers it's own STDOUT but not STDERR.
             ignoreReturnCode: true
         };
 
+        // Listen for stderr.
+        let stderrFailure = false;
+        if (_vsts_input_failOnStandardError) {
+            powershell.on('stderr', (data) => {
+                stderrFailure = true;
+            });
+        }
 
         // Run bash.
         let exitCode: number = await powershell.exec(options);
+
+        // Fail on exit code.
+        if (exitCode !== 0) {
+            tl.setResult(tl.TaskResult.Failed, tl.loc('JS_ExitCode', exitCode));
+        }
+
+        // Fail on stderr.
+        if (stderrFailure) {
+            tl.setResult(tl.TaskResult.Failed, tl.loc('JS_Stderr'));
+        }
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
