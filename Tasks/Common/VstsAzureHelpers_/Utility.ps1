91dca99120c64c07a43b62c4e372d406cd40a164
Vincent A (vinca) <vinca@microsoft.com>
2019-01-09 09:55:05 +0530

    Revert "[Azure PowerShell V2 & V3] Set & Clear context at process Scope (#9251)" (#9267)
    This reverts commit e22c5479e29af57078f0805a288851b8b9c8b89e.

--- Tasks/Common/VstsAzureHelpers_/Utility.ps1
+++ Tasks/Common/VstsAzureHelpers_/Utility.ps1
@@ -496,21 +496,23 @@ function Disconnect-AzureAndClearContext {
             Write-Verbose "Trying to disconnect from Azure and clear context"
 
             if (Get-Command -Name "Disconnect-AzureRmAccount" -ErrorAction "SilentlyContinue") {
-                Write-Host "##[command]Disconnect-AzureRmAccount -Scope Process"
-                $null = Disconnect-AzureRmAccount -Scope Process
+                Write-Host "##[command]Disconnect-AzureRmAccount"
+                $null = Disconnect-AzureRmAccount
             }
             elseif (Get-Command -Name "Remove-AzureRmAccount" -ErrorAction "SilentlyContinue") {
-                Write-Host "##[command]Remove-AzureRmAccount -Scope Process"
-                $null = Remove-AzureRmAccount -Scope Process
+                Write-Host "##[command]Remove-AzureRmAccount"
+                $null = Remove-AzureRmAccount
             }
             elseif (Get-Command -Name "Logout-AzureRmAccount" -ErrorAction "SilentlyContinue") {
-                Write-Host "##[command]Logout-AzureRmAccount -Scope Process"
-                $null = Logout-AzureRmAccount -Scope Process
+                Write-Host "##[command]Logout-AzureRmAccount"
+                $null = Logout-AzureRmAccount
             }
 
             if (Get-Command -Name "Clear-AzureRmContext" -ErrorAction "SilentlyContinue") {
                 Write-Host "##[command]Clear-AzureRmContext -Scope Process"
                 $null = Clear-AzureRmContext -Scope Process
+                Write-Host "##[command]Clear-AzureRmContext -Scope CurrentUser -Force -ErrorAction SilentlyContinue"
+                $null = Clear-AzureRmContext -Scope CurrentUser -Force -ErrorAction SilentlyContinue
             }
         }
     } catch {
