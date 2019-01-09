0c68402a11b24a8b84126b926feaaa468a83f68a
Shivangi <Shivangi.Mittal@microsoft.com>
2019-01-09 14:59:23 +0530

    Making AzurePowerShell cross-platform

--- Tasks/AzurePowerShellV4/InitializeAz.ps1
+++ Tasks/AzurePowerShellV4/InitializeAz.ps1
@@ -17,10 +17,30 @@ $module = Get-Module -Name $moduleName -ListAvailable | Sort-Object Version -Des
 $module = Import-Module -Name $module.Path -Global -PassThru -Force
 $environmentName = $endpointObject.environment
 
-$psCredential = New-Object System.Management.Automation.PSCredential(
+# Clear context
+Write-Host "##[command]Clear-AzContext -Scope Process"
+$null = Clear-AzContext -Scope Process
+Write-Host "##[command]Clear-AzContext -Scope CurrentUser -Force -ErrorAction SilentlyContinue"
+$null = Clear-AzContext -Scope CurrentUser -Force -ErrorAction SilentlyContinue 
+
+if ($endpointObject.scheme -eq 'ServicePrincipal') {
+    try {
+        if ($endpointObject.authenticationType -ne 'SPNCertificate') {
+            $psCredential = New-Object System.Management.Automation.PSCredential(
                     $endpointObject.servicePrincipalClientID,
                     (ConvertTo-SecureString $endpointObject.servicePrincipalKey -AsPlainText -Force))
-
-$null = Connect-AzAccount -ServicePrincipal -Tenant $endpointObject.tenantId `
-        -Credential $psCredential `
-        -Environment $environmentName -WarningAction SilentlyContinue
+            Write-Host "##[command]Connect-AzAccount -ServicePrincipal -Tenant $($endpointObject.tenantId) -Credential $psCredential -Environment $environmentName"
+            $null = Connect-AzAccount -ServicePrincipal -Tenant $endpointObject.tenantId `
+            -Credential $psCredential `
+            -Environment $environmentName -WarningAction SilentlyContinue
+        }
+    }
+    catch {
+        # Provide an additional, custom, credentials-related error message. Will handle localization later
+        throw ("Only support SPN credentials cross platform for now in Azure PowerShell")
+    }
+}
+else {
+    #  Provide an additional, custom, credentials-related error message. Will handle localization later
+    throw ("Only support SPN credentials cross platform for now in Azure PowerShell")
+}
