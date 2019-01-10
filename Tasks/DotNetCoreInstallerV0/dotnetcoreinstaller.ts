43352e86412bcfb8f6f5a08d047d0a392a71fbfb
Paul Jeschke <Paul-Jeschke@outlook.com>
2019-01-10 23:43:06 +0100

    fix node 5 errors

--- Tasks/DotNetCoreInstallerV0/dotnetcoreinstaller.ts
+++ Tasks/DotNetCoreInstallerV0/dotnetcoreinstaller.ts
@@ -8,7 +8,7 @@ import * as os from 'os';
 import * as path from 'path';
 
 class DotnetCoreInstaller {
-    constructor(workingDirectory: string, packageType: string, version?: string, useGlobalJson: boolean = false) {
+    constructor(workingDirectory: string, packageType: string, version?: string, useGlobalJson?: boolean) {
         this.useGlobalJson = useGlobalJson;
         this.packageType = packageType;
         this.workingDirectory = workingDirectory;
