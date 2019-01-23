b24b5b716eed80027e2fdb8cbd14a8a183f13bd7
Danny McCormick <damccorm@microsoft.com>
2019-01-23 11:12:38 -0500

    Begin to enforce changes to common=version bump

--- ci/filter-tasks.js
+++ ci/filter-tasks.js
@@ -155,10 +155,10 @@ var getTasksToBuildForPR = function() {
             toBeBuilt.push(task);
         }
     });
-    // TODO: Add this back once diffing is working 100%
-//     if (shouldBeBumped.length > 0) {
-//         throw new Error('The following tasks should have their versions bumped due to changes in common: ' + shouldBeBumped);
-//     }
+    if (shouldBeBumped.length > 0) {
+        throw new Error('The following tasks should have their versions bumped due to changes in common: ' + shouldBeBumped);
+    }
+
     return toBeBuilt;
 }
 
