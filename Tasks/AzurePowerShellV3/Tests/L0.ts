5ab23484c3549356d5922949d93189b9812d3658
RoshanKumarMicrosoft <33001640+RoshanKumarMicrosoft@users.noreply.github.com>
2019-01-25 21:44:47 +0530

    Azure powershellv3 L0 Failed Fixed (#9422)
    * Azure powershell v3 tL0 Failed
    
    * Azure Powershell V3 task fixed
    
    * Enabled L0

--- Tasks/AzurePowerShellV3/Tests/L0.ts
+++ Tasks/AzurePowerShellV3/Tests/L0.ts
@@ -55,7 +55,7 @@ describe('AzurePowerShell Suite', function () {
         it('throws when invalid script path', (done) => {
             psr.run(path.join(__dirname, 'ThrowsWhenInvalidScriptPath.ps1'), done);
         })
-        /*it('does not fail if native command writes to stderr and failonstderr is false', (done) => {
+        it('does not fail if native command writes to stderr and failonstderr is false', (done) => {
             psr.run(path.join(__dirname, 'DoesNotThrowForNativeCommandError.ps1'), done);
         })
         it('fails for native command error if fail on standard error is true', (done) => {
@@ -66,6 +66,6 @@ describe('AzurePowerShell Suite', function () {
         })
         it('Update-PSModulePathForHostedAgent updated psmodulepath correctly', (done) => {
             psr.run(path.join(__dirname, 'Utility.UpdatePSModulePathForHostedAgentWorksCorrectly.ps1'), done);
-        })*/
+        })
     }
 });
