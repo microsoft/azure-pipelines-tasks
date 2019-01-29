a3ff767cabfb433225565e53bb00215d23fd5dea
Aasim Malladi <aasim.malladi@microsoft.com>
2019-01-28 16:18:38 -0800

    test

--- Tasks/DownloadPackageV1/main.ts
+++ Tasks/DownloadPackageV1/main.ts
@@ -12,22 +12,33 @@ tl.setResourcePath(path.join(__dirname, "task.json"));
 
 async function main(): Promise<void> {
     // TODO Get that logic to check for GUID from v0 after verifying why its needed.
-    // TODO fix getvar to get input
+ 
 
-    // Getting variables.
-    let packageType = tl.getVariable("packageType");
+    // Getting inputs.
+    let packageType = tl.getInput("packageType");
     let feedId = tl.getInput("feed");
-    let viewId = tl.getVariable("view");
-    let packageId = tl.getVariable("definition");
-    let version = tl.getVariable("version");
+    let viewId = tl.getInput("view");
+    let packageId = tl.getInput("definition");
+    let version = tl.getInput("version");
     let downloadPath = tl.getInput("downloadPath");
-    let collectionUrl = tl.getVariable("System.TeamFoundationCollectionUri");
-    let filesPattern = tl.getVariable("files");
-    let files: string[] = nutil.getPatternsArrayFromInput(filesPattern);
-    let extractPackage = tl.getVariable("extract") === "true";
-
+    let filesPattern = tl.getInput("files");
+    let extractPackage = tl.getInput("extract") === "true";
+    
+    // Getting variables.
+    const collectionUrl = tl.getVariable("System.TeamFoundationCollectionUri");
     const retryLimitValue: string = tl.getVariable("VSTS_HTTP_RETRY");
     const retryLimit: number = !!retryLimitValue && !isNaN(parseInt(retryLimitValue)) ? parseInt(retryLimitValue) : 4;
+    const deleteArchive = tl.getVariable("deleteArchive") === "true";
+    const skipDownload = tl.getVariable("skipDownload") === "true";
+
+    if(viewId) {
+        feedId = feedId + "@" + viewId;
+    }
+        
+    let files: string[] = [];
+    if(filesPattern) {
+        files = nutil.getPatternsArrayFromInput(filesPattern);
+    }
 
     var feedConnection = await getConnection("7AB4E64E-C4D8-4F50-AE73-5EF2E21642A5", collectionUrl);
     var pkgsConnection = await getConnection("B3BE7473-68EA-4A81-BFC7-9530BAAA19AD", collectionUrl);
