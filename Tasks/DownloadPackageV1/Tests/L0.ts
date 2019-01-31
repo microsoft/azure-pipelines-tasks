a6fe4315cf942d74a9c5a3ebfd2dd1ef7c56e27f
Aasim Malladi <aasim.malladi@microsoft.com>
2019-01-30 18:22:09 -0800

    added unit tests.

--- Tasks/DownloadPackageV1/Tests/L0.ts
+++ Tasks/DownloadPackageV1/Tests/L0.ts
@@ -25,7 +25,48 @@ describe('Download single file package suite', function () {
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
         tr.run();
-        
+        assert(tl.ls(null, [outputDir]).length == 1, "should have only 1 file.");
+        const zipPath = path.join(outputDir, "singlePackageName.zip");
+        const stats = tl.stats(zipPath);
+        assert(stats && stats.isFile(), "zip file should be downloaded");
+        assert(tr.stderr.length === 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+});
+
+
+
+describe('Download multi file package suite', function () {
+    this.timeout(parseInt(process.env.TASK_TEST_TIMEOUT) || 20000);
+
+    before(() => {
+        fs.mkdir(outputDir);
+    });
+
+    after(() => {
+        tl.rmRF(outputDir);
+    });
+
+    it('only downloads jar and pom files from the maven archive and doesn\'t extract them', (done: MochaDone) => {
+        this.timeout(1000);
+
+        let tp: string = path.join(__dirname, 'L0DownloadMultiFilePackage.js');
+
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+
+        let outputJarPath: string = path.join(outputDir, "packageName.jar");
+        let outputPomPath: string = path.join(outputDir, "packageName.pom");
+        assert(tl.ls(null, [outputDir]).length == 2, "should have only 2 files.");
+        const statsJar = tl.stats(outputJarPath);
+        const statsPom = tl.stats(outputPomPath);
+
+        assert(statsJar && statsJar.isFile(), "jar file should be downloaded");
+        assert(statsPom && statsPom.isFile(), "pom file should be downloaded");
+
         assert(tr.stderr.length === 0, 'should not have written to stderr');
         assert(tr.succeeded, 'task should have succeeded');
 
--- Tasks/DownloadPackageV1/Tests/helpers/webapimock.ts
+++ Tasks/DownloadPackageV1/Tests/helpers/webapimock.ts
@@ -22,7 +22,8 @@ class VsoClientMock {
     private packageUrlMap = {
         "7A20D846-C929-4ACC-9EA2-0D5A7DF1B197": "singlePackageMetadataUrl",
         "6EA81B8C-7386-490B-A71F-6CF23C80B388": "singlePackageDownloadUrl",
-        "3B331909-6A86-44CC-B9EC-C1834C35498F": "multiPackageMetadataUrl"
+        "3B331909-6A86-44CC-B9EC-C1834C35498F": "multiPackageMetadataUrl",
+        "F285A171-0DF5-4C49-AAF2-17D0D37D9F0E": "multiPackageDownloadUrl"
     };
     async getVersioningData(
         apiVersion: string,
@@ -31,6 +32,7 @@ class VsoClientMock {
         routeValues: any,
         queryParams?: any
     ): Promise<any> {
+        console.log("returning url " + this.packageUrlMap[locationId]);
         return Promise.resolve({ requestUrl: this.packageUrlMap[locationId] });
     }
 }
@@ -39,9 +41,47 @@ class RestMock {
     private metadataMap = {
         singlePackageMetadataUrl: {
             name: "singlePackageName"
+        },
+        multiPackageMetadataUrl: {
+            protocolMetadata: {
+                data: {
+                    groupId: "groupid",
+                    artifactId: "artifactId",
+                    version: "version"
+                }
+            },
+            files: [
+                {
+                    name: "packageName.jar",
+                    protocolMetadata: {
+                        data: {
+                            storageId: "storageId"
+                        }
+                    }
+                },
+                {
+                    name: "packageName.pom",
+                    protocolMetadata: {
+                        data: {
+                            storageId: null,
+                            content: "pom content"
+                        }
+                    }
+                },
+                {
+                    name: "packageName.xml",
+                    protocolMetadata: {
+                        data: {
+                            storageId: null
+                        }
+                    }
+                }
+            ]
         }
     };
     async get(resource: string, options?: any): Promise<any> {
+        console.log("URl resource " + resource);
+        console.log("url res val " + JSON.stringify(this.metadataMap[resource]));
         return Promise.resolve({ result: this.metadataMap[resource] });
     }
 }
