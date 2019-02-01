c4d32a38981653a701482f72ffb0ba479856e443
Satu Bailey <24945574+satbai@users.noreply.github.com>
2019-02-01 13:14:40 -0800

    Reverting 9407 origin/users/magleaso/try catch fix (#9490)
    * Revert "Adding dockercli installer task in task list (#9358)"
    
    This reverts commit d15ac705b15d99cd335ed550da39382eb0245e2e.
    
    * Revert "Porting L0 fixed in m146 (#9424)"
    
    This reverts commit 68c9576e87dd5736c2914dd8255bdac085d20019.
    
    * Revert "Fix Failing Catch Block (#9385) (#9407)"
    
    This reverts commit b50a4a7b564a25e614f7bbae0c63f79c79aeeea4.
    
    * Bump version

--- Tasks/NpmAuthenticateV0/npmauth.ts
+++ Tasks/NpmAuthenticateV0/npmauth.ts
@@ -71,9 +71,13 @@ async function main(): Promise<void> {
     try {
         packagingLocation = await pkgLocationUtils.getPackagingUris(pkgLocationUtils.ProtocolType.Npm);
     } catch (error) {
-        tl.debug('Unable to get packaging URIs');
+        tl.debug('Unable to get packaging URIs, using default collection URI');
         tl.debug(JSON.stringify(error));
-        throw new Error(error);
+        const collectionUrl = tl.getVariable('System.TeamFoundationCollectionUri');
+        packagingLocation = {
+            PackagingUris: [collectionUrl],
+            DefaultPackagingUri: collectionUrl
+        };
     }
     let LocalNpmRegistries = await npmutil.getLocalNpmRegistries(workingDirectory, packagingLocation.PackagingUris);
     
