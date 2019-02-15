8e1a019acd8ab3db9b120482784eea075f2815d7
Shreyas R <shrer@microsoft.com>
2019-02-15 16:15:25 +0530

    Added VS 2019 support to VsTest V2 (#9595)
    * Added VS 2019 support to VsTest V2
    
    * Fixed version param

--- Tasks/VsTestV2/taskinputparser.ts
+++ Tasks/VsTestV2/taskinputparser.ts
@@ -165,7 +165,7 @@ function initTestConfigurations(testConfiguration: models.TestConfigurations) {
             testConfiguration.vsTestLocation = testConfiguration.toolsInstallerConfig.vsTestConsolePathFromPackageLocation;
 
             testConfiguration.toolsInstallerConfig.isToolsInstallerInUse = true;
-        } else if ((testConfiguration.vsTestVersion !== '15.0') && (testConfiguration.vsTestVersion !== '14.0')
+        } else if ((testConfiguration.vsTestVersion !== '16.0') && (testConfiguration.vsTestVersion !== '15.0') && (testConfiguration.vsTestVersion !== '14.0')
             && (testConfiguration.vsTestVersion.toLowerCase() !== 'latest')) {
             throw new Error(tl.loc('vstestVersionInvalid', testConfiguration.vsTestVersion));
         }
