4d51c6ef5f8b9497f8f39f59dc83ef71f2c46173
madhurig <madhuri.gummalla@hotmail.com>
2019-02-20 17:35:45 -0500

    Remove warning from maven task when test results are missing

--- Tasks/MavenV3/maventask.ts
+++ Tasks/MavenV3/maventask.ts
@@ -334,7 +334,7 @@ function publishJUnitTestResults(testResultsFiles: string) {
     }
 
     if (!matchingJUnitResultFiles || matchingJUnitResultFiles.length == 0) {
-        tl.warning('No test result files matching ' + testResultsFiles + ' were found, so publishing JUnit test results is being skipped.');
+        console.log('No test result files matching ' + testResultsFiles + ' were found, so publishing JUnit test results is being skipped.');
         return 0;
     }
 
