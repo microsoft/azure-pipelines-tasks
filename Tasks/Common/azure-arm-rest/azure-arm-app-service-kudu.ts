5b26db571c5560a8bfc761868bec6efa6761bf3c
vinca <vinca@microsoft.com>
2019-02-20 11:18:15 +0530

    log affinity-cookie

--- Tasks/Common/azure-arm-rest/azure-arm-app-service-kudu.ts
+++ Tasks/Common/azure-arm-rest/azure-arm-app-service-kudu.ts
@@ -11,7 +11,7 @@ import { KUDU_DEPLOYMENT_CONSTANTS } from './constants';
 export class KuduServiceManagementClient {
     private _scmUri;
     private _accesssToken: string;
-    private _cookie: string[];
+    private _cookie: string[] = undefined;
 
     constructor(scmUri: string, accessToken: string) {
         this._accesssToken = accessToken;
@@ -26,7 +26,7 @@ export class KuduServiceManagementClient {
         }
         
         if(!!this._cookie) {
-            tl.debug(`setting affinity cookie ${this._cookie.join(",")}`);
+            tl.debug(`setting affinity cookie ${JSON.stringify(this._cookie)}`);
             request.headers['Cookie'] = this._cookie;
         }
 
@@ -35,9 +35,9 @@ export class KuduServiceManagementClient {
         while(retryCount >= 0) {
             try {
                 let httpResponse = await webClient.sendRequest(request, reqOptions);
-                if(!this._cookie) {
+                if(httpResponse.headers['set-cookie'] && !this._cookie) {
                     this._cookie = httpResponse.headers['set-cookie'];
-                    tl.debug(`loaded affinity cookie ${this._cookie.join(",")}`);
+                    tl.debug(`loaded affinity cookie ${JSON.stringify(this._cookie)}`);
                 }
 
                 return httpResponse;
