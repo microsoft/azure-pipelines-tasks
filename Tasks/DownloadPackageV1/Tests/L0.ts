072a913ecbb9d97f460ea445fd97e8ee3d03fbf7
Aasim Malladi <aasim.malladi@gmail.com>
2019-02-25 11:46:38 -0800

    Added download packages task V1 (#9433)
    * working md
    
    * single file download successful
    
    * added npm support as well... make sure to check the package extension
    
    * fixed connections
    
    * almost working maven
    
    * multiple maven working
    
    * final working without python
    
    * remove unused stuff
    
    * fixes
    
    * working all
    
    * fixed content files, added pattern
    
    * all working
    
    * added download utility
    
    * fixed all
    ;
    
    * added retries
    
    * working
    
    * fixed strings
    
    * test
    
    * everything working
    
    * fixed cred provider
    
    * minor fixes.
    
    * fixes for test
    
    * added unit tests.
    
    * working code
    
    * res
    
    * test
    
    * removed log
    
    * Remove 7zip
    
    * added retry logic
    
    * fixed main
    
    * fixed tests
    
    * added upack support
    
    * fixed unit tests
    
    * added coment;
    
    * Fixed comments
    
    * fixed comments
    
    * fixed tests
    
    * fixed npm file
    
    * Fixes
    
    * fixed package extraction logic
    
    * Added telemetry and icon.
    
    * FIxed review comments

---
+++ Tasks/DownloadPackageV1/Tests/L0.ts
@@ -0,0 +1,105 @@
+import fs = require("fs");
+import assert = require("assert");
+import path = require("path");
+import * as tl from "vsts-task-lib/task";
+import * as ttm from "vsts-task-lib/mock-test";
+
+const outputDir = path.join(__dirname, "out");
+const outputExtractDir = path.join(outputDir, "packageOutput");
+
+describe("Download single file package suite", function() {
+    this.timeout(parseInt(process.env.TASK_TEST_TIMEOUT) || 20000);
+
+    beforeEach(() => {
+        tl.mkdirP(outputExtractDir);
+    });
+
+    afterEach(() => {
+        tl.rmRF(outputDir);
+    });
+
+    it("downloads nuget file as zip and extracts it", (done: MochaDone) => {
+        this.timeout(1000);
+
+        let tp: string = path.join(__dirname, "L0DownloadNugetPackage.js");
+
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+
+        assert(tl.ls(null, [outputDir]).length == 2, "should have only 1 file and 1 folder.");
+        const zipPath = path.join(outputDir, "singlePackageName.nupkg");
+        const zipStats = tl.stats(zipPath);
+        assert(zipStats && zipStats.isFile(), "zip file should be downloaded");
+
+        var extractedFilePath = path.join(outputDir, "packageOutput", "nugetFile");
+        const fileStats = tl.stats(extractedFilePath);
+        assert(fileStats && fileStats.isFile(), "zip file should be extracted");
+
+        assert(tr.stderr.length === 0, "should not have written to stderr");
+        assert(tr.succeeded, "task should have succeeded");
+
+        done();
+    });
+
+    it("downloads npm file as tgz and extracts it", (done: MochaDone) => {
+        this.timeout(1000);
+
+        let tp: string = path.join(__dirname, "L0DownloadNpmPackage.js");
+
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+
+        assert(tl.ls(null, [outputDir]).length == 2, "should have only 1 file and 1 folder.");
+        const zipPath = path.join(outputDir, "singlePackageName.tgz");
+        const zipStats = tl.stats(zipPath);
+        assert(zipStats && zipStats.isFile(), "tgz file should be downloaded");
+
+        var extractedFilePath = path.join(outputExtractDir, "npmFile");
+        const fileStats = tl.stats(extractedFilePath);
+        assert(fileStats && fileStats.isFile(), "tgz file should be extracted");
+
+        assert(tr.stderr.length === 0, "should not have written to stderr");
+        assert(tr.succeeded, "task should have succeeded");
+
+        done();
+    });
+});
+
+
+describe("Download multi file package suite", function() {
+    this.timeout(parseInt(process.env.TASK_TEST_TIMEOUT) || 20000);
+
+    before(() => {
+        tl.mkdirP(outputDir);
+    });
+
+    after(() => {
+        tl.rmRF(outputDir);
+    });
+
+    it("only downloads jar and pom files from the maven archive and doesn't extract them", (done: MochaDone) => {
+        this.timeout(1000);
+
+        let tp: string = path.join(__dirname, "L0DownloadMultiFilePackage.js");
+
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+
+        let outputJarPath: string = path.join(outputDir, "packageName.jar");
+        let outputPomPath: string = path.join(outputDir, "packageName.pom");
+        assert(tl.ls(null, [outputDir]).length == 2, "should have only 2 files.");
+        const statsJar = tl.stats(outputJarPath);
+        const statsPom = tl.stats(outputPomPath);
+
+        assert(statsJar && statsJar.isFile(), "jar file should be downloaded");
+        assert(statsPom && statsPom.isFile(), "pom file should be downloaded");
+
+        assert(tr.stderr.length === 0, "should not have written to stderr");
+        assert(tr.succeeded, "task should have succeeded");
+
+        done();
+    });
+});
---
+++ Tasks/DownloadPackageV1/extractor.ts
@@ -0,0 +1,68 @@
+import * as tl from "vsts-task-lib/task";
+import * as path from "path";
+import * as fs from "fs";
+var tar = require("tar-fs");
+var zlib = require("zlib");
+var DecompressZip = require('decompress-zip');
+
+export class Extractor {
+    public readonly win: boolean;
+
+    private zipLocation: string;
+    private unzipLocation: string;
+
+    constructor(zipLocation: string, unzipLocation: string) {
+        this.zipLocation = zipLocation;
+        this.unzipLocation = unzipLocation;
+    }
+
+    async process(extractPackage: boolean): Promise<void> {
+        return extractPackage ? this.extract() : this.move();
+    }
+
+    private async extract(): Promise<void> {
+        const fileEnding = path.parse(this.zipLocation).ext;
+        switch (fileEnding) {
+            case ".zip":
+            case ".nupkg":
+                return this.unzip(this.zipLocation, this.unzipLocation);
+            case ".tgz":
+                return this.unTarGz(this.zipLocation, this.unzipLocation);
+            default:
+                return Promise.reject(tl.loc("UnsupportedArchiveType", fileEnding));
+        }
+    }
+
+
+    private async move(): Promise<void> {
+        return Promise.resolve(tl.mv(this.zipLocation, this.unzipLocation));
+    }
+
+    private async unTarGz(zipLocation: string, unzipLocation: string): Promise<void> {
+        return Promise.resolve(
+            fs
+                .createReadStream(zipLocation)
+                .pipe(zlib.createGunzip())
+                .pipe(tar.extract(unzipLocation))
+        );
+    }
+
+    private async unzip(zipLocation: string, unzipLocation: string): Promise<void> {
+        return new Promise<void>(function(resolve, reject) {
+            tl.debug("Extracting " + zipLocation + " to " + unzipLocation);
+
+            var unzipper = new DecompressZip(zipLocation);
+            unzipper.on("error", err => {
+                return reject(tl.loc("ExtractionFailed", err));
+            });
+            unzipper.on("extract", () => {
+                tl.debug("Extracted " + zipLocation + " to " + unzipLocation + " successfully");
+                return resolve();
+            });
+
+            unzipper.extract({
+                path: unzipLocation
+            });
+        });
+    }
+}
