c5fd5014c69adfcab7066e4a12be066c5d27e315
Deepak Sattiraju <desattir@microsoft.com>
2019-02-27 13:07:46 +0530

    [KubernetesManifest] Artifact Substitution Logic fix (#9683) (#9693)
    * [KubernetesManifest] Artifact Substitution Logic fix
    
    * Addressing PR comments

--- Tasks/KubernetesManifestV0/src/actions/deploy.ts
+++ Tasks/KubernetesManifestV0/src/actions/deploy.ts
@@ -123,9 +123,11 @@ function updateContainerImagesInConfigFiles(filePaths: string[], containers): st
         filePaths.forEach((filePath: string) => {
             var contents = fs.readFileSync(filePath).toString();
             containers.forEach((container: string) => {
-                let imageName = container.split(":")[0] + ":";
+                let imageName = container.split(":")[0];
+                if (imageName.indexOf("@") > 0)
+                    imageName = imageName.split("@")[0];
                 if (contents.indexOf(imageName) > 0) {
-                    contents = utils.replaceAllTokens(contents, imageName, container);
+                    contents = utils.substituteImageNameInSpecFile(contents, imageName, container);
                 }
             });
 
