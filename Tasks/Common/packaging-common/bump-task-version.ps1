7fb653403cf46fdec3fd967336bb92d9de29b054
Jovana Taylor <jotaylo@microsoft.com>
2019-03-06 17:43:37 -0800

    Su -> sprint

--- Tasks/Common/packaging-common/bump-task-version.ps1
+++ Tasks/Common/packaging-common/bump-task-version.ps1
@@ -2,8 +2,8 @@
 # Please validate the updated versions to confirm that nothing went horribly wrong.
 Param(
 [parameter(Mandatory=$true)]
-# The current SU, find at https://whatsprintis.it/
-[int] $currentSU,
+# The current sprint, find at https://whatsprintis.it/
+[int] $currentSprint,
 # The path to the Tasks folder
 [parameter()]
 [string] $taskRoot = (Join-Path $PSScriptRoot "..\..")
@@ -15,14 +15,14 @@ Param(
     $task = $taskContent | ConvertFrom-Json
     $v = $task.version
     $newPatch = 0
-    if ($v.Minor -eq $currentSU) {
+    if ($v.Minor -eq $currentSprint) {
         $newPatch = $v.Patch + 1
     }
-    Write-Output "${_}: $($v.Major).$($v.Minor).$($v.Patch) to $($v.Major).$currentSU.$newPatch"
+    Write-Output "${_}: $($v.Major).$($v.Minor).$($v.Patch) to $($v.Major).$currentSprint.$newPatch"
     # Doing an awkward string replace to not interfere with any custom task.json formatting
     $versionMatch = $taskContent | Select-String '"version":'
     $versionLine = $versionMatch.LineNumber
-    $taskContent[$versionLine+1] = $taskContent[$versionLine+1].Replace(" $($v.Minor),", " $currentSU,")
+    $taskContent[$versionLine+1] = $taskContent[$versionLine+1].Replace(" $($v.Minor),", " $currentSprint,")
     $taskContent[$versionLine+2] = $taskContent[$versionLine+2].Replace(" $($v.Patch)", " $newPatch")
     $taskContent | Set-Content $taskLocation
 }
