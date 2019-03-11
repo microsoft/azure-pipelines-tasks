94d7f0e5f558a6c52177804e81b7923d5a517916
Evgenii Khramkov <evkhramk@microsoft.com>
2019-03-11 12:11:36 +0300

    add comment

--- Tasks/AppCenterDistributeV3/Tests/L0.ts
+++ Tasks/AppCenterDistributeV3/Tests/L0.ts
@@ -10,6 +10,8 @@ describe('AppCenterDistribute L0 Suite', function () {
     before(() => {
         //Enable this for output
         //process.env['TASK_TEST_TRACE'] = 1;
+
+        //clean env variables
         delete process.env['BUILD_BUILDID'];
         delete process.env['BUILD_SOURCEBRANCH'];
         delete process.env['BUILD_SOURCEVERSION'];
