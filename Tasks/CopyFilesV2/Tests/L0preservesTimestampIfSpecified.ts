7bc84d1d808bf1c0723851366367a3a61493fb8e
Carlo Wahlstedt <carlowahlstedt@gmail.com>
2019-03-16 22:12:35 -0400

    Updated the tests so it should succeed

--- Tasks/CopyFilesV2/Tests/L0preservesTimestampIfSpecified.ts
+++ Tasks/CopyFilesV2/Tests/L0preservesTimestampIfSpecified.ts
@@ -21,22 +21,26 @@ answers.find[path.normalize('/srcDir')] = [
     path.normalize('/srcDir/someOtherDir'),
     path.normalize('/srcDir/someOtherDir/file1.file'),
     path.normalize('/srcDir/someOtherDir/file2.file'),
+    path.normalize('/srcDir/someOtherDir2'),
+    path.normalize('/srcDir/someOtherDir2/file1.file'),
+    path.normalize('/srcDir/someOtherDir2/file2.file'),
+    path.normalize('/srcDir/someOtherDir2/file3.file'),
+    path.normalize('/srcDir/someOtherDir3'),
 ];
 runner.setAnswers(answers);
-runner.registerMockExport('stats', (itemPath: string): any => {
+runner.registerMockExport('stats', (itemPath: string) => {
     console.log('##vso[task.debug]stats ' + itemPath);
     switch (itemPath) {
         case path.normalize('/srcDir/someOtherDir'):
-        case path.normalize('/destDir/someOtherDir'):
+        case path.normalize('/srcDir/someOtherDir2'):
+        case path.normalize('/srcDir/someOtherDir3'):
             return { isDirectory: () => true };
         case path.normalize('/srcDir/someOtherDir/file1.file'):
         case path.normalize('/srcDir/someOtherDir/file2.file'):
+        case path.normalize('/srcDir/someOtherDir2/file1.file'):
+        case path.normalize('/srcDir/someOtherDir2/file2.file'):
+        case path.normalize('/srcDir/someOtherDir2/file3.file'):
             return { isDirectory: () => false };
-        case path.normalize('/destDir/someOtherDir/file1.file'):
-            return {
-                isDirectory: () => false,
-                mode: (6 << 6) + (6 << 3) + 6, // rw-rw-rw-
-            };
         default:
             throw { code: 'ENOENT' };
     }
