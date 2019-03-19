5750aa413b45a9a14744736e044edb1ac8ddf0bd
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2019-03-19 12:26:39 -0400

    Use Python Version: add support for PyPy and -dev syntax (#9866)
    * Test prepending PATH in E2E tests
    
    * Let UsePythonVersion summon Anaconda
    
    * Bump version
    
    * Update to task-lib 2.8
    
    * Add desugarDevVersion
    
    * Add PyPy support
    
    * Error handling for PyPy
    
    * Hold off on naked 'pypy' version spec for now
    
    * Add test for PyPy3
    
    * Adapt task to work with all PyPy versions in the same tool cache directory
    
    * Add test for -dev syntax
    
    * Fix usePypy
    
    * Fall back to x86 on Windows

--- Tasks/UsePythonVersionV0/Tests/L0PyPy3.ts
+++ Tasks/UsePythonVersionV0/Tests/L0PyPy3.ts
@@ -5,17 +5,20 @@ import { TaskMockRunner } from 'azure-pipelines-task-lib/mock-run';
 const taskPath = path.join(__dirname, '..', 'main.js');
 const taskRunner = new TaskMockRunner(taskPath);
 
-taskRunner.setInput('versionSpec', '3.x');
-taskRunner.setInput('addToPath', 'false');
+taskRunner.setInput('versionSpec', 'pypy3');
+taskRunner.setInput('addToPath', 'true');
 taskRunner.setInput('architecture', 'x64');
 
-// `getVariable` is not supported by `TaskLibAnswers`
-process.env['AGENT_TOOLSDIRECTORY'] = '$(Agent.ToolsDirectory)';
-
 // Mock azure-pipelines-tool-lib
+const toolPath = path.join('/', 'PyPy', '3.5.2', 'x64');
 taskRunner.registerMock('azure-pipelines-tool-lib/tool', {
-    findLocalTool: () => null,
-    findLocalToolVersions: () => ['2.6.0', '2.7.13']
+    findLocalTool: () => toolPath
+});
+
+taskRunner.registerMock('./toolutil', {
+    prependPathSafe: (toolPath: string) => {
+        console.log('##vso[task.prependpath]' + toolPath);
+    }
 });
 
 taskRunner.run();
