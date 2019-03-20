030ec5d81c43b1aaa985ea07ffb4d8ad85f18032
Dhashrath Govindarajan <dhgovind@microsoft.com>
2019-03-20 13:40:51 +0530

    add utils to be consumed for publishing image metadata

--- Tasks/Common/docker-common/dockercommandutils.ts
+++ Tasks/Common/docker-common/dockercommandutils.ts
@@ -72,3 +72,97 @@ export function push(connection: ContainerConnection, image: string, commandArgu
         onCommandOut(output + "\n");
     });
 }
+
+export function getLayers(connection: ContainerConnection, imageId: string): any {
+    const NONE_TAG = '<none>:<none>';
+    const NOP_PREFIX = '#(nop) ';
+    const TOKEN_WHITESPACE = RegExp(/[\t\v\f\r ]+/);
+
+    var imageDetails = images(connection, "--format \"{{json . }}\"").split(/[\r?\n]/);;
+    var i = 0;
+    var tags: { [imageId: string] : string }  = {}
+    for (i = 0; i < imageDetails.length; i++){
+        var imageDetail = JSON.parse(imageDetails[i]);
+        var tag = imageDetail.info['RepoTags'].first
+        if (tag != NONE_TAG){
+            tags[imageDetail['Id']] = tag;
+        }
+    }
+
+    var layers = [];
+    while (true){
+        if(tags[imageId]){
+            layers.push({ Directive : "FROM", Arguments : "", Id : imageId, Size : "", CreatedOn : "" });
+        }
+
+        var imageDetail = inspect(connection, imageId);
+
+        if (!imageDetail){
+            tl.warning("Specified Image Id : " + imageId + "could not be found.")
+            break;
+        }
+
+        var cmds = imageDetail.info['ContainerConfig']['Cmd']
+
+        if (cmds && cmds.length > 0){
+            var cmd : string = cmds.last;
+            if (cmd.startsWith(NOP_PREFIX)){
+                cmd.replace(NOP_PREFIX, "");
+                var match = cmd.match(TOKEN_WHITESPACE);
+                if (!match) {
+                    layers.push ({ Directive: "UNSPECIFIED", Arguments: cmd, Id: imageId, Size: "", CreatedOn: "" });
+                }
+                else {
+                    var directive = cmd.substr(0, match.index).toUpperCase().trim();
+                    var rest = cmd.substr(match.index + match[0].length).trim();
+                    layers.push ({ Directive: directive, Arguments: rest, Id: imageId, Size: "", CreatedOn: "" });
+                }
+            }
+            else {
+                layers.push({ Directive: "RUN", Arguments: cmd, Id: imageId, Size: "", CreatedOn: "" });
+            }       
+        }
+
+        imageId = imageDetail.info['Parent'];
+
+        if (imageId == ''){
+            break;
+        }
+    }
+    
+    return layers.reverse();
+}
+
+export function images(connection: ContainerConnection, commandArguments: string): any {
+    var command = connection.createCommand();
+    command.arg("images");
+    command.line(commandArguments);
+
+    // setup variable to store the command output
+    let output = "";
+    command.on("stdout", data => {
+        output += data;
+    });
+
+    return connection.execCommand(command).then(() => {
+        // Return the std output of the command by calling the delegate
+        output;
+    });
+}
+
+export function inspect(connection: ContainerConnection, image: string): any {
+    var command = connection.createCommand();
+    command.arg("inspect");
+    command.arg(image);
+
+    // setup variable to store the command output
+    let output = "";
+    command.on("stdout", data => {
+        output += data;
+    });
+
+    return connection.execCommand(command).then(() => {
+        // Return the std output of the command by calling the delegate
+        output;
+    });
+}
