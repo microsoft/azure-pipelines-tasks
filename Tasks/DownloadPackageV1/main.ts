c6dce46d2eef71e32177a9f4ee48d64cdb275025
Jovana Taylor <jotaylo@microsoft.com>
2019-03-25 16:23:48 -0700

    DownloadPackageV1

--- Tasks/DownloadPackageV1/main.ts
+++ Tasks/DownloadPackageV1/main.ts
@@ -1,6 +1,6 @@
 var path = require("path");
 
-import * as tl from "vsts-task-lib/task";
+import * as tl from "azure-pipelines-task-lib/task";
 import * as nutil from "packaging-common/nuget/Utility";
 import * as telemetry from "utility-common/telemetry";
 
