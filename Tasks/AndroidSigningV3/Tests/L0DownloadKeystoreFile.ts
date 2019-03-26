ff38c1bf2335bd2ebf4869aae410a07427a47a71
madhurig <madhuri.gummalla@hotmail.com>
2019-03-26 11:24:43 -0400

    update AndroidSigning tasks

--- Tasks/AndroidSigningV3/Tests/L0DownloadKeystoreFile.ts
+++ Tasks/AndroidSigningV3/Tests/L0DownloadKeystoreFile.ts
@@ -1,5 +1,5 @@
-import ma = require('vsts-task-lib/mock-answer');
-import tmrm = require('vsts-task-lib/mock-run');
+import ma = require('azure-pipelines-task-lib/mock-answer');
+import tmrm = require('azure-pipelines-task-lib/mock-run');
 import path = require('path');
 import fs = require('fs');
 
@@ -9,8 +9,6 @@ const tr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 tr.setInput('apksign', 'true');
 tr.setInput('keystoreFile', 'mySecureFileId');
 
-process.env['AGENT_VERSION'] = '2.116.0';
-
 const secureFileHelperMock = require('securefiles-common/securefiles-common-mock');
 tr.registerMock('securefiles-common/securefiles-common', secureFileHelperMock);
 
@@ -19,6 +17,8 @@ tr.registerMock('fs', {
         // empty
     }
 });
+
+process.env['AGENT_VERSION'] = '2.116.0';
 
 // provide answers for task mock
 const a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
