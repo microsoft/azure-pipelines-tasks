8bd36df81fc311a732ab853a7d311df8dda56154
Carlo Wahlstedt <carlowahlstedt@gmail.com>
2019-04-10 11:15:04 -0400

    Added the ability to keep the timestamp of target files (#8946)
    * Added the ability to keep the timestamp of target files
    
    * Made the name consistent using Preserve
    
    * fixed variable name and added callback to log error
    
    * Replaced targetStats with the current file stats
    
    * Changed to const per review
    
    * switched to const per review
    
    * I should verify that preserve timestamps works
    
    * Updated the tests so it should succeed
    
    * mocked up utimes to fix the test failure
    
    * wrapped everything timestamp related in try/catch
    logged to the console with .warn
    
    * Was using file before, must be a copy/paste bug
    
    * Added logging the call and asserting it
    
    * Added a second message to tell what was wrong

---
+++ Tasks/CopyFilesV2/Tests/L0preservesTimestampIfSpecified.ts
@@ -0,0 +1,54 @@
+import fs = require('fs');
+import mockanswer = require('vsts-task-lib/mock-answer');
+import mockrun = require('vsts-task-lib/mock-run');
+import path = require('path');
+
+let taskPath = path.join(__dirname, '..', 'copyfiles.js');
+let runner: mockrun.TaskMockRunner = new mockrun.TaskMockRunner(taskPath);
+runner.setInput('Contents', '**');
+runner.setInput('SourceFolder', path.normalize('/srcDir'));
+runner.setInput('TargetFolder', path.normalize('/destDir'));
+runner.setInput('CleanTargetFolder', 'false');
+runner.setInput('Overwrite', 'false');
+runner.setInput('preserveTimestamp', 'true');
+let answers = <mockanswer.TaskLibAnswers>{
+    checkPath: {},
+    find: {},
+};
+answers.checkPath[path.normalize('/srcDir')] = true;
+answers.find[path.normalize('/srcDir')] = [
+    path.normalize('/srcDir'),
+    path.normalize('/srcDir/someOtherDir'),
+    path.normalize('/srcDir/someOtherDir/file1.file'),
+    path.normalize('/srcDir/someOtherDir/file2.file'),
+    path.normalize('/srcDir/someOtherDir2'),
+    path.normalize('/srcDir/someOtherDir2/file1.file'),
+    path.normalize('/srcDir/someOtherDir2/file2.file'),
+    path.normalize('/srcDir/someOtherDir2/file3.file'),
+    path.normalize('/srcDir/someOtherDir3'),
+];
+runner.setAnswers(answers);
+runner.registerMockExport('stats', (itemPath: string) => {
+    console.log('##vso[task.debug]stats ' + itemPath);
+    switch (itemPath) {
+        case path.normalize('/srcDir/someOtherDir'):
+        case path.normalize('/srcDir/someOtherDir2'):
+        case path.normalize('/srcDir/someOtherDir3'):
+            return { isDirectory: () => true };
+        case path.normalize('/srcDir/someOtherDir/file1.file'):
+        case path.normalize('/srcDir/someOtherDir/file2.file'):
+        case path.normalize('/srcDir/someOtherDir2/file1.file'):
+        case path.normalize('/srcDir/someOtherDir2/file2.file'):
+        case path.normalize('/srcDir/someOtherDir2/file3.file'):
+            return { isDirectory: () => false };
+        default:
+            throw { code: 'ENOENT' };
+    }
+});
+
+fs.utimes = function (targetPath, atime, mtime, err) {
+    console.log('Calling fs.utimes on', targetPath);
+}
+runner.registerMock('fs', fs);
+
+runner.run();
