28297c22f0de3a96634ebc6cae818d65511bd9cc
Deepak Sattiraju <desattir@microsoft.com>
2019-04-22 12:06:00 +0530

    [KubernetesManifest] Annotation fix

--- Tasks/KubernetesManifestV0/src/models/constants.ts
+++ Tasks/KubernetesManifestV0/src/models/constants.ts
@@ -17,25 +17,38 @@ export const recognizedWorkloadTypesWithRolloutStatus: string[] = ["deployment",
 let isRelease = utils.isEqual(tl.getVariable("SYSTEM_HOSTTYPE"), "release", utils.StringComparer.OrdinalIgnoreCase);
 
 export let pipelineAnnotations: string[] = [];
+const devAzurePrefix = "dev.azure.com";
+const vstsPrefix = "visualstudio.com";
+
+let orgName = "";
+let orgUrl = tl.getVariable("System.TeamFoundationCollectionUri");
+if (orgUrl.indexOf(devAzurePrefix) > -1) {
+    orgName = orgName.substring(orgName.indexOf(devAzurePrefix) + devAzurePrefix.length).trim().replace("/", "");
+}
+
+if (orgUrl.indexOf(vstsPrefix) > -1) {
+    orgName = orgName.split(".")[0].replace("https://", "");
+}
+
 if (isRelease) {
     pipelineAnnotations = [
-        `azure-pipelines/execution=${tl.getVariable("Release.ReleaseId")}`,
+        `azure-pipelines/run=${tl.getVariable("Release.ReleaseId")}`,
         `azure-pipelines/pipeline="${tl.getVariable("Release.DefinitionName")}"`,
         `azure-pipelines/pipelineId="${tl.getVariable("Release.DefinitionId")}"`,
         `azure-pipelines/jobName="${tl.getVariable("Agent.JobName")}"`,
-        `azure-pipelines/executionuri=${tl.getVariable("System.TeamFoundationCollectionUri")}${tl.getVariable("System.TeamProject")}/_releaseProgress?releaseId=${tl.getVariable("Release.ReleaseId")}`,
+        `azure-pipelines/runuri=${orgUrl}${tl.getVariable("System.TeamProject")}/_releaseProgress?releaseId=${tl.getVariable("Release.ReleaseId")}`,
         `azure-pipelines/project=${tl.getVariable("System.TeamProject")}`,
-        `azure-pipelines/org=${tl.getVariable("System.CollectionId")}`
+        `azure-pipelines/org=${orgName}`
     ];
 }
 else {
     pipelineAnnotations = [
-        `azure-pipelines/execution=${tl.getVariable("Build.BuildNumber")}`,
+        `azure-pipelines/run=${tl.getVariable("Build.BuildNumber")}`,
         `azure-pipelines/pipeline="${tl.getVariable("Build.DefinitionName")}"`,
         `azure-pipelines/pipelineId="${tl.getVariable("System.DefinitionId")}"`,
         `azure-pipelines/jobName="${tl.getVariable("Agent.JobName")}"`,
-        `azure-pipelines/executionuri=${tl.getVariable("System.TeamFoundationCollectionUri")}${tl.getVariable("System.TeamProject")}/_build/results?buildId=${tl.getVariable("Build.BuildId")}`,
+        `azure-pipelines/runuri=${orgUrl}${tl.getVariable("System.TeamProject")}/_build/results?buildId=${tl.getVariable("Build.BuildId")}`,
         `azure-pipelines/project=${tl.getVariable("System.TeamProject")}`,
-        `azure-pipelines/org=${tl.getVariable("System.CollectionId")}`
+        `azure-pipelines/org=${orgName}`
     ];
 }
