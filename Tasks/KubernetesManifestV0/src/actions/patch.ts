34eec9f070a5d248a8252977de37bea815f3e93c
Deepak Sattiraju <desattir@microsoft.com>
2019-04-24 13:40:12 +0530

    [KubManifestV0] Ignore SSL errors conditonally (#10183) (#10193)
    * [KubManifestV0] Ignore SSL errors conditonally
    
    * undoing utility-common

--- Tasks/KubernetesManifestV0/src/actions/patch.ts
+++ Tasks/KubernetesManifestV0/src/actions/patch.ts
@@ -5,8 +5,8 @@ import { Kubectl } from "kubernetes-common/kubectl-object-model";
 import * as utils from "../utils/utilities";
 import * as constants from "../models/constants";
 
-export async function patch() {
-    let kubectl = new Kubectl(await utils.getKubectl(), tl.getInput("namespace", false));
+export async function patch(ignoreSslErrors?: boolean) {
+    let kubectl = new Kubectl(await utils.getKubectl(), tl.getInput("namespace", false), ignoreSslErrors);
     let kind = tl.getInput("kind", false).toLowerCase();
     let name = tl.getInput("name", false);
     let filePath = tl.getInput("resourceFileToPatch", false);
