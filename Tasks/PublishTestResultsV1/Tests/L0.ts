e0694e2cf87ea83545100084e0d32e3aea85b0be
Danny McCormick <damccorm@microsoft.com>
2019-04-25 17:34:28 -0400

    Nit const

--- Tasks/PublishTestResultsV1/Tests/L0.ts
+++ Tasks/PublishTestResultsV1/Tests/L0.ts
@@ -1,7 +1,7 @@
 import Q = require('q');
 import assert = require('assert');
 import path = require('path');
-let ff = require(path.join(__dirname, '..', 'find-files-legacy.js'));
+const ff = require(path.join(__dirname, '..', 'find-files-legacy.js'));
 
 describe('PublishTestResultsV1 Find files legacy suite', function () {
     this.timeout(parseInt(process.env.TASK_TEST_TIMEOUT) || 20000);
