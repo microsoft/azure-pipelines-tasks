fe9db022daf68c4bbb3f333fad9118bce31f397d
Chaoyi Yuan <blackchoey@users.noreply.github.com>
2019-05-10 09:34:49 +0800

    generate temp env list for iotedgedev so it can get the value of secret

--- Tasks/AzureIoTEdgeV2/genconfig.ts
+++ Tasks/AzureIoTEdgeV2/genconfig.ts
@@ -3,6 +3,7 @@ import * as fs from "fs";
 import * as path from "path";
 import util from "./util";
 import Constants from "./constant";
+import { IExecOptions } from 'azure-pipelines-task-lib/toolrunner';
 
 export async function run() {
     let templateFilePath: string = tl.getPathInput("templateFilePath", true);
@@ -14,11 +15,27 @@ export async function run() {
     
       util.setupIotedgedev();
     
-      tl.setVariable(Constants.iotedgedevEnv.deploymentFileOutputFolder, tl.getVariable(Constants.outputFileFolder));
+      let envList = {
+        [Constants.iotedgedevEnv.deploymentFileOutputFolder]: tl.getVariable(Constants.outputFileFolder),
+      };
+    
+      // Pass task variable to sub process
+      let tlVariables = tl.getVariables();
+      for (let v of tlVariables) {
+        // The variables in VSTS build contains dot, need to convert to underscore.
+        let name = v.name.replace('.', '_').toUpperCase();
+        if (!envList[name]) {
+          envList[name] = v.value;
+        }
+      }
 
+      let execOptions: IExecOptions = {
+        cwd: tl.cwd(),
+        env: envList
+      } as IExecOptions;
       let defaultPlatform = tl.getInput('defaultPlatform', true);
       let command: string = `genconfig`;
       command += ` --file "${templateFilePath}"`;
       command += ` --platform "${defaultPlatform}"`;
-      await tl.exec(`${Constants.iotedgedev}`, command);
+      await tl.exec(`${Constants.iotedgedev}`, command, execOptions);
 }
