c70f41f0038a7c45665ec9492eebd1375e3db81b
Sven Pfleiderer <svpfleid@microsoft.com>
2019-05-30 11:24:43 -0700

    Fix tests

--- Tasks/AppCenterDistributeV1/Tests/L0SymMultipleDSYMs_single.ts
+++ Tasks/AppCenterDistributeV1/Tests/L0SymMultipleDSYMs_single.ts
@@ -3,6 +3,8 @@ import ma = require('vsts-task-lib/mock-answer');
 import tmrm = require('vsts-task-lib/mock-run');
 import path = require('path');
 import fs = require('fs');
+import azureBlobUploadHelper = require('../azure-blob-upload-helper');
+
 var Readable = require('stream').Readable
 var Writable = require('stream').Writable
 var Stats = require('fs').Stats
@@ -75,7 +77,7 @@ nock('https://example.test')
     })
     .reply(201, {
         symbol_upload_id: 100,
-        upload_url: 'https://example.upload.test/symbol_upload',
+        upload_url: 'https://example.upload.test/bogus-container/bogus-blob',
         expiration_date: 1234567
     });
 
@@ -193,6 +195,12 @@ fs.statSync = (s: string) => {
 
     return stat;
 }
+
+azureBlobUploadHelper.AzureBlobUploadHelper.prototype.upload = async () => {
+    return Promise.resolve();
+}
+
+tmr.registerMock('azure-blob-upload-helper', azureBlobUploadHelper);
 tmr.registerMock('fs', fs);
 
 tmr.run();
