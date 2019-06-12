7b7115026719ef3c739d123f36eafb948470d1b0
Diederik Jan Lemkes <dj.lemkes@gmail.com>
2019-06-12 23:10:29 +0200

    Only substitute manifest image when there's a full match
    Previously, the line `image: nginx-init` would already match
    an 'nginx:42' container in the task definition resulting in
    `image: nginx:42`. This leads to unexpected images being deployed.

--- Tasks/KubernetesManifestV0/Tests/L0.ts
+++ Tasks/KubernetesManifestV0/Tests/L0.ts
@@ -1,8 +1,11 @@
+import * as fs from 'fs';
 import * as path from 'path';
 import * as assert from 'assert';
 import * as ttm from 'azure-pipelines-task-lib/mock-test';
 import * as tl from 'azure-pipelines-task-lib';
 import * as shared from './TestShared';
+import * as utils from '../src/utils/utilities';
+import * as yaml from 'js-yaml';
 
 describe('Kubernetes Manifests Suite', function () {
     this.timeout(30000);
@@ -23,6 +26,7 @@ describe('Kubernetes Manifests Suite', function () {
         delete process.env[shared.TestEnvVars.isBaselineDeploymentPresent];
         delete process.env[shared.TestEnvVars.arguments];
         delete process.env[shared.TestEnvVars.namespace];
+        delete process.env[shared.TestEnvVars.containers];
         delete process.env.RemoveNamespaceFromEndpoint;
     });
 
@@ -50,6 +54,7 @@ describe('Kubernetes Manifests Suite', function () {
         process.env[shared.TestEnvVars.isStableDeploymentPresent] = 'true';
         process.env[shared.TestEnvVars.isCanaryDeploymentPresent] = 'false';
         process.env[shared.TestEnvVars.isBaselineDeploymentPresent] = 'false';
+        process.env[shared.TestEnvVars.containers] = "nginx:1.2"
         tr.run();
         assert(tr.succeeded, 'task should have succeeded');
         assert(tr.stderr.indexOf('"nginx-deployment-canary" not found') != -1, 'Canary deployment is not present');
@@ -224,6 +229,27 @@ describe('Kubernetes Manifests Suite', function () {
         process.env[shared.TestEnvVars.patch] = 'somePatch';
         tr.run();
         assert(tr.succeeded, 'task should have succeeded');
+        done();
+    });
+
+    it('Run should successfully add container image tags', (done: MochaDone) => {
+        const testFile = path.join(__dirname, './manifests/', 'deployment-image-substitution.yaml');
+        const deploymentFile = fs.readFileSync(testFile).toString();
+
+        const bigNameEditFirst = utils.substituteImageNameInSpecFile(deploymentFile, 'nginx-init', 'nginx-init:42.1');
+        const smallNameEditSecond = utils.substituteImageNameInSpecFile(bigNameEditFirst, 'nginx', 'nginx:42');
+        const smallSecondYaml = yaml.load(smallNameEditSecond);
+
+        assert(smallSecondYaml.spec.template.spec.containers[0].image === 'nginx:42', 'nginx image not tagged correctly');
+        assert(smallSecondYaml.spec.template.spec.initContainers[0].image === 'nginx-init:42.1', 'nginx-init image not tagged correctly');
+
+        const smallNameEditFirst = utils.substituteImageNameInSpecFile(deploymentFile, 'nginx', 'nginx:42');
+        const bigNameEditSecond = utils.substituteImageNameInSpecFile(smallNameEditFirst, 'nginx-init', 'nginx-init:42.1');
+        const bigSecondYaml = yaml.load(bigNameEditSecond);
+
+        assert(bigSecondYaml.spec.template.spec.containers[0].image === 'nginx:42', 'nginx image not tagged correctly');
+        assert(bigSecondYaml.spec.template.spec.initContainers[0].image === 'nginx-init:42.1', 'nginx-init image not tagged correctly');
+
         done();
     });
 });
--- Tasks/KubernetesManifestV0/src/actions/createSecret.ts
+++ Tasks/KubernetesManifestV0/src/actions/createSecret.ts
@@ -3,13 +3,14 @@
 import { Kubectl } from 'kubernetes-common-v2/kubectl-object-model';
 import * as utils from '../utils/utilities';
 import * as TaskInputParameters from '../models/TaskInputParameters';
+import {StringComparer, isEqual} from '../utils/StringComparison';
 import AuthenticationToken from 'docker-common-v2/registryauthenticationprovider/registryauthenticationtoken';
 import { getDockerRegistryEndpointAuthenticationToken } from 'docker-common-v2/registryauthenticationprovider/registryauthenticationtoken';
 
 export async function createSecret(ignoreSslErrors?: boolean) {
     const kubectl = new Kubectl(await utils.getKubectl(), TaskInputParameters.namespace, ignoreSslErrors);
     let result;
-    if (utils.isEqual(TaskInputParameters.secretType, 'dockerRegistry', utils.StringComparer.OrdinalIgnoreCase)) {
+    if (isEqual(TaskInputParameters.secretType, 'dockerRegistry', StringComparer.OrdinalIgnoreCase)) {
         const authProvider: AuthenticationToken = getDockerRegistryEndpointAuthenticationToken(TaskInputParameters.dockerRegistryEndpoint);
         result = kubectl.createDockerSecret(TaskInputParameters.secretName.trim(),
             authProvider.getLoginServerUrl(),
