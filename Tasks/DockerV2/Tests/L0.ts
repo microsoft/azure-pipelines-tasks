1ab4aa8aff3b00f28f3f2dfae235cf58a519044c
Rajendra Kolli <rakol@microsoft.com>
2019-06-20 18:23:56 +0530

    Bumping up required task versions
    Signed-off-by: Rajendra Kolli <rakol@microsoft.com>

--- Tasks/DockerV2/Tests/L0.ts
+++ Tasks/DockerV2/Tests/L0.ts
@@ -27,6 +27,7 @@ describe("DockerV2 Suite", function () {
         delete process.env[shared.TestEnvVars.buildContext];
         delete process.env[shared.TestEnvVars.tags];
         delete process.env[shared.TestEnvVars.arguments];
+        delete process.env[shared.TestEnvVars.addPipelineData];
     });
     
     after(function () {
@@ -551,18 +552,20 @@ describe("DockerV2 Suite", function () {
         done();
     });
 
-    it("getDefaultLabels returns selected labels when addPipelineData is false", (done: MochaDone) => {
-        console.log("TestCaseName: getDefaultLabels returns selected labels when addPipelineData is false");
-        console.log("\n");
+    it("Runs successfully for docker build selected labels when addPipelineData is false", (done: MochaDone) => {
+        let tp = path.join(__dirname, 'TestSetup.js');
+        process.env[shared.TestEnvVars.containerRegistry] = "dockerhubendpoint";
+        process.env[shared.TestEnvVars.repository] = "testuser/testrepo";
+        process.env[shared.TestEnvVars.command] = shared.CommandTypes.build;
+        process.env[shared.TestEnvVars.addPipelineData] = "false";
+        let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+        tr.run();
 
-        setEnvironmentVariables();
-        process.env['SYSTEM_HOSTTYPE'] = 'build';
-        const labels = pipelineutils.getDefaultLabels(false); // addPipelineData is set to false
-
-        // update the label count in assert when newer labels are added
-        assert.equal(labels.length, 2, "Only selected labels are returned when addPipelineData is false");
-
-        delete process.env['SYSTEM_HOSTTYPE'];
+        assert(tr.invokedToolCount == 1, 'should have invoked tool one time. actual: ' + tr.invokedToolCount);
+        assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
+        //assert(tr.succeeded, 'task should have succeeded');
+        assert(tr.stdout.indexOf(`[command]docker build -f ${shared.formatPath("a/w/Dockerfile")} ${shared.DockerCommandArgs.BuildLabelsWithAddPipelineFalse} -t testuser/testrepo:11 ${shared.formatPath("a/w")}`) != -1, "docker build should run with expected arguments");
+        console.log(tr.stderr);
         done();
     });
 
