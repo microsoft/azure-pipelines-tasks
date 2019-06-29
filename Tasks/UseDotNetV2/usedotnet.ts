9b49ef1dbc250e63115bfe5514522cc53ef1c796
Paul Jeschke <Paul-Jeschke@outlook.com>
2019-06-29 18:51:19 +0200

    revert TaskLib and ToolLib to tl / tol

--- Tasks/UseDotNetV2/usedotnet.ts
+++ Tasks/UseDotNetV2/usedotnet.ts
@@ -1,7 +1,7 @@
 "use strict";
 import * as path from 'path';
 
-import * as taskLib from 'azure-pipelines-task-lib/task';
+import * as tl from 'azure-pipelines-task-lib/task';
 import { DotNetCoreVersionFetcher } from "./versionfetcher";
 import { globalJsonFetcher } from "./globaljsonfetcher";
 import { VersionInstaller } from "./versioninstaller";
@@ -10,17 +10,17 @@ import { VersionInfo, VersionParts } from "./models"
 import { NuGetInstaller } from "./nugetinstaller";
 
 async function run() {
-    let useGlobalJson: boolean = taskLib.getBoolInput('useGlobalJson');
-    let versionSpec = taskLib.getInput('version');
-    const nugetVersion = taskLib.getInput('nugetVersion') || '4.4.1';
+    let useGlobalJson: boolean = tl.getBoolInput('useGlobalJson');
+    let versionSpec = tl.getInput('version');
+    const nugetVersion = tl.getInput('nugetVersion') || '4.4.1';
 
     // Check if we want install dotnet
     if (versionSpec || useGlobalJson) {
-        let packageType = taskLib.getInput('packageType') || "sdk";
-        let installationPath = taskLib.getInput('installationPath');
-        let includePreviewVersions: boolean = taskLib.getBoolInput('includePreviewVersions');
-        let workingDirectory: string | null = taskLib.getPathInput("workingDirectory", false) || null;
-        let performMultiLevelLookup = taskLib.getBoolInput("performMultiLevelLookup", false);
+        let packageType = tl.getInput('packageType') || "sdk";
+        let installationPath = tl.getInput('installationPath');
+        let includePreviewVersions: boolean = tl.getBoolInput('includePreviewVersions');
+        let workingDirectory: string | null = tl.getPathInput("workingDirectory", false) || null;
+        let performMultiLevelLookup = tl.getBoolInput("performMultiLevelLookup", false);
         installDotNet(installationPath, packageType, versionSpec, useGlobalJson, workingDirectory, includePreviewVersions, performMultiLevelLookup);
     }
     // Install NuGet version specified by user or 4.4.1 in case none is specified
@@ -48,7 +48,7 @@ async function installDotNet(
     performMultiLevelLookup: boolean) {
 
     if (!installationPath && installationPath.length == 0) {
-        installationPath = path.join(taskLib.getVariable('Agent.ToolsDirectory'), "dotnet");
+        installationPath = path.join(tl.getVariable('Agent.ToolsDirectory'), "dotnet");
     }
     let versionFetcher = new DotNetCoreVersionFetcher();
     let dotNetCoreInstaller = new VersionInstaller(packageType, installationPath);
@@ -62,24 +62,24 @@ async function installDotNet(
             await dotNetCoreInstaller.downloadAndInstall(version, url);            
         }
     } else if (versionSpec) {
-        console.log(taskLib.loc("ToolToInstall", packageType, versionSpec));
+        console.log(tl.loc("ToolToInstall", packageType, versionSpec));
         let versionSpecParts = new VersionParts(versionSpec);        
         let versionInfo: VersionInfo = await versionFetcher.getVersionInfo(versionSpecParts.versionSpec, packageType, includePreviewVersions);
         if (!versionInfo) {
-            throw taskLib.loc("MatchingVersionNotFound", versionSpecParts.versionSpec);
+            throw tl.loc("MatchingVersionNotFound", versionSpecParts.versionSpec);
         }
         if (!dotNetCoreInstaller.isVersionInstalled(versionInfo.getVersion())) {
             await dotNetCoreInstaller.downloadAndInstall(versionInfo, versionFetcher.getDownloadUrl(versionInfo));
         }
     }else{
-        throw taskLib.loc("installDotNetCalledWithWrongParameters")
+        throw tl.loc("installDotNetCalledWithWrongParameters")
     }
 
-    taskLib.prependPath(installationPath);
+    tl.prependPath(installationPath);
     // Set DOTNET_ROOT for dotnet core Apphost to find runtime since it is installed to a non well-known location.
-    taskLib.setVariable('DOTNET_ROOT', installationPath);
+    tl.setVariable('DOTNET_ROOT', installationPath);
     // By default disable Multi Level Lookup unless user wants it enabled.
-    taskLib.setVariable("DOTNET_MULTILEVEL_LOOKUP", !performMultiLevelLookup ? "0" : "1");
+    tl.setVariable("DOTNET_MULTILEVEL_LOOKUP", !performMultiLevelLookup ? "0" : "1");
     // Add dot net tools path to "PATH" environment variables, so that tools can be used directly.
     addDotNetCoreToolPath();
 }
@@ -87,25 +87,25 @@ async function installDotNet(
 function addDotNetCoreToolPath() {
     try {
         let globalToolPath: string = "";
-        if (taskLib.osType().match(/^Win/)) {
+        if (tl.osType().match(/^Win/)) {
             globalToolPath = path.join(process.env.USERPROFILE, Constants.relativeGlobalToolPath);
         } else {
             globalToolPath = path.join(process.env.HOME, Constants.relativeGlobalToolPath);
         }
 
-        console.log(taskLib.loc("PrependGlobalToolPath"));
-        taskLib.mkdirP(globalToolPath);
-        taskLib.prependPath(globalToolPath);
+        console.log(tl.loc("PrependGlobalToolPath"));
+        tl.mkdirP(globalToolPath);
+        tl.prependPath(globalToolPath);
     } catch (error) {
         //nop
-        console.log(taskLib.loc("ErrorWhileSettingDotNetToolPath", JSON.stringify(error)));
+        console.log(tl.loc("ErrorWhileSettingDotNetToolPath", JSON.stringify(error)));
     }
 }
 
 var taskManifestPath = path.join(__dirname, "task.json");
-taskLib.debug("Setting resource path to " + taskManifestPath);
-taskLib.setResourcePath(taskManifestPath);
+tl.debug("Setting resource path to " + taskManifestPath);
+tl.setResourcePath(taskManifestPath);
 
 run()
-    .then(() => taskLib.setResult(taskLib.TaskResult.Succeeded, ""))
-    .catch((error) => taskLib.setResult(taskLib.TaskResult.Failed, !!error.message ? error.message : error));
+    .then(() => tl.setResult(tl.TaskResult.Succeeded, ""))
+    .catch((error) => tl.setResult(tl.TaskResult.Failed, !!error.message ? error.message : error));
--- Tasks/UseDotNetV2/versioninstaller.ts
+++ Tasks/UseDotNetV2/versioninstaller.ts
@@ -3,8 +3,8 @@ import * as path from 'path';
 import * as fs from "fs";
 import * as url from "url";
 
-import * as taskLib from 'azure-pipelines-task-lib/task';
-import * as toolLib from 'azure-pipelines-tool-lib/tool';
+import * as tl from 'azure-pipelines-task-lib/task';
+import * as tol from 'azure-pipelines-tool-lib/tool';
 
 import * as utils from "./versionutilities";
 import { VersionInfo } from "./models"
@@ -12,10 +12,10 @@ import { VersionInfo } from "./models"
 export class VersionInstaller {
     constructor(packageType: string, installationPath: string) {
         try {
-            taskLib.exist(installationPath) || taskLib.mkdirP(installationPath);
+            tl.exist(installationPath) || tl.mkdirP(installationPath);
         }
         catch (ex) {
-            throw taskLib.loc("UnableToAccessPath", installationPath, JSON.stringify(ex));
+            throw tl.loc("UnableToAccessPath", installationPath, JSON.stringify(ex));
         }
 
         this.packageType = packageType;
@@ -28,56 +28,56 @@ export class VersionInstaller {
      */
     public async downloadAndInstall(versionInfo: VersionInfo, downloadUrl: string): Promise<void> {
         if (!versionInfo || !versionInfo.getVersion() || !downloadUrl || !url.parse(downloadUrl)) {
-            throw taskLib.loc("VersionCanNotBeDownloadedFromUrl", versionInfo, downloadUrl);
+            throw tl.loc("VersionCanNotBeDownloadedFromUrl", versionInfo, downloadUrl);
         }
         let version = versionInfo.getVersion();
 
         try {
             try {
-                var downloadPath = await toolLib.downloadTool(downloadUrl)
+                var downloadPath = await tol.downloadTool(downloadUrl)
             }
             catch (ex) {
-                throw taskLib.loc("CouldNotDownload", downloadUrl, ex);
+                throw tl.loc("CouldNotDownload", downloadUrl, ex);
             }
 
             // Extract
-            console.log(taskLib.loc("ExtractingPackage", downloadPath));
+            console.log(tl.loc("ExtractingPackage", downloadPath));
             try {
-                var extPath = taskLib.osType().match(/^Win/) ? await toolLib.extractZip(downloadPath) : await toolLib.extractTar(downloadPath);
+                var extPath = tl.osType().match(/^Win/) ? await tol.extractZip(downloadPath) : await tol.extractTar(downloadPath);
             }
             catch (ex) {
-                throw taskLib.loc("FailedWhileExtractingPacakge", ex);
+                throw tl.loc("FailedWhileExtractingPacakge", ex);
             }
 
             // Copy folders
-            taskLib.debug(taskLib.loc("CopyingFoldersIntoPath", this.installationPath));
-            var allRootLevelEnteriesInDir: string[] = taskLib.ls("", [extPath]).map(name => path.join(extPath, name));
+            tl.debug(tl.loc("CopyingFoldersIntoPath", this.installationPath));
+            var allRootLevelEnteriesInDir: string[] = tl.ls("", [extPath]).map(name => path.join(extPath, name));
             var directoriesTobeCopied: string[] = allRootLevelEnteriesInDir.filter(path => fs.lstatSync(path).isDirectory());
             directoriesTobeCopied.forEach((directoryPath) => {
-                taskLib.cp(directoryPath, this.installationPath, "-rf", false);
+                tl.cp(directoryPath, this.installationPath, "-rf", false);
             });
 
             // Copy files
             try {
                 if (this.packageType == utils.Constants.sdk && this.isLatestInstalledVersion(version)) {
-                    taskLib.debug(taskLib.loc("CopyingFilesIntoPath", this.installationPath));
+                    tl.debug(tl.loc("CopyingFilesIntoPath", this.installationPath));
                     var filesToBeCopied = allRootLevelEnteriesInDir.filter(path => !fs.lstatSync(path).isDirectory());
                     filesToBeCopied.forEach((filePath) => {
-                        taskLib.cp(filePath, this.installationPath, "-f", false);
+                        tl.cp(filePath, this.installationPath, "-f", false);
                     });
                 }
             }
             catch (ex) {
-                taskLib.warning(taskLib.loc("FailedToCopyTopLevelFiles", this.installationPath, ex));
+                tl.warning(tl.loc("FailedToCopyTopLevelFiles", this.installationPath, ex));
             }
 
             // Cache tool
             this.createInstallationCompleteFile(versionInfo);
 
-            console.log(taskLib.loc("SuccessfullyInstalled", this.packageType, version));
+            console.log(tl.loc("SuccessfullyInstalled", this.packageType, version));
         }
         catch (ex) {
-            throw taskLib.loc("FailedWhileInstallingVersionAtPath", version, this.installationPath, ex);
+            throw tl.loc("FailedWhileInstallingVersionAtPath", version, this.installationPath, ex);
         }
     }
 
@@ -87,54 +87,54 @@ export class VersionInstaller {
      * @param version An explicit version. Like 1.0.1
      */
     public isVersionInstalled(version: string): boolean {
-        if (!toolLib.isExplicitVersion(version)) {
-            throw taskLib.loc("ExplicitVersionRequired", version);
+        if (!tol.isExplicitVersion(version)) {
+            throw tl.loc("ExplicitVersionRequired", version);
         }
 
         var isInstalled: boolean = false;
         if (this.packageType == utils.Constants.sdk) {
-            isInstalled = taskLib.exist(path.join(this.installationPath, utils.Constants.relativeSdkPath, version)) && taskLib.exist(path.join(this.installationPath, utils.Constants.relativeSdkPath, `${version}.complete`));
+            isInstalled = tl.exist(path.join(this.installationPath, utils.Constants.relativeSdkPath, version)) && tl.exist(path.join(this.installationPath, utils.Constants.relativeSdkPath, `${version}.complete`));
         }
         else {
-            isInstalled = taskLib.exist(path.join(this.installationPath, utils.Constants.relativeRuntimePath, version)) && taskLib.exist(path.join(this.installationPath, utils.Constants.relativeRuntimePath, `${version}.complete`));
+            isInstalled = tl.exist(path.join(this.installationPath, utils.Constants.relativeRuntimePath, version)) && tl.exist(path.join(this.installationPath, utils.Constants.relativeRuntimePath, `${version}.complete`));
         }
 
-        isInstalled ? console.log(taskLib.loc("VersionFoundInCache", version)) : console.log(taskLib.loc("VersionNotFoundInCache", version));
+        isInstalled ? console.log(tl.loc("VersionFoundInCache", version)) : console.log(tl.loc("VersionNotFoundInCache", version));
         return isInstalled;
     }
 
     private createInstallationCompleteFile(versionInfo: VersionInfo): void {
-        taskLib.debug(taskLib.loc("CreatingInstallationCompeleteFile", versionInfo.getVersion(), this.packageType));
+        tl.debug(tl.loc("CreatingInstallationCompeleteFile", versionInfo.getVersion(), this.packageType));
         // always add for runtime as it is installed with sdk as well.
         var pathToVersionCompleteFile: string = "";
         if (this.packageType == utils.Constants.sdk) {
             let sdkVersion = versionInfo.getVersion();
             pathToVersionCompleteFile = path.join(this.installationPath, utils.Constants.relativeSdkPath, `${sdkVersion}.complete`);
-            taskLib.writeFile(pathToVersionCompleteFile, `{ "version": "${sdkVersion}" }`);
+            tl.writeFile(pathToVersionCompleteFile, `{ "version": "${sdkVersion}" }`);
         }
 
         let runtimeVersion = versionInfo.getRuntimeVersion();
         if (runtimeVersion) {
             pathToVersionCompleteFile = path.join(this.installationPath, utils.Constants.relativeRuntimePath, `${runtimeVersion}.complete`);
-            taskLib.writeFile(pathToVersionCompleteFile, `{ "version": "${runtimeVersion}" }`);
+            tl.writeFile(pathToVersionCompleteFile, `{ "version": "${runtimeVersion}" }`);
         }
         else if (this.packageType == utils.Constants.runtime) {
-            throw taskLib.loc("CannotFindRuntimeVersionForCompletingInstallation", this.packageType, versionInfo.getVersion());
+            throw tl.loc("CannotFindRuntimeVersionForCompletingInstallation", this.packageType, versionInfo.getVersion());
         }
     }
 
     private isLatestInstalledVersion(version: string): boolean {
         var pathTobeChecked = this.packageType == utils.Constants.sdk ? path.join(this.installationPath, utils.Constants.relativeSdkPath) : path.join(this.installationPath, utils.Constants.relativeRuntimePath);
-        if (!taskLib.exist(pathTobeChecked)) {
-            throw taskLib.loc("PathNotFoundException", pathTobeChecked);
+        if (!tl.exist(pathTobeChecked)) {
+            throw tl.loc("PathNotFoundException", pathTobeChecked);
         }
 
-        var allEnteries: string[] = taskLib.ls("", [pathTobeChecked]).map(name => path.join(pathTobeChecked, name));
+        var allEnteries: string[] = tl.ls("", [pathTobeChecked]).map(name => path.join(pathTobeChecked, name));
         var folderPaths: string[] = allEnteries.filter(element => fs.lstatSync(element).isDirectory());
         var isLatest: boolean = folderPaths.findIndex(folderPath => {
             try {
                 let versionFolderName = path.basename(folderPath);
-                taskLib.debug(taskLib.loc("ComparingInstalledFolderVersions", version, versionFolderName));
+                tl.debug(tl.loc("ComparingInstalledFolderVersions", version, versionFolderName));
                 return utils.versionCompareFunction(versionFolderName, version) > 0;
             }
             catch (ex) {
@@ -146,7 +146,7 @@ export class VersionInstaller {
         isLatest = isLatest && filePaths.findIndex(filePath => {
             try {
                 var versionCompleteFileName = this.getVersionCompleteFileName(path.basename(filePath));
-                taskLib.debug(taskLib.loc("ComparingInstalledFileVersions", version, versionCompleteFileName));
+                tl.debug(tl.loc("ComparingInstalledFileVersions", version, versionCompleteFileName));
                 return utils.versionCompareFunction(versionCompleteFileName, version) > 0
             }
             catch (ex) {
@@ -154,7 +154,7 @@ export class VersionInstaller {
             }
         }) < 0;
 
-        isLatest ? taskLib.debug(taskLib.loc("VersionIsLocalLatest", version, this.installationPath)) : taskLib.debug(taskLib.loc("VersionIsNotLocalLatest", version, this.installationPath));
+        isLatest ? tl.debug(tl.loc("VersionIsLocalLatest", version, this.installationPath)) : tl.debug(tl.loc("VersionIsNotLocalLatest", version, this.installationPath));
         return isLatest;
     }
 
@@ -167,7 +167,7 @@ export class VersionInstaller {
             }
         }
 
-        throw taskLib.loc("FileNameNotCorrectCompleteFileName", name);
+        throw tl.loc("FileNameNotCorrectCompleteFileName", name);
     }
 
     private packageType: string;
