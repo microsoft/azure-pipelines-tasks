118b33ff30d7373e48928b58edfa9c71cccafb75
Shivangi <Shivangi.Mittal@microsoft.com>
2019-07-08 14:33:46 +0530

    Resolving ccheck

--- Tasks/AzurePowerShellV4/Tests/DoesNotUnravelOutput.ps1
+++ Tasks/AzurePowerShellV4/Tests/DoesNotUnravelOutput.ps1
@@ -14,6 +14,7 @@ Register-Mock Initialize-AzModule
 Register-Mock Remove-EndpointSecrets
 Register-Mock Disconnect-AzureAndClearContext
 Register-Mock Get-VstsEndpoint
+Register-Mock Get-VstsTaskVariable
 
 # Act.
 $actual = @( & $PSScriptRoot\..\AzurePowerShell.ps1 )
