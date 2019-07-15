8467ca2ff806d87d767f2ece6269be8f6af0fd1b
George <georgeveneeldogga@gmail.com>
2019-07-15 12:05:25 +0530

    GitHubReleaseTask: updates L0 Tests (#10849)
    * updated L0s for GitHubReleaseTask
    *Added tests for tagPattern validation and ChangeLogGeneration changes

--- Tasks/GitHubReleaseV0/Tests/ChangeLogL0Tests.ts
+++ Tasks/GitHubReleaseV0/Tests/ChangeLogL0Tests.ts
@@ -4,17 +4,28 @@ import { ChangeLogStartCommit } from "../operations/Utility";
 
 export class ChangeLogL0Tests {
     public static async startTests() {
-        await this.validateGetChangeLog();
+        await this.validateGetChangeLog1();
+        await this.validateGetChangeLog2();
+        await this.validateGetChangeLog3();
     }
 
-    public static async validateGetChangeLog() {
+    public static async validateGetChangeLog1() {
         let changes = await new ChangeLog().getChangeLog("endpoint", "owner/repo", "target", 250, ChangeLogStartCommit.lastFullRelease);
-        let expectedChanges = "\n\n## Changes:\n\n* xyz Fixing issue #56. [ #9 ]\n* abc Fixing issue #2 #3. [ #4, #5 ]\n\nThis list of changes was [auto generated](MOCK_RELEASE_URL).";
-
-        if (changes === expectedChanges) {
+    
+        if (changes === this.expectedChanges) {
             console.log(TestString.getChangeLogKeyword);
         }
     }
+
+    public static async validateGetChangeLog2() {
+        await new ChangeLog().getChangeLog("endpoint", "owner/repo", "target", 250, ChangeLogStartCommit.lastRelease);
+    }
+
+    public static async validateGetChangeLog3() {
+        await new ChangeLog().getChangeLog("endpoint", "owner/repo", "target", 250, ChangeLogStartCommit.lastReleaseByTag, "v1.*");
+    }
+
+    public static readonly expectedChanges = "\n\n## Changes:\n\n* xyz Fixing issue #56. [ #9 ]\n* abc Fixing issue #2 #3. [ #4, #5 ]\n\nThis list of changes was [auto generated](MOCK_RELEASE_URL).";
 
 }
 
