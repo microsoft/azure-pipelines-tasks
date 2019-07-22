3893df7a3e7218f006b33eaa9ca8c0568fa5c101
George <georgeveneeldogga@gmail.com>
2019-07-22 14:30:38 +0530

    Revert changes to GitHubReleaseTask in 155 (#10962)
    * Revert "GitHubReleaseTask - Initial set of changes - ChangeLog Enhancements (#10770)"
    
    This reverts commit 94206c8e95afe6a50ca671869157d0c19c144cd4.
    
    * revert tagfilters in m155

--- Tasks/GitHubReleaseV0/operations/Constants.ts
+++ Tasks/GitHubReleaseV0/operations/Constants.ts
@@ -16,7 +16,4 @@ export class Inputs {
     public static readonly releaseNotes = "releaseNotes";
     public static readonly addChangeLog = "addChangeLog";
     public static readonly deleteExistingAssets = "deleteExistingAssets";
-    public static readonly tagPattern = "tagPattern";
-    public static readonly compareWith = "compareWith";
-    public static readonly releaseTag = "releaseTag";
 }
