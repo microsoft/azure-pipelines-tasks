b6a95fb74f23c96fccff6d81c991a632507dcbce
Shivangi <Shivangi.Mittal@microsoft.com>
2019-07-22 12:00:13 +0530

    checking

--- Tasks/AzurePowerShellV4/Tests/PreJobExecutionAzurePowerShell.Get-LatestModule.ps1
+++ Tasks/AzurePowerShellV4/Tests/PreJobExecutionAzurePowerShell.Get-LatestModule.ps1
@@ -21,6 +21,12 @@ $mockDirectoryStructure = @(
     }
 )
 
+Register-Mock Get-VstsInput { "FilePath" } -- -Name ScriptType -Require
+Register-Mock Get-VstsInput { "$PSScriptRoot/PerformsBasicFlow_TargetScript.ps1" } -- -Name ScriptPath
+Register-Mock Get-VstsInput { $targetAzurePs } -- -Name TargetAzurePs
+Register-Mock Get-VstsInput { 'arg1 arg2' } -- -Name ScriptArguments
+Register-Mock Get-VstsInput { "continue" } -- -Name errorActionPreference
+Register-Mock Get-VstsInput { $true } -- -Name FailOnStandardError
 Register-Mock Get-ChildItem { $mockDirectoryStructure } -- -Directory -Path "C:\Modules"
 Register-Mock Test-Path { $true }
 
--- Tasks/AzurePowerShellV4/Tests/PreJobExecutionAzurePowerShell.UpdatePSModulePathForHostedAgentWorksCorrectly.ps1
+++ Tasks/AzurePowerShellV4/Tests/PreJobExecutionAzurePowerShell.UpdatePSModulePathForHostedAgentWorksCorrectly.ps1
@@ -5,6 +5,13 @@ param()
 . $PSScriptRoot\..\..\..\Tests\lib\Initialize-Test.ps1
 . $PSScriptRoot\..\PreJobExecutionAzurePowerShell.ps1
 
+Register-Mock Get-VstsInput { "FilePath" } -- -Name ScriptType -Require
+Register-Mock Get-VstsInput { "$PSScriptRoot/PerformsBasicFlow_TargetScript.ps1" } -- -Name ScriptPath
+Register-Mock Get-VstsInput { $targetAzurePs } -- -Name TargetAzurePs
+Register-Mock Get-VstsInput { 'arg1 arg2' } -- -Name ScriptArguments
+Register-Mock Get-VstsInput { "continue" } -- -Name errorActionPreference
+Register-Mock Get-VstsInput { $true } -- -Name FailOnStandardError
+
 $azModulePath = "c:\modules\az_3.6.0"
 
 $azModulePattern = "^az_[0-9]+\.[0-9]+\.[0-9]+$"
