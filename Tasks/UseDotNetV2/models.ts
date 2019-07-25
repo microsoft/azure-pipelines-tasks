07123113ab77d51ac35a5f1214576acc818e9f0a
himanshu yadav <hiyada@microsoft.com>
2019-07-25 19:25:46 +0530

    Users/hiyada/global json fixes (#10991)
    * changed error message for invalid version in global.json
    
    * Added localization messages and formated code.
    
    * bumped up task version and changed instanceNameFormat
    
    * removed html tags and markdown from error message
    
    * resolved comments
    
    * adding resource localization change
    
    * made required=false for version input

--- Tasks/UseDotNetV2/models.ts
+++ Tasks/UseDotNetV2/models.ts
@@ -87,8 +87,13 @@ export class Channel {
 }
 
 export class VersionParts {
-    constructor(version: string) {
-        VersionParts.ValidateVersionSpec(version);
+    constructor(version: string, explicitVersion: boolean = false) {
+        if(explicitVersion){
+            VersionParts.ValidateExplicitVersionNumber(version);
+        }else{
+            VersionParts.ValidateVersionSpec(version);
+        }
+        this.versionSpec = version;
         let parts: string[] = version.split(".");
 
         this.versionSpec = version;
@@ -104,15 +109,52 @@ export class VersionParts {
         try {
             let parts = version.split('.');
             // validate version
-            if (parts.length < 2 || parts.length > 3 || (parts[1] == "x" && parts.length > 2) || (parts[1] != "x" && parts.length <= 2) || !parts[0] || !parts[1] || (parts.length == 3 && !parts[2]) || Number.isNaN(Number.parseInt(parts[0])) || (Number.isNaN(Number.parseInt(parts[1])) && parts[1] != "x")) {
-                throw "";
+            if ((parts.length != 3) || // check if the version has 3 parts
+                !parts[0] || // The major version must always be set
+                !parts[1] || // The minor version must always be set
+                !parts[2] || // The patch version must always be set
+                Number.isNaN(Number.parseInt(parts[0])) || // the major version number must be a number
+                Number.isNaN(Number.parseInt(parts[1])) || // the minor version number must be a number
+                Number.isNaN(Number.parseInt(parts[2].split('-')[0])) // the patch version number must be a number. (the patch version can have a '-' because of version numbers like: 1.0.0-beta-50)
+            ) {
+                throw tl.loc("OnlyExplicitVersionAllowed", version);
+            }
+
+            new semver.Range(version);
+        }
+        catch (ex) {
+            throw tl.loc("VersionNotAllowed", version, ex);
+        }
+    }
+
+    /**
+     * Validate the version. Returns an exception if the version number is wrong.
+     * @param version the input version number as string
+     */
+    private static ValidateVersionSpec(version: string): void {
+        try {
+            let parts = version.split('.');
+            // validate version
+            if ((parts.length < 2 || parts.length > 3) || // check if the version has 2 or 3 parts
+                (parts[1] == "x" && parts.length > 2) ||  // a version number like `1.x` must have only major and minor version
+                (parts[1] != "x" && parts.length <= 2) ||  // a version number like `1.1` must have a patch version
+                !parts[0] || // The major version must always be set
+                !parts[1] || // The minor version must always be set
+                (parts.length == 3 && !parts[2]) || // a version number like `1.1.` is invalid because the patch version is missing
+                Number.isNaN(Number.parseInt(parts[0])) || // the major version number must be a number
+                ( // The next lines check the minor version
+                    Number.isNaN(Number.parseInt(parts[1])) &&  // the minor version number must be a number
+                    parts[1] != "x" // the minor version can't be `x`
+                )
+            ) {
+                throw tl.loc("VersionNumberHasTheWrongFormat", version);
             }
 
             new semver.Range(version);
             return true;
         }
         catch (ex) {
-            throw tl.loc("VersionNotAllowed", version)
+            throw tl.loc("VersionNotAllowed", version, ex);
         }
     }
 
