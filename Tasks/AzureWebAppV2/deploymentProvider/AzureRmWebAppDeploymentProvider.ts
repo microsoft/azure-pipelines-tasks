65dd1490017d6a159a491353806f606b4c754bcc
Eavanshi Arora <eaarora@microsoft.com>
2019-07-25 15:15:20 +0530

    Existing App Settings parsing updated

--- Tasks/AzureWebAppV2/deploymentProvider/AzureRmWebAppDeploymentProvider.ts
+++ Tasks/AzureWebAppV2/deploymentProvider/AzureRmWebAppDeploymentProvider.ts
@@ -50,12 +50,12 @@ export class AzureRmWebAppDeploymentProvider implements IWebAppDeploymentProvide
 
     protected async PostDeploymentStep() {
         if(this.taskParams.AppSettings) {
-            var customApplicationSettings = ParameterParser.parse(this.taskParams.AppSettings);
+            var customApplicationSettings = JSON.parse(this.taskParams.AppSettings);
             await this.appServiceUtility.updateAndMonitorAppSettings(customApplicationSettings);
         }
 
         if(this.taskParams.ConfigurationSettings) {
-            var customApplicationSettings = ParameterParser.parse(this.taskParams.ConfigurationSettings);
+            var customApplicationSettings = JSON.parse(this.taskParams.ConfigurationSettings);
             await this.appServiceUtility.updateConfigurationSettings(customApplicationSettings);
         }
 
