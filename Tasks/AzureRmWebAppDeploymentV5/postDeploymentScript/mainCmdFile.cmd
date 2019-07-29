b2929a1d7b83d0eb83ac4b8827f3f4d6c0289119
Eavanshi Arora <eaarora@microsoft.com>
2019-07-29 13:59:52 +0530

    No version update

--- Tasks/AzureRmWebAppDeploymentV5/postDeploymentScript/mainCmdFile.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-:: Send stdout and stderr to seperate file
-call cmd /c "%Home%\\site\\VSTS_PostDeployment_%1\\kuduPostDeploymentScript.cmd" > "%Home%\\site\\VSTS_PostDeployment_%1\\stdout.txt" 2> "%Home%\\site\\VSTS_PostDeployment_%1\\stderr.txt"
-
-:: write return code of previous command to script_result file
-:: only return code (Numeric code) should present inside script_result
-
-echo %errorlevel% > "%Home%\\site\\VSTS_PostDeployment_%1\\script_result.txt"
--- Tasks/AzureRmWebAppDeploymentV5/postDeploymentScript/mainCmdFile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Send stdout and stderr to seperate file
-sh "$HOME/site/VSTS_PostDeployment_$1/kuduPostDeploymentScript.sh" 2> "$HOME/site/VSTS_PostDeployment_$1/stderr.txt" 1> "$HOME/site/VSTS_PostDeployment_$1/stdout.txt" 
-echo "$?" > "$HOME/site/VSTS_PostDeployment_$1/script_result.txt"
