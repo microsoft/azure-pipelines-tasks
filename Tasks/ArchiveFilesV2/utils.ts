0687fa6550175533be98e884e374fcc6b79e8de0
Tommy Petty <jopetty@microsoft.com>
2019-08-21 15:04:35 -0400

    Updated the "more" message to contain number of reamining files

--- Tasks/ArchiveFilesV2/utils.ts
+++ Tasks/ArchiveFilesV2/utils.ts
@@ -6,8 +6,9 @@ export function reportArchivePlan(files: string[], max: number=10) {
     if (files.length > 0) {
         for (var i = 0; i < files.length; i++) {
             plan.push(tl.loc('ArchivingFile',files[i]));
-            if (i >= max-1 && i < files.length-1) {
-                plan.push('...');
+            let remaining = files.length - (i+1);
+            if (i >= max-1 && remaining > 0) {
+                plan.push(tl.loc('MoreFiles', remaining));
                 break;
             }
         }
