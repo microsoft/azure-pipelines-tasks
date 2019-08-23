0cdd44138ff7773f1f624366b2a6cc33decf8036
Tanu Shree <tashree@microsoft.com>
2019-08-23 10:26:36 +0530

    adding dotnet version in telemetry

--- Tasks/PublishTestResultsV2/publishtestresults.ts
+++ Tasks/PublishTestResultsV2/publishtestresults.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as publishTestResultsTool from './publishtestresultstool';
 import * as tl from 'azure-pipelines-task-lib/task';
+import * as tr from 'azure-pipelines-task-lib/toolrunner';
 import * as ci from './cieventlogger';
 
 const MERGE_THRESHOLD = 100;
@@ -41,6 +42,20 @@ function publish(testRunner, resultFiles, mergeResults, failTaskOnFailedTests, p
     properties['testRunSystem'] = testRunSystem;
 
     tl.command('results.publish', properties, '');
+}
+
+function getDotNetVersion() {
+    let dotnet: tr.ToolRunner;
+    const dotnetPath = tl.which('dotnet', false);
+    
+    if (dotnetPath){
+        try {
+            dotnet = tl.tool(dotnetPath);
+            dotnet.arg('--version');
+            return dotnet.execSync().stdout.trim();
+        } catch (err) {}
+    }
+    return '';
 }
 
 async function run() {
@@ -90,6 +105,9 @@ async function run() {
         ci.addToConsolidatedCi('config', config);
         ci.addToConsolidatedCi('platform', platform);
         ci.addToConsolidatedCi('testResultsFilesCount', testResultsFilesCount);
+
+        const dotnetVersion = getDotNetVersion();
+        ci.addToConsolidatedCi('dotnetVersion', dotnetVersion);
 
         const forceMerge = testResultsFilesCount > MERGE_THRESHOLD;
         if (forceMerge) {
