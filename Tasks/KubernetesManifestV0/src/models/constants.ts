5b90186cb617e4fb028b8ff8c27b383d186ad4f7
anuragc617 <44864882+anuragc617@users.noreply.github.com>
2019-09-18 23:31:14 +0530

    added wait for service external Ip assignment (#11325)
    * service wait for ip
    
    * updated task.json
    
    * code refactoring
    
    * addressed review comments
    
    * addressing review comments
    
    * some more review comments
    
    * added test for service IP

--- Tasks/KubernetesManifestV0/src/models/constants.ts
+++ Tasks/KubernetesManifestV0/src/models/constants.ts
@@ -13,6 +13,17 @@ export class KubernetesWorkload {
     public static cronjob: string = 'cronjob';
 }
 
+export class DiscoveryAndLoadBalancerResource {
+    public static service: string = 'service';
+    public static ingress: string = 'ingress';
+}
+
+export class ServiceTypes {
+    public static loadBalancer: string = 'LoadBalancer';
+    public static nodePort: string = 'NodePort';
+    public static clusterIP: string = 'ClusterIP'
+}
+
 export const deploymentTypes: string[] = ['deployment', 'replicaset', 'daemonset', 'pod', 'statefulset'];
 export const workloadTypes: string[] = ['deployment', 'replicaset', 'daemonset', 'pod', 'statefulset', 'job', 'cronjob'];
 export const workloadTypesWithRolloutStatus: string[] = ['deployment', 'daemonset', 'statefulset'];
