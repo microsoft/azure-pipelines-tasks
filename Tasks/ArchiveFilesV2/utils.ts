dc594fe465683181862a939a6163c706996e1304
Tommy Petty <jopetty@microsoft.com>
2019-09-19 09:16:37 -0400

    Updates from review

--- Tasks/ArchiveFilesV2/utils.ts
+++ Tasks/ArchiveFilesV2/utils.ts
@@ -1,14 +1,12 @@
 import tl = require("vsts-task-lib/task");
 
-export function reportArchivePlan(files: string[], max: number=10) {
+export function reportArchivePlan(files: string[], max: number=10) : string[] {
     var plan: string[] = [];
     plan.push(tl.loc('FoundNFiles', files.length));
     if (files.length > 0) {
-        for (var i = 0; i < files.length; i++) {
+        var limit = Math.min(files.length, max);
+        for (var i = 0; i < limit; i++) {
             plan.push(tl.loc('ArchivingFile', files[i]));
-            if (i >= max-1) {
-                break;
-            }
         }
         if (files.length > max) {
             plan.push(tl.loc('MoreFiles', files.length - max));
