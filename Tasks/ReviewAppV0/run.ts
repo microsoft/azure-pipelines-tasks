7c6bd9b19cf2520e9a287972ac2c30558587581c
Deepak Sattiraju <desattir@microsoft.com>
2019-10-14 12:44:03 +0530

    Renaming task to ReviewAppV0

--- Tasks/ReviewAppV0/run.ts
+++ Tasks/ReviewAppV0/run.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 async function run() {
     try {
         tl.setResourcePath(path.join(__dirname, 'task.json'));
-        const source = tl.getInput('source', true);
+        const source = tl.getInput('resourceName', true);
         console.log(tl.loc('CreatedDynamicResource', source));
     } catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed', true);
