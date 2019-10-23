99e057ce4068ba783b3d62f5273c0aa476f6bef1
damccorm <damccorm@microsoft.com>
2019-10-23 15:19:18 -0400

    Casing issue

--- Tasks/DeleteFilesV1/Tests/L0SingleFile.ts
+++ Tasks/DeleteFilesV1/Tests/L0SingleFile.ts
@@ -1,7 +1,7 @@
 import tmrm = require('azure-pipelines-task-lib/mock-run');
 import path = require('path');
 
-let taskPath = path.join(__dirname, '..', 'deleteFiles.js');
+let taskPath = path.join(__dirname, '..', 'deletefiles.js');
 let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
 const testRoot: string = path.join(__dirname, "test_structure", "singleFile");
