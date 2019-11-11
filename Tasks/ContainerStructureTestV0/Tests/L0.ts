b91ae7efad3f48702eec18086529a7b18d9cff51
damccorm <damccorm@microsoft.com>
2019-11-11 16:36:58 -0500

    Add test scaffolding for node tasks

---
+++ Tasks/ContainerStructureTestV0/Tests/L0.ts
@@ -0,0 +1,16 @@
+import fs = require('fs');
+import assert = require('assert');
+import path = require('path');
+
+describe('AntV1 Suite', function () {
+    before(() => {
+    });
+
+    after(() => {
+    });
+
+    it('Does a basic hello world test', (done: MochaDone) => {
+        // TODO - add real tests
+        done();
+    });
+});
