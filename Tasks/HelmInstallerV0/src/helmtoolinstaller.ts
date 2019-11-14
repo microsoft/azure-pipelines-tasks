23216bec89ac422d3a728aac4acb420c3b24f26d
Anumita Shenoy <ansheno@microsoft.com>
2019-11-14 20:13:55 +0530

    Helm v3 fix refactor

--- Tasks/HelmInstallerV0/src/helmtoolinstaller.ts
+++ Tasks/HelmInstallerV0/src/helmtoolinstaller.ts
@@ -10,6 +10,8 @@ import * as helminstaller from "./helminstaller"
 
 tl.setResourcePath(path.join(__dirname, '..', 'task.json'));
 
+var versionToInstall = ""
+
 async function configureKubectl() {
     var version = await kubectlinstaller.getKuberctlVersion();
     var kubectlPath = await kubectlinstaller.downloadKubectl(version);
@@ -22,6 +24,7 @@ async function configureKubectl() {
 
 async function configureHelm() {
     var version = await helminstaller.getHelmVersion();
+    versionToInstall = version
     var helmPath = await helminstaller.downloadHelm(version);
 
     // prepend the tools path. instructs the agent to prepend for future tasks
@@ -32,12 +35,11 @@ async function configureHelm() {
 
 async function verifyHelm() {
     console.log(tl.loc("VerifyHelmInstallation"));
-    var helmVersion = await helminstaller.getHelmVersion();
     var helmToolPath = tl.which("helm", true);
     var helmTool = tl.tool(helmToolPath);
 
     // Check if using Helm 2 or Helm 3
-    if (helmVersion.startsWith("v2")) {
+    if (versionToInstall.startsWith("v2")) {
         helmTool.arg("init");
         helmTool.arg("--client-only");
     } else {
--- Tasks/HelmInstallerV1/src/helmtoolinstaller.ts
+++ Tasks/HelmInstallerV1/src/helmtoolinstaller.ts
@@ -7,9 +7,11 @@ import utils = require("./utils");
 
 tl.setResourcePath(path.join(__dirname, '..', 'task.json'));
 
+var versionToInstall = ""
 
 async function configureHelm() {
     var version = await utils.getHelmVersion();
+    versionToInstall = version;
     var helmPath = await utils.downloadHelm(version);
 
     // prepend the tools path. instructs the agent to prepend for future tasks
@@ -20,12 +22,11 @@ async function configureHelm() {
 
 async function verifyHelm() {
     console.log(tl.loc("VerifyHelmInstallation"));
-    var helmVersion = await utils.getHelmVersion();
     var helmToolPath = tl.which("helm", true);
     var helmTool = tl.tool(helmToolPath);
 
     // Check if using Helm 2 or Helm 3
-    if (helmVersion.startsWith("v2")) {
+    if (versionToInstall.startsWith("v2")) {
         helmTool.arg("init");
         helmTool.arg("--client-only");
     } else {
