980c99c2db6fcb52f17f844dfcbe3a500fd87f58
PREETI BANSAL <prebansa@microsoft.com>
2019-11-14 14:35:28 +0530

    Addressing review comments

--- Tasks/ContainerBuildV0/src/utils.ts
+++ Tasks/ContainerBuildV0/src/utils.ts
@@ -101,36 +101,41 @@ export async function getServiceDetails() {
     var serviceResponse = kubectlTool.execSync(executionOption);
 
     if (serviceResponse && serviceResponse.stderr) {
-        tl.setResult(tl.TaskResult.Failed, serviceResponse.stderr);
+        throw new Error(serviceResponse.stderr);
     }
     else if (serviceResponse && serviceResponse.stdout) {
-        namespace = JSON.parse(serviceResponse.stdout).metadata.namespace ? JSON.parse(serviceResponse.stdout).metadata.namespace : namespace;
-        port = JSON.parse(serviceResponse.stdout).spec.ports[0].port ? JSON.parse(serviceResponse.stdout).spec.ports[0].port : port;
-        clusteruri = JSON.parse(serviceResponse.stdout).status.loadBalancer.ingress[0].ip ? JSON.parse(serviceResponse.stdout).status.loadBalancer.ingress[0].ip : clusteruri;
+        var responseOutput = JSON.parse(serviceResponse.stdout);
+        namespace = responseOutput.metadata.namespace ? responseOutput.metadata.namespace : namespace;
+        port = responseOutput.spec.ports[0].port ? responseOutput.spec.ports[0].port : port;
+        clusteruri = responseOutput.status.loadBalancer.ingress[0].ip ? responseOutput.status.loadBalancer.ingress[0].ip : clusteruri;
     }
 }
 
 export async function getBuildKitPod() {
 
-    await getServiceDetails();
+    try {
+        await getServiceDetails();
 
-    let request = new webclient.WebRequest();
-    let headers = {
-        "key": tl.getVariable('Build.Repository.Name') + tl.getInput("Dockerfile", true)
-    };
-    let webRequestOptions: webclient.WebRequestOptions = { retriableErrorCodes: [], retriableStatusCodes: [], retryCount: 1, retryIntervalInSeconds: 5, retryRequestTimedout: true };
+        let request = new webclient.WebRequest();
+        let headers = {
+            "key": tl.getVariable('Build.Repository.Name') + tl.getInput("Dockerfile", true)
+        };
+        let webRequestOptions: webclient.WebRequestOptions = { retriableErrorCodes: [], retriableStatusCodes: [], retryCount: 1, retryIntervalInSeconds: 5, retryRequestTimedout: true };
 
-    request.uri = `http://${clusteruri}:${port}/buildPod`;
-    request.headers = headers
-    request.method = "GET";
+        request.uri = `http://${clusteruri}:${port}/buildPod`;
+        request.headers = headers
+        request.method = "GET";
 
-    var response = await webclient.sendRequest(request, webRequestOptions);
-    var podname = response.body.Message;
+        var response = await webclient.sendRequest(request, webRequestOptions);
+        var podname = response.body.Message;
 
-    tl.debug("Podname " + podname);
+        tl.debug("Podname " + podname);
 
-    // set the environment variable
-    process.env["BUILDKIT_HOST"] = "kube-pod://" + podname + "?namespace=" + namespace;
+        // set the environment variable
+        process.env["BUILDKIT_HOST"] = "kube-pod://" + podname + "?namespace=" + namespace;
+	} catch (err) {
+        tl.setResult(tl.TaskResult.Failed, `${err}`);
+    }
 }
 
 function findBuildctl(rootFolder: string) {
