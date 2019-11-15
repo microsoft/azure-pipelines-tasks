5c9b5ec8feda285c29e9b9f60ccec5ce3f63e584
William Reznak <44002111+wireznak@users.noreply.github.com>
2019-11-15 14:35:16 -0500

    Adds validation to Platform and Configuration parameters in MSBuild Task (#11782)
    * Adds validation to Platform and Configuration parameters to MSBuild Task to
    match character restrictions on these values by Visual Studio.
    
    * - Bumped version numbers for Tasks affected.
    - Changed error message to provide more information since the input value is
      not printed before the exception occurs.
    
    * - Fix up some whitespace nits.
    - Changed version increase from patch on 160 to minor version increase.
    
    * Forgot to update task.loc.json as well.

--- Tasks/Common/MSBuildHelpers/Tests/L0.ts
+++ Tasks/Common/MSBuildHelpers/Tests/L0.ts
@@ -42,6 +42,9 @@ describe('Common-MSBuildHelpers Suite', function () {
         it('(Format-MSBuildArguments) adds VS version property', (done) => {
             psr.run(path.join(__dirname, 'Format-MSBuildArguments.AddsVSVersionProperty.ps1'), done);
         })
+        it('(Format-MSBuildArguments) rejects invalid characters', (done) => {
+            psr.run(path.join(__dirname, 'Format-MSBuildArguments.RejectsInvalidCharacters.ps1'), done);
+        })
         it('(Get-SolutionFiles) resolves wildcards', (done) => {
             psr.run(path.join(__dirname, 'Get-SolutionFiles.ResolvesWildcards.ps1'), done);
         })
