777976348e92f3a98ff1d8f7c0040a26af204179
Nitin Issac Joy <nijoy@microsoft.com>
2019-12-06 15:14:47 +0530

    Port to M161: AzureResourceGroupDeploymentTask: Improving error log to point to Deployment logs in Ibiza Portal (#11853) (#11904)
    * AzureResourceGroupDeploymentTask: Improving error log to point to Deployment logs in Ibiza Portal (#11853)
    
    * Init
    
    * Refactoring
    
    * Update Tasks/AzureResourceManagerTemplateDeploymentV3/task.json
    
    Co-Authored-By: Bishal Prasad <biprasad@microsoft.com>
    
    * Adding functionality for AzureResourceGroupDeploymentV2
    
    * Updating error message
    
    * Handling deploymentScope as Resource Group
    
    * Handling management group
    
    * Updating message
    
    * Review comments
    
    * Updating task version
    
    * Bug fix
    
    * Print log for subscription as well
    
    * Bug fix
    
    * Adding check for Subscription
    
    * Bug fix
    
    * Bug fix
    
    * 500 errors are shown
    
    * Bug fix
    
    * Bug fix

--- Tasks/AzureResourceGroupDeploymentV2/operations/ResourceGroup.ts
+++ Tasks/AzureResourceGroupDeploymentV2/operations/ResourceGroup.ts
@@ -514,6 +514,7 @@ export class ResourceGroup {
                 }
                 if (result.error) {
                     this.writeDeploymentErrors(result.error);
+                    tl.error(tl.loc("FindMoreDeploymentDetailsAzurePortal", this.getAzurePortalDeploymentURL()));
                     return reject(tl.loc("CreateTemplateDeploymentFailed"));
                 } else {
                     console.log(tl.loc("ValidDeployment"));
@@ -537,6 +538,7 @@ export class ResourceGroup {
                             return this.waitAndPerformAzureDeployment(armClient, deployment, retryCount);
                         }
                         this.writeDeploymentErrors(error);
+                        this.checkAndPrintPortalDeploymentURL(error);
                         return reject(tl.loc("CreateTemplateDeploymentFailed"));
                     }
                     if (result && result["properties"] && result["properties"]["outputs"] && utils.isNonEmpty(this.taskParameters.deploymentOutputs)) {
@@ -548,6 +550,12 @@ export class ResourceGroup {
                     resolve();
                 });
             });
+        }
+    }
+
+    protected checkAndPrintPortalDeploymentURL(error: any) {
+        if(!!error && (error.statusCode < 400 || error.statusCode >= 500)) {
+            tl.error(tl.loc("FindMoreDeploymentDetailsAzurePortal", this.getAzurePortalDeploymentURL()));
         }
     }
 
@@ -567,6 +575,18 @@ export class ResourceGroup {
             throw new Error(tl.loc("InvalidTemplateLocation"));
         }
         await this.performAzureDeployment(armClient, deployment, 3);
+    }
+
+    private getAzurePortalDeploymentURL() {
+        try {
+            let portalUrl = this.taskParameters.endpointPortalUrl ? this.taskParameters.endpointPortalUrl : "https://portal.azure.com";
+            portalUrl += "/#blade/HubsExtension/DeploymentDetailsBlade/overview/id/";
+            let subscriptionSpecificURL = "/subscriptions/" + this.taskParameters.subscriptionId + "/resourceGroups/" + this.taskParameters.resourceGroupName + "/providers/Microsoft.Resources/deployments/" + this.taskParameters.deploymentName;
+            return portalUrl + subscriptionSpecificURL.replace(/\//g, '%2F');
+        } catch (error) {
+            tl.error(error);
+            return error;
+        }
     }
 
     private escapeBlockCharacters(str: string): string {
