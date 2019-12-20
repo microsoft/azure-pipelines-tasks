90b9976cf34ce701155ea420758469ec129df00a
Nitin ISsac Joy <nitin.i.joy@gmail.com>
2019-12-20 10:57:22 +0530

    Revert "AzureResourceGroupDeploymentTask: Improving error log to point to Deployment logs in Ibiza Portal (#11853)"
    This reverts commit 8f02a1ca236450d5c766fbf5754984e8cdf215ef.

--- Tasks/AzureResourceGroupDeploymentV2/operations/ResourceGroup.ts
+++ Tasks/AzureResourceGroupDeploymentV2/operations/ResourceGroup.ts
@@ -514,7 +514,6 @@ export class ResourceGroup {
                 }
                 if (result.error) {
                     this.writeDeploymentErrors(result.error);
-                    tl.error(tl.loc("FindMoreDeploymentDetailsAzurePortal", this.getAzurePortalDeploymentURL()));
                     return reject(tl.loc("CreateTemplateDeploymentFailed"));
                 } else {
                     console.log(tl.loc("ValidDeployment"));
@@ -538,7 +537,6 @@ export class ResourceGroup {
                             return this.waitAndPerformAzureDeployment(armClient, deployment, retryCount);
                         }
                         this.writeDeploymentErrors(error);
-                        this.checkAndPrintPortalDeploymentURL();
                         return reject(tl.loc("CreateTemplateDeploymentFailed"));
                     }
                     if (result && result["properties"] && result["properties"]["outputs"] && utils.isNonEmpty(this.taskParameters.deploymentOutputs)) {
@@ -551,10 +549,6 @@ export class ResourceGroup {
                 });
             });
         }
-    }
-
-    protected checkAndPrintPortalDeploymentURL() {
-        tl.error(tl.loc("FindMoreDeploymentDetailsAzurePortal", this.getAzurePortalDeploymentURL()));
     }
 
     private async waitAndPerformAzureDeployment(armClient: armResource.ResourceManagementClient, deployment: Deployment, retryCount): Promise<void> {
@@ -573,18 +567,6 @@ export class ResourceGroup {
             throw new Error(tl.loc("InvalidTemplateLocation"));
         }
         await this.performAzureDeployment(armClient, deployment, 3);
-    }
-
-    private getAzurePortalDeploymentURL() {
-        try {
-            let portalUrl = this.taskParameters.endpointPortalUrl ? this.taskParameters.endpointPortalUrl : "https://portal.azure.com";
-            portalUrl += "/#blade/HubsExtension/DeploymentDetailsBlade/overview/id/";
-            let subscriptionSpecificURL = "/subscriptions/" + this.taskParameters.subscriptionId + "/resourceGroups/" + this.taskParameters.resourceGroupName + "/providers/Microsoft.Resources/deployments/" + this.taskParameters.deploymentName;
-            return portalUrl + subscriptionSpecificURL.replace(/\//g, '%2F');
-        } catch (error) {
-            tl.error(error);
-            return error;
-        }
     }
 
     private escapeBlockCharacters(str: string): string {
