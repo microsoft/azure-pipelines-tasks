8c4cb269af3d2339b601529cdfdc5fd8b77d6ec0
Roshan Kumar <rok@microsoft.com>
2020-01-03 10:26:26 +0530

    Fixed L0

--- Tasks/AzureResourceGroupDeploymentV2/Tests/L0.ts
+++ Tasks/AzureResourceGroupDeploymentV2/Tests/L0.ts
@@ -15,11 +15,15 @@ describe('Azure Resource Group Deployment', function () {
     });
     after(function () {
     });
-
+    
     process.env['AGENT_HOMEDIRECTORY'] = process.env['AGENT_HOMEDIRECTORY'] || "C:\\temp\\agent\\home";
-	process.env['BUILD_SOURCESDIRECTORY'] = process.env['BUILD_SOURCESDIRECTORY'] || "C:\\temp\\agent\\home\\sources",
-	process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'] = process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'] || "C:\\temp\\agent\\home";
-	process.env["AGENT_TEMPDIRECTORY"] = process.env["AGENT_TEMPDIRECTORY"] || "C:\\temp\\agent\\home\\temp";
+    process.env['BUILD_SOURCESDIRECTORY'] = process.env['BUILD_SOURCESDIRECTORY'] || "C:\\temp\\agent\\home\\sources",
+    process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'] = process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'] || "C:\\temp\\agent\\home";
+    process.env["AGENT_TEMPDIRECTORY"] = process.env["AGENT_TEMPDIRECTORY"] || "C:\\temp\\agent\\home\\temp";
+    
+    beforeEach(() => {
+        process.env["AGENT_TEMPDIRECTORY"] = "temp/agent";
+    });
 
 // uncomment to get test traces
 //	process.env['TASK_TEST_TRACE'] = "1";
