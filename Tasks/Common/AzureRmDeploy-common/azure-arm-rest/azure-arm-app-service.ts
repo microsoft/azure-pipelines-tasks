3de21c79c1b838b61203a7cdb6108bd61b58d39f
Eavanshi Arora <52486630+eaarora-ms@users.noreply.github.com>
2020-01-08 13:47:24 +0530

    API version updated (#11871)
    * API version updated
    
    * mockutils updated
    
    * Task help url updated

--- Tasks/Common/AzureRmDeploy-common/azure-arm-rest/azure-arm-app-service.ts
+++ Tasks/Common/AzureRmDeploy-common/azure-arm-rest/azure-arm-app-service.ts
@@ -312,7 +312,7 @@ export class AzureAppService {
             {
                 '{resourceGroupName}': this._resourceGroup,
                 '{name}': this._name,
-            }, null, '2016-08-01');
+            }, null, '2018-02-01');
             
             var response = await this._client.beginRequest(httpRequest);
             if(response.statusCode != 200) {
@@ -360,7 +360,7 @@ export class AzureAppService {
             {
                 '{resourceGroupName}': this._resourceGroup,
                 '{name}': this._name,
-            }, null, '2016-08-01');
+            }, null, '2018-02-01');
             
             var response = await this._client.beginRequest(httpRequest);
             if(response.statusCode != 200) {
