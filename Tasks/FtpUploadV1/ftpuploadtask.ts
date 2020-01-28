3cadfc71ef2196258440f586659d0cb1421441c5
damccorm <damccorm@microsoft.com>
2020-01-28 16:04:45 -0500

    Update FtpUpload task-lib

--- Tasks/FtpUploadV1/ftpuploadtask.ts
+++ Tasks/FtpUploadV1/ftpuploadtask.ts
@@ -1,4 +1,4 @@
-import tl = require('vsts-task-lib/task');
+import tl = require('azure-pipelines-task-lib/task');
 import url = require('url');
 import Q = require('q');
 import path = require('path');
