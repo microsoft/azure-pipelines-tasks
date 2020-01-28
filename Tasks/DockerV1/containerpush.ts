778ad55aa386b67c1d3e5a891bf9ee9c33119103
Shivam Gupta <shigupt@microsoft.com>
2020-01-28 17:15:07 +0530

    Added try catch block

--- Tasks/DockerV1/containerpush.ts
+++ Tasks/DockerV1/containerpush.ts
@@ -40,9 +40,13 @@ function dockerPush(connection: ContainerConnection, image: string, imageDigestF
 }
 
 export function run(connection: ContainerConnection): any {
-    var imageLsCommand = connection.createCommand();
-    imageLsCommand.arg("images");
-    connection.execCommand(imageLsCommand);
+    try {
+        var imageLsCommand = connection.createCommand();
+        imageLsCommand.arg("images");
+        connection.execCommand(imageLsCommand);
+    } catch (ex) {
+        tl.warning(ex);
+    }
 
     let command = tl.getInput("command", true);    
     var commandArguments = dockerCommandUtils.getCommandArguments(tl.getInput("arguments", false));
