7670cd36a42a7cdc05a09646583c2de0e217c0da
Danny McCormick <damccorm@microsoft.com>
2020-01-29 15:45:53 -0500

    Update decryptFile task-lib (#12253)

--- Tasks/DecryptFileV1/decrypt.ts
+++ Tasks/DecryptFileV1/decrypt.ts
@@ -1,7 +1,7 @@
 import fs = require('fs');
 import path = require('path');
-import tl = require('vsts-task-lib/task');
-import trm = require('vsts-task-lib/toolrunner');
+import tl = require('azure-pipelines-task-lib/task');
+import trm = require('azure-pipelines-task-lib/toolrunner');
 
 async function run() {
     try {
