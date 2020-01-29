565416a3d78f7879f8b178c3f6dbd4d87d73528a
Himanshu Yadav <hiyada@microsoft.com>
2020-01-29 17:11:00 +0530

    deprecating 2.2 removal message in .NET Core CLI task

--- Tasks/DotNetCoreCLIV2/dotnetcore.ts
+++ Tasks/DotNetCoreCLIV2/dotnetcore.ts
@@ -85,6 +85,8 @@ export class dotNetExe {
     private async executeBasicCommand() {
         var dotnetPath = tl.which("dotnet", true);
 
+        tl.loc('DeprecatingDotnet2_2');
+
         this.extractOutputArgument();
 
         // Use empty string when no project file is specified to operate on the current directory
@@ -133,6 +135,7 @@ export class dotNetExe {
 
     private async executeTestCommand(): Promise<void> {
         const dotnetPath = tl.which('dotnet', true);
+        tl.loc('DeprecatingDotnet2_2');
         const enablePublishTestResults: boolean = tl.getBoolInput('publishTestResults', false) || false;
         const resultsDirectory = tl.getVariable('Agent.TempDirectory');
         if (enablePublishTestResults && enablePublishTestResults === true) {
