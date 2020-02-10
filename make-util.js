20f8bd4c9f9e65a4c8ba0e60decc8c69bba00492
damccorm <damccorm@microsoft.com>
2020-02-10 17:55:40 -0500

    Get different part working

--- make-util.js
+++ make-util.js
@@ -1442,7 +1442,7 @@ var createPushCmd = function (taskPublishFolder, fullTaskName, taskVersion) {
     var taskFeedUrl = process.env.AGGREGATE_TASKS_FEED_URL;
     var apiKey = 'Skyrise';
 
-    fs.writeFileSync(taskPushCmdPath, `nuget.exe push ${nupkgName} -source "${taskFeedUrl}" -apikey ${apiKey} -skip-duplicate`);
+    fs.writeFileSync(taskPushCmdPath, `nuget.exe push ${nupkgName} -source "${taskFeedUrl}" -apikey ${apiKey}`);
 }
 
 // Rename task folders that are created from the aggregate. Allows NuGet generation from aggregate using same process as normal.
