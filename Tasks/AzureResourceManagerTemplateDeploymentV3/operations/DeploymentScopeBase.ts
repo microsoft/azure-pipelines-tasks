a950157cc7e0aeadbc197b03ab20fb158346c86e
Tejas Deshpande <tedeshpa@microsoft.com>
2020-02-14 14:48:44 +0530

    correcting implicit set to incremental mode

--- Tasks/AzureResourceManagerTemplateDeploymentV3/operations/DeploymentScopeBase.ts
+++ Tasks/AzureResourceManagerTemplateDeploymentV3/operations/DeploymentScopeBase.ts
@@ -48,6 +48,7 @@ export class DeploymentScopeBase {
         }
 
         if (this.deploymentParameters.properties["mode"] === "Validation") {
+            this.deploymentParameters.properties["mode"] = "Incremental";
             return this.validateDeployment();
         } else {
             try {
@@ -139,7 +140,10 @@ export class DeploymentScopeBase {
     protected validateDeployment(): Promise<void> {
         return new Promise<void>((resolve, reject) => {
             console.log(tl.loc("StartingValidation"));
-            this.deploymentParameters.properties["mode"] = "Incremental";
+            if(!(!!this.deploymentParameters.properties["mode"] && (this.deploymentParameters.properties["mode"] === "Complete" || this.deploymentParameters.properties["mode"] === "Incremental")))
+            {
+                this.deploymentParameters.properties["mode"] = "Incremental";
+            }
             this.taskParameters.deploymentName = this.taskParameters.deploymentName || utils.createDeploymentName(this.taskParameters);
             console.log(tl.loc("LogDeploymentName", this.taskParameters.deploymentName));
             this.armClient.deployments.validate(this.taskParameters.deploymentName, this.deploymentParameters, (error, result, request, response) => {
