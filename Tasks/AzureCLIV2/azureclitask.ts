072f3290b84e83def5d14d6c6e4f81681217363e
Mike Brancato <mbrancato@humana.com>
2020-03-11 06:36:30 -0400

    Support Azure CLI login for AzureAD and Azure DevOps

--- Tasks/AzureCLIV2/azureclitask.ts
+++ Tasks/AzureCLIV2/azureclitask.ts
@@ -105,7 +105,7 @@ export class azureclitask {
             let escapedCliPassword = cliPassword.replace(/"/g, '\\"');
             tl.setSecret(escapedCliPassword.replace(/\\/g, '\"'));
             //login using svn
-            Utility.throwIfError(tl.execSync("az", `login --service-principal -u "${servicePrincipalId}" -p "${escapedCliPassword}" --tenant "${tenantId}"`), tl.loc("LoginFailed"));
+            Utility.throwIfError(tl.execSync("az", `login --service-principal -u "${servicePrincipalId}" -p "${escapedCliPassword}" --tenant "${tenantId}" --allow-no-subscriptions`), tl.loc("LoginFailed"));
         }
         else if(authScheme.toLowerCase() == "managedserviceidentity") {
             //login using msi
