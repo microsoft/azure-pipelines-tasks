0cac09346cc19d7acdba7d33b447391ac721524f
John Erickson <jerick@microsoft.com>
2020-04-07 10:13:16 -0700

    only change default for 16.5 or higher

--- Tasks/NuGetToolInstallerV0/nugettoolinstaller.ts
+++ Tasks/NuGetToolInstallerV0/nugettoolinstaller.ts
@@ -1,4 +1,5 @@
 import * as taskLib from 'azure-pipelines-task-lib/task';
+import * as semver from 'semver';
 import * as path from "path";
 
 import nuGetGetter = require("packaging-common/nuget/NuGetToolGetter");
@@ -7,7 +8,15 @@ async function run() {
     try {
         taskLib.setResourcePath(path.join(__dirname, "task.json"));
 
-        let versionSpec = taskLib.getInput('versionSpec', true);
+        let versionSpec = taskLib.getInput('versionSpec', false);
+        if (!versionSpec) {
+            const msbuildSemVer = await getMSBuildVersion();
+            if (msbuildSemVer && semver.gt(msbuildSemVer, '16.5.0')) {
+                versionSpec = '4.8.2';
+            } else {
+                versionSpec = '4.3.0';
+            }
+        }
         let checkLatest = taskLib.getBoolInput('checkLatest', false);
         await nuGetGetter.getNuGet(versionSpec, checkLatest, true);
     }
