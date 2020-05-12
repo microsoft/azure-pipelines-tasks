c9abb2c6eed8a92f10260bf6f846b5248893c241
Egor Bryzgalov <v-egbryz@microsoft.com>
2020-05-12 14:32:14 +0300

    Fixed code formatting.

--- Tasks/CMakeV1/cmaketask.ts
+++ Tasks/CMakeV1/cmaketask.ts
@@ -3,25 +3,25 @@ import tl = require('azure-pipelines-task-lib/task');
 import trm = require('azure-pipelines-task-lib/toolrunner');
 
 async function run() {
-    try {   
-		tl.setResourcePath(path.join( __dirname, 'task.json'));
+    try {
+        tl.setResourcePath(path.join(__dirname, 'task.json'));
 
-		const cmake: trm.ToolRunner = tl.tool(tl.which('cmake', true));
+        const cmake: trm.ToolRunner = tl.tool(tl.which('cmake', true));
 
-		const cwd: string = tl.getPathInput('cwd', true, false);
-		tl.mkdirP(cwd);
-		tl.cd(cwd);
+        const cwd: string = tl.getPathInput('cwd', true, false);
+        tl.mkdirP(cwd);
+        tl.cd(cwd);
 
         cmake.line(tl.getInput('cmakeArgs', false));
-        
+
         const options: trm.IExecOptions = <trm.IExecOptions>{
             shell: true
         };
 
-		const code: number = await cmake.exec(options);
+        const code: number = await cmake.exec(options);
         tl.setResult(tl.TaskResult.Succeeded, tl.loc('CMakeReturnCode', code));
     }
-    catch(err) {
+    catch (err) {
         tl.error(err.message);
         tl.setResult(tl.TaskResult.Failed, tl.loc('CMakeFailed', err.message));
     }    
