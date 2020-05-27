d955d522cfd368fab9483854210374bc1e8899a8
Nitin <nitin.i.joy@gmail.com>
2020-05-27 19:33:23 +0530

    Fixing bug:

--- Tasks/DotNetCoreCLIV2/Common/utility.ts
+++ Tasks/DotNetCoreCLIV2/Common/utility.ts
@@ -1,10 +1,10 @@
 import * as tl from "azure-pipelines-task-lib/task";
 
-export function getProjectFiles(projectPattern: string[], cwd: string): string[] {
+export function getProjectFiles(projectPattern: string[], cwd: string|null = null): string[] {
     if (projectPattern.length == 0) {
         return [""];
     }
-    var projectFiles: string[] = tl.findMatch(tl.getVariable("System.DefaultWorkingDirectory") || cwd, projectPattern);
+    var projectFiles: string[] = tl.findMatch(cwd || tl.getVariable("System.DefaultWorkingDirectory"), projectPattern);
 
     if (!projectFiles || !projectFiles.length) {
         return [];
