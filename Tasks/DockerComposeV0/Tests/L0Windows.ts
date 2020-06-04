80d3c42d8cfaba821432172df64e9da380537eda
raghaves <raghaves@microsoft.com>
2020-06-04 18:13:03 +0530

    Chnages to take a userdefined path variable for docker-compose

--- Tasks/DockerComposeV0/Tests/L0Windows.ts
+++ Tasks/DockerComposeV0/Tests/L0Windows.ts
@@ -18,6 +18,7 @@ tr.setInput('dockerComposeCommand', process.env["__dockerComposeCommand__"] || n
 tr.setInput('azureSubscriptionEndpoint', 'AzureRMSpn');
 tr.setInput('azureContainerRegistry', '{"loginServer":"ajgtestacr1.azurecr.io", "id" : "/subscriptions/c00d16c7-6c1f-4c03-9be1-6934a4c49682/resourcegroups/ajgtestacr1rg/providers/Microsoft.ContainerRegistry/registries/ajgtestacr1"}');
 tr.setInput('arguments', process.env["__arguments__"] || '');
+tr.setInput('dockerComposeExePath', process.env["__dockerComposeExePath__"] || '');
 
 console.log("Inputs have been set");
 
@@ -39,13 +40,21 @@ process.env['AGENT_HOMEDIRECTORY'] = 'F:\\dir2\\';
 let a: any = <any>{
     "which": {
         "docker": "docker",
-        "docker-compose": "docker-compose"
+        "docker-compose" : "docker-compose"
     },
     "checkPath": {
         "docker": true,
         "docker-compose": true
     },
     "exec": {
+        "docker-compose-userdefined -f F:\\dir2\\docker-compose.yml config" :{
+            "code": 0,
+            "stdout": "services:\n  redis:\n    image: redis:alpine\n  web:\n    build:\n      context: C:\\docketest\n    ports:\n    - 5000:5000/tcp\n    volumes:\n    - C:\\docketest:/code:rw\nversion: '2.0'"
+        },
+        "docker-compose-userdefined -f F:\\dir2\\docker-compose.yml build" :{
+            "code": 0,
+            "stdout": "sucessfully built the service images"
+        },
         "docker-compose -f F:\\dir2\\docker-compose.yml build" :{
             "code": 0,
             "stdout": "sucessfully built the service images"
