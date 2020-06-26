a6eb69bd6ada613ecc13451e926aca768f8b3281
Dmitrii Chichuk <jamminroot@github.com>
2020-06-26 18:37:13 +0400

    Fix L0ApiRejectsFail test

--- Tasks/AppCenterDistributeV1/Tests/L0ApiRejectsFail.ts
+++ Tasks/AppCenterDistributeV1/Tests/L0ApiRejectsFail.ts
@@ -3,12 +3,12 @@ import ma = require('vsts-task-lib/mock-answer');
 import tmrm = require('vsts-task-lib/mock-run');
 import path = require('path');
 import fs = require('fs');
-var Readable = require('stream').Readable
+const Readable = require('stream').Readable
 
-var nock = require('nock');
+const nock = require('nock');
 
-let taskPath = path.join(__dirname, '..', 'appcenterdistribute.js');
-let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+const taskPath = path.join(__dirname, '..', 'appcenterdistribute.js');
+const tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
 tmr.setInput('serverEndpoint', 'MyTestEndpoint');
 tmr.setInput('appSlug', 'testuser/testapp');
@@ -18,8 +18,9 @@ tmr.setInput('releaseNotesInput', 'my release notes');
 
 //prepare upload
 nock('https://example.test')
-    .post('/v0.1/apps/testuser/testapp/package_uploads')
-    .reply(403);
+    .post('/v0.1/apps/testuser/testapp/uploads/releases')
+    .query(true)
+    .reply(403).log(console.log);
 
 // provide answers for task mock
 let a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
