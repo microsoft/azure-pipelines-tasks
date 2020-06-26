b70a472fae030fcca44c0cac599a38787645fa83
Koushik Dey <kodey@microsoft.com>
2020-06-26 15:07:22 +0530

    Test cases and addressed comments

--- Tasks/KubernetesV1/src/kubernetescommand.ts
+++ Tasks/KubernetesV1/src/kubernetescommand.ts
@@ -27,6 +27,7 @@ function getCommandOutputFormat(kubecommand: string): string[] {
     var outputFormat = tl.getInput("outputFormat", false);
     switch (outputFormat) {
         case null:
+        case '':
         case 'none':
             tl.debug(`Skipping -o in args as outputFormat is 'none' or empty.`);
             return args;
