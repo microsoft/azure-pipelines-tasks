c5692850e5c70003974249c64f0954365a258088
Alex Peck <alpeck@microsoft.com>
2020-07-09 11:14:52 -0400

    Add docker stop/start commands

---
+++ Tasks/DockerV2/dockerlifecycle.ts
@@ -0,0 +1,36 @@
+"use strict";
+
+import * as tl from "azure-pipelines-task-lib/task";
+import ContainerConnection from "docker-common-v2/containerconnection";
+import * as dockerCommandUtils from "docker-common-v2/dockercommandutils";
+import * as utils from "./utils";
+
+export function run(connection: ContainerConnection, outputUpdate: (data: string) => any): any {
+    let output = "";
+    let command = tl.getInput("command", true);
+    let containerName = tl.getInput("container", true);
+    let commandArguments = dockerCommandUtils.getCommandArguments(tl.getInput("arguments", false));
+
+    let containerMap = tl.getVariable("agent.containermapping");
+    if (!containerMap) {
+        throw new Error(tl.loc('ContainerNotFound', containerName));
+    }
+
+    let map = JSON.parse(containerMap);
+    let containerId = map[containerName].id;
+    if (!containerId || containerId == "") {
+        throw new Error(tl.loc('ContainerNotFound', containerName));
+    }
+
+    if (command == "start")  {
+        return dockerCommandUtils.start(connection, containerId, commandArguments, (data) => output += data).then(() => {
+            let taskOutputPath = utils.writeTaskOutput("start", output);
+            outputUpdate(taskOutputPath);
+        });
+    } else {
+        return dockerCommandUtils.stop(connection, containerId, commandArguments, (data) => output += data).then(() => {
+            let taskOutputPath = utils.writeTaskOutput("stop", output);
+            outputUpdate(taskOutputPath);
+        });
+    }
+}
