f9d70ef1624169744c8abc9b67f0b737e13b1799
Aasim Malladi <aamallad@microsoft.com>
2020-08-14 15:24:04 -0700

    Added cleanup task to MavenAuthenticate to remove creds

---
+++ Tasks/MavenAuthenticateV0/cleanup.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as tl from 'azure-pipelines-task-lib/task';
+
+async function run() {
+    try {
+        tl.setResourcePath(path.join(__dirname, 'task.json'));
+
+        const userM2SettingsXmlPath: string = tl.getTaskVariable('userM2SettingsXmlPath');
+        const backupUserM2SettingsFilePath: string = tl.getTaskVariable('backupUserM2SettingsFilePath');
+
+        if (userM2SettingsXmlPath && tl.exist(userM2SettingsXmlPath)) {
+            fs.unlinkSync(userM2SettingsXmlPath);
+            tl.debug('Deleted user m2 settings.xml file: ' + userM2SettingsXmlPath);
+            
+            if (backupUserM2SettingsFilePath && tl.exist(backupUserM2SettingsFilePath)) {
+                fs.renameSync(backupUserM2SettingsFilePath, userM2SettingsXmlPath);
+                tl.debug('Deleted user m2 settings.xml file: ' + backupUserM2SettingsFilePath);
+            }
+        }
+    } catch (err) {
+        tl.warning(tl.loc('Error_FailedCleanupM2', err));
+    }
+}
+
+run();
