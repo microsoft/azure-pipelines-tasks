682fad6b64d65471dae44e99a0fff7457f4a95a3
Danny McCormick <damccorm@microsoft.com>
2020-09-01 11:37:41 -0400

    Move docker-common to shared npm package (#13494)

--- Tasks/DockerV0/containerrun.ts
+++ Tasks/DockerV0/containerrun.ts
@@ -2,9 +2,8 @@
 
 import * as path from "path";
 import * as tl from "vsts-task-lib/task";
-import ContainerConnection from "docker-common/containerconnection";
+import ContainerConnection from "azure-pipelines-tasks-docker-common/containerconnection";
 import * as utils from "./utils";
-import * as imageUtils from "docker-common/containerimageutils";
 
 export function run(connection: ContainerConnection): any {
     var command = connection.createCommand();
