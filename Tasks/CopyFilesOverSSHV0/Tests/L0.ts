9429159141e595b76a6cb0b7014dae5076d1db24
Egor Bryzgalov <v-egbryz@microsoft.com>
2020-09-02 14:37:54 +0300

    Added tests for task's utils

--- Tasks/CopyFilesOverSSHV0/Tests/L0.ts
+++ Tasks/CopyFilesOverSSHV0/Tests/L0.ts
@@ -1,6 +1,7 @@
 import fs = require('fs');
 import assert = require('assert');
 import path = require('path');
+import * as  utils from '../utils';
 
 describe('CopyFilesOverSSHV0 Suite', function () {
     before(() => {
@@ -9,8 +10,32 @@ describe('CopyFilesOverSSHV0 Suite', function () {
     after(() => {
     });
 
-    it('Does a basic hello world test', function(done: MochaDone) {
-        // TODO - add real tests
-        done();
+    context('Utils tests: ', function () {
+        it('Should recognize UNC paths', function (done: MochaDone) {
+            const paths: string[] = [
+                '\\\\host\\one\\two',
+                '\\\\host\\one\\two\\',
+                '\\\\host\\one\\two/file',
+                '\\\\host/one/two/file'
+            ];
+            for (const path of paths) {
+                assert(utils.pathIsUNC(path), `Should be recognized as UNC path: ${path}`);
+            }
+            done();
+        });
+
+        it('Should not recognize strings as UNC paths', function (done: MochaDone) {
+            const paths: string[] = [
+                '//host\\one\\two',
+                '//host\\one\\two\\',
+                '//host\\one\\two/file',
+                '//host/one/two/file',
+                '\\host\\one\\two',
+            ];
+            for (const path of paths) {
+                assert(!utils.pathIsUNC(path), `Should be recognized as UNC path: ${path}`);
+            }
+            done();
+        });
     });
 });
