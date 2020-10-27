a43dbe6999ae6a5d8cab508b644a921622e286ed
Amruta Kawade <amrutakawade@github.com>
2020-10-27 20:41:58 +0530

    virtual directories are not created in AzureFileCopyV4 using blob prefix

--- Tasks/AzureFileCopyV4/Utility.ps1
+++ Tasks/AzureFileCopyV4/Utility.ps1
@@ -213,7 +213,8 @@ function Upload-FilesToAzureContainer
         Write-Output (Get-VstsLocString -Key "AFC_UploadFilesStorageAccount" -ArgumentList $sourcePath, $storageAccountName, $containerName, $blobPrefix)
 
         $blobPrefix = $blobPrefix.Trim()
-        $containerURL = [string]::Format("{0}/{1}/{2}", $blobStorageEndpoint.Trim("/"), $containerName, $blobPrefix).Trim("/")
+        $containerURL = [string]::Format("{0}/{1}/{2}", $blobStorageEndpoint.Trim("/"), $containerName, $blobPrefix.Trim("/"))
+        $containerURL = $containerURL + "/"
         $containerURL = $containerURL.Replace('$','`$')
         $azCopyExeLocation = Join-Path -Path $azCopyLocation -ChildPath "AzCopy.exe"
 
