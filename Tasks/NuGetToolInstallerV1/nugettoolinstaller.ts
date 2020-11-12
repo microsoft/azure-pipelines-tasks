0680a3a255c8ea70b8ed15848b645fccaa02cee0
Shradha Verma <shradha.verma@microsoft.com>
2020-11-12 11:57:52 -0800

    add msbuild info to telemetry

--- Tasks/NuGetToolInstallerV1/nugettoolinstaller.ts
+++ Tasks/NuGetToolInstallerV1/nugettoolinstaller.ts
@@ -1,36 +1,51 @@
 import * as taskLib from 'azure-pipelines-task-lib/task';
 import * as path from 'path';
 import * as nuGetGetter from 'packaging-common/nuget/NuGetToolGetter';
+import * as peParser from "packaging-common/pe-parser";
+import {VersionInfo} from "packaging-common/pe-parser/VersionResource";
 import * as telemetry from "utility-common/telemetry";
 
 const DEFAULT_NUGET_VERSION = '>=4.9';
 
 async function run() {
-    let versionSpec: string;
+    let nugetVersion: string;
     let checkLatest: boolean;
     let nuGetPath: string;
+    let msBuildSemVer: string;
     try {
         taskLib.setResourcePath(path.join(__dirname, 'task.json'));
 
-        versionSpec = taskLib.getInput('versionSpec', false) || DEFAULT_NUGET_VERSION;
+        const versionSpec = taskLib.getInput('versionSpec', false) || DEFAULT_NUGET_VERSION;
         checkLatest = taskLib.getBoolInput('checkLatest', false);
         nuGetPath = await nuGetGetter.getNuGet(versionSpec, checkLatest, true);
+
+        const nugetVersionInfo: VersionInfo = await peParser.getFileVersionInfoAsync(nuGetPath);
+        if (nugetVersionInfo && nugetVersionInfo.fileVersion){
+            nugetVersion = nugetVersionInfo.fileVersion.toString();
+        }
+
+        msBuildSemVer = await nuGetGetter.getMSBuildVersionString();
     } catch (error) {
         console.error('ERR:' + error.message);
         taskLib.setResult(taskLib.TaskResult.Failed, '');
     } finally {
-        _logNugetToolInstallerStartupVariables(versionSpec, checkLatest, nuGetPath)
+        _logNugetToolInstallerStartupVariables(nugetVersion, checkLatest, nuGetPath, msBuildSemVer)
     }
 }
 
-function _logNugetToolInstallerStartupVariables(versionSpec: string, checkLatest: boolean, nuGetPath: string) {
+function _logNugetToolInstallerStartupVariables(nugetVersion: string, 
+    checkLatest: boolean, 
+    nuGetPath: string,
+    msBuildSemVer: string) {
     try {
         const telem = {
             "NUGET_EXE_TOOL_PATH_ENV_VAR": taskLib.getVariable(nuGetGetter.NUGET_EXE_TOOL_PATH_ENV_VAR),
+            "DEFAULT_NUGET_VERSION": DEFAULT_NUGET_VERSION,
             "isCheckLatestEnabled": checkLatest,
-            "nuGetPath": nuGetPath,
             "requestedNuGetVersionSpec": taskLib.getInput('versionSpec', false),
-            "DEFAULT_NUGET_VERSION": DEFAULT_NUGET_VERSION
+            "nuGetPath": nuGetPath,
+            "nugetVersion": nugetVersion,
+            "msBuildVersion": msBuildSemVer
         };
         telemetry.emitTelemetry("Packaging", "NuGetToolInstaller", telem);
     } catch (err) {
