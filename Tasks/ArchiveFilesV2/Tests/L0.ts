4927cc97733c1f8b61b54cb1730d11502e813654
Egor Bryzgalov <v-egbryz@microsoft.com>
2020-11-12 13:33:06 +0300

    minor changes

--- Tasks/ArchiveFilesV2/Tests/L0.ts
+++ Tasks/ArchiveFilesV2/Tests/L0.ts
@@ -41,7 +41,7 @@ describe('ArchiveFiles L0 Suite', function () {
     
     tl.setResourcePath(path.join( __dirname, '..', 'task.json'));
     cases.forEach(function(numberOfFiles) {
-        it('Verify plan output for ' + numberOfFiles + ' files has correct number of lines', (done: Mocha.Done) => {
+        it(`Verify plan output for ${numberOfFiles} files has correct number of lines`, (done: Mocha.Done) => {
             test.timeout(1000);
             let max = 10;
             let plan = utils.reportArchivePlan(files(numberOfFiles), max);
