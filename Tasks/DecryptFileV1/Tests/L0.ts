4864fafbc3dc9559f89ef1be53c7d00397db5170
Nikita Ezzhev <v-niezz@microsoft.com>
2020-12-07 10:41:37 +0300

    Added simple L0 test

--- Tasks/DecryptFileV1/Tests/L0.ts
+++ Tasks/DecryptFileV1/Tests/L0.ts
@@ -4,7 +4,7 @@ import assert = require('assert');
 import path = require('path');
 
 describe('DecryptFileV1 Suite', function () {
-    this.timeout(60000);
+    this.timeout(parseInt(process.env.TASK_TEST_TIMEOUT) || 20000);
 
     function runValidations(validator: () => void, tr, done) {
         try {
@@ -19,13 +19,14 @@ describe('DecryptFileV1 Suite', function () {
     }
 
     it('Successfully decrypt file', (done: Mocha.Done) => {
-        this.timeout(5000);
+        this.timeout(1000);
+
         process.env['cipher'] = 'des3';
         process.env['inFile'] = 'encryptedTestFile.txt';
         process.env['outFile'] = 'decryptedTestFile.txt';
         process.env['passphrase'] = 'very-secret-password';
 
-        let tp: string = path.join(__dirname, 'LoDecryptFile.js');
+        let tp: string = path.join(__dirname, 'L0DecryptFile.js');
         let tr: mocktest.MockTestRunner = new mocktest.MockTestRunner(tp);
 
         tr.run();
