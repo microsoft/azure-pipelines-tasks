bd921bc3e1f3a717fd8fb5e5804a69b44d9646ba
Egor Bryzgalov <v-egbryz@microsoft.com>
2020-12-14 16:25:46 +0300

    Update L0DefaultsWithHomeSet.ts

--- Tasks/MavenV3/Tests/L0DefaultsWithHomeSet.ts
+++ Tasks/MavenV3/Tests/L0DefaultsWithHomeSet.ts
@@ -20,7 +20,7 @@ const inputs: MavenTaskInputs = {
     goals: "package",
     javaHomeSelection: "JDKVersion",
     jdkVersion: "default",
-    publishJUnitResults: true,
+    publishJUnitResults: false,
     testResultsFiles: "**/TEST-*.xml",
     mavenOpts: "-Xmx2048m",
     checkstyleAnalysisEnabled: false,
