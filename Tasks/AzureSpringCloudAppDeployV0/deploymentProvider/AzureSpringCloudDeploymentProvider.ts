a14cfe6af2cd6789e0a4b6a3131d61f6751e0687
Yev Bronshteyn <yebronsh@microsoft.com>
2020-12-17 16:40:05 -0500

    Initial commit as task builds

---
+++ Tasks/AzureSpringCloudAppDeployV0/deploymentProvider/AzureSpringCloudDeploymentProvider.ts
@@ -0,0 +1,52 @@
+import path = require('path');
+import { Package, PackageType } from 'webdeployment-common-v2/packageUtility';
+import { TaskParameters } from '../operations/taskparameters';
+import { AzureResourceFilterUtility } from '../operations/AzureResourceFilterUtility';
+import { AzureSpringCloud } from './azure-arm-spring-cloud';
+import { AzureRMEndpoint } from 'azure-pipelines-tasks-azure-arm-rest-v2/azure-arm-endpoint';
+import { AzureEndpoint } from 'azure-pipelines-tasks-azure-arm-rest-v2/azureModels';
+import tl = require('azure-pipelines-task-lib/task');
+
+
+export class AzureSpringCloudDeploymentProvider {
+    protected taskParameters: TaskParameters;
+    protected azureEndpoint: AzureEndpoint;
+    protected azureSpringCloud: AzureSpringCloud;
+
+    constructor(taskParameters: TaskParameters) {
+        this.taskParameters = taskParameters;
+    }
+
+    public async PreDeploymentStep() {
+        this.azureEndpoint = await new AzureRMEndpoint(this.taskParameters.ConnectedServiceName).getEndpoint();
+        console.log(tl.loc('GotconnectiondetailsforazureSpringCloudDeploy', this.taskParameters.AppName));
+        this.taskParameters.ResourceGroupName = await AzureResourceFilterUtility.getResourceGroupName(this.azureEndpoint, this.taskParameters.SpringCloudServiceName);
+        this.azureSpringCloud = new AzureSpringCloud(this.azureEndpoint, this.taskParameters.ResourceGroupName, this.taskParameters.SpringCloudServiceName);
+
+        let packageType = this.taskParameters.Package.getPackageType();
+        switch (packageType) {
+            case PackageType.jar:
+                if (this.taskParameters.Package.isFolder()){
+                    tl.error("A folder cannot be deployed in a Jar deployment: "+this.taskParameters.Package.getPath);
+                    throw new Error(tl.loc('Invalidwebapppackageorfolderpathprovided', this.taskParameters.Package.getPath()));
+                }
+                tl.debug("Initiated jar deployment to Azure Spring Cloud: " + this.taskParameters.Package.getPath());
+            break;
+
+
+        }
+    }
+
+    public async PostDeploymentStep() {
+    }
+
+    public async UpdateDeploymentStatus(isDeploymentSuccessful: boolean) {
+
+    }
+
+    public async DeployAppStep() {
+        let packageType = this.taskParameters.Package.getPackageType();
+    }
+
+
+}
