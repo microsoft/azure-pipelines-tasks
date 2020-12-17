89634a1d9c5b5c67a9f832f7d1d7665614acfa2f
Divanshu Aggarwal <diaggarw@microsoft.com>
2020-12-18 00:31:33 +0530

    DotNetCOreInstallerV0

--- Tasks/DotNetCoreInstallerV0/Tests/InstallWindows.ts
+++ Tasks/DotNetCoreInstallerV0/Tests/InstallWindows.ts
@@ -10,7 +10,7 @@ tr.setInput("packageType", process.env["__package_type__"] || 'sdk');
 tr.setInput("version", "1.0.4");
 
 process.env["AGENT_TOOLSDIRECTORY"] = "C:\\agent\\_tools";
-process.env["AGENT_TEMPDIRECTORY"] = "C:\\agent\\_temp";
+process.env["AGENT_TEMPDIRECTORY"] = "C:\\temp\\agent\\home\\temp";
 
 let a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
     "exec": {
