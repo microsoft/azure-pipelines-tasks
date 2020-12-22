4aacfd7f4c83134fccf27da6296e68349b53486b
Maxim Zaytsev <v-mazayt@microsoft.com>
2020-12-22 07:22:09 -0800

    Migrate legacy tests. Initial

--- Tasks/CUrlUploaderV2/Tests/L0.ts
+++ Tasks/CUrlUploaderV2/Tests/L0.ts
@@ -1,16 +1,75 @@
 import fs = require('fs');
 import assert = require('assert');
 import path = require('path');
+import * as ttm from 'azure-pipelines-task-lib/mock-test';
 
 describe('CUrlUploaderV2 Suite', function () {
-    before(() => {
-    });
 
-    after(() => {
-    });
+    it('runs a curl with single file', (done: Mocha.Done) => {
+        this.timeout(parseInt(process.env.TASK_TEST_TIMEOUT) || 20000);
 
-    it('Does a basic hello world test', function(done: MochaDone) {
-        // TODO - add real tests
+        const tp = path.join(__dirname, 'L0CurlGoodSingleFile.js');
+        const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+
+        assert(tr.invokedToolCount == 1, 'should have only run curl');
+        assert(tr.stderr.length == 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
         done();
     });
+
+    it('fails if url (req) input not set', (done: Mocha.Done) => {
+        this.timeout(parseInt(process.env.TASK_TEST_TIMEOUT) || 20000);
+
+        const tp = path.join(__dirname, 'L0NoUrl.js');
+        const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+
+        assert(tr.stdOutContained('Input required: url'), 'Should have printed: Input required: url');
+        assert(tr.failed, 'task should have failed');
+        assert(tr.invokedToolCount == 0, 'should exit before running curl');
+
+        done();
+    });
+
+    it('fails if files (req) input not set', (done: Mocha.Done) => {
+        this.timeout(parseInt(process.env.TASK_TEST_TIMEOUT) || 20000);
+
+        const tp = path.join(__dirname, 'L0NoFiles.js');
+        const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+
+        assert(tr.stdOutContained('Input required: files'), 'Should have printed: Input required: files');
+        assert(tr.failed, 'task should have failed');
+        assert(tr.invokedToolCount == 0, 'should exit before running curl');
+
+        done();
+    });
+
+    it('run curl with multiple files', (done: Mocha.Done) => {
+        this.timeout(parseInt(process.env.TASK_TEST_TIMEOUT) || 20000);
+
+        const tp = path.join(__dirname, 'L0CurlGoodMultiFiles.js');
+        const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+
+        console.log("----");
+        console.log(tr.stdout);
+        console.log("----");
+        console.log(tr.stderr);
+        console.log("----");
+        console.log(tr.errorIssues);
+        console.log("----");
+        assert(tr.invokedToolCount == 1, 'should have only run curl');
+        assert(tr.stderr.length == 0, 'should not have written to stderr');
+        assert(tr.succeeded, 'task should have succeeded');
+
+        done();
+    });
+
 });
