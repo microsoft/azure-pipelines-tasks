ea1ee846e127b1cdecb6fb23e7cebf4ef7f0b300
Egor Bryzgalov <v-egbryz@microsoft.com>
2020-12-28 17:47:14 +0300

    Revert "Added done() to tests"
    This reverts commit e4267c01d762b4c809e070fb60156362b0759617.

--- Tasks/UsePythonVersionV0/Tests/L0.ts
+++ Tasks/UsePythonVersionV0/Tests/L0.ts
@@ -24,7 +24,7 @@ describe('UsePythonVersion L0 Suite', function () {
         require('./L0_versionspec');
     });
 
-    it('succeeds when version is found', function (done: Mocha.Done) {
+    it('succeeds when version is found', function () {
         const testFile = path.join(__dirname, 'L0SucceedsWhenVersionIsFound.js');
         const testRunner = new MockTestRunner(testFile);
 
@@ -40,10 +40,9 @@ describe('UsePythonVersion L0 Suite', function () {
         assert(didPrependPath(testRunner, pythonBinDir));
         assert.strictEqual(testRunner.stderr.length, 0, 'should not have written to stderr');
         assert(testRunner.succeeded, 'task should have succeeded');
-        done();
     });
 
-    it('fails when version is not found', function (done: Mocha.Done) {
+    it('fails when version is not found', function () {
         const testFile = path.join(__dirname, 'L0FailsWhenVersionIsMissing.js');
         const testRunner = new MockTestRunner(testFile);
 
@@ -62,10 +61,9 @@ describe('UsePythonVersion L0 Suite', function () {
 
         assert(testRunner.createdErrorIssue(errorMessage));
         assert(testRunner.failed, 'task should have failed');
-        done();
     });
 
-    it('selects architecture passed as input', function (done: Mocha.Done) {
+    it('selects architecture passed as input', function () {
         const testFile = path.join(__dirname, 'L0SelectsArchitecture.js');
         const testRunner = new MockTestRunner(testFile);
 
@@ -74,10 +72,9 @@ describe('UsePythonVersion L0 Suite', function () {
         assert(didSetVariable(testRunner, 'pythonLocation', 'x86ToolPath'));
         assert.strictEqual(testRunner.stderr.length, 0, 'should not have written to stderr');
         assert(testRunner.succeeded, 'task should have succeeded');
-        done();
     });
 
-    it('finds PyPy2', function (done: Mocha.Done) {
+    it('finds PyPy2', function () {
         const testFile = path.join(__dirname, 'L0PyPy2.js');
         const testRunner = new MockTestRunner(testFile);
 
@@ -94,10 +91,9 @@ describe('UsePythonVersion L0 Suite', function () {
         assert(didPrependPath(testRunner, pypyBinDir));
         assert.strictEqual(testRunner.stderr.length, 0, 'should not have written to stderr');
         assert(testRunner.succeeded, 'task should have succeeded');
-        done();
     });
 
-    it('finds PyPy3', function (done: Mocha.Done) {
+    it('finds PyPy3', function () {
         const testFile = path.join(__dirname, 'L0PyPy3.js');
         const testRunner = new MockTestRunner(testFile);
 
@@ -114,6 +110,5 @@ describe('UsePythonVersion L0 Suite', function () {
         assert(didPrependPath(testRunner, pypyBinDir));
         assert.strictEqual(testRunner.stderr.length, 0, 'should not have written to stderr');
         assert(testRunner.succeeded, 'task should have succeeded');
-        done();
     });
 });
