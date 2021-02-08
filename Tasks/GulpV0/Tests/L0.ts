96c5636f99cfeed9b9bae0e9309527fdce9e988a
Maxim Zaytsev <v-mazayt@microsoft.com>
2021-02-08 02:33:06 -0800

    fx

--- Tasks/GulpV0/Tests/L0.ts
+++ Tasks/GulpV0/Tests/L0.ts
@@ -5,7 +5,7 @@ import * as ttm from 'azure-pipelines-task-lib/mock-test';
 
 const isWin = os.type().match(/^Win/);
 
-describe('GulpV Suite', function () {
+describe('GulpV0 Suite', function () {
     this.timeout(parseInt(process.env.TASK_TEST_TIMEOUT) || 20000);
 
     before((done: Mocha.Done) => {
