3d492a8b915029c6feacceb99b236c5b8177d69f
t-dedah <>
2021-02-23 02:07:32 +0530

    Removed debug setting

--- Tasks/AzureResourceGroupDeployment/operations/ResourceGroup.ts
+++ Tasks/AzureResourceGroupDeployment/operations/ResourceGroup.ts
@@ -29,7 +29,6 @@ class Deployment {
     }
     public updateCommonProperties(mode: string) {
         this.properties["mode"] = mode;
-        this.properties["debugSetting"] = { "detailLevel": "requestContent, responseContent" };
     }
 }
 
