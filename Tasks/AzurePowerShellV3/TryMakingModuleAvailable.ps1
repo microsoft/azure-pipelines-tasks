d4373f9fb02d36b4535047c24b64be84903e7c0f
Amit Kumar <amit-avit@github.com>
2021-02-25 19:12:40 +0530

    Added verbose logs

--- Tasks/AzurePowerShellV3/TryMakingModuleAvailable.ps1
+++ Tasks/AzurePowerShellV3/TryMakingModuleAvailable.ps1
@@ -8,10 +8,12 @@ param (
 $moduleContainerPath = Get-SavedModuleContainerPath
 
 if (-not(Test-Path -Path $moduleContainerPath)) {
+    Write-Verbose "Folder layout not as per hosted agent, considering self hosted agent skipping module unzip logic."
     return;
 }
 
 if (!$targetVersion) {
+    Write-Verbose "Latest module selected which will be available as folder."
     return;
 }
 
@@ -19,15 +21,19 @@ if (!$targetVersion) {
 @($false, $true).ForEach({
     $modulePath = Get-SavedModulePath -azurePowerShellVersion $targetVersion -Classic:$_;
     if (Test-Path -Path $modulePath) {
+        Write-Verbose "Module available as folder at $modulePath"
         return;
     }
 
     $moduleZipPath = $modulePath + ".zip";
     if (-not(Test-Path -Path $moduleZipPath)) {
+        Write-Verbose "Module zip not available to unzip at $moduleZipPath"
         return;
     }
 
+    Write-Verbose "Extracting zip $moduleZipPath"
     $parameter = @("x", "-o$moduleContainerPath", "$moduleZipPath")
     $command = "$PSScriptRoot\7zip\7z.exe"
     &$command @parameter
+    Write-Verbose "Extraction complete"
 })
