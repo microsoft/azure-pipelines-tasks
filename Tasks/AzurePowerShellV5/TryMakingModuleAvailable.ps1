75a05f67b2de3b84689eb184f6c6ebe9eeb923d5
Amit Kumar <amit-avit@github.com>
2021-02-26 16:41:32 +0530

    Review coomments

--- Tasks/AzurePowerShellV5/TryMakingModuleAvailable.ps1
+++ Tasks/AzurePowerShellV5/TryMakingModuleAvailable.ps1
@@ -15,20 +15,20 @@ try {
  
     if (!$isHostedAgent) {
         Write-Verbose "Module path not present as expected in hosted agent, skipping step to make module available."
-        $moduleSource = "Others"
+        $moduleSource = "privateAgent"
         return
     }
 
     if (!$targetVersion) {
         Write-Verbose "Latest selected, will make use of the latest available in agent as folder."
-        $moduleSource = "Folder"
+        $moduleSource = "hostedAgentFolder"
         return
     }
 
     $modulePath = Get-SavedModulePath -azurePowerShellVersion $targetVersion -isWin $isWin
     if (Test-Path $modulePath) {
         Write-Verbose "Az $targetVersion present at $modulePath as folder."
-        $moduleSource = "Folder"
+        $moduleSource = "hostedAgentFolder"
         return
     }
 
@@ -38,7 +38,7 @@ try {
         Write-Verbose "Az $targetVersion present at $moduleZipPath as zip, expanding it."
         Expand-ModuleZip -zipPath $moduleZipPath -destination $moduleContainerPath -isWin $isWin
         Write-Verbose "Zip expanded"
-        $moduleSource = "Zip"
+        $moduleSource = "hostedAgentZip"
         return
     }
 
@@ -47,16 +47,19 @@ try {
         $versionsManifest = Invoke-RestMethod -Method Get `
             -Headers @{ "Accept" = "application/vnd.github.VERSION.raw" } `
             -Uri "https://api.github.com/repos/Azure/az-ps-module-versions/contents/versions-manifest.json"
+        Write-Verbose "Versions manifest downloaded with below content."
+        Write-Verbose $versionsManifest
         $downloadUrlEntity = $versionsManifest | Where-Object version -eq $targetVersion
         if ($downloadUrlEntity) {
             $downloadUrl = $downloadUrlEntity.files[0].download_url
             Write-Verbose "Downloading Az $targetVersion from GHRelease"
             (New-Object System.Net.WebClient).DownloadFile($downloadUrl, $moduleZipPath)
+            Write-Verbose "Download succeeded"
             if (Test-Path $moduleZipPath) {
                 Write-Verbose "Expanding Az $targetVersion downloaded at $moduleZipPath as zip."
                 Expand-ModuleZip -zipPath $moduleZipPath -destination $moduleContainerPath -isWin $isWin
                 Write-Verbose "Zip expanded"
-                $moduleSource = "GHRelease"
+                $moduleSource = "hostedAgentGHRelease"
                 return
             }
         } else {
@@ -69,9 +72,13 @@ try {
 
     Write-Verbose "Downloading Az $targetVersion from PSGallery."
     Save-Module -Path $modulePath -Name Az -RequiredVersion $targetVersion -Force -ErrorAction Stop
-    $moduleSource = "PSGallery"
+    $moduleSource = "hostedAgentPSGallery"
 } finally {
     # Telemetry
+    # moduleSource value will be privateAgent(in case of self hosted private agents), hostedAgentFolder(when a version is present as folder or
+    # when latest is selected we will use the one latest available as folder), hostedAgentZip(when the module is available as a zip locally),
+    # hostedAgentGHRelease(when the module zip is downloaded from our GitHub releases management), hostedAgentPSGallery(when we download from PSGallery
+    # using the Save-Module cmdlet).
     $telemetryJsonContent = @{ targetAzurePs = $targetVersion; moduleSource = $moduleSource } | ConvertTo-Json -Compress
     Write-Host "##vso[telemetry.publish area=TaskHub;feature=AzurePowerShellV5]$telemetryJsonContent"
 }
