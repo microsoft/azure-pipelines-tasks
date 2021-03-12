fe79bf3cd2480dbf4b96e8b31603a94e00028771
Vladislav Ryzhov (Akvelon INC) <v-vryzhov@microsoft.com>
2021-03-12 13:58:35 +0300

    Added changed name for tests folders and corrected helpMarkDown for new input

--- Tasks/DeleteFilesV1/Tests/L0DoesntRemoveDotFiles.ts
+++ Tasks/DeleteFilesV1/Tests/L0DoesntRemoveDotFiles.ts
@@ -4,7 +4,7 @@ import path = require('path');
 let taskPath = path.join(__dirname, '..', 'deletefiles.js');
 let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
-const testRoot: string = path.join(__dirname, "test_structure", "dot2");
+const testRoot: string = path.join(__dirname, "test_structure", "DoesntRemoveDotFiles");
 
 tmr.setInput('Contents', '/**/*.txt');
 tmr.setInput('SourceFolder', testRoot);
--- Tasks/DeleteFilesV1/Tests/L0RemoveDotFiles.ts
+++ Tasks/DeleteFilesV1/Tests/L0RemoveDotFiles.ts
@@ -4,7 +4,7 @@ import path = require('path');
 let taskPath = path.join(__dirname, '..', 'deletefiles.js');
 let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
-const testRoot: string = path.join(__dirname, "test_structure", "dot");
+const testRoot: string = path.join(__dirname, "test_structure", "removeDotFiles");
 
 tmr.setInput('Contents', '/**/*.txt');
 tmr.setInput('SourceFolder', testRoot);
