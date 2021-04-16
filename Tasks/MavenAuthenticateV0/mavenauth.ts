9a42ad755b39538ce548e991241f840653caede7
Steven Cady <steven@cadpg.org>
2021-04-15 22:21:51 -0700

    Updated task.json to reflect current sprint, Updated mavenauth.ts to use getBoolInput.

--- Tasks/MavenAuthenticateV0/mavenauth.ts
+++ Tasks/MavenAuthenticateV0/mavenauth.ts
@@ -25,7 +25,7 @@ async function run(): Promise<void> {
 
         let userM2FolderPath: string = "";
 
-        if (tl.getInput("useAgentTempDir",false)) {
+        if (tl.getBoolInput("useAgentTempDir",false)) {
             userM2FolderPath = path.join(process.env.AGENT_TEMPDIRECTORY, M2FolderName);
         } else if (tl.osType().match(/^Win/)) {
             userM2FolderPath = path.join(process.env.USERPROFILE, M2FolderName);
