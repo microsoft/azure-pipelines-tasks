0176a31e9fb0bbbd6fdb8a7072061273f9752497
Juan Fiorenzano <jufioren@microsoft.com>
2021-05-04 17:16:47 -0700

    Taking into consideration the maximum size for environment variables

--- Tasks/DockerV2/dockerbuild.ts
+++ Tasks/DockerV2/dockerbuild.ts
@@ -10,18 +10,18 @@ export function run(connection: ContainerConnection, outputUpdate: (data: string
     // find dockerfile path
     let dockerfilepath = tl.getInput("Dockerfile", true);
     let dockerFile = fileUtils.findDockerFile(dockerfilepath);
-    
-    if(!tl.exist(dockerFile)) {
+
+    if (!tl.exist(dockerFile)) {
         throw new Error(tl.loc('ContainerDockerFileNotFound', dockerfilepath));
     }
 
     // get command arguments
     // ignore the arguments input if the command is buildAndPush, as it is ambiguous
     let commandArguments = isBuildAndPushCommand ? "" : dockerCommandUtils.getCommandArguments(tl.getInput("arguments", false));
-    
+
     // get qualified image names by combining container registry(s) and repository
     let repositoryName = tl.getInput("repository");
-    let imageNames: string[] = [];    
+    let imageNames: string[] = [];
     // if container registry is provided, use that
     // else, use the currently logged in registries
     if (tl.getInput("containerRegistry")) {
@@ -48,8 +48,7 @@ export function run(connection: ContainerConnection, outputUpdate: (data: string
         imageNames.forEach(imageName => {
             if (tags && tags.length > 0) {
                 tags.forEach(tag => {
-                    if(tag)
-                    {
+                    if (tag) {
                         tagArguments.push(imageName + ":" + tag);
                     }
                 });
@@ -70,18 +69,26 @@ export function run(connection: ContainerConnection, outputUpdate: (data: string
         outputUpdate(taskOutputPath);
 
         const builtImageId = getBuiltImageIdFromDockerBuiltOutput(output);
-        if (builtImageId && builtImageId != ""){
+        if (builtImageId && builtImageId != "") {
             shareBuiltImageId(builtImageId);
         }
     });
 }
 
 function shareBuiltImageId(builtImageId: string) {
+    const IMAGE_SEPARATOR_CHAR: string = ";";
+    const ENV_VARIABLE_MAX_SIZE = 32766;
     let builtImages: string = tl.getVariable("DOCKER_TASK_BUILT_IMAGES");
-    const IMAGE_SEPARATOR_CHAR:string = ";";
 
-    if (builtImages && builtImages != ""){
-        builtImages += `${IMAGE_SEPARATOR_CHAR}${builtImages}`;
+    if (builtImages && builtImages != "") {
+        const newImageId = `${IMAGE_SEPARATOR_CHAR}${builtImages}`;
+
+        if (newImageId.length + builtImages.length > ENV_VARIABLE_MAX_SIZE) {
+            tl.debug("Images id truncated maximum environment variable size reached.")
+            return;
+        }
+
+        builtImages += newImageId;
     }
     else {
         builtImages = builtImageId;
@@ -90,29 +97,29 @@ function shareBuiltImageId(builtImageId: string) {
     tl.setVariable("DOCKER_TASK_BUILT_IMAGES", builtImages);
 }
 
-function getBuiltImageIdFromDockerBuiltOutput(output: string) : string {
-    const standardParser = (text:string): string => {
-        let parsedOutput: string[] = text.match(new RegExp("Successfully built ([0-9a-f]{12})",'g'));
+function getBuiltImageIdFromDockerBuiltOutput(output: string): string {
+    const standardParser = (text: string): string => {
+        let parsedOutput: string[] = text.match(new RegExp("Successfully built ([0-9a-f]{12})", 'g'));
 
         return !parsedOutput || parsedOutput.length == 0
             ? ""
-            : parsedOutput[parsedOutput.length-1].substring(19); // This remove the Succesfully built section
+            : parsedOutput[parsedOutput.length - 1].substring(19); // This remove the Succesfully built section
     };
 
     const buildKitParser = (text: string): string => {
-        let parsedOutput : string[] = text.match(new RegExp("Writing image sha256:([0-9a-f]{64}): done",'g'));
+        let parsedOutput: string[] = text.match(new RegExp("Writing image sha256:([0-9a-f]{64}): done", 'g'));
 
         return !parsedOutput || parsedOutput.length == 0
             ? ""
-            : parsedOutput[parsedOutput.length].substring(21,33); // This remove the section Writing Image Sha256 and takes 12 characters from the Id.
+            : parsedOutput[parsedOutput.length].substring(21, 33); // This remove the section Writing Image Sha256 and takes 12 characters from the Id.
     }
 
     let buildOutputParserFuncs = [standardParser, buildKitParser]
-    for(let parserFunc of buildOutputParserFuncs){
+    for (let parserFunc of buildOutputParserFuncs) {
         const builtImageId = parserFunc(output);
-        if (builtImageId && builtImageId != ""){
+        if (builtImageId && builtImageId != "") {
             return builtImageId;
         }
     }
     return "";
- }
+}
