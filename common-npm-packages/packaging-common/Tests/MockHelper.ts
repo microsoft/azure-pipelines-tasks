ebf34762e1f86402b77233889c77482e39af657a
Shradha Verma <shradha.verma@microsoft.com>
2021-05-07 10:40:07 -0700

    update locationUtilities MockHelper

--- common-npm-packages/packaging-common/Tests/MockHelper.ts
+++ common-npm-packages/packaging-common/Tests/MockHelper.ts
@@ -37,6 +37,9 @@ export function registerLocationHelpersMock(tmr: tmrm.TaskMockRunner) {
         retryOnExceptionHelper: async function<T>(action: () => Promise<T>, maxTries: number, retryIntervalInMilliseconds: number) {
             return await action();
         },
+        retryOnNullOrExceptionHelper: async function<T>(action: () => Promise<T>, maxTries: number, retryIntervalInMilliseconds: number) {
+            return await action();
+        },
         ProtocolType: {NuGet: 1, Npm: 2, Maven: 3, PyPi: 4},
         RegistryType: {npm: 1, NuGetV2: 2, NuGetV3: 3, PyPiSimple: 4, PyPiUpload: 5}
     };
