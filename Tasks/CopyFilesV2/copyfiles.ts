d96cd33614f8135842a7b4a35e29765ae59ae00e
Anatolii Bolshakov (Akvelon INC) <v-anbols@microsoft.com>
2021-06-09 13:07:48 +0300

    Moved find operation to main block

--- Tasks/CopyFilesV2/copyfiles.ts
+++ Tasks/CopyFilesV2/copyfiles.ts
@@ -4,18 +4,15 @@ import tl = require('azure-pipelines-task-lib/task');
 
 /**
  * Input task data - including some processed inputs
+ * This data is consistent between retries
  */
 interface ITaskInputData {
     contents: string[];
-    sourceFolder: string;
-    targetFolder: string;
     cleanTargetFolder: boolean;
     overWrite: boolean;
     flattenFolders: boolean;
     preserveTimestamp: boolean;
     ignoreMakeDirErrors: boolean;
-    matchedPaths: string[];
-    matchedFiles: string[];
     retryCount: number;
 }
 
@@ -59,16 +56,24 @@ function makeDirP(targetFolder: string, ignoreErrors: boolean): void {
  */
 function main(inputsData: ITaskInputData) {
     const {
-        sourceFolder,
-        targetFolder,
+        contents,
         cleanTargetFolder,
         overWrite,
         flattenFolders,
         preserveTimestamp,
         ignoreMakeDirErrors,
-        matchedFiles,
         retryCount,
     } = inputsData;
+
+    let sourceFolder: string = tl.getPathInput('SourceFolder', true, true);
+
+    sourceFolder = path.normalize(sourceFolder);
+
+    const targetFolder: string = tl.getPathInput('TargetFolder', true);
+    let allPaths: string[] = tl.find(sourceFolder, findOptions);
+    let sourceFolderPattern = sourceFolder.replace('[', '[[]'); // directories can have [] in them, and they have special meanings as a pattern, so escape them
+    const matchedPaths: string[] = tl.match(allPaths, contents, sourceFolderPattern); // default match options
+    const matchedFiles: string[] = matchedPaths.filter((itemPath: string) => !tl.stats(itemPath).isDirectory()); // filter-out directories
 
     // copy the files to the target folder
     console.log(tl.loc('FoundNFiles', matchedFiles.length));
@@ -107,95 +112,57 @@ function main(inputsData: ITaskInputData) {
 
         // make sure the target folder exists
         makeDirP(targetFolder, ignoreMakeDirErrors);
+        let createdFolders: { [folder: string]: boolean } = {};
+        matchedFiles.forEach((file: string) => {
+            let relativePath;
+            if (flattenFolders) {
+                relativePath = path.basename(file);
+            } else {
+                relativePath = file.substring(sourceFolder.length);
 
-        try {
-            let createdFolders: { [folder: string]: boolean } = {};
-            matchedFiles.forEach((file: string) => {
-                let relativePath;
-                if (flattenFolders) {
-                    relativePath = path.basename(file);
-                } else {
-                    relativePath = file.substring(sourceFolder.length);
+                // trim leading path separator
+                // note, assumes normalized above
+                if (relativePath.startsWith(path.sep)) {
+                    relativePath = relativePath.substr(1);
+                }
+            }
 
-                    // trim leading path separator
-                    // note, assumes normalized above
-                    if (relativePath.startsWith(path.sep)) {
-                        relativePath = relativePath.substr(1);
+            let targetPath = path.join(targetFolder, relativePath);
+            let targetDir = path.dirname(targetPath);
+
+            if (!createdFolders[targetDir]) {
+                makeDirP(targetDir, ignoreMakeDirErrors);
+                createdFolders[targetDir] = true;
+            }
+
+            // stat the target
+            let targetStats: tl.FsStats;
+            if (!cleanTargetFolder) { // optimization - no need to check if relative target exists when CleanTargetFolder=true
+                try {
+                    targetStats = tl.stats(targetPath);
+                }
+                catch (err) {
+                    if (err.code != 'ENOENT') {
+                        throw err;
                     }
                 }
+            }
 
-                let targetPath = path.join(targetFolder, relativePath);
-                let targetDir = path.dirname(targetPath);
+            // validate the target is not a directory
+            if (targetStats && targetStats.isDirectory()) {
+                throw new Error(tl.loc('TargetIsDir', file, targetPath));
+            }
 
-                if (!createdFolders[targetDir]) {
-                    makeDirP(targetDir, ignoreMakeDirErrors);
-                    createdFolders[targetDir] = true;
+            if (!overWrite) {
+                if (targetStats) { // exists, skip
+                    console.log(tl.loc('FileAlreadyExistAt', file, targetPath));
                 }
-
-                // stat the target
-                let targetStats: tl.FsStats;
-                if (!cleanTargetFolder) { // optimization - no need to check if relative target exists when CleanTargetFolder=true
-                    try {
-                        targetStats = tl.stats(targetPath);
-                    }
-                    catch (err) {
-                        if (err.code != 'ENOENT') {
-                            throw err;
-                        }
-                    }
-                }
-
-                // validate the target is not a directory
-                if (targetStats && targetStats.isDirectory()) {
-                    throw new Error(tl.loc('TargetIsDir', file, targetPath));
-                }
-
-                if (!overWrite) {
-                    if (targetStats) { // exists, skip
-                        console.log(tl.loc('FileAlreadyExistAt', file, targetPath));
-                    }
-                    else { // copy
-                        console.log(tl.loc('CopyingTo', file, targetPath));
-                        tl.cp(file, targetPath, undefined, undefined, retryCount);
-                        if (preserveTimestamp) {
-                            try {
-                                const fileStats = tl.stats(file);
-                                fs.utimes(targetPath, fileStats.atime, fileStats.mtime, (err) => {
-                                    displayTimestampChangeResults(fileStats, err);
-                                });
-                            }
-                            catch (err) {
-                                console.warn(`Problem preserving the timestamp: ${err}`)
-                            }
-                        }
-                    }
-                }
-                else {
+                else { // copy
                     console.log(tl.loc('CopyingTo', file, targetPath));
-                    if (process.platform == 'win32' && targetStats && (targetStats.mode & 146) != 146) {
-                        // The readonly attribute can be interpreted by performing a bitwise-AND operation on
-                        // "fs.Stats.mode" and the integer 146. The integer 146 represents "-w--w--w-" or (128 + 16 + 2),
-                        // see following chart:
-                        //     R   W  X  R  W X R W X
-                        //   256 128 64 32 16 8 4 2 1
-                        //
-                        // "fs.Stats.mode" on Windows is based on whether the readonly attribute is set.
-                        // If the readonly attribute is set, then the mode is set to "r--r--r--".
-                        // If the readonly attribute is not set, then the mode is set to "rw-rw-rw-".
-                        //
-                        // Note, additional bits may also be set (e.g. if directory). Therefore, a bitwise
-                        // comparison is appropriate.
-                        //
-                        // For additional information, refer to the fs source code and ctrl+f "st_mode":
-                        //   https://github.com/nodejs/node/blob/v5.x/deps/uv/src/win/fs.c#L1064
-                        tl.debug(`removing readonly attribute on '${targetPath}'`);
-                        fs.chmodSync(targetPath, targetStats.mode | 146);
-                    }
-
-                    tl.cp(file, targetPath, "-f", undefined, retryCount);
+                    tl.cp(file, targetPath, undefined, undefined, retryCount);
                     if (preserveTimestamp) {
                         try {
-                            const fileStats: tl.FsStats = tl.stats(file);
+                            const fileStats = tl.stats(file);
                             fs.utimes(targetPath, fileStats.atime, fileStats.mtime, (err) => {
                                 displayTimestampChangeResults(fileStats, err);
                             });
@@ -205,11 +172,43 @@ function main(inputsData: ITaskInputData) {
                         }
                     }
                 }
-            });
-        }
-        catch (err) {
-            tl.setResult(tl.TaskResult.Failed, err);
-        }
+            }
+            else {
+                console.log(tl.loc('CopyingTo', file, targetPath));
+                if (process.platform == 'win32' && targetStats && (targetStats.mode & 146) != 146) {
+                    // The readonly attribute can be interpreted by performing a bitwise-AND operation on
+                    // "fs.Stats.mode" and the integer 146. The integer 146 represents "-w--w--w-" or (128 + 16 + 2),
+                    // see following chart:
+                    //     R   W  X  R  W X R W X
+                    //   256 128 64 32 16 8 4 2 1
+                    //
+                    // "fs.Stats.mode" on Windows is based on whether the readonly attribute is set.
+                    // If the readonly attribute is set, then the mode is set to "r--r--r--".
+                    // If the readonly attribute is not set, then the mode is set to "rw-rw-rw-".
+                    //
+                    // Note, additional bits may also be set (e.g. if directory). Therefore, a bitwise
+                    // comparison is appropriate.
+                    //
+                    // For additional information, refer to the fs source code and ctrl+f "st_mode":
+                    //   https://github.com/nodejs/node/blob/v5.x/deps/uv/src/win/fs.c#L1064
+                    tl.debug(`removing readonly attribute on '${targetPath}'`);
+                    fs.chmodSync(targetPath, targetStats.mode | 146);
+                }
+
+                tl.cp(file, targetPath, "-f", undefined, retryCount);
+                if (preserveTimestamp) {
+                    try {
+                        const fileStats: tl.FsStats = tl.stats(file);
+                        fs.utimes(targetPath, fileStats.atime, fileStats.mtime, (err) => {
+                            displayTimestampChangeResults(fileStats, err);
+                        });
+                    }
+                    catch (err) {
+                        console.warn(`Problem preserving the timestamp: ${err}`)
+                    }
+                }
+            }
+        });
     }
 }
 /**
@@ -225,8 +224,10 @@ function runWithRetries(taskInputData: ITaskInputData) {
             let errorMessage = `Error while task execution: ${e}.`;
             if (taskInputData.retryCount) {
                 errorMessage += `Remaining attempts: ${taskInputData.retryCount}`;
+                console.log(errorMessage);
+            } else {
+                tl.setResult(tl.TaskResult.Failed, e);
             }
-            console.log(errorMessage);
         }
 
         --taskInputData.retryCount;
@@ -236,27 +237,17 @@ function runWithRetries(taskInputData: ITaskInputData) {
 function readInputsData(): ITaskInputData {
     const taskInputsData: ITaskInputData = {
         contents: tl.getDelimitedInput('Contents', '\n', true),
-        sourceFolder: tl.getPathInput('SourceFolder', true, true),
-        targetFolder: tl.getPathInput('TargetFolder', true),
         cleanTargetFolder: tl.getBoolInput('CleanTargetFolder', false),
         overWrite: tl.getBoolInput('OverWrite', false),
         flattenFolders: tl.getBoolInput('flattenFolders', false),
         preserveTimestamp: tl.getBoolInput('preserveTimestamp', false),
         ignoreMakeDirErrors: tl.getBoolInput('ignoreMakeDirErrors', false),
         retryCount: parseInt(tl.getInput('retryCount')),
-        matchedPaths: [],
-        matchedFiles: []
     };
 
     if (isNaN(taskInputsData.retryCount) || taskInputsData.retryCount < 0) {
         taskInputsData.retryCount = 0;
     }
-
-    taskInputsData.sourceFolder = path.normalize(taskInputsData.sourceFolder);
-    let allPaths: string[] = tl.find(taskInputsData.sourceFolder, findOptions);
-    let sourceFolderPattern = taskInputsData.sourceFolder.replace('[', '[[]'); // directories can have [] in them, and they have special meanings as a pattern, so escape them
-    taskInputsData.matchedPaths = tl.match(allPaths, taskInputsData.contents, sourceFolderPattern); // default match options
-    taskInputsData.matchedFiles = taskInputsData.matchedPaths.filter((itemPath: string) => !tl.stats(itemPath).isDirectory()); // filter-out directories
 
     return taskInputsData;
 }
