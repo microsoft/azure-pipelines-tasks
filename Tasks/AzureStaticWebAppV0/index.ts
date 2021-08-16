cb8e0c2929b62ce448a1bdd88a5389cfe9e41499
brhopcra <brhopcra@microsoft.com>
2021-08-16 06:52:53 -0700

    added config_file_location, added system.debug compatibility, added logic to delete the env file, and fixed a typo

--- Tasks/AzureStaticWebAppV0/index.ts
+++ Tasks/AzureStaticWebAppV0/index.ts
@@ -4,6 +4,8 @@ import trm = require('azure-pipelines-task-lib/toolrunner');
 import fs = require('fs');
 
 async function run() {
+    const envVarFilePath: string = path.join(__dirname, 'env.list');
+
     try {
         tl.setResourcePath(path.join(__dirname, 'task.json'));
 
@@ -20,7 +22,7 @@ async function run() {
 
         bash.line(tl.getInput('args', false));
 
-        createDockerEnvVarFile()
+        createDockerEnvVarFile(envVarFilePath)
         
         const options = {
             failOnStdErr: false
@@ -28,13 +30,16 @@ async function run() {
 
         await bash.exec(<any>options);
         tl.setResult(tl.TaskResult.Succeeded, null);
-    }
-    catch (err) {
+    } catch (err) {
         tl.setResult(tl.TaskResult.Failed, null);
+    } finally {
+        await fs.unlink(envVarFilePath, (error) => {
+            if(error) throw error;
+        });
     }
 }
 
-function createDockerEnvVarFile() {
+async function createDockerEnvVarFile(envVarFilePath: string) {
     const taskVariables = tl.getVariables();
     var variableString: string = ""
 
@@ -50,24 +55,30 @@ function createDockerEnvVarFile() {
     const workingDirectory: string = tl.getInput('cwd', false) || "";
     const appLocation: string = tl.getInput('app_location', false) || "";
     const appBuildCommand: string = tl.getInput('app_build_command', false) || "";
-    const outputLoction: string = tl.getInput('output_location', false) || "";
+    const outputLocation: string = tl.getInput('output_location', false) || "";
     const apiLocation: string = tl.getInput('api_location', false) || "";
     const apiBuildCommand: string = tl.getInput('api_build_command', false) || "";
     const routesLocation: string = tl.getInput('routes_location', false) || "";
     const buildTimeoutInMinutes: string = tl.getInput('build_timeout_in_minutes', false) || "";
+    const configFileLocation: string = tl.getInput('config_file_location', false) || "";
     const skipAppBuild: boolean = tl.getBoolInput('skip_app_build', false);
-    const verbose: boolean = tl.getBoolInput('verbose', false);
     const apiToken: string = process.env['azure_static_web_apps_api_token'] || tl.getInput('azure_static_web_apps_api_token', false) || "";
 
+    const systemVerbose = getNullableBooleanFromString(process.env['SYSTEM_DEBUG']);
+    const inputVerbose = getNullableBooleanFromString(tl.getInput('verbose', false));
+
+    const verbose = inputVerbose === true ? true : (inputVerbose === false ? false : systemVerbose === true);
+    
     const deploymentClient = "mcr.microsoft.com/appsvc/staticappsclient:stable";
 
     addSystemVariableToString("APP_LOCATION", appLocation);
     addSystemVariableToString("APP_BUILD_COMMAND", appBuildCommand);
-    addSystemVariableToString("OUTPUT_LOCATION", outputLoction);
+    addSystemVariableToString("OUTPUT_LOCATION", outputLocation);
     addSystemVariableToString("API_LOCATION", apiLocation);
     addSystemVariableToString("API_BUILD_COMMAND", apiBuildCommand);
     addSystemVariableToString("ROUTES_LOCATION", routesLocation);
     addSystemVariableToString("BUILD_TIMEOUT_IN_MINUTES", buildTimeoutInMinutes);
+    addSystemVariableToString("CONFIG_FILE_LOCATION", configFileLocation);
 
     addSystemVariableToString("SKIP_APP_BUILD", skipAppBuild.toString());
     addSystemVariableToString("VERBOSE", verbose.toString());
@@ -91,9 +102,24 @@ function createDockerEnvVarFile() {
         }
     })
 
-    const envVarFilePath: string = path.join(__dirname, 'env.list');
+    await fs.writeFile(envVarFilePath, variableString, (error) => {
+        if(error) throw error;
+    });
+}
 
-    fs.writeFileSync(envVarFilePath, variableString);
+function getNullableBooleanFromString(boolString:string): boolean {
+    if (boolString == null) return null;
+    boolString = boolString.toLowerCase();
+
+    if(boolString === "true") {
+        return true;
+    }
+
+    if(boolString === "false") {
+        return false;
+    }
+
+    return null;
 }
 
 run();
