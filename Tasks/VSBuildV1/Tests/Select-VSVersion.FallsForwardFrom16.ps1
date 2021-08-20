1ab60be5bdc1560644723d3bcc44547e005d9e82
AnnaOpareva <84513972+AnnaOpareva@users.noreply.github.com>
2021-08-20 15:29:10 +0300

    Add VS2022 to the picklist for VSBuild, MSBuild, XamarinAndroid tasks (#15179)
    * fix latest in  VSbuild, MSBuild, XamarinAndroid
    
    * add unit test for VSBuild
    
    Co-authored-by: Anna Opareva <v-aopareva@microsoft.com>

--- Tasks/VSBuildV1/Tests/Select-VSVersion.FallsForwardFrom16.ps1
+++ Tasks/VSBuildV1/Tests/Select-VSVersion.FallsForwardFrom16.ps1
@@ -5,12 +5,12 @@ param()
 . $PSScriptRoot\..\..\..\Tests\lib\Initialize-Test.ps1
 . $PSScriptRoot\..\Select-VSVersion.ps1
 Register-Mock Write-Warning
-Register-Mock Get-VSPath { "Some resolved location" } -- -Version '14.0'
+Register-Mock Get-VSPath { "Some resolved location" } -- -Version '17.0'
 
 # Act.
-$actual = Select-VSVersion -PreferredVersion '12.0'
+$actual = Select-VSVersion -PreferredVersion '16.0'
 
 # Assert.
 Assert-WasCalled Write-Warning
-Assert-WasCalled Get-VSPath -Times 5
-Assert-AreEqual -Expected '14.0' -Actual $actual
+Assert-WasCalled Get-VSPath -Times 2
+Assert-AreEqual -Expected '17.0' -Actual $actual
