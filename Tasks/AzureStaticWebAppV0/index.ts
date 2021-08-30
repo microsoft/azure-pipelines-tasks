ce06d8389c9666a6b0f64d73d032fa64391ed158
brhopcra <brhopcra@microsoft.com>
2021-08-30 06:44:53 -0700

    implemented and modified Anthony's changes to get build vars from the task environment

--- Tasks/AzureStaticWebAppV0/index.ts
+++ Tasks/AzureStaticWebAppV0/index.ts
@@ -3,6 +3,16 @@ import tl = require('azure-pipelines-task-lib/task');
 import trm = require('azure-pipelines-task-lib/toolrunner');
 import fs = require('fs');
 
+const appLocationInputName = 'app_location';
+const appBuildCommandInputName = 'app_build_command';
+const outputLocationInputName = 'output_location';
+const apiLocationInputName = 'api_location';
+const apiBuildCommandInputName = 'api_build_command';
+const routesLocationInputName = 'routes_location';
+const buildTimeoutInMinutesInputName = 'build_timeout_in_minutes';
+const configFileLocationInputName = 'config_file_location';
+const apiTokenInputName = 'azure_static_web_apps_api_token';
+
 async function run() {
     const envVarFilePath: string = path.join(__dirname, 'env.list');
 
@@ -22,7 +32,7 @@ async function run() {
 
         bash.line(tl.getInput('args', false));
 
-        createDockerEnvVarFile(envVarFilePath)
+        await createDockerEnvVarFile(envVarFilePath);
         
         const options = {
             failOnStdErr: false
@@ -33,78 +43,101 @@ async function run() {
     } catch (err) {
         tl.setResult(tl.TaskResult.Failed, null);
     } finally {
-        await fs.unlink(envVarFilePath, (error) => {
-            if(error) throw error;
-        });
+        await fs.promises.unlink(envVarFilePath);
     }
 }
 
 async function createDockerEnvVarFile(envVarFilePath: string) {
-    const taskVariables = tl.getVariables();
     var variableString: string = ""
 
     const systemVariableNames: Set<string> = new Set<string>();
 
-    const addVariableToString = (variableName: string, variableValue: string) => variableString += variableName + "=" + variableValue + "\n"
+    const addVariableToString = (envVarName: string, envVarValue: string) => variableString += envVarName + "=" + envVarValue + "\n"
 
-    const addSystemVariableToString = (variableName: string, variableValue: string) => {
-        addVariableToString(variableName, variableValue)
-        systemVariableNames.add(variableName)
+    const addSystemVariableToString = (envVarName: string, envVarValue: string) => {
+        addVariableToString(envVarName, envVarValue)
+        systemVariableNames.add(envVarName)
     }
 
-    const workingDirectory: string = tl.getInput('cwd', false) || "";
-    const appLocation: string = tl.getInput('app_location', false) || "";
-    const appBuildCommand: string = tl.getInput('app_build_command', false) || "";
-    const outputLocation: string = tl.getInput('output_location', false) || "";
-    const apiLocation: string = tl.getInput('api_location', false) || "";
-    const apiBuildCommand: string = tl.getInput('api_build_command', false) || "";
-    const routesLocation: string = tl.getInput('routes_location', false) || "";
-    const buildTimeoutInMinutes: string = tl.getInput('build_timeout_in_minutes', false) || "";
-    const configFileLocation: string = tl.getInput('config_file_location', false) || "";
+    const addInputStringToString = (envVarName: string, envVarValue: string, inputName: string, ) => {
+        if (envVarValue.includes("\n")) {
+            tl.error("Input "+inputName+" is a multiline string and cannot be added to the build environment.");
+            throw "";
+        }
+
+        addSystemVariableToString(envVarName, envVarValue)
+    }
+
+    const workingDirectory: string = tl.getInput('cwd', false) || process.env.SYSTEM_DEFAULTWORKINGDIRECTORY;
+    const appLocation: string = tl.getInput(appLocationInputName, false) || "";
+    const appBuildCommand: string = tl.getInput(appBuildCommandInputName, false) || "";
+    const outputLocation: string = tl.getInput(outputLocationInputName, false) || "";
+    const apiLocation: string = tl.getInput(apiLocationInputName, false) || "";
+    const apiBuildCommand: string = tl.getInput(apiBuildCommandInputName, false) || "";
+    const routesLocation: string = tl.getInput(routesLocationInputName, false) || "";
+    const buildTimeoutInMinutes: string = tl.getInput(buildTimeoutInMinutesInputName, false) || "";
+    const configFileLocation: string = tl.getInput(configFileLocationInputName, false) || "";
+
     const skipAppBuild: boolean = tl.getBoolInput('skip_app_build', false);
-    const apiToken: string = process.env['azure_static_web_apps_api_token'] || tl.getInput('azure_static_web_apps_api_token', false) || "";
+    const apiToken: string = process.env[apiTokenInputName] || tl.getInput(apiTokenInputName, false) || "";
 
     const systemVerbose = getNullableBooleanFromString(process.env['SYSTEM_DEBUG']);
     const inputVerbose = getNullableBooleanFromString(tl.getInput('verbose', false));
 
     const verbose = inputVerbose === true ? true : (inputVerbose === false ? false : systemVerbose === true);
-    
-    const deploymentClient = "mcr.microsoft.com/appsvc/staticappsclient:stable";
 
-    addSystemVariableToString("APP_LOCATION", appLocation);
-    addSystemVariableToString("APP_BUILD_COMMAND", appBuildCommand);
-    addSystemVariableToString("OUTPUT_LOCATION", outputLocation);
-    addSystemVariableToString("API_LOCATION", apiLocation);
-    addSystemVariableToString("API_BUILD_COMMAND", apiBuildCommand);
-    addSystemVariableToString("ROUTES_LOCATION", routesLocation);
-    addSystemVariableToString("BUILD_TIMEOUT_IN_MINUTES", buildTimeoutInMinutes);
-    addSystemVariableToString("CONFIG_FILE_LOCATION", configFileLocation);
+    const deploymentClient = "mcr.microsoft.com/appsvc/staticappsclient:stable";
+    const containerWorkingDir = "/working_dir";
+
+    addInputStringToString("APP_LOCATION", appLocation, appLocationInputName);
+    addInputStringToString("APP_BUILD_COMMAND", appBuildCommand, appBuildCommandInputName);
+    addInputStringToString("OUTPUT_LOCATION", outputLocation, outputLocationInputName);
+    addInputStringToString("API_LOCATION", apiLocation, apiLocationInputName);
+    addInputStringToString("API_BUILD_COMMAND", apiBuildCommand, apiBuildCommandInputName);
+    addInputStringToString("ROUTES_LOCATION", routesLocation, routesLocationInputName);
+    addInputStringToString("BUILD_TIMEOUT_IN_MINUTES", buildTimeoutInMinutes, buildTimeoutInMinutesInputName);
+    addInputStringToString("CONFIG_FILE_LOCATION", configFileLocation, configFileLocationInputName);
 
     addSystemVariableToString("SKIP_APP_BUILD", skipAppBuild.toString());
     addSystemVariableToString("VERBOSE", verbose.toString());
 
-    addSystemVariableToString("DEPLOYMENT_TOKEN", apiToken);
+    addInputStringToString("DEPLOYMENT_TOKEN", apiToken, apiTokenInputName);
 
-    process.env['SWA_WORKING_DIR'] = workingDirectory;
     process.env['SWA_DEPLOYMENT_CLIENT'] = deploymentClient;
+    process.env['SWA_WORKING_DIR'] = workingDirectory;
+    process.env['SWA_WORKSPACE_DIR'] = containerWorkingDir;
 
-    systemVariableNames.add("GITHUB_WORKSPACE")
-    systemVariableNames.add("DEPLOYMENT_PROVIDER")
-    systemVariableNames.add("REPOSITORY_URL")
-    systemVariableNames.add("IS_PULL_REQUEST")
-    systemVariableNames.add("BASE_BRANCH")
+    addSystemVariableToString("GITHUB_WORKSPACE", "");
+    addSystemVariableToString("DEPLOYMENT_PROVIDER", "DevOps");
+    addSystemVariableToString("REPOSITORY_URL", process.env.BUILD_REPOSITORY_URI || "");
+    addSystemVariableToString("IS_PULL_REQUEST", "");
+    addSystemVariableToString("BASE_BRANCH", "");
+    addSystemVariableToString("REPOSITORY_BASE", containerWorkingDir);
+    addSystemVariableToString("BRANCH", process.env.BUILD_SOURCEBRANCHNAME || "");
+    addSystemVariableToString("DEPLOYMENT_ACTION", "upload");
 
-    taskVariables.forEach((taskVariable) => {
-        if (systemVariableNames.has(taskVariable.name)) {
-            tl.warning("custom variable overlapping with reserved SWA variable: " + taskVariable.name)
-        } else {
-            addVariableToString(taskVariable.name, taskVariable.value)
+    const blacklistString = await fs.promises.readFile(path.join(__dirname, 'envVarBlacklist.json'), 'utf8');
+    const blacklist = JSON.parse(blacklistString);
+
+    Object.keys(process.env).forEach( (envVarKey: string) => {
+        const envVarValue = process.env[envVarKey];
+
+        if (envVarValue.includes("\n")) {
+            tl.warning("Environment variable "+envVarKey+" is a multiline string and cannot be added to the build environment.");
+            return;
         }
-    })
 
-    await fs.writeFile(envVarFilePath, variableString, (error) => {
-        if(error) throw error;
+        if (systemVariableNames.has(envVarKey)) {
+            tl.warning("custom variable overlapping with reserved SWA variable: " + envVarKey);
+            return;
+        }
+
+        if(!blacklist.includes(envVarKey.toUpperCase())) {
+            addVariableToString(envVarKey, envVarValue);
+        }
     });
+
+    await fs.promises.writeFile(envVarFilePath, variableString);
 }
 
 function getNullableBooleanFromString(boolString:string): boolean {
--- Tasks/AzureStaticWebAppV0/launch-docker.sh
+++ Tasks/AzureStaticWebAppV0/launch-docker.sh
@@ -1,14 +1,5 @@
-workspace="/working_dir"
-mount_dir=$SYSTEM_DEFAULTWORKINGDIRECTORY
-[[ -n "$SWA_WORKING_DIR" ]] && mount_dir=$SWA_WORKING_DIR
-
 docker run \
-    -e REPOSITORY_BASE=$workspace \
-    -e DEPLOYMENT_PROVIDER=DevOps \
-    -e REPOSITORY_URL="$BUILD_REPOSITORY_URI" \
-    -e BRANCH="$BUILD_SOURCEBRANCHNAME" \
-    -e DEPLOYMENT_ACTION=upload \
     --env-file ./env.list \
-    -v "$mount_dir:$workspace" \
+    -v "$SWA_WORKING_DIR:$SWA_WORKSPACE_DIR" \
     "$SWA_DEPLOYMENT_CLIENT" \
     ./bin/staticsites/StaticSitesClient run
