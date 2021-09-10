11f641a1738a1aa26294c9d5d658610500ec37ce
annaOpareva <v-aopareva@microsoft.com>
2021-09-10 15:30:46 +0300

    fix

--- Tasks/VSBuildV1/Get-VSPath.ps1
+++ Tasks/VSBuildV1/Get-VSPath.ps1
@@ -9,21 +9,21 @@ function Get-VSPath {
 
     try {
         if ( !($Version -in $Versions )) {
-          Write-Warning "Please enter one of the versions 10.0, 11.0, 12.0, 14.0, 15.0, 16.0, 17.0" 
-          } else { 
+            Write-Warning "Please enter one of the versions 10.0, 11.0, 12.0, 14.0, 15.0, 16.0, 17.0" 
+            } else { 
             $VersionNumber = [int]$Version.Remove(2)
-                # Search for more than 15.0 Willow instance.
-                if ($VersionNumber -ge 15) {
-                    if (($instance = Get-VisualStudio $VersionNumber) -and
-                        $instance.installationPath) {                    
-                        return $instance.installationPath
-                        }
-                }
-                # Fallback to searching for an older install.
-                if ($path = (Get-ItemProperty -LiteralPath "HKLM:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\$Version" -Name 'ShellFolder' -ErrorAction Ignore).ShellFolder) {
-                    return $path
-                }
-          }
+            # Search for more than 15.0 Willow instance.
+            if ($VersionNumber -ge 15) {
+                if (($instance = Get-VisualStudio $VersionNumber) -and
+                    $instance.installationPath) {                    
+                    return $instance.installationPath
+                    }
+            }
+            # Fallback to searching for an older install.
+            if ($path = (Get-ItemProperty -LiteralPath "HKLM:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\$Version" -Name 'ShellFolder' -ErrorAction Ignore).ShellFolder) {
+                return $path
+            }
+        }
     } finally {
         Trace-VstsLeavingInvocation $MyInvocation
     }
