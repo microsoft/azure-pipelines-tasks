65f5cf0e2e86c6bca38c73d62a4ae0b6746fe012
annaOpareva <v-aopareva@microsoft.com>
2021-09-17 15:21:45 +0300

    add act

--- Tasks/VSBuildV1/Tests/Get-VSPath-Use-RegKey.ps1
+++ Tasks/VSBuildV1/Tests/Get-VSPath-Use-RegKey.ps1
@@ -10,14 +10,13 @@ Register-Mock Get-ItemProperty { @{ShellFolder = 'use_reg_key'} } -- -LiteralPat
 Register-Mock Get-ItemProperty { @{ShellFolder = 'use_reg_key'} } -- -LiteralPath HKLM:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\11.0 -Name ShellFolder -ErrorAction Ignore
 Register-Mock Get-ItemProperty { @{ShellFolder = 'use_reg_key'} } -- -LiteralPath HKLM:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\10.0 -Name ShellFolder -ErrorAction Ignore
 
-
-# Assert.
-
+# Aсt.
 $path_14 = Get-VSPath '14.0'
 $path_12 = Get-VSPath '12.0'
 $path_11 = Get-VSPath '11.0'
 $path_10 = Get-VSPath '10.0'
 
+# Assert.
 Assert-AreEqual 'use_reg_key' $path_14
 Assert-AreEqual 'use_reg_key' $path_12
 Assert-AreEqual 'use_reg_key' $path_11
--- Tasks/VSBuildV1/Tests/Get-VSPath-Use-VsWhere.ps1
+++ Tasks/VSBuildV1/Tests/Get-VSPath-Use-VsWhere.ps1
@@ -12,12 +12,12 @@ Register-Mock Get-VisualStudio { $instance } -- '17'
 Register-Mock Get-VisualStudio { $instance } -- '16'
 Register-Mock Get-VisualStudio { $instance } -- '15'
 
-# Assert.
-
+# Act.
 $path_17 = Get-VSPath '17.0'
 $path_16 = Get-VSPath '16.0'
 $path_15 = Get-VSPath '15.0'
 
+# Assert.
 Assert-AreEqual 'use_vswhere' $path_17
 Assert-AreEqual 'use_vswhere' $path_16
 Assert-AreEqual 'use_vswhere' $path_15
