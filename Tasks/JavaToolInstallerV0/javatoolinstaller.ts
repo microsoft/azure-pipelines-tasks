a6afb8987642ec0461a228887ecd0f01a18a3881
Anna Opareva <v-aopareva@microsoft.com>
2021-09-22 09:44:17 +0300

    fix archive name

--- Tasks/JavaToolInstallerV0/javatoolinstaller.ts
+++ Tasks/JavaToolInstallerV0/javatoolinstaller.ts
@@ -62,7 +62,8 @@ async function getJava(versionSpec: string, jdkArchitectureOption: string): Prom
                 taskLib.getInput('azureStorageAccountName', true), taskLib.getInput('azureContainerName', true), "");
             await azureDownloader.downloadArtifacts(extractLocation, '*' + fileNameAndPath);
             await taskutils.sleepFor(250); //Wait for the file to be released before extracting it.
-            jdkFileName = path.join(extractLocation, fileNameAndPath);
+            let jdkArchiveName = fileNameAndPath.slice(fileNameAndPath.lastIndexOf('/')+1);
+            jdkFileName = path.join(extractLocation, jdkArchiveName);
         } else {
             // get from local directory
             console.log(taskLib.loc('RetrievingJdkFromLocalPath'));
