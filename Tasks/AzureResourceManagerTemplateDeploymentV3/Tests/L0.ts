7a55cc6c3a72f7a97bf56d02401b57633dc4fce3
t-dedah <t-dedah@github.com>
2022-01-18 05:15:34 +0530

    Removed parameter test

--- Tasks/AzureResourceManagerTemplateDeploymentV3/Tests/L0.ts
+++ Tasks/AzureResourceManagerTemplateDeploymentV3/Tests/L0.ts
@@ -182,7 +182,6 @@ describe('Azure Resource Manager Template Deployment', function () {
         tr.run();
         try {
             assert(tr.succeeded, "Should have succeeded");
-            assert(tr.stdout.indexOf("properly sanitized") > 0, "Parameters should have been sanitized");
             assert(tr.stdout.indexOf("deployments.createOrUpdate is called") > 0, "deployments.createOrUpdate function should have been called from azure-sdk");
             assert(tr.stdout.indexOf("##vso[task.setvariable variable=someVar;]") >= 0, "deploymentsOutput should have been updated");
             done();
