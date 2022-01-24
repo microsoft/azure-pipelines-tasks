a4031b34e52598918cd2c66ad01aca965af6d683
Apple <apple@Apples-MacBook-Pro.local>
2022-01-25 04:09:12 +0530

    Resolved test issue

--- Tasks/AzureResourceManagerTemplateDeploymentV3/Tests/L0.ts
+++ Tasks/AzureResourceManagerTemplateDeploymentV3/Tests/L0.ts
@@ -219,14 +219,9 @@ describe('Azure Resource Manager Template Deployment', function () {
         process.env["csmParametersFile"] = "";
         let tr = new ttm.MockTestRunner(tp);
         tr.run();
-
-        console.log(tr.stdout)
-
-        
         try {
             assert(!tr.succeeded, "Should have failed");
             assert(tr.stdout.indexOf("BicepBuildFailed") > 0, "should have printed BicepBuildFailed")
-            assert(tr.stdout.indexOf("This declaration type is not recognized. Specify a parameter, variable, resource, or output declaration.") > 0, "should have printed the error message")
             assert(tr.stdout.indexOf("deployments.createOrUpdate is called") < 0, "deployments.createOrUpdate function should not have been called from azure-sdk");
             done();
         }
