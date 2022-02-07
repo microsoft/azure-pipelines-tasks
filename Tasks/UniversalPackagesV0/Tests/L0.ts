7c8681304cf794af98501a5d2459aaebbb13b9ee
Denis Rumyantsev <v-denisr@microsoft.com>
2022-02-07 12:55:35 +0300

    timeout 5 sec

--- Tasks/UniversalPackagesV0/Tests/L0.ts
+++ Tasks/UniversalPackagesV0/Tests/L0.ts
@@ -10,7 +10,7 @@ describe('UniversalPackages Suite', function () {
     });
 
     it('downloads package from current organization', (done: MochaDone) => {
-        this.timeout(1000);
+        this.timeout(5000);
         let tp = path.join(__dirname, './downloadInternal.js')
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
