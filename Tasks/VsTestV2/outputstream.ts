48cba74268b9ae9977cc6e04db078442bd998f85
Kirill Palatnikov <kirillp@microsoft.com>
2022-02-22 15:09:17 -0800

    Update stream to remove ##vso commands.

--- Tasks/VsTestV2/outputstream.ts
+++ Tasks/VsTestV2/outputstream.ts
@@ -4,9 +4,11 @@ import * as tl from 'azure-pipelines-task-lib/task';
 
 export class StringErrorWritable extends stream.Writable {
     private value: string = '';
+    private isErrorStream: boolean;
 
-    constructor(options: any) {
+    constructor(isErrorStream: boolean, options: any) {
         super(options);
+        this.isErrorStream = isErrorStream;
     }
 
     _write(data: any, encoding: string, callback: Function): void {
@@ -15,8 +17,15 @@ export class StringErrorWritable extends stream.Writable {
         let errorString: string = data.toString();
         let n = errorString.indexOf(os.EOL);
         while (n > -1) {
-            const line = errorString.substring(0, n);
-            tl.error(line);
+            var line = errorString.substring(0, n);
+            line = line.replace('##vso','');
+
+            if (this.isErrorStream) {
+                tl.error(line);
+            }
+            else {
+                console.log(line);
+            }
 
             // the rest of the string ...
             errorString = errorString.substring(n + os.EOL.length);
