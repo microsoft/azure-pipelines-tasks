e320f03af60cc19cac7709b30d54c61e5f345c91
Tatyana Kostromskaya (Akvelon) <v-takos@microsoft.com>
2022-02-22 08:51:43 +0100

    Migrate legasy tests to common tests

--- Tasks/PublishTestResultsV1/Tests/L0.ts
+++ Tasks/PublishTestResultsV1/Tests/L0.ts
@@ -101,7 +101,7 @@ describe('PublishTestResultsV1 Find files legacy suite', function () {
 
     it('Search simple pattern (relative path)', (done) => {
         let relativePath = path.relative(process.cwd(), path.join(__dirname, 'data', '*.log'));
-        let test = ff.findFiles(path.join('L0', '..' , relativePath));
+        let test = ff.findFiles(path.join('L0', '..', relativePath));
         assert(test.length === 2);
         assert(test[0] === posixFormat(path.join(data, 'a.log')));
         assert(test[1] === posixFormat(path.join(data, 'b.log')));
@@ -109,7 +109,7 @@ describe('PublishTestResultsV1 Find files legacy suite', function () {
     });
 
     it('Search pattern seperated by semi-colon(delimiter)', (done) => {
-        let test = ff.findFiles(path.join(data, '*.log') + ";" +path.join(data, '*.txt'));
+        let test = ff.findFiles(path.join(data, '*.log') + ";" + path.join(data, '*.txt'));
         assert(test.length === 4);
         assert(test[0] === posixFormat(path.join(data, 'a.log')));
         assert(test[1] === posixFormat(path.join(data, 'b.log')));
@@ -117,7 +117,7 @@ describe('PublishTestResultsV1 Find files legacy suite', function () {
         assert(test[3] === posixFormat(path.join(data, 'b.txt')));
         done();
     });
-    
+
     it('Search pattern seperated by semi-colon(delimiter)', (done) => {
         let test = ff.findFiles(path.join(data, 'a*') + ";-:" + path.join(data, 'a.txt'));
         assert(test.length === 1);
@@ -125,7 +125,7 @@ describe('PublishTestResultsV1 Find files legacy suite', function () {
         done();
     });
 
-    it.only('Publish test results with resultFiles filter that does not match with any files', function (done: Mocha.Done) {
+    it('Publish test results with resultFiles filter that does not match with any files', function (done: Mocha.Done) {
         const testPath = path.join(__dirname, 'L0FilterDoesNotMatchAnyFile.js')
         const tr: MockTestRunner = new MockTestRunner(testPath);
         tr.run();
@@ -135,6 +135,55 @@ describe('PublishTestResultsV1 Find files legacy suite', function () {
         assert(tr.invokedToolCount == 0, 'should exit before running PublishTestResults');
         done();
     });
+
+    it('Publish test results with resultFiles filter that matches with some files', function (done: Mocha.Done) {
+        const testPath = path.join(__dirname, 'L0FilterMatchesSomeFile.js')
+        const tr: MockTestRunner = new MockTestRunner(testPath);
+        tr.run();
+
+        assert(tr.stderr.length == 0, 'should not have written to stderr. error: ' + tr.stderr);
+        assert(tr.succeeded, 'task should have succeeded');
+        assert(tr.stdout.search(/##vso\[results.publish type=JUnit;mergeResults=true;resultFiles=/) >= 0, 'should publish test results.');
+        done();
+    });
+
+    it('Publish test results with resultFiles as file path', function (done: Mocha.Done) {
+        const testPath = path.join(__dirname, 'L0ResultFilesAsFilePath.js')
+        const tr: MockTestRunner = new MockTestRunner(testPath);
+        tr.run();
+
+        assert(tr.stderr.length == 0, 'should not have written to stderr. error: ' + tr.stderr);
+        assert(tr.succeeded, 'task should have succeeded');
+        assert(tr.stdout.search(/##vso\[results.publish type=JUnit;resultFiles=/) >= 0, 'should publish test results.');
+        done();
+    });
+
+    it('Publish test results when test result files input is not provided', function (done: Mocha.Done) {
+        const testPath = path.join(__dirname, 'L0InputIsNotProvided.js')
+        const tr: MockTestRunner = new MockTestRunner(testPath);
+        tr.run();
+
+        assert(tr.stdout.length > 0, 'should have written to stderr');
+        assert(tr.stdout.indexOf('Input required: testResultsFiles') >= 0, 'wrong error message: "' + tr.stdout + '"');
+        assert(tr.failed, 'task should have failed');
+        assert(tr.invokedToolCount == 0, 'should exit before running PublishTestResults');
+
+        done();
+    });
+
+    it('Publish test results when test runner type input is not provided', function (done: Mocha.Done) {
+        const testPath = path.join(__dirname, 'L0TestRunnerTypeIsNotProvided.js')
+        const tr: MockTestRunner = new MockTestRunner(testPath);
+        tr.run();
+
+        assert(tr.stdout.length > 0, 'should have written to stdout');
+        assert(tr.stdout.indexOf('Input required: testRunner') >= 0, 'wrong error message: "' + tr.stdout + '"');
+        assert(tr.failed, 'task should have failed');
+        assert(tr.invokedToolCount == 0, 'should exit before running PublishTestResults');
+
+        done();
+    });
+
 });
 
 function posixFormat(p: string): string {
