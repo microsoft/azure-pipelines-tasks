7bc6a59820e45a510c2c3061ced5c706347c61a8
Denis Tikhomirov <v-dentikhomi@microsoft.com>
2022-03-04 10:26:42 +0300

    Moved sending of notifications to PS script

---
+++ ci/courtesy-push/send-notification.ps1
@@ -0,0 +1,45 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [bool]$IsPRCreated
+)
+
+# Function sends Office 365 connector card to webhook.
+# It requires title and message text displyed in card and theme color used to hignlight card.
+function Send-Notification {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$titleText,
+        [Parameter(Mandatory = $true)]
+        [string]$messageText,
+        [Parameter(Mandatory = $true)]
+        [string]$themeColor
+    )
+    
+    $body = [PSCustomObject]@{
+        title = $titleText
+        text = $messageText
+        themeColor = $themeColor
+    } | ConvertTo-Json
+    
+    Invoke-RestMethod -Uri $(MSTeamsUri) -Method Post -Body $body -ContentType 'application/json'    
+}
+
+if ($IsPRCreated) {
+    $pullRequestLink = "[PR $($env:PrID)]($($env:PrLink))"
+}
+else {
+    $pipelineLink = "$env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI$env:SYSTEM_TEAMPROJECT/_build/results?buildId=$env:BUILD_BUILDID&_a=summary"
+    $buildLink = "[ID $($env:BUILD_BUILDID)]($($pipelineLink))"
+}
+
+$titleText = $IsPRCreated ? "Courtesy Bump of Tasks PR created - ID $($env:PrID)" : "Courtesy push build failed - ID $($env:BUILD_BUILDID)"
+
+$wikiLink = "[Wiki](https://mseng.visualstudio.com/AzureDevOps/_wiki/wikis/AzureDevOps.wiki/25317/Release-of-pipeline-tasks)"
+
+$messageText = $IsPRCreated ? `
+    "Created Courtesy Bump of Tasks PR. Please review and approve/merge $pullRequestLink. Related article in $wikiLink." : `
+    "Failed to create Courtesy Bump of Tasks PR. Please review the results of failed build $buildLink. Related article in $wikiLink."
+
+$themeColor = $IsPRCreated ? "#FFFF00" : "#FF0000"
+
+Send-Notification($titleText, $messageText, $themeColor)
