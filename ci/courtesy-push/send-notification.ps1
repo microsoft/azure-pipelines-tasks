28597bdc77ccbbb033e0dcab3b7eea5da4de4e93
Denis Tikhomirov <v-dentikhomi@microsoft.com>
2022-03-04 13:48:17 +0300

    Updated if statements for title, message and theme color

--- ci/courtesy-push/send-notification.ps1
+++ ci/courtesy-push/send-notification.ps1
@@ -24,22 +24,20 @@ function Send-Notification {
     Invoke-RestMethod -Uri $(MSTeamsUri) -Method Post -Body $body -ContentType 'application/json'    
 }
 
+$wikiLink = "[Wiki](https://mseng.visualstudio.com/AzureDevOps/_wiki/wikis/AzureDevOps.wiki/25317/Release-of-pipeline-tasks)"
+
 if ($IsPRCreated) {
     $pullRequestLink = "[PR $($env:PrID)]($($env:PrLink))"
+    $titleText = "Courtesy Bump of Tasks PR created - ID $($env:PrID)"
+    $messageText = "Created Courtesy Bump of Tasks PR. Please review and approve/merge $pullRequestLink. Related article in $wikiLink."
+    $themeColor = "#FFFF00"
 }
 else {
     $pipelineLink = "$env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI$env:SYSTEM_TEAMPROJECT/_build/results?buildId=$env:BUILD_BUILDID&_a=summary"
     $buildLink = "[ID $($env:BUILD_BUILDID)]($($pipelineLink))"
+    $titleText = "Courtesy push build failed - ID $($env:BUILD_BUILDID)"
+    $messageText = "Failed to create Courtesy Bump of Tasks PR. Please review the results of failed build $buildLink. Related article in $wikiLink."
+    $themeColor = "#FF0000"
 }
-
-$titleText = if ($IsPRCreated -eq $true) {"Courtesy Bump of Tasks PR created - ID $($env:PrID)"} else {"Courtesy push build failed - ID $($env:BUILD_BUILDID)"}
-
-$wikiLink = "[Wiki](https://mseng.visualstudio.com/AzureDevOps/_wiki/wikis/AzureDevOps.wiki/25317/Release-of-pipeline-tasks)"
-
-$messageText = if ($IsPRCreated -eq $true) `
-    {"Created Courtesy Bump of Tasks PR. Please review and approve/merge $pullRequestLink. Related article in $wikiLink."} else `
-    {"Failed to create Courtesy Bump of Tasks PR. Please review the results of failed build $buildLink. Related article in $wikiLink."}
-
-$themeColor = if ($IsPRCreated -eq $true) {"#FFFF00"} else {"#FF0000"}
 
 Send-Notification($titleText, $messageText, $themeColor)
