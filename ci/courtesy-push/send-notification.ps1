187d8a5acfcfde2c68fb41f64dc7cd25caf1cb83
Denis Tikhomirov <v-dentikhomi@microsoft.com>
2022-03-05 12:03:00 +0300

    Added test changes

--- ci/courtesy-push/send-notification.ps1
+++ ci/courtesy-push/send-notification.ps1
@@ -31,7 +31,8 @@ function Send-Notification {
         themeColor = $themeColor
     } | ConvertTo-Json
     
-    Invoke-RestMethod -Uri $(MSTeamsUri) -Method Post -Body $body -ContentType 'application/json'    
+    #Invoke-RestMethod -Uri $($MSTeamsUri) -Method Post -Body $body -ContentType 'application/json'
+    Invoke-RestMethod -Uri $($env:MSTeamsUri) -Method Post -Body $body -ContentType 'application/json' 
 }
 
 $wikiLink = "[Wiki](https://mseng.visualstudio.com/AzureDevOps/_wiki/wikis/AzureDevOps.wiki/25317/Release-of-pipeline-tasks)"
