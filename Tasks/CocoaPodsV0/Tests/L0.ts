0b16b3474e2fa0188d29ff3e2b526dac0344a7cf
kirill-ivlev <kirill.ivlev@akvelon.com>
2022-04-26 11:01:44 +0400

    added unit tests

--- Tasks/CocoaPodsV0/Tests/L0.ts
+++ Tasks/CocoaPodsV0/Tests/L0.ts
@@ -1,6 +1,7 @@
 import fs = require('fs');
 import assert = require('assert');
 import path = require('path');
+import * as ttm from 'azure-pipelines-task-lib/mock-test';
 
 describe('CocoaPodsV0 Suite', function () {
     before(() => {
@@ -9,8 +10,19 @@ describe('CocoaPodsV0 Suite', function () {
     after(() => {
     });
 
-    it('Does a basic hello world test', function(done: MochaDone) {
-        // TODO - add real tests
-        done();
+    it('run pod', function(done: Mocha.Done) {
+        let tp: string = path.join(__dirname, 'L0DefaultRunner.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+        try {
+            tr.run();
+            assert(tr.ran('pod --version'), 'it should run pod --version');
+            assert(tr.ran('pod install'), 'it should run pod install');
+        } catch(err) {
+             console.log(tr.stdout);
+             console.log(tr.stderr);
+             console.log(err);
+        } finally {
+            done();
+        }
     });
 });
