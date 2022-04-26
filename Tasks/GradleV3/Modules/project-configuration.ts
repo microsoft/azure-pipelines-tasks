9990d94fcd22406eff55315f8d0593248e460ecd
SvetlanaMaliugina <v-smaliugina@microsoft.com>
2022-04-26 15:37:19 +0300

    Added changes

--- Tasks/GradleV3/Modules/project-configuration.ts
+++ Tasks/GradleV3/Modules/project-configuration.ts
@@ -52,7 +52,6 @@ export function configureWrapperScript(wrapperScript: string): string {
             fs.accessSync(script, fs.constants.X_OK)
         } catch (err) {
             // If not, show warning and chmodding the gradlew file to make it executable
-            tl.debug('Im here')
             tl.warning(tl.loc('chmodGradlew'))
             fs.chmodSync(script, '755');
         }   
