3853186d92b659b43c9a0865118f667d963492a7
Tom Hubelbauer <tohubelb@microsoft.com>
2022-05-03 12:47:51 +0200

    Print Node versions used by each task
    Later this will be used to find corresponding update issue or create one if it doesn't exist yet.

--- .github/workflows/trackNodeMigration.js
+++ .github/workflows/trackNodeMigration.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import fetchAllPages from './fetchAllPages.js';
 
 const token = process.argv[2];
@@ -7,3 +8,13 @@ if (!token) {
 
 const issues = await fetchAllPages(token, 'repos/microsoft/azure-pipelines-tasks/issues', { labels: 'node-migration', state: 'all' });
 console.log('Found', issues.length, 'Node migration issues');
+
+for (const entry of await fs.promises.readdir('../../Tasks', { withFileTypes: true })) {
+  if (!entry.isDirectory()) {
+    continue;
+  }
+
+  console.log('Parsing task', entry.name, 'manifest');
+  const manifest = JSON.parse(await fs.promises.readFile('../../Tasks/' + entry.name + '/task.json'));
+  console.log(entry.name, 'uses', Object.keys(manifest.execution));
+}
