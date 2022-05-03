3972724c5fc760304462465d5e097ccb353599c1
Tom Hubelbauer <tohubelb@microsoft.com>
2022-05-03 12:42:16 +0200

    Fix a typo in the issue count print statement
    Two typos actually, issue->issues and label->length.

--- .github/workflows/trackNodeMigration.js
+++ .github/workflows/trackNodeMigration.js
@@ -6,4 +6,4 @@ if (!token) {
 }
 
 const issues = await fetchAllPages(token, 'repos/microsoft/azure-pipelines-tasks/issues', { labels: 'node-migration', state: 'all' });
-console.log('Found', issue.labels, 'Node migration issues');
+console.log('Found', issues.length, 'Node migration issues');
