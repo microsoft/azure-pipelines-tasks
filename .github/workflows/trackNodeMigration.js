c5e83510dec31399c7fe521cb34f360437da7896
Tom Hubelbauer <tohubelb@microsoft.com>
2022-05-03 12:41:02 +0200

    Disable the Tasks directory trigger
    It seems Dependabot is pushing dep changes in droves (not sure why yet) which keeps triggering this workflow.

--- .github/workflows/trackNodeMigration.js
+++ .github/workflows/trackNodeMigration.js
@@ -1,4 +1,9 @@
 import fetchAllPages from './fetchAllPages.js';
 
+const token = process.argv[2];
+if (!token) {
+  throw new Error('Provide a PAT as a CLI argument!');
+}
+
 const issues = await fetchAllPages(token, 'repos/microsoft/azure-pipelines-tasks/issues', { labels: 'node-migration', state: 'all' });
 console.log('Found', issue.labels, 'Node migration issues');
