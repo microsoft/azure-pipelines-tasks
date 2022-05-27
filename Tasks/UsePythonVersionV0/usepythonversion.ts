23aa7bd2a7abb4bdcba07716c8b5599674354bee
SvetlanaMaliugina <v-smaliugina@microsoft.com>
2022-05-27 23:57:17 +0300

    Removed redundant indent

--- Tasks/UsePythonVersionV0/usepythonversion.ts
+++ Tasks/UsePythonVersionV0/usepythonversion.ts
@@ -43,7 +43,7 @@ function pypyNotFoundError(versionSpec: string) {
         // 'Python' is intentional here
         task.loc('ToolNotFoundMicrosoftHosted', 'Python', 'https://aka.ms/hosted-agent-software'),
         task.loc('ToolNotFoundSelfHosted', 'Python', 'https://go.microsoft.com/fwlink/?linkid=871498')
-        ].join(os.EOL));
+    ].join(os.EOL));
 }
 
 // Note on the tool cache layout for PyPy:
@@ -87,7 +87,7 @@ function usePyPy(versionSpec: string, parameters: TaskParameters, platform: Plat
         toolUtil.prependPathSafe(_binDir);
         // Starting from PyPy 7.3.1, the folder that is used for pip and anything that pip installs should be "Scripts" on Windows.
         if (platform === Platform.Windows) {
-        toolUtil.prependPathSafe(path.join(installDir, 'Scripts'));
+            toolUtil.prependPathSafe(path.join(installDir, 'Scripts'));
         }
     }
 }
@@ -108,33 +108,32 @@ async function useCpythonVersion(parameters: Readonly<TaskParameters>, platform:
 
     let installDir: string | null = tool.findLocalTool('Python', semanticVersionSpec, parameters.architecture);
     // Python version not found in local cache, try to download and install
-
     if (!installDir) {
         task.debug(`Could not find a local python installation matching ${semanticVersionSpec}.`);
         if (!parameters.disableDownloadFromRegistry) {
-        try {
-            task.debug('Trying to download python from registry.');
-            await installPythonVersion(semanticVersionSpec, parameters);
-            installDir = tool.findLocalTool('Python', semanticVersionSpec, parameters.architecture);
-            if (installDir) {
-            task.debug(`Successfully installed python from registry to ${installDir}.`);
+            try {
+                task.debug('Trying to download python from registry.');
+                await installPythonVersion(semanticVersionSpec, parameters);
+                installDir = tool.findLocalTool('Python', semanticVersionSpec, parameters.architecture);
+                if (installDir) {
+                task.debug(`Successfully installed python from registry to ${installDir}.`);
+                }
+            } catch (err) {
+                task.error(task.loc('DownloadFailed', err.toString()));
             }
-        } catch (err) {
-            task.error(task.loc('DownloadFailed', err.toString()));
-        }
         }
     }
 
-  // If still not found, then both local check and download have failed
+    // If still not found, then both local check and download have failed
     if (!installDir) {
         // Fail and list available versions
         const x86Versions = tool.findLocalToolVersions('Python', 'x86')
-        .map(s => `${s} (x86)`)
-        .join(os.EOL);
+            .map(s => `${s} (x86)`)
+            .join(os.EOL);
 
         const x64Versions = tool.findLocalToolVersions('Python', 'x64')
-        .map(s => `${s} (x64)`)
-        .join(os.EOL);
+            .map(s => `${s} (x64)`)
+            .join(os.EOL);
 
         throw new Error([
             task.loc('VersionNotFound', parameters.versionSpec, parameters.architecture),
@@ -152,21 +151,21 @@ async function useCpythonVersion(parameters: Readonly<TaskParameters>, platform:
         toolUtil.prependPathSafe(binDir(installDir, platform))
 
         if (platform === Platform.Windows) {
-        // Add --user directory
-        // `installDir` from tool cache should look like $AGENT_TOOLSDIRECTORY/Python/<semantic version>/x64/
-        // So if `findLocalTool` succeeded above, we must have a conformant `installDir`
-        const version = path.basename(path.dirname(installDir));
-        const major = semver.major(version);
-        const minor = semver.minor(version);
+            // Add --user directory
+            // `installDir` from tool cache should look like $AGENT_TOOLSDIRECTORY/Python/<semantic version>/x64/
+            // So if `findLocalTool` succeeded above, we must have a conformant `installDir`
+            const version = path.basename(path.dirname(installDir));
+            const major = semver.major(version);
+            const minor = semver.minor(version);
 
-        const userScriptsDir = path.join(process.env['APPDATA'] as string, 'Python', `Python${major}${minor}`, 'Scripts');
-        toolUtil.prependPathSafe(userScriptsDir);
+            const userScriptsDir = path.join(process.env['APPDATA'] as string, 'Python', `Python${major}${minor}`, 'Scripts');
+            toolUtil.prependPathSafe(userScriptsDir);
         }
         // On Linux and macOS, pip will create the --user directory and add it to PATH as needed.
     }
 }
 
-    export async function usePythonVersion(parameters: Readonly<TaskParameters>, platform: Platform): Promise<void> {
+export async function usePythonVersion(parameters: Readonly<TaskParameters>, platform: Platform): Promise<void> {
     let fullVersionSpec: string = parameters.versionSpec.toUpperCase();
 
     if (fullVersionSpec.startsWith("PYPY")) {
