a540fe3d9c7fc91df0d2c216e0e5085b68388356
Igor Kozlov <v-kozlovigor@microsoft.com>
2022-06-27 17:19:14 +0200

    initial research

---
+++ ci/verify-versions/task-lib.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+var path = require('path');
+
+const tasks = []  // get list of tasks
+
+console.log(fs.readFileSync(path.resolve(__dirname, '..', '..', 'make-options.json')))
