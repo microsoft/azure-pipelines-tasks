e43f44b98eb3336d34c2afdf1db4f6d2be6300bd
Igor Kozlov <v-kozlovigor@microsoft.com>
2022-06-30 18:45:12 +0200

    dg

--- ci/verify-versions/task-lib.js
+++ ci/verify-versions/task-lib.js
@@ -1,19 +1,21 @@
 var fs = require('fs');
 var path = require('path');
 
-const tasks = ['InstallSSHKeyV0']//JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', '..', 'make-options.json'), 'utf8')).tasks;
+const tasks = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', '..', 'make-options.json'), 'utf8')).tasks;
 
 tasks.forEach(task => {
-    const pathToTask = path.resolve(__dirname, '..', '..', 'Tasks', task)
-    const packageJSON = JSON.parse(fs.readFileSync(path.resolve(pathToTask, 'node_modules', 'azure-pipelines-task-lib', 'package.json')));
-    const dependencies = Object.keys(packageJSON.dependencies)//.includes('')
-    dependencies.forEach(dep => {
-        if (dep.match('common')) {
-            if (fs.existsSync(path.resolve(pathToTask, 'node_modules', dep, 'node_modules'))) {
-                const commonPackageJSON = JSON.parse(fs.readFileSync(path.resolve(pathToTask, 'node_modules', dep, 'package.json')));
-                if (packageJSON.dependencies.vesrion === commonPackageJSON.dependencies['azure-pipelines-task-lib'])
-                    throw new Error('task-lib versions are different')
+    const pathToTask = path.resolve(__dirname, '..', '..', '_build', 'Tasks', task)
+    if(fs.existsSync(path.resolve(pathToTask, 'node_modules', 'azure-pipelines-task-lib'))) {
+        const packageJSON = JSON.parse(fs.readFileSync(path.resolve(pathToTask, 'node_modules', 'azure-pipelines-task-lib', 'package.json')));
+        const dependencies = Object.keys(packageJSON.dependencies)//.includes('')
+        dependencies.forEach(dependency => {
+            if (dependency.match('common')) {
+                if (fs.existsSync(path.resolve(pathToTask, 'node_modules', dependency, 'node_modules'))) {
+                    const commonPackageJSON = JSON.parse(fs.readFileSync(path.resolve(pathToTask, 'node_modules', dependency, 'node_modules', 'package.json')));
+                    if (packageJSON.version !== commonPackageJSON.version)
+                        throw new Error('task-lib versions are different')
+                }
             }
-        }
-    })
+        })
+    }
 })
