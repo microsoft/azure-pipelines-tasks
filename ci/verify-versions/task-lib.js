36026b8310e77877a38d615a0de6b1a1050dbbec
Denis Rumyantsev <mr.denis.rumyantsev@gmail.com>
2022-07-08 23:32:52 +0200

    refactoring

--- ci/verify-versions/task-lib.js
+++ ci/verify-versions/task-lib.js
@@ -4,7 +4,12 @@ const path = require('path');
 const AZURE_PIPELINE_TASK_LIB = 'azure-pipelines-task-lib';
 const NODE_MODULES = 'node_modules';
 const PACKAGE_JSON = 'package.json';
-const pathToTasksBuild = path.resolve(__dirname, '..', '..', '_build', 'Tasks');
+const rootDirectory = path.resolve(__dirname, '..', '..');
+const pathToTasksBuild = path.resolve(rootDirectory, '_build', 'Tasks');
+const pathToCommonNpmPackages = path.resolve(rootDirectory, 'common-npm-packages');
+const commonNpmPackages = fs.readdirSync(pathToCommonNpmPackages).filter(folder =>
+    fs.statSync(path.resolve(pathToCommonNpmPackages, folder)).isDirectory() && folder != 'build-scripts'
+).map(package => parseJsonFromPath(pathToCommonNpmPackages, package, PACKAGE_JSON).name);
 
 function parseJsonFromPath(...args) {
     const pathToJson = path.resolve(...args);
@@ -15,21 +20,21 @@ function parseJsonFromPath(...args) {
 
 if (fs.existsSync(pathToTasksBuild)) {
     const tasks = fs.readdirSync(pathToTasksBuild);
-    tasks.forEach(task => {
+    for (const task of tasks) {
         const pathToTask = path.resolve(pathToTasksBuild, task);
         const pathToTaskLib = path.resolve(pathToTask, NODE_MODULES, AZURE_PIPELINE_TASK_LIB);
         if (fs.existsSync(pathToTaskLib)) {
             const taskPackageJson = parseJsonFromPath(pathToTask, PACKAGE_JSON);
             const taskLibPackageJson = parseJsonFromPath(pathToTaskLib, PACKAGE_JSON);
             const dependencies = Object.keys(taskPackageJson.dependencies);
-            dependencies.forEach(dependency => {
-                if (dependency.match('common')) {
+            for (const dependency of dependencies) {
+                if (dependency in commonNpmPackages) {
                     const commonPackageJson = parseJsonFromPath(pathToTask, NODE_MODULES, dependency, NODE_MODULES, AZURE_PIPELINE_TASK_LIB, PACKAGE_JSON);
-                    if (commonPackageJson && taskLibPackageJson.version !== commonPackageJson.version) {
+                    if (commonPackageJson && taskLibPackageJson.version != commonPackageJson.version) {
                         throw new Error(`node_modules includes different azure-pipeline-task-lib versions in root and in ${dependency}`);
                     }
                 }
-            });
+            }
         }
-    });
+    }
 }
