4d099b3ccceff3b27cc8790deb06a7e29cd90888
Denis Rumyantsev <mr.denis.rumyantsev@gmail.com>
2022-07-18 12:46:36 +0200

    check if source branch is defined

--- ci/verify-versions/task-lib.js
+++ ci/verify-versions/task-lib.js
@@ -62,13 +62,14 @@ function chechTaskLibVersion () {
         }
     }
     if (warningMessage) {
-        const messageType = sourceBranch == 'master' ? 'warning' : 'error';
+        const isError = sourceBranch && sourceBranch != 'master';
+        const messageType = isError ? 'error' : 'warning';
         console.log('\n=============     =============     > > > > > > >     =============     =============\n');
         console.log(`##vso[task.logissue type=${messageType}]"${AZURE_PIPELINES_TASK_LIB}" version is not the same in common npm packages and tasks.`);
         console.log(warningMessage);
         console.log(stepsToFix);
         console.log('\n=============     =============     < < < < < < <     =============     =============\n');
-        if (sourceBranch != 'master') console.log('##vso[task.complete result=Failed]');
+        if (isError) console.log('##vso[task.complete result=Failed]');
     }
 }
 
