d6e24e249ea21d24ab72aefb4560a691cc7b4f7b
Denis Rumyantsev <mr.denis.rumyantsev@gmail.com>
2022-08-19 08:32:41 +0200

    vsts-task-lib check only

--- ci/verify-versions/task-lib.js
+++ ci/verify-versions/task-lib.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 const VSTS_TASK_LIB = 'vsts-task-lib';
 const AZURE_PIPELINES_TASK_LIB = 'azure-pipelines-task-lib';
-const AZURE_PIPELINES_TOOL_LIB = 'azure-pipelines-tool-lib';
 const PACKAGE_LOCK_JSON = 'package-lock.json';
 const NPM_SHRINKWRAP_JSON = 'npm-shrinkwrap.json';
 const rootDirectory = path.resolve(__dirname, '..', '..');
@@ -12,34 +11,6 @@ const tasks = fs.readdirSync(pathToTasks).filter(task => task != 'Common');
 const taskPattern = process.argv[2];
 const tasksToCheck = taskPattern ? tasks.filter(task => taskPattern.includes(task)) : tasks;
 const sourceBranch = process.env['SYSTEM_PULLREQUEST_SOURCEBRANCH'];
-const pathToCommonNpmPackages = path.resolve(rootDirectory, 'common-npm-packages');
-const commonNpmPackagesFolders = fs.readdirSync(pathToCommonNpmPackages).filter(folder =>
-    fs.statSync(path.resolve(pathToCommonNpmPackages, folder)).isDirectory() && folder != 'build-scripts'
-);
-const commonNpmPackagesNames = commonNpmPackagesFolders.map(package =>
-    parseJsonFromPath(pathToCommonNpmPackages, package, PACKAGE_LOCK_JSON).name
-);
-
-const template = `
-Task Name :: Version of "${AZURE_PIPELINES_TASK_LIB}" used by this task
-   ---> First Dependency Name :: Version of "${AZURE_PIPELINES_TASK_LIB}" used by first dependency
-   ---> Second Dependency Name :: Version of "${AZURE_PIPELINES_TASK_LIB}" used by second dependency`;
-
-const stepsToFix = `Follow these steps for each task in the above list to fix this issue:
-
-Open "Tasks/<task-name>/package.json" and make sure that for the "${AZURE_PIPELINES_TASK_LIB}" dependency
-and for dependencies which use "${AZURE_PIPELINES_TASK_LIB}" as dependency only the major version is frozen.
-Use ^ to freeze only the major version.
-
-Run these commands:
-
->>> node make.js build --task <task-name>
->>> cd Tasks/<task-name>
->>> rm package-lock.json
->>> rm -Recurse -Force node_modules
->>> npm update
-
-Upgrade "${AZURE_PIPELINES_TASK_LIB}" dependency version in the task or in the related package if they are incompatible.`;
 
 function parseJsonFromPath(...args) {
     const pathToJson = path.resolve(...args);
@@ -60,25 +31,12 @@ function chechTaskLibVersion () {
             warningMessage += `\nThe "${task}" task dependencies include "${VSTS_TASK_LIB}"; change it to "${AZURE_PIPELINES_TASK_LIB}" to resolve this issue.\n`;
             continue;
         }
-        if (!taskDependenciesNames.includes(AZURE_PIPELINES_TASK_LIB)) continue;
-        let packagesWarning = '';
-        for (const taskDependencyName of taskDependenciesNames) {
-            if (!commonNpmPackagesNames.includes(taskDependencyName) && taskDependencyName != AZURE_PIPELINES_TOOL_LIB) continue;
-            const commonNpmPackageDependencies = taskDependencies[taskDependencyName].dependencies;
-            if (!commonNpmPackageDependencies) continue;
-            const commonNpmPackageDependenciesNames = Object.keys(commonNpmPackageDependencies);
-            if (!commonNpmPackageDependenciesNames.includes(AZURE_PIPELINES_TASK_LIB)) continue;
-            packagesWarning += `   ---> ${taskDependencyName} :: ${commonNpmPackageDependencies[AZURE_PIPELINES_TASK_LIB].version}\n`;
-        }
-        if (packagesWarning) warningMessage += `\n${task} :: ${taskDependencies[AZURE_PIPELINES_TASK_LIB].version}\n` + packagesWarning;
     }
     if (warningMessage) {
         const isError = sourceBranch && sourceBranch != 'master';
         const messageType = isError ? 'error' : 'warning';
         console.log(`##vso[task.logissue type=${messageType}]"${AZURE_PIPELINES_TASK_LIB}" version is not the same in common npm packages and tasks.`);
-        console.log(template);
         console.log(warningMessage);
-        console.log(stepsToFix);
         if (isError) console.log('##vso[task.complete result=Failed]');
     }
 }
