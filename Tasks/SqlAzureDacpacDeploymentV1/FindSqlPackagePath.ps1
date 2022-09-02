cc8b0dc1c8219176460ecfe14c01f1518ef27284
Andras Zoltan <andras.zoltan@blackrainbow.com>
2022-09-02 15:46:03 +0100

    remove default version

--- Tasks/SqlAzureDacpacDeploymentV1/FindSqlPackagePath.ps1
+++ Tasks/SqlAzureDacpacDeploymentV1/FindSqlPackagePath.ps1
@@ -362,7 +362,6 @@ function Get-LatestVersionSqlPackageInDacDirectory([string] $dacParentDir)
 {
     if (Test-Path $dacParentDir)
     {
-        $dacVersion = "150"
         $dacFullPath = [System.IO.Path]::Combine($dacParentDir, "SqlPackage.exe")
 
         if(Test-Path $dacFullPath -pathtype leaf)
