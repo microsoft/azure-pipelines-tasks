3aee2077492ca08804cd6954947d1188b0110af4
Sergey Koryshev <sergey.koryshev@akvelon.com>
2022-09-07 19:10:25 +0200

    corrected naming of fake variables

--- common-npm-packages/codecoverage-tools/Tests/codecoverageconstantsTests.ts
+++ common-npm-packages/codecoverage-tools/Tests/codecoverageconstantsTests.ts
@@ -41,89 +41,89 @@ export function codecoverageconstantsTests() {
         });
 
         it('function jacocoGradleSingleModuleEnable should return correct configuration', () => {
-            const actual = codecoverageconstantsRewire.jacocoGradleSingleModuleEnable(fakeData.excludeFilterFakeStringified, fakeData.includeFilterFakeStringified, fakeData.classDirFake, fakeData.reportDirFake, false);
+            const actual = codecoverageconstantsRewire.jacocoGradleSingleModuleEnable(fakeData.excludeFilterStringified, fakeData.includeFilterStringified, fakeData.classDir, fakeData.reportDir, false);
             assert.strictEqual(actual, expectedResults.jacocoGradleSingleModule);
         });
 
         it('function jacocoGradleMultiModuleEnable should return correct configuration', () => {
-            const actual = codecoverageconstantsRewire.jacocoGradleMultiModuleEnable(fakeData.excludeFilterFakeStringified, fakeData.includeFilterFakeStringified, fakeData.classDirFake, fakeData.reportDirFake, false);
+            const actual = codecoverageconstantsRewire.jacocoGradleMultiModuleEnable(fakeData.excludeFilterStringified, fakeData.includeFilterStringified, fakeData.classDir, fakeData.reportDir, false);
             assert.strictEqual(actual, expectedResults.jacocoGradleMultiModule);
         });
 
         it('function coberturaGradleSingleModuleEnable should return correct configuration', () => {
-            const actual = codecoverageconstantsRewire.coberturaGradleSingleModuleEnable(fakeData.excludeFilterFakeStringified, fakeData.includeFilterFakeStringified, fakeData.classDirFake, fakeData.sourceDirFake, fakeData.reportDirFake);
+            const actual = codecoverageconstantsRewire.coberturaGradleSingleModuleEnable(fakeData.excludeFilterStringified, fakeData.includeFilterStringified, fakeData.classDir, fakeData.sourceDir, fakeData.reportDir);
             assert.strictEqual(actual, expectedResults.coberturaGradleSingleModule);
         });
 
         it('function coberturaGradleSingleModuleEnable should return correct configuration if source dir is not specified', () => {
-            const actual = codecoverageconstantsRewire.coberturaGradleSingleModuleEnable(fakeData.excludeFilterFakeStringified, fakeData.includeFilterFakeStringified, fakeData.classDirFake, null, fakeData.reportDirFake);
+            const actual = codecoverageconstantsRewire.coberturaGradleSingleModuleEnable(fakeData.excludeFilterStringified, fakeData.includeFilterStringified, fakeData.classDir, null, fakeData.reportDir);
             assert.strictEqual(actual, expectedResults.coberturaGradleSingleModuleWithNotSpecifiedSourceDir);
         });
 
         it('function coberturaGradleSingleModuleEnable should return correct configuration if class dir is not specified', () => {
-            const actual = codecoverageconstantsRewire.coberturaGradleSingleModuleEnable(fakeData.excludeFilterFakeStringified, fakeData.includeFilterFakeStringified, null, fakeData.sourceDirFake, fakeData.reportDirFake);
+            const actual = codecoverageconstantsRewire.coberturaGradleSingleModuleEnable(fakeData.excludeFilterStringified, fakeData.includeFilterStringified, null, fakeData.sourceDir, fakeData.reportDir);
             assert.strictEqual(actual, expectedResults.coberturaGradleSingleModuleWithNotSpecifiedClassDir);
         });
 
         it('function coberturaGradleMultiModuleEnable should return correct configuration', () => {
-            const actual = codecoverageconstantsRewire.coberturaGradleMultiModuleEnable(fakeData.excludeFilterFakeStringified, fakeData.includeFilterFakeStringified, fakeData.classDirFake, fakeData.sourceDirFake, fakeData.reportDirFake);
+            const actual = codecoverageconstantsRewire.coberturaGradleMultiModuleEnable(fakeData.excludeFilterStringified, fakeData.includeFilterStringified, fakeData.classDir, fakeData.sourceDir, fakeData.reportDir);
             assert.strictEqual(actual, expectedResults.coberturaGradleMultiModule);
         });
 
         it('function coberturaGradleMultiModuleEnable should return correct configuration if source dir is not specified', () => {
-            const actual = codecoverageconstantsRewire.coberturaGradleMultiModuleEnable(fakeData.excludeFilterFakeStringified, fakeData.includeFilterFakeStringified, fakeData.classDirFake, null, fakeData.reportDirFake);
+            const actual = codecoverageconstantsRewire.coberturaGradleMultiModuleEnable(fakeData.excludeFilterStringified, fakeData.includeFilterStringified, fakeData.classDir, null, fakeData.reportDir);
             assert.strictEqual(actual, expectedResults.coberturaGradleMultiModuleWithNotSpecifiedSourceDir);
         });
 
         it('function coberturaGradleMultiModuleEnable should return correct configuration if class dir is not specified', () => {
-            const actual = codecoverageconstantsRewire.coberturaGradleMultiModuleEnable(fakeData.excludeFilterFakeStringified, fakeData.includeFilterFakeStringified, null, fakeData.sourceDirFake, fakeData.reportDirFake);
+            const actual = codecoverageconstantsRewire.coberturaGradleMultiModuleEnable(fakeData.excludeFilterStringified, fakeData.includeFilterStringified, null, fakeData.sourceDir, fakeData.reportDir);
             assert.strictEqual(actual, expectedResults.coberturaGradleMultiModuleWithNotSpecifiedClassDir);
         });
 
         it('function jacocoMavenPluginEnable  should return correct configuration', () => {
-            const actual = codecoverageconstantsRewire.jacocoMavenPluginEnable(fakeData.includeFilterFake, fakeData.excludeFilterFake, fakeData.reportDirFake);
+            const actual = codecoverageconstantsRewire.jacocoMavenPluginEnable(fakeData.includeFilter, fakeData.excludeFilter, fakeData.reportDir);
             assert.deepStrictEqual(actual, expectedResults.jacocoMavenSingleProject);
         });
 
         it('function jacocoMavenMultiModuleReport should return correct configuration', () => {
-            const actual = codecoverageconstantsRewire.jacocoMavenMultiModuleReport(fakeData.reportDirFake, fakeData.sourceDirsFake, fakeData.classDirsFake, fakeData.includeFilterFakeStringified, fakeData.excludeFilterFakeStringified);
+            const actual = codecoverageconstantsRewire.jacocoMavenMultiModuleReport(fakeData.reportDir, fakeData.sourceDirs, fakeData.classDirs, fakeData.includeFilterStringified, fakeData.excludeFilterStringified);
             assert.strictEqual(actual, expectedResults.jacocoMavenMultiProject);
         });
 
         it('function coberturaMavenEnable should call \'util.convertXmlStringToJson\' once with correct parameters', async () => {
             var convertXmlStringToJsonStub = sinon.stub(util, "convertXmlStringToJson").callsFake();
-            codecoverageconstantsRewire.coberturaMavenEnable(fakeData.includeFilterFakeStringified, fakeData.excludeFilterFakeStringified, fakeData.aggregateFake);
+            codecoverageconstantsRewire.coberturaMavenEnable(fakeData.includeFilterStringified, fakeData.excludeFilterStringified, fakeData.aggregate);
             sinon.assert.calledOnceWithExactly(convertXmlStringToJsonStub, expectedResults.coberturaMavenEnableConfiguration);
             convertXmlStringToJsonStub.restore();
         });
 
         it('function jacocoAntReport should return correct configuration', () => {
-            const actual = codecoverageconstantsRewire.jacocoAntReport(fakeData.reportDirFake, fakeData.classDirFake, fakeData.sourceDirFake);
+            const actual = codecoverageconstantsRewire.jacocoAntReport(fakeData.reportDir, fakeData.classDir, fakeData.sourceDir);
             assert.strictEqual(actual, expectedResults.jacocoAntReportConfiguration);
         });
 
         it('function jacocoAntReport should return correct configuration', () => {
-            const actual = codecoverageconstantsRewire.jacocoAntReport(fakeData.reportDirFake, fakeData.classDirFake, fakeData.sourceDirFake);
+            const actual = codecoverageconstantsRewire.jacocoAntReport(fakeData.reportDir, fakeData.classDir, fakeData.sourceDir);
             assert.strictEqual(actual, expectedResults.jacocoAntReportConfiguration);
         });
 
         it('function jacocoAntCoverageEnable should return correct configuration', () => {
-            const actual = codecoverageconstantsRewire.jacocoAntCoverageEnable(fakeData.reportDirFake);
+            const actual = codecoverageconstantsRewire.jacocoAntCoverageEnable(fakeData.reportDir);
             assert.deepStrictEqual(actual, expectedResults.jacocoAntCoverageEnableConfiguration);
         });
 
         it('function coberturaAntReport should return correct configuration', () => {
-            const actual = codecoverageconstantsRewire.coberturaAntReport(fakeData.sourceDirFake, fakeData.reportDirFake);
+            const actual = codecoverageconstantsRewire.coberturaAntReport(fakeData.sourceDir, fakeData.reportDir);
             assert.deepStrictEqual(actual, expectedResults.coberturaAntReportConfiguration);
         });
 
         it('function coberturaAntInstrumentedClasses should return correct configuration', () => {
-            const actual = codecoverageconstantsRewire.coberturaAntInstrumentedClasses(fakeData.baseDir, fakeData.reportDirFake, fakeData.classDirFake);
+            const actual = codecoverageconstantsRewire.coberturaAntInstrumentedClasses(fakeData.baseDir, fakeData.reportDir, fakeData.classDir);
             assert.deepStrictEqual(actual, expectedResults.coberturaAntInstrumentedClassesConfiguration);
         });
 
         it('function coberturaAntProperties should return correct configuration', () => {
-            const actual = codecoverageconstantsRewire.coberturaAntProperties(fakeData.reportDirFake, fakeData.baseDir);
+            const actual = codecoverageconstantsRewire.coberturaAntProperties(fakeData.reportDir, fakeData.baseDir);
             assert.deepStrictEqual(actual, expectedResults.coberturaAntPropertiesConfiguration);
         });
     });
