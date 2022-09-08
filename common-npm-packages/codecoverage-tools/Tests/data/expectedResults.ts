d39d1b281113ec7373758e8d359ebd541f503daf
Sergey Koryshev <sergey.koryshev@akvelon.com>
2022-09-08 14:03:48 +0200

    improved code coverage

--- common-npm-packages/codecoverage-tools/Tests/data/expectedResults.ts
+++ common-npm-packages/codecoverage-tools/Tests/data/expectedResults.ts
@@ -437,7 +437,7 @@ export const correctFilters = {
     excludeFilter: ":**/*$ViewInjector:**/*$ViewBinder:**/Manifest"
 }
 
-export const sortedStringArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+export const sortedStringArray = ['a', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 export const emptyObjectWithAddedProperty = {
     someProperty: 108
