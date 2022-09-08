92b9a1f9abe5328320f481acd0815ca6500dff0d
Sergey Koryshev <sergey.koryshev@akvelon.com>
2022-09-08 10:33:34 +0200

    made tests for script "codecoverageenabler" more isolated

--- common-npm-packages/codecoverage-tools/Tests/codecoverageenablerTests.ts
+++ common-npm-packages/codecoverage-tools/Tests/codecoverageenablerTests.ts
@@ -1,40 +1,56 @@
 import * as assert from 'assert';
 import * as rewire from 'rewire';
-import { Promise } from 'q';
+import * as sinon from 'sinon';
+import * as tl from 'azure-pipelines-task-lib/task';
+import * as util from "../utilities";
 
-import * as fakeData from './data/fakeData';
 import * as expectedResults from './data/expectedResults';
-
-import * as codecoverageenabler from '../codecoverageenabler';
-
-
-class CodeCoverageEnablerTestClass extends codecoverageenabler.CodeCoverageEnabler {
-    enableCodeCoverage(ccProps: { [name: string]: string; }): Promise<boolean> {
-        throw new Error('Method not implemented.');
-    }
-}
+import * as fakeData from './data/fakeData';
 
 export function codecoverageenablerTests() {
+    const debugStub = sinon.stub(tl, "debug").callsFake();
+    const isNullOrWhitespaceStub = sinon.stub(util, "isNullOrWhitespace").callsFake();
+
+    after(() => {
+        debugStub.restore();
+        isNullOrWhitespaceStub.restore();
+    });
+
     describe('class CodeCoverageEnabler', () => {
-        const codeCoverageEnablerTestClass = new CodeCoverageEnablerTestClass();
-        const codeCoverageEnablerTestClassRewired = rewire(codeCoverageEnablerTestClass);
-        const extractFilters = codeCoverageEnablerTestClassRewired.__get__('extractFilters');
+        const codecoverageenablerRewired = rewire('../codecoverageenabler');
+        const codeCoverageEnablerClass = codecoverageenablerRewired.__get__('CodeCoverageEnabler')
+        const codeCoverageEnablerTestClass = new codeCoverageEnablerClass();
+
+        afterEach(() => {
+            isNullOrWhitespaceStub.reset();
+        })
 
         it('function extractFilters should return empty filters when specified empty class filter', () => {
-            const actual = extractFilters("");
-            assert.deepStrictEqual(actual, expectedResults.emptyFilters)
+            isNullOrWhitespaceStub.returns(true);
+            const actual = codeCoverageEnablerTestClass.extractFilters(fakeData.classFilter);
+            assert.deepStrictEqual(actual, expectedResults.emptyFilters);
         });
 
-        it('function extractFilters should throw exception if invalid class filter specified #1', () => {
-            assert.throws(extractFilters(fakeData.invalidClassFilter1), Error);
+        it('function extractFilters should throw exception if class filter contains at least one empty filter', () => {
+            isNullOrWhitespaceStub
+                .onCall(0).returns(false)
+                .returns(true);
+            assert.throws(() => codeCoverageEnablerTestClass.extractFilters(fakeData.classFilter), Error);
         });
 
-        it('function extractFilters should throw exception if invalid class filter specified #2', () => {
-            assert.throws(extractFilters(fakeData.invalidClassFilter2), Error);
+        it('function extractFilters should throw exception if class filter contains at least one filter with length < 2', () => {
+            isNullOrWhitespaceStub.returns(false);
+            assert.throws(() => codeCoverageEnablerTestClass.extractFilters(fakeData.invalidClassFilter1), Error);
+        });
+
+        it('function extractFilters should throw exception if class filter contains at least one unsupported prefix', () => {
+            isNullOrWhitespaceStub.returns(false);
+            assert.throws(() => codeCoverageEnablerTestClass.extractFilters(fakeData.invalidClassFilter2), Error);
         });
 
         it('function extractFilters should return correct class filters', () => {
-            const actual = extractFilters(fakeData.correctClassFilter);
+            isNullOrWhitespaceStub.returns(false);
+            const actual = codeCoverageEnablerTestClass.extractFilters(fakeData.classFilter);
             assert.deepStrictEqual(actual, expectedResults.correctFilters);
         });
     });
