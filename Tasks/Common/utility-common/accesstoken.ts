3bfb93c10752a2078970e3758480639b6f46391a
v-mohithgc <97882857+v-mohithgc@users.noreply.github.com>
2022-09-19 17:47:07 +0530

    npm audit fix

--- Tasks/Common/utility-common/accesstoken.ts
+++ Tasks/Common/utility-common/accesstoken.ts
@@ -1,4 +1,4 @@
-import tl = require('vsts-task-lib/task');
+import tl = require('azure-pipelines-task-lib/task');
 
 export function getSystemAccessToken(): string {
     tl.debug('Getting credentials for account feeds');
