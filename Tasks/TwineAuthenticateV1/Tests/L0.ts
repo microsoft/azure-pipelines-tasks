c49b00c5175dcd922f484e0c5c96c2486f82645a
bothzoli <bothzoli@gmail.com>
2022-10-21 22:53:25 +0200

    Fix Python Twine Upload Authenticate Task (#16182)
    * Fix Python Twine Upload Authenticate Task
    
    The Python Twine Upload Authenticate task will fail on feeds that contain a dot in their
    name. The dot in the feed name will be escaped in the generated .pypirc
    file in the corresponding section headers (e.g. [Feed\.Name]).
    
    The issue is due to how the `ini` npm package encode works. Thus
    providing a custom `toString` implementation that convert repositories
    to their .pypirc representation fixes the issue.
    
    Resolves: #14220
    
    * Bump version number
    
    Co-authored-by: Jonathan Myers <11822817+jmyersmsft@users.noreply.github.com>

--- Tasks/TwineAuthenticateV1/Tests/L0.ts
+++ Tasks/TwineAuthenticateV1/Tests/L0.ts
@@ -1,6 +1,5 @@
 import * as path from 'path';
 import * as assert from 'assert';
-import * as ini from 'ini';
 import * as fs from 'fs';
 import * as ttm from 'azure-pipelines-task-lib/mock-test';
 import * as tl from "azure-pipelines-task-lib/task";
@@ -25,21 +24,48 @@ describe('Twine Authenticate V1 Suite', function () {
         assert(tr.invokedToolCount == 0, 'no tool should be invoked.');
         assert(tr.succeeded, 'should have succeeded');
         assert.strictEqual(tr.errorIssues.length, 0, "should have no errors");
-        let fileContent = ini.parse(fs.readFileSync(tempDir + path.sep + ".pypirc", "utf-8"));
 
-        assert.strictEqual(fileContent["distutils"]["index-servers"], "TestFeed", "Test Feed should be added to auth list.");
+        let fileContent = fs.readFileSync(tempDir + path.sep + ".pypirc", "utf-8");
 
-        assert.strictEqual(fileContent["TestFeed"]["repository"],
-        "https://vsts/packagesource/TestFeed",
-        "Test Feed repository should be correct.");
+        let lines = fileContent.split(/\r?\n/);
 
-        assert.strictEqual(fileContent["TestFeed"]["username"],
-        "build",
-        "Default username should be correct.");
+        assert.strictEqual(lines[0], "[distutils]");
+        assert.strictEqual(lines[1], "index-servers=TestFeed",
+            "Test Feed should be added to auth list.");
+        assert.strictEqual(lines[2], "[TestFeed]");
+        assert.strictEqual(lines[3], "repository=https://vsts/packagesource/TestFeed",
+            "Test Feed repository should be correct.");
+        assert.strictEqual(lines[4], "username=build",
+            "Default username should be correct.");
+        assert.strictEqual(lines[5], "password=token",
+            "Default password from environment variable should be correct.");
 
-        assert.strictEqual(fileContent["TestFeed"]["password"],
-        "token",
-        "Default password from environment variable should be correct.");
+        done();
+    });
+
+    it('sets authentication for current organization feed', (done: MochaDone) => {
+        this.timeout(1000);
+        let tp = path.join(__dirname, './setAuthInternalFeedWithDot.js')
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run();
+        assert(tr.invokedToolCount == 0, 'no tool should be invoked.');
+        assert(tr.succeeded, 'should have succeeded');
+        assert.strictEqual(tr.errorIssues.length, 0, "should have no errors");
+        let fileContent = fs.readFileSync(tempDir + path.sep + ".pypirc", "utf-8");
+
+        let lines = fileContent.split(/\r?\n/);
+
+        assert.strictEqual(lines[0], "[distutils]");
+        assert.strictEqual(lines[1], "index-servers=Test.Feed",
+            "Test Feed should be added to auth list.");
+        assert.strictEqual(lines[2], "[Test.Feed]");
+        assert.strictEqual(lines[3], "repository=https://vsts/packagesource/Test.Feed",
+            "Test Feed repository should be correct.");
+        assert.strictEqual(lines[4], "username=build",
+            "Default username should be correct.");
+        assert.strictEqual(lines[5], "password=token",
+            "Default password from environment variable should be correct.");
 
         done();
     });
