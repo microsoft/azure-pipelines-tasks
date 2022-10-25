c941031cc99d65baf65c5b3bb91ec4507fde1182
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2022-10-25 14:55:35 +0200

    Upgrade AppCenterDistributeV2 to NodeJS 16
    - Increased test timeout because tests failed with 6000ms

--- Tasks/AppCenterDistributeV2/Tests/L0.ts
+++ Tasks/AppCenterDistributeV2/Tests/L0.ts
@@ -7,7 +7,7 @@ import * as assert from 'assert';
 import * as ttm from 'azure-pipelines-task-lib/mock-test';
 
 describe('AppCenterDistribute L0 Suite', function () {
-    const timeout = 6000;
+    const timeout = 8000;
 
     before(() => {
         //Enable this for output
