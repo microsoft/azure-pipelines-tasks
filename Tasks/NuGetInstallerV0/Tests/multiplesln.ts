41f59c43e1a11c4f1509051f37b10234e461175f
Pranshu Negi <pranshunegi@microsoft.com>
2022-11-16 15:01:49 +0100

    vulnerability fix for nugetinstallerv0

--- Tasks/NuGetInstallerV0/Tests/multiplesln.ts
+++ Tasks/NuGetInstallerV0/Tests/multiplesln.ts
@@ -2,7 +2,7 @@ import ma = require('azure-pipelines-task-lib/mock-answer');
 import tmrm = require('azure-pipelines-task-lib/mock-run');
 import path = require('path');
 import util = require('./NugetMockHelper');
-import nMockHelper = require('packaging-common/Tests/NuGetMockHelper');
+import nMockHelper = require('azure-pipelines-tasks-packaging-common/Tests/NuGetMockHelper');
 
 let taskPath = path.join(__dirname, '..', 'nugetinstaller.js');
 let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
