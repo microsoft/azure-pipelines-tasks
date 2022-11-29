2ee1948afba156101951ba93bed952d65ff2ad82
zhenghao104 <zhentan@microsoft.com>
2022-11-29 13:17:42 -0800

    Verify cargo registry is from artifacts

--- Tasks/CargoAuthenticateV0/cargoauthenticatemain.ts
+++ Tasks/CargoAuthenticateV0/cargoauthenticatemain.ts
@@ -3,23 +3,23 @@ import * as tl from 'azure-pipelines-task-lib/task';
 import * as toml from 'toml';
 import * as fs from 'fs';
 import * as constants from './constants';
+import * as util from 'azure-pipelines-tasks-packaging-common/util';
+import * as pkgLocationUtils from 'azure-pipelines-tasks-packaging-common/locationUtilities';
+import * as url from 'url';
 
 async function main(): Promise<void> {
     tl.setResourcePath(path.join(__dirname, 'task.json'));
 
     try {
         let configtoml = tl.getInput(constants.CargoAuthenticateTaskInput.WorkingFile);
-        if (!(configtoml.endsWith('.toml'))) {
-            throw new Error(tl.loc('ConfigTomlNotToml', configtoml));
-        }
-        else if (!tl.exist(configtoml)) {
+        if (!tl.exist(configtoml)) {
             throw new Error(tl.loc('ConfigTomlDoesNotExist', configtoml));
         }
         else {
             console.log(tl.loc("AuthenticatingThisConfigToml", configtoml));
         }
         
-        // These two format will work
+        // These two formats will work
         // [registries]
         // zhentan-test = { index = "sparse+https://pkgs.dev.azure.com/codesharing-SU0/zhentan-test/_packaging/zhentan-test/Cargo/index/" }
         // [registries.zhentan-test1]
@@ -30,13 +30,32 @@ async function main(): Promise<void> {
         {
             throw new Error("config.toml must contains registries field");
         }
-        var registries = Object.keys(result.registries);
-        var token = `Bearer ${tl.getVariable('System.AccessToken')}`;
 
-        for(let registry of registries) {
-            var tokenName = `CARGO_REGISTRIES_${registry.toLocaleUpperCase().replace("-", "_")}_TOKEN`;
-            tl.debug(tl.loc('AddingAuthRegistry', registry, tokenName));
-            tl.setVariable(tokenName, token);
+        let packagingLocation: pkgLocationUtils.PackagingLocation;
+        try {
+            packagingLocation = await pkgLocationUtils.getPackagingUris(pkgLocationUtils.ProtocolType.Npm);
+        } catch (error) {
+            tl.debug('Unable to get packaging URIs');
+            util.logError(error);
+            throw error;
+        }
+
+        const collectionHosts = packagingLocation.PackagingUris.map((pkgUrl: string) => {
+            const parsedUrl = url.parse(pkgUrl);
+            if (parsedUrl && parsedUrl.host) {
+                return parsedUrl.host.toLowerCase();
+            }
+            return undefined;
+        });
+
+        const token = `Bearer ${tl.getVariable('System.AccessToken')}`;
+        for (let registry of Object.keys(result.registries)) {
+            const registryUrl = url.parse(result.registries[registry].index);
+            if(registryUrl && registryUrl.host && collectionHosts.indexOf(registryUrl.host.toLowerCase()) >= 0) {
+                const tokenName = `CARGO_REGISTRIES_${registry.toLocaleUpperCase().replace("-", "_")}_TOKEN`;
+                tl.debug(tl.loc('AddingAuthRegistry', registry, tokenName));
+                tl.setVariable(tokenName, token);
+            }   
         }
 
         //External endpoints
