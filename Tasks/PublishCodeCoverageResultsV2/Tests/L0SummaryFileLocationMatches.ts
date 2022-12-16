90ce3c0c48b54f2d1ec0b318b5e3590f7e51c677
Vinayak Joshi <vinayakjoshi@microsoft.com>
2022-12-16 15:36:50 +0530

    Using Publish Code coverage V2

---
+++ Tasks/PublishCodeCoverageResultsV2/Tests/L0SummaryFileLocationMatches.ts
@@ -0,0 +1,11 @@
+import path = require('path');
+import { TaskMockRunner } from 'azure-pipelines-task-lib/mock-run';
+import * as answers from './answers';
+
+const taskPath = path.join(__dirname, '..', 'publishcodecoverageresults.js');
+const tr: TaskMockRunner = new TaskMockRunner(taskPath);
+
+tr.setInput('summaryFileLocation', 'C:\Users\vinayakjoshi\Downloads\coverage.cobertura (4) (1).xml');
+tr.setAnswers(answers.defaultAnswers);
+
+tr.run();
