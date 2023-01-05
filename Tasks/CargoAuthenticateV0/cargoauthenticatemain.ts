08617422f30c520ff00daf1ef731be0de3c0f44c
zhenghao104 <zhentan@microsoft.com>
2023-01-05 13:00:40 -0800

    Cargo AuthenticateV0 Service Connection

--- Tasks/CargoAuthenticateV0/cargoauthenticatemain.ts
+++ Tasks/CargoAuthenticateV0/cargoauthenticatemain.ts
@@ -6,6 +6,9 @@ import * as constants from './constants';
 import * as util from 'azure-pipelines-tasks-packaging-common/util';
 import * as pkgLocationUtils from 'azure-pipelines-tasks-packaging-common/locationUtilities';
 import * as url from 'url';
+import * as base64 from 'base-64';
+import * as utf8 from 'utf8';
+import { getPackagingServiceConnections, ServiceConnectionAuthType, UsernamePasswordServiceConnection, TokenServiceConnection } from "azure-pipelines-tasks-artifacts-common/serviceConnectionUtils";
 
 async function main(): Promise<void> {
     tl.setResourcePath(path.join(__dirname, 'task.json'));
@@ -49,16 +52,39 @@ async function main(): Promise<void> {
         });
 
         const localAccesstoken = `Bearer ${tl.getVariable('System.AccessToken')}`;
+        const serviceConnections = getPackagingServiceConnections('cargoServiceConnections');
+
         for (let registry of Object.keys(result.registries)) {
             const registryUrl = url.parse(result.registries[registry].index);
-            if(registryUrl && registryUrl.host && collectionHosts.indexOf(registryUrl.host.toLowerCase()) >= 0) {
-                const tokenName = `CARGO_REGISTRIES_${registry.toLocaleUpperCase().replace("-", "_")}_TOKEN`;
-                tl.debug(tl.loc('AddingAuthRegistry', registry, tokenName));
-                tl.setVariable(tokenName, localAccesstoken);
+            const tokenName = `CARGO_REGISTRIES_${registry.toLocaleUpperCase().replace("-", "_")}_TOKEN`;
+            if (registryUrl && registryUrl.host && collectionHosts.indexOf(registryUrl.host.toLowerCase()) >= 0) {
+                let currentRegistry : string;
+                if (serviceConnections && serviceConnections.length > 0) {
+                    for (let serviceConnection of serviceConnections) {  
+                        if (util.toNerfDart(serviceConnection.packageSource.uri + "/") == util.toNerfDart(result.registries[registry].index.replace("sparse+", ""))) {
+                            let serviceConnectionAccessToken : string;
+                            switch (serviceConnection.authType) {
+                                case (ServiceConnectionAuthType.Token):
+                                    const tokenAuthInfo = serviceConnection as TokenServiceConnection;
+                                    serviceConnectionAccessToken = `Basic ${base64.encode(utf8.encode(`AzureDevOps:${tokenAuthInfo.token}`))}`;
+                                    tl.debug(`Detected token credentials for '${serviceConnection.packageSource.uri}'`);
+                                    break;
+                                default:
+                                    throw Error(tl.loc('Error_InvalidServiceConnection', serviceConnection.packageSource.uri));
+                            }
+                            tl.debug(tl.loc('AddingAuthExternalRegistry', registry, tokenName));
+                            currentRegistry = registry;
+                            tl.setVariable(tokenName, serviceConnectionAccessToken);
+                            break;
+                        }
+                    }
+                }
+                if (!currentRegistry) {
+                    tl.debug(tl.loc('AddingAuthRegistry', registry, tokenName));
+                    tl.setVariable(tokenName, localAccesstoken);
+                }  
             }   
-        }
-
-        //External endpoints
+        } 
     }
 
     catch(error) {
