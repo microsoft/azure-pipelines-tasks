4ed07f8c79ad90c12f25c6e227146d7d61cc3853
Pavlo Andriiesh <pavlo.andriiesh@gmail.com>
2023-01-18 16:50:30 +0100

    b

--- ci/testing/get_recently_updated_taskname.js
+++ ci/testing/get_recently_updated_taskname.js
@@ -1,1 +1,14 @@
-console.log('WIP');
+const { spawn } = require('node:child_process');
+const ls = spawn('ls', ['-lh', '/usr']);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
