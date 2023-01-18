5e55d6bf35fb735616e92cc07c1498d7ded12669
Pavlo Andriiesh <pavlo.andriiesh@gmail.com>
2023-01-18 18:08:24 +0100

    b

--- ci/testing/get_recently_updated_taskname.js
+++ ci/testing/get_recently_updated_taskname.js
@@ -1,16 +1,39 @@
-const { spawn } = require('node:child_process');
-const ls = spawn('git', ['diff', '--name-only', 'master']);
-console.log(`---------------------------------------`);
+const { spawn, exec } = require('node:child_process');
+const git = spawn('git', ['diff', '--name-only', 'master'], {cwd: '../..', shell: true});
 
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
+
+let gitDiffOutput = "";
+
+git.stdout.setEncoding('utf8');
+git.stdout.on('data', (data) => {
+  gitDiffOutput += data.toString();
 });
 
-ls.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
+
+git.stderr.on('data', (data) => {
+  console.error(`git stderr: ${data}`);
 });
 
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-  console.log(`---------------------------------------`);
+git.on('close', (code) => {
+  console.log(`git child process exited with code ${code}`);
+  console.log('Full output of script: ', gitDiffOutput);
+
+  console.log('--------------------------------------------')
+  console.log(gitDiffOutput);
+
+  getTaskNamesFromOutput(gitDiffOutput);
 });
+
+
+function getTaskNamesFromOutput(output) {
+  const taskNames = new Set();
+  const lines = output.split(/\r?\n/).filter(s => s);
+  const taskLines = lines.filter(line => line.startsWith('Tasks/'));
+  taskLines.forEach(pathToFile => {
+    let taskName = pathToFile.slice(6); // remove Tasks/ prefix
+    taskName = taskName.slice(0, taskName.indexOf('/')); // remove path after task name
+    console.log('taskName', taskName)
+  })
+
+  console.log(taskLines);
+}
