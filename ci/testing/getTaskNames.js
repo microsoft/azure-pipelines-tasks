5b7bce509b849037edf5550794ae9bff645a7319
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-01-24 17:35:12 +0100

    b

--- ci/testing/getTaskNames.js
+++ ci/testing/getTaskNames.js
@@ -3,44 +3,44 @@ const { Octokit } = require("@octokit/core");
 
 console.log(process.argv);
 
-const BuildSourceBranch = process.argv[2];
-const BuildSourceBranchName = process.argv[3];
-const githubPAT = process.argv[4];
-const octokit = new Octokit({ auth: githubPAT });
+// const BuildSourceBranch = process.argv[2];
+// const BuildSourceBranchName = process.argv[3];
+// const githubPAT = process.argv[4];
+// const octokit = new Octokit({ auth: githubPAT });
 
-console.log('BuildSourceBranch', BuildSourceBranch, BuildSourceBranchName);
+// console.log('BuildSourceBranch', BuildSourceBranch, BuildSourceBranchName);
 
-octokit.request('GET /repos/{owner}/{repo}/compare/{basehead}{?page,per_page}', {
-  owner: 'PavloAndriiesh',
-  repo: 'azure-pipelines-tasks',
-  basehead: 'master...develop'
-//  basehead: 'microsoft:master...develop'
-}).then(res => {
-  const fileNames = res.data.files.map(props => props.filename);
-  const taskNames = getTaskNames(fileNames);
-  const tasksMeta = fillTaskMeta(taskNames);
+// octokit.request('GET /repos/{owner}/{repo}/compare/{basehead}{?page,per_page}', {
+//   owner: 'PavloAndriiesh',
+//   repo: 'azure-pipelines-tasks',
+//   basehead: 'master...develop'
+// //  basehead: 'microsoft:master...develop'
+// }).then(res => {
+//   const fileNames = res.data.files.map(props => props.filename);
+//   const taskNames = getTaskNames(fileNames);
+//   const tasksMeta = fillTaskMeta(taskNames);
 
-  console.log(JSON.stringify(tasksMeta));
-}).catch(err => {
-  console.error(err);
-})
+//   console.log(JSON.stringify(tasksMeta));
+// }).catch(err => {
+//   console.error(err);
+// })
 
-function getTaskNames(files) {
-  const taskNames = new Set();
+// function getTaskNames(files) {
+//   const taskNames = new Set();
 
-  files.filter(filePath => filePath.startsWith('Tasks/')).forEach(filePath => {
-    taskNames.add(filePath.split('/')[1]);
-  });
+//   files.filter(filePath => filePath.startsWith('Tasks/')).forEach(filePath => {
+//     taskNames.add(filePath.split('/')[1]);
+//   });
 
-  return [...taskNames];
-}
+//   return [...taskNames];
+// }
 
-function fillTaskMeta(taskNames) {
-  return taskNames.map(name => {
-    const filePath = 'Tasks/' + name + '/task.json';
-    const rawdata = fs.readFileSync(filePath);
-    const taskJsonFile = JSON.parse(rawdata);
+// function fillTaskMeta(taskNames) {
+//   return taskNames.map(name => {
+//     const filePath = 'Tasks/' + name + '/task.json';
+//     const rawdata = fs.readFileSync(filePath);
+//     const taskJsonFile = JSON.parse(rawdata);
 
-    return {[name]: taskJsonFile.id}
-  })
-}
+//     return {[name]: taskJsonFile.id}
+//   })
+// }
