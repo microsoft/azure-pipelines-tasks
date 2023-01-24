1b4599b65d6806ceee6de03d746ab0be852ff2b5
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-01-24 10:46:36 +0100

    b

--- ci/testing/getTaskIds.js
+++ ci/testing/getTaskIds.js
@@ -2,16 +2,18 @@
 const fs = require('fs');
 
 const files = process.argv.slice(2);
-const tasks = getTaskIds(files);
+const tasks = getTaskIds(files.slice(0,2));
 
 console.log(tasks);
 
 function getTaskIds(files) {
   const taskJsonFiles = files.filter(line => line.startsWith('Tasks/') && line.split('/').length === 3 && line.endsWith('/task.json'));
+  console.log(taskJsonFiles);
 
   return taskJsonFiles.map(path => {
     const rawdata = fs.readFileSync(path);
     const taskJsonFile = JSON.parse(rawdata);
+    console.log(taskJsonFile);
 
     return {name: taskJsonFile.name, id: taskJsonFile.id, path}
   })
