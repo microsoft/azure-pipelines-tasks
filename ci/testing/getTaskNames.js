4a1a644c6286820c9c67e09a67de1bddb3ef389e
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-01-24 18:12:29 +0100

    b

--- ci/testing/getTaskNames.js
+++ ci/testing/getTaskNames.js
@@ -1,44 +1,51 @@
 const fs = require('fs');
 const { Octokit } = require("@octokit/core");
 
-console.log('process.argv', process.argv);
+const commitHashLength = 40;
 
-// const githubPAT = process.argv[2];
-// const SourceVersionMessage = process.argv[3];
-// const octokit = new Octokit({ auth: githubPAT });
-// const source = SourceVersionMessage.split(' ')[1];
-// const target = SourceVersionMessage.split(' ')[3];
+const githubPAT = process.argv[2];
+const source =  process.argv[4];
+const target =  process.argv[6];
 
-// octokit.request('GET /repos/{owner}/{repo}/compare/{basehead}{?page,per_page}', {
-//   owner: 'PavloAndriiesh', // TODO: replace to 'microsoft'
-//   repo: 'azure-pipelines-tasks',
-//   basehead: target + '...' + source
-// }).then(res => {
-//   const fileNames = res.data.files.map(props => props.filename);
-//   const taskNames = getTaskNames(fileNames);
-//   const tasksMeta = fillTaskMeta(taskNames);
+if (!githubPAT) {
+  throw new Error('Github PAT is missing');
+} else if (!source || source.length !== commitHashLength || !target || target.length !== commitHashLength) { 
+  throw new Error('Build.SourceVersionMessage is invalid. Expected similar to "Merge 03030baaa23bad9c201711375827a80f36120fc7 into 04aa704021853c2a79620ce544b0ade5252d34c7"');
+}
 
-//   console.log(JSON.stringify(tasksMeta));
-// }).catch(err => {
-//   console.error(err);
-// })
+const octokit = new Octokit({ auth: githubPAT });
 
-// function getTaskNames(files) {
-//   const taskNames = new Set();
 
-//   files.filter(filePath => filePath.startsWith('Tasks/')).forEach(filePath => {
-//     taskNames.add(filePath.split('/')[1]);
-//   });
+octokit.request('GET /repos/{owner}/{repo}/compare/{basehead}{?page,per_page}', {
+  owner: 'PavloAndriiesh', // TODO: replace to 'microsoft'
+  repo: 'azure-pipelines-tasks',
+  basehead: target + '...' + source
+}).then(res => {
+  const fileNames = res.data.files.map(props => props.filename);
+  const taskNames = getTaskNames(fileNames);
+  const tasksMeta = fillTaskMeta(taskNames);
 
-//   return [...taskNames];
-// }
+  console.log(JSON.stringify(tasksMeta));
+}).catch(err => {
+  console.error(err);
+})
 
-// function fillTaskMeta(taskNames) {
-//   return taskNames.map(name => {
-//     const filePath = 'Tasks/' + name + '/task.json';
-//     const rawdata = fs.readFileSync(filePath);
-//     const taskJsonFile = JSON.parse(rawdata);
+function getTaskNames(files) {
+  const taskNames = new Set();
 
-//     return {[name]: taskJsonFile.id}
-//   })
-// }
+  files.filter(filePath => filePath.startsWith('Tasks/')).forEach(filePath => {
+    taskNames.add(filePath.split('/')[1]);
+  });
+
+  return [...taskNames];
+}
+
+function fillTaskMeta(taskNames) {
+  return taskNames.map(name => {
+    const filePath = 'Tasks/' + name + '/task.json';
+    const rawdata = fs.readFileSync(filePath);
+    const taskJsonFile = JSON.parse(rawdata);
+
+    return {[name]: taskJsonFile.id}
+  })
+}
