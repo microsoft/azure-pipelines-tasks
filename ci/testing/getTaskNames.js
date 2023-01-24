7653db4e1318b15b2757224ea7d69157cdc593ef
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-01-24 11:37:02 +0100

    b

--- ci/testing/getTaskNames.js
+++ ci/testing/getTaskNames.js
@@ -1,8 +1,9 @@
-const githubPAT = process.argv[2];
-
+const fs = require('fs');
 const { Octokit } = require("@octokit/core");
 
+const githubPAT = process.argv[2];
 const octokit = new Octokit({ auth: githubPAT });
+
 
 octokit.request('GET /repos/{owner}/{repo}/compare/{basehead}{?page,per_page}', {
   owner: 'PavloAndriiesh',
@@ -11,26 +12,23 @@ octokit.request('GET /repos/{owner}/{repo}/compare/{basehead}{?page,per_page}', 
 //  basehead: 'microsoft:master...develop'
 }).then(res => {
   const filenames = res.data.files.map(props => props.filename);
-  const taskNames = getTaskNamesFromOutput(filenames)
-  const taskNamesAndIds = fillTaskIds(taskNames);
+  const tasksJsonFiles = filterTaskJsonFiles(filenames)
+  const tasksMeta = fillTaskMeta(tasksJsonFiles);
 
-  console.log(taskNamesAndIds);
+  console.log(JSON.stringify(tasksMeta));
 }).catch(err => {
   console.error(err);
 })
 
-function getTaskNamesFromOutput(files) {
-  const taskNames = new Set();
-  const taskFiles = files.filter(line => line.startsWith('Tasks/'));
-  taskFiles.forEach(pathToFile => {
-    let taskName = pathToFile.slice(6); // remove Tasks/ prefix
-    taskName = taskName.slice(0, taskName.indexOf('/')); // remove path after task name
-    taskNames.add(taskName);
-  })
-
-  return [...taskNames];
+function filterTaskJsonFiles(files) {
+  return files.filter(line => line.startsWith('Tasks/') && line.endsWith('/task.json') && line.split('/').length === 3);
 }
 
-function fillTaskIds(taskNames) {
-  return taskNames.map(taskName => ({name: taskName, id: 'id_placeholder'}))
+function fillTaskMeta(taskJsonFiles) {
+  return taskJsonFiles.map(path => {
+    const rawdata = fs.readFileSync(path);
+    const taskJsonFile = JSON.parse(rawdata);
+
+    return {name: taskJsonFile.name, id: taskJsonFile.id, folderName: path.split('/')[1]}
+  })
 }
