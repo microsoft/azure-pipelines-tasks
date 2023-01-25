c407ec96fcd8be6944f4bff53c5153b32dd146d8
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-01-25 12:50:35 +0100

    Test PR

--- ci/testing/getTaskNames.js
+++ ci/testing/getTaskNames.js
@@ -3,53 +3,52 @@ const { Octokit } = require("@octokit/core");
 
 const commitHashLength = 40;
 
-console.log('env vars', process.env);
-// const githubPAT = process.argv[2];
-// const source =  process.argv[4];
-// const target =  process.argv[6];
+const githubPAT = process.argv[2];
+const source =  process.argv[4];
+const target =  process.argv[6];
 
-// if(process.argv.length < 7) {
-//   throw new Error('Missing github PAT or Build.SourceVersionMessage arguments', process.argv);
-// } else if (!githubPAT) {
-//   throw new Error('Github PAT is missing');
-// } else if (!source || source.length !== commitHashLength || !target || target.length !== commitHashLength) { 
-//   throw new Error('Build.SourceVersionMessage is invalid. Expected similar to "Merge 03030baaa23bad9c201711375827a80f36120fc7 into 04aa704021853c2a79620ce544b0ade5252d34c7"');
-// }
+if(process.argv.length < 7) {
+  throw new Error('Missing github PAT or Build.SourceVersionMessage arguments', process.argv);
+} else if (!githubPAT) {
+  throw new Error('Github PAT is missing');
+} else if (!source || source.length !== commitHashLength || !target || target.length !== commitHashLength) { 
+  throw new Error('Build.SourceVersionMessage is invalid. Expected similar to "Merge 03030baaa23bad9c201711375827a80f36120fc7 into 04aa704021853c2a79620ce544b0ade5252d34c7"');
+}
 
-// const octokit = new Octokit({ auth: githubPAT });
+const octokit = new Octokit();
 
-// octokit.request('GET /repos/{owner}/{repo}/compare/{basehead}{?page,per_page}', {
-//   owner: 'PavloAndriiesh', // TODO: replace to 'microsoft'
-//   repo: 'azure-pipelines-tasks',
-//   basehead: target + '...' + source
-// }).then(res => {
-//   const fileNames = res.data.files.map(props => props.filename);
-//   const taskNames = getTaskNames(fileNames);
-//   const tasksMeta = fillTaskMeta(taskNames);
+octokit.request('GET /repos/{owner}/{repo}/compare/{basehead}{?page,per_page}', {
+  owner: 'PavloAndriiesh', // TODO: replace to 'microsoft'
+  repo: 'azure-pipelines-tasks',
+  basehead: target + '...' + source
+}).then(res => {
+  const fileNames = res.data.files.map(props => props.filename);
+  const taskNames = getTaskNames(fileNames);
+  const tasksMeta = fillTaskMeta(taskNames);
 
-//   if (tasksMeta.length > 0) {
-//     console.log(JSON.stringify(tasksMeta));
-//   } else {
-//     console.log('No tasks were changed. Skip testing.')
-//   }
-// })
+  if (tasksMeta.length > 0) {
+    console.log(JSON.stringify(tasksMeta));
+  } else {
+    console.log('No tasks were changed. Skip testing.')
+  }
+})
 
-// function getTaskNames(files) {
-//   const taskNames = new Set();
+function getTaskNames(files) {
+  const taskNames = new Set();
 
-//   files.filter(filePath => filePath.startsWith('Tasks/')).forEach(filePath => {
-//     taskNames.add(filePath.split('/')[1]);
-//   });
+  files.filter(filePath => filePath.startsWith('Tasks/')).forEach(filePath => {
+    taskNames.add(filePath.split('/')[1]);
+  });
 
-//   return [...taskNames];
-// }
+  return [...taskNames];
+}
 
-// function fillTaskMeta(taskNames) {
-//   return taskNames.map(name => {
-//     const filePath = 'Tasks/' + name + '/task.json';
-//     const rawdata = fs.readFileSync(filePath);
-//     const taskJsonFile = JSON.parse(rawdata);
+function fillTaskMeta(taskNames) {
+  return taskNames.map(name => {
+    const filePath = 'Tasks/' + name + '/task.json';
+    const rawdata = fs.readFileSync(filePath);
+    const taskJsonFile = JSON.parse(rawdata);
 
-//     return {[name]: taskJsonFile.id}
-//   })
-// }
+    return {[name]: taskJsonFile.id}
+  })
+}
