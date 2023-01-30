fdb17fa8196a78c9449c5c8984cb88efe9e0f7b0
zhenghao104 <zhentan@microsoft.com>
2023-01-30 12:41:42 -0800

    Refactoring to support crates.io auth token

--- Tasks/CargoAuthenticateV0/cargoauthenticatemain.ts
+++ Tasks/CargoAuthenticateV0/cargoauthenticatemain.ts
@@ -8,7 +8,7 @@ import * as pkgLocationUtils from 'azure-pipelines-tasks-packaging-common/locati
 import * as url from 'url';
 import * as base64 from 'base-64';
 import * as utf8 from 'utf8';
-import { getPackagingServiceConnections, ServiceConnectionAuthType, UsernamePasswordServiceConnection, TokenServiceConnection } from "azure-pipelines-tasks-artifacts-common/serviceConnectionUtils";
+import { ServiceConnection, getPackagingServiceConnections, ServiceConnectionAuthType, UsernamePasswordServiceConnection, TokenServiceConnection } from "azure-pipelines-tasks-artifacts-common/serviceConnectionUtils";
 
 async function main(): Promise<void> {
     tl.setResourcePath(path.join(__dirname, 'task.json'));
@@ -53,37 +53,41 @@ async function main(): Promise<void> {
 
         const localAccesstoken = `Bearer ${tl.getVariable('System.AccessToken')}`;
         const serviceConnections = getPackagingServiceConnections('cargoServiceConnections');
+        let externalServiceConnections: ServiceConnection[] = [];
+
+        for (let serviceConnection of serviceConnections) {
+            switch (serviceConnection.authType) {
+                case (ServiceConnectionAuthType.UsernamePassword):
+                    externalServiceConnections.push(serviceConnection);
+                    break;
+                case (ServiceConnectionAuthType.Token):
+                    // We only support crates.io
+                    if (url.parse(serviceConnection.packageSource.uri).host !== "crates.io") throw Error(tl.loc('Error_InvalidServiceConnection', serviceConnection.packageSource.uri));
+
+                    const tokenAuthInfo = serviceConnection as TokenServiceConnection;
+                    tl.debug(`Detected token credentials for '${serviceConnection.packageSource.uri}'`);
+                    tl.setVariable("CARGO_REGISTRY_TOKEN", tokenAuthInfo.token)
+                    break;
+                default:
+                    throw Error(tl.loc('Error_InvalidServiceConnection', serviceConnection.packageSource.uri));
+            }
+        }
 
         for (let registry of Object.keys(result.registries)) {
             const registryUrl = url.parse(result.registries[registry].index);
-            const tokenName = `CARGO_REGISTRIES_${registry.toLocaleUpperCase().replace("-", "_")}_TOKEN`;
+            let tokenName = `CARGO_REGISTRIES_${registry.toLocaleUpperCase().replace("-", "_")}_TOKEN`;
             if (registryUrl && registryUrl.host && collectionHosts.indexOf(registryUrl.host.toLowerCase()) >= 0) {
                 let currentRegistry : string;
-                if (serviceConnections && serviceConnections.length > 0) {
-                    for (let serviceConnection of serviceConnections) {  
-                        if (url.parse(serviceConnection.packageSource.uri).href === url.parse(result.registries[registry].index.replace("sparse+", "")).href) {
-                            let serviceConnectionAccessToken : string;
-                            switch (serviceConnection.authType) {
-                                case (ServiceConnectionAuthType.UsernamePassword):
-                                    const usernamePasswordAuthInfo = serviceConnection as UsernamePasswordServiceConnection;
-                                    serviceConnectionAccessToken = `Basic ${base64.encode(utf8.encode(`${usernamePasswordAuthInfo.username}:${usernamePasswordAuthInfo.password}`))}`;   
-                                    tl.debug(`Detected username/password or PAT credentials for '${serviceConnection.packageSource.uri}'`);
-                                    break;
-                                case (ServiceConnectionAuthType.Token):
-                                    const tokenAuthInfo = serviceConnection as TokenServiceConnection;
-                                    serviceConnectionAccessToken = tokenAuthInfo.token;
-                                    tl.debug(`Detected token credentials for '${serviceConnection.packageSource.uri}'`);
-                                    break;
-                                default:
-                                    throw Error(tl.loc('Error_InvalidServiceConnection', serviceConnection.packageSource.uri));
-                            }
-                            tl.debug(tl.loc('AddingAuthExternalRegistry', registry, tokenName));
-                            currentRegistry = registry;
-                            tl.setVariable(tokenName, serviceConnectionAccessToken);
-                            break;
-                        }
-                    }
+                for (let serviceConnection of externalServiceConnections) {
+                    if (url.parse(serviceConnection.packageSource.uri).href === url.parse(result.registries[registry].index.replace("sparse+", "")).href) {
+                        const usernamePasswordAuthInfo = serviceConnection as UsernamePasswordServiceConnection;
+                        currentRegistry = registry;
+                        tl.debug(`Detected username/password or PAT credentials for '${serviceConnection.packageSource.uri}'`);
+                        tl.debug(tl.loc('AddingAuthExternalRegistry', registry, tokenName));
+                        tl.setVariable(tokenName, `Basic ${base64.encode(utf8.encode(`${usernamePasswordAuthInfo.username}:${usernamePasswordAuthInfo.password}`))}`);
+                    }      
                 }
+                // Default to internal registry if no token has been set yet
                 if (!currentRegistry) {
                     tl.debug(tl.loc('AddingAuthRegistry', registry, tokenName));
                     tl.setVariable(tokenName, localAccesstoken);
