4ec64adcdc84e91bac696758713507f38b6b5631
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-01-31 13:43:43 +0100

    bump

--- ci/testing/testAndVerify.js
+++ ci/testing/testAndVerify.js
@@ -1,2 +1,13 @@
-console.log('process.argv');
+const axios = require('axios');
+const url = 'https://dev.azure.com/canary2-poc/tasks-canary/_apis/pipelines/5/runs?api-version=7';
+
+const ADOToken = process.argv[2];
+const tasks = process.argv[3];
+
 console.log(process.argv);
+
+// console.log(axios.isCancel('something'));
+
+
+
+// curl --user "":"$(ADOToken)" -H "Content-Type: application/json" -H "Accept:application/json" "https://dev.azure.com/canary2-poc/tasks-canary/_apis/pipelines/5/runs?api-version=7" -X POST -d "{\"templateParameters\": { \"tasks\": \"$TASKS\"}}"
