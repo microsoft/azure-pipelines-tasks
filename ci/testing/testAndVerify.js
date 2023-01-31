0aa26d8e4e08ea7ec61010e2bce1ecdfed0c36b5
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-01-31 14:47:36 +0100

    bump

--- ci/testing/testAndVerify.js
+++ ci/testing/testAndVerify.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
-console.log(axios.isCancel('something'));
-
 const url = 'https://dev.azure.com/canary2-poc/tasks-canary/_apis/pipelines/5/runs?api-version=7';
 
-const ADOToken = process.argv[2];
+const AUTH_TOKEN = process.argv[2];
 const tasks = process.argv[3];
+axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 if (tasks) {
   const taskNames = tasks.split(',');
@@ -18,6 +18,26 @@ if (tasks) {
 
 function triggerTestPipeline(taskName) {
   console.log(`Trigger test pipeline for ${taskName} task`);
+
+  axios.post(url, {
+    templateParameters: {}
+  },{ 
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  })
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
 }
 
 function observeTestPipeline(taskName, pipeline) {
