7220e2f6727508ff3c9be638724de1642aaada4e
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-02-01 11:55:18 +0100

    bump

--- ci/testing/testAndVerify.js
+++ ci/testing/testAndVerify.js
@@ -1,15 +1,19 @@
 const axios = require('axios');
-const hostname = 'https://dev.azure.com';
-const organization = 'canary2-poc';
-const project = 'tasks-canary';
-const apiVersion = '7';
-const url = 'https://dev.azure.com/canary2-poc/tasks-canary/_apis/pipelines/5/runs?api-version=7';
 
+const HOSTNAME = 'https://dev.azure.com';
+const ORGANIZATION = 'canary2-poc';
+const PROJECT = 'tasks-canary';
+const apiVersion = 'api-version=7.0';
+const apiUrl = `${HOSTNAME}/${ORGANIZATION}/${PROJECT}/_apis/pipelines`;
 const AUTH_TOKEN = process.argv[2];
 const tasks = process.argv[3];
+const auth = {
+  username: 'Basic',
+  password: AUTH_TOKEN
+};
 
 if (tasks) {
-  start(tasks);
+  return start(tasks);
 } else {
   console.log('Skip test verification');
 }
@@ -17,18 +21,19 @@ if (tasks) {
 async function start(tasks) {
   const taskNames = tasks.split(',');
 
-  const pipelines = await getPipelines();
-  console.log(pipelines);
-
-  const map = mapPipelines(pipelines);
-  return Promise.all(taskNames.map(async taskName => {
-    if (map[taskName]) {
-      const pipelineBuild = await runTestPipeline(map[taskName]);
-      return verifyTestRunResults(pipelineBuild);  
-    } else {
-      console.error(`Error: pipeline ${taskName} was not found`);
-    }
-  }))
+  if (taskNames.length) {
+    const pipelines = await fetchPipelines();
+    const map = mapPipelines(pipelines);
+  
+    return Promise.all(taskNames.map(async taskName => {
+      if (map[taskName]) {
+        const pipelineBuild = await runTestPipeline(map[taskName]);
+        return verifyTestRunResults(pipelineBuild);  
+      } else {
+        console.error(`Error: cannot build and run tests for task ${taskName} - corresponding pipeline was not found`);
+      }
+    }))
+  }
 }
 
 function mapPipelines(pipelines) { 
@@ -41,54 +46,47 @@ function mapPipelines(pipelines) {
   return map;
 }
 
-function getPipelines() {
-  return axios.get(`https://dev.azure.com/${organization}/${project}/_apis/pipelines?api-version=7.0`, { 
-    auth: {
-       username: 'Basic',
-       password: AUTH_TOKEN
-    }
-  })
+function fetchPipelines() {
+  return axios.get(`${apiUrl}?${apiVersion}`, { auth })
   .then(res => res.data.value)
   .catch(err => {
-    console.error(err);
+    console.error('Error fetching pipelines', err);
     throw err;
   });
 }
 
 function runTestPipeline(pipeline) {
-  console.log(`Run test pipeline for ${pipeline.name} task, pipelineId: ${pipeline.id}`);
+  console.log(`Run ${pipeline.name} pipeline, pipelineId: ${pipeline.id}`);
 
-  return axios.post(`https://dev.azure.com/${organization}/${project}/_apis/pipelines/${pipeline.id}/runs?api-version=7.0`, {},{
-    auth: {
-      username: 'Basic',
-      password: AUTH_TOKEN
-   }
-  })
+  return axios.post(`${apiUrl}/${pipeline.id}/runs?${apiVersion}`, {}, { auth })
   .then(res => res.data)
-  .catch(err => err)
+  .catch(err => {
+    console.error(`Error running ${pipeline.name} pipeline, pipelineId: ${pipeline.id}`, err)
+    throw err;
+  })
 }
 
 function verifyTestRunResults(pipelineBuild) {
   return new Promise((resolve, reject) => {
-    const timeout = setTimeout(() => {
-      verifyBuildStatus(pipelineBuild, timeout, resolve, reject);
+    const interval = setInterval(() => {
+      verifyBuildStatus(pipelineBuild, interval, resolve, reject);
     }, 1000)
   
-    console.log(pipelineBuild)
-    console.log(`Observe test pipeline for ${pipelineBuild.name} task`);
+    console.log(`Check status for build ${pipelineBuild.name}, id: ${pipelineBuild.id} task...`);
   })
 }
 
 async function verifyBuildStatus(pipelineBuild, timeout, resolve, reject) {
-  const response = await axios.get(pipelineBuild.url, {
-    auth: {
-      username: 'Basic',
-      password: AUTH_TOKEN
-    }
-  })
+  const response = await axios.get(pipelineBuild.url, { auth })
+    .then(res => res.data)
+    .catch(err => {
+      console.error('Error verifying build status', err);
+      throw err;
+    })
+  
+  console.log(`Verify build status... ${response.data.state}`);
   
   if (response.data.state === 'inProgress') {
-    console.log('Verify build status... in progress')
     return;
   }
 
@@ -100,8 +98,3 @@ async function verifyBuildStatus(pipelineBuild, timeout, resolve, reject) {
     resolve('Test pipeline build succeeded')
   }
 }
-
-
-
-
-// curl --user "":"$(ADOToken)" -H "Content-Type: application/json" -H "Accept:application/json" "https://dev.azure.com/canary2-poc/tasks-canary/_apis/pipelines/5/runs?api-version=7" -X POST -d "{\"templateParameters\": { \"tasks\": \"$TASKS\"}}"
