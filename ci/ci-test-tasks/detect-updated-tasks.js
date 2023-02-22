5dd1a20ff69eaad389d040557cd2867b43348940
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-02-22 10:31:25 +0100

    bump

--- ci/ci-test-tasks/detect-updated-tasks.js
+++ ci/ci-test-tasks/detect-updated-tasks.js
@@ -1,8 +1,5 @@
-const childProcess = require('child_process');
-const allowedTasks = new Set(['DownloadPackageV1']); // TODO: remove after testing
-
 try {
-  const files = ((childProcess.execSync('git diff --name-only ms/master') || '').toString().trim()).split(/\r?\n/);
+  const files = process.argv.slice(2);
   const taskNames = getTaskNames(files);
 
   if (taskNames.length > 0) {
@@ -21,5 +18,5 @@ function getTaskNames(files) {
     taskNames.add(filePath.split('/')[1]);
   });
 
-  return [...taskNames].filter(taskName => allowedTasks.has(taskName)); //TODO: remove after testing
+  return [...taskNames];
 }
