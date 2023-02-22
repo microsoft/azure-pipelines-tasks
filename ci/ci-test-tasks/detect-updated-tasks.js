bca46ca6232bd667dae2994f426d30456e534f3d
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-02-22 16:59:22 +0100

    bump

--- ci/ci-test-tasks/detect-updated-tasks.js
+++ ci/ci-test-tasks/detect-updated-tasks.js
@@ -1,18 +1,21 @@
 const files = process.argv.slice(2);
 const taskNames = getTaskNames(files);
 
-if (taskNames.length > 0) {
-  console.log(taskNames.join(','));
-} else {
-  throw new Error('No tasks were changed. Skip testing.')
-}
 
-function getTaskNames(files) {
-  const taskNames = new Set();
+console.error('Test error')
 
-  files.filter(filePath => filePath.startsWith('Tasks/')).forEach(filePath => {
-    taskNames.add(filePath.split('/')[1]);
-  });
+// if (taskNames.length > 0) {
+//   console.log(taskNames.join(','));
+// } else {
+//   throw new Error('No tasks were changed. Skip testing.')
+// }
 
-  return [...taskNames];
-}
+// function getTaskNames(files) {
+//   const taskNames = new Set();
+
+//   files.filter(filePath => filePath.startsWith('Tasks/')).forEach(filePath => {
+//     taskNames.add(filePath.split('/')[1]);
+//   });
+
+//   return [...taskNames];
+// }
