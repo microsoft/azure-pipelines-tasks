3109fcbc92c25525919df9012a11e73dd46a8414
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-02-22 18:10:05 +0100

    bump

--- ci/ci-test-tasks/detect-updated-tasks.js
+++ ci/ci-test-tasks/detect-updated-tasks.js
@@ -4,8 +4,7 @@ const taskNames = getTaskNames(files);
 if (taskNames.length > 0) {
   console.log(taskNames.join(','));
 } else {
-  // the output of this console.log is used for conditional check in the "detect changes" pipeline. If you change it change it in the both places
-  console.log('No tasks were changed. Skip testing.')
+  console.error('No tasks were changed. Skip testing.')
 }
 
 function getTaskNames(files) {
