f07316ae11ec79e5a31e322e2a6d11736a88a1d4
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-02-22 16:52:49 +0100

    bump

--- ci/ci-test-tasks/detect-updated-tasks.js
+++ ci/ci-test-tasks/detect-updated-tasks.js
@@ -1,19 +1,21 @@
 const files = process.argv.slice(2);
 const taskNames = getTaskNames(files);
 
-if (taskNames.length > 0) {
-  console.log(taskNames.join(','));
-} else {
-  // the output of this console.log is used for conditional check in the "detect changes" pipeline. If you change it change it in the both places
-  console.log('No tasks were changed. Skip testing.')
-}
+console.error('skip')
 
-function getTaskNames(files) {
-  const taskNames = new Set();
+// if (taskNames.length > 0) {
+//   console.log(taskNames.join(','));
+// } else {
+//   // the output of this console.log is used for conditional check in the "detect changes" pipeline. If you change it change it in the both places
+//   console.log('No tasks were changed. Skip testing.')
+// }
 
-  files.filter(filePath => filePath.startsWith('Tasks/')).forEach(filePath => {
-    taskNames.add(filePath.split('/')[1]);
-  });
+// function getTaskNames(files) {
+//   const taskNames = new Set();
 
-  return [...taskNames];
-}
+//   files.filter(filePath => filePath.startsWith('Tasks/')).forEach(filePath => {
+//     taskNames.add(filePath.split('/')[1]);
+//   });
+
+//   return [...taskNames];
+// }
