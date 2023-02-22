4f14f0c0c1c122fbd85a599140cd817bda1fca12
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-02-22 17:55:27 +0100

    bump

--- ci/ci-test-tasks/detect-changed-tasks.js
+++ ci/ci-test-tasks/detect-changed-tasks.js
@@ -9,11 +9,11 @@ if (taskNames.length > 0) {
 }
 
 function getTaskNames(files) {
-  const taskNames = new Set();
+  const tasks = new Set();
 
   files.filter(filePath => filePath.startsWith('Tasks/')).forEach(filePath => {
-    taskNames.add(filePath.split('/')[1]);
+    tasks.add(filePath.split('/')[1]);
   });
 
-  return [...taskNames];
+  return [...tasks];
 }
