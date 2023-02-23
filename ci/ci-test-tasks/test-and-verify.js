a916255aac19e01c333c8476fd78f7864a1d9980
Pavlo Andriiesh <pavloandriiesh@github.com>
2023-02-23 13:59:09 +0100

    bump

--- ci/ci-test-tasks/test-and-verify.js
+++ ci/ci-test-tasks/test-and-verify.js
@@ -2,9 +2,10 @@ const axios = require('axios');
 
 const AUTH_TOKEN = process.argv[2];
 const ADOUrl = process.argv[3];
-const task = process.argv[4];
+const ProjectName = process.argv[4];
+const task = process.argv[5];
 const apiVersion = 'api-version=7.0';
-const apiUrl = ADOUrl + '/PipelineTasks/_apis/pipelines';
+const apiUrl = `${ADOUrl}/${ProjectName}/_apis/pipelines`;
 
 const auth = {
   username: 'Basic',
