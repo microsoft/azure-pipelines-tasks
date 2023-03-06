3e4ad1d6a779a1942ba4e3e7d12b65b52fcf4ad4
Merlyn Oppenheim <merlynop@microsoft.com>
2023-03-05 21:49:49 -0800

    wip

---
+++ TaskUpdater/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.CommandLine;
+using System.Text.Json.Nodes;
+
+class Program
+{
+    static void Main()
+    {
+        string taskTarget = @"C:\repos\azure-pipelines-tasks\Tasks\DownloadBuildArtifactsV0";
+
+        string taskPath = Path.Combine(taskTarget, "task.json");
+        string taskLocPath = Path.Combine(taskTarget, "task.loc.json");
+        string packagePath = Path.Combine(taskTarget, "package.json");
+
+        var taskJson = File.ReadAllText(taskPath);
+
+        JsonNode taskNode = JsonNode.Parse(File.ReadAllText(taskPath))!;
+        JsonNode taskLocNode = JsonNode.Parse(File.ReadAllText(taskLocPath))!;
+        JsonNode packageNode = JsonNode.Parse(File.ReadAllText(packagePath))!;
+
+        taskNode["version"]["Patch"] = "2";
+        packageNode["dependencies"]["@types/node"] = "^16.11.39";
+
+        taskNode.AsObject()?.Remove("_buildConfigMapping");
+
+        taskNode.AsObject().Add("_buildConfigMapping", new JsonObject
+        {
+            ["Default"] = "0.219.1",
+            ["Node16"] = "0.219.2"
+        });
+
+
+    }
+}
