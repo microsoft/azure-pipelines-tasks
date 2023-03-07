bec89abebff12818004209820a290d3da1ab5e9b
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2023-03-07 11:11:28 +0100

    Get rid of azure-arm-rest-v2
    Removing azure-arm-rest-v2 folder since the package was migrated to common-npm-packages repo(https://github.com/microsoft/azure-pipelines-tasks-common-packages/)

--- common-npm-packages/azure-arm-rest-v2/AzureServiceClient.d.ts
+++
@@ -1,9 +0,0 @@
-import msRestAzure = require("./azure-arm-common");
-import { AzureServiceClientBase } from './AzureServiceClientBase';
-export declare class ServiceClient extends AzureServiceClientBase {
-    subscriptionId: string;
-    constructor(credentials: msRestAzure.ApplicationTokenCredentials, subscriptionId: string, timeout?: number);
-    getRequestUri(uriFormat: string, parameters: {}, queryParameters?: string[], apiVersion?: string): string;
-    isValidResourceGroupName(resourceGroupName: string): void;
-    protected validateInputs(subscriptionId: string): void;
-}
