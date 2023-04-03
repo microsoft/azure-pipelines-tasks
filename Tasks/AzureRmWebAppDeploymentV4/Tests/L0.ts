d6b700101d98223b46f2f205297bafd343f25282
Ondrej Netocny <onetocny@microsoft.com>
2023-04-03 13:41:16 +0200

    Log tests

--- Tasks/AzureRmWebAppDeploymentV4/Tests/L0.ts
+++ Tasks/AzureRmWebAppDeploymentV4/Tests/L0.ts
@@ -197,6 +197,10 @@ describe('AzureRmWebAppDeployment Suite', function() {
             assert(tr.stdOutContained('PreDeployment steps with slot enabled should succeeded'), 'Should have printed: PreDeployment steps withSlotEnabled should succeeded');
             assert(tr.stdOutContained('Active DeploymentId :MOCK_DEPLOYMENT_ID'), 'Should have printed: Active DeploymentId :MOCK_DEPLOYMENT_ID.');
             assert(tr.stdOutContained('PreDeployment steps with virtual application should succeeded'), 'Should have printed: PreDeployment steps with slot enabled should succeeded');
+
+            tl.debug("STDERR: " + tr.stderr);
+            tl.debug("STDOUT: " + tr.stdout);
+
             done();
         }
         catch(error) {
