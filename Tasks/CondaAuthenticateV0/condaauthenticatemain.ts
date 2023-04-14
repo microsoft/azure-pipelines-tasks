435a599906cbb4e0d74102b16d321a0d3ec6e26b
Abby Powell <abpowell@microsoft.com>
2023-04-14 16:42:20 -0400

    Simplified version of conda authenticate

--- Tasks/CondaAuthenticateV0/condaauthenticatemain.ts
+++ Tasks/CondaAuthenticateV0/condaauthenticatemain.ts
@@ -22,63 +22,18 @@ async function main(): Promise<void> {
 
         const condarcFile = fs.readFileSync(configCondarc, 'utf8');
         const condarcContents = jsyaml.load(condarcFile);
-        const channelsAlias = condarcContents['channel_alias'] ? condarcContents['channel_alias'] : '';
-        const channelsList = condarcContents['channels'] ? condarcContents['channels']: condarcContents['default_channels']; //A list of strings
-        const defaultChannels = condarcContents['channels'] ? condarcContents['default_channels'] : condarcContents['channels']; //A list of strings
-        const defaultsCalled = (!condarcContents['channels'] && condarcContents['default_channels']);
-        const customChannels = condarcContents['custom_channels']; //An object with key-value pairs
-        const customMultiChannels = condarcContents['custom_multichannels'];  //An object with string-list pairs
-        const restoreFreeChannel = condarcContents['restore_free_channe'] ? condarcContents['restore_free_channel'] : false;
-
-        if (!channelsList && !defaultChannels) {
-            // Throws error for having no channels
-            throw new Error(tl.loc('CondarcMissingChannels'));
-        }
-
-        const usedMultichannels = new Set<string>();
-        const channelPieces = {};  //Entries:  <full channel str as listed> : [<half after '@' symbol>, <TOKEN_ENV_VAR>]
-
-        iterateThroughChannels(channelsList, channelsAlias, channelPieces, usedMultichannels, defaultsCalled, defaultChannels, 
-            restoreFreeChannel, customChannels, customMultiChannels);
-
-        if (Object.keys(channelPieces).length === 0) {
-            // Throw error for having no channels
-            throw new Error(tl.loc('CondarcMissingChannels'));
-        }
-
-        let packagingLocation;
-        try {
-            packagingLocation = await pkgLocationUtils.getPackagingUris(pkgLocationUtils.ProtocolType.Npm);
-        } catch (error) {
-            tl.debug('Unable to get packaging URIs');
-            util.logError(error);
-            throw error;
-        }
-    
-        const collectionHosts = packagingLocation.PackagingUris.map((pkgUrl) => {
-        const parsedUrl = url.parse(pkgUrl);
-            if (parsedUrl && parsedUrl.host) {
-                return parsedUrl.host.toLowerCase();
-            }
-            return undefined;
+        const condarcLowercaseContents = {};
+        Object.keys(condarcContents).forEach(key => {
+            condarcLowercaseContents[key.toLowerCase()] = condarcContents[key];
         });
-
-        let allChannels = '';
-            
-        Object.entries(channelPieces).forEach(
-            ([key, value]) => {
-                //Entries:  <full channel str as listed> : [<half after '@' symbol>, <TOKEN_ENV_VAR>]
-                const channelUrl = url.parse(value[0]);
-                if (channelUrl && channelUrl.host && collectionHosts.indexOf(channelUrl.host.toLowerCase()) >= 0) {
-                    if (allChannels !== '') {
-                        allChannels += ', ';
-                    }
-                    allChannels += key;
-                }
-            }
-        )
-
-        tl.setVariable('CONDA_CHANNELS', allChannels);
+        const channelsAlias = condarcLowercaseContents['channel_alias']; 
+        if (!channelsAlias) {
+            throw new Error(tl.loc('CondarcMissingChannels'));
+        }
+        const tokenName = checkTokenName(channelsAlias);
+        const localAccesstoken = tl.getVariable('System.AccessToken');
+        console.log(tl.loc('AddingAuthChannel', tokenName, channelsAlias));
+        tl.setVariable('ARTIFACTS_CONDA_TOKEN', localAccesstoken);
     }
     catch (error) {
         tl.error(error);
@@ -88,77 +43,26 @@ async function main(): Promise<void> {
 }
 main();
 
-function getAliasPieces(fullAlias: string, channelPieces?: {}) {  //returns void
-    const condaSplitUrl = fullAlias.split('@');
-    if (condaSplitUrl.length > 2) {
-        // Throw error for invalid format
-       throw new Error(tl.loc('InvalidCondarcFormat', 'a channels'));
+function checkTokenName(channelsAlias: string): string { //Returns the token's name
+    const alias_pieces = channelsAlias.split('@');
+    if (alias_pieces.length !== 2) {
+        throw new Error(tl.loc('AliasMissingInfo'));
     }
-    const condaChannelPieces = condaSplitUrl[0].split(':');
-    if (condaChannelPieces.length !== 3) {
-        //Throw error for having the wrong number of pieces
-        //  Pieces should be: ['https', '//<username>', '<PAT>']
-        throw new Error(tl.loc('InvalidCondarcFormat', 'a channels'));
+    const user_pieces = alias_pieces[0].split(':');
+    if (user_pieces.length !== 3) {
+        throw new Error(tl.loc('AliasMissingInfo'));
     }
-    let tokenEnvVar = condaChannelPieces[2];  //The 3rd piece
-    if (tokenEnvVar[0] === '$') {
-        fullAlias = fullAlias.replace(condaChannelPieces[2], tl.getVariable('System.AccessToken'));
-        let start = 1;
-        let end = tokenEnvVar.length;
-        if ((tokenEnvVar.length > 3) && (tokenEnvVar[1] === '{') && (tokenEnvVar[tokenEnvVar.length - 1] === '}')) {
-            start++;
-            end--;
-        }
-        tokenEnvVar = tokenEnvVar.substring(start, end);
+    if (user_pieces[2][0] !== '$') {
+        //Throw error that channels_alias is not in the accepted format
+        throw new Error(tl.loc('InvalidTokenFormat'));
     }
-    
-    if (channelPieces) {
-        channelPieces[fullAlias] = [`https://${condaSplitUrl[1]}`, tokenEnvVar];
+    let token_env_var = user_pieces[2].substring(1);
+    if (token_env_var.length > 2 && token_env_var[0] === '{' && token_env_var[token_env_var.length - 1] === '}') {
+        token_env_var = token_env_var.substring(1, token_env_var.length - 1);
     }
-}
-
-function iterateThroughChannels(channels: string[], alias: string, channelPieces: {}, usedMultichannels: Set<string>, 
-            defaultsCalled: boolean, defaultChannels: string[] | undefined, restoreFreeChannel: boolean, 
-            customChannels: {} | undefined, customMultiChannels: {} | undefined) {
-    for (let i = 0; i < channels.length; i++) {
-        const channel = channels[i];
-        if ((channel in channelPieces) || (`${alias}/${channel}` in channelPieces) || 
-            (customChannels && (`${customChannels[channel]}/${channel}` in channelPieces)) || 
-            (channel in usedMultichannels) || 
-            ((channel === 'defaults') && defaultsCalled)) {
-            // Throw warning that the same channel is listed multiple times
-            console.warn(tl.loc('DuplicateChannel', 
-                `${channel} or ${alias}/${channel} or ${customChannels[channel]}/${channel}`));
-            continue;
-        }
-        if (channel.includes('@')) {
-            getAliasPieces(channel, channelPieces);
-        }
-        else if (channel === 'defaults') {
-            defaultsCalled = true;
-            if (!defaultChannels) {
-                defaultChannels = ['main', 'r', 'msys2'];
-                if (restoreFreeChannel) {
-                    defaultChannels.splice(1, 0, 'free');
-                }
-            }
-            iterateThroughChannels(defaultChannels, alias, channelPieces, usedMultichannels, defaultsCalled, undefined,
-                restoreFreeChannel, customChannels, customMultiChannels)
-        }
-        else if (customMultiChannels && (channel in customMultiChannels)) {
-            // Create set for used custom multichannels
-            usedMultichannels.add(channel);
-            const customChannelsList = customMultiChannels[channel];
-            iterateThroughChannels(customChannelsList, '', channelPieces, usedMultichannels, defaultsCalled, defaultChannels, 
-                restoreFreeChannel, undefined, undefined);
-        }
-        else if (customChannels && (channel in customChannels)) {
-            getAliasPieces(`${customChannels[channel]}/${channel}`, channelPieces);
-        }
-        else if (alias) {
-            // Append channel to the end of alias
-            // Entries:  <full channel str> : [<half after '@' symbol>, <TOKEN_ENV_VAR>]
-            getAliasPieces(`${alias}/${channel}`, channelPieces);
-        }
+    if (token_env_var !== 'ARTIFACTS_CONDA_TOKEN') {
+        // Throw error for not using the designated name
+        throw new Error(tl.loc('InvalidTokenName'));
     }
+    return token_env_var;
 }
