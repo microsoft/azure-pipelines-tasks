8db217c0398dfb4e964bbfb4ded2a079e4685a0b
Ondrej Netocny <onetocny@microsoft.com>
2023-04-21 10:02:57 +0200

    Revert "azure-arm-rest-v2 bearer token updates"

--- common-npm-packages/azure-arm-rest-v2/azure-arm-app-service-kudu.ts
+++ common-npm-packages/azure-arm-rest-v2/azure-arm-app-service-kudu.ts
@@ -11,26 +11,17 @@ tl.setResourcePath(path.join(__dirname, 'module.json'), true);
 
 export class KuduServiceManagementClient {
     private _scmUri;
-    private _accessToken: string;
+    private _accesssToken: string;
     private _cookie: string[] = undefined;
-    private _scmAccessCheck : boolean;
 
-    constructor(scmUri: string, accessToken: string, scmAccessCheck: boolean) {
-        this._accessToken = accessToken;
+    constructor(scmUri: string, accessToken: string) {
+        this._accesssToken = accessToken;
         this._scmUri = scmUri;
-        this._scmAccessCheck = scmAccessCheck;
     }
 
     public async beginRequest(request: webClient.WebRequest, reqOptions?: webClient.WebRequestOptions, contentType?: string): Promise<webClient.WebResponse> {
         request.headers = request.headers || {};
-        if(!this._scmAccessCheck) {
-            request.headers["Authorization"] = "Bearer " + this._accessToken;
-            tl.debug('Using Bearer Authentication');
-        }
-        else{
-            request.headers["Authorization"] = "Basic " + this._accessToken;
-            tl.debug('Using Basic Authentication');
-        }
+        request.headers["Authorization"] = "Basic " + this._accesssToken;
         if (!request.headers['Content-Type'] || !!contentType) {
             request.headers['Content-Type'] = contentType || 'application/json; charset=utf-8';
         }
@@ -89,8 +80,9 @@ export class KuduServiceManagementClient {
 export class Kudu {
     private _client: KuduServiceManagementClient;
 
-    constructor(scmUri: string, accessToken: string, scmPolicyCheck: boolean) {
-        this._client = new KuduServiceManagementClient(scmUri, accessToken, scmPolicyCheck);
+    constructor(scmUri: string, username: string, password: string) {
+        var base64EncodedCredential = (Buffer.from(username + ':' + password).toString('base64'));
+        this._client = new KuduServiceManagementClient(scmUri, base64EncodedCredential);
     }
 
     public async updateDeployment(requestBody: any): Promise<string> {
