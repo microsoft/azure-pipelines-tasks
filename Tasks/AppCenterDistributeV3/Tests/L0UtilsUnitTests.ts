f982e1e5d9081ded2043c930cd1c7d3c248e7cf0
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2023-04-26 16:19:18 +0200

    Adding code coverage analysis for tasks
    - Increase timeout for test tasks

--- Tasks/AppCenterDistributeV3/Tests/L0UtilsUnitTests.ts
+++ Tasks/AppCenterDistributeV3/Tests/L0UtilsUnitTests.ts
@@ -7,19 +7,19 @@ export function utilsUnitTests() {
         describe('findCommonParent', function() {
             it('returns containing directory for single file', function() {
                 const tp = path.join(__dirname, 'UnitTests', 'Utils', 'FindCommonParentForSingleFileReturnsContainingDirectory.js');
-                const spawn = spawnSync('node', [tp], {timeout: 2000});
+                const spawn = spawnSync('node', [tp], {timeout: 5000});
                 assert.equal(spawn.status, 0);
             });
 
             it('returns directory itself for single directory', function() {
                 const tp = path.join(__dirname, 'UnitTests', 'Utils', 'FindCommonParentForSingleDirectoryReturnsDirectoryItself.js');
-                const spawn = spawnSync('node', [tp], {timeout: 2000});
+                const spawn = spawnSync('node', [tp], {timeout: 5000});
                 assert.equal(spawn.status, 0);
             });
 
             it('returns common parent for multiple values', function() {
                 const tp = path.join(__dirname, 'UnitTests', 'Utils', 'FindCommonParentForMultipleFilesReturnsCorrectResult.js');
-                const spawn = spawnSync('node', [tp], {timeout: 2000});
+                const spawn = spawnSync('node', [tp], {timeout: 5000});
                 assert.equal(spawn.status, 0);
             });
         });
