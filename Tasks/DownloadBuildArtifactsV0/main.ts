8986748cd3efd954f6a24592d912183924e60a9a
Liliia Sabitova <v-lisabitova@microsoft.com>
2023-05-04 14:11:12 +0200

    Adding toLowerCase() to support capital letters in AZP_TASK_FF_ENABLE_BUILDARTIFACTS_PLUS_SIGN_WORKAROUND

--- Tasks/DownloadBuildArtifactsV0/main.ts
+++ Tasks/DownloadBuildArtifactsV0/main.ts
@@ -245,7 +245,7 @@ async function main(): Promise<void> {
             // if FF EnableBuildArtifactsPlusSignWorkaround is enabled or AZP_TASK_FF_ENABLE_BUILDARTIFACTS_PLUS_SIGN_WORKAROUND environment variable is set:
             // replacing '+' symbol by its representation ' '(space) - workaround for the DownloadBuildArtifactV0 task,
             // where downloading of part of artifact is not possible if there is a plus symbol
-            const enableBuildArtifactsPlusSignWorkaround = process.env.AZP_TASK_FF_ENABLE_BUILDARTIFACTS_PLUS_SIGN_WORKAROUND ? process.env.AZP_TASK_FF_ENABLE_BUILDARTIFACTS_PLUS_SIGN_WORKAROUND === "true" : false;
+            const enableBuildArtifactsPlusSignWorkaround = process.env.AZP_TASK_FF_ENABLE_BUILDARTIFACTS_PLUS_SIGN_WORKAROUND ? process.env.AZP_TASK_FF_ENABLE_BUILDARTIFACTS_PLUS_SIGN_WORKAROUND.toLowerCase() === "true" : false;
                         
             if (enableBuildArtifactsPlusSignWorkaround)
                 artifactName = artifactName.replace(/\+/g, ' ');
