841bc97beec9f758ae3f5e0785bdac465fbbd3cd
Sergei Nesterov <snesterov@microsoft.com>
2023-05-15 16:51:33 +0200

    Replaced custom auth with arm-rest-v2 library usage

--- Tasks/AzureNLBManagementV1/nlbtask.ts
+++ Tasks/AzureNLBManagementV1/nlbtask.ts
@@ -1,4 +1,6 @@
 import * as tl from 'azure-pipelines-task-lib/task';
+import { AzureRMEndpoint } from 'azure-pipelines-tasks-azure-arm-rest-v2/azure-arm-endpoint';
+import { AzureEndpoint } from 'azure-pipelines-tasks-azure-arm-rest-v2/azureModels';
 import taskinternal = require('azure-pipelines-task-lib/internal');
 import * as path from 'path'
 
@@ -13,31 +15,23 @@ async function run() {
 		var resourceGroupName: string = tl.getInput("ResourceGroupName", true);
 		var loadBalancerName: string = tl.getInput("LoadBalancer", true);
 		var action: string = tl.getInput("Action", true);
-		var endPointAuthCreds = tl.getEndpointAuthorization(connectedServiceName, true);
-		var endpointUrl = tl.getEndpointUrl(connectedServiceName, true);
 
-		var SPN = new Array();
-		SPN["servicePrincipalClientID"] = endPointAuthCreds.parameters["serviceprincipalid"];
-		SPN["servicePrincipalKey"] = endPointAuthCreds.parameters["serviceprincipalkey"];
-		SPN["tenantID"] = endPointAuthCreds.parameters["tenantid"];
-		SPN["subscriptionId"] = tl.getEndpointDataParameter(connectedServiceName, 'subscriptionid', true);
-		SPN["envAuthUrl"] = tl.getEndpointDataParameter(connectedServiceName, 'environmentAuthorityUrl', true);
-		SPN["url"] = tl.getEndpointUrl(connectedServiceName, true);
-		
-		var nicVm = await getNetworkInterface(SPN, endpointUrl, resourceGroupName);
+		var armEndpoint = await new AzureRMEndpoint(connectedServiceName).getEndpoint();
+
+		var nicVm = await getNetworkInterface(armEndpoint, resourceGroupName);
 		tl.debug(`Network Interface - ${nicVm.name}'s configuration details fetched for the virtual machine ${process.env.COMPUTERNAME}`);
 
 		var nicLbBackendPoolConfig = null;
 		if (action == "Connect") {
 			taskinternal._writeLine(tl.loc("ConnectingVMtoLB", loadBalancerName));
-			var lb = await nlbUtility.getLoadBalancer(SPN, endpointUrl, loadBalancerName, resourceGroupName);
+			var lb = await nlbUtility.getLoadBalancer(armEndpoint, loadBalancerName, resourceGroupName);
 			nicLbBackendPoolConfig = lb.properties.backendAddressPools;
 		}
 		else {
 			taskinternal._writeLine(tl.loc("DisconnectingVMfromLB", loadBalancerName));
 		}
 		nicVm.properties.ipConfigurations[0].properties['loadBalancerBackendAddressPools'] = nicLbBackendPoolConfig;
-		var setNIStatus = await nlbUtility.setNetworkInterface(SPN, endpointUrl, nicVm, resourceGroupName);
+		var setNIStatus = await nlbUtility.setNetworkInterface(armEndpoint, nicVm, resourceGroupName);
 		taskinternal._writeLine(tl.loc(setNIStatus, nicVm.name));
 		taskinternal._writeLine(tl.loc("ActionCompletedSuccefully", action, process.env.COMPUTERNAME, loadBalancerName));
 	}
@@ -46,13 +40,13 @@ async function run() {
 	}
 }
 
-async function getNetworkInterface(SPN, endpointUrl: string, resourceGroupName: string) {
-	var nics =  await nlbUtility.getNetworkInterfacesInRG(SPN, endpointUrl, resourceGroupName);
+async function getNetworkInterface(endpoint: AzureEndpoint, resourceGroupName: string) {
+	var nics =  await nlbUtility.getNetworkInterfacesInRG(endpoint, resourceGroupName);
 	tl.debug(`Getting Primary Network Interface for the virtual machine : ${process.env.COMPUTERNAME}`);
 	var nicVm = utility.getPrimaryNetworkInterface(nics);
-	
+
 	if (!nicVm) {
-		throw tl.loc("CouldNotFetchNicDetails", process.env.COMPUTERNAME);	
+		throw tl.loc("CouldNotFetchNicDetails", process.env.COMPUTERNAME);
 	}
 	return nicVm;
 }
