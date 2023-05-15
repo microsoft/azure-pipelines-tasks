4f02c7883a72ff93e162338883325c70bb35e0d3
v-mazayt <v-mazayt@microsoft.com>
2023-05-15 11:09:07 +0200

    Update jobs

--- ci/courtesy-push/create-hotfix-release.js
+++ ci/courtesy-push/create-hotfix-release.js
@@ -15,41 +15,41 @@ if (!taskName) {
   throw new Error('No description provided');
 }
 
-const scriptPath = `${hotfixFolder}/${taskName}.ps1`;
-console.log(scriptPath);
-
-const description = `Hotfix for ${taskName} task`;
+const scriptPath = `${hotfixFolder}/hotfix.ps1`;
+const description = `Hotfixing tasks: ${taskName.split(',')}`;
 
 const authHandler = azdev.getPersonalAccessTokenHandler(token);
 const orgUrl = 'https://dev.azure.com/mseng';
 const definitionId = 120; // "TFS - Prod Config Change" release definition id
 
-const createRelease = async () => {
-  console.log('Getting connection');
+const projectName = 'AzureDevOps';
 
-  const connection = new azdev.WebApi(orgUrl, authHandler);
-  console.log('Getting Git API');
-  const releaseApi = await connection.getReleaseApi();
-
-  console.log('Creating TFS - Prod Config Change release');
-
-  const projectName = 'AzureDevOps';
-
-  const releaseMetadata = {
-    definitionId: definitionId,
-    description: description,
-    variables: {
-      ScriptPath: { value: scriptPath }
-    }
-  };
-  const release = await releaseApi.createRelease(releaseMetadata, projectName);
-  const releaseLink = `${orgUrl}/${projectName}/_releaseProgress?_a=release-pipeline-progress&releaseId=${release.id}`;
-  console.log(`Link to the Release: ${releaseLink}`);
+const releaseMetadata = {
+  definitionId: definitionId,
+  description: description,
+  variables: {
+    ScriptPath: { value: scriptPath }
+  }
 };
 
-try {
-  createRelease();
-} catch (err) {
-  console.log(err);
-  throw err;
-}
+const createRelease = async () => {
+  console.log('Getting connection');
+  const connection = new azdev.WebApi(orgUrl, authHandler);
+
+  try {
+    console.log('Getting Release API');
+    const releaseApi = await connection.getReleaseApi();
+
+    console.log('Creating TFS - Prod Config Change release');
+    const release = await releaseApi.createRelease(releaseMetadata, projectName);
+
+    const releaseLink = `${orgUrl}/${projectName}/_releaseProgress?_a=release-pipeline-progress&releaseId=${release.id}`;
+    console.log(`Link to the Release: ${releaseLink}`);
+  } catch (err) {
+    console.log('##vso[task.logissue type=error]Failed to create config change release');
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+createRelease();
