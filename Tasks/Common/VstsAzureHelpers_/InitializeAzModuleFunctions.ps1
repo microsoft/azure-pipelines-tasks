2b94d16f1da5a29db6b98279b8820e0700c1e085
Denis Rumyantsev <mr.denis.rumyantsev@gmail.com>
2023-05-16 09:34:23 +0200

    fix Initialize-AzSubscription

--- Tasks/Common/VstsAzureHelpers_/InitializeAzModuleFunctions.ps1
+++ Tasks/Common/VstsAzureHelpers_/InitializeAzModuleFunctions.ps1
@@ -102,6 +102,9 @@ function Initialize-AzSubscription {
             $null = Clear-AzContext -Scope CurrentUser -Force -ErrorAction SilentlyContinue
             Write-Host "##[command]Clear-AzContext -Scope Process"
             $null = Clear-AzContext -Scope Process
+            Write-Host "##[command]Clear-AzConfig -DefaultSubscriptionForLogin"
+            $null = Clear-AzConfig -DefaultSubscriptionForLogin
+
             if ($Endpoint.Auth.Parameters.AuthenticationType -eq 'SPNCertificate') {
                 $servicePrincipalCertificate = Add-CertificateForAz -Endpoint $Endpoint
 
