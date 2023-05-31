d3ddc7c0914930d7ae08a747d0b29d6123552d1a
Ozan Aydin <ozanaydin@microsoft.com>
2023-05-31 14:17:43 +0200

    - temporary debug code is deleted

--- Tasks/KubernetesV1/Tests/L0.ts
+++ Tasks/KubernetesV1/Tests/L0.ts
@@ -83,11 +83,6 @@ describe('Kubernetes Suite', function() {
         process.env[shared.isKubectlPresentOnMachine] = "false";
         tr.run();
 
-        console.log(`!!! TEST LOG - ${ tr.stdout }`);
-        console.log(`!!! TEST RESULT - ${tr.succeeded}`);
-        console.log(`!!! TEST INVOKED - ${tr.invokedToolCount}`);
-        console.log(`!!! TEST ERRORS - errCount = ${tr.stderr?.length} errIssCount = ${tr.errorIssues?.length}`);
-
         assert(tr.succeeded, 'task should have succeeded');
         assert(tr.invokedToolCount == 1, 'should have invoked tool one times. actual: ' + tr.invokedToolCount);
         assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
