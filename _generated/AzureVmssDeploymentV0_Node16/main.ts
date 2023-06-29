d5dd34a3a81b97bb1ba2b09b57e18461fffb2943
Aleksandr Levochkin <v-allevockin@microsoft.com>
2023-06-29 11:16:17 +0200

    Migrate AzureVmssDeploymentV0-to-Node16

---
+++ _generated/AzureVmssDeploymentV0_Node16/main.ts
@@ -0,0 +1,22 @@
+import tl = require("azure-pipelines-task-lib/task");
+import path = require("path");
+
+import AzureVmssTaskParameters from "./models/AzureVmssTaskParameters";
+import VirtualMachineScaleSet from "./operations/VirtualMachineScaleSet";
+
+async function run(): Promise<void> {
+    var taskParameters = await new AzureVmssTaskParameters().getAzureVmssTaskParameters();
+    var vmssOperation = new VirtualMachineScaleSet(taskParameters);
+    await vmssOperation.execute();
+}
+
+var taskManifestPath = path.join(__dirname, "task.json");
+tl.debug("Setting resource path to " + taskManifestPath);
+tl.setResourcePath(taskManifestPath);
+tl.setResourcePath(path.join( __dirname, 'node_modules/azure-pipelines-tasks-azure-arm-rest-v2/module.json'));
+
+run().then((result) =>
+    tl.setResult(tl.TaskResult.Succeeded, "")
+).catch((error) =>
+    tl.setResult(tl.TaskResult.Failed, error)
+    );
