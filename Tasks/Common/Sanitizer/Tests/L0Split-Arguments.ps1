d0106f9bf02043b4e804c52bcefedc54c7fa3142
Vladyslav Horbachov <vhorbachov@microsoft.com>
2023-07-21 18:36:05 +0200

    Add L0 tests for Split-Arguments function

---
+++ Tasks/Common/Sanitizer/Tests/L0Split-Arguments.ps1
@@ -0,0 +1,29 @@
+[CmdletBinding()]
+param()
+
+. $PSScriptRoot\..\..\..\..\Tests\lib\Initialize-Test.ps1
+. $PSScriptRoot\..\ArgumentsSanitizer.ps1
+
+# Arrange
+
+$argumentsFormats = @(
+    "/parameter",                            # Single word
+    "/parameter1 /parameter2",               # Multiple words separated by space
+    "/parameter1 '/path/my file.txt'",       # Argument with spaces in quotes
+    "/parameter1 '/path/my file.txt' /parameter2 'value with spaces'" # Complex example
+)
+
+$expectedOutputs = @(
+    @("/parameter"),                        
+    @("/parameter1", "/parameter2"),                    
+    @("/parameter1", "/path/my file.txt"),                  
+    @("/parameter1", "/path/my file.txt", "/parameter2", "value with spaces")         
+)
+
+for ($i = 0; $i -lt $argumentsFormats.Length; $i++) {
+    # Act
+    $splitArguments = Split-Arguments -arguments $argumentsFormats[$i]
+
+    # Assert
+    Assert-AreEqual $splitArguments $expectedOutputs[$i]
+}
