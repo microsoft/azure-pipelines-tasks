36d69db86c4603deb9e890fca2e3109e12ef71f5
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2023-08-03 20:33:15 +0200

    Fix telemetry

--- Tasks/PowerShellV2/helpers.ps1
+++ Tasks/PowerShellV2/helpers.ps1
@@ -31,12 +31,15 @@ function Sanitize-Arguments([string]$InputArgs) {
 
     # We're splitting by ``, removing all suspicious characters and then join
     $argsArr = $InputArgs -split $argsSplitSymbols;
+
+    ## '?<!`' - checking if before character no backtick. '^a-zA-Z0-9` _'"-' - checking if character is allowed. Insead replacing to #removed#
     $regex = '(?<!`)([^a-zA-Z0-9\\` _''"\-=\/:\.])'
     for ($i = 0; $i -lt $argsArr.Length; $i++ ) {
-        ## '?<!`' - checking if before character no backtick. '^a-zA-Z0-9` _'"-' - checking if character is allowed. Insead replacing to #removed#
-        $argsArr[$i] -match $regex > $null;
-        $matchesChunks += , $Matches.Values;
-        $argsArr[$i] = $argsArr[$i] -replace $regex, $removedSymbolSign;
+        [string[]]$matches = (Select-String $regex -input $argsArr[$i] -AllMatches) | ForEach-Object { $_.Matches }
+        if ($null -ne $matches ) {
+            $matchesChunks += , $matches;
+            $argsArr[$i] = $argsArr[$i] -replace $regex, $removedSymbolSign;
+        }
     }
 
     $resultArgs = $argsArr -join $argsSplitSymbols;
