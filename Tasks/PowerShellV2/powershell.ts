663b549c7be0d0c50fa09e769d471fc186fb331e
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2023-08-19 04:05:36 +0200

    Update error telemetry

--- Tasks/PowerShellV2/powershell.ts
+++ Tasks/PowerShellV2/powershell.ts
@@ -87,7 +87,12 @@ async function run() {
                     throw error;
                 }
 
-                emitTelemetry('TaskHub', 'PowerShellV2', { UnexpectedError: error.stack ?? error.message ?? JSON.stringify(error) ?? 'unknown' });
+                emitTelemetry('TaskHub', 'PowerShellV2',
+                    {
+                        UnexpectedError: error.message ?? JSON.stringify(error) ?? null,
+                        ErrorStackTrace: error.stack ?? null
+                    }
+                );
             }
 
             script = `. '${input_filePath.replace(/'/g, "''")}' ${input_arguments}`.trim();
