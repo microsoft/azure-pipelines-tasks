cc13d5bf80d87c7b01d54e7ab174b2fcb4fdb34c
Grzegorz Gurgul (from Dev Box) <ggurgul@microsoft.com>
2023-09-06 22:09:07 +0200

    This is a combination of 2 commits.
    Prefer redirect
    
    Prefer redirect
    
    bump version
    
    enable for now
    
    Prefer redirect
    
    Correctly follows redirect
    
    Correctly follows redirect
    
    Separate
    
    1
    
    golden commit
    
    refactor
    
    Refactor
    
    fix
    
    Updates versions

--- Tasks/DownloadBuildArtifactsV0/DownloadHandlers/HandlerConfigs.ts
+++ Tasks/DownloadBuildArtifactsV0/DownloadHandlers/HandlerConfigs.ts
@@ -12,6 +12,7 @@ export interface IBaseHandlerConfig {
 export interface IContainerHandlerConfig extends IBaseHandlerConfig {
     endpointUrl: string;
     templatePath: string;
+    preferRedirect: boolean;
     handler: PersonalAccessTokenCredentialHandler;
 }
 
