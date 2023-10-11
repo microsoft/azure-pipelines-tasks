7fe82dc6af77faeaef9a44b5a79cb3eeb0018f23
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2023-10-11 16:36:25 +0400

    Update regex

--- Tasks/PowerShellV2/helpers.ps1
+++ Tasks/PowerShellV2/helpers.ps1
@@ -33,7 +33,7 @@ function Sanitize-Arguments([string]$InputArgs) {
     $argsArr = $InputArgs -split $argsSplitSymbols;
 
     ## '?<!`' - checking if before character no backtick. '^a-zA-Z0-9` _'"-=\/:\.*,+~?%\n' - checking if character is allowed. Insead replacing to #removed#
-    $regex = '(?<!`)([^a-zA-Z0-9\\` _''"\-=\/:\.*,+~?%\n])'
+    $regex = '(?<!`)([^a-zA-Z0-9\\` _''"\-=\/:\.*,+~?%\n#])(?i)(?!true|false)'
     for ($i = 0; $i -lt $argsArr.Length; $i++ ) {
         [string[]]$matches = (Select-String $regex -input $argsArr[$i] -AllMatches) | ForEach-Object { $_.Matches }
         if ($null -ne $matches ) {
