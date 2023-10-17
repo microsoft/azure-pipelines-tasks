ebb3247084492ff8c5c74b1c070fd92cd69c4b8c
Stella Qian <zqian@microsoft.com>
2023-10-17 15:36:22 -0700

    Migrate 21 artifact package tasks to Node20 (#19101)
    * migrate 21 tasks owned by artifact packages

---
+++ _generated/NuGetV0/Tests/L0.ts
@@ -0,0 +1,24 @@
+import * as path from 'path';
+import * as assert from 'assert';
+import * as ttm from 'azure-pipelines-task-lib/mock-test';
+
+describe('NuGet Task Suite', function () {
+    before(() => {
+    });
+
+    after(() => {
+    });
+
+    it('Happy path', (done: Mocha.Done) => {
+        let tp = path.join(__dirname, 'happypath.js')
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.run()
+        assert(tr.ran('c:\\from\\tool\\installer\\nuget.exe testCommand -NonInteractive testArgument'), 'NuGet was not run with the expected parameters');
+        assert(tr.stdOutContained('setting console code page'), 'It should have run chcp');
+        assert(tr.stdOutContained('NuGet output here'), "The NuGet output was not found on stdOut");
+        assert(tr.succeeded, 'should have succeeded');
+        assert.strictEqual(tr.errorIssues.length, 0, "should have no errors");
+        done();
+    }).timeout(60000);
+});
