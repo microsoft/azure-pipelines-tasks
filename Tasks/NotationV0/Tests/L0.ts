9a6f465cafce86ec344fc3453748164cee3c30a8
Junjie Gao <junjiegao@microsoft.com>
2023-11-08 18:49:49 +0800

    fix: update test
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- Tasks/NotationV0/Tests/L0.ts
+++ Tasks/NotationV0/Tests/L0.ts
@@ -3,32 +3,32 @@ import * as ttm from 'azure-pipelines-task-lib/mock-test';
 import assert = require('assert');
 
 describe('NotationV0 Suite', function () {
-    it('install notation', function () {
-        this.timeout(1000);
+    it('install notation', async function () {
+        this.timeout(2000);
 
         let tp = path.join(__dirname, 'L0Install.js');
         let tr = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync()
 
         assert(tr.succeeded, 'should have succeeded');
     })
 
-    it('notation sign', function () {
-        this.timeout(1000);
+    it('notation sign', async function () {
+        this.timeout(2000);
 
         let tp = path.join(__dirname, 'L0Sign.js');
         let tr = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync()
 
         assert(tr.succeeded, 'should have succeeded');
     })
 
-    it('notation verify', function () {
-        this.timeout(1000);
+    it('notation verify', async function () {
+        this.timeout(2000);
 
         let tp = path.join(__dirname, 'L0Verify.js');
         let tr = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync()
 
         assert(tr.succeeded, 'should have succeeded');
     })
