1c267e236b1397491198b3a6edbad4ac50392652
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2023-11-27 09:31:10 +0100

    [Courtesy push] Remove unused configs for courtesy push
    - Rewrited courtesy-push.js, added possibility to remove unused configs

--- ci/courtesy-push/courtesy-push.js
+++ ci/courtesy-push/courtesy-push.js
@@ -5,8 +5,7 @@ const azureSourceFolder = process.argv[2];
 const newDeps = process.argv[3];
 const unifiedDepsPath = path.join(azureSourceFolder, '.nuget', 'externals', 'UnifiedDependencies.xml');
 const tfsServerPath = path.join(azureSourceFolder, 'Tfs', 'Service', 'Deploy', 'components', 'TfsServer.Servicing.core.xml');
-const msPrefix = "Mseng.MS.TF.DistributedTask.Tasks.";
-const directoryTag = new RegExp('<Directory (.*)>');
+const msPrefix = 'Mseng.MS.TF.DistributedTask.Tasks.';
 
 function formDirectoryString(nugetTaskName) {
     const taskName = nugetTaskName.replace(msPrefix, '');
@@ -16,8 +15,13 @@ function formDirectoryString(nugetTaskName) {
   </Directory>`;
 }
 
-function formatDeps(depArr) {
-    const newDepsDict = {};
+/**
+* The function to form a dictionary of dependencies
+* @param {Array} depArr - array of dependencies
+* @returns {Object} - dictionary of dependencies
+*/
+function getDeps(depArr) {
+    const deps = {};
 
     depArr.forEach(newDep => {
         // add to dictionary
@@ -26,114 +30,175 @@ function formatDeps(depArr) {
         const name = depDetails[1];
         const version = depDetails[3];
         console.log(name + ' ' + version);
-        newDepsDict[name] = version;
+
+        if (name.includes('_')) {
+            const basicName = name.split('_')[0];
+            if (!deps[basicName]) deps[basicName] = {};
+            if (!deps[basicName].configs) deps[basicName].configs = [];
+
+            deps[basicName].configs.push({ name, version, depStr: newDep });
+        } else {
+            if (!deps[name]) deps[name] = {};
+
+            deps[name].name = name;
+            deps[name].version = version;
+            deps[name].depStr = newDep; 
+        }
     });
 
-    return newDepsDict;
+    return deps;
 }
 
-function findLastIndex(array, predicate) {
-    let l = array.length;
+/**
+ * The function removed all generated configs such as Node16/Node20 from the list of dependencies
+ * @param {Array} depsArray - array of parsed dependencies from UnifiedDependencies.xml
+ * @param {Object} depsForUpdate - dictionary of dependencies from getDeps method
+ * @param {Object} updatedDeps - structure to track added/removed dependencies
+ * @returns {Array} - updated array of dependencies and updatedDeps object { added: [], removed: []
+ */
+function removeConfigsForTasks(depsArray, depsForUpdate, updatedDeps) {
+    const newDepsArr = depsArray.slice();
+    const updatedDepsObj = Object.assign({}, updatedDeps);
+    const basicDepsForUpdate = new Set(Object.keys(depsForUpdate));
+    let index = 0;
 
-    while (l--) {
-        if (predicate(array[l], l, array)) {
-            return l;
+    while (index < newDepsArr.length) {
+        const currentDep = newDepsArr[index];
+        const depDetails = currentDep.split('"');
+        const name = depDetails[1];
+        if (!name || !name.includes('_')) {
+            index++;
+            continue;
         }
+
+        const basicName = name.split('_')[0];
+
+        if (!basicDepsForUpdate.has(basicName)) {
+            index++;
+            continue;
+        }
+
+        newDepsArr.splice(index, 1);
+        updatedDepsObj.removed.push(name);
     }
-    return -1;
+
+    return [newDepsArr, updatedDepsObj];
 }
 
-/* Function updating existing deps version and also add new deps with postfix 
- * Example: If we have dependency with name 
- * Mseng.MS.TF.DistributedTask.Tasks.AndroidSigningV2
- * It will add Mseng.MS.TF.DistributedTask.Tasks.AndroidSigningV2_Node16 */
-function updateUnifiedDeps(pathToUnifiedDeps, pathToNewUnifiedDeps, outputPath) {
-    const currentDeps = fs.readFileSync(pathToUnifiedDeps, 'utf8');
-    const newDeps = fs.readFileSync(pathToNewUnifiedDeps, 'utf8');
+/**
+ * The function update tasks dependencies with configs such as Node16/Node20
+ * @param {Array} depsArray - array of parsed dependencies from UnifiedDependencies.xml
+ * @param {Object} depsForUpdate - dictionary of dependencies from getDeps method
+ * @param {Object} updatedDeps - structure to track added/removed dependencies
+ * @returns {Array} - updated array of dependencies and updatedDeps object { added: [], removed: []
+ */
+function updateConfigsForTasks(depsArray, depsForUpdate, updatedDeps) {
+    const newDepsArr = depsArray.slice();
+    const updatedDepsObj = Object.assign({}, updatedDeps);
+    const basicDepsForUpdate = new Set(Object.keys(depsForUpdate));
+    let index = 0;
 
-    const currentDepsArr = currentDeps.split('\n');
-    const newDepsArr = newDeps.split('\n');
-    const newDepsDict = formatDeps(newDepsArr);
-
-    const updatedDeps = [];
-    // Tasks that was updated and should be presented in TfsServer.Servicing.core.xml
-    const changedTasks = [];
-
-    currentDepsArr.forEach(currentDep => {
+    while (index < newDepsArr.length) {
+        const currentDep = newDepsArr[index];
         const depDetails = currentDep.split('"');
         const name = depDetails[1];
 
-        // find if there is a match in new (ignoring case)
-        if (name) {
-            const newDepsKey = Object.keys(newDepsDict).find(key => key.toLowerCase() === name.toLowerCase());
-            if (newDepsKey && newDepsDict[newDepsKey]) {
-                // update the version
-                depDetails[3] = newDepsDict[newDepsKey];
-                updatedDeps.push(depDetails.join('"'));
-
-                changedTasks.push(newDepsKey);
-                delete newDepsDict[newDepsKey];
-            } else {
-                updatedDeps.push(currentDep);
-                console.log(`"${currentDep}"`);
-            }
-        } else {
-            updatedDeps.push(currentDep);
+        if (!name || !basicDepsForUpdate.has(name)) {
+            index++;
+            continue;
         }
-    });
 
-    // add the new deps from the start
-    // working only for generated deps
+        newDepsArr.splice(index, 1, depsForUpdate[name].depStr);
+        index++;
 
-    if (Object.keys(newDepsDict).length > 0) {
-        for (let packageName in newDepsDict) {
-            // new deps should include old packages completely
-            // Example:
-            // Mseng.MS.TF.DistributedTask.Tasks.AndroidSigningV2-Node16(packageName) should include 
-            // Mseng.MS.TF.DistributedTask.Tasks.AndroidSigningV2(basePackageName)
-            const depToBeInserted = newDepsArr.find(dep => dep.includes(packageName));
-            const pushingIndex = findLastIndex(updatedDeps, basePackage => {
-                if (!basePackage) return false;
-
-                const depDetails = basePackage.split('"');
-                const name = depDetails[1];
-                return name && name.startsWith(msPrefix) && packageName.includes(name.split("_")[0])
+        if (depsForUpdate[name].configs) {
+            depsForUpdate[name].configs.
+                sort((a,b) => a.name > b.name).
+                forEach(config => {
+                    updatedDepsObj.added.push(config.name);
+                    newDepsArr.splice(index, 0, config.depStr);
+                    index++;
             });
-
-            if (pushingIndex !== -1) {
-                // We need to insert new package after the old one
-                updatedDeps.splice(pushingIndex + 1, 0, depToBeInserted);
-                changedTasks.push(packageName);
-            }
         }
     }
-    // write it as a new file where currentDeps is
-    fs.writeFileSync(outputPath, updatedDeps.join('\n'));
-    console.log('Updating Unified Dependencies file done.');
-    return changedTasks;
-};
 
-/* Function to insert new tasks into TfsServer.Servicing.core.xml
- * Only if the was modified/added into UnifiedDependencies.xml and not exists in the TfsServer.Servicing.core.xml file
- */
-function updateTfsServerDeps(pathToTfsCore, depsToUpdateArr, outputPath) {
+    return [newDepsArr, updatedDepsObj];
+}
+
+/**
+ * The main function for unified dependencies update
+ * The function will parse unified dependencies file and update it with new dependencies/remove unused
+ * Since the generated tasks could be used and build only with default version, if unified_deps.xml doesn't contain
+ * the default version, the specific config (e.g. Node16) will be removed from the list of dependencies
+* @param {String} pathToUnifiedDeps - path to UnifiedDependencies.xml
+* @param {String} pathToNewUnifiedDeps - path to unified_deps.xml which contains dependencies updated on current week  
+*/
+function updateUnifiedDeps(pathToUnifiedDeps, pathToNewUnifiedDeps) {
+    const currentDeps = fs.readFileSync(pathToUnifiedDeps, 'utf8');
+    const newDeps = fs.readFileSync(pathToNewUnifiedDeps, 'utf8');
+
+    const newDepsArr = newDeps.split('\n');
+    const depsForUpdate = getDeps(newDepsArr);
+
+    let depsArray = currentDeps.split('\n');
+    let updatedDeps = { added: [], removed: [] };
+
+    [depsArray, updatedDeps] = removeConfigsForTasks(depsArray, depsForUpdate, updatedDeps);
+    [depsArray, updatedDeps] = updateConfigsForTasks(depsArray, depsForUpdate, updatedDeps);
+
+
+    fs.writeFileSync(pathToUnifiedDeps, depsArray.join('\n'));
+    console.log('Updating Unified Dependencies file done.');
+    return updatedDeps;
+}
+
+/**
+ * The function update TfsServer.Servicing.core.xml with new dependencies
+ * The function check the depsToUpdate which was created during updateUnifiedDeps 
+ * and add/remove dependencies from TfsServer.Servicing.core.xml
+ * @param {String} pathToTfsCore - path to TfsServer.Servicing.core.xml
+ * @param {Object} depsToUpdate - structure to track added/removed dependencies (formed in updateUnifiedDeps)
+*/
+function updateTfsServerDeps(pathToTfsCore, depsToUpdate) {
+    const directoryTagOpen = new RegExp('<Directory (.*)>');
+    const directoryTagClose = new RegExp('</Directory>');
+
     const tfsCore = fs.readFileSync(pathToTfsCore, 'utf8');
     const tfsToUpdate = tfsCore.split('\n');
-    const tfsCoreLowerCase = tfsCore.toLowerCase();
+    const depsToAdd = depsToUpdate.added;
+    const depsToRemove = depsToUpdate.removed.filter(dep => depsToAdd.indexOf(dep) === -1);
 
-    const insertedIndex = tfsToUpdate.findIndex(tfsString => directoryTag.test(tfsString));
-    depsToUpdateArr.forEach(dependencyName => {
-        const dependencyNameLower = dependencyName.toLowerCase();
-        if (tfsCoreLowerCase.indexOf(dependencyNameLower) === -1) {
-            const insertedString = formDirectoryString(dependencyName);
-            tfsToUpdate.splice(insertedIndex, 0, insertedString);
-            console.log(`${insertedString}`);
-        } 
+    depsToRemove.forEach(dependencyName => {
+        const refLine = dependencyName.replace(msPrefix, '');
+        const refIndexStart = tfsToUpdate.findIndex(tfsString => tfsString.match(directoryTagOpen) && tfsString.includes(`${refLine}\\"`));
+        if (refIndexStart === -1) return;
+
+        console.log(`Looking directory range for ${dependencyName} from line ${refIndexStart}`);
+        let refIndexEnd = refIndexStart;
+        while (!tfsToUpdate[refIndexEnd].match(directoryTagClose) && refIndexEnd < tfsToUpdate.length) {
+            refIndexEnd++;
+        }
+
+        tfsToUpdate.splice(refIndexStart, refIndexEnd - refIndexStart + 1);
+        console.log(`Removing ${dependencyName} from line ${refIndexStart} to ${refIndexEnd}`);
     });
 
-    fs.writeFileSync(outputPath, tfsToUpdate.join('\n'));
+
+    const insertedIndex = tfsToUpdate.findIndex(tfsString => directoryTagOpen.test(tfsString));
+    depsToAdd.forEach(dependencyName => {
+        const refLine = dependencyName.replace(msPrefix, '');
+        const refIndex = tfsToUpdate.findIndex(tfsString => tfsString.match(directoryTagOpen) && tfsString.includes(`${refLine}\\"`));
+        if (refIndex === -1) {
+            const insertedString = formDirectoryString(dependencyName);
+            tfsToUpdate.splice(insertedIndex, 0, insertedString);
+            console.log(`Inserting ${dependencyName} at line ${insertedIndex}`)
+            console.log(`${insertedString}`);
+        }
+    });
+
+    fs.writeFileSync(pathToTfsCore, tfsToUpdate.join('\n'));
     console.log('Inserting into Tfs Servicing Core file done.');
 }
 
-const changedTasks = updateUnifiedDeps(unifiedDepsPath, newDeps, unifiedDepsPath);
-updateTfsServerDeps(tfsServerPath, changedTasks, tfsServerPath);
+const changedTasks = updateUnifiedDeps(unifiedDepsPath, newDeps);
+updateTfsServerDeps(tfsServerPath, changedTasks);
