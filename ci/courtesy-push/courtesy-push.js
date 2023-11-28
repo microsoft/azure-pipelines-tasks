a82e646f156bcf678a413650fe6d628fde6de5bc
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2023-11-28 12:14:13 +0100

    [Courtesy push] Remove unused configs for courtesy push
    - Added xml2js package for xml convertation
    - Refactored getDeps to get rid of "_"
    - Refactore removeConfigsForTasks to identigicate configs properly

--- ci/courtesy-push/courtesy-push.js
+++ ci/courtesy-push/courtesy-push.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const xml2js = require('xml2js');
 
 const azureSourceFolder = process.argv[2];
 const newDeps = process.argv[3];
@@ -7,45 +8,73 @@ const unifiedDepsPath = path.join(azureSourceFolder, '.nuget', 'externals', 'Uni
 const tfsServerPath = path.join(azureSourceFolder, 'Tfs', 'Service', 'Deploy', 'components', 'TfsServer.Servicing.core.xml');
 const msPrefix = 'Mseng.MS.TF.DistributedTask.Tasks.';
 
-function formDirectoryString(nugetTaskName) {
-    const taskName = nugetTaskName.replace(msPrefix, '');
+/**
+ * Helper function to check if the value is included in the array but not equal to the value in the array
+ * E.g.  We compared generated task names such as Mseng.MS.TF.DistributedTask.Tasks.AppCenterDistributeV1_Node20
+ * with basic Mseng.MS.TF.DistributedTask.Tasks.AppCenterDistributeV1
+ * If the name is included in the list of dependencies but not equal to the name in the list, we assume its a config
+ */
+const isIncludeButNotEqual = (arr, value) => arr.reduce((acc, item) => acc || (value.includes(item) && value !== item), false);
 
-    return `  <Directory Path="[ServicingDir]Tasks\\Individual\\${taskName}\\">
-    <File Origin="nuget://Mseng.MS.TF.DistributedTask.Tasks.${taskName}/content/*" />
-  </Directory>`;
+function formDirectoryTag(nugetTaskName) {
+    const taskName = nugetTaskName.replace(msPrefix, '');
+    return {
+        $: {
+            Path: `[ServicingDir]Tasks\\Individual\\${taskName}\\`
+        },
+        File: [
+            {
+                $: {
+                    Origin: `nuget://Mseng.MS.TF.DistributedTask.Tasks.${taskName}/content/*`
+                }
+            }
+        ]
+    };
 }
 
 /**
-* The function to form a dictionary of dependencies
-* @param {Array} depArr - array of dependencies
-* @returns {Object} - dictionary of dependencies
-*/
+ * The function to form a dictionary of dependencies
+ * @param {Array} depArr - array of dependencies
+ * @returns {Object} - dictionary of dependencies
+ */
 function getDeps(depArr) {
     const deps = {};
+    const getDependantConfigs = (arrKeys, packageName) => arrKeys.filter(key => key.includes(packageName) && key !== packageName);
 
-    depArr.forEach(newDep => {
-        // add to dictionary
+    // first run we form structures 
+    for (let i = 0; i < depArr.length; i++) {
+        const newDep = depArr[i];
         const depDetails = newDep.split('"');
+
         console.log(JSON.stringify(depDetails));
         const name = depDetails[1];
         const version = depDetails[3];
         console.log(name + ' ' + version);
 
-        if (name.includes('_')) {
-            const basicName = name.split('_')[0];
-            if (!deps[basicName]) deps[basicName] = {};
-            if (!deps[basicName].configs) deps[basicName].configs = [];
+        if (!deps[name]) deps[name] = {};
+        deps[name].name = name;
+        deps[name].version = version;
+        deps[name].depStr = newDep;
+    }
 
-            deps[basicName].configs.push({ name, version, depStr: newDep });
-        } else {
-            if (!deps[name]) deps[name] = {};
+    const keys = Object.keys(deps);
+    for (let dep in deps) {
+        const configs = getDependantConfigs(keys, dep);
+        if (!configs.length) continue;
 
-            deps[name].name = name;
-            deps[name].version = version;
-            deps[name].depStr = newDep;
-        }
-    });
+        deps[dep].configs = [];
+        configs.forEach(config => {
+            const configDep = deps[config];
+            deps[dep].configs.push({
+                name: configDep.name,
+                version: configDep.version,
+                depStr: configDep.depStr
+            });
 
+            delete deps[config];
+        });
+    }
+    
     return deps;
 }
 
@@ -59,27 +88,27 @@ function getDeps(depArr) {
 function removeConfigsForTasks(depsArray, depsForUpdate, updatedDeps) {
     const newDepsArr = depsArray.slice();
     const updatedDepsObj = Object.assign({}, updatedDeps);
-    const basicDepsForUpdate = new Set(Object.keys(depsForUpdate));
+    const basicDepsForUpdate = Object.keys(depsForUpdate).map(dep => dep.toLowerCase());
     let index = 0;
 
     while (index < newDepsArr.length) {
         const currentDep = newDepsArr[index];
         const depDetails = currentDep.split('"');
         const name = depDetails[1];
-        if (!name || !name.includes('_')) {
+        if (!name) {
             index++;
             continue;
         }
 
-        const basicName = name.split('_')[0];
+        const basicName = name.toLowerCase();
 
-        if (!basicDepsForUpdate.has(basicName)) {
-            index++;
+        if (isIncludeButNotEqual(basicDepsForUpdate, basicName)) {
+            newDepsArr.splice(index, 1);
+            updatedDepsObj.removed.push(name);
             continue;
         }
 
-        newDepsArr.splice(index, 1);
-        updatedDepsObj.removed.push(name);
+        index++;
     }
 
     return [newDepsArr, updatedDepsObj];
@@ -112,8 +141,8 @@ function updateConfigsForTasks(depsArray, depsForUpdate, updatedDeps) {
         index++;
 
         if (depsForUpdate[name].configs) {
-            depsForUpdate[name].configs.
-                sort((a, b) => a.name > b.name)
+            depsForUpdate[name].configs
+                .sort((a, b) => a.name > b.name)
                 .forEach(config => {
                     updatedDepsObj.added.push(config.name);
                     newDepsArr.splice(index, 0, config.depStr);
@@ -130,9 +159,9 @@ function updateConfigsForTasks(depsArray, depsForUpdate, updatedDeps) {
  * The function parses unified dependencies file and updates it with new dependencies/remove unused
  * Since the generated tasks can only be used and build with default version, if unified_deps.xml doesn't contain
  * the default version, the specific config (e.g. Node16) will be removed from the list of dependencies
-* @param {String} pathToUnifiedDeps - path to UnifiedDependencies.xml
-* @param {String} pathToNewUnifiedDeps - path to unified_deps.xml which contains dependencies updated on current week  
-*/
+ * @param {String} pathToUnifiedDeps - path to UnifiedDependencies.xml
+ * @param {String} pathToNewUnifiedDeps - path to unified_deps.xml which contains dependencies updated on current week
+ */
 function updateUnifiedDeps(pathToUnifiedDeps, pathToNewUnifiedDeps) {
     const currentDeps = fs.readFileSync(pathToUnifiedDeps, 'utf8');
     const newDeps = fs.readFileSync(pathToNewUnifiedDeps, 'utf8');
@@ -146,7 +175,6 @@ function updateUnifiedDeps(pathToUnifiedDeps, pathToNewUnifiedDeps) {
     [depsArray, updatedDeps] = removeConfigsForTasks(depsArray, depsForUpdate, updatedDeps);
     [depsArray, updatedDeps] = updateConfigsForTasks(depsArray, depsForUpdate, updatedDeps);
 
-
     fs.writeFileSync(pathToUnifiedDeps, depsArray.join('\n'));
     console.log('Updating Unified Dependencies file done.');
     return updatedDeps;
@@ -154,49 +182,40 @@ function updateUnifiedDeps(pathToUnifiedDeps, pathToNewUnifiedDeps) {
 
 /**
  * The function update TfsServer.Servicing.core.xml with new dependencies
- * The function check the depsToUpdate which was created during updateUnifiedDeps 
+ * The function check the depsToUpdate which was created during updateUnifiedDeps
  * and add/remove dependencies from TfsServer.Servicing.core.xml
  * @param {String} pathToTfsCore - path to TfsServer.Servicing.core.xml
  * @param {Object} depsToUpdate - structure to track added/removed dependencies (formed in updateUnifiedDeps)
-*/
-function updateTfsServerDeps(pathToTfsCore, depsToUpdate) {
-    const directoryTagOpen = new RegExp('<Directory (.*)>');
-    const directoryTagClose = new RegExp('</Directory>');
-
+ */
+async function updateTfsServerDeps(pathToTfsCore, depsToUpdate) {
     const tfsCore = fs.readFileSync(pathToTfsCore, 'utf8');
-    const tfsToUpdate = tfsCore.split('\n');
-    const depsToAdd = depsToUpdate.added;
-    const depsToRemove = depsToUpdate.removed.filter(dep => depsToAdd.indexOf(dep) === -1);
+    const tfxCoreJson = await xml2js.parseStringPromise(tfsCore);
+    const depsToAdd = depsToUpdate.added.filter(dep => depsToUpdate.removed.indexOf(dep) === -1);
+    const depsToRemove = depsToUpdate.removed.filter(dep => depsToUpdate.added.indexOf(dep) === -1);
 
-    depsToRemove.forEach(dependencyName => {
-        const refLine = dependencyName.replace(msPrefix, '');
-        const refIndexStart = tfsToUpdate.findIndex(tfsString => tfsString.match(directoryTagOpen) && tfsString.includes(`${refLine}\\"`));
-        if (refIndexStart === -1) return;
-
-        console.log(`Looking directory range for ${dependencyName} from line ${refIndexStart}`);
-        let refIndexEnd = refIndexStart;
-        while (!tfsToUpdate[refIndexEnd].match(directoryTagClose) && refIndexEnd < tfsToUpdate.length) {
-            refIndexEnd++;
+    // removing dependencies
+    for (let idx = 0; idx < tfxCoreJson.Component.Directory.length; idx++) {
+        const directory = tfxCoreJson.Component.Directory[idx];
+        const files = directory.File;
+        const needToRemove = files.filter(file => depsToRemove.findIndex(dep => file.$.Origin.includes(dep)) !== -1);
+        if (needToRemove.length) {
+            tfxCoreJson.Component.Directory.splice(idx, 1);
+            idx--;
         }
+    }
 
-        tfsToUpdate.splice(refIndexStart, refIndexEnd - refIndexStart + 1);
-        console.log(`Removing ${dependencyName} from line ${refIndexStart} to ${refIndexEnd}`);
+    depsToAdd.forEach(dep => {
+        const directory = formDirectoryTag(dep);
+        tfxCoreJson.Component.Directory.unshift(directory);
     });
 
-
-    const insertedIndex = tfsToUpdate.findIndex(tfsString => directoryTagOpen.test(tfsString));
-    depsToAdd.forEach(dependencyName => {
-        const refLine = dependencyName.replace(msPrefix, '');
-        const refIndex = tfsToUpdate.findIndex(tfsString => tfsString.match(directoryTagOpen) && tfsString.includes(`${refLine}\\"`));
-        if (refIndex === -1) {
-            const insertedString = formDirectoryString(dependencyName);
-            tfsToUpdate.splice(insertedIndex, 0, insertedString);
-            console.log(`Inserting ${dependencyName} at line ${insertedIndex}`)
-            console.log(`${insertedString}`);
-        }
+    const builder = new xml2js.Builder({
+        xmldec: { version: '1.0', encoding: 'utf-8' },
+        renderOpts: { pretty: true, indent: '  ', newline: '\n', allowEmpty: false, spacebeforeslash: ' ' }
     });
+    const xml = builder.buildObject(tfxCoreJson);
 
-    fs.writeFileSync(pathToTfsCore, tfsToUpdate.join('\n'));
+    fs.writeFileSync(pathToTfsCore, xml);
     console.log('Inserting into Tfs Servicing Core file done.');
 }
 
