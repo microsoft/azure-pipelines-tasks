0def19ac376ab907868ba36e5945d9faf623029a
Zixuan Qian <zqian@microsoft.com>
2024-01-09 16:15:32 -0800

    Update make-util.js

--- make-util.js
+++ make-util.js
@@ -1890,8 +1890,7 @@ var mergeBuildConfigIntoBaseTasks = function(buildConfig) {
 
                 // Update versionmap.txt file
                 var versionmapFile = fs.readFileSync(versionmapFilePath, { encoding: 'utf-8' });
-                const originalLineEnding = versionmapFile.includes('\r\n') ? '\r\n' : '\n';
-                const lines = versionmapFile.split(originalLineEnding);
+                const lines = versionmapFile.split(/\r?\n/);
                 var buildConfigVersion = null;
                 for (let i = lines.length - 1; i >= 0; i--) {
                     if (!lines[i]) continue;
@@ -1904,7 +1903,7 @@ var mergeBuildConfigIntoBaseTasks = function(buildConfig) {
                         lines[i] = `Default|${buildConfigVersion}`;
                     }
                 }
-                const updatedContent = lines.join(originalLineEnding);
+                const updatedContent = lines.join(os.EOL);
                 fs.writeFileSync(versionmapFilePath, updatedContent, { encoding: 'utf-8' });
                 console.log(`Updated ${versionmapFilePath} file`);
 
