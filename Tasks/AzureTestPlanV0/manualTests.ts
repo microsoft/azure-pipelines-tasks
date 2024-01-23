2eaf669efe97621f558efd05a6713171bfcfb7d4
triptijain2112 <92331194+triptijain2112@users.noreply.github.com>
2024-01-23 14:53:28 +0530

    New Azure Test plan task  (#19440)
    * created new PR
    
    * updated package.json
    
    * updating packages file
    
    * updated commit with retrieval logic
    
    * rebuilt local
    
    * updated api call method name
    
    * added sample command and removed sleep statement
    
    * updated icon
    
    * updated task description
    
    * updated logic for selecting tests
    
    * Updated error message
    
    * added null check
    
    * removed -v from python invoker
    
    * updated resources file
    
    * updated task fail logic
    
    * updated task fail condition
    
    * updated task name
    
    * updated
    
    * updated deleted files
    
    * added utils file
    
    * added constants file and console.error
    
    * added public doc link
    
    * added task failre on api failure
    
    * updated if condition
    
    * updated error scenario
    
    * review comments update
    
    * updated indentation
    
    * added null checks

---
+++ Tasks/AzureTestPlanV0/manualTests.ts
@@ -0,0 +1,6 @@
+import tl = require('azure-pipelines-task-lib/task');
+
+export function manualTestsFlow() {
+    console.log("To do:");
+    console.log("Manual trigger flow to be implemented");
+}
