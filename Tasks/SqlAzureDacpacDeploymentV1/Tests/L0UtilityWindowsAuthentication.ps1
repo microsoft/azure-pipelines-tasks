b9973593b9dd147fad67f685fe1a8ada4bfebd79
Erin Dempster <erin.dempster@outlook.com>
2024-03-16 19:58:10 -0500

    Finishing up changes with unit test

---
+++ Tasks/SqlAzureDacpacDeploymentV1/Tests/L0UtilityWindowsAuthentication.ps1
@@ -0,0 +1,10 @@
+[CmdletBinding()]
+param()
+
+
+. $PSScriptRoot\..\..\..\Tests\lib\Initialize-Test.ps1
+. $PSScriptRoot\MockVariable.ps1
+
+$connectionString = Get-WindowsAuthenticationConnectionString -serverName "localhost" -databaseName "test"
+
+Assert-AreEqual -Expected "Data Source=localhost; Initial Catalog=test; Integrated Security=SSPI;" -Actual $connectionString
