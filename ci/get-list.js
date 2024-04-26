7d5077b28163a29f9ae1245d84a54cf019b5848e
v-mazayt <v-mazayt@microsoft.com>
2024-04-26 11:59:33 +0200

    Upd

--- ci/get-list.js
+++ ci/get-list.js
@@ -12,13 +12,35 @@ const tasksWithoutVersion = [];
 
 content.forEach(folderName => {
   var packageJsonPath = path.join(tasksSourcePath, folderName, 'package.json');
+
   if (fs.existsSync(packageJsonPath)) {
+    var packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
     tasksWithPackageJson.push(folderName);
-    var packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
     if (packageJson.name) {
       tasksWithName.push(folderName);
     } else {
       tasksWithoutName.push(folderName);
+
+      //packageJson.name = folderName.toLocaleLowerCase();
+
+      const name = folderName.toLocaleLowerCase().replace(/v\d*/g, '');
+      packageJson = insertKey('name', name, packageJson, 0);
+
+      fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+      var packageLockJsonPath = path.join(tasksSourcePath, folderName, 'package-lock.json');
+      if (fs.existsSync(packageLockJsonPath)) {
+        var packageLockJson = JSON.parse(fs.readFileSync(packageLockJsonPath));
+        packageLockJson = insertKey('name', name, packageLockJson, 0);
+        fs.writeFileSync(packageLockJsonPath, JSON.stringify(packageLockJson, null, 2));
+      } else {
+        var npmShrinkwrapJsonPath = path.join(tasksSourcePath, folderName, 'npm-shrinkwrap.json');
+        if (fs.existsSync(npmShrinkwrapJsonPath)) {
+          var npmShrinkwrapJson = JSON.parse(fs.readFileSync(npmShrinkwrapJsonPath));
+          npmShrinkwrapJson = insertKey('name', name, npmShrinkwrapJson, 0);
+          fs.writeFileSync(npmShrinkwrapJsonPath, JSON.stringify(npmShrinkwrapJson, null, 2));
+        }
+      }
     }
     if (packageJson.version) {
       tasksWithVersion.push(folderName);
@@ -30,9 +52,38 @@ content.forEach(folderName => {
   }
 });
 
+function insertKey(key, value, obj, pos) {
+  return Object.keys(obj).reduce((ac, a, i) => {
+    if (i === pos) ac[key] = value;
+    ac[a] = obj[a];
+    return ac;
+  }, {});
+}
+
 console.log(tasksWithPackageJson.length);
 console.log(tasksWithoutPackageJson.length);
-console.log('with Name: ' + tasksWithName.length);
-console.log('w/o Name: ' + tasksWithoutName.length);
-console.log('with versions: ' + tasksWithVersion.length);
-console.log('w/o versions: ' + tasksWithoutVersion.length);
+console.log('with Name: ' + tasksWithName);
+console.log(tasksWithoutName);
+console.log('with versions: ' + tasksWithVersion);
+console.log('w/o versions: ' + tasksWithoutVersion);
+
+// ContainerBuildV0,
+//   DockerComposeV0,
+//   DockerInstallerV0,
+//   DockerV0,
+//   DockerV1,
+//   DockerV2,
+//   DuffleInstallerV0,
+//   FuncToolsInstallerV0,
+//   HelmDeployV0,
+//   HelmInstallerV0,
+//   HelmInstallerV1,
+//   KubectlInstallerV0,
+//   KubernetesManifestV0,
+//   KubernetesManifestV1,
+//   KubernetesV0,
+//   KubernetesV1,
+//   NodeTaskRunnerInstallerV0,
+//   PublishCodeCoverageResultsV1,
+//   PublishCodeCoverageResultsV2,
+//   SshV0;
