0a09ea2008b1eaaa4f2d7df833f3ec7ac597efff
Junjie Gao <junjiegao@microsoft.com>
2024-04-30 21:23:38 +0800

    feat: support workload identity
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- Tasks/NotationV0/src/sign.ts
+++ Tasks/NotationV0/src/sign.ts
@@ -27,8 +27,9 @@ export async function sign(): Promise<void> {
             const akvPluginVersion = taskLib.getInput('akvPluginVersion', true) || '';
             const caCertBundle = taskLib.getInput('caCertBundle', false) || '';
             const selfSignedCert = taskLib.getBoolInput('selfSigned', false);
-            const keyVaultPluginEnv = { ...env, ...await getVaultCredentials() }
-            await installAzureKV(akvPluginVersion);
+            const [credentialEnvs, credentialType] = await getVaultCredentials();
+            const keyVaultPluginEnv = { ...env, ...credentialEnvs}
+            const installedVersion = await installAzureKV(akvPluginVersion);
             await notationRunner(artifactRefs, async (notation: ToolRunner, artifactRef: string, execOptions: IExecOptions) => {
                 execOptions.env = keyVaultPluginEnv;
                 return notation
@@ -39,6 +40,7 @@ export async function sign(): Promise<void> {
                     .argIf(allowReferrerAPI, '--allow-referrers-api')
                     .argIf(caCertBundle, `--plugin-config=ca_certs=${caCertBundle}`)
                     .argIf(selfSignedCert, '--plugin-config=self_signed=true')
+                    .argIf(isSupportCredentialType(installedVersion), `--plugin-config=credential_type=${credentialType}`)
                     .argIf(debug && debug.toLowerCase() === 'true', '--debug')
                     .exec(execOptions);
             })
@@ -48,20 +50,68 @@ export async function sign(): Promise<void> {
     }
 }
 
-// install azurekv plugin
-async function installAzureKV(versionPrefix: string): Promise<void> {
+// install azurekv plugin and return the installed version
+//
+// the versionPrefix is the version range to match the version of the plugin
+// For example: 1, 1.1, 1.1.0
+async function installAzureKV(versionPrefix: string): Promise<string> {
     // check if the plugin is already installed
     let binaryName = 'notation-azure-kv';
     if (os.platform() == 'win32') {
         binaryName += '.exe';
     }
     const pluginDir = path.join(getConfigHome(), NOTATION, PLUGINS, 'azure-kv');
+    const downloadInfo = getDownloadInfo(versionPrefix, AZURE_KV_PLUGIN_VERSION_FILE);
+
     if (taskLib.exist(path.join(pluginDir, binaryName))) {
-        console.log(taskLib.loc('AzureKVPluginAlreadyInstalled'));
+        // if the plugin is already installed, check if the version is the same
+        // as the expected version
+        const currentVersion = await getPluginVersion('azure-kv');
+        if (currentVersion === downloadInfo.version) {
+            console.log(taskLib.loc('AzureKVPluginAlreadyInstalled', currentVersion));
+        }
     } else {
-        // get azure-kv latest v1.x download info
-        const downloadInfo = getDownloadInfo(versionPrefix, AZURE_KV_PLUGIN_VERSION_FILE);
         await installFromURL(downloadInfo.url, downloadInfo.checksum, pluginDir);
     }
+
     taskLib.tool(NOTATION_BINARY).arg(['plugin', 'list']).execSync();
+    return downloadInfo.version;
+}
+
+function isSupportCredentialType(version: string): boolean {
+    // if version >= 1.1.0, support credential type
+    const versionParts = version.split('.');
+    if (versionParts.length < 2) {
+        return false;
+    }
+
+    if (parseInt(versionParts[0]) > 1) {
+        return true;
+    }
+
+    if (parseInt(versionParts[0]) === 1 && parseInt(versionParts[1]) >= 1) {
+        return true;
+    }
+
+    return false
+}
+
+function getPluginVersion(pluginName: string): string {
+    const result = taskLib.tool(NOTATION).arg(['plugin', 'list']).execSync();
+    if (result.code !== 0) {
+        return '';
+    }
+
+    const lines = result.stdout.split('\n');
+    for (const line of lines.slice(1)) {
+        // example:
+        // azure-kv  Notation Azure Key Vault plugin  1.1.0  [SIGNATURE_GENERATOR.RAW]  <nil> 
+        if (line.startsWith(pluginName)) {
+            const parts = line.trim().split('  ');
+            let nonEmptyParts = parts.filter(part => part !== '');
+            console.log(nonEmptyParts);
+            return nonEmptyParts.slice(-3)[0].trim();
+        }
+    }
+    return '';
 }
