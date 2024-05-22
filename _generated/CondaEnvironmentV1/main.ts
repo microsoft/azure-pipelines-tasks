a4348f032e33ef8d38b4610a4aa43cee8d0fa286
Ismayil Ismayilov <ismayil.ismayilov@akvelon.com>
2024-05-22 20:13:34 +0400

    Fix version map

--- _generated/CondaEnvironmentV1/main.ts
+++ _generated/CondaEnvironmentV1/main.ts
@@ -4,6 +4,9 @@ import { getPlatform } from './taskutil';
 import { condaEnvironment } from './conda';
 
 (async () => {
+    #if NODE20
+    let error: any | undefined;
+    #endif
     try {
         task.setResourcePath(path.join(__dirname, 'task.json'));
         await condaEnvironment({
@@ -15,14 +18,19 @@ import { condaEnvironment } from './conda';
             createOptions: task.getInput('createOptions'),
             cleanEnvironment: task.getBoolInput('cleanEnvironment')
         },
-        getPlatform());
+            getPlatform());
         let shouldFail = task.getVariable('FAIL_DEPRECATED_BUILD_TASK');
 
-	    if (shouldFail != null && shouldFail.toLowerCase() === 'true') {
-	        throw new Error(task.loc("DeprecatedTask"));
-	    }
+        if (shouldFail != null && shouldFail.toLowerCase() === 'true') {
+            throw new Error(task.loc("DeprecatedTask"));
+        }
         task.setResult(task.TaskResult.Succeeded, "");
     } catch (e) {
+        #if NODE20
+        error = e;
+        task.setResult(task.TaskResult.Failed, error.message);
+        #else
         task.setResult(task.TaskResult.Failed, e.message);
+        #endif
     }
 })();
