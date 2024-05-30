b4dd02cde3981b414b0efc58c65dbcd5eff11b69
Kirill Ivlev <v-kivlev@microsoft.com>
2024-05-30 17:17:53 +0400

    Add support of HTTP Proxy for AzureCLIV2 task

--- _generated/AzureCLIV2_Node20/azureclitask.ts
+++ _generated/AzureCLIV2_Node20/azureclitask.ts
@@ -14,6 +14,16 @@ export class azureclitask {
     public static async runMain(): Promise<void> {
         var toolExecutionError = null;
         var exitCode: number = 0;
+
+        if(tl.getBoolFeatureFlag('AZP_AZURECLIV2_SETUP_PROXY_ENV')) {
+            const proxyConfig: tl.ProxyConfiguration | null = tl.getHttpProxyConfiguration();
+            if (proxyConfig) {
+                process.env['HTTP_PROXY'] = proxyConfig.proxyFormattedUrl;
+                process.env['HTTPS_PROXY'] = proxyConfig.proxyFormattedUrl;
+                tl.debug(tl.loc('ProxyConfig', proxyConfig.proxyUrl));
+            }
+        }
+
         try{
             var scriptType: ScriptType = ScriptTypeFactory.getSriptType();
             var tool: any = await scriptType.getTool();
