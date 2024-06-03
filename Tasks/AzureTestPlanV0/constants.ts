4384c21e5660487da804dd745918aa72983bfc20
Shreya Singh <t-shreysingh@microsoft.com>
2024-06-03 10:19:57 +0530

    Adding support for go language.

--- Tasks/AzureTestPlanV0/constants.ts
+++ Tasks/AzureTestPlanV0/constants.ts
@@ -13,4 +13,4 @@ export const AUTOMATED_EXECUTION = "AutomatedExecutionPhase";
 export const AUTOMATED_PUBLISHING = "PublishingAutomatedResultsPhase";
 export const MANUALTESTS_PUBLISHING = "ManualTestResultsPublishingPhase";
 
-export const GO_EXECUTABLE: string = 'go';
+
