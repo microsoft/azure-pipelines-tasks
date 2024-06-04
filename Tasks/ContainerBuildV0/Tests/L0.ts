56905b871a3a8c3c0a3e012b5c2cf12a27250872
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2024-06-04 12:09:50 +0200

    [ContainerBuildV0] Fix CVEs

--- Tasks/ContainerBuildV0/Tests/L0.ts
+++ Tasks/ContainerBuildV0/Tests/L0.ts
@@ -2,8 +2,6 @@ import * as path from "path";
 import * as assert from "assert";
 import * as ttm from "azure-pipelines-task-lib/mock-test";
 import * as tl from "azure-pipelines-task-lib/task";
-import * as dockerCommandUtils from "azure-pipelines-tasks-docker-common/dockercommandutils";
-import * as pipelineutils from "azure-pipelines-tasks-docker-common/pipelineutils";
 import * as shared from "./TestShared";
 import ConsistentHashing = require("consistent-hashing");
 
@@ -14,12 +12,12 @@ describe("ContainerBuildV0 Suite", function () {
         return;
     }
 
-    before((done) => {
+    before(async () => {
         process.env[shared.TestEnvVars.operatingSystem] = tl.osType().match(/^Win/) ? shared.OperatingSystems.Windows : shared.OperatingSystems.Other;
-        done();
+        
     });
 
-    beforeEach(() => {
+    beforeEach(async () => {
         delete process.env[shared.TestEnvVars.runningOn];
         delete process.env[shared.TestEnvVars.dockerRegistryServiceConnection];
         delete process.env[shared.TestEnvVars.repository];
@@ -28,7 +26,7 @@ describe("ContainerBuildV0 Suite", function () {
         delete process.env[shared.TestEnvVars.tags];
     });
 
-    after(function () {
+    after(async function () {
         delete process.env['SYSTEM_TEAMFOUNDATIONCOLLECTIONURI'];
         delete process.env['BUILD_SOURCEVERSION'];
         delete process.env['RELEASE_RELEASEID'];
@@ -45,41 +43,39 @@ describe("ContainerBuildV0 Suite", function () {
     });
 
     // Docker build tests begin
-    it('Runs successfully for docker build', (done:Mocha.Done) => {
+    it('Runs successfully for docker build', async () => {
         let tp = path.join(__dirname, 'TestSetup.js');
         process.env[shared.TestEnvVars.repository] = "testuser/testrepo";
         let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync();
 
         assert(tr.invokedToolCount == 1, 'should have invoked tool one time. actual: ' + tr.invokedToolCount);
         assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
         assert(tr.succeeded, 'task should have succeeded');
         assert(tr.stdout.indexOf(`[command]docker build -f ${shared.formatPath("a/w/Dockerfile")} -t testuser/testrepo:11 ${shared.formatPath("a/w")}`) != -1, "docker build should run with expected arguments");
         console.log(tr.stderr);
-        done();
     });
 
-    it('Runs successfully for docker build with tags', (done:Mocha.Done) => {
+    it('Runs successfully for docker build with tags', async () => {
         let tp = path.join(__dirname, 'TestSetup.js');
         process.env[shared.TestEnvVars.repository] = "testuser/testrepo";
         process.env[shared.TestEnvVars.tags] = "tag1";
         let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync();
 
         assert(tr.invokedToolCount == 1, 'should have invoked tool one time. actual: ' + tr.invokedToolCount);
         assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
         assert(tr.succeeded, 'task should have succeeded');
         assert(tr.stdout.indexOf(`[command]docker build -f ${shared.formatPath("a/w/Dockerfile")} -t testuser/testrepo:tag1 ${shared.formatPath("a/w")}`) != -1, "docker build should run with expected arguments");
         console.log(tr.stderr);
-        done();
     });
 
-    it('Runs successfully for docker build and push', (done:Mocha.Done) => {
+    it('Runs successfully for docker build and push', async () => {
         let tp = path.join(__dirname, 'TestSetup.js');
         process.env[shared.TestEnvVars.dockerRegistryServiceConnection] = "dockerhubendpoint";
         process.env[shared.TestEnvVars.repository] = "testuser/testrepo";
         let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync();
 
         assert(tr.invokedToolCount == 2, 'should have invoked tool one time. actual: ' + tr.invokedToolCount);
         assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
@@ -87,16 +83,15 @@ describe("ContainerBuildV0 Suite", function () {
         assert(tr.stdout.indexOf(`[command]docker build -f ${shared.formatPath("a/w/Dockerfile")} -t testuser/testrepo:11 ${shared.formatPath("a/w")}`) != -1, "docker build should run with expected arguments");
         assert(tr.stdout.indexOf(`[command]docker push testuser/testrepo:11`) != -1, "docker push should run with expected arguments");
         console.log(tr.stderr);
-        done();
     });
 
-    it('Runs successfully for docker build and push with tags', (done:Mocha.Done) => {
+    it('Runs successfully for docker build and push with tags', async () => {
         let tp = path.join(__dirname, 'TestSetup.js');
         process.env[shared.TestEnvVars.dockerRegistryServiceConnection] = "dockerhubendpoint";
         process.env[shared.TestEnvVars.repository] = "testuser/testrepo";
         process.env[shared.TestEnvVars.tags] = "tag1";
         let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync();
 
         assert(tr.invokedToolCount == 2, 'should have invoked tool two times. actual: ' + tr.invokedToolCount);
         assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
@@ -104,16 +99,15 @@ describe("ContainerBuildV0 Suite", function () {
         assert(tr.stdout.indexOf(`[command]docker build -f ${shared.formatPath("a/w/Dockerfile")} -t testuser/testrepo:tag1 ${shared.formatPath("a/w")}`) != -1, "docker build should run with expected arguments");
         assert(tr.stdout.indexOf(`[command]docker push testuser/testrepo:tag1`) != -1, "docker push should run with expected arguments");
         console.log(tr.stderr);
-        done();
     });
 
-    it('Runs successfully for docker build and push with mutiple tags', (done:Mocha.Done) => {
+    it('Runs successfully for docker build and push with mutiple tags', async () => {
         let tp = path.join(__dirname, 'TestSetup.js');
         process.env[shared.TestEnvVars.dockerRegistryServiceConnection] = "dockerhubendpoint";
         process.env[shared.TestEnvVars.repository] = "testuser/testrepo";
         process.env[shared.TestEnvVars.tags] = "tag1\ntag2";
         let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync();
 
         assert(tr.invokedToolCount == 3, 'should have invoked tool three times. actual: ' + tr.invokedToolCount);
         assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
@@ -122,15 +116,14 @@ describe("ContainerBuildV0 Suite", function () {
         assert(tr.stdout.indexOf(`[command]docker push testuser/testrepo:tag1`) != -1, "docker push should run with expected arguments");
         assert(tr.stdout.indexOf(`[command]docker push testuser/testrepo:tag2`) != -1, "docker push should run with expected arguments");
         console.log(tr.stderr);
-        done();
     });
 
-    it('Runs successfully for docker build when registry other than Docker hub is used', (done:Mocha.Done) => {
+    it('Runs successfully for docker build when registry other than Docker hub is used', async () => {
         let tp = path.join(__dirname, 'TestSetup.js');
         process.env[shared.TestEnvVars.dockerRegistryServiceConnection] = "acrendpoint";
         process.env[shared.TestEnvVars.repository] = "testrepo";
         let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync();
 
         assert(tr.invokedToolCount == 2, 'should have invoked tool twice. actual: ' + tr.invokedToolCount);
         assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
@@ -138,15 +131,14 @@ describe("ContainerBuildV0 Suite", function () {
         assert(tr.stdout.indexOf(`[command]docker build -f ${shared.formatPath("a/w/Dockerfile")} -t testacr.azurecr.io/testrepo:11 ${shared.formatPath("a/w")}`) != -1, "docker build should run with expected arguments");
         assert(tr.stdout.indexOf(`[command]docker push testacr.azurecr.io/testrepo:11`) != -1, "docker push should run with expected arguments");
         console.log(tr.stderr);
-        done();
     });
 
-    it('Runs successfully for docker build when registry type is ACR and registry URL contains uppercase characters', (done:Mocha.Done) => {
+    it('Runs successfully for docker build when registry type is ACR and registry URL contains uppercase characters', async () => {
         let tp = path.join(__dirname, 'TestSetup.js');
         process.env[shared.TestEnvVars.dockerRegistryServiceConnection] = "acrendpoint2";
         process.env[shared.TestEnvVars.repository] = "testrepo";
         let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync();
 
         assert(tr.invokedToolCount == 2, 'should have invoked tool twice. actual: ' + tr.invokedToolCount);
         assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
@@ -154,44 +146,41 @@ describe("ContainerBuildV0 Suite", function () {
         assert(tr.stdout.indexOf(`[command]docker build -f ${shared.formatPath("a/w/Dockerfile")} -t testacr2.azurecr.io/testrepo:11 ${shared.formatPath("a/w")}`) != -1, "docker build should run with expected arguments");
         assert(tr.stdout.indexOf(`[command]docker push testacr2.azurecr.io/testrepo:11`) != -1, "docker push should run with expected arguments");
         console.log(tr.stderr);
-        done();
     });
 
-    it('Docker build should honour Dockerfile and buildcontext input', (done:Mocha.Done) => {
+    it('Docker build should honour Dockerfile and buildcontext input', async () => {
         let tp = path.join(__dirname, 'TestSetup.js');
         process.env[shared.TestEnvVars.repository] = "testuser/testrepo";
         process.env[shared.TestEnvVars.dockerFile] = shared.formatPath("a/w/meta/Dockerfile");
         process.env[shared.TestEnvVars.buildContext] = shared.formatPath("a/w/context");
         let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync();
 
         assert(tr.invokedToolCount == 1, 'should have invoked tool one time. actual: ' + tr.invokedToolCount);
         assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
         assert(tr.succeeded, 'task should have succeeded');
         assert(tr.stdout.indexOf(`[command]docker build -f ${shared.formatPath("a/w/meta/Dockerfile")} -t testuser/testrepo:11 ${shared.formatPath("a/w/context")}`) != -1, "docker build should run with expected arguments");
         console.log(tr.stderr);
-        done();
     });
 
     //buildctl
-    it('Buildctl should honour Dockerfile and buildcontext input', (done:Mocha.Done) => {
+    it('Buildctl should honour Dockerfile and buildcontext input', async () => {
         let tp = path.join(__dirname, 'TestSetup.js');
         process.env['RUNNING_ON'] = 'KUBERNETES';
         process.env[shared.TestEnvVars.repository] = "testuser/testrepo";
         process.env[shared.TestEnvVars.dockerFile] = shared.formatPath("a/w/meta/Dockerfile");
         process.env[shared.TestEnvVars.buildContext] = shared.formatPath("a/w/context");
         let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync();
 
         assert(tr.invokedToolCount == 1, 'should have invoked tool one time. actual: ' + tr.invokedToolCount);
         assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
         assert(tr.succeeded, 'task should have succeeded');
         assert(tr.stdout.indexOf(`[command]buildctl build --frontend=dockerfile.v0 --local=context=${shared.formatPath("a/w/context")} --local=dockerfile=${shared.formatPath("a/w/meta/")}`) != -1, "buildctl build should run with expected arguments");
         console.log(tr.stderr);
-        done();
     });
 
-    it('Buildctl should perform build as well as push if dockerregistryserviceconnect is present', (done:Mocha.Done) => {
+    it('Buildctl should perform build as well as push if dockerregistryserviceconnect is present', async () => {
         let tp = path.join(__dirname, 'TestSetup.js');
         process.env['RUNNING_ON'] = 'KUBERNETES';
         process.env[shared.TestEnvVars.dockerRegistryServiceConnection] = "dockerhubendpoint";
@@ -199,24 +188,23 @@ describe("ContainerBuildV0 Suite", function () {
         process.env[shared.TestEnvVars.dockerFile] = shared.formatPath("a/w/meta/Dockerfile");
         process.env[shared.TestEnvVars.buildContext] = shared.formatPath("a/w/context");
         let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync();
 
         assert(tr.invokedToolCount == 1, 'should have invoked tool one time. actual: ' + tr.invokedToolCount);
         assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
         assert(tr.succeeded, 'task should have succeeded');
         assert(tr.stdout.indexOf(`[command]buildctl build --frontend=dockerfile.v0 --local=context=${shared.formatPath("a/w/context")} --local=dockerfile=${shared.formatPath("a/w/meta/")} --exporter=image --exporter-opt=name=testuser/testrepo:11 --exporter-opt=push=true`) != -1, "buildctl build should run with expected arguments");
         console.log(tr.stderr);
-        done();
     });
 
-    it('Runs successfully for buildctl build and push with multiple tags', (done:Mocha.Done) => {
+    it('Runs successfully for buildctl build and push with multiple tags', async () => {
         let tp = path.join(__dirname, 'TestSetup.js');
         process.env['RUNNING_ON'] = 'KUBERNETES';
         process.env[shared.TestEnvVars.dockerRegistryServiceConnection] = "dockerhubendpoint";
         process.env[shared.TestEnvVars.repository] = "testuser/testrepo";
         process.env[shared.TestEnvVars.tags] = "tag1\ntag2";
         let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync();
 
         assert(tr.invokedToolCount == 1, 'should have invoked tool one time. actual: ' + tr.invokedToolCount);
         assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
@@ -224,23 +212,21 @@ describe("ContainerBuildV0 Suite", function () {
         //assert(tr.stdout.indexOf(`[command]docker build -f ${shared.formatPath("a/w/Dockerfile")} -t testuser/testrepo:tag1 ${shared.formatPath("a/w")}`) != -1, "docker build should run with expected arguments");
         assert(tr.stdout.indexOf(`[command]buildctl build --frontend=dockerfile.v0 --local=context=${shared.formatPath("a/w/**")} --local=dockerfile=${shared.formatPath("a/w/**/")} --exporter=image --exporter-opt=name=testuser/testrepo:tag1,testuser/testrepo:tag2 --exporter-opt=push=true`) != -1, "buildctl build should run with expected arguments");
         console.log(tr.stderr);
-        done();
     });
 
-    it('Runs successfully for buildctl build when registry other than Docker hub is used', (done:Mocha.Done) => {
+    it('Runs successfully for buildctl build when registry other than Docker hub is used', async () => {
         let tp = path.join(__dirname, 'TestSetup.js');
         process.env['RUNNING_ON'] = 'KUBERNETES';
         process.env[shared.TestEnvVars.dockerRegistryServiceConnection] = "acrendpoint";
         process.env[shared.TestEnvVars.repository] = "testrepo";
         let tr : ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-        tr.run();
+        await tr.runAsync();
 
         assert(tr.invokedToolCount == 1, 'should have invoked tool once. actual: ' + tr.invokedToolCount);
         assert(tr.stderr.length == 0 || tr.errorIssues.length, 'should not have written to stderr');
         assert(tr.succeeded, 'task should have succeeded');
         assert(tr.stdout.indexOf(`[command]buildctl build --frontend=dockerfile.v0 --local=context=${shared.formatPath("a/w/**")} --local=dockerfile=${shared.formatPath("a/w/**/")} --exporter=image --exporter-opt=name=testacr.azurecr.io/testrepo:11 --exporter-opt=push=true`) != -1, "buildctl build should run with expected arguments");
         console.log(tr.stderr);
-        done();
     });
 
     it('Consistent hash must be computed correctly', (done) => {
