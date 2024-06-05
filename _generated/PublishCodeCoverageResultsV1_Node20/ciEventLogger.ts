2c70daa925a6a3250fac16947dbc84921254d39d
Durga Vijay Prabhakar Ogirala (from Dev Box) <dogirala@microsoft.com>
2024-06-05 07:04:41 +0530

    Changes to fix PCCRV1

--- _generated/PublishCodeCoverageResultsV1_Node20/ciEventLogger.ts
+++ _generated/PublishCodeCoverageResultsV1_Node20/ciEventLogger.ts
@@ -2,32 +2,52 @@ import * as tl from 'azure-pipelines-task-lib/task';
 
 const area: string = 'CodeCoverage';
 const feature: string = 'PublishCodeCoverageResultsV1';
+const consolidatedCiData: { [key: string]: any; } = <{ [key: string]: any; }>{};
 
 function getDefaultProps() {
     return {
         releaseuri: tl.getVariable('Release.ReleaseUri'),
         releaseid: tl.getVariable('Release.ReleaseId'),
         builduri: tl.getVariable('Build.BuildUri'),
-        buildid: tl.getVariable('Build.Buildid')
+        buildid: tl.getVariable('Build.Buildid'),
+        osType: tl.osType()
     };
 }
 
-export function publishEvent(properties: { [key: string]: any }): void {
-    try {
-        tl.assertAgent('2.144.0');
-        publishTelemetry(area, feature, Object.assign(getDefaultProps(), properties));
+export function addToConsolidatedCi(key: string, value: any) {
+    consolidatedCiData[key] = value;
+}
 
+export function fireConsolidatedCi() {
+    publishEvent('publishCodeCoverageResultsTaskConsolidatedCiEvent', consolidatedCiData);
+}
+
+export function publishEvent(subFeature: string, properties: { [key: string]: any }): void {
+    try {
+        properties.subFeature = subFeature;
+        _writeTelemetry(area, feature, Object.assign(getDefaultProps(), properties));
     } catch (err) {
         tl.debug('Unable to publish telemetry due to lower agent version.');
     }
 }
 
-export function publishTelemetry(area: string, feature: string, properties: { [key: string]: any }): void {
-    const data = JSON.stringify(properties);
-    tl.debug('telemetry area: ' + area + ' feature: ' + feature + ' data: ' + data);
-    tl.command('telemetry.publish', { 'area': area, 'feature': feature }, data);
-}
 
-export interface ciDictionary {
-    [key: string]: any;
+function _writeTelemetry(area, feature, properties) {
+    // The telemetry command was added in agent version 2.120.0.
+    try {
+        var splitVersion = (process.env.AGENT_VERSION || '').split('.');
+        var major = parseInt(splitVersion[0] || '0');
+        var minor = parseInt(splitVersion[1] || '0');
+        if (major > 2 || (major == 2 && minor >= 120)) {
+            const data = JSON.stringify(properties);
+            tl.debug('telemetry area: ' + area + ' feature: ' + feature + ' data: ' + data);
+            tl.command('telemetry.publish', { 'area': area, 'feature': feature }, data);
+        }
+        else {
+            tl.debug(`cannot write telemetry in agent ${process.env.AGENT_VERSION}`);
+        }
+    }
+    catch (err) {
+        tl.debug(`Error in writing telemetry : ${err}`);
+    }
 }
