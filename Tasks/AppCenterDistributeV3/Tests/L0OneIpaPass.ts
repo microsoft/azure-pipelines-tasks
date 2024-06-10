3fc806bfa301ef88e2a7173f07e356bab86088d6
Dmitrii Bobreshev <106314398+DmitriiBobreshev@users.noreply.github.com>
2024-06-10 10:29:51 +0200

    [AppCenterTestV1,AppCenterDistributeV3] CVE fix (#19958)
    * [AppCenterDistributeV3] Fix CVEs
    
    * [AppCenterDistributeV3] Fix CVEs
    
    - Fixed CVEs
    
    * [AppCenterDistributeV3] Fix CVEs
    
    - Fixed tests
    
    * [AppCenterDistributeV3] Fix CVEs
    
    - Bump task version
    
    * [AppCenterDistributeV3] Fix CVEs
    
    - Fix Node20 test

--- Tasks/AppCenterDistributeV3/Tests/L0OneIpaPass.ts
+++ Tasks/AppCenterDistributeV3/Tests/L0OneIpaPass.ts
@@ -6,7 +6,7 @@ import fs = require('fs');
 import azureBlobUploadHelper = require('../azure-blob-upload-helper');
 import { basicSetup, mockFs, mockAzure } from './UnitTests/TestHelpers';
 
-const mockery = require('mockery');
+const libMocker = require('azure-pipelines-task-lib/lib-mocker');
 const nock = require('nock');
 
 let taskPath = path.join(__dirname, '..', 'appcenterdistribute.js');
@@ -33,6 +33,11 @@ nock('https://example.test')
         expiration_date: 1234567
     });
 
+nock('https://example.test')
+    .post('/v0.1/apps/testuser/testapp/releases/1/groups', {
+        id: "00000000-0000-0000-0000-000000000000",
+        mandatory_update: false
+    }).reply(200);
 // provide answers for task mock
 let a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
     "checkPath": {
@@ -50,7 +55,7 @@ let a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
 };
 tmr.setAnswers(a);
 
-const mockedFs = {...fs, ...mockFs()};
+const mockedFs = { ...fs, ...mockFs() };
 
 mockAzure();
 
@@ -59,5 +64,5 @@ tmr.registerMock('fs', mockedFs);
 
 tmr.run();
 
-mockery.deregisterMock('fs');
-mockery.deregisterMock('azure-blob-upload-helper');
+libMocker.deregisterMock('fs');
+libMocker.deregisterMock('azure-blob-upload-helper');
