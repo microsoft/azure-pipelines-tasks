ea3ef8b487b49c3c0446482bc113d26a8f23ed6c
Shreya Singh <t-shreysingh@microsoft.com>
2024-06-11 11:39:27 +0530

    Adding support for Go in ATP.

--- _generated/AzureTestPlanV0/constants.ts
+++ _generated/AzureTestPlanV0/constants.ts
@@ -12,3 +12,6 @@ export const MERGE_THRESHOLD = 100;
 export const AUTOMATED_EXECUTION = "AutomatedExecutionPhase";
 export const AUTOMATED_PUBLISHING = "PublishingAutomatedResultsPhase";
 export const MANUALTESTS_PUBLISHING = "ManualTestResultsPublishingPhase";
+export const INSTALL = 'install';
+export const GOTESTSUM_PACKAGE = 'gotest.tools/gotestsum@latest';
+export const GOTESTSUM_JUNITFILE = '--junitfile';
