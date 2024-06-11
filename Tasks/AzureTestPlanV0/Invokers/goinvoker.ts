6b0c9583ac3f37ecfb767f917b250da3ee9eebcd
Shreya Singh <t-shreysingh@microsoft.com>
2024-06-11 12:46:50 +0530

    Adding required changes in goinvoker.ts after review.

--- Tasks/AzureTestPlanV0/Invokers/goinvoker.ts
+++ Tasks/AzureTestPlanV0/Invokers/goinvoker.ts
@@ -10,17 +10,18 @@ export async function executeGoTests
     let command = constants.INSTALL;
     let argument = constants.GOTESTSUM_PACKAGE;
     const installGotestsum = await executeGo(command, argument);
-    console.log(installGotestsum);
+    tl.debug("Installing Gotestsum : " + installGotestsum);
 
+//testsToBeExecuted: go.mod/01-normal/normal.Test1,go.mod/01-normal/normal.Test11,go.mod/04-testmain.Test1,go.mod/05-parallel.TestSumParalel
     let i = 0;
     for (let tests of testsToBeExecuted) {
         try {
             const modifiedPath = utils.separatePath(tests);
             const modifiedTest = utils.separateTestName(tests);
-            const command = constants.GOTESTSUM_JUNITFILE;
+            const command = constants.GOTESTSUM_JUNITFILE;    
             const argument = `TEST-Go${i}-junit.xml -- ${modifiedPath} -v -run ^${modifiedTest}$ `;
             const testsToRun = await executeGotestsum(command, argument);
-            console.log(testsToRun);
+            tl.debug("Tests to Run : " + testsToRun);
         } catch (error) {
             console.error(`Error executing test case:`, error);
         }
