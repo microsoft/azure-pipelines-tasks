d05fe20144f77d40c60723ee884e2ad9a38e49db
Viral Vasaiwala <vivasa@microsoft.com>
2024-07-12 15:55:19 -0700

    Update warning changes

--- Tasks/PublishSymbolsV2/PublishSymbols.ts
+++ Tasks/PublishSymbolsV2/PublishSymbols.ts
@@ -9,7 +9,7 @@ import { IExecSyncResult, IExecOptions } from "azure-pipelines-task-lib/toolrunn
 
 const nodeVersion = parseInt(process.version.split('.')[0].replace('v', ''));
 if(nodeVersion < 16) {
-    console.warn("Support for symbolpublish task with node version less than 16 is ending at 07/31/2024. Please upgrade your agent to node 16 and above to avoid any failures in pipeline related to the task.");
+    tl.warning(tl.loc('Node10NotSupported', nodeVersion));
 }
 
 
