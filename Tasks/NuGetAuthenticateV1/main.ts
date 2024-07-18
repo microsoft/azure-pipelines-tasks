37187d44cfa2b4f20439cfd1cfc3e6da2cd44b60
Alex Torres <alex.torresv@outlook.com>
2024-07-18 13:33:42 -0700

    Adding task changes

--- Tasks/NuGetAuthenticateV1/main.ts
+++ Tasks/NuGetAuthenticateV1/main.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as tl from 'azure-pipelines-task-lib/task';
-import { installCredProviderToUserProfile, configureCredProvider } from 'azure-pipelines-tasks-artifacts-common/credentialProviderUtils'
+import { installCredProviderToUserProfile, configureCredProvider, configureEntraCredProvider } from 'azure-pipelines-tasks-artifacts-common/credentialProviderUtils'
 import { ProtocolType } from 'azure-pipelines-tasks-artifacts-common/protocols';
 import { getPackagingServiceConnections } from 'azure-pipelines-tasks-artifacts-common/serviceConnectionUtils'
 import { emitTelemetry } from 'azure-pipelines-tasks-artifacts-common/telemetry'
@@ -14,11 +14,31 @@ async function main(): Promise<void> {
         forceReinstallCredentialProvider = tl.getBoolInput("forceReinstallCredentialProvider", false);
         await installCredProviderToUserProfile(forceReinstallCredentialProvider);
 
-        // Configure the credential provider for both same-organization feeds and service connections
-        const serviceConnections = getPackagingServiceConnections('nuGetServiceConnections');
-        await configureCredProvider(ProtocolType.NuGet, serviceConnections);
+        var serviceConnections = [];
+
+        const feedUrl = tl.getInput("feedUrl");
+        const entraWifServiceConnectionName = tl.getInput("workloadIdentityServiceConnection");
+
+        if (feedUrl && entraWifServiceConnectionName)
+        {
+            // Is there a better way to enforce this? 
+            if (feedUrl.includes(",") || entraWifServiceConnectionName.includes(",")) {
+                throw new Error(tl.loc("MultipleValuesInSingleInput"));
+            }
+
+            await configureEntraCredProvider(ProtocolType.NuGet, feedUrl, entraWifServiceConnectionName);
+        }
+        else
+        {
+            // Configure the credential provider for both same-organization feeds and service connections
+            serviceConnections = getPackagingServiceConnections('nuGetServiceConnections');
+            await configureCredProvider(ProtocolType.NuGet, serviceConnections);
+        }
     } catch (error) {
-        tl.setResult(tl.TaskResult.Failed, error);
+        if (error.message.includes("existing service connection"))
+            tl.setResult(tl.TaskResult.SucceededWithIssues, error.message);
+        else
+            tl.setResult(tl.TaskResult.Failed, error);
     } finally {
         emitTelemetry("Packaging", "NuGetAuthenticateV1", {
             'NuGetAuthenticate.ForceReinstallCredentialProvider': forceReinstallCredentialProvider
