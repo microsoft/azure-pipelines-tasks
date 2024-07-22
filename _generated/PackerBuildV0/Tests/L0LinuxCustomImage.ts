39e6da239db67427d9285d71d2d180d5106d8aab
Dmitrii Bobreshev <106314398+DmitriiBobreshev@users.noreply.github.com>
2024-07-22 10:00:20 +0200

    msal-node fix incompatible version (#20170)
    * [AzureRmWebAppDeploymentV3/V4] fix msal-node error
    
    * [AzureWebAppContainerV1] fix msal-node error
    
    * [AzureWebAppV1] Remove node20 handler from default task
    
    * [ContainerBuildV0] fix msal-node error
    
    * [PackerBuildV0] fix msal-node error
    
    * [KubernetesManifestV1] fix msal-node error
    
    * [KubernetesV1] fix msal-node error
    
    * [DockerComposeV0/V1] fix msal-node error
    
    * [DockerV0/V1/V2] fix msal-node error
    
    * [KubernetesV0] fix msal-node error
    
    * [PackerBuildV0] fix getPackerVersion method
    
    - transffered the getPackerVersion method from the PackerBuildV1 task to the PackerBuildV0 task
     based on the PR https://github.com/microsoft/azure-pipelines-tasks/pull/19626/files
    
    * [PackerBuildV0] fix PackerBuildV0 e2e tests
    
    - transffered WIF implementation ref: https://github.com/microsoft/azure-pipelines-tasks/pull/19835
    
    * Revert "[PackerBuildV0] fix PackerBuildV0 e2e tests"
    
    This reverts commit 88e0c981f335f62d2015bbf37bd52f939ec86eb6.
    
    * Fixed old agent-base version
    
    ---------
    
    Co-authored-by: v-schhabra <100183307+v-schhabra@users.noreply.github.com>

--- _generated/PackerBuildV0/Tests/L0LinuxCustomImage.ts
+++ _generated/PackerBuildV0/Tests/L0LinuxCustomImage.ts
@@ -52,6 +52,10 @@ let a: any = <any>{
             "code": 0,
             "stdout": "0.12.3"
         },
+        "packer -machine-readable --version": {
+            "code": 0,
+            "stdout": "1234567,,version,1.2.4"
+        },
         "packer fix -validate=false /tmp/tempdir/100/custom.linux.template.json": {
             "code": 0,
             "stdout": "{ \"some-key\": \"some-value\" }"
