3e33638073b561d917e8b13188db845aeae575fa
Zheng Hao Tang <zhentan@microsoft.com>
2024-08-01 16:58:46 -0700

    Remove comments

--- _generated/NpmAuthenticateV0_Wif/npmauth.ts
+++ _generated/NpmAuthenticateV0_Wif/npmauth.ts
@@ -139,9 +139,9 @@ async function main(): Promise<void> {
         }
         if (registry) {
             tl.debug(tl.loc('AddingAuthRegistry', registry.url));
-            npmutil.appendToNpmrc(npmrc, os.EOL + registry.auth + os.EOL); //need this
+            npmutil.appendToNpmrc(npmrc, os.EOL + registry.auth + os.EOL);
             tl.debug(tl.loc('SuccessfulAppend'));
-            npmrcFile.push(os.EOL + registry.auth + os.EOL); //need this
+            npmrcFile.push(os.EOL + registry.auth + os.EOL);
             tl.debug(tl.loc('SuccessfulPush'));
         }
         else {
