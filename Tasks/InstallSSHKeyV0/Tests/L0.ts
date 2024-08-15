1cbcb25480366c34b81db19a5de465f6aee05556
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2024-08-15 18:32:54 +0400

    Update InstallSSHKeyV0

--- Tasks/InstallSSHKeyV0/Tests/L0.ts
+++ Tasks/InstallSSHKeyV0/Tests/L0.ts
@@ -11,88 +11,76 @@ describe('InstallSSHKey Suite', function () {
     after(() => {
     });
 
-    it('Start ssh-agent', (done: Mocha.Done) => {
+    it('Start ssh-agent', async () => {
         this.timeout(1000);
 
         let testPath: string = path.join(__dirname, 'L0StartAgent.js');
         let testRunner: MockTestRunner = new MockTestRunner(testPath);
 
-        testRunner.run();
+        await testRunner.runAsync();
 
         assert.equal(testRunner.stderr.length, 0, 'should not have written to stderr');
         assert(testRunner.succeeded, 'task should have succeeded');
-
-        done();
     });
 
-    it('Start ssh-agent (no public key specified)', (done: Mocha.Done) => {
+    it('Start ssh-agent (no public key specified)', async () => {
         this.timeout(1000);
 
         let testPath: string = path.join(__dirname, 'L0StartAgentWithoutPubKey.js');
         let testRunner: MockTestRunner = new MockTestRunner(testPath);
 
-        testRunner.run();
+        await testRunner.runAsync();
 
         assert.equal(testRunner.stderr.length, 0, 'should not have written to stderr');
         assert(testRunner.succeeded, 'task should have succeeded');
-
-        done();
     });
 
-    it('SSH key already installed', (done: Mocha.Done) => {
+    it('SSH key already installed', async () => {
         this.timeout(1000);
 
         let testPath: string = path.join(__dirname, 'L0KeyAlreadyInstalled.js');
         let testRunner: MockTestRunner = new MockTestRunner(testPath);
 
-        testRunner.run();
+        await testRunner.runAsync();
 
         assert(testRunner.failed, 'task should have failed');
         assert(testRunner.stdOutContained('loc_mock_SSHKeyAlreadyInstalled'), 'expected error: SSH key already installed');
-
-        done();
     });
 
-    it('SSH key already installed (no public key specified)', (done: Mocha.Done) => {
+    it('SSH key already installed (no public key specified)', async () => {
         this.timeout(1000);
 
         let testPath: string = path.join(__dirname, 'L0KeyAlreadyInstalledWithoutPubKey.js');
         let testRunner: MockTestRunner = new MockTestRunner(testPath);
 
-        testRunner.run();
+        await testRunner.runAsync();
 
         assert(testRunner.failed, 'task should have failed');
         assert(testRunner.stdOutContained('loc_mock_SSHKeyAlreadyInstalled'), 'expected error: SSH key already installed');
-
-        done();
     });
 
-    it('SSH key malformed', (done: Mocha.Done) => {
+    it('SSH key malformed', async () => {
         this.timeout(1000);
 
         let testPath: string = path.join(__dirname, 'L0KeyMalformed.js');
         let testRunner: MockTestRunner = new MockTestRunner(testPath);
 
-        testRunner.run();
+        await testRunner.runAsync();
 
         assert(testRunner.failed, 'task should have failed');
         assert(testRunner.stdOutContained('loc_mock_SSHPublicKeyMalformed'), 'expected error: SSH key malformed');
-
-        done();
     });
 
-    it('SSH key uninstalled from running agent', (done: Mocha.Done) => {
+    it('SSH key uninstalled from running agent', async () => {
         this.timeout(1000);
 
         const testPath: string = path.join(__dirname, 'L0RemoveFromAgent.js');
         const testRunner: MockTestRunner = new MockTestRunner(testPath);
 
-        testRunner.run();
+        await testRunner.runAsync();
 
         assert(testRunner.succeeded, 'task should have succeeded');
         assert(testRunner.stdOutContained('removed from running agent'), 'expected message: removed from running agent');
         assert(testRunner.ran('/usr/bin/ssh-add -d keyToRemove'),'ssh should have been uninstalled');
-
-        done();
     });
 });
