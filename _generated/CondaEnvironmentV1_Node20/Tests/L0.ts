da032bbc1cf718a6e9a37b0cdd25f9f4ee8d9a93
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2024-08-15 17:10:58 +0400

    Add test validator for CondaEnvironmentV1

--- _generated/CondaEnvironmentV1_Node20/Tests/L0.ts
+++ _generated/CondaEnvironmentV1_Node20/Tests/L0.ts
@@ -15,46 +15,63 @@ describe('CondaEnvironment L0 Suite', function () {
         require('./L0_conda_internal');
     });
 
-    it('succeeds when creating and activating an environment', async function() {
+    it('succeeds when creating and activating an environment', async function () {
         this.timeout(4000);
         const testFile = path.join(__dirname, 'L0CreateEnvironment.js');
         const testRunner = new MockTestRunner(testFile);
 
         await testRunner.runAsync();
 
-        if (getPlatform() === Platform.Windows) {
-            assert(testRunner.ran('conda create --quiet --prefix \\userprofile\\.conda\\envs\\test --mkdir --yes'));
-        } else {
-            assert(testRunner.ran('conda create --quiet --prefix /home/.conda/envs/test --mkdir --yes'));
-        }
+        runValidations(() => {
+            if (getPlatform() === Platform.Windows) {
+                assert(testRunner.ran('conda create --quiet --prefix \\userprofile\\.conda\\envs\\test --mkdir --yes'));
+            } else {
+                assert(testRunner.ran('conda create --quiet --prefix /home/.conda/envs/test --mkdir --yes'));
+            }
 
-        assert.strictEqual(testRunner.stderr.length, 0, 'should not have written to stderr');
-        assert(testRunner.succeeded, 'task should have succeeded');
+            assert.strictEqual(testRunner.stderr.length, 0, 'should not have written to stderr');
+            assert(testRunner.succeeded, 'task should have succeeded');
+        }, testRunner);
     });
 
-    it('succeeds when using the `base` environment', async function() {
+    it('succeeds when using the `base` environment', async function () {
         const testFile = path.join(__dirname, 'L0BaseEnvironment.js');
         const testRunner = new MockTestRunner(testFile);
 
         await testRunner.runAsync();
 
-        if (getPlatform() === Platform.Windows) {
-            assert(testRunner.ran('conda install python=3 --quiet --yes --json'));
-        } else {
-            assert(testRunner.ran('sudo /miniconda/bin/conda install python=3 --quiet --yes --json'));
-        }
+        runValidations(() => {
+            if (getPlatform() === Platform.Windows) {
+                assert(testRunner.ran('conda install python=3 --quiet --yes --json'));
+            } else {
+                assert(testRunner.ran('sudo /miniconda/bin/conda install python=3 --quiet --yes --json'));
+            }
 
-        assert.strictEqual(testRunner.stderr.length, 0, 'should not have written to stderr');
-        assert(testRunner.succeeded, 'task should have succeeded');
+            assert.strictEqual(testRunner.stderr.length, 0, 'should not have written to stderr');
+            assert(testRunner.succeeded, 'task should have succeeded');
+        }, testRunner);
     });
 
-    it('fails when a Conda installation is not found', async function() {
+    it('fails when a Conda installation is not found', async function () {
         const testFile = path.join(__dirname, 'L0CondaNotFound.js');
         const testRunner = new MockTestRunner(testFile);
 
         await testRunner.runAsync();
 
-        assert(testRunner.createdErrorIssue('loc_mock_CondaNotFound'));
-        assert(testRunner.failed, 'task should have failed');
+        runValidations(() => {
+            assert(testRunner.createdErrorIssue('loc_mock_CondaNotFound'));
+            assert(testRunner.failed, 'task should have failed');
+        }, testRunner);
     });
+
+    function runValidations(validator: () => void, mtr: MockTestRunner) {
+        try {
+            validator();
+        }
+        catch (error) {
+            console.log("STDERR", mtr.stderr);
+            console.log("STDOUT", mtr.stdout);
+            throw error;
+        }
+    }
 });
