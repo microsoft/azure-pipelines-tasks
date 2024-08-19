e0693ba32e8161e50f901d608ca207d6207a44fd
embetten <53092095+embetten@users.noreply.github.com>
2024-08-19 13:06:50 -0700

    NuGetAuthenticateV1: Add WIF Service Connection Support (#20299)
    * Adding task changes
    
    * Adding TaskJsonOverride to support new input
    
    * Adding generated wif
    
    * Updating logic in main, adding locstrings
    
    * task bump && bump common package
    
    * removing groupname from task json
    
    * adding url check and spelling
    
    * Removing duplicate error checking, try parse url
    
    * Improved feed url check
    
    * Adding telemetry for federatedFeedAuthCount
    
    * bumping azure-pipelines-task-lib
    
    * UX improvements
    
    * merge conflict fix
    
    * Bump Task Version
    
    * run --write-updates
    
    * Fix Url Validation and PR Comments
    
    * --write-updates
    
    * bump common task
    
    * --write-updates
    
    * --write-updates
    
    * write updates
    
    * add input validation
    
    * write updates
    
    * build validation passes locally
    
    * revert en-us changes
    
    * Add en-us strings
    
    * fix strings part 2
    
    * write updates
    
    ---------
    
    Co-authored-by: Alex Torres <alex.torresv@outlook.com>
    Co-authored-by: Alex Torres <95253638+AlexVTor@users.noreply.github.com>

--- _generated/NuGetAuthenticateV1_Wif/main.ts
+++ _generated/NuGetAuthenticateV1_Wif/main.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import * as tl from 'azure-pipelines-task-lib/task';
+import { configureEntraCredProvider } from "azure-pipelines-tasks-artifacts-common/credentialProviderUtils";
 import { installCredProviderToUserProfile, configureCredProvider} from 'azure-pipelines-tasks-artifacts-common/credentialProviderUtils'
 import { ProtocolType } from 'azure-pipelines-tasks-artifacts-common/protocols';
 import { getPackagingServiceConnections } from 'azure-pipelines-tasks-artifacts-common/serviceConnectionUtils'
@@ -16,6 +17,19 @@ async function main(): Promise<void> {
         forceReinstallCredentialProvider = tl.getBoolInput("forceReinstallCredentialProvider", false);
         await installCredProviderToUserProfile(forceReinstallCredentialProvider);
 
+        const feedUrl = tl.getInput("feedUrl");
+        const entraWifServiceConnectionName = tl.getInput("workloadIdentityServiceConnection");
+
+        if (feedUrl && entraWifServiceConnectionName) {
+            tl.debug(tl.loc("Info_AddingFederatedFeedAuth", entraWifServiceConnectionName, feedUrl));
+            await configureEntraCredProvider(ProtocolType.NuGet, feedUrl, entraWifServiceConnectionName);
+            console.log(tl.loc("Info_SuccessAddingFederatedFeedAuth", feedUrl));
+            federatedFeedAuthSuccessCount++;
+            
+            return;
+        }  else if (feedUrl || entraWifServiceConnectionName) {
+            throw new Error(tl.loc("Error_MissingFeedUrlOrServiceConnection"));
+        }
 
         // Configure the credential provider for both same-organization feeds and service connections
         var serviceConnections = getPackagingServiceConnections('nuGetServiceConnections');
