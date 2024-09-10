08fe39dd626355ac94a81aaa2e494d5a649df80c
Maksim Petrov (AKVELON INC) <v-mpetrov@microsoft.com>
2024-09-10 19:25:34 +0200

    Merge master

--- _generated/NuGetInstallerV0/Tests/L0.ts
+++ _generated/NuGetInstallerV0/Tests/L0.ts
@@ -9,15 +9,14 @@ describe('NuGetInstaller Suite', function () {
     after(() => {
     });
 
-    it('restore solution', (done: Mocha.Done) => {
+    it('restore solution', async () => {
         let tp = path.join(__dirname, 'singlesln.js')
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
-        tr.run()
+        await tr.runAsync();
         assert(tr.failed, 'should have failed');
         assert.equal(tr.errorIssues.length, 1, "should have 1 error");
         assert.equal(tr.errorIssues[0], "loc_mock_DeprecatedTask", "Error should be about deprecation");
-        done();
     }).timeout(20000);
 
 });
