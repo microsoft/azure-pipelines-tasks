27ed415c802608bb3fe7ef86766a37f321f3b083
Aditya Mankal <adityamankal@microsoft.com>
2024-09-19 18:29:43 +0530

    [JavaToolInstallerV1] Added JavaToolInstallerV1 Task

---
+++ Tasks/JavaToolInstallerV1/Tests/L0NoDestinationFolder.ts
@@ -0,0 +1,23 @@
+import mockanswer = require('azure-pipelines-task-lib/mock-answer');
+import mockrun = require('azure-pipelines-task-lib/mock-run');
+import path = require('path');
+
+const taskPath = path.join(__dirname, '..', 'javatoolinstaller.js');
+const srcPath = 'source/foo.zip';
+const destDir = '/destDir';
+const tr: mockrun.TaskMockRunner = new mockrun.TaskMockRunner(taskPath);
+tr.setInput("versionSpec", "8.1");
+tr.setInput("jdkSource", "Local Directory")
+tr.setInput("jdkPath", srcPath);
+//tr.setInput('destinationFolder', 'dirName');
+tr.setInput("cleanDestinationFolder", "false");
+
+// provide answers for task mock
+const a: mockanswer.TaskLibAnswers = <mockanswer.TaskLibAnswers>{
+    checkPath: { },
+    find: { },
+    rmRF: { },
+};
+tr.setAnswers(a);
+
+tr.run();
