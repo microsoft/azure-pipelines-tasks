81346c1c2ce62b3465b782d23a252605765a0b58
Ivan Duplenskikh <115665590+ivanduplenskikh@users.noreply.github.com>
2024-09-25 13:48:24 +0200

    Insert a break statement to exit the loop. Add gte to equality Handle only filtered tasks

--- ci/check-node-executor-agent-version.js
+++ ci/check-node-executor-agent-version.js
@@ -1,12 +1,33 @@
 const fs = require('fs');
 const path = require('path');
 const semver = require('semver');
+const { resolveTaskList } = require('./ci-util');
+
+const argv = require('minimist')(process.argv.slice(2));
+
+// --task "@({tasks_names})"
+if (!argv.task) {
+  console.log(`$(task_pattern) variable is empty or not set. Aborting...`);
+  process.exit(0);
+};
+
+const handleAllTasks = argv.task === 'all';
+const changedTasks = [];
+
+if (!handleAllTasks) {
+    changedTasks.push(...resolveTaskList(argv.task));
+
+    if (changedTasks.length === 0) {
+        console.log(`There are no changed tasks`);
+        process.exit(0);
+    }
+};
 
 const basePath = path.join(__dirname, '..', 'Tasks');
-const tasks = fs.readdirSync(basePath).map(x => ({
-    name: x,
-    path: path.resolve(basePath, x),
-})).filter(x => fs.existsSync(path.join(x.path, 'task.json')));
+const tasks = fs.readdirSync(basePath)
+    .filter(x => changedTasks.includes(x) || handleAllTasks)
+    .map(x => ({ name: x, path: path.resolve(basePath, x) }))
+    .filter(x => fs.existsSync(path.join(x.path, 'task.json')));
 
 const versionMap = new Map([
     ['Node10', '2.144.0'],
@@ -33,15 +54,17 @@ for (const task of tasks) {
     const handlers = Object.keys(taskJson.execution);
 
     for (const key of versionMap.entries()) {
-        if (handlers.includes(key[0]) && semver.gt(taskMinimumAgentVersion, key[1])) {
+        if (handlers.includes(key[0]) && semver.gte(taskMinimumAgentVersion, key[1])) {
             break;
         }
 
         problems.push(`The minimum agent version is not correct for ${key[0]} executor in ${task.name} task, should be min ${key[1]}`);
+        break;
     }
 }
 
 if (problems.length > 0) {
+    console.log(`Found ${problems.length} problems:`);
     console.log(problems.join('\n'));
     process.exit(1);
 }
