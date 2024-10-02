1bace2a901e3a3b66d1e30bb425942f6554d7245
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2024-10-02 13:18:35 +0400

    Update Get-TasksByCodeOwner

--- scripts/Get-TasksByCodeOwner.ps1
+++ scripts/Get-TasksByCodeOwner.ps1
@@ -1,10 +1,13 @@
 Param(
+    [Parameter(Position = 0, mandatory = $true)]
     [string]$OwnerName
 )
 
-$codeowners = Get-Content (Join-Path  $PSScriptRoot .. .github CODEOWNERS)
+$tasksDir = Join-Path $PSScriptRoot .. Tasks
 
 $tasks = $()
+
+$codeowners = Get-Content (Join-Path  $PSScriptRoot .. .github CODEOWNERS)
 $codeowners | ForEach-Object {
     if ($_ -notlike "* $($OwnerName)*") {
         return
@@ -14,11 +17,17 @@ $codeowners | ForEach-Object {
     }
 
     $taskName = $_ -replace 'Tasks/', '' -replace '/.*', ''
-    $tasks += , $taskName
+    $taskJson = Get-Content (Join-Path $tasksDir $taskName task.json) | ConvertFrom-Json -AsHashtable
+    $task = [pscustomobject]@{
+        Name = $taskName
+        Id   = $taskJson.id
+    }
+
+    $tasks += , $task
 }
 
 if ($tasks.Count -eq 0) {
     Write-Error "No tasks found for owner $OwnerName" -ErrorAction Stop
 }
 
-$tasks | Out-File -FilePath (Join-Path $PSScriptRoot "TaskNames_$($OwnerName -replace '(\\|\/)', '_').txt")
+return $tasks
