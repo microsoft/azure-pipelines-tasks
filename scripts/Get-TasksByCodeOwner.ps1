130112b13ee329e7476d1da809b87cbf3742b9dd
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2024-10-02 19:55:56 +0400

    Reuse Get-TasksInfo.ps1

--- scripts/Get-TasksByCodeOwner.ps1
+++ scripts/Get-TasksByCodeOwner.ps1
@@ -1,35 +1,15 @@
 Param(
-    [Parameter(Position = 0, mandatory = $true)]
+    [Parameter(Mandatory = $true)]
     [string]$OwnerName
 )
 
-$tasksDir = Join-Path $PSScriptRoot .. Tasks
+$tasks = (& "$(Join-Path $PSScriptRoot Get-TasksInfo.ps1)")
 
-$tasks = $()
-
-$codeowners = Get-Content (Join-Path  $PSScriptRoot .. .github CODEOWNERS)
-$codeowners | ForEach-Object {
-    if ($_ -notlike "* $($OwnerName)*") {
-        return
+$tasksByOwner = @()
+foreach ($task in $tasks) {
+    if ($task.Owners -contains $OwnerName) {
+        $tasksByOwner += , $task
     }
-    if ($_ -notlike '*Tasks/*') {
-        return
-    }
-
-    $taskName = $_ -replace 'Tasks/', '' -replace '/.*', ''
-    $taskJson = Get-Content (Join-Path $tasksDir $taskName task.json) | ConvertFrom-Json -AsHashtable
-    $task = [pscustomobject]@{
-        DirName = $taskName
-        Name     = $taskJson.name
-        Id       = $taskJson.id
-        Version  = "$($taskJson.version.Major).$($taskJson.version.Minor).$($taskJson.version.Patch)"
-    }
-
-    $tasks += , $task
 }
 
-if ($tasks.Count -eq 0) {
-    Write-Error "No tasks found for owner $OwnerName" -ErrorAction Stop
-}
-
-return $tasks
+return $tasksByOwner
