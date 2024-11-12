f6611ed1ac0c89f436e66eba9c85e7d56607b414
Philipson <v-phiv@microsoft.com>
2024-11-12 23:08:11 +0530

    2142647 Update AKS API used to get cluster credentials in AKS tasks

--- _generated/KubernetesV1_Node20/src/clusters/armkubernetescluster.ts
+++ _generated/KubernetesV1_Node20/src/clusters/armkubernetescluster.ts
@@ -3,17 +3,23 @@
 import tl = require('azure-pipelines-task-lib/task');
 import { AzureAksService } from 'azure-pipelines-tasks-azure-arm-rest/azure-arm-aks-service';
 import { AzureRMEndpoint } from 'azure-pipelines-tasks-azure-arm-rest/azure-arm-endpoint';
-import { AzureEndpoint, AKSCluster, AKSClusterAccessProfile} from 'azure-pipelines-tasks-azure-arm-rest/azureModels';
+import { AzureEndpoint, AKSClusterAccessProfile, AKSCredentialResult} from 'azure-pipelines-tasks-azure-arm-rest/azureModels';
+const USE_AKS_CREDENTIAL_API = tl.getBoolFeatureFlag('USE_AKS_CREDENTIAL_API')
 
 // get kubeconfig file content
 async function getKubeConfigFromAKS(azureSubscriptionEndpoint: string, resourceGroup: string, clusterName: string, useClusterAdmin?: boolean) : Promise<string> {
     var azureEndpoint: AzureEndpoint = await (new AzureRMEndpoint(azureSubscriptionEndpoint)).getEndpoint();
     var aks = new AzureAksService(azureEndpoint);
-    
+    tl.debug(`USE_AKS_CREDENTIAL_API Feature flag status is ${USE_AKS_CREDENTIAL_API}`);
     tl.debug(tl.loc("KubernetesClusterResourceGroup", clusterName, resourceGroup));
-
-    var clusterInfo : AKSClusterAccessProfile = await aks.getAccessProfile(resourceGroup, clusterName, useClusterAdmin);
-    var base64Kubeconfig = Buffer.from(clusterInfo.properties.kubeConfig, 'base64');
+    let base64Kubeconfig;
+    if (USE_AKS_CREDENTIAL_API) {
+        let clusterInfo : AKSCredentialResult = await aks.getClusterCredential(resourceGroup, clusterName, useClusterAdmin);
+        base64Kubeconfig = Buffer.from(clusterInfo.value, 'base64');
+    } else {
+        let clusterInfo : AKSClusterAccessProfile = await aks.getAccessProfile(resourceGroup, clusterName, useClusterAdmin);
+        base64Kubeconfig = Buffer.from(clusterInfo.properties.kubeConfig, 'base64');
+    }
     return base64Kubeconfig.toString();
 }
 
