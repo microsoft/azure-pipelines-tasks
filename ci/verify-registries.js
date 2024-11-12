ede0b2a0396c9ad0ff375cfa0b5c334f441eecb3
Ivan Duplenskikh <115665590+ivanduplenskikh@users.noreply.github.com>
2024-11-12 11:57:45 +0100

    Add the full path to incorrect package-lock.json paths

--- ci/verify-registries.js
+++ ci/verify-registries.js
@@ -1,7 +1,9 @@
 const { run } = require('./ci-util');
+const { resolve } = require('node:path');
 const { readFileSync } = require('node:fs');
 
-const packageLocks = run(`git --no-pager diff --name-only origin/master`).split('\n').filter(x => x.endsWith('package-lock.json'));
+const cwd = process.cwd();
+const packageLocks = run(`git --no-pager diff --name-only origin/master`).split('\n').filter(x => x.endsWith('package-lock.json')).map(x => resolve(cwd, x));
 const incorrectPackageLocks = [];
 
 for (const packageLock of packageLocks) {
