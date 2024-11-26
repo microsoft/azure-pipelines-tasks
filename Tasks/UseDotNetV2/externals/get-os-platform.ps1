3057335be951efd991144c0707d52f0c3216a187
Michel Zehnder <MichelZ@users.noreply.github.com>
2024-11-26 19:40:33 +0100

    Improvements

--- Tasks/UseDotNetV2/externals/get-os-platform.ps1
+++ Tasks/UseDotNetV2/externals/get-os-platform.ps1
@@ -1,6 +1,4 @@
 function Get-Machine-Architecture() {
-    Say-Invocation $MyInvocation
-
     # On PS x86, PROCESSOR_ARCHITECTURE reports x86 even on x64 systems.
     # To get the correct architecture, we need to use PROCESSOR_ARCHITEW6432.
     # PS x64 doesn't define this, so we fall back to PROCESSOR_ARCHITECTURE.
@@ -10,7 +8,7 @@ function Get-Machine-Architecture() {
     }
 
     try {        
-        if( ((Get-CimInstance -ClassName CIM_OperatingSystem).OSArchitecture) -like "ARM*") {
+        if( ((Get-CimInstance -ClassName CIM_OperatingSystem -OperationTimeoutSec 30).OSArchitecture) -like "ARM*") {
             if( [Environment]::Is64BitOperatingSystem )
             {
                 return "arm64"
@@ -26,12 +24,6 @@ function Get-Machine-Architecture() {
 }
 
 function Get-CLIArchitecture-From-Architecture([string]$Architecture) {
-    Say-Invocation $MyInvocation
-
-    if ($Architecture -eq "<auto>") {
-        $Architecture = Get-Machine-Architecture
-    }
-
     switch ($Architecture.ToLowerInvariant()) {
         { ($_ -eq "amd64") -or ($_ -eq "x64") } { return "x64" }
         { $_ -eq "x86" } { return "x86" }
