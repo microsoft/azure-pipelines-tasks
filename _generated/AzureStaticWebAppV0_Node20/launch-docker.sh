c7f2ab44439690dc0f1c1e26e7037d7da8a5ae3c
MerlynOMsft <44985659+merlynomsft@users.noreply.github.com>
2025-02-03 20:03:42 -0800

    Merge Node20 build configurations (#20819)
    * Set mergeBase: true in Node20 configs
    
    * Generated output from node make.js
    
    * make-options update
    
    * Skip downgrading check for this PR, as the changes are mechanical only
    
    * Empty-Commit
    
    * Empty-Commit
    
    * merge NuGetCommandV2 changes make-options.json
    
    * update NugetCommandV2
    
    * Trigger Build
    
    * Trigger Build
    
    * pipeline fix
    
    * Fix registry
    
    * Registry fix
    
    * Update package-lock with npm ci to fix cfs issues
    
    * Fix localbuild after cleanup
    
    * Fix package-lock.json sync
    
    * sync correct package.json
    
    * sync package.json
    
    * merge prep
    
    * merge
    
    * deletes
    
    * sync resources
    
    * merge continued
    
    * merge continued
    
    * wip
    
    * cleanup
    
    * add  --no-launch-profile
    
    * merge AzureTestPlanV0
    
    * merge

--- _generated/AzureStaticWebAppV0_Node20/launch-docker.sh
+++
@@ -1,6 +0,0 @@
-docker run \
-    --env-file ./env.list \
-    --pull=always \
-    -v "$SWA_WORKING_DIR:$SWA_WORKSPACE_DIR" \
-    "$SWA_DEPLOYMENT_CLIENT" \
-    ./bin/staticsites/StaticSitesClient run
