62c961505673ce3a093366cea7bbf255563fa8df
Ivan Duplenskikh <115665590+ivanduplenskikh@users.noreply.github.com>
2025-07-17 13:51:39 +0200

    Add the option to bypass the auditing step

--- make-util.js
+++ make-util.js
@@ -1,15 +1,20 @@
-var check = require('validator').default;
-var fs = require('fs');
-var makeOptions = require('./make-options.json');
-var minimatch = require('minimatch');
-var ncp = require('child_process');
-var os = require('os');
-var path = require('path');
-var process = require('process');
-var semver = require('semver');
-var shell = require('shelljs');
-const { XMLParser } = require("fast-xml-parser");
-const Downloader = require("nodejs-file-downloader");
+const ncp = require('child_process');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const process = require('process');
+
+const { XMLParser } = require('fast-xml-parser');
+const minimatch = require('minimatch');
+const minimist = require('minimist');
+const Downloader = require('nodejs-file-downloader');
+const check = require('validator').default;
+const semver = require('semver');
+const shell = require('shelljs');
+
+const makeOptions = require('./make-options.json');
+
+const args = minimist(process.argv.slice(2));
 
 // global paths
 var repoPath = __dirname;
@@ -157,6 +162,16 @@ exports.getCommonPackInfo = getCommonPackInfo;
 function performNpmAudit(taskPath) {
     console.log('\n🛫 Running npm audit...');
 
+    if (process.env['TF_BUILD']) {
+        console.log(`\x1b[A\x1b[K⏭️  Skipping npm audit in build pipeline because it is not supported in the pipeline.`);
+        return;
+    }
+
+    if (args.BypassNpmAudit) {
+        console.log(`\x1b[A\x1b[K⏭️  Skipping npm audit because --BypassNpmAudit argument is set.`);
+        return;
+    }
+
     try {
         const auditResult = ncp.spawnSync('npm', ['audit', '--prefix', taskPath, '--audit-level=high'], {
             stdio: 'pipe',
@@ -165,7 +180,10 @@ function performNpmAudit(taskPath) {
 
         if (auditResult.error) {
             console.log(`\x1b[A\x1b[K❌ npm audit failed because the build task at "${taskPath}" has vulnerable dependencies.`);
-            console.log(`👉 Please see details by running the command: npm audit --prefix ${taskPath}`);
+            console.log('👉 Please see details by running the command');
+            console.log(`\tnpm audit --prefix ${taskPath}`);
+            console.log('or execute the command with --BypassNpmAudit argument to skip the auditing');
+            console.log(`\tnode make.js --build --task ${args.task} --BypassNpmAudit`);
             process.exit(1);
         } else {
             console.log('\x1b[A\x1b[K✅ npm audit completed successfully.');
@@ -213,9 +231,7 @@ var buildNodeTask = function (taskPath, outDir, isServerBuild) {
         cd(taskPath);
     }
 
-    if (!process.env['TF_BUILD']) {
-        performNpmAudit(taskPath);
-    }
+    performNpmAudit(taskPath);
 
     // Use the tsc version supplied by the task if it is available, otherwise use the global default.
     if (overrideTscPath) {
