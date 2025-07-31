8e766db22259604a7eb5c4db0102e6ac46c75e11
Bhavesh Bhati <bhatibhavesh@microsoft.com>
2025-07-31 18:39:45 +0530

    Update make.js to remove the CI check

--- make.js
+++ make.js
@@ -229,10 +229,6 @@ CLI.gendocs = function() {
 //
 CLI.build = async function(/** @type {{ task: string }} */ argv)
 {
-    if (process.env.TF_BUILD) {
-        fail('Please use serverBuild for CI builds for proper validation');
-    }
-
     writeUpdatedsFromGenTasks = true;
     await CLI.serverBuild(argv);
 }
