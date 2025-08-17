f0496f72eafb01e8366e3afb9dea9e5ee172dd90
Akshay Kumar <kumaraksh@microsoft.com>
2025-08-17 17:10:44 +0530

    add changes for sitecontainers

---
+++ Tasks/AzureWebAppContainerV1/operations/SiteContainersDeploymentUtility.ts
@@ -0,0 +1,24 @@
+import tl = require('azure-pipelines-task-lib/task');
+import { AzureAppService } from "azure-pipelines-tasks-azure-arm-rest/azure-arm-app-service";
+import { AzureAppServiceUtility } from 'azure-pipelines-tasks-azure-arm-rest/azureAppServiceUtility';
+import { SiteContainer } from 'azure-pipelines-tasks-azure-arm-rest/azureModels';
+
+export class ContainerBasedDeploymentUtility {
+    private _appService: AzureAppService;
+    private _appServiceUtility: AzureAppServiceUtility;
+
+    constructor(appService: AzureAppService) {
+        this._appService = appService;
+        this._appServiceUtility = new AzureAppServiceUtility(appService);
+    }
+
+    public async updateSiteContainers(siteContainers: Array<SiteContainer>): Promise<void> {
+
+        for (const siteContainer of siteContainers) {
+            if (siteContainer.isEnabled) {
+                tl.debug(`Updating site container: ${siteContainer.name}`);
+                await this._appServiceUtility.updateSiteContainer(siteContainer);
+            }
+        }
+    }
+}
