b19ead1b8f919903f316ad35149fd1741ea3d767
Teodora Pavlović <tpavlovi@microsoft.com>
2025-08-21 13:51:39 +0200

    Added L0 tests

--- Tasks/AzureCLIV3/Tests/L0.ts
+++ Tasks/AzureCLIV3/Tests/L0.ts
@@ -1,16 +1,52 @@
-import fs = require('fs');
 import assert = require('assert');
 import path = require('path');
+import * as ttm from 'azure-pipelines-task-lib/mock-test';
 
 describe('AzureCLIV3 Suite', function () {
+    const timeout = 20000;
+
     before(() => {
     });
 
     after(() => {
     });
 
-    it('Does a basic hello world test', function (done: MochaDone) {
-        // TODO - add real tests
-        done();
+    it('Should handle Azure DevOps connection with Workload Identity Federation', function (done) {
+        this.timeout(timeout);
+
+        let tp = path.join(__dirname, 'L0AzureDevOpsWifConnection.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.runAsync().then(() => {
+            
+            assert(tr.stdout.includes('az --version'), 'Should execute az --version command');
+            assert(tr.stdout.includes('az extension add -n azure-devops'), 'Should install Azure DevOps extension');
+            assert(tr.stdout.includes('az login --service-principal'), 'Should login with service principal');
+            assert(tr.stdout.includes('az devops configure --defaults organization'), 'Should configure Azure DevOps organization');
+            assert(tr.stdout.includes('az devops configure --defaults project'), 'Should configure Azure DevOps project');
+            
+            assert(tr.stdout.indexOf('Azure DevOps CLI extension installed') >= 0, 'should install Azure DevOps extension');
+            assert(tr.stdout.indexOf('organization configured') >= 0, 'should configure organization');
+            assert(tr.stdout.indexOf('project configured') >= 0, 'should configure project');
+            done();
+        }).catch((err) => {
+            done(err);
+        });
     });
+
+    it('Should fail with unsupported authentication scheme for Azure DevOps', function (done) {
+        this.timeout(timeout);
+
+        let tp = path.join(__dirname, 'L0AzureDevOpsUnsupportedAuthScheme.js');
+        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+        tr.runAsync().then(() => {
+            assert(tr.failed, 'should have failed');
+            assert(tr.stdout.indexOf('loc_mock_AuthSchemeNotSupported ServicePrincipal') >= 0, 'Should have failed with unsupported auth scheme error');
+            done();
+        }).catch((err) => {
+            done(err);
+        });
+    });
+
 });
---
+++ Tasks/AzureCLIV3/Tests/L0AzureDevOpsWifConnection.ts
@@ -0,0 +1,102 @@
+import ma = require('azure-pipelines-task-lib/mock-answer');
+import tmrm = require('azure-pipelines-task-lib/mock-run');
+import path = require('path');
+
+let taskPath = path.join(__dirname, '..', 'azureclitask.js');
+let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+
+tmr.setInput('connectionType', 'azureDevOps');
+tmr.setInput('azureDevOpsServiceConnection', 'TestAzureDevOpsConnection');
+tmr.setInput('scriptType', 'bash');
+tmr.setInput('scriptLocation', 'inlineScript');
+tmr.setInput('inlineScript', 'echo "test"');
+tmr.setInput('failOnStandardError', 'false');
+tmr.setInput('visibleAzLogin', 'false');
+tmr.setInput('useGlobalConfig', 'false');
+tmr.setInput('cwd', 'C:\\test');
+
+process.env['ENDPOINT_AUTH_TestAzureDevOpsConnection'] = JSON.stringify({
+    scheme: 'WorkloadIdentityFederation',
+    parameters: {
+        serviceprincipalid: 'test-sp-id',
+        tenantid: 'test-tenant-id'
+    }
+});
+process.env['ENDPOINT_AUTH_SCHEME_TestAzureDevOpsConnection'] = 'WorkloadIdentityFederation';
+process.env['ENDPOINT_AUTH_PARAMETER_TestAzureDevOpsConnection_SERVICEPRINCIPALID'] = 'test-sp-id';
+process.env['ENDPOINT_AUTH_PARAMETER_TestAzureDevOpsConnection_TENANTID'] = 'test-tenant-id';
+
+process.env['ENDPOINT_DATA_TestAzureDevOpsConnection'] = JSON.stringify({
+    organizationUrl: 'https://dev.azure.com/testorg/'
+});
+process.env['ENDPOINT_URL_TestAzureDevOpsConnection'] = 'https://dev.azure.com/testorg/';
+
+process.env['SYSTEM_COLLECTIONURI'] = 'https://dev.azure.com/testorg/';
+process.env['SYSTEM_TEAMPROJECT'] = 'TestProject';
+process.env['AGENT_TEMPDIRECTORY'] = 'C:\\ado\\temp';
+process.env['AGENT_WORKFOLDER'] = 'C:\\ado';
+
+process.env['AZP_AZURECLIV2_SETUP_PROXY_ENV'] = 'false';
+process.env['ShowWarningOnOlderAzureModules'] = 'false';
+process.env['UseAzVersion'] = 'false';
+
+let mockAnswers: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
+    "which": {
+        "az": "az",
+        "bash": "bash"
+    },
+    "checkPath": {
+        "az": true,
+        "bash": true
+    },
+    "exec": {
+        "az --version": {
+            "code": 0,
+            "stdout": "azure-cli                         2.50.0"
+        },
+        "az extension add -n azure-devops -y": {
+            "code": 0,
+            "stdout": "Azure DevOps CLI extension installed"
+        },
+        "az login --service-principal -u \"test-sp-id\" --tenant \"test-tenant-id\" --allow-no-subscriptions --federated-token \"mock-token\" --output none": {
+            "code": 0,
+            "stdout": "Login successful"
+        },
+        "az devops configure --defaults organization=\"https://dev.azure.com/testorg/\"": {
+            "code": 0,
+            "stdout": "organization configured"
+        },
+        "az devops configure --defaults project=\"TestProject\"": {
+            "code": 0,
+            "stdout": "project configured"
+        },
+        "*": {
+            "code": 0,
+            "stdout": "test completed"
+        }
+    },
+    "exists": {
+        "bash": true,
+        "C:\\ado\\temp": true,
+        "C:\\ado": true
+    }
+};
+
+tmr.setAnswers(mockAnswers);
+
+tmr.registerMock('azure-devops-node-api', {
+    getHandlerFromToken: () => ({}),
+    WebApi: function() {
+        return {
+            getTaskApi: () => Promise.resolve({
+                createOidcToken: () => Promise.resolve({ oidcToken: 'mock-token' })
+            })
+        };
+    }
+});
+
+tmr.registerMock('azure-pipelines-tasks-artifacts-common/webapi', {
+    getSystemAccessToken: () => 'system-token'
+});
+
+tmr.run();
