0997d4048dacd0823b066f94b5acccd907c01fde
Teodora Pavlović <tpavlovi@microsoft.com>
2025-09-04 17:08:50 +0200

    updated openssl version

--- Tasks/Common/VstsAzureHelpers_/Utility.ps1
+++ Tasks/Common/VstsAzureHelpers_/Utility.ps1
@@ -1,5 +1,6 @@
 ﻿$featureFlags = @{
     retireAzureRM  = [System.Convert]::ToBoolean($env:RETIRE_AZURERM_POWERSHELL_MODULE)
+    useOpenssLatestVersion = [System.Convert]::ToBoolean($env:USE_OPENSSL_LATEST_VERSION_3_4_2)
 }
 
 function Add-Certificate {
@@ -362,8 +363,14 @@ function ConvertTo-Pfx {
     else {
         [System.IO.File]::WriteAllText($pfxPasswordFilePath, $pfxFilePassword, [System.Text.Encoding]::ASCII)
     }
-     $openSSLExePath = "$PSScriptRoot\opensslv4\openssl.exe"
-     $env:OPENSSL_CONF = "$PSScriptRoot\opensslv4\openssl.cnf"
+    if(-not $featureFlags.useOpenssLatestVersion) {
+        $openSSLExePath = "$PSScriptRoot\opensslv4\openssl.exe"
+        $env:OPENSSL_CONF = "$PSScriptRoot\opensslv4\openssl.cnf"
+    }
+    else{
+        $openSSLExePath = "$PSScriptRoot\opensslv3.4.2\openssl.exe"
+        $env:OPENSSL_CONF = "$PSScriptRoot\opensslv3.4.2\openssl.cnf"
+     }
      $env:RANDFILE=".rnd"
      $openSSLArgs = "pkcs12 -export -certpbe PBE-SHA1-3DES -keypbe PBE-SHA1-3DES -macalg sha1 -in `"$pemFilePath`" -out `"$pfxFilePath`" -password file:`"$pfxPasswordFilePath`""
      $procExitCode = Invoke-VstsProcess -FileName $openSSLExePath -Arguments $openSSLArgs -RequireExitCodeZero
