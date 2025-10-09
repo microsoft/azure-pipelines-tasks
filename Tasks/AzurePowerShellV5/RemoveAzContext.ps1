7ac65b6c6f95823327b850dd3b46a40b5b846c6c
Teodora Pavlović <tpavlovi@microsoft.com>
2025-10-09 15:19:29 +0200

    Fix for OpenSSL

--- Tasks/AzurePowerShellV5/RemoveAzContext.ps1
+++ Tasks/AzurePowerShellV5/RemoveAzContext.ps1
@@ -1,4 +1,5 @@
 . "$PSScriptRoot/Utility.ps1"
+Import-Module "$PSScriptRoot\ps_modules\VstsAzureHelpers_"
 . "$PSScriptRoot/ps_modules/VstsAzureHelpers_/Utility.ps1"
 
 Update-PSModulePathForHostedAgentLinux
--- Tasks/AzurePowerShellV5/azurepowershell.ts
+++ Tasks/AzurePowerShellV5/azurepowershell.ts
@@ -187,7 +187,8 @@ async function run() {
                 .arg('-ExecutionPolicy')
                 .arg('Unrestricted')
                 .arg('-Command')
-                .arg(`. '${path.join(path.resolve(__dirname),'RemoveAzContext.ps1')}'`);
+                .arg(`. '${path.join(path.resolve(__dirname),'RemoveAzContext.ps1')}'`)
+                .arg(`. '${path.join(path.resolve(__dirname),'ImportVstsAzureHelpers.ps1')}'`);
 
             let options = <tr.IExecOptions>{
                     cwd: input_workingDirectory,
