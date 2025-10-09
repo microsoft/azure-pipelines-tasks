acf314305a7a22a956090adf9ef5135028694766
Nemanja Rogic <nemanjarogic@microsoft.com>
2025-10-09 21:31:53 +0200

    Remove invalid import

--- Tasks/AzurePowerShellV5/ImportVstsAzureHelpers.ps1
+++ Tasks/AzurePowerShellV5/ImportVstsAzureHelpers.ps1
@@ -8,9 +8,5 @@ Write-Verbose "Import VstsAzureHelpers_ 3"
 $moduleName = "$PSScriptRoot\ps_modules\VstsAzureHelpers_"
 $module = Get-Module -Name $moduleName -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1
 
-Write-Verbose "Import VstsAzureHelpers_ 4"
-$moduleName2 = $PSScriptRoot\ps_modules\VstsAzureHelpers_
-$module2 = Get-Module -Name $moduleName2 -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1
-
 . "$PSScriptRoot/Utility.ps1"
 . "$PSScriptRoot/ps_modules/VstsAzureHelpers_/Utility.ps1"
--- Tasks/AzurePowerShellV5/RemoveAzContext.ps1
+++ Tasks/AzurePowerShellV5/RemoveAzContext.ps1
@@ -8,10 +8,6 @@ Write-Verbose "Import VstsAzureHelpers_ 3c"
 $moduleName = "$PSScriptRoot\ps_modules\VstsAzureHelpers_"
 $module = Get-Module -Name $moduleName -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1
 
-Write-Verbose "Import VstsAzureHelpers_ 4c"
-$moduleName2 = $PSScriptRoot\ps_modules\VstsAzureHelpers_
-$module2 = Get-Module -Name $moduleName2 -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1
-
 . "$PSScriptRoot/Utility.ps1"
 . "$PSScriptRoot/ps_modules/VstsAzureHelpers_/Utility.ps1"
 
