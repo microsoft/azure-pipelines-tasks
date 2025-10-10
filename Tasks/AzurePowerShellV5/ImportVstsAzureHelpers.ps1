0cb11a059da2d0098b2a2993b6b41b4040b8a675
Nemanja Rogic <nemanjarogic@microsoft.com>
2025-10-10 10:54:26 +0200

    Remove unnecessary changes

--- Tasks/AzurePowerShellV5/ImportVstsAzureHelpers.ps1
+++
@@ -1,5 +0,0 @@
-Import-Module "$PSScriptRoot\ps_modules\VstsTaskSdk" -ArgumentList @{ NonInteractive = $true }
-Import-Module $PSScriptRoot\ps_modules\VstsTaskSdk -ArgumentList @{ NonInteractive = $true }
-
-. "$PSScriptRoot/Utility.ps1"
-. "$PSScriptRoot/ps_modules/VstsAzureHelpers_/Utility.ps1"
