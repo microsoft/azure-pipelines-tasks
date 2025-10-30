d70c0cc32fdc4b9f855292b00b608a2b2534f9c7
dmiri <david.miric@microsoft.com>
2025-10-30 13:05:01 +0100

    Extend GoToolV0 task with optional GoDownloadUrl parameter and env variable GOTOOL_GODOWNLOADURL
    * Configure gotools task to pass custom url from customer
    
    * Update task to fetch latest patch version
    
    * Add caching to aka.ms go version
    
    * Refactor to address revision from microsoft go build
    
    * Update Tasks/GoToolV0/gotool.ts
    
    Co-authored-by: Davis Goodin <dagood@users.noreply.github.com>
    
    * Unify baseurl parsing
    
    * Update comment
    
    * bump the version to current sprint
    
    * Use download URL as optional parameter instead of download source and recreate unit tests
    
    * Add unit tests and fix download official URL
    
    * Try to fix unit test
    
    * Purify unit tests
    
    * Add anti SSRF check unit test
    
    * Update input parameter type and readme
    
    * Implement PR comments
    
    * Address PR comments
    
    * Implement PR comments and do minor refactoring in gotool.ts
    
    * Add environment variable
    
    * Code polishing
    
    * Rename env variable
    
    * Extract URL resolution to separate method
    
    * Remove base from names of task parameter and environment variable
    
    * Update readme
    
    * Update param description in task.json and resources
    
    * Update readme
    
    * Reorder functions in gotool.ts
    
    ---------
    
    Co-authored-by: sb464f <32311730+sb464f@users.noreply.github.com>
    Co-authored-by: Davis Goodin <dagood@users.noreply.github.com>
    Co-authored-by: nagarajku <151506811+nagarajku@users.noreply.github.com>
    Co-authored-by: Razvan Manole <razvanmanole@microsoft.com>

---
+++ Tasks/GoToolV0/Tests/L0InvalidVersionFormatParseError.ts
@@ -0,0 +1,27 @@
+import ma = require('azure-pipelines-task-lib/mock-answer');
+import tmrm = require('azure-pipelines-task-lib/mock-run');
+import path = require('path');
+
+let taskPath = path.join(__dirname, '..', 'gotool.js');
+let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+
+// Set invalid version format that fails parsing (e.g., "1.x" with letter)
+tmr.setInput('version', '1.x');
+
+// Mock environment variables
+process.env['Agent.TempDirectory'] = path.join(__dirname, 'temp');
+
+// Mock os module
+tmr.registerMock('os', {
+    platform: () => 'linux',
+    arch: () => 'x64'
+});
+
+// Mock telemetry (may not be called due to early failure)
+tmr.registerMock('azure-pipelines-tasks-utility-common/telemetry', {
+    emitTelemetry: function(area: string, feature: string, properties: any) {
+        console.log(`Telemetry: ${area}.${feature}`);
+    }
+});
+
+tmr.run();
