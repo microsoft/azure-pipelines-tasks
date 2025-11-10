d973b0b36b39a98bdb2ac47c128ffa5c02cc4b6f
Benoit Tigeot <benoit.tigeot@lifen.fr>
2025-11-10 15:54:12 +0100

    Filter all Helm prerelease versions (beta, alpha, rc) in HelmInstaller
    Prevent installing unreleased final version
    
    Related:
    - https://github.com/helm/helm/issues/31472
    
    We will probably have to limit to v3 soon.
    
    Signed-off-by: Benoit Tigeot <benoit.tigeot@lifen.fr>

--- Tasks/HelmInstallerV1/Tests/L0.ts
+++ Tasks/HelmInstallerV1/Tests/L0.ts
@@ -13,4 +13,19 @@ describe('HelmInstallerV1 Suite', function () {
         // TODO - add real tests
         done();
     });
+
+    it('Should filter out prerelease versions using prerelease field', function() {
+        const mockResponse = [
+            { tag_name: "v4.0.0-rc.1", prerelease: true },
+            { tag_name: "v4.0.0-beta.1", prerelease: true },
+            { tag_name: "v4.0.0-alpha.1", prerelease: true },
+            { tag_name: "v3.15.4", prerelease: false },
+            { tag_name: "v3.15.3", prerelease: false }
+        ];
+
+        const stableVersions = mockResponse.filter(r => !r.prerelease);
+        assert.strictEqual(stableVersions.length, 2, 'Should filter out all prerelease versions');
+        assert.strictEqual(stableVersions[0].tag_name, 'v3.15.4');
+        assert.strictEqual(stableVersions[1].tag_name, 'v3.15.3');
+    });
 });
