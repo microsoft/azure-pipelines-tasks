70df218eedfef53131fa41d3070f8993debfef29
Madala Sai Deekshitha <madalas@microsoft.com>
2025-11-13 14:15:31 +0530

    Adding configs support to build command

--- make-util.js
+++ make-util.js
@@ -1876,18 +1876,23 @@ exports.ensureBuildConfigGeneratorPrereqs = ensureBuildConfigGeneratorPrereqs;
  * @param {Boolean} includeLocalPackagesBuildConfig When set to true, generate LocalPackages BuildConfig
  * @param {Boolean} useSemverBuildConfig When set to true, use semver build config and A/B releases
  */
-var processGeneratedTasks = function (baseConfigToolPath, taskList, makeOptions, writeUpdates, sprintNumber, debugAgentDir, includeLocalPackagesBuildConfig, useSemverBuildConfig) {
+var processGeneratedTasks = function (baseConfigToolPath, taskList, makeOptions, writeUpdates, sprintNumber, debugAgentDir, includeLocalPackagesBuildConfig, useSemverBuildConfig, configs) {
     if (!makeOptions) fail("makeOptions is not defined");
     if (sprintNumber && !Number.isInteger(sprintNumber)) fail("Sprint is not a number");
 
     var tasks = taskList.join('|')
     ensureBuildConfigGeneratorPrereqs(baseConfigToolPath);
-    var programPath = `dotnet run --no-launch-profile --project "${baseConfigToolPath}/BuildConfigGen.csproj" -- `
+    var programPath = `dotnet run --no-launch-profile --project "${baseConfigToolPath}/BuildConfigGen.csproj" `
 
     const args = [
         "--task",
         `"${tasks}"`
     ];
+
+    if (configs) {
+        args.push("--configs");
+        args.push(`"${configs}"`);
+    }
 
     if (sprintNumber) {
         args.push("--current-sprint");
