5427899ddfcf2c22c87d920dd3fa4a03af6a8eb6
Ivan Duplenskikh <115665590+ivanduplenskikh@users.noreply.github.com>
2025-11-26 11:58:36 +0100

    Implement makeLatest input for GitHubRelease task
    - Added new input option 'makeLatest' to specify if the release should be marked as the latest release.
    - Bump dependencies in package.json and package-lock.json:
      - Updated azure-pipelines-tasks-utility-common to version 3.266.0
      - Updated brace-expansion to version 1.1.12
    - Increment task version to 1.266 in task.json and task.loc.json

--- Tasks/GitHubReleaseV1/Tests/ActionL0Tests.ts
+++ Tasks/GitHubReleaseV1/Tests/ActionL0Tests.ts
@@ -9,17 +9,16 @@ export class ActionL0Tests {
     }
 
     public static async validateCreateReleaseAction() {
-        await new Action().createReleaseAction("endpoint", "repo", "target", "tagName", "title", "note", false, false, []);
+        await new Action().createReleaseAction("endpoint", "repo", "target", "tagName", "title", "note", false, false, [], "false");
     }
 
     public static async validateEditReleaseAction() {
-        await new Action().editReleaseAction("endpoint", "repo", "target", "tagName", "title", "note", false, false, [], "id");
+        await new Action().editReleaseAction("endpoint", "repo", "target", "tagName", "title", "note", false, false, [], "id", "false");
     }
 
     public static async validateDeleteReleaseAction() {
         await new Action().deleteReleaseAction("endpoint", "repo", "tag");
     }
-
 }
 
 ActionL0Tests.startTests();
