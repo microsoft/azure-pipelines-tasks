93bc14da3cf45c77042f111c7fdf58fdd77d879b
dassayantan24 <dassayantan@microsoft.com>
2025-12-02 12:39:01 +0530

    Update teams notifictaion

--- ci/courtesy-push/send-notification.ps1
+++ ci/courtesy-push/send-notification.ps1
@@ -2,11 +2,13 @@
 # Body of message is compiled as Office 365 connector card
 # More details about cards - https://docs.microsoft.com/en-us/microsoftteams/platform/task-modules-and-cards/cards/cards-reference#office-365-connector-card
 
-$wikiLink = "[Wiki](https://mseng.visualstudio.com/AzureDevOps/_wiki/wikis/AzureDevOps.wiki/25317/Release-of-pipeline-tasks)"
+$wikiLink = "[Wiki](https://mseng.visualstudio.com/AzureDevOps/_wiki/wikis/AzureDevOps.wiki/49963/Task-Release-Process)"
 
 if ($env:PR_ID) {
     $pullRequestLink = "[PR $env:PR_ID]($env:PR_LINK)"
-    $title = "Courtesy Bump of Tasks PR created - ID $env:PR_ID"
+    $pipelineUrl = "$env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI$env:SYSTEM_TEAMPROJECT/_build/results?buildId=$env:BUILD_BUILDID&_a=summary"
+    $pipelineLink = "[Build $env:BUILD_BUILDID]($pipelineUrl)"
+    $title = "Courtesy Bump of Tasks PR created - $pipelineLink"
     $text = "Created Courtesy Bump of Tasks PR. Please review and approve/merge $pullRequestLink. Related article in $wikiLink."
     $themeColor = "#FFFF00"
 }
