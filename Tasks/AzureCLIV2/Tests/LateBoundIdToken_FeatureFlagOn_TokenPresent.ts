8c7128348f050e6f3b770756a5b859e2df36200e
miricdavid <david.miric@microsoft.com>
2025-12-10 16:16:00 +0100

    Add unit tests

---
+++ Tasks/AzureCLIV2/Tests/LateBoundIdToken_FeatureFlagOn_TokenPresent.ts
@@ -0,0 +1,100 @@
+import ma = require('azure-pipelines-task-lib/mock-answer');
+import tmrm = require('azure-pipelines-task-lib/mock-run');
+import path = require('path');
+
+let taskPath = path.join(__dirname, '..', 'azureclitask.js');
+let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+
+// Inputs
+tmr.setInput('connectedServiceNameARM', 'AzureRM');
+tmr.setInput('scriptType', 'bash');
+tmr.setInput('scriptLocation', 'inlineScript');
+tmr.setInput('inlineScript', 'echo hello');
+tmr.setInput('cwd', '/tmp');
+tmr.setInput('visibleAzLogin', 'true');
+
+// Environment variables for Feature Flag
+process.env['DISTRIBUTEDTASK_TASKS_EnableLateBoundIdToken'] = 'true';
+
+// Mock Endpoint
+process.env['ENDPOINT_URL_AzureRM'] = 'https://management.azure.com/';
+process.env['ENDPOINT_AUTH_AzureRM'] = '{"parameters":{"serviceprincipalid":"spId","serviceprincipalkey":"spKey","tenantid":"tenantId","idToken":"myLateBoundToken"},"scheme":"WorkloadIdentityFederation"}';
+process.env['ENDPOINT_AUTH_SCHEME_AzureRM'] = 'WorkloadIdentityFederation';
+process.env['ENDPOINT_AUTH_PARAMETER_AzureRM_SERVICEPRINCIPALID'] = 'spId';
+process.env['ENDPOINT_AUTH_PARAMETER_AzureRM_SERVICEPRINCIPALKEY'] = 'spKey';
+process.env['ENDPOINT_AUTH_PARAMETER_AzureRM_TENANTID'] = 'tenantId';
+process.env['ENDPOINT_AUTH_PARAMETER_AzureRM_IDTOKEN'] = 'myLateBoundToken';
+process.env['ENDPOINT_DATA_AzureRM'] = '{"environment":"AzureCloud"}';
+
+// Mock Telemetry
+tmr.registerMock('azure-pipelines-tasks-artifacts-common/telemetry', {
+    emitTelemetry: (area, feature, data) => {
+        console.log(`MOCK_TELEMETRY: ${area}, ${feature}, ${JSON.stringify(data)}`);
+    }
+});
+
+// Mock WebApi (should not be called in this case)
+tmr.registerMock('azure-devops-node-api', {
+    getHandlerFromToken: () => {},
+    WebApi: class {
+        getTaskApi() { return { createOidcToken: () => { throw new Error("Should not be called"); } }; }
+    }
+});
+
+// Mock Utility
+tmr.registerMock('./src/Utility', {
+    Utility: {
+        checkIfAzurePythonSdkIsInstalled: () => true,
+        throwIfError: () => {}
+    }
+});
+
+// Mock ScriptType
+tmr.registerMock('./src/ScriptType', {
+    ScriptTypeFactory: {
+        getSriptType: () => {
+            return {
+                getTool: () => {
+                    return {
+                        exec: () => Promise.resolve(0),
+                        on: () => {}
+                    };
+                },
+                cleanUp: () => Promise.resolve()
+            };
+        }
+    }
+});
+
+// Mock azCliUtility
+tmr.registerMock('azure-pipelines-tasks-azure-arm-rest/azCliUtility', {
+    validateAzModuleVersion: () => Promise.resolve()
+});
+
+// Mock toolrunner
+tmr.registerMock('azure-pipelines-task-lib/toolrunner', require('azure-pipelines-task-lib/mock-toolrunner'));
+
+// Answers
+let a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
+    "which": {
+        "az": "az"
+    },
+    "checkPath": {
+        "az": true
+    },
+    "exec": {
+        "az --version": {
+            "code": 0,
+            "stdout": "azure-cli 2.66.0"
+        },
+        "az account clear": {
+            "code": 0
+        },
+        "az login --service-principal -u \"spId\" --tenant \"tenantId\" --allow-no-subscriptions --federated-token \"myLateBoundToken\"": {
+            "code": 0
+        }
+    }
+};
+tmr.setAnswers(a);
+
+tmr.run();
