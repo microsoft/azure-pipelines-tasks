f3e4178fd38cf9cacb745c7c1fb5ca0de7fe6dc6
Abhishek Raj (LTIMindtree Limited) <v-abhishera@microsoft.com>
2026-01-08 23:27:47 +0530

    Revert "addition of --tiller-namespace based on helm version"
    This reverts commit 1495a3db133b020224131430a38e16b0a9bf4422.

--- Tasks/HelmDeployV1/src/commoncommandoption.ts
+++ Tasks/HelmDeployV1/src/commoncommandoption.ts
@@ -8,12 +8,7 @@ export function addArguments(helmCli: helmcli) : void {
     var debugMode = tl.getVariable('system.debug');
 
     if(tillernamespace) {
-        // --tiller-namespace flag only exists in Helm v2, not in v3+
-        if (!helmCli.isHelmV3orHigher()) {
-            helmCli.addArgument("--tiller-namespace ".concat(tillernamespace));
-        } else {
-            tl.warning("The 'tillernamespace' parameter is ignored in Helm v3 and higher as Tiller has been removed.");
-        }
+        helmCli.addArgument("--tiller-namespace ".concat(tillernamespace));
     }
 
     if(debugMode === 'true') {
