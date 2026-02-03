0977cd44725a61aae84e918fa5cefc60b429fa6d
D Kodanda Ramudu (LTIMindtree Limited) <v-dko@microsoft.com>
2026-02-03 14:21:33 +0530

    modified code as per suggestions

--- Tasks/JenkinsDownloadArtifactsV1/AzureStorageArtifacts/azureBlobStorageProvider.ts
+++ Tasks/JenkinsDownloadArtifactsV1/AzureStorageArtifacts/azureBlobStorageProvider.ts
@@ -20,15 +20,13 @@ export class AzureBlobProvider implements models.IArtifactProvider {
             this._prefixFolderPath = "";
         }
         
-        // Migration: azure.createBlobService() -> new BlobServiceClient() with StorageSharedKeyCredential
         const sharedKeyCredential = new StorageSharedKeyCredential(storageAccount, accessKey);
         const blobEndpoint = host || `https://${storageAccount}.blob.core.windows.net`;
-        this._blobServiceClient = new BlobServiceClient(blobEndpoint, sharedKeyCredential);
-        this._containerClient = this._blobServiceClient.getContainerClient(container);
+        const blobServiceClient = new BlobServiceClient(blobEndpoint, sharedKeyCredential);
+        this._containerClient = blobServiceClient.getContainerClient(container);
         this._addPrefixToDownloadedItems = !!addPrefixToDownloadedItems;
     }
 
-    // Migration: callback-based -> async/await with blockBlobClient.uploadStream()
     public async putArtifactItem(item: models.ArtifactItem, readStream: NodeJS.ReadableStream): Promise<models.ArtifactItem> {
         await this._ensureContainerExistence();
 
@@ -37,10 +35,8 @@ export class AzureBlobProvider implements models.IArtifactProvider {
 
         try {
             const blockBlobClient = this._containerClient.getBlockBlobClient(blobPath);
-            // Migration: createWriteStreamToBlockBlob -> uploadStream
             await blockBlobClient.uploadStream(readStream as Readable);
             
-            // Migration: blobSvc.getUrl() -> blockBlobClient.url
             var blobUrl = blockBlobClient.url;
             console.log(tl.loc("CreatedBlobForItem", item.path, blobUrl));
             item.metadata["destinationUrl"] = blobUrl;
@@ -59,17 +55,14 @@ export class AzureBlobProvider implements models.IArtifactProvider {
         throw new Error(tl.loc("GetArtifactItemsNotSupported"));
     }
 
-    // Migration: callback-based createReadStream -> async blobClient.download()
     public async getArtifactItem(artifactItem: models.ArtifactItem): Promise<NodeJS.ReadableStream> {
         let blobPath: string;
         if (!this._addPrefixToDownloadedItems && !!this._prefixFolderPath) {
-            // Adding prefix path to get the absolute path
             blobPath = this._prefixFolderPath + artifactItem.path;
         } else {
             blobPath = artifactItem.path;
         }
         
-        // Migration: blobSvc.createReadStream() -> blobClient.download().readableStreamBody
         const blobClient = this._containerClient.getBlobClient(blobPath);
         const downloadResponse = await blobClient.download(0);
         return downloadResponse.readableStreamBody as NodeJS.ReadableStream;
@@ -78,12 +71,11 @@ export class AzureBlobProvider implements models.IArtifactProvider {
     public dispose() {
     }
 
-    // Migration: callback-based createContainerIfNotExists -> async containerClient.createIfNotExists()
     private async _ensureContainerExistence(): Promise<void> {
-        if (!this._isContainerExists) {
+        if (!this._containerExists) {
             try {
                 await this._containerClient.createIfNotExists();
-                this._isContainerExists = true;
+                this._containerExists = true;
                 console.log(tl.loc("CreatedContainer", this._container));
             } catch (error) {
                 console.log(tl.loc("FailedToCreateContainer", this._container, error.message));
@@ -92,23 +84,18 @@ export class AzureBlobProvider implements models.IArtifactProvider {
         }
     }
 
-    // Migration: listBlobsSegmentedWithPrefix with continuationToken -> listBlobsFlat() async iterator
     private async _getItems(container: string, parentRelativePath?: string): Promise<models.ArtifactItem[]> {
         var items: models.ArtifactItem[] = [];
         
-        // Migration: Use PagedAsyncIterableIterator instead of manual pagination with continuationToken
         const listOptions = parentRelativePath ? { prefix: parentRelativePath } : {};
         
         for await (const blob of this._containerClient.listBlobsFlat(listOptions)) {
             var artifactitem: models.ArtifactItem = new models.ArtifactItem();
             artifactitem.itemType = models.ItemType.File;
-            // Migration: blob.properties.contentLength instead of parseInt(element.contentLength)
             artifactitem.fileLength = blob.properties.contentLength || 0;
-            // Migration: blob.properties.lastModified is already a Date
             artifactitem.lastModified = blob.properties.lastModified || new Date();
             
             if (!this._addPrefixToDownloadedItems && !!this._prefixFolderPath) {
-                // Supplying relative path without prefix; removing the first occurence
                 artifactitem.path = blob.name.replace(this._prefixFolderPath, "").trim();
             } else {
                 artifactitem.path = blob.name;
@@ -124,9 +111,7 @@ export class AzureBlobProvider implements models.IArtifactProvider {
     private _accessKey: string;
     private _container: string;
     private _prefixFolderPath: string;
-    private _isContainerExists: boolean = false;
-    // Migration: BlobService -> BlobServiceClient + ContainerClient
-    private _blobServiceClient: BlobServiceClient;
+    private _containerExists: boolean = false;
     private _containerClient: ContainerClient;
     private _addPrefixToDownloadedItems: boolean = false;
 }
