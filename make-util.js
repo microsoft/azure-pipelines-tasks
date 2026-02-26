5d9536a688eae7d5e51de70d80497002d7f500e8
sanjays-ms <v-sanjayse@microsoft.com>
2026-02-26 18:05:19 +0530

    update node 24 minor version and change to usenode task in pipeline

--- make-util.js
+++ make-util.js
@@ -410,7 +410,7 @@ exports.ensureTool = ensureTool;
 const node20Version = '20.20.0';
 exports.node20Version = node20Version;
 
-const node24Version = '24.13.0';
+const node24Version = '24.14.0';
 exports.node24Version = node24Version;
 
 var installNodeAsync = async function (nodeVersion) {
