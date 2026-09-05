901b56528f052cca9f240674170af9720a477c7b
sanjays-ms <v-sanjayse@microsoft.com>
2026-09-05 20:01:10 +0530

    update test cases for MSBuildV1 task

---
+++ Tasks/MSBuildV1/Tests/L0MSBuildMultipleSlnxSolutions.ts
@@ -0,0 +1,49 @@
+import ma = require('azure-pipelines-task-lib/mock-answer');
+import tmrm = require('azure-pipelines-task-lib/mock-run');
+import path = require('path');
+
+let taskPath = path.join(__dirname, '..', 'msbuild.js');
+let tr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+
+tr.setInput('solution', '**/*.slnx');
+tr.setInput('platform', '$(Platform)');
+tr.setInput('configuration', '$(Configuration)');
+tr.setInput('clean', 'true');
+tr.setInput('msbuildArguments', '/p:TestProp=TestValue /p:TestProp1=TestValue');
+
+// provide answers for task mock
+let a: ma.TaskLibAnswers = <ma.TaskLibAnswers>{
+    "which": {
+        "xbuild": "/home/bin/xbuild"
+    },
+    "checkPath": {
+        "/home/bin/xbuild": true
+    },
+    "findMatch": {
+        "**/*.slnx": [
+            "/user/build/fun.slnx",
+            "/user/build/test/fun.slnx"
+        ]
+    },
+    "exec": {
+        "/home/bin/xbuild /user/build/fun.slnx /t:Clean /p:Platform=$(Platform) /p:Configuration=$(Configuration) /p:TestProp=TestValue /p:TestProp1=TestValue": {
+            "code": 0,
+            "stdout": "xbuild output here"
+        },
+        "/home/bin/xbuild /user/build/test/fun.slnx /t:Clean /p:Platform=$(Platform) /p:Configuration=$(Configuration) /p:TestProp=TestValue /p:TestProp1=TestValue": {
+            "code": 0,
+            "stdout": "xbuild output here"
+        },
+        "/home/bin/xbuild /user/build/fun.slnx /p:Platform=$(Platform) /p:Configuration=$(Configuration) /p:TestProp=TestValue /p:TestProp1=TestValue": {
+            "code": 0,
+            "stdout": "xbuild output here"
+        },
+        "/home/bin/xbuild /user/build/test/fun.slnx /p:Platform=$(Platform) /p:Configuration=$(Configuration) /p:TestProp=TestValue /p:TestProp1=TestValue": {
+            "code": 0,
+            "stdout": "xbuild output here"
+        }
+    }
+};
+tr.setAnswers(a);
+
+tr.run();
