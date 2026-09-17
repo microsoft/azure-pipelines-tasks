e63698ac7500f90fde4ae18262cc09a5928d9bbc
Shaurya Parashar <shaparashar@microsoft.com>
2026-09-17 17:36:29 +0530

    Add more Jenkins logs under filtering :

--- Tasks/JenkinsQueueJobV2/jenkinsqueuejobtask.ts
+++ Tasks/JenkinsQueueJobV2/jenkinsqueuejobtask.ts
@@ -58,7 +58,7 @@ export class TaskOptions {
     constructor() {
         this.serverEndpoint = tl.getInput('serverEndpoint', true);
         this.serverEndpointUrl = tl.getEndpointUrl(this.serverEndpoint, false);
-        tl.debug('serverEndpointUrl=' + this.serverEndpointUrl);
+        tl.debugExternalOutput('serverEndpointUrl=' + this.serverEndpointUrl, { source: 'remote' });
         this.serverEndpointAuth = tl.getEndpointAuthorization(this.serverEndpoint, false);
         this.username = this.serverEndpointAuth['parameters']['username'];
         this.password = this.serverEndpointAuth['parameters']['password'];
@@ -84,11 +84,11 @@ export class TaskOptions {
         this.failOnUnstableResult = tl.getBoolInput('failOnUnstableResult', false);
 
         this.jobQueueUrl = util.addUrlSegment(this.serverEndpointUrl, util.convertJobName(this.jobName)) + ((this.parameterizedJob) ? '/buildWithParameters?delay=0sec' : '/build?delay=0sec');
-        tl.debug('jobQueueUrl=' + this.jobQueueUrl);
+        tl.debugExternalOutput('jobQueueUrl=' + this.jobQueueUrl, { source: 'remote' });
         this.teamJobQueueUrl = util.addUrlSegment(this.serverEndpointUrl, '/team-build/' + ((this.parameterizedJob) ? 'buildWithParameters/' : 'build/') + this.jobName + '?delay=0sec');
-        tl.debug('teamJobQueueUrl=' + this.teamJobQueueUrl);
+        tl.debugExternalOutput('teamJobQueueUrl=' + this.teamJobQueueUrl, { source: 'remote' });
         this.teamPluginUrl = util.addUrlSegment(this.serverEndpointUrl, '/pluginManager/available');
-        tl.debug('teamPluginUrl=' + this.teamPluginUrl);
+        tl.debugExternalOutput('teamPluginUrl=' + this.teamPluginUrl, { source: 'remote' });
 
         this.teamBuildPluginAvailable = false;
         // 'Build.StagingDirectory' is available during build.
@@ -133,10 +133,10 @@ async function doWork() {
             tl.writeExternalOutput(String(e.body) + os.EOL, { source: 'remote', destination: process.stderr });
         } else if (e instanceof Error) {
             message = e.message;
-            console.error(e);
+            tl.writeExternalOutput(String(e) + os.EOL, { source: 'remote', destination: process.stderr });
         } else {
             message = e;
-            console.error(e);
+            tl.writeExternalOutput(String(e) + os.EOL, { source: 'remote', destination: process.stderr });
         }
         tl.setResult(tl.TaskResult.Failed, message);
     }
--- Tasks/JenkinsQueueJobV2/jobqueue.ts
+++ Tasks/JenkinsQueueJobV2/jobqueue.ts
@@ -49,7 +49,7 @@ export class JobQueue {
                     this.FlushJobConsolesSafely();
                 }
             } catch (err) {
-                tl.debug(err.message);
+                tl.debugExternalOutput(String(err.message), { source: 'remote' });
                 tl.setResult(tl.TaskResult.Failed, err.message);
                 this.stop(false);
             }
@@ -296,7 +296,7 @@ export class JobQueue {
         const thisQueue: JobQueue = this;
         const tempDir: string = shell.tempdir();
         const linkMarkdownFile: string = path.join(tempDir, 'JenkinsJob_' + this.RootJob.Name + '_' + this.RootJob.ExecutableNumber + '.md');
-        tl.debug('markdown location: ' + linkMarkdownFile);
+        tl.debugExternalOutput('markdown location: ' + linkMarkdownFile, { source: 'remote' });
         const tab: string = '  ';
         const paddingTab: number = 4;
         generateMarkdownContent(this.RootJob, thisQueue.TaskOptions, (markdownContents) => {
@@ -305,9 +305,12 @@ export class JobQueue {
 
                 if (err) {
                     //don't fail the build -- there just won't be a link
-                    console.log('Error creating link to Jenkins job: ' + err);
+                    tl.writeExternalOutput('Error creating link to Jenkins job: ' + String(err) + os.EOL, { source: 'remote' });
                 } else {
-                    console.log('##vso[task.addattachment type=Distributedtask.Core.Summary;name=Jenkins Results;]' + linkMarkdownFile);
+                    tl.command('task.addattachment', {
+                        type: 'Distributedtask.Core.Summary',
+                        name: 'Jenkins Results'
+                    }, linkMarkdownFile);
                 }
 
                 let message: string = null;
