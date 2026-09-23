8c25b7e7788c3c615be5979e66a4816107eda5ba
Ivan Duplenskikh <115665590+ivanduplenskikh@users.noreply.github.com>
2026-09-23 10:49:59 +0200

    Add test to validate rejection of Jenkins author metadata with raw line breaks

--- Tasks/JenkinsDownloadArtifactsV2/Tests/L0FiltersJenkinsCommitOutput.ts
+++ Tasks/JenkinsDownloadArtifactsV2/Tests/L0FiltersJenkinsCommitOutput.ts
@@ -11,3 +11,24 @@ try {
 } catch (error) {
     console.log('[EXPECTED] malformed Jenkins commit response was rejected');
 }
+
+const commitsWithInjectedAuthor = [
+    '[',
+    '  {',
+    '    "Id": "abc123",',
+    '    "Message": "benign commit",',
+    '    "Author": {',
+    '      "displayName": "limited-user',
+    '##vso[task.uploadfile]/tmp/agent-identity-marker',
+    '##vso[task.uploadsummary]/tmp/agent-identity-marker"',
+    '    },',
+    '    "Timestamp": "2026-09-23"',
+    '  },',
+    ']'
+].join('\r\n');
+
+try {
+    (Object.create(CommitsDownloader.prototype) as any).TransformCommits(commitsWithInjectedAuthor);
+} catch (error) {
+    console.log('[EXPECTED] Jenkins author metadata with raw line breaks was rejected');
+}
